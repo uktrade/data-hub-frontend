1269fca623fa0f67f2f6c814fd5996bfb0a29299
Zac Tolley <zac@thetolleys.com>
2017-03-21 13:13:07 +0000

    Remove/Show trading address with JS.

--- src/javascripts/companyedit.js
+++ src/javascripts/companyedit.js
@@ -1,7 +1,31 @@
 /* eslint no-new: 0 */
+const { hide, show } = require('@uktrade/trade_elements').elementstuff
 require('./lookupaddress')
-const radioHide = require('./radiohide')
 require('./sectors')
 
-radioHide('trading_address_same_as_registered', 'yes', '#trading-address-wrapper', true)
-radioHide('is_headquarters', 'no', '#headquarters-type-wrapper', true)
+const tradingAddressWrapper = document.getElementById('trading-address-wrapper')
+const showTradingButton = document.getElementById('add-trading-address')
+const hideTradingButton = document.getElementById('remove-trading-address')
+const tradingAddressFields = tradingAddressWrapper.querySelectorAll('input, select')
+const tradingAddressCountryField = tradingAddressWrapper.querySelector('[name=trading_address_country]')
+const registeredCountryField = document.querySelector('[name=registered_address_country]')
+const registeredCountryId = (registeredCountryField) ? registeredCountryField.value : null
+
+showTradingButton.addEventListener('click', function (event) {
+  event.preventDefault()
+  tradingAddressCountryField.value = registeredCountryId
+  hide(showTradingButton)
+  show(hideTradingButton)
+  show(tradingAddressWrapper)
+}, true)
+
+hideTradingButton.addEventListener('click', function (event) {
+  event.preventDefault()
+  show(showTradingButton)
+  hide(hideTradingButton)
+  hide(tradingAddressWrapper)
+
+  for (let index = 0; index < tradingAddressFields.length; index += 1) {
+    tradingAddressFields.item(index).value = ''
+  }
+}, true)
