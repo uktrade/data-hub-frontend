078f6844d4dadc430ef5c6264afd010ab79fa403
jimsmith <jim.smith@digital.trade.gov.uk>
2017-05-05 17:02:56 +0100

    v2 error testing part 1

--- test/lib/controllerutils.test.js
+++ test/lib/controllerutils.test.js
@@ -1,6 +1,6 @@
 /* globals expect: true, describe: true, it: true */
 /* eslint no-unused-expressions: 0 */
-const { isBlank } = require('../../src/lib/controllerutils')
+const { isBlank, transformV2Errors } = require('../../src/lib/controllerutils')
 
 describe('isBlank', function () {
   it('should detects undefined variables', function () {
@@ -22,5 +22,21 @@ describe('isBlank', function () {
   })
   it('should know when it is sent a valid object', function () {
     expect(isBlank({x: 1})).to.be.false
+  })
+})
+
+describe('transformV2Errors: Formatting V2 service delivery endpoint errors', function () {
+  it('Should warn if the Service Delivery triple does not exist', function () {
+    const source = [
+      {
+        'detail': 'This combination of service and service provider does not exist.',
+        'source': {
+          'pointer': '/data/relationships/service'
+        }
+      }
+    ]
+    const actual = transformV2Errors(source)
+    expect(actual.Alert).to.exist
+    expect(actual.Alert).to.equal('This combination of service and service provider does not exist.')
   })
 })
