c35e05dfd3754a2dc7cd9fb0eace52966ffcd9b8
jimsmith <jim.smith@digital.trade.gov.uk>
2017-05-08 10:48:20 +0100

    v2 error testing part 2

--- test/lib/controllerutils.test.js
+++ test/lib/controllerutils.test.js
@@ -39,4 +39,85 @@ describe('transformV2Errors: Formatting V2 service delivery endpoint errors', fu
     expect(actual.Alert).to.exist
     expect(actual.Alert).to.equal('This combination of service and service provider does not exist.')
   })
+  it('Should return multiple errors when presented with an array of errors', function () {
+    const source = [
+      {
+        'detail': 'Required',
+        'source': {
+          'pointer': '/data/attributes/subject'
+        }
+      },
+      {
+        'detail': 'Required',
+        'source': {
+          'pointer': '/data/attributes/notes'
+        }
+      },
+      {
+        'detail': "{'data': {'type': 'ServiceDeliveryStatus'}} has no key id",
+        'source': {
+          'pointer': '/data/relationships/status'
+        }
+      },
+      {
+        'detail': "{'data': {'type': 'Contact'}} has no key id",
+        'source': {
+          'pointer': '/data/relationships/contact'
+        }
+      },
+      {
+        'detail': "{'data': {'type': 'Service'}} has no key id",
+        'source': {
+          'pointer': '/data/relationships/service'
+        }
+      },
+      {
+        'detail': "{'data': {'type': 'Team'}} has no key id",
+        'source': {
+          'pointer': '/data/relationships/dit_team'
+        }
+      },
+      {
+        'detail': "{'data': {'type': 'Sector'}} has no key id",
+        'source': {
+          'pointer': '/data/relationships/sector'
+        }
+      },
+      {
+        'detail': "{'data': {'type': 'UKRegion'}} has no key id",
+        'source': {
+          'pointer': '/data/relationships/uk_region'
+        }
+      },
+      {
+        'detail': "{'data': {'type': 'Country'}} has no key id",
+        'source': {
+          'pointer': '/data/relationships/country_of_interest'
+        }
+      }
+    ]
+    const actual = transformV2Errors(source)
+    expect((Object.keys(actual)).length).to.equal(9)
+    expect(actual.subject).to.be.defined
+    expect(actual.notes).to.be.defined
+    expect(actual.status).to.be.defined
+    expect(actual.contact).to.be.defined
+    expect(actual.service).to.be.defined
+    expect(actual.dit_team).to.be.defined
+    expect(actual.sector).to.be.defined
+    expect(actual.uk_region).to.be.defined
+    expect(actual.country_of_interest).to.be.defined
+  })
+  it('Should match keys not specially defined', function () {
+    const source = [
+      {
+        'detail': "{'data': {'type': 'Foo'}} has no key id",
+        'source': {
+          'pointer': '/data/relationships/foo'
+        }
+      }
+    ]
+    const actual = transformV2Errors(source)
+    expect(actual.foo).to.be.defined
+  })
 })
