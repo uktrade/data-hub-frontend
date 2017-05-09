929189e73aed151bff116a57cbc416ff01e9a364
Zac Tolley <zac@thetolleys.com>
2017-05-09 17:54:06 +0100

    Add a controller to handle archive company

---
+++ src/controllers/companyarchivecontroller.js
@@ -0,0 +1,47 @@
+/* eslint new-cap: 0 */
+const express = require('express')
+const winston = require('winston')
+const Q = require('q')
+const companyRepository = require('../repositorys/companyrepository')
+const companyService = require('../services/companyservice')
+const router = express.Router()
+
+function archiveCompany (req, res, next) {
+  Q.spawn(function * () {
+    try {
+      const company = yield companyRepository.getDitCompany(req.session.token, req.params.id)
+      const url = companyService.getViewCompanyLink(company)
+
+      if (!req.body.reason || req.body.reason.length === 0) {
+        return res.redirect(`${url}?error=Provide+a+reason`)
+      }
+
+      yield companyRepository.archiveCompany(req.session.token, company.id, req.body.reason)
+      req.flash('success-message', 'Updated company record')
+      res.redirect(url)
+    } catch (error) {
+      winston.error(error)
+      next(error)
+    }
+  })
+}
+
+function unArchiveCompany (req, res, next) {
+  Q.spawn(function * () {
+    try {
+      const company = yield companyRepository.getDitCompany(req.session.token, req.params.id)
+      const url = companyService.getViewCompanyLink(company)
+      yield companyRepository.unArchiveCompany(req.session.token, company.id)
+      req.flash('success-message', 'Updated company record')
+      res.redirect(url)
+    } catch (error) {
+      winston.error(error)
+      next(error)
+    }
+  })
+}
+
+router.post('/company/archive/:id', archiveCompany)
+router.get('/company/unarchive/:id', unArchiveCompany)
+
+module.exports = { router, archiveCompany }
