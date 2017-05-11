23dce82060fb0d8fd3153503256562445bf94ea2
Zac Tolley <zac@thetolleys.com>
2017-05-11 21:29:25 +0100

    Add archive contact feature and fix contact controller bug introduced by mistake.

--- src/controllers/contactcontroller.js
+++ src/controllers/contactcontroller.js
@@ -2,19 +2,28 @@ const express = require('express')
 const winston = require('winston')
 const Q = require('q')
 
+const { genCSRF } = require('../lib/controllerutils')
 const contactRepository = require('../repositorys/contactrepository')
 const contactFormattingService = require('../services/contactformattingservice')
 const companyService = require('../services/companyservice')
 const { contactDetailsLabels } = require('../labels/contactlabels')
 const router = express.Router()
 
+const reasonForArchiveOptions = [
+  'Contact has left the company',
+  'Contact does not want to be contacted',
+  'Contact changed role/responsibility'
+]
+
 function getCommon (req, res, next) {
   return new Promise((resolve, reject) => {
     Q.spawn(function * () {
       try {
         res.locals.id = req.params.contactId
-        res.locals.contact = yield contactRepository.getContact(req.session.token, req.params.id)
+        res.locals.contact = yield contactRepository.getContact(req.session.token, req.params.contactId)
         res.locals.companyUrl = companyService.getViewCompanyLink(res.locals.contact.company)
+        res.locals.reasonForArchiveOptions = reasonForArchiveOptions
+        genCSRF(req, res)
         next()
       } catch (error) {
         winston.error(error)
