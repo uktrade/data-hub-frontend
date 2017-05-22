58a7ff1a2ed67eed1947bf5d0f923234b05c2a9c
Tyom Semonov <inspiro@gmail.com>
2017-05-22 11:38:15 +0100

    Improve Investment details page

--- src/controllers/investment-details.controller.js
+++ src/controllers/investment-details.controller.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const Q = require('q')
+const moment = require('moment')
 
+const {buildCompanyUrl} = require('../services/company.service')
 const {
   getInvestmentProjectSummary,
   getInvestmentValue,
@@ -17,35 +19,73 @@ const localNavItems = [
   { label: 'Audit history', slug: 'audit' }
 ]
 
+function formatProjectStatusData (data) {
+  return {
+    id: data.id,
+    name: data.name,
+    projectCode: data.project_code,
+    phaseName: data.phase.name
+  }
+}
+
+function formatProjectData (data) {
+  return {
+    'Client': {
+      name: data.investor_company.name,
+      url: buildCompanyUrl(data.investor_company)
+    },
+    'Type of investment': data.investment_type.name,
+    'Primary sector': data.sector,
+    'Sub-sector': null,
+    'Business activity': data.business_activity,
+    'Project description': data.description,
+    'Non-disclosure agreement': data.nda_signed ? 'Signed' : 'Not signed',
+    'Shareable with UK partners': null,
+    'Anonymous description': null,
+    'Estimated land date': data.estimated_land_date ? moment(data.estimated_land_date).format('MMMM YYYY') : null
+  }
+}
+
+function formatValueData (data) {
+  return {
+    'Total investment': data.total_investment,
+    'Foreign equity investment': data.foreign_equity_investment,
+    'Government assistance': data.government_assistance,
+    'New jobs': data.number_new_jobs,
+    'Average salary': data.average_salary,
+    'Safeguarded jobs': data.number_safeguarded_jobs,
+    'R&D budget': data.r_and_d_budget,
+    'Non-FDI R&D project': data.non_fdi_r_and_d_budget,
+    'New-to-world tech': data.new_tech_to_uk,
+    'Export revenue': data.export_revenue
+  }
+}
+
+function formatRequirementsData (data) {
+  return {
+    'Main strategic drivers': data.strategic_drivers,
+    'Client requirements': data.client_requirements,
+    'Competitor countries': data.competitor_countries,
+    'Possible UK locations': data.uk_region_locations,
+    'Investment location': null,
+    'UK recipient company': data.uk_company
+  }
+}
+
 function getDetails (req, res, next) {
   Q.spawn(function * () {
     try {
-      const project = yield getInvestmentProjectSummary(req.session.token, req.params.id)
-      const value = yield getInvestmentValue(req.session.token, req.params.id)
-      const requirements = yield getInvestmentRequirements(req.session.token, req.params.id)
+      const projectData = yield getInvestmentProjectSummary(req.session.token, req.params.id)
+      const valueData = yield getInvestmentValue(req.session.token, req.params.id)
+      const requirementsData = yield getInvestmentRequirements(req.session.token, req.params.id)
 
       res.render('investment/details', {
-        project,
-        value,
-        requirements,
-        localNavItems,
-        currentNavItem: 'details'
-      })
-    } catch (error) {
-      next(error)
-    }
-  })
-}
-
-function editDetails (req, res, next) {
-  Q.spawn(function * () {
-    try {
-      const project = yield getInvestmentProjectSummary(req.session.token, req.params.id)
-
-      res.render('investment/details-edit', {
-        project,
-        localNavItems,
-        tab: 'project-details'
+        currentNavItem: 'details',
+        projectMeta: formatProjectStatusData(projectData),
+        project: formatProjectData(projectData),
+        value: formatValueData(valueData),
+        requirements: formatRequirementsData(requirementsData),
+        localNavItems
       })
     } catch (error) {
       next(error)
@@ -59,6 +99,5 @@ function redirectToDetails (req, res) {
 
 router.get('/investment/:id', redirectToDetails)
 router.get('/investment/:id/details', getDetails)
-router.get('/investment/:id/details/edit', editDetails)
 
 module.exports = { router, getDetails }
