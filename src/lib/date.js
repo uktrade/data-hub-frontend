e557d9b6677e1db15ed0f54b467d22c14e203f8b
Dom Smith <dom@teneightfive.com>
2017-06-09 13:18:37 +0100

    Add ability to sort a data-table component
    This adds the javascript functionality to support tables being
    sorted by a column.
    
    Original commit: ztolley

--- src/lib/date.js
+++ src/lib/date.js
@@ -1,20 +1,37 @@
 const { longDateFormat, mediumDateFormat } = require('../config')
-const moment = require('moment')
+const format = require('date-fns/format')
 
-function formatLongDate (date) {
-  if (!date) {
-    return null
+function formatLongDate (dateString = []) {
+  if (dateString) {
+    return format(parseDateString(dateString), longDateFormat)
   }
-  return moment(date).format(longDateFormat)
+
+  return null
 }
 
-function formatMediumDate (date) {
-  if (!date) {
-    return null
+function formatMediumDate (dateString = []) {
+  if (dateString) {
+    return format(parseDateString(dateString), mediumDateFormat)
   }
-  return moment(date).format(mediumDateFormat)
+
+  return null
+}
+
+function parseDateString (dateString) {
+  if (dateString.indexOf('/') !== -1) {
+    const parts = dateString.split('/')
+    return new Date(parts[2], parts[1] - 1, parts[0])
+  } else {
+    const date = new Date(dateString)
+    if (date.toString() !== 'Invalid Date') {
+      return date
+    }
+  }
+  return null
 }
 
 module.exports = {
-  formatLongDate, formatMediumDate,
+  formatLongDate,
+  formatMediumDate,
+  parseDateString,
 }
