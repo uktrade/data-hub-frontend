756d3d751fd83367fb8a785e2ec103ae2c861e00
Tyom Semonov <inspiro@gmail.com>
2017-06-13 15:02:57 +0100

    Add rudimentary support for checkboxes

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,7 +1,9 @@
 const ConditionalSubfields = require('./modules/conditional-subfields')
 const SortableTable = require('./modules/sortable-table')
 const DetailsList = require('./modules/details-list')
+const LabelSelect = require('./modules/label-select')
 
+LabelSelect.init()
 ConditionalSubfields.init()
 SortableTable.init()
 DetailsList.init()
