2f069491c94e7afc69b0687112c751f54aeae7ad
Venu <venureddy95>
2017-06-21 17:40:45 +0100

    Add new tests to create new contact

---
+++ test/acceptance/features/create-a-contact.feature
@@ -0,0 +1,30 @@
+@contact-create
+Feature: Create New Contact
+  As a existing user
+  I would like to create a new contact for a company
+
+  Scenario: Verify Add new Contact option
+    Given I am an authenticated user on Data Hub website
+    When I navigate to Contacts page of any company
+    Then I verify an option to add a new contact
+
+  Scenario: Add a new Primary Contact
+    Given I am an authenticated user on Data Hub website
+    When I add a new Primary Contact
+    Then I verify my newly added contact in company profile
+
+  Scenario: Add a new Primary Contact with new Company address
+    Given I am an authenticated user on Data Hub website
+    When I add a new Primary Contact with a new company address
+    Then I verify my newly added contact in company profile
+
+  Scenario: Add a new non Primary Contact
+    Given I am an authenticated user on Data Hub website
+    When I add a new non Primary Contact
+    Then I verify my newly added contact in company profile
+
+  Scenario: Verify newly added contact under search landing page
+    Given I am an authenticated user on Data Hub website
+    When I add a new Primary Contact
+    Then I verify my newly added contact under search landing page
+
---
+++ test/acceptance/page_objects/Contact.js
@@ -0,0 +1,149 @@
+// const shortId = require('shortid')
+// const { getRandomNumber } = require('../../helper')
+const faker = require('faker')
+
+module.exports = {
+  url: process.env.QA_HOST,
+  props: {},
+  elements: {
+    contactsTab: {
+      selector: "//nav/a[contains(@href, 'contacts')]",
+      locateStrategy: 'xpath',
+    },
+    firstCompanyFromList: {
+      selector: ".//*[@id='main-content']/div/article/ol/li[1]/h3/a",
+      locateStrategy: 'xpath',
+    },
+    addContactButton: '#add-contact-link',
+    noContactWarning: '#no-contact-warning',
+    contactFirstname: '#first_name',
+    contactLastname: '#last_name',
+    contactJobTitle: '#job_title',
+    contactPrimaryContactYes: {
+      selector: ".//*[@id='primary-wrapper']/label[1]/input",
+      locateStrategy: 'xpath',
+    },
+    contactPrimaryContactNo: {
+      selector: ".//*[@id='primary-wrapper']/label[2]/input",
+      locateStrategy: 'xpath',
+    },
+    contactTelephoneCountryCode: '#telephone_countrycode',
+    contactTelephoneNumber: '#telephone_number',
+    contactEmailAddress: '#email',
+    contactSameAddressYes: '#address-same-radio',
+    contactSameAddressNo: {
+      selector: ".//*[@id='address_same_as_company-wrapper']/label[2]",
+      locateStrategy: 'xpath',
+    },
+    contactCountry: '#address_country',
+    contactAlternatePhonenumber: '#telephone_alternative',
+    contactAlternativeEmail: '#email_alternative',
+    contactNotes: '#notes',
+    contactCancelButton: {
+      selector: '//a[contains(@class, "button-cancel")]',
+      locateStrategy: 'xpath',
+    },
+    contactUnderSearchPage: {
+      selector: ".//*[@id='contacts-list']/li[1]",
+      locateStrategy: 'xpath',
+    },
+    contactFullname: {
+      selector: "//ul[@id='contact-list']/li[1]/div/a/span[2]",
+      locateStrategy: 'xpath',
+    },
+    submitButton: {
+      selector: "//button[@type='submit']",
+      locateStrategy: 'xpath',
+    },
+    ukPostcode: {
+      selector: ".//*[@id='address-wrapper']/div[2]/input",
+      locateStrategy: 'xpath',
+    },
+    findUkAddressButton: {
+      selector: ".//*[@id='address-wrapper']/div[2]/button",
+      locateStrategy: 'xpath',
+    },
+    selectUkAddressDropdown: {
+      selector: ".//*[@id='address-wrapper']/div[3]/select",
+      locateStrategy: 'xpath',
+    },
+    selectAnUkAddressFromList: '#address-wrapper div:nth-child(3) select option:nth-child(3)',
+    // selectAnUkAddressFromList: {
+    //   selector: ".//*[@id='address-wrapper']/div[3]/select/option[3]",
+    //   locateStrategy: 'xpath',
+    // },
+  },
+
+  commands: [
+    {
+      findCompany (name) {
+        return this
+          .setValue('#search', name)
+          .submitForm('@searchForm')
+      },
+
+      navigateToContactsPage () {
+        return this
+          .click('@firstCompanyFromList')
+          .click('@contactsTab')
+          // .api.pause()
+      },
+
+      createNewPrimaryContact (firstName, lastName) {
+        return this
+          .click('@addContactButton')
+          .setValue('@contactFirstname', firstName)
+          .setValue('@contactLastname', lastName)
+          .setValue('@contactJobTitle', faker.name.jobTitle())
+          .click('@contactPrimaryContactNo')
+          .setValue('@contactTelephoneCountryCode', faker.random.number())
+          .setValue('@contactTelephoneNumber', faker.phone.phoneNumberFormat())
+          .setValue('@contactEmailAddress', firstName + lastName + '@dit.gov.uk')
+          .setValue('@contactAlternatePhonenumber', '666555444')
+          .setValue('@contactAlternativeEmail', lastName + firstName + '@dit.gov.uk')
+          .setValue('@contactNotes', faker.name.jobDescriptor() + firstName)
+          .submitForm('form')
+      },
+
+      createNewNonPrimaryContact (firstName, lastName) {
+        return this
+          .click('@addContactButton')
+          .setValue('@contactFirstname', firstName)
+          .setValue('@contactLastname', lastName)
+          .setValue('@contactJobTitle', faker.name.jobTitle())
+          .click('@contactPrimaryContactYes')
+          .setValue('@contactTelephoneCountryCode', faker.random.number())
+          .setValue('@contactTelephoneNumber', faker.phone.phoneNumberFormat())
+          .setValue('@contactEmailAddress', firstName + lastName + '@dit.gov.uk')
+          .setValue('@contactAlternatePhonenumber', '666555444')
+          .setValue('@contactAlternativeEmail', lastName + firstName + '@dit.gov.uk')
+          .setValue('@contactNotes', faker.name.jobDescriptor() + firstName)
+          .api.pause()
+          .submitForm('form')
+      },
+
+      createNewPrimaryContactWithNewCompanyAddress (firstName, lastName) {
+        return this
+          .click('@addContactButton')
+          .setValue('@contactFirstname', firstName)
+          .setValue('@contactLastname', lastName)
+          .setValue('@contactJobTitle', faker.name.jobTitle())
+          .click('@contactPrimaryContactNo')
+          .setValue('@contactTelephoneCountryCode', faker.random.number())
+          .setValue('@contactTelephoneNumber', faker.phone.phoneNumberFormat())
+          .setValue('@contactEmailAddress', firstName + lastName + '@dit.gov.uk')
+          .click('@contactSameAddressNo')
+          .setValue('@ukPostcode', 'EC2Y 9AE')
+          .click('@findUkAddressButton')
+          .click('@selectUkAddressDropdown')
+          .waitForElementPresent('@selectAnUkAddressFromList', 5000)
+          .click('@selectAnUkAddressFromList')
+          // .api.keys(this.api.keys.TAB)
+          .setValue('@contactAlternatePhonenumber', '666555444')
+          .setValue('@contactAlternativeEmail', lastName + firstName + '@dit.gov.uk')
+          .setValue('@contactNotes', faker.name.jobDescriptor() + firstName)
+          .submitForm('form')
+      },
+    },
+  ],
+}
