2ee0133dcc66cdcb74489ed939e26a5f8fd33628
Dom Smith <dom@teneightfive.com>
2017-06-27 17:11:44 +0100

    Refactor user middleware
    Refactor the middleware to return early when cases match.

--- src/middleware/user.js
+++ src/middleware/user.js
@@ -5,23 +5,19 @@ module.exports = (req, res, next) => {
   const token = req.session.token
   const user = req.session.user
 
-  if (token && !user) {
-    authorisedRequest(token, `${config.apiRoot}/whoami/`)
-      .then((userInfo) => {
-        req.session.user = {
-          id: userInfo.id, // DIT Adviser id
-          name: userInfo.name,
-          team: userInfo.dit_team,
-        }
-
-        res.locals.user = req.session.user
-        next()
-      })
-      .catch(next)
-  } else {
-    if (user) {
-      res.locals.user = user
-    }
-    next()
+  if (user) {
+    res.locals.user = user
+    return next()
   }
+
+  if (!token) {
+    return next()
+  }
+
+  authorisedRequest(token, `${config.apiRoot}/whoami/`)
+    .then((user) => {
+      req.session.user = res.locals.user = user
+      next()
+    })
+    .catch(next)
 }
