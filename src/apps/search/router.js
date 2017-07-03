415891a57241b716903d531fc1cb42f3da70f8ea
Zac Tolley <zac@thetolleys.com>
2017-07-03 17:19:36 +0100

    Remove dependency on search controller to figure out which company url to use.

--- src/apps/search/router.js
+++ src/apps/search/router.js
@@ -4,11 +4,8 @@ const {
   indexAction,
   searchAction,
   searchActionValidationSchema,
-  viewCompanyResult,
 } = require('./controllers')
 
 router.get('/search/:searchType?', searchActionValidationSchema, indexAction, searchAction)
-
-router.get('/viewcompanyresult/:id', viewCompanyResult) // TODO is this in the right controller
 
 module.exports = router
