2415e9225d814014ece83717f6effb3d8bff5de6
Tyom Semonov <inspiro@gmail.com>
2017-07-04 10:42:53 +0100

    Refactor ErrorSummary component

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -5,6 +5,7 @@ const DetailsList = require('./modules/details-list')
 const LabelSelect = require('./modules/label-select')
 const ArchiveForm = require('./modules/archive-form')
 const Messages = require('./modules/messages')
+const FormErrors = require('./modules/form-errors')
 
 const AddAnotherField = require('./_deprecated/add-another-field')
 const CompanyAdd = require('./_deprecated/company-add')
@@ -19,6 +20,7 @@ SortableTable.init()
 DetailsList.init()
 ArchiveForm.init()
 Messages.init()
+FormErrors.init()
 
 // Deprecated
 AddAnotherField.init()
