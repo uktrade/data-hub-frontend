2fdcb05abcf40ea9df51a59e62e86a1e35fbf8cb
Tyom Semonov <inspiro@gmail.com>
2017-07-12 10:20:19 +0100

    Add Pagination component

--- src/lib/pagination.js
+++ src/lib/pagination.js
@@ -1,3 +1,4 @@
+const { range, take, uniq, get } = require('lodash')
 const { buildQueryString } = require('./url-helpers')
 
 const NEXTLABEL = 'Next'
@@ -72,11 +73,76 @@ function getPagination (req, result) {
 }
 
 function getPageLink (page, req) {
-  // Get the current params, remove the existing pages param and put in the desired
-  let params = req.query
-  delete params.page
-  params.page = page
-  return buildQueryString(params)
+  const query = Object.assign({}, req.query, {
+    page,
+  })
+  return buildQueryString(query)
 }
 
-module.exports = { getPageIndexes, getPagination, getPageLink }
+function truncatePages (pagination, blockSize) {
+  const pages = pagination.pages
+
+  if (!pages.length) { return pagination }
+
+  const currentPageNum = pagination.currentPage
+  const currentPageIndex = pagination.currentPage - 1
+  const firstPage = pages[0]
+  const lastPage = pages[pages.length - 1]
+  const truncationSymbol = '…'
+
+  const blockPivot = Math.round(blockSize / 2)
+  const startOfCurrentBlock = Math.abs(currentPageNum - blockPivot)
+  const startOfLastBlock = lastPage.label - blockSize
+  const blockStartIndex = Math.min(startOfCurrentBlock, startOfLastBlock, currentPageIndex)
+
+  let truncatedPages = take(pages.slice(blockStartIndex), blockSize)
+  truncatedPages.unshift(firstPage)
+  truncatedPages.push(lastPage)
+
+  truncatedPages = uniq(truncatedPages)
+
+  if (pagination.totalPages > blockSize + 1) {
+    // Add truncation after the first page number
+    if (currentPageNum > firstPage.label + blockPivot) {
+      truncatedPages.splice(1, 0, { label: truncationSymbol })
+    }
+    // Add truncation before the last page number
+    if (currentPageNum < lastPage.label - blockPivot) {
+      truncatedPages.splice(-1, 0, { label: truncationSymbol })
+    }
+  }
+
+  pagination.pages = truncatedPages
+
+  return pagination
+}
+
+function buildPagination (req, results, truncate = 4) {
+  const limit = results.limit || Math.max(get(results, 'results.length', 10), 10)
+  const totalPages = results.count ? Math.ceil(results.count / limit) : 0
+  results.page = parseInt(results.page, 10)
+
+  if (!results.page || totalPages < 2) { return null }
+
+  const pagination = {
+    totalPages,
+    currentPage: results.page,
+    prev: results.page > 1 ? getPageLink(results.page - 1, req) : null,
+    next: results.page === totalPages ? null : getPageLink(results.page + 1, req),
+    pages: range(0, totalPages).map((page, idx) => {
+      return {
+        label: idx + 1,
+        url: getPageLink(idx + 1, req),
+      }
+    }),
+  }
+
+  return truncatePages(pagination, truncate)
+}
+
+module.exports = {
+  getPageIndexes,
+  getPagination,
+  getPageLink,
+  buildPagination,
+}
--- test/unit/components/pagination.test.js
+++ test/unit/components/pagination.test.js
@@ -1,42 +1,95 @@
-const { renderComponentToDom } = require('../component-helper')
+const { getMacros } = require('~/test/unit/component-helper')
+const commonMacros = getMacros('common')
 
-describe('Pagination component', () => {
-  it('should not render if pagination array not provided', () => {
-    const component = renderComponentToDom(
-      'pagination',
-      {
-        pages: [],
-      }
-    )
-
-    expect(component).to.be.null
+describe('Nunjucks Pagination macro', () => {
+  describe('invalid props', () => {
+    it('should not render if pagination object is not given', () => {
+      const component = commonMacros.renderToDom('Pagination')
+      expect(component).to.be.null
+    })
   })
 
-  it('should render with correct pagination elements', () => {
-    const mockData = {
-      pages: [
-        {
-          label: 'example-label',
-          link: '?example-link',
-          currentPage: true,
-        },
-        {
-          label: 'example-label-1',
-          link: '?example-link-1',
-          currentPage: false,
-        },
-      ],
-    }
-    const component = renderComponentToDom(
-      'pagination',
-      mockData
-    )
+  describe('valid props', () => {
+    it('should render short pagination list without truncation', () => {
+      const paginationObj = {
+        totalPages: 2,
+        currentPage: 1,
+        prev: null,
+        next: '?term=samsung&page=2',
+        pages: [
+          { label: 1, url: '?term=samsung&page=1' },
+          { label: 2, url: '?term=samsung&page=2' },
+        ],
+      }
+      const component = commonMacros.renderToDom('Pagination', paginationObj)
 
-    const paginationItems = component.querySelectorAll('.pagination__item')
+      expect(component.querySelectorAll('.c-pagination__list-item')).to.have.lengthOf(2)
+      expect(component.querySelector('.c-pagination__label.is-current').textContent.trim()).to.equal('1')
+      expect(component.querySelector('.c-pagination__label--prev')).to.be.null
+      expect(component.querySelector('.c-pagination__label--next')).to.not.be.null
+    })
 
-    expect(component.className).to.equal('pagination')
-    expect(paginationItems[0].textContent).to.contain(mockData.pages[0].label)
-    expect(paginationItems[1].querySelector('a').href).to.equal(`${mockData.pages[1].link}`)
-    expect(paginationItems[1].textContent).to.contain(mockData.pages[1].label)
+    it('should render long pagination list with truncation', () => {
+      const paginationObj = {
+        totalPages: 5,
+        currentPage: 1,
+        prev: null,
+        next: '?term=samsung&page=2',
+        pages: [
+          { label: 1, url: '?term=samsung&page=1' },
+          { label: 2, url: '?term=samsung&page=2' },
+          { label: '…' },
+          { label: 5, url: '?term=samsung&page=5' },
+        ],
+      }
+      const component = commonMacros.renderToDom('Pagination', paginationObj)
+
+      expect(component.querySelectorAll('.c-pagination__list-item')).to.have.lengthOf(4)
+      expect(component.querySelector('.c-pagination__label--truncation').textContent.trim()).to.equal('…')
+      expect(component.querySelector('.c-pagination__label--prev')).to.be.null
+      expect(component.querySelector('.c-pagination__label--next')).to.not.be.null
+    })
+
+    it('should render long pagination list with truncation and set to different page', () => {
+      const paginationObj = {
+        totalPages: 5,
+        currentPage: 4,
+        prev: '?term=samsung&page=3',
+        next: '?term=samsung&page=5',
+        pages: [
+          { label: 1, url: '?term=samsung&page=1' },
+          { label: '…' },
+          { label: 4, url: '?term=samsung&page=4' },
+          { label: 5, url: '?term=samsung&page=5' },
+        ],
+      }
+      const component = commonMacros.renderToDom('Pagination', paginationObj)
+
+      expect(component.querySelectorAll('.c-pagination__list-item')).to.have.lengthOf(4)
+      expect(component.querySelector('.c-pagination__label--truncation').textContent.trim()).to.equal('…')
+      expect(component.querySelector('.c-pagination__label--prev')).to.not.be.null
+      expect(component.querySelector('.c-pagination__label--next')).to.not.be.null
+    })
+  })
+
+  describe('customise component', () => {
+    it('should render pagination without page number links', () => {
+      const paginationObj = {
+        totalPages: 2,
+        currentPage: 1,
+        prev: null,
+        next: '?term=samsung&page=2',
+        pages: [
+          { label: 1, url: '?term=samsung&page=1' },
+          { label: 2, url: '?term=samsung&page=2' },
+        ],
+      }
+
+      const component = commonMacros.renderToDom('Pagination', paginationObj, false)
+
+      expect(component.querySelectorAll('.c-pagination__list-item')).to.have.lengthOf(0)
+      expect(component.querySelector('.c-pagination__label--prev')).to.be.null
+      expect(component.querySelector('.c-pagination__label--next')).to.not.be.null
+    })
   })
 })
