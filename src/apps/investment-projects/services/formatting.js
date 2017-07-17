e53eab941bef2e18707655e3062644ee4def9f7b
Ben Chidgey <ben@feedmypixel.com>
2017-07-17 17:27:17 +0100

    filter any falsy values from address

--- src/apps/investment-projects/services/formatting.js
+++ src/apps/investment-projects/services/formatting.js
@@ -215,13 +215,13 @@ function transformInvestmentLandingForView (data) {
     uk_company: data.uk_company ? { name: data.uk_company.name, url: buildCompanyUrl(data.uk_company) } : null,
     company_number: get(data, 'uk_company.company_number'),
     registered_address: data.uk_company ? [
-      get(data, 'uk_company.registered_address_1'),
-      get(data, 'uk_company.registered_address_2'),
-      get(data, 'uk_company.registered_address_town'),
-      get(data, 'uk_company.registered_address_country.name'),
-      get(data, 'uk_company.registered_address_county'),
-      get(data, 'uk_company.registered_address_postcode'),
-    ] : null,
+      data.uk_company.registered_address_1,
+      data.uk_company.registered_address_2,
+      data.uk_company.registered_address_town,
+      data.uk_company.registered_address_country.name,
+      data.uk_company.registered_address_county,
+      data.uk_company.registered_address_postcode,
+    ].filter((address) => address) : null,
     investment_land_date: data.actual_land_date ? moment(data.actual_land_date).format('Do MMMM YYYY') : null,
   })
 }
