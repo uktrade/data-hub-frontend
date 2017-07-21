faac281b614fe191c1eacd13d34462f96cbaeced
Tyom Semonov <inspiro@gmail.com>
2017-07-21 13:17:43 +0100

    Refactor macro test helper to test callers

--- test/unit/component-helper.js
+++ test/unit/component-helper.js
@@ -28,17 +28,45 @@ const normaliseHtml = (string) => {
 function getMacros (fileName) {
   const filePath = `${MACROS_PATH}${fileName}.${EXT}`
 
+  function renderMacro (name, caller, props, ...rest) {
+    const importString = `{% from "${filePath}" import ${name} %}`
+    const args = rest.length ? `,${[...rest].map(JSON.stringify)}` : ''
+    let macroOutput
+
+    if (caller) {
+      macroOutput = nunjucks.renderString(`
+        ${importString}
+        {% call ${name}(${JSON.stringify(props, undefined, 1)}${args}) %}
+          ${caller.join(' ')}
+        {% endcall %}
+      `)
+    } else {
+      macroOutput = nunjucks.renderString(`${importString} {{ ${name}(${JSON.stringify(props)}${args}) }}`)
+    }
+
+    return normaliseHtml(macroOutput)
+  }
+
   return {
     render (macroName, props, ...rest) {
-      const importString = `{% from "${filePath}" import ${macroName} %}`
-      const args = rest.length ? `,${[...rest].map(JSON.stringify)}` : ''
-      const macroOutput = nunjucks.renderString(`${importString} {{ ${macroName}(${JSON.stringify(props)}${args}) }}`)
-      return normaliseHtml(macroOutput)
+      return renderMacro(macroName, null, props, ...rest)
+    },
+
+    renderWithCaller (macroName, caller, props, ...rest) {
+      return renderMacro(macroName, caller, props, ...rest)
     },
 
     renderToDom (macroName, props, ...rest) {
       const macroOutput = this.render(macroName, props, ...rest)
       return (new JSDOM(macroOutput)).window.document.body.firstElementChild
+    },
+
+    renderWithCallerToDom (macroName, props, ...rest) {
+      return function (...caller) {
+        if (!caller.length) { return null }
+        const macroOutput = this.renderWithCaller(macroName, caller, props, ...rest)
+        return (new JSDOM(macroOutput)).window.document.body.firstElementChild
+      }.bind(this)
     },
   }
 }
