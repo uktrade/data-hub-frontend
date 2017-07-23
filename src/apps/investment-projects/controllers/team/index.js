8d35de59e8a5422f5371fcf2f018ff04a34cb3cb
Zac Tolley <zac@thetolleys.com>
2017-07-23 22:49:59 +0100

    Allow user to edit client relationship manager in investment project

--- src/apps/investment-projects/controllers/team/index.js
+++ src/apps/investment-projects/controllers/team/index.js
@@ -1,7 +1,9 @@
 const details = require('./details')
+const editClientRelationshipManagement = require('./edit-client-relationship-management')
 const editProjectManagement = require('./edit-project-management')
 
 module.exports = {
   details,
+  editClientRelationshipManagement,
   editProjectManagement,
 }
---
+++ src/apps/investment-projects/middleware/forms/client-relationship-management.js
@@ -0,0 +1,63 @@
+const { get } = require('lodash')
+const { getAdvisers } = require('../../../adviser/repos')
+const { updateCompany } = require('../../../companies/repos')
+const { updateInvestment } = require('../../repos')
+const { clientRelationshipManagementLabels } = require('../../labels')
+
+async function populateForm (req, res, next) {
+  try {
+    const investmentData = res.locals.investmentData
+
+    const advisersResponse = await getAdvisers(req.session.token)
+    const advisers = advisersResponse.results.map((adviser) => {
+      return {
+        value: adviser.id,
+        label: `${adviser.first_name} ${adviser.last_name}`,
+      }
+    })
+
+    res.locals.form = Object.assign({}, res.locals.form, {
+      labels: clientRelationshipManagementLabels.edit,
+      state: {
+        client_relationship_manager: get(investmentData, 'client_relationship_manager.id', null),
+        account_manager: get(investmentData, 'investor_company.account_manager.id', null),
+      },
+      options: {
+        advisers,
+      },
+      hiddenFields: {
+        investor_company: get(investmentData, 'investor_company.id'),
+      },
+      buttonText: 'Save',
+      returnLink: `/investment-projects/${investmentData.id}/team`,
+    })
+
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
+
+async function handleFormPost (req, res, next) {
+  try {
+    res.locals.projectId = req.params.id
+    await updateCompany(req.session.token, req.body.investor_company, { account_manager: req.body.account_manager })
+    await updateInvestment(req.session.token, res.locals.projectId, { client_relationship_manager: req.body.client_relationship_manager })
+    next()
+  } catch (err) {
+    if (err.statusCode === 400) {
+      res.locals.form = Object.assign({}, res.locals.form, {
+        errors: err.error,
+        state: req.body,
+      })
+      next()
+    } else {
+      next(err)
+    }
+  }
+}
+
+module.exports = {
+  populateForm,
+  handleFormPost,
+}
