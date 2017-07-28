0af2d1efbaee25d3ecb2c33d2216c1588b7637ce
jimsmith <jim.smith@digital.trade.gov.uk>
2017-07-28 11:34:38 +0100

    Created api/advisersearch route to fix issue with interactions

--- src/apps/api/index.js
+++ src/apps/api/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router()
-const { postcodeLookupHandler } = require('./controllers')
+const { adviserSearchHandler, postcodeLookupHandler } = require('./controllers')
+
+router.get('/adviserlookup/:term', adviserSearchHandler)
+router.get('/postcodelookup/:postcode', postcodeLookupHandler)
 
 module.exports = {
   mountpath: '/api',
-  router: router.get('/postcodelookup/:postcode', postcodeLookupHandler),
+  router,
 }
