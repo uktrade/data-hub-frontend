5340626c90baae082da7b976ba5f69f88c7696c2
Tyom Semonov <inspiro@gmail.com>
2017-07-31 12:25:24 +0100

    Refactor apps loader to use map instead of forEach

--- src/apps/routers.js
+++ src/apps/routers.js
@@ -3,19 +3,17 @@ const fs = require('fs')
 
 const { setHomeBreadcrumb } = require('./middleware')
 
-const appsRouters = []
 const subApps = fs.readdirSync(__dirname)
 
-subApps.forEach(subAppDir => {
+const appsRouters = subApps.map(subAppDir => {
   const subApp = require(`./${subAppDir}`)
 
   if (subApp.mountpath) {
-    appsRouters.push(router.use(subApp.mountpath, setHomeBreadcrumb(subApp.displayName), subApp.router))
+    return router.use(subApp.mountpath, setHomeBreadcrumb(subApp.displayName), subApp.router)
   } else if (subApp.router) {
-    appsRouters.push(router.use(subApp.router))
-  } else {
-    appsRouters.push((req, res, next) => next())
+    return router.use(subApp.router)
   }
+  return (req, res, next) => next()
 })
 
 module.exports = appsRouters
