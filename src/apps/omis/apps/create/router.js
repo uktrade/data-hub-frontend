c95b471019237b655bbbeb947b865c54051a533e
Dom Smith <dom@teneightfive.com>
2017-08-02 15:01:26 +0100

    Move i18n locales to OMIS sub-app level
    This allows translations to be used by other sub-apps within OMIS.

--- src/apps/omis/apps/create/router.js
+++ src/apps/omis/apps/create/router.js
@@ -4,7 +4,7 @@ const wizard = require('hmpo-form-wizard')
 const i18nFuture = require('i18n-future')
 
 const i18n = i18nFuture({
-  path: path.resolve(__dirname, './locales/__lng__/__ns__.json'),
+  path: path.resolve(__dirname, '../../locales/__lng__/__ns__.json'),
 })
 const steps = require('./steps')
 const fields = require('./fields')
