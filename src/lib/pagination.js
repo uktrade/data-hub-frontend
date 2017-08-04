f813ef3f2c45ff40766dc4a4516a5c9013891a6f
Tyom Semonov <inspiro@gmail.com>
2017-08-04 12:27:06 +0100

    Avoid mutating query in `getPageLink`

--- src/lib/pagination.js
+++ src/lib/pagination.js
@@ -2,8 +2,10 @@ const { range, take, get, omitBy } = require('lodash')
 const { buildQueryString } = require('./url-helpers')
 
 function getPageLink (page, query = {}) {
-  query.page = page
-  return buildQueryString(omitBy(query, val => val === ''))
+  const newQuery = Object.assign({}, query, {
+    page,
+  })
+  return buildQueryString(omitBy(newQuery, val => val === ''))
 }
 
 function truncatePages (pagination, blockSize) {
