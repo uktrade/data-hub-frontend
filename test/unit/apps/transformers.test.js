df0868dbf8428c199ad88fbe72ac3b3646f3e15d
Tyom Semonov <inspiro@gmail.com>
2017-08-11 19:23:40 +0100

    Update tests

--- test/unit/apps/transformers.test.js
+++ test/unit/apps/transformers.test.js
@@ -51,4 +51,98 @@ describe('Global transformers', () => {
       })
     })
   })
+
+  describe('transformFieldsObjectToMacrosObject()', () => {
+    it('should return empty array with no args', () => {
+      const actual = transformers.transformFieldsObjectToMacrosObject()
+
+      expect(actual).to.be.an('array').with.length(0)
+    })
+
+    it('should return an array of macro configuration objects from object', () => {
+      const actual = transformers.transformFieldsObjectToMacrosObject({
+        stage: {
+          macroName: 'MultipleChoiceField',
+          type: 'checkbox',
+          value: 'prospect',
+        },
+      })
+
+      expect(actual).to.deep.equal([
+        {
+          MultipleChoiceField:
+            { name: 'stage', type: 'checkbox', value: 'prospect' },
+        },
+      ])
+    })
+
+    it('should return an array of macro configuration objects from array', () => {
+      const actual = transformers.transformFieldsObjectToMacrosObject([
+        {
+          macroName: 'MultipleChoiceField',
+          type: 'checkbox',
+          name: 'stage',
+          value: 'prospect',
+        },
+        {
+          macroName: 'MetaItem',
+          type: 'date',
+          name: 'dob',
+          value: '2017-08-11',
+        },
+      ])
+
+      expect(actual).to.deep.equal([
+        {
+          MultipleChoiceField:
+            { name: 'stage', type: 'checkbox', value: 'prospect' },
+        },
+        {
+          MetaItem:
+            { name: 'dob', type: 'date', value: '2017-08-11' },
+        },
+      ])
+    })
+
+    it('should return a assign sharedProps', () => {
+      const actual = transformers.transformFieldsObjectToMacrosObject([
+        {
+          macroName: 'MultipleChoiceField',
+          type: 'checkbox',
+          name: 'stage',
+          value: 'prospect',
+        },
+        {
+          macroName: 'MetaItem',
+          type: 'date',
+          name: 'dob',
+          value: '2017-08-11',
+        },
+      ], {
+        shared: 'prop',
+        modifier: 'small',
+      })
+
+      expect(actual).to.deep.equal([
+        {
+          MultipleChoiceField: {
+            name: 'stage',
+            type: 'checkbox',
+            value: 'prospect',
+            shared: 'prop',
+            modifier: 'small',
+          },
+        },
+        {
+          MetaItem: {
+            name: 'dob',
+            type: 'date',
+            value: '2017-08-11',
+            shared: 'prop',
+            modifier: 'small',
+          },
+        },
+      ])
+    })
+  })
 })
