6b4a44f9234910c277db13b554475fd284257dfe
Dom Smith <dom@teneightfive.com>
2017-08-16 07:53:30 +0100

    Move add another functionality to base form controller
    This allows the add another functionality to be shared between create
    and edit.

--- src/apps/omis/controllers/form.js
+++ src/apps/omis/controllers/form.js
@@ -1,4 +1,4 @@
-const { get, last, mapValues } = require('lodash')
+const { get, filter, flatten, forEach, last, mapValues } = require('lodash')
 const { Controller } = require('hmpo-form-wizard')
 
 class FormController extends Controller {
@@ -10,6 +10,44 @@ class FormController extends Controller {
     })
 
     return errors
+  }
+
+  process (req, res, next) {
+    const addKey = req.body['add-item']
+    const removeKey = req.body['remove-item']
+
+    if (addKey || removeKey) {
+      if (addKey) {
+        req.form.values[addKey] = flatten([req.form.values[addKey]])
+        // add empty value to array to show add a new add adviser input
+        req.form.values[addKey].push('')
+      }
+
+      if (removeKey) {
+        const [ key, index ] = removeKey.split('::')
+        req.form.values[key].splice(index, 1)
+      }
+
+      return this.saveValues(req, res, () => {
+        res.redirect(req.baseUrl + req.path)
+      })
+    }
+
+    next()
+  }
+
+  saveValues (req, res, next) {
+    if (!req.body['add-item'] && !req.body['remove-item']) {
+      const fields = req.form.options.fields
+
+      forEach(fields, (options, key) => {
+        if (options.repeatable) {
+          req.form.values[key] = filter(flatten([req.form.values[key]]))
+        }
+      })
+    }
+
+    super.saveValues(req, res, next)
   }
 
   errorHandler (err, req, res, next) {
