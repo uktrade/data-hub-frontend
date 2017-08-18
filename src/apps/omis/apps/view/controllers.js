7bff71ae2b057fc2c688bb5854cf3453f70e66a5
Dom Smith <dom@teneightfive.com>
2017-08-18 13:08:46 +0200

    Display assignee estimated time in work order
    This adds a custom body for post advisers (assignees) to display
    their estimated hours.

--- src/apps/omis/apps/view/controllers.js
+++ src/apps/omis/apps/view/controllers.js
@@ -1,6 +1,14 @@
+const { sumBy } = require('lodash')
+
 function renderWorkOrder (req, res, next) {
+  const order = res.locals.order
+
+  const values = Object.assign({}, order, {
+    estimatedTimeSum: sumBy(order.assignees, 'estimated_time'),
+  })
+
   res.render('omis/apps/view/views/work-order', {
-    values: res.locals.order,
+    values,
   })
 }
 
