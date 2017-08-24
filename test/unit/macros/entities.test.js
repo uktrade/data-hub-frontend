657ca9b81ca9e4016ded62b191006791158db895
Zac Tolley <zac@thetolleys.com>
2017-08-24 21:44:43 +0100

    Allow entity macro titles without links and override metalist modifiers
    Change the entity macro so that it only makes the entity title a link
    if an id is passed to it, otherwise make it a plain title.
    
    Add an optional parameter to the entity macro 'contentMetaModifier' to
    allow an alternative modifier to applied to the metalist macro call
    to allow alernative layouts to be requested when generating entity
    collections.

--- test/unit/macros/entities.test.js
+++ test/unit/macros/entities.test.js
@@ -46,6 +46,46 @@ describe('Entities macros', () => {
         expect(component.querySelector('.c-entity__content .c-meta-list__item').textContent.replace(/\s+/g, ' ').trim())
           .to.equal('DOB: 10 November 2015')
       })
+
+      it('should render a title without a link if no id passed', () => {
+        const component = entitiesMacros.renderToDom('Entity', {
+          name: 'Horse',
+          type: 'animal',
+        })
+
+        expect(component.querySelector('.c-entity__title').textContent.replace(/\s+/g, ' ').trim())
+          .to.equal('Horse')
+        expect(component.querySelector('.c-entity__title a')).not.to.exist
+      })
+
+      it('should use a default modifier of inline split it no modifier specified', () => {
+        const component = entitiesMacros.renderToDom('Entity', {
+          id: '12345',
+          name: 'Horse',
+          type: 'animal',
+          meta: [
+            { label: 'Colour', value: 'brown', type: 'badge' },
+            { label: 'DOB', value: '2015-11-10', type: 'date', name: 'date_of_birth' },
+          ],
+        })
+        expect(component.querySelector('.c-meta-list.c-meta-list--split.c-meta-list--inline')).to.exist
+      })
+
+      it('should use a content meta modifier if specified', () => {
+        const component = entitiesMacros.renderToDom('Entity', {
+          id: '12345',
+          name: 'Horse',
+          type: 'animal',
+          contentMetaModifier: 'test',
+          meta: [
+            { label: 'Colour', value: 'brown', type: 'badge' },
+            { label: 'DOB', value: '2015-11-10', type: 'date', name: 'date_of_birth' },
+          ],
+        })
+
+        expect(component.querySelector('.c-meta-list.c-meta-list--split.c-meta-list--inline')).not.to.exist
+        expect(component.querySelector('.c-meta-list.c-meta-list--test')).to.exist
+      })
     })
   })
 
