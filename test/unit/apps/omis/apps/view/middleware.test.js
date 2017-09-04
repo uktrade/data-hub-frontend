33b08c02c98e392d0cdad2e3630d5671985f7570
Dom Smith <dom@teneightfive.com>
2017-09-04 15:27:40 +0100

    Add ability to generate a quote for an OMIS order
    This adds the controller and middleware logic to handle generating
    a quote from an order.
    
    Has some basic error handling on API responses but more detailed
    user errors will come later.

--- test/unit/apps/omis/apps/view/middleware.test.js
+++ test/unit/apps/omis/apps/view/middleware.test.js
@@ -5,6 +5,9 @@ describe('OMIS View middleware', () => {
     this.setHomeBreadcrumbReturnSpy = this.sandbox.spy()
     this.setHomeBreadcrumbStub = this.sandbox.stub().returns(this.setHomeBreadcrumbReturnSpy)
     this.getQuoteStub = this.sandbox.stub()
+    this.createQuoteStub = this.sandbox.stub()
+    this.flashSpy = this.sandbox.spy()
+    this.nextSpy = this.sandbox.spy()
 
     this.resMock = {
       locals: {
@@ -14,6 +17,12 @@ describe('OMIS View middleware', () => {
         },
       },
     }
+    this.reqMock = {
+      session: {
+        token: '12345',
+      },
+      flash: this.flashSpy,
+    }
 
     this.middleware = proxyquire('~/src/apps/omis/apps/view/middleware', {
       '../../../middleware': {
@@ -22,6 +31,7 @@ describe('OMIS View middleware', () => {
       '../../models': {
         Order: {
           getQuote: this.getQuoteStub,
+          createQuote: this.createQuoteStub,
         },
       },
     })
@@ -33,28 +43,18 @@ describe('OMIS View middleware', () => {
 
   describe('setOrderBreadcrumb()', () => {
     it('should call setHomeBreadcrumb with order reference', () => {
-      const nextSpy = this.sandbox.spy()
-
-      this.middleware.setOrderBreadcrumb({}, this.resMock, nextSpy)
+      this.middleware.setOrderBreadcrumb({}, this.resMock, this.nextSpy)
 
       expect(this.setHomeBreadcrumbStub).to.have.been.calledOnce
       expect(this.setHomeBreadcrumbStub).to.have.been.calledWith('12345/AS')
 
       expect(this.setHomeBreadcrumbReturnSpy).to.have.been.calledOnce
-      expect(this.setHomeBreadcrumbReturnSpy).to.have.been.calledWith({}, this.resMock, nextSpy)
+      expect(this.setHomeBreadcrumbReturnSpy).to.have.been.calledWith({}, this.resMock, this.nextSpy)
     })
   })
 
   describe('getQuote()', () => {
-    beforeEach(() => {
-      this.reqMock = {
-        session: {
-          token: '12345',
-        },
-      }
-    })
-
-    context('when Order.getQoute resolves', () => {
+    context('when Order.getQuote resolves', () => {
       beforeEach(() => {
         this.getQuoteStub.resolves({
           id: '12345',
@@ -122,6 +122,126 @@ describe('OMIS View middleware', () => {
 
         this.middleware.getQuote(this.reqMock, this.resMock, nextSpy)
       })
+    })
+  })
+
+  describe('generateQuote()', () => {
+    context('when Order.createQuote resolves', () => {
+      beforeEach(() => {
+        this.createQuoteStub.resolves({
+          created_on: '2017-08-31T15:10:41.119609',
+          created_by: {
+            'name': 'Rebecca Bah',
+            'id': '3cfad090-8f7e-4a8b-beb0-14c909d6f052',
+          },
+        })
+      })
+
+      it('should redirect back to order page', (done) => {
+        const resMock = Object.assign({}, this.resMock, {
+          redirect: (path) => {
+            try {
+              expect(this.nextSpy).not.to.have.been.called
+              expect(this.flashSpy).to.have.been.calledOnce
+              expect(path).to.equal('/omis/123456789')
+              done()
+            } catch (error) {
+              done(error)
+            }
+          },
+        })
+
+        this.middleware.generateQuote(this.reqMock, resMock, this.nextSpy)
+      })
+    })
+
+    context('when Order.createQuote returns a 400', () => {
+      beforeEach(() => {
+        const error = {
+          statusCode: 400,
+        }
+        this.createQuoteStub.rejects(error)
+      })
+
+      it('should set flash and redirect', (done) => {
+        const resMock = Object.assign({}, this.resMock, {
+          redirect: (path) => {
+            try {
+              expect(this.nextSpy).not.to.have.been.called
+              expect(this.flashSpy).to.have.been.calledOnce
+              expect(path).to.equal('/omis/123456789')
+              done()
+            } catch (error) {
+              done(error)
+            }
+          },
+        })
+
+        this.middleware.generateQuote(this.reqMock, resMock, this.nextSpy)
+      })
+    })
+
+    context('when Order.createQuote returns a 409', () => {
+      beforeEach(() => {
+        const error = {
+          statusCode: 409,
+        }
+        this.createQuoteStub.rejects(error)
+      })
+
+      it('should set flash and redirect', (done) => {
+        const resMock = Object.assign({}, this.resMock, {
+          redirect: (path) => {
+            try {
+              expect(this.nextSpy).not.to.have.been.called
+              expect(this.flashSpy).to.have.been.calledOnce
+              expect(path).to.equal('/omis/123456789')
+              done()
+            } catch (error) {
+              done(error)
+            }
+          },
+        })
+
+        this.middleware.generateQuote(this.reqMock, resMock, this.nextSpy)
+      })
+    })
+
+    context('when Order.createQuote returns any other error', () => {
+      beforeEach(() => {
+        this.error = new Error('Server error')
+        this.error.statusCode = 500
+
+        this.createQuoteStub.rejects(this.error)
+      })
+
+      it('should set an empty quote object on locals object', (done) => {
+        const nextSpy = (error) => {
+          try {
+            expect(error).to.deep.equal(this.error)
+            done()
+          } catch (error) {
+            done(error)
+          }
+        }
+
+        this.middleware.generateQuote(this.reqMock, this.resMock, nextSpy)
+      })
+    })
+  })
+
+  describe('setQuoteForm()', () => {
+    it('should set quoteForm on locals object', (done) => {
+      const nextSpy = () => {
+        try {
+          expect(this.resMock.locals).to.have.property('quoteForm')
+          done()
+        } catch (error) {
+          done(error)
+        }
+      }
+
+      this.middleware.setQuoteForm({}, this.resMock, nextSpy)
     })
   })
 })
