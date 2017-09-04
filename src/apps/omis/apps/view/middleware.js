58aa187d146fb60d1d74e3dd1c787dfdd70a7909
Dom Smith <dom@teneightfive.com>
2017-09-04 15:18:10 +0100

    Add initial route for OMIS quote preview
    This adds a placeholder view and route for the preview and generate
    quote logic that will follow.

--- src/apps/omis/apps/view/middleware.js
+++ src/apps/omis/apps/view/middleware.js
@@ -1,4 +1,5 @@
 const { setHomeBreadcrumb } = require('../../../middleware')
+const { Order } = require('../../models')
 
 function setOrderBreadcrumb (req, res, next) {
   const order = res.locals.order
@@ -6,6 +7,21 @@ function setOrderBreadcrumb (req, res, next) {
   return setHomeBreadcrumb(order.reference)(req, res, next)
 }
 
+async function getQuote (req, res, next) {
+  try {
+    res.locals.quote = await Order.getQuote(req.session.token, res.locals.order.id)
+    next()
+  } catch (error) {
+    if (error.statusCode === 404) {
+      res.locals.quote = {}
+      return next()
+    }
+
+    next(error)
+  }
+}
+
 module.exports = {
   setOrderBreadcrumb,
+  getQuote,
 }
