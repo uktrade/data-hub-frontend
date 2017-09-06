d0badcac1a12703969a5da3ec0a3718f59f38157
Dom Smith <dom@teneightfive.com>
2017-09-06 16:43:50 +0100

    Add OMIS translation support to view app

--- src/apps/omis/apps/view/router.js
+++ src/apps/omis/apps/view/router.js
@@ -1,8 +1,15 @@
 const router = require('express').Router()
 
 const { setLocalNav, redirectToFirstNavItem } = require('../../../middleware')
-const { setOrderBreadcrumb, getQuote, setQuoteForm, generateQuote, cancelQuote } = require('./middleware')
 const { renderWorkOrder, renderQuote } = require('./controllers')
+const {
+  setOrderBreadcrumb,
+  setTranslation,
+  getQuote,
+  setQuoteForm,
+  generateQuote,
+  cancelQuote,
+} = require('./middleware')
 
 const LOCAL_NAV = [
   { path: 'work-order', label: 'Work order' },
@@ -12,6 +19,7 @@ const LOCAL_NAV = [
 ]
 
 router.use(setLocalNav(LOCAL_NAV))
+router.use(setTranslation)
 router.use(setOrderBreadcrumb)
 
 router.get('/', redirectToFirstNavItem)
