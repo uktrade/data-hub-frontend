a86011ee20f369406145922cb96761167114b386
Zac Tolley <zac@thetolleys.com>
2017-09-08 14:03:44 +0100

    fixup! Add interaction transformer and add interactions to search results

--- src/apps/search/services.js
+++ src/apps/search/services.js
@@ -18,6 +18,13 @@ const entities = [
     count: 0,
   },
   {
+    entity: 'interaction',
+    path: 'interactions',
+    text: 'Interactions',
+    noun: 'interaction',
+    count: 0,
+  },
+  {
     entity: 'investment_project',
     path: 'investment-projects',
     text: 'Investment projects',
@@ -29,13 +36,6 @@ const entities = [
     path: 'omis',
     text: 'Orders',
     noun: 'order',
-    count: 0,
-  },
-  {
-    entity: 'interaction',
-    path: 'interactions',
-    text: 'Interactions',
-    noun: 'interaction',
     count: 0,
   },
 ]
