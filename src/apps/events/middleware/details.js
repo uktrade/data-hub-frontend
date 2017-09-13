ca99da59537eeb9954388df705b52ea470b3f851
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2017-09-13 15:23:18 +0100

    Add middleware for event POST to API

---
+++ src/apps/events/middleware/details.js
@@ -0,0 +1,40 @@
+const { transformToApi } = require('../services/formatting')
+const { createEvent } = require('../repos')
+
+async function populateForm (req, res, next) {
+  // todo
+}
+
+async function handleFormPost (req, res, next) {
+  const formattedBody = transformToApi(Object.assign({}, req.body))
+
+  try {
+    await createEvent(req.session.token, formattedBody)
+
+    res.locals.resultId = '1'
+
+    next()
+  } catch (err) {
+    if (err.statusCode === 400) {
+      res.locals.form = Object.assign({}, res.locals.form, {
+        state: req.body,
+        errors: {
+          messages: err.error,
+        },
+      })
+      next()
+    } else {
+      next(err)
+    }
+  }
+}
+
+function validateForm (req, res, next) {
+  // todo
+}
+
+module.exports = {
+  populateForm,
+  handleFormPost,
+  validateForm,
+}
