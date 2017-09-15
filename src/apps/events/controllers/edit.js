2c057baf609398841dc4fabfadc9f41f4605ba54
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2017-09-15 12:18:11 +0100

    Add setting of teams and related programmes before POST to API

--- src/apps/events/controllers/edit.js
+++ src/apps/events/controllers/edit.js
@@ -2,7 +2,7 @@ const { eventFormConfig } = require('../macros')
 const { getAdvisers } = require('../../adviser/repos')
 const { transformObjectToOption } = require('../../transformers')
 const { buildFormWithState } = require('../../builders')
-const { get, castArray, compact } = require('lodash')
+const { get } = require('lodash')
 
 async function renderEventPage (req, res, next) {
   try {
@@ -29,14 +29,6 @@ async function renderEventPage (req, res, next) {
 }
 
 function postHandler (req, res, next) {
-  const setAddAnotherField = (value) => compact(castArray(value))
-  req.body.teams = setAddAnotherField(req.body.teams)
-  req.body.related_programmes = setAddAnotherField(req.body.related_programmes)
-
-  if (req.body.add_team || req.body.add_related_programme) {
-    return next()
-  }
-
   if (get(res.locals, 'form.errors')) {
     return next()
   }
--- src/apps/events/middleware/details.js
+++ src/apps/events/middleware/details.js
@@ -1,8 +1,21 @@
 const { transformToApi } = require('../services/formatting')
 const { createEvent } = require('../repos')
+const { includes, assign, castArray, compact } = require('lodash')
 
 async function handleFormPost (req, res, next) {
-  const formattedBody = transformToApi(Object.assign({}, req.body))
+  const castToArrayAndRemoveEmpty = (value) => compact(castArray(value))
+  req.body.teams = castToArrayAndRemoveEmpty(req.body.teams)
+  req.body.related_programmes = castToArrayAndRemoveEmpty(req.body.related_programmes)
+
+  if (!includes(req.body.teams, req.body.lead_team)) {
+    req.body.teams.push(req.body.lead_team)
+  }
+
+  if (req.body.add_team || req.body.add_related_programme) {
+    return next()
+  }
+
+  const formattedBody = transformToApi(assign({}, req.body))
 
   try {
     const result = await createEvent(req.session.token, formattedBody)
@@ -12,7 +25,7 @@ async function handleFormPost (req, res, next) {
     next()
   } catch (err) {
     if (err.statusCode === 400) {
-      res.locals.form = Object.assign({}, res.locals.form, {
+      res.locals.form = assign({}, res.locals.form, {
         state: req.body,
         errors: {
           messages: err.error,
