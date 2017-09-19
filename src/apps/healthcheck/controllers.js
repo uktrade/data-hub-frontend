01f1d5d1c939b648adab7a5aa6f79d918330d6c5
Ben Chidgey <ben@feedmypixel.com>
2017-09-19 11:08:58 +0100

    add pingdom xml route to healthcheck

--- src/apps/healthcheck/controllers.js
+++ src/apps/healthcheck/controllers.js
@@ -1,6 +1,15 @@
 const logger = require('../../../config/logger')
 const serviceDependencies = require('./serviceDependencies')
 
+function pingdomTemplate (statusMessage) {
+  return `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <pingdom_http_custom_check>
+      <status>${statusMessage}</status>
+    </pingdom_http_custom_check>
+  `.trim()
+}
+
 function healthCheck (dependencies) {
   const promiseArray = dependencies.map((dependency) => {
     return dependency.healthCheck()
@@ -11,6 +20,32 @@ function healthCheck (dependencies) {
   })
 
   return Promise.all(promiseArray)
+}
+
+function renderPingdomXml (req, res, next) {
+  return healthCheck(serviceDependencies)
+    .then((results) => results.filter((result) => result.statusText !== 'OK'))
+    .then((errors) => {
+      res.set({
+        'Content-Type': 'text/xml',
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+      })
+
+      if (errors.length) {
+        errors.forEach((dependency) => {
+          logger.error(`${dependency.name} health check failed`, dependency.error)
+        })
+
+        return res
+          .status(503)
+          .send(pingdomTemplate('Service Unavailable'))
+      }
+
+      return res
+        .status(200)
+        .send(pingdomTemplate('OK'))
+    })
+    .catch(next)
 }
 
 function getHandler (req, res, next) {
@@ -24,14 +59,19 @@ function getHandler (req, res, next) {
           logger.error(`${dependency.name} health check failed`, dependency.error)
         })
 
-        return res.status(503).send('Service Unavailable')
+        return res
+          .status(503)
+          .send('Service Unavailable')
       }
 
-      return res.status(200).send('OK')
+      return res
+        .status(200)
+        .send('OK')
     })
     .catch(next)
 }
 
 module.exports = {
   getHandler,
+  renderPingdomXml,
 }
