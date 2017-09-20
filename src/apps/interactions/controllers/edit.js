a8c94ae2a09f2e7357fe2f7e76c44fde32c541bf
Tyom Semonov <inspiro@gmail.com>
2017-09-20 23:52:11 +0100

    Refactor interactions to use new form macros

--- src/apps/interactions/controllers/edit.js
+++ src/apps/interactions/controllers/edit.js
@@ -1,67 +1,56 @@
 /* eslint camelcase: 0 */
-const interactionLabels = require('../labels')
-const contactsRepository = require('../../contacts/repos')
-const companyRepository = require('../../companies/repos')
+const { get } = require('lodash')
+const { getContactsForCompany } = require('../../contacts/repos')
 const metadataRepository = require('../../../lib/metadata')
-const adviserRepository = require('../../adviser/repos')
-const interactionDataService = require('../services/data')
-const interactionFormService = require('../services/form')
-const { containsFormData } = require('../../../lib/controller-utils')
-const { transformContactToOption } = require('../../transformers')
+const { getAllAdvisers } = require('../../adviser/repos')
+const { createBlankInteractionForCompany, createBlankInteractionForContact } = require('../services/data')
+const { saveInteractionForm } = require('../services/form')
+const { transformInteractionResponseToForm } = require('../transformers')
+const { interactionEditFormConfig } = require('../macros')
+const { buildFormWithStateAndErrors } = require('../../builders')
 
 async function editDetails (req, res, next) {
+  const token = req.session.token
+  const defaultDitAdviser = req.session.user
+
+  let returnLink = `/interactions/${get(res.locals, 'interaction.id')}`
+  let interaction = res.locals.interaction
+
   try {
-    const token = req.session.token
-    const default_dit_adviser = req.session.user
-
-    // Work out what to use for the form data
-    // This can either be data recently posted, to be re-rendered with errors
-    // or an interaction that the user wishes to edit
-    // or a new interaction for a company or contact
-    if (containsFormData(req)) {
-      res.locals.formData = req.body
-      res.locals.company = await companyRepository.getDitCompany(token, req.body.company)
-      res.locals.interaction_type = interactionDataService.getInteractionType(req.body.interaction_type)
-    } else if (res.locals.interaction) {
-      res.locals.formData = interactionFormService.getInteractionAsFormData(res.locals.interaction)
-      res.locals.interaction_type = res.locals.interaction.interaction_type
-      res.locals.company = res.locals.interaction.company
-    } else if (req.query.company) {
-      res.locals.interaction = await interactionDataService.createBlankInteractionForCompany(token, default_dit_adviser, req.query.interaction_type, req.query.company)
-      res.locals.formData = interactionFormService.getInteractionAsFormData(res.locals.interaction)
-      res.locals.company = res.locals.interaction.company
-      res.locals.interaction_type = res.locals.interaction.interaction_type
-    } else if (req.query.contact) {
-      res.locals.interaction = await interactionDataService.createBlankInteractionForContact(token, default_dit_adviser, req.query.interaction_type, req.query.contact)
-      res.locals.formData = interactionFormService.getInteractionAsFormData(res.locals.interaction)
-      res.locals.company = res.locals.interaction.company
-      res.locals.interaction_type = res.locals.interaction.interaction_type
-    } else {
-      return next('Unable to edit interaction')
+    if (req.query) {
+      if (req.query.company) {
+        interaction = await createBlankInteractionForCompany(token, defaultDitAdviser, req.query.interaction_type, req.query.company)
+        returnLink = `/companies/${req.query.company}/interactions`
+      }
+      if (req.query.contact) {
+        interaction = await createBlankInteractionForContact(token, defaultDitAdviser, req.query.interaction_type, req.query.contact)
+        returnLink = `/contacts/${req.query.contact}/interactions`
+      }
     }
 
-    if (req.query && req.query.company) {
-      res.locals.backUrl = `/companies/${req.query.company}/interactions`
-    } else if (req.query && req.query.contact) {
-      res.locals.backUrl = `/contacts/${req.query.contact}/interactions`
-    } else if (res.locals.interaction) {
-      res.locals.backUrl = `/interactions/${res.locals.interaction.id}`
-    }
+    const pageTitle = `${interaction.id ? 'Edit' : 'Add'} interaction for ${interaction.company.name}`
+    const formData = Object.assign({}, transformInteractionResponseToForm(interaction), req.body)
 
-    const companyContacts = await contactsRepository.getContactsForCompany(req.session.token, res.locals.formData.company)
-    res.locals.contacts = companyContacts.map(transformContactToOption)
+    res.locals.editInteractionForm = buildFormWithStateAndErrors(
+      interactionEditFormConfig({
+        returnLink,
+        contacts: await getContactsForCompany(token, formData.company),
+        advisers: await getAllAdvisers(token),
+        services: await metadataRepository.getServices(token),
+      }),
+      formData,
+      res.locals.errors,
+    )
 
-    if (res.locals.formData.dit_adviser) {
-      res.locals.dit_adviser = await adviserRepository.getAdviser(req.session.token, res.locals.formData.dit_adviser)
-    } else {
-      res.locals.dit_adviser = default_dit_adviser
-    }
+    res.locals.editInteractionForm.hiddenFields = Object.assign({}, res.locals.editInteractionForm.hiddenFields, {
+      interaction_type: formData.interaction_type,
+      company: formData.company,
+    })
 
-    res.locals.serviceOptions = await metadataRepository.getServices(token)
-    res.locals.serviceProviderOptions = metadataRepository.teams
-    res.locals.labels = interactionLabels
+    res.locals.interactionTypeLabel = interaction.interaction_type.name
+
     res
-      .breadcrumb(`Add interaction for ${res.locals.company.name}`)
+      .breadcrumb(pageTitle)
       .render('interactions/views/edit')
   } catch (error) {
     console.log(error)
@@ -71,19 +60,14 @@ async function editDetails (req, res, next) {
 
 async function postDetails (req, res, next) {
   try {
-    const result = await interactionFormService.saveInteractionForm(req.session.token, req.body)
+    const result = await saveInteractionForm(req.session.token, req.body)
     res.redirect(`/interactions/${result.id}`)
   } catch (errors) {
     if (errors.error) {
-      if (errors.error.errors) {
-        res.locals.errors = errors.error.errors
-      } else {
-        res.locals.errors = errors.error
-      }
-      editDetails(req, res, next)
-    } else {
-      next(errors)
+      res.locals.errors = errors.error.errors || errors.error
+      return editDetails(req, res, next)
     }
+    next(errors)
   }
 }
 
