6b532b3fcec845b35e53c83254159038aa3358a0
Dom Smith <dom@teneightfive.com>
2017-09-22 16:30:22 +0100

    Add company transformer to transform from API to form view

--- src/apps/companies/transformers.js
+++ src/apps/companies/transformers.js
@@ -1,5 +1,7 @@
 /* eslint-disable camelcase */
-const { assign, get } = require('lodash')
+const { assign, find, get, isPlainObject, mapValues } = require('lodash')
+
+const metadataRepository = require('../../lib/metadata')
 
 function transformCompanyToListItem ({
   id,
@@ -103,7 +105,42 @@ function transformCompaniesHouseCompanyToListItem (props) {
   })
 }
 
+// TODO: This is a temporary transformer to transform an API response into
+// a format needed for the form view
+//
+// Will be replaced with newer form builder transformers once the form view is
+// replaced with newer form macros
+function transformCompanyResponseToForm (body) {
+  if (!isPlainObject(body)) { return }
+
+  const schema = {
+    registered_address_country: String,
+    trading_address_country: String,
+    business_type: String,
+    headquarter_type: String,
+    uk_region: String,
+    sector: String,
+    employee_range: String,
+    turnover_range: String,
+  }
+
+  const formatted = mapValues(schema, (type, key) => {
+    return get(body, `${key}.id`)
+  })
+
+  if (body.company_category) {
+    const businessType = find(metadataRepository.businessTypeOptions, (type) => {
+      return type.name.toLowerCase() === body.company_category.toLowerCase()
+    })
+
+    formatted.business_type = get(businessType, 'id')
+  }
+
+  return assign({}, body, formatted)
+}
+
 module.exports = {
   transformCompanyToListItem,
   transformCompaniesHouseCompanyToListItem,
+  transformCompanyResponseToForm,
 }
