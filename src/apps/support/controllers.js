199b89a0a9d491ef20a62147a0fa49d2c68c3e8d
Tyom Semonov <inspiro@gmail.com>
2017-09-23 18:42:21 +0100

    Refactor feedback page to use new form builder
    Align the structure with patterns and get the benefit of improved error
    summary for free.

--- src/apps/support/controllers.js
+++ src/apps/support/controllers.js
@@ -1,7 +1,29 @@
+const { get, merge } = require('lodash')
+const { title } = require('case')
+const Sniffr = require('sniffr')
+const { feedbackFormConfig } = require('./macros')
+const { buildFormWithStateAndErrors } = require('../builders')
+
 function renderFeedbackPage (req, res) {
-  res.render('support/views/feedback', {
-    heading: 'Report a problem or leave feedback',
-  })
+  const sniffr = new Sniffr()
+  sniffr.sniff(req.headers['user-agent'])
+
+  const browserInfo = `${title(sniffr.browser.name)} ${sniffr.browser.versionString}, ${title(sniffr.os.name)} ${sniffr.os.versionString}`
+
+  const feedbackForm = buildFormWithStateAndErrors(
+    feedbackFormConfig(browserInfo),
+    req.body,
+    get(res.locals, 'formErrors.messages'),
+  )
+
+  res
+    .title('Report a problem or leave feedback')
+    .render('support/views/feedback', {
+      // TODO: Fix form builder to ensure it accepts the whole form object not just the messages
+      feedbackForm: merge(feedbackForm, {
+        errors: res.locals.formErrors,
+      }),
+    })
 }
 
 function renderThankYouPage (req, res) {
