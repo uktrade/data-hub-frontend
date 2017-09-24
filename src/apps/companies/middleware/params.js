56d9fc6817de749cc335035c86397002a1ba0097
Dom Smith <dom@teneightfive.com>
2017-09-24 20:55:09 +0100

    Extend company view params to set address for shared header

--- src/apps/companies/middleware/params.js
+++ src/apps/companies/middleware/params.js
@@ -1,8 +1,30 @@
+const { getFormattedAddress } = require('../../../lib/address')
 const { getDitCompany, getCHCompany } = require('../repos')
+
+// TODO: Use transformer to return correct address object and handle display
+// in view layer
+function getHeadingAddress (company) {
+  // If this is a CDMS company
+  const cdmsTradingAddress = getFormattedAddress(company, 'trading')
+  if (cdmsTradingAddress) {
+    return cdmsTradingAddress
+  }
+
+  if (company.companies_house_data && company.companies_house_data !== null) {
+    return getFormattedAddress(company.companies_house_data, 'registered')
+  }
+
+  return getFormattedAddress(company, 'registered')
+}
 
 async function getCompany (req, res, next, id) {
   try {
-    res.locals.company = await getDitCompany(req.session.token, id)
+    const company = await getDitCompany(req.session.token, id)
+
+    company.address = getHeadingAddress(company)
+
+    res.locals.company = company
+
     next()
   } catch (error) {
     next(error)
