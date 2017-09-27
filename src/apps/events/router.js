efb874adbfc6a8908871ea9eb53374da879be700
Ben Chidgey <ben@feedmypixel.com>
2017-09-27 17:13:17 +0100

    add organisers dropdown to event list filters

--- src/apps/events/router.js
+++ src/apps/events/router.js
@@ -3,7 +3,7 @@ const router = require('express').Router()
 const { setDefaultQuery } = require('../middleware')
 const { renderDetailsPage } = require('./controllers/details')
 const { renderEditPage } = require('./controllers/edit')
-const { postDetails, getEventDetails } = require('./middleware/details')
+const { postDetails, getEventDetails, getAdviserDetails } = require('./middleware/details')
 const { getRequestBody, getEventsCollection } = require('./middleware/collection')
 const { renderEventList } = require('./controllers/list')
 
@@ -15,14 +15,16 @@ router.param('id', getEventDetails)
 
 router.get('/',
   setDefaultQuery(DEFAULT_COLLECTION_QUERY),
+  getAdviserDetails,
   getRequestBody,
   getEventsCollection,
   renderEventList,
 )
 
 router.route(['/create', '/:id/edit'])
-  .post(postDetails)
-  .all(renderEditPage)
+  .all(getAdviserDetails)
+  .post(postDetails, renderEditPage)
+  .get(renderEditPage)
 
 router.get('/:id', renderDetailsPage)
 
