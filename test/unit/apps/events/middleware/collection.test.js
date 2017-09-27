800c3b9ab163fce0def6e5f26705a5e2e78e91c6
Tyom Semonov <inspiro@gmail.com>
2017-09-27 13:19:07 +0100

    Improve event collection unit tests

--- test/unit/apps/events/middleware/collection.test.js
+++ test/unit/apps/events/middleware/collection.test.js
@@ -1,22 +1,21 @@
 const { assign } = require('lodash')
-const nock = require('nock')
-const config = require('~/config')
 const eventCollectionData = require('~/test/unit/data/events/collection.json')
 
 describe('Event collection middleware', () => {
   beforeEach(() => {
-    nock(config.apiRoot)
-      .post(`/v3/search/event`)
-      .reply(200, eventCollectionData)
-
     this.sandbox = sinon.sandbox.create()
     this.next = this.sandbox.spy()
     this.req = assign({}, globalReq, {
       session: { token: 'abcd' },
     })
     this.res = assign({}, globalRes)
+    this.searchStub = this.sandbox.stub()
 
-    this.controller = require('~/src/apps/events/middleware/collection')
+    this.controller = proxyquire('~/src/apps/events/middleware/collection', {
+      '../../search/services': {
+        search: this.searchStub.resolves(eventCollectionData),
+      },
+    })
   })
 
   afterEach(() => {
@@ -24,20 +23,43 @@ describe('Event collection middleware', () => {
   })
 
   describe('#getEventsCollection', () => {
-    beforeEach(async () => {
+    beforeEach(() => {
       this.req.query = {
         sortby: 'modified_on:desc',
       }
-      await this.controller.getEventsCollection(this.req, this.res, this.next)
     })
 
-    it('should set results property on locals with pagination', () => {
-      const actual = this.res.locals.results
-      expect(actual).to.have.property('count')
-      expect(actual).to.have.property('items')
-      expect(actual).to.have.property('pagination')
-      expect(actual.count).to.equal(3)
-      expect(this.next).to.have.been.calledOnce
+    context('when success', () => {
+      it('should with correct options', async () => {
+        await this.controller.getEventsCollection(this.req, this.res, this.next)
+
+        expect(this.searchStub).to.have.been.calledWith(sinon.match({
+          searchEntity: 'event',
+          token: this.req.session.token,
+          isAggregation: false,
+        }))
+      })
+
+      it('should set results property on locals with pagination', async () => {
+        await this.controller.getEventsCollection(this.req, this.res, this.next)
+
+        const actual = this.res.locals.results
+        expect(actual).to.have.property('count')
+        expect(actual).to.have.property('items')
+        expect(actual).to.have.property('pagination')
+        expect(actual.count).to.equal(3)
+        expect(this.next).to.have.been.calledOnce
+      })
+    })
+
+    context('when error', () => {
+      it('should call next middleware with error', async () => {
+        const error = Error('nope')
+        this.searchStub.rejects(error)
+        await this.controller.getEventsCollection(this.req, this.res, this.next)
+
+        expect(this.next).to.have.been.calledWith(error)
+      })
     })
   })
 
