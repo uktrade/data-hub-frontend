d544dfece6239bffd715728e00be81f316775538
Tyom Semonov <inspiro@gmail.com>
2017-09-30 13:59:33 +0100

    Move “I submit the form” to Form step definitions

--- test/acceptance/features/form/step_definitions/form.js
+++ test/acceptance/features/form/step_definitions/form.js
@@ -1,8 +1,13 @@
 const { client } = require('nightwatch-cucumber')
 const { defineSupportCode } = require('cucumber')
 
-defineSupportCode(({ Then }) => {
+defineSupportCode(({ When, Then }) => {
   const Form = client.page.Form()
+
+  When(/^I submit the form$/, async () => {
+    await Form
+      .submitForm('form')
+  })
 
   Then(/^I see the error summary$/, async () => {
     await Form
