8dad79f1eec4e881fb0adb5697da4808006e4111
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2017-10-10 15:09:05 +0100

    Add interactions sub app router

---
+++ src/apps/interactions/router.sub-app.js
@@ -0,0 +1,37 @@
+const router = require('express').Router()
+
+const { renderEditPage } = require('./controllers/edit')
+const { renderDetailsPage } = require('./controllers/details')
+const { postCreate, renderCreate } = require('./controllers/create')
+
+const { postDetails, getInteractionOptions, getInteractionDetails } = require('./middleware/details')
+
+router.param('interactionId', getInteractionDetails)
+
+router
+  .route('/interactions/create')
+  .post(
+    postCreate,
+    renderCreate,
+  )
+  .get(
+    renderCreate,
+  )
+
+router.route([
+  '/interactions/create/interaction',
+  '/interactions/:interactionId/edit',
+])
+  .post(
+    getInteractionOptions,
+    postDetails,
+    renderEditPage,
+  )
+  .get(
+    getInteractionOptions,
+    renderEditPage,
+  )
+
+router.get('/interactions/:interactionId', renderDetailsPage)
+
+module.exports = router
