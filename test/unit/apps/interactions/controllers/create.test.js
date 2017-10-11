3c0950b6240b4ca3d25d1925d6d16f4f1b85b5aa
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2017-10-11 15:47:04 +0100

    Remove setting of query string for interaction create page

--- test/unit/apps/interactions/controllers/create.test.js
+++ test/unit/apps/interactions/controllers/create.test.js
@@ -24,14 +24,16 @@ describe('Create interaction, step 1', () => {
       breadcrumb: this.sandbox.stub().returnsThis(),
       redirect: this.sandbox.spy(),
       render: this.sandbox.spy(),
-      locals: {},
+      locals: {
+        returnLink: '/return/',
+      },
     }
 
     this.next = this.sandbox.spy()
   })
 
   describe('#postcreate', () => {
-    context('when a request is made to add an interaction from a company contact', () => {
+    context('when a request is made to add an interaction', () => {
       beforeEach(() => {
         this.req = assign({}, this.req, {
           body: {
@@ -49,94 +51,11 @@ describe('Create interaction, step 1', () => {
 
       it('should forward the user to the create interaction page', () => {
         const redirectUrl = this.res.redirect.firstCall.args[0]
-        expect(redirectUrl).to.contain('/interactions/create/interaction?')
-      })
-
-      it('should pass on the company id in the url', () => {
-        const redirectUrl = this.res.redirect.firstCall.args[0]
-        expect(redirectUrl).to.contain('company=1234')
-      })
-
-      it('should pass on the contact id in the url', () => {
-        const redirectUrl = this.res.redirect.firstCall.args[0]
-        expect(redirectUrl).to.contain('contact=4321')
-      })
-
-      it('should pass on the return url, correctly encoded', () => {
-        const redirectUrl = this.res.redirect.firstCall.args[0]
-        expect(redirectUrl).to.contain('returnLink=%2Fcontacts%2F4321%2Finteractions')
+        expect(redirectUrl).to.contain('/return/create/interaction')
       })
     })
 
-    context('whan a request is made to add an interaction from a company', () => {
-      beforeEach(() => {
-        this.req = assign({}, this.req, {
-          body: {
-            kind: 'interaction',
-          },
-          query: {
-            company: '1234',
-            returnLink: '/companies/1234/interactions',
-          },
-        })
-
-        this.create.postCreate(this.req, this.res, this.next)
-      })
-
-      it('should forward the user to the create interaction page', () => {
-        const redirectUrl = this.res.redirect.firstCall.args[0]
-        expect(redirectUrl).to.contain('/interactions/create/interaction?')
-      })
-
-      it('should pass on the company id in the url', () => {
-        const redirectUrl = this.res.redirect.firstCall.args[0]
-        expect(redirectUrl).to.contain('company=1234')
-      })
-
-      it('should pass on the return url, correctly encoded', () => {
-        const redirectUrl = this.res.redirect.firstCall.args[0]
-        expect(redirectUrl).to.contain('returnLink=%2Fcompanies%2F1234%2Finteractions')
-      })
-    })
-
-    context('when a request is made to add an interaction from an investment project', () => {
-      beforeEach(() => {
-        this.req = assign({}, this.req, {
-          body: {
-            kind: 'interaction',
-          },
-          query: {
-            investment: '4444',
-            company: '1234',
-            returnLink: '/investment-projects/444/interactions',
-          },
-        })
-
-        this.create.postCreate(this.req, this.res, this.next)
-      })
-
-      it('should forward the user to the create interaction page', () => {
-        const redirectUrl = this.res.redirect.firstCall.args[0]
-        expect(redirectUrl).to.contain('/interactions/create/interaction?')
-      })
-
-      it('should pass on the company id in the url', () => {
-        const redirectUrl = this.res.redirect.firstCall.args[0]
-        expect(redirectUrl).to.contain('company=1234')
-      })
-
-      it('should pass on the investment id in the url', () => {
-        const redirectUrl = this.res.redirect.firstCall.args[0]
-        expect(redirectUrl).to.contain('investment=4444')
-      })
-
-      it('should pass on the return url, correctly encoded', () => {
-        const redirectUrl = this.res.redirect.firstCall.args[0]
-        expect(redirectUrl).to.contain('returnLink=%2Finvestment-projects%2F444%2Finteractions')
-      })
-    })
-
-    context('when a request is made to add a service delivery from a company contact', () => {
+    context('when a request is made to add a service delivery', () => {
       beforeEach(() => {
         this.req = assign({}, this.req, {
           body: {
@@ -154,53 +73,7 @@ describe('Create interaction, step 1', () => {
 
       it('should forward the user to the create service delivery page', () => {
         const redirectUrl = this.res.redirect.firstCall.args[0]
-        expect(redirectUrl).to.contain('/interactions/create/service-delivery?')
-      })
-
-      it('should pass on the company id in the url', () => {
-        const redirectUrl = this.res.redirect.firstCall.args[0]
-        expect(redirectUrl).to.contain('company=1234')
-      })
-
-      it('should pass on the contact id in the url', () => {
-        const redirectUrl = this.res.redirect.firstCall.args[0]
-        expect(redirectUrl).to.contain('contact=4321')
-      })
-
-      it('should pass on the return url, correctly encoded', () => {
-        const redirectUrl = this.res.redirect.firstCall.args[0]
-        expect(redirectUrl).to.contain('returnLink=%2Fcontacts%2F4321%2Finteractions')
-      })
-    })
-
-    context('when a request is made to add a service delivery from a company', () => {
-      beforeEach(() => {
-        this.req = assign({}, this.req, {
-          body: {
-            kind: 'service_delivery',
-          },
-          query: {
-            company: '1234',
-            returnLink: '/companies/1234/interactions',
-          },
-        })
-
-        this.create.postCreate(this.req, this.res, this.next)
-      })
-
-      it('should forward the user to the create service delivery page', () => {
-        const redirectUrl = this.res.redirect.firstCall.args[0]
-        expect(redirectUrl).to.contain('/interactions/create/service-delivery?')
-      })
-
-      it('should pass on the company id in the url', () => {
-        const redirectUrl = this.res.redirect.firstCall.args[0]
-        expect(redirectUrl).to.contain('company=1234')
-      })
-
-      it('should pass on the return url, correctly encoded', () => {
-        const redirectUrl = this.res.redirect.firstCall.args[0]
-        expect(redirectUrl).to.contain('returnLink=%2Fcompanies%2F1234%2Finteractions')
+        expect(redirectUrl).to.contain('/return/create/service-delivery')
       })
     })
 
