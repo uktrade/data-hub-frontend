4d9b41e5e64711ca362759b2d8bc8e11e9fd7200
Dom Smith <dom@teneightfive.com>
2017-10-18 14:44:33 +0100

    Add filtering for reconciliation collection view
    This adds support for the macros form builder to display filters to
    the side of the results as well as changing the middleware and
    controllers to support sending the request data to the API.

--- test/unit/apps/omis/apps/list/controllers.test.js
+++ test/unit/apps/omis/apps/list/controllers.test.js
@@ -9,6 +9,7 @@ describe('OMIS list controllers', () => {
       query: {},
     }
     this.res = {
+      breadcrumb: this.sandbox.stub().returnsThis(),
       render: this.sandbox.spy(),
       query: {},
     }
@@ -22,6 +23,10 @@ describe('OMIS list controllers', () => {
         collectionFiltersFields: [
           { macroName: 'useful' },
           { macroName: 'exciting' },
+        ],
+        reconciliationFiltersFields: [
+          { macroName: 'foo' },
+          { macroName: 'bar' },
         ],
       },
     })
@@ -61,6 +66,23 @@ describe('OMIS list controllers', () => {
 
     it('should call render method', () => {
       expect(this.res.render).to.have.been.calledOnce
+    })
+
+    it('should set a breadcrumb item', () => {
+      expect(this.res.breadcrumb).to.have.been.calledOnce
+    })
+
+    it('should pass the correct data to the view', () => {
+      expect(this.res.render).to.have.been.calledWith(this.sandbox.match.any, this.sandbox.match.hasOwn('sortForm'))
+      expect(this.res.render).to.have.been.calledWith(this.sandbox.match.any, this.sandbox.match.hasOwn('filtersFields'))
+      expect(this.res.render).to.have.been.calledWith(this.sandbox.match.any, this.sandbox.match.hasOwn('selectedFilters'))
+    })
+
+    it('should build filters summary', () => {
+      expect(this.buildSelectedFiltersSummaryStub).to.have.been.calledWith([
+        { macroName: 'foo' },
+        { macroName: 'bar' },
+      ], this.req.query)
     })
   })
 })
