f7fb713d7a609ba88b69f62aea29d1a2c72805f2
venu <venu.reddy@digital.trade.gov.uk>
2017-10-19 12:09:31 +0100

    fixed css changes around Company/Contacts Audit history acceptance tests

--- test/acceptance/features/messages/page-objects/Message.js
+++ test/acceptance/features/messages/page-objects/Message.js
@@ -2,4 +2,14 @@ module.exports = {
   elements: {
     flashMessage: '.c-message-list li:first-child',
   },
+
+  commands: [
+    {
+      verifySuccessMessage () {
+        return this
+          .waitForElementPresent('@flashMessage')
+          .assert.cssClassPresent('@flashMessage', 'c-message--success')
+      },
+    },
+  ],
 }
