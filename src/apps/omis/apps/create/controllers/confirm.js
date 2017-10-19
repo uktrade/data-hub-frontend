b4621181f83ac8ceb4ae0b2f9a10d82bff1e12ee
Dom Smith <dom@teneightfive.com>
2017-10-19 17:30:34 +0100

    Use lodash's get for finding first and last name of contact
    This protects against the contact object not existing.

--- src/apps/omis/apps/create/controllers/confirm.js
+++ src/apps/omis/apps/create/controllers/confirm.js
@@ -14,8 +14,8 @@ class ConfirmController extends FormController {
       const company = res.locals.company
       const contact = find(company.contacts, { id: values.contact })
 
-      values.contact = `${contact.first_name} ${contact.last_name}`
       values.company = company
+      values.contact = `${get(contact, 'first_name')} ${get(contact, 'last_name')}`
       values.primary_market = find(metadataRepo.countryOptions, { id: values.primary_market })
       values.subscribers = filter(values.subscribers).map((id) => {
         const adviser = find(advisers.results, { id })
