c94da32ac880233e47f7c679e8fd82a8814b6d7c
jimsmith <jim.smith@digital.trade.gov.uk>
2017-10-23 15:36:21 +0100

    doublechecks whether company is *really* a CH company or not

--- src/apps/companies/transformers.js
+++ src/apps/companies/transformers.js
@@ -65,7 +65,7 @@ function transformCompanyToListItem ({
     })
   }
 
-  const url = companies_house_data ? `/companies/view/ch/${companies_house_data.company_number}` : `/companies/${id}`
+  const url = companies_house_data && !id ? `/companies/view/ch/${companies_house_data.company_number}` : `/companies/${id}`
 
   return {
     type: 'company',
