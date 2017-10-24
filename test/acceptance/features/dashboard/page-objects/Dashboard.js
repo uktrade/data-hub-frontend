beaf70cb3de1b67676028512dad381b5c407abc6
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2017-10-24 10:52:34 +0100

    Add Performance dashboards global nav link

--- test/acceptance/features/dashboard/page-objects/Dashboard.js
+++ test/acceptance/features/dashboard/page-objects/Dashboard.js
@@ -1,7 +1,32 @@
+const { getSelectorForElementWithText } = require('../../../helpers/selectors')
+
+const getGlobalNavSelector = (text) =>
+  getSelectorForElementWithText(
+    text,
+    {
+      el: '//a',
+      className: 'c-global-nav__link',
+    }
+  )
+
 module.exports = {
   url: process.env.QA_HOST,
   props: {},
   elements: {
     term: '#field-term',
+  },
+  sections: {
+    globalNav: {
+      selector: '.c-global-nav__container',
+      elements: {
+        companies: getGlobalNavSelector('Companies'),
+        contacts: getGlobalNavSelector('Contacts'),
+        events: getGlobalNavSelector('Events'),
+        interactions: getGlobalNavSelector('Interactions'),
+        investmentProjects: getGlobalNavSelector('Investment projects'),
+        ordersOmis: getGlobalNavSelector('Orders (OMIS)'),
+        performanceDashboards: getGlobalNavSelector('Performance dashboards'),
+      },
+    },
   },
 }
