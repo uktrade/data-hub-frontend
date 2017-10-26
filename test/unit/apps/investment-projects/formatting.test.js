a935b0b7279d0857e012fa32cb17aa76823bd388
Zac Tolley <zac@thetolleys.com>
2017-10-26 20:44:58 +0100

    Investment recipient company
    Allow user to search for and select a company the investment money will
    be sent to, stored as intermediate company in the back end.
    
    Show the intermediate company in the investment details screen and allow
    the user to search for a company value and remove if required

--- test/unit/apps/investment-projects/formatting.test.js
+++ test/unit/apps/investment-projects/formatting.test.js
@@ -157,4 +157,56 @@ describe('Investment project formatting service', () => {
       expect(actual).to.deep.equal(expected)
     })
   })
+
+  describe('#transformInvestmentRequirementsForView', () => {
+    context('when no uk company is provided', () => {
+      beforeEach(() => {
+        this.investmentData = Object.assign({}, investmentData, {
+          id: 1,
+          uk_company: null,
+        })
+
+        this.transformedInvestmentRequirements = formattingService.transformInvestmentRequirementsForView(this.investmentData)
+      })
+
+      it('should set the investment details to display a link to find a company', () => {
+        expect(this.transformedInvestmentRequirements.uk_company).to.deep.equal({
+          name: 'Find company',
+          url: `/investment-projects/1/edit-ukcompany`,
+        })
+      })
+    })
+
+    context('when a uk company has previously been set', () => {
+      beforeEach(() => {
+        this.investmentData = Object.assign({}, investmentData, {
+          id: 1,
+          uk_company: {
+            id: '1234',
+            name: 'Freds',
+          },
+        })
+
+        this.transformedInvestmentRequirements = formattingService.transformInvestmentRequirementsForView(this.investmentData)
+      })
+
+      it('should set the investment details to display the company name', () => {
+        expect(this.transformedInvestmentRequirements.uk_company.name).to.equal('Freds')
+      })
+
+      it('should set the investment details to display an action to edit the company money is going through', () => {
+        expect(this.transformedInvestmentRequirements.uk_company.actions[0]).to.deep.equal({
+          label: 'Edit company',
+          url: `/investment-projects/1/edit-ukcompany?term=Freds`,
+        })
+      })
+
+      it('should set the investment details to display an action to remove the company the money is going through', () => {
+        expect(this.transformedInvestmentRequirements.uk_company.actions[1]).to.deep.equal({
+          label: 'Remove company',
+          url: `/investment-projects/1/remove-ukcompany`,
+        })
+      })
+    })
+  })
 })
