fb2cd0296500d43a362976f68ceb81bfbbc02d65
Dom Smith <dom@teneightfive.com>
2017-10-31 12:27:30 +0000

    Add complete order step for OMIS orders
    This adds a step within the edit sub-app to record actual orders for
    assignees and complete an order.

---
+++ src/apps/omis/apps/edit/controllers/complete-order.js
@@ -0,0 +1,64 @@
+const { assign, filter, flatten, get, pick } = require('lodash')
+
+const { EditController } = require('../../../controllers')
+const { Order } = require('../../../models')
+
+class CompleteOrderController extends EditController {
+  async configure (req, res, next) {
+    if (get(res.locals, 'order.status') !== 'paid') {
+      return res.redirect(`/omis/${res.locals.order.id}`)
+    }
+
+    const orderId = get(res.locals, 'order.id')
+    const token = get(req.session, 'token')
+    const assignees = await Order.getAssignees(token, orderId)
+
+    if (!assignees.length) {
+      req.form.options.hidePrimaryFormAction = true
+    }
+
+    req.form.options = assign({}, req.form.options, {
+      disableFormAction: false,
+      buttonText: 'Complete order',
+    })
+
+    res.locals.assignees = assignees
+
+    super.configure(req, res, next)
+  }
+
+  async successHandler (req, res, next) {
+    const data = pick(req.sessionModel.toJSON(), Object.keys(req.form.options.fields))
+    const timeValues = flatten([data.assignee_actual_time])
+    const assignees = timeValues.map((value, index) => {
+      if (!value) { return }
+
+      const [ hours, minutes ] = value.split(':')
+
+      return {
+        adviser: {
+          id: res.locals.assignees[index].adviser.id,
+        },
+        actual_time: parseInt((hours * 60)) + parseInt(minutes),
+      }
+    })
+
+    try {
+      await Order.saveAssignees(req.session.token, res.locals.order.id, filter(assignees))
+      await Order.complete(req.session.token, res.locals.order.id)
+      const nextUrl = req.form.options.next || `/omis/${res.locals.order.id}`
+
+      req.journeyModel.reset()
+      req.journeyModel.destroy()
+      req.sessionModel.reset()
+      req.sessionModel.destroy()
+
+      req.flash('success', 'Order completed')
+      res.redirect(nextUrl)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
+
+module.exports = CompleteOrderController
