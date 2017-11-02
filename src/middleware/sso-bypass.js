8639b5a4098c482e5da5d886ec8511fd82ff4e42
Ben Chidgey <ben@feedmypixel.com>
2017-11-02 16:37:31 +0000

    sso bypass middleware so developers can bypass SSO if required

---
+++ src/middleware/sso-bypass.js
@@ -0,0 +1,21 @@
+const { get } = require('lodash')
+const config = require('../../config')
+
+/**
+ * If a development bearer token has been provided then assign that to req.session.token
+ * so SSO functionality can be bypassed for developers.
+ * @returns {Function}
+ */
+
+const ssoBypass = () => {
+  return function (req, res, next) {
+    const token = get(config, 'oauth.token')
+
+    if (config.isDev && token) {
+      req.session.token = token
+    }
+    next()
+  }
+}
+
+module.exports = ssoBypass
