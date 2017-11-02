1392430eb9d378f31b61317b5ff1e98f371c1ec8
Ben Chidgey <ben@feedmypixel.com>
2017-11-02 16:31:38 +0000

    store isProd as const

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
-const isDev = (process.env.NODE_ENV !== 'production')
+const isDev = process.env.NODE_ENV !== 'production'
+const isProd = process.env.NODE_ENV === 'production'
 const root = path.normalize(`${__dirname}/..`)
 
 const config = {
