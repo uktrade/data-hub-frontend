d7bab593fcc4a342536c6c8060c9c942b8e32d03
Zac Tolley <zac@thetolleys.com>
2017-11-10 22:43:27 +0000

    Updated companies house company transformer
    Created a new transformer to accomodate the data returned from the new companies house search endpoint.

--- test/unit/apps/companies/transformers.test.js
+++ test/unit/apps/companies/transformers.test.js
@@ -1,6 +1,10 @@
 const { some } = require('lodash')
+const companiesHouseSearchResults = require('~/test/unit/data/companies/companiesHouseSearch.json')
 
-const { transformCompanyToListItem } = require('~/src/apps/companies/transformers')
+const {
+  transformCompanyToListItem,
+  transformCompaniesHouseCompanyToListItem,
+} = require('~/src/apps/companies/transformers')
 
 describe('Company transformers', function () {
   describe('#transformCompanyToListItem', () => {
@@ -75,6 +79,64 @@ describe('Company transformers', function () {
       }))
 
       expect(actual).to.have.property('url').to.be.equal(`/companies/${companyData.id}`)
+    })
+  })
+
+  describe('#transformCompaniesHouseCompanyTolistItem', () => {
+    beforeEach(() => {
+      this.transformed = transformCompaniesHouseCompanyToListItem(companiesHouseSearchResults.results[0])
+    })
+
+    it('should include the company number as an id', () => {
+      expect(this.transformed.id).to.equal('01234567896')
+    })
+
+    it('should format the company name', () => {
+      expect(this.transformed.name).to.equal('Fred consultants limited')
+    })
+
+    it('should include the company status as a badge', () => {
+      expect(this.transformed.meta[0]).to.deep.equal({
+        label: 'Status',
+        value: 'Active',
+        type: 'badge',
+      })
+    })
+
+    it('should include the company number as meta data', () => {
+      expect(this.transformed.meta[1]).to.deep.equal({
+        label: 'Company number',
+        value: '01234567896',
+      })
+    })
+
+    it('should include the type of company as meta data', () => {
+      expect(this.transformed.meta[2]).to.deep.equal({
+        label: 'Type',
+        value: 'Private Limited Company',
+      })
+    })
+
+    it('should join sic codes and return as meta data', () => {
+      expect(this.transformed.meta[3]).to.deep.equal({
+        label: 'Nature of business (SIC)',
+        value: '62020 - Information technology consultancy activities, 9999 - Animal trainer',
+      })
+    })
+
+    it('should include the incorporation date', () => {
+      expect(this.transformed.meta[4]).to.deep.equal({
+        label: 'Incorporated on',
+        type: 'date',
+        value: '2002-01-07',
+      })
+    })
+
+    it('should format the address and return as meta data', () => {
+      expect(this.transformed.meta[5]).to.deep.equal({
+        label: 'Address',
+        value: 'Fred cottage smith lane, Winkfield, Windsor, Berkshire, SL9 9RE',
+      })
     })
   })
 })
