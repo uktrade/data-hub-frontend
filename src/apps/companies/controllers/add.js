9d0fac6e1429d17bbed463924a7d2f98005b2f0c
Zac Tolley <zac@thetolleys.com>
2017-11-10 22:51:33 +0000

    refactor add company router and controllers
    Refactored the controllers for add company to follow the collection patterns and the pattern having a post handler fail through to the form render.
    
    Split the add company step 2 into middleware to get search results and a controller to simply render the result.
    
    Removed hidden fields and lookups used to get the company type in step 2 because the current flow dictates that this screen is only used to search for limited companies.

--- src/apps/companies/controllers/add.js
+++ src/apps/companies/controllers/add.js
@@ -1,16 +1,10 @@
-const { assign } = require('lodash')
 const queryString = require('query-string')
 
-const logger = require('../../../../config/logger')
 const { buildUkOtherCompanyOptions, buildForeignOtherCompanyOptions } = require('../options')
-const { getCHCompany } = require('../repos')
 const { isBlank } = require('../../../lib/controller-utils')
-const { searchLimitedCompanies } = require('../../search/services')
-const { transformApiResponseToSearchCollection } = require('../../search/transformers')
-const { transformCompaniesHouseCompanyToListItem } = require('../../companies/transformers')
 const { companyDetailsLabels, companyTypeOptions } = require('../labels')
 
-function getAddStepOne (req, res) {
+function renderAddStepOne (req, res) {
   const ukOtherCompanyOptions = buildUkOtherCompanyOptions()
   const foreignOtherCompanyOptions = buildForeignOtherCompanyOptions()
 
@@ -43,7 +37,7 @@ function postAddStepOne (req, res, next) {
     res.locals.errors = {
       messages: errorMessages,
     }
-    return getAddStepOne(req, res)
+    return next()
   }
 
   let params
@@ -75,69 +69,12 @@ function postAddStepOne (req, res, next) {
   return res.redirect(`/companies/add-step-2?${queryString.stringify(params)}`)
 }
 
-async function getAddStepTwo (req, res, next) {
-  const searchTerm = req.query.term
-  const businessType = req.query.business_type
-  const token = req.session.token
-  let results
-
-  if (!searchTerm) {
-    return res.render('companies/views/add-step-2.njk', {
-      companyTypeOptions,
-      businessType,
-    })
-  }
-
-  try {
-    results = await searchLimitedCompanies({
-      token,
-      searchTerm,
-    })
-      .then(async (response) => {
-        // TODO: Remove the need to make another call to the API to get the companies house details.
-        // The search API should return companies house companies and their relevant information
-        return Promise.all(response.results.map(async (result) => {
-          try {
-            result.companies_house_data = await getCHCompany(token, result.company_number)
-            result.url = `/companies/add/${result.company_number}`
-          } catch (error) {
-            logger.error(error)
-          }
-          return result
-        }))
-      })
-      .then((results) => {
-        return {
-          page: 1,
-          items: results,
-          count: results.length,
-        }
-      })
-      .then(
-        transformApiResponseToSearchCollection(
-          { query: req.query },
-          transformCompaniesHouseCompanyToListItem,
-          (item) => {
-            return assign({}, item, {
-              meta: item.meta.filter(x => x.label !== 'Sector'),
-            })
-          }
-        )
-      )
-  } catch (error) {
-    logger.error(error)
-  }
-
-  res.render('companies/views/add-step-2.njk', {
-    companyTypeOptions,
-    results,
-    searchTerm,
-    businessType,
-  })
+function renderAddStepTwo (req, res, next) {
+  res.render('companies/views/add-step-2.njk')
 }
 
 module.exports = {
-  getAddStepOne,
+  renderAddStepOne,
   postAddStepOne,
-  getAddStepTwo,
+  renderAddStepTwo,
 }
