db74febda4e0d1ce9b387aea7a05e0d932b8d055
Dom Smith <dom@teneightfive.com>
2017-11-13 19:38:35 +0000

    Add step to create journey to add sector
    This adds a separate step to the creation journey to choose the primary
    sector for the order at an earlier stage.

--- src/apps/omis/apps/create/controllers/index.js
+++ src/apps/omis/apps/create/controllers/index.js
@@ -1,9 +1,11 @@
 const ClientDetailsController = require('./client-details')
 const MarketController = require('./market')
+const SectorController = require('./sector')
 const ConfirmController = require('./confirm')
 
 module.exports = {
   ClientDetailsController,
   MarketController,
+  SectorController,
   ConfirmController,
 }
