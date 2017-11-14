f7e6aeec978f3abed5ccba3e099057b7848120c7
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2017-11-14 15:52:38 +0000

    Remove redundant acceptance tests and related steps

--- test/acceptance/features/companies/step_definitions/company.js
+++ test/acceptance/features/companies/step_definitions/company.js
@@ -53,39 +53,4 @@ defineSupportCode(({ Then, When }) => {
       .findCompany(getUid(companyName))
       .assert.containsText('@collectionResultsCompanyName', companyName)
   })
-
-  Then(/^The company name is present in the collections results/, async function () {
-    const companyName = this.state.company.name
-
-    await client
-      .url(dashboardPage)
-
-    await Company
-      .searchForCompanyInCollection(getUid(companyName))
-      .assert.containsText('@collectionResultsCompanyName', companyName)
-  })
-
-  Then(/^The company sector is present in the collections results$/, async function () {
-    await Company
-      .searchForCompanyInCollection(getUid(this.state.company.name))
-      .assert.containsText('@collectionResultsSectorLabel', 'Sector')
-  })
-
-  Then(/^The company region is present in the collections results$/, async function () {
-    await Company
-      .searchForCompanyInCollection(getUid(this.state.company.name))
-      .assert.containsText('@collectionResultsRegionLabel', 'UK region')
-  })
-
-  Then(/^The company registered address is present in the collections results$/, async function () {
-    await Company
-      .searchForCompanyInCollection(getUid(this.state.company.name))
-      .assert.containsText('@collectionResultsRegisteredAddressLabel', 'Registered address')
-  })
-
-  Then(/^Clicking the company name takes me to the companies page$/, async function () {
-    await Company
-      .click('@collectionResultsCompanyName')
-      .assert.containsText('@companyPageHeading', this.state.company.name)
-  })
 })
