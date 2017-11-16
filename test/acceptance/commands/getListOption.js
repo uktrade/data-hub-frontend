bc770286cf137fc04be5cb39a70cc1e7d952e1f0
Ben Chidgey <ben@feedmypixel.com>
2017-11-16 21:03:35 +0000

    normalise the selector sent as an argument to a command function, it is different when called from a Page Object or a section within a, Page Object

--- test/acceptance/commands/getListOption.js
+++ test/acceptance/commands/getListOption.js
@@ -1,10 +1,14 @@
+const getSectionElementSelector = require('./helpers/getSectionElementSelector')
+
 /**
  * Get a random option from a select not including the first option element as this is generally a placeholder
  * @param selector
  * @param callback
  */
 exports.command = function getListOption (selector, callback) {
-  return this.elements('css selector', `${selector} option`, (result) => {
+  const normalisedSelector = getSectionElementSelector(selector)
+
+  return this.elements('css selector', `${normalisedSelector} option`, (result) => {
     if (result.value.length) {
       const random = Math.floor((Math.random() * (result.value.length - 1)) + 1) // we don't want the first option in a select
 
@@ -14,7 +18,7 @@ exports.command = function getListOption (selector, callback) {
         }
       })
     } else {
-      this.log(`check the contents of the selector: ${selector}`)
+      this.log(`check the contents of the selector: ${normalisedSelector}`)
     }
   })
 }
