c251169b0a68f3943ccbdef9ffaf0a9f2f4f8e5c
Ben Chidgey <ben@feedmypixel.com>
2017-11-16 21:14:50 +0000

    removes duplications from collections and combines Interactions work into collections work

--- test/acceptance/features/collection/step_definitions/collection.js
+++ test/acceptance/features/collection/step_definitions/collection.js
@@ -67,47 +67,40 @@ defineSupportCode(({ When, Then }) => {
   })
 
   Then(/^I can view the (.+) in the collection$/, async function (entityType, dataTable) {
-    const entityHeaderName = get(this.state, `${camelCase(entityType)}.header`)
+    const entityHeading = get(this.state, `${camelCase(entityType)}.heading`)
 
     await Collection
       .section.firstCollectionItem
       .waitForElementPresent('@header')
-      .assert.containsText('@header', entityHeaderName)
+      .assert.containsText('@header', entityHeading)
 
-    for (let row of dataTable.hashes()) {
-      const element = Collection.getSelectorForMetaListItem(row.label)
-      const expectedText = get(this.state, row.statePath)
-
-      if (expectedText) {
-        await Collection
-          .section.firstCollectionItem
-          .api.useXpath()
-          .assert.containsText(element.selector, expectedText)
-          .useCss()
-      } else {
-        if (row.statePath) {
-          Collection
-            .assert.fail(`Invalid statePath for ${row.label}`)
-        }
-
-        await Collection
-          .section.firstCollectionItem
-          .api.useXpath()
-          .assert.visible(element.selector)
-          .useCss()
-      }
-    }
-  })
-
-  Then(/^the (.+) has badges$/, async function (entityType, dataTable) {
-    for (let row of dataTable.hashes()) {
-      const selector = Collection.getSelectorForBadgeWithText(row.text)
+    for (const row of dataTable.hashes()) {
+      const metaListValueElement = Collection.getSelectorForMetaListItemValue(row.text)
+      const expectedMetaListText = get(this.state, row.expected)
 
       await Collection
         .section.firstCollectionItem
         .api.useXpath()
-        .assert.visible(selector.selector)
+        .assert.containsText(metaListValueElement.selector, expectedMetaListText)
         .useCss()
+    }
+  })
+
+  Then(/^the (.+) has badges$/, async function (entityType, dataTable) {
+    for (const row of dataTable.hashes()) {
+      const badgeValueElement = Collection.getSelectorForBadgeWithText(row.text)
+      const expectedBadgeValue = get(this.state, row.expected)
+
+      // If we have a value in state that we expecting then test for its contents
+      // meaning we can specify badges that only appear when specific entries have been made to a form. e.g Uk region
+      if (expectedBadgeValue) {
+        await Collection
+          .section.firstCollectionItem
+          .api.useXpath()
+          .assert.visible(badgeValueElement.selector)
+          .assert.containsText(badgeValueElement.selector, expectedBadgeValue)
+          .useCss()
+      }
     }
   })
 
@@ -126,5 +119,12 @@ defineSupportCode(({ When, Then }) => {
         const expected = get(this.state, 'collection.resultCount')
         client.expect(result.value).to.equal(expected)
       })
+  })
+
+  Then(/^I choose the first item in the collection$/, async function () {
+    await Collection
+      .section.firstCollectionItem
+      .waitForElementVisible('@header')
+      .click('@header')
   })
 })
