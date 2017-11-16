518b0cb2a6ff521976dd5bfb3077a4d26dcd1a6d
Zac Tolley <zac@thetolleys.com>
2017-11-16 13:28:21 +0000

    New companies house details transformer
    Created a new transformer for company house details that is compatible with the key value table component.
    Improved test coverage.

--- src/apps/companies/transformers.js
+++ src/apps/companies/transformers.js
@@ -1,4 +1,5 @@
 /* eslint-disable camelcase */
+const Case = require('case')
 const {
   assign,
   find,
@@ -11,11 +12,13 @@ const {
 
 const metadataRepository = require('../../lib/metadata')
 const { getFormattedAddress } = require('../../lib/address')
+const { formatLongDate } = require('../../../common/date')
 const { getPrimarySectorName } = require('../../../common/transform-sectors')
 const { getDataLabels } = require('../../lib/controller-utils')
 const {
   companyDetailsLabels,
   hqLabels,
+  chDetailsLabels,
 } = require('./labels')
 
 function transformCompanyToListItem ({
@@ -270,6 +273,43 @@ function transformCompanyResponseToViewRecord ({
   return pickBy(getDataLabels(viewRecord, companyDetailsLabels))
 }
 
+function transformCompaniesHouseResponseToViewRecord ({
+  name,
+  registered_address_1,
+  registered_address_2,
+  registered_address_town,
+  registered_address_county,
+  registered_address_postcode,
+  registered_address_country,
+  company_number,
+  company_category,
+  company_status,
+  sic_code_1,
+  sic_code_2,
+  sic_code_3,
+  sic_code_4,
+  incorporation_date,
+} = {}) {
+  const viewRecord = {
+    company_number,
+    company_status,
+    name: Case.title(name),
+    business_type: company_category,
+    registered_address: getFormattedAddress({
+      address_1: registered_address_1,
+      address_2: registered_address_2,
+      address_town: registered_address_town,
+      address_county: registered_address_county,
+      address_postcode: registered_address_postcode,
+      address_country: registered_address_country,
+    }),
+    incorporation_date: formatLongDate(incorporation_date),
+    sic_code: transformSicCodes({ sic_code_1, sic_code_2, sic_code_3, sic_code_4 }),
+  }
+
+  return pickBy(getDataLabels(viewRecord, chDetailsLabels))
+}
+
 function transformSicCodes ({
   sic_code_1,
   sic_code_2,
@@ -291,4 +331,5 @@ module.exports = {
   transformCompaniesHouseCompanyToListItem,
   transformCompanyResponseToForm,
   transformCompanyResponseToViewRecord,
+  transformCompaniesHouseResponseToViewRecord,
 }
--- test/unit/apps/companies/transformers.test.js
+++ test/unit/apps/companies/transformers.test.js
@@ -8,6 +8,7 @@ const {
   transformCompanyToListItem,
   transformCompaniesHouseCompanyToListItem,
   transformCompanyResponseToViewRecord,
+  transformCompaniesHouseResponseToViewRecord,
 } = require('~/src/apps/companies/transformers')
 
 describe('Company transformers', function () {
@@ -301,6 +302,54 @@ describe('Company transformers', function () {
 
       it('should supply sector', () => {
         expect(this.viewRecord['Sector']).to.equal('Aerospace')
+      })
+    })
+  })
+
+  describe('#transformCompaniesHouseResponseToViewRecord', () => {
+    context('when companies data is provided', () => {
+      beforeEach(() => {
+        this.viewRecord = transformCompaniesHouseResponseToViewRecord(companiesHouseCompany.companies_house_data)
+      })
+
+      it('should return the fields expected in the correct order', () => {
+        expect(this.viewRecord).to.have.ordered.keys([
+          'Registered name',
+          'Companies House No',
+          'Company type',
+          'Company status',
+          'Registered office address',
+          'Incorporated on',
+          'Nature of business (SIC)',
+        ])
+      })
+
+      it('should return the name', () => {
+        expect(this.viewRecord).to.have.property('Registered name', 'Samsung Bioepis Uk Limited')
+      })
+
+      it('should return the company number', () => {
+        expect(this.viewRecord).to.have.property('Companies House No', '08840722')
+      })
+
+      it('should return the business type', () => {
+        expect(this.viewRecord).to.have.property('Company type', 'Private Limited Company')
+      })
+
+      it('should return the company status', () => {
+        expect(this.viewRecord).to.have.property('Company status', 'Active')
+      })
+
+      it('should return the registered address', () => {
+        expect(this.viewRecord).to.have.property('Registered office address', '5th Floor, Profile West, 950 Great West Road, Brentford, Middlesex, TW8 9ES, United Kingdom')
+      })
+
+      it('should return the incorporation date', () => {
+        expect(this.viewRecord).to.have.property('Incorporated on', '10 January 2014')
+      })
+
+      it('should return the formatted SIC codes', () => {
+        expect(this.viewRecord).to.have.property('Nature of business (SIC)', '82990 - Other business support service activities n.e.c., more stuff')
       })
     })
   })
