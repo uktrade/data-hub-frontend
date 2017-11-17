a75b9398cb5ade1cad0e0e8a8d613a2b94b1a22b
Zac Tolley <zac@thetolleys.com>
2017-11-17 20:32:37 +0000

    Add transformer for company export details

---
+++ test/unit/apps/companies/transformers/company-response-to-export-details-view-record.test.js
@@ -0,0 +1,96 @@
+const { assign } = require('lodash')
+
+const minimalCompany = require('~/test/unit/data/companies/minimal-company.json')
+const { transformCompanyResponseToExportDetailsViewRecord } = require('~/src/apps/companies/transformers')
+
+describe('transformCompanyResponseToExportDetailsViewRecord', () => {
+  context('when the company is exporting to one country', () => {
+    beforeEach(() => {
+      const company = assign({}, minimalCompany, {
+        export_to_countries: [{
+          id: '1234',
+          name: 'France',
+        }],
+        future_interest_countries: [{
+          id: '4321',
+          name: 'Germany',
+        }],
+      })
+
+      this.viewRecord = transformCompanyResponseToExportDetailsViewRecord(company)
+    })
+
+    it('Should show the country currently exporting to', () => {
+      expect(this.viewRecord).to.have.property('Currently exporting to', 'France')
+    })
+  })
+
+  context('when the company is exporting to multiple countries', () => {
+    beforeEach(() => {
+      const company = assign({}, minimalCompany, {
+        export_to_countries: [{
+          id: '1234',
+          name: 'France',
+        }, {
+          id: '5511',
+          name: 'Spain',
+        }],
+        future_interest_countries: [{
+          id: '4321',
+          name: 'Germany',
+        }],
+      })
+
+      this.viewRecord = transformCompanyResponseToExportDetailsViewRecord(company)
+    })
+
+    it('Should show the countries currently exporting to', () => {
+      expect(this.viewRecord).to.have.property('Currently exporting to', 'France, Spain')
+    })
+  })
+
+  context('when the company wants to export to one country', () => {
+    beforeEach(() => {
+      const company = assign({}, minimalCompany, {
+        export_to_countries: [{
+          id: '1234',
+          name: 'France',
+        }],
+        future_interest_countries: [{
+          id: '4321',
+          name: 'Germany',
+        }],
+      })
+
+      this.viewRecord = transformCompanyResponseToExportDetailsViewRecord(company)
+    })
+
+    it('Should show the country the company wants to export to', () => {
+      expect(this.viewRecord).to.have.property('Future countries of interest', 'Germany')
+    })
+  })
+
+  context('when the company wants to export to multiple countries', () => {
+    beforeEach(() => {
+      const company = assign({}, minimalCompany, {
+        export_to_countries: [{
+          id: '1234',
+          name: 'France',
+        }],
+        future_interest_countries: [{
+          id: '4321',
+          name: 'Germany',
+        }, {
+          id: '4123',
+          name: 'Sweden',
+        }],
+      })
+
+      this.viewRecord = transformCompanyResponseToExportDetailsViewRecord(company)
+    })
+
+    it('Should show the coiuntries the company wants to export to', () => {
+      expect(this.viewRecord).to.have.property('Future countries of interest', 'Germany, Sweden')
+    })
+  })
+})
