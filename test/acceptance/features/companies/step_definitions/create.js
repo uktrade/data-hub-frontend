2eca2a7b04e781cf93b8ac97186815b69815c6c2
Ben Chidgey <ben@feedmypixel.com>
2017-11-18 12:49:50 +0000

    use primaryAddresss this is already available on state

--- test/acceptance/features/companies/step_definitions/create.js
+++ test/acceptance/features/companies/step_definitions/create.js
@@ -5,18 +5,13 @@ const { set } = require('lodash')
 defineSupportCode(({ Given }) => {
   const Company = client.page.Company()
 
-  Given('a company is created', async function () {
+  Given(/^a company is created$/, async function () {
     await client
       .url(this.urls.companies.search)
 
     await Company
       .createUkNonPrivateOrNonPublicLimitedCompany({}, (company) => {
         set(this.state, 'company', company)
-        set(this.state, 'company.registeredAddress', [
-          this.state.company.address1,
-          this.state.company.postcode,
-          this.state.company.town,
-        ].join(', '))
       })
       .wait() // wait for backend to sync
   })
