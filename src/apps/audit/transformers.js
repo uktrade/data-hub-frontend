32479f9898a0e6256dc060eec554848e04a94f05
Zac Tolley <zac@thetolleys.com>
2017-11-22 11:19:43 +0000

    Explain field name mapping in audit transformer

--- src/apps/audit/transformers.js
+++ src/apps/audit/transformers.js
@@ -3,6 +3,18 @@ const { get } = require('lodash')
 const dateFns = require('date-fns')
 const { mediumDateTimeFormat } = require('../../../config')
 
+/**
+ * Transforms an audit log of changes into a common seperated list
+ * summerizing the fields that were modified in the change.
+ *
+ * Note that the field name that changed is stripped of '_id', if it ends in it.
+ * by convention the back end API includes the _id part of a fieldname in the
+ * original table even though all of references to the field do not include it
+ *
+ *
+ * @param {Array[Object]} changes
+ * @param {Object} labels
+ */
 function transformChanges (changes, labels) {
   return Object.keys(changes)
     .map(key => labels[key] || labels[key.replace(/_id$/, '')] || key)
