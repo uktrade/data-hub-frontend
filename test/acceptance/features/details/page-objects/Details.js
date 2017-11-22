19e27d18f57311281d64b0e039e68e868ef2d86d
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2017-11-22 16:35:36 +0000

    Rename function to be more self documenting

--- test/acceptance/features/details/page-objects/Details.js
+++ test/acceptance/features/details/page-objects/Details.js
@@ -3,13 +3,14 @@ const { getSelectorForElementWithText } = require('../../../helpers/selectors')
 module.exports = {
   elements: {
     heading: '.c-local-header__heading',
+    localNav: '.c-local-nav',
   },
   commands: [
     {
       getDetailFor (label) {
         return getSelectorForElementWithText(label, { el: '//th', child: '/following-sibling::td' })
       },
-      getDetailsTabSelector (text) {
+      getLocalNavItemSelector (text) {
         return getSelectorForElementWithText(
           text,
           {
