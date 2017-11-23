93c3a376ce2aa0459cfb4f505c6b6333459b6cf4
Dom Smith <dom@teneightfive.com>
2017-11-23 15:38:33 +0000

    Validate the EU VAT number to ensure it includes the country code
    When we display the EU VAT number on the invoice we need to know
    that it has the correct country code. This change adds a regex check
    to ensure we are using a valid EU VAT number.

--- src/apps/omis/apps/edit/fields.js
+++ src/apps/omis/apps/edit/fields.js
@@ -1,4 +1,5 @@
 const { isArray, flatten, merge } = require('lodash')
+const FormController = require('hmpo-form-controller')
 
 const globalFields = require('../../fields')
 
@@ -29,6 +30,12 @@ function arrayRequired (value) {
   })
 
   return invalidItems.length === 0
+}
+
+function validEUVatNumber (value) {
+  const regex = /^(ATU[0-9]{8}|BE0[0-9]{9}|BG[0-9]{9,10}|CY[0-9]{8}L|CZ[0-9]{8,10}|DE[0-9]{9}|DK[0-9]{8}|EE[0-9]{9}|EL|GR[0-9]{9}|ES[0-9A-Z][0-9]{7}[0-9A-Z]|FI[0-9]{8}|FR[0-9A-Z]{2}[0-9]{9}|GB([0-9]{9}([0-9]{3})?|[A-Z]{2}[0-9]{3})|HU[0-9]{8}|IE[0-9]S[0-9]{5}L|IT[0-9]{11}|LT([0-9]{9}|[0-9]{12})|LU[0-9]{8}|LV[0-9]{11}|MT[0-9]{8}|NL[0-9]{9}B[0-9]{2}|PL[0-9]{10}|PT[0-9]{9}|RO[0-9]{2,10}|SE[0-9]{12}|SI[0-9]{8}|SK[0-9]{10})$/
+
+  return value === '' || FormController.validators.regex(value, regex)
 }
 
 const editFields = merge({}, globalFields, {
@@ -132,6 +139,7 @@ const editFields = merge({}, globalFields, {
     fieldType: 'TextField',
     label: 'fields.vat_number.label',
     modifier: ['subfield', 'medium'],
+    validate: [validEUVatNumber],
     condition: {
       name: 'vat_status',
       value: 'eu',
