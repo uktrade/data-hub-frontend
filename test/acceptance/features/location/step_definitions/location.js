4faa8f4b5ce8041db513940ee98ff0f56cfaea54
Ben Chidgey <ben.chidgey@digital.trade.gov.uk>
2017-11-25 09:11:28 +0000

    refactor details tabs work to be in one place

--- test/acceptance/features/location/step_definitions/location.js
+++ test/acceptance/features/location/step_definitions/location.js
@@ -1,8 +1,20 @@
+const { lowerCase } = require('lodash')
+
 const { client } = require('nightwatch-cucumber')
 const { defineSupportCode } = require('cucumber')
 
 defineSupportCode(({ When, Then }) => {
   const Location = client.page.Location()
+
+  Then(/^I click the (.+) local nav link$/, async (navItemText) => {
+    const tag = `@${lowerCase(navItemText)}`
+
+    await Location
+      .section.detailsTabs
+      .waitForElementPresent(tag)
+      .assert.containsText(tag, navItemText)
+      .click(tag)
+  })
 
   Then(/^I am taken to the "(.+)" page$/, async (text) => {
     await Location
