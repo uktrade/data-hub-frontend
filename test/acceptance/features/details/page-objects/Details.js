5f823ee1069732c861ccfb3b67bf531fc8fbd792
Ben Chidgey <ben.chidgey@digital.trade.gov.uk>
2017-11-25 09:27:05 +0000

    add work to check for document link

--- test/acceptance/features/details/page-objects/Details.js
+++ test/acceptance/features/details/page-objects/Details.js
@@ -4,6 +4,11 @@ module.exports = {
   elements: {
     heading: '.c-local-header__heading',
     localNav: '.c-local-nav',
+    documentsLink: {
+      selector: '//a[contains(.,"View files and documents")][contains(@aria-labelledby,"external-link-label")]',
+      locateStrategy: 'xpath',
+    },
+    noDocumentsMessage: getSelectorForElementWithText('There are no files or documents', { el: '//article//p' }),
   },
   commands: [
     {
