c34b6044209e95eb0796329ec1df81cd6798ee49
Zac Tolley <zac@thetolleys.com>
2017-11-25 18:51:38 +0000

    Allow get advisers to return active advisers
    Updated the getAdvisers function to allow the caller to ask for only active advisers and also specify an adviser that must always be returned.
    
    Return just the array instead of a collection like object as all the callers only want the array.

--- test/unit/apps/adviser/repos.test.js
+++ test/unit/apps/adviser/repos.test.js
@@ -12,11 +12,13 @@ describe('Adviser repository', () => {
           .get(`/adviser/?limit=100000&offset=0`)
           .reply(200, badAdviserData)
       })
+
       it('will be filtered out', async () => {
         const actual = await repos.getAdvisers(123)
-        expect(actual.results.length).to.equal(4)
+        expect(actual.length).to.equal(4)
       })
     })
+
     context('when all advisers have names', () => {
       beforeEach(() => {
         nock(config.apiRoot)
@@ -25,7 +27,79 @@ describe('Adviser repository', () => {
       })
       it('will not filter out any advisers', async () => {
         const actual = await repos.getAdvisers(123)
-        expect(actual.results.length).to.equal(5)
+        expect(actual.length).to.equal(5)
+      })
+    })
+
+    context('when advisers contains disabled advisers', () => {
+      beforeEach(() => {
+        this.advisers = [{
+          id: '1',
+          first_name: 'Fred',
+          last_name: 'Flintstone',
+          disabled_on: '2017-01-01',
+        }, {
+          id: '2',
+          first_name: 'Wilma',
+          last_name: 'Flintstone',
+          disabled_on: '2017-01-01',
+        }, {
+          id: '3',
+          first_name: 'Barney',
+          last_name: 'Rubble',
+          disabled_on: null,
+        }]
+
+        nock(config.apiRoot)
+          .get(`/adviser/?limit=100000&offset=0`)
+          .reply(200, { results: this.advisers })
+      })
+
+      context('and when the caller wishes to only see active users', () => {
+        context('and when the caller does not specify an adviser that must be returned', () => {
+          beforeEach(async () => {
+            this.advisersResult = await repos.getAdvisers('1234', false)
+          })
+
+          it('should return just the active adviser', () => {
+            expect(this.advisersResult).to.deep.equal([{
+              id: '3',
+              first_name: 'Barney',
+              last_name: 'Rubble',
+              disabled_on: null,
+            }])
+          })
+        })
+
+        context('and when the caller specified an adviser that must be returned', () => {
+          beforeEach(async () => {
+            this.advisersResult = await repos.getAdvisers('1234', false, '2')
+          })
+
+          it('should return just the active adviser', () => {
+            expect(this.advisersResult).to.deep.equal([{
+              id: '2',
+              first_name: 'Wilma',
+              last_name: 'Flintstone',
+              disabled_on: '2017-01-01',
+            }, {
+              id: '3',
+              first_name: 'Barney',
+              last_name: 'Rubble',
+              disabled_on: null,
+            }])
+          })
+        })
+      })
+
+      context('and when the caller wishes to see all advisers', () => {
+        beforeEach(async () => {
+          this.advisersResult = await repos.getAdvisers('1234')
+        })
+
+        it('should return just the active adviser', () => {
+          expect(this.advisersResult).to.deep.equal(this.advisers)
+        })
       })
     })
   })
