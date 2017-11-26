5f10224209e8313c97365e4449836f82e51cd8d1
Zac Tolley <zac@thetolleys.com>
2017-11-26 22:47:40 +0000

    fixup! Create a middleware to set a list of advisers

--- src/apps/adviser/middleware.js
+++ src/apps/adviser/middleware.js
@@ -2,7 +2,7 @@ const { getAdvisers } = require('./repos')
 
 async function setAllAdvisers (req, res, next) {
   try {
-    res.locals.advisers = await getAdvisers({ token: req.session.token })
+    res.locals.advisers = await getAdvisers(req.session.token)
     next()
   } catch (err) {
     next(err)
