0354f125446045a69079896ceebdf7e51e0dc8d1
Zac Tolley <zac@thetolleys.com>
2017-11-26 14:48:37 +0000

    fixup! Allow get advisers to return active advisers

--- test/unit/apps/adviser/repos.test.js
+++ test/unit/apps/adviser/repos.test.js
@@ -1,6 +1,4 @@
 const nock = require('nock')
-const adviserData = require('~/test/unit/data/advisers/advisers.json')
-const badAdviserData = require('~/test/unit/data/advisers/advisers-with-bad-data.json')
 const config = require('~/config')
 const repos = require('~/src/apps/adviser/repos')
 
@@ -8,26 +6,32 @@ describe('Adviser repository', () => {
   describe('getAdvisers', () => {
     context('when an adviser without a name is encountered', () => {
       beforeEach(() => {
+        this.advisers = [{
+          id: '1',
+          first_name: 'Fred',
+          last_name: null,
+          disabled_on: null,
+        }, {
+          id: '2',
+          first_name: null,
+          last_name: 'Flintstone',
+          disabled_on: null,
+        }, {
+          id: '3',
+          first_name: null,
+          last_name: null,
+          disabled_on: null,
+        }]
+
         nock(config.apiRoot)
           .get(`/adviser/?limit=100000&offset=0`)
-          .reply(200, badAdviserData)
+          .reply(200, { results: this.advisers })
       })
 
       it('will be filtered out', async () => {
-        const actual = await repos.getAdvisers({ token: '1234' })
-        expect(actual.length).to.equal(4)
-      })
-    })
-
-    context('when all advisers have names', () => {
-      beforeEach(() => {
-        nock(config.apiRoot)
-          .get(`/adviser/?limit=100000&offset=0`)
-          .reply(200, adviserData)
-      })
-      it('will not filter out any advisers', async () => {
-        const actual = await repos.getAdvisers({ token: '123' })
-        expect(actual.length).to.equal(5)
+        const actualAdvisers = await repos.getAdvisers({ token: '1234' })
+        const expectedAdvisers = [this.advisers[0], this.advisers[1]]
+        expect(actualAdvisers).to.deep.equal(expectedAdvisers)
       })
     })
 
@@ -62,12 +66,8 @@ describe('Adviser repository', () => {
           })
 
           it('should return just the active adviser', () => {
-            expect(this.advisersResult).to.deep.equal([{
-              id: '3',
-              first_name: 'Barney',
-              last_name: 'Rubble',
-              disabled_on: null,
-            }])
+            const expectedActiveAdvisers = [this.advisers[2]]
+            expect(this.advisersResult).to.deep.equal(expectedActiveAdvisers)
           })
         })
 
@@ -77,17 +77,8 @@ describe('Adviser repository', () => {
           })
 
           it('should return just the active adviser', () => {
-            expect(this.advisersResult).to.deep.equal([{
-              id: '2',
-              first_name: 'Wilma',
-              last_name: 'Flintstone',
-              disabled_on: '2017-01-01',
-            }, {
-              id: '3',
-              first_name: 'Barney',
-              last_name: 'Rubble',
-              disabled_on: null,
-            }])
+            const expectedActiveAdvisers = [this.advisers[1], this.advisers[2]]
+            expect(this.advisersResult).to.deep.equal(expectedActiveAdvisers)
           })
         })
       })
