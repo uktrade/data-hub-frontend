7cd87d837aa34b129bf4bfbce85133da2fce01a2
Zac Tolley <zac@thetolleys.com>
2017-11-29 11:23:21 +0000

    Update event edit controller for new active adviser call
    Modifed the event edit controller to use the common function to filter out inactive advisers.

--- src/apps/events/controllers/edit.js
+++ src/apps/events/controllers/edit.js
@@ -1,20 +1,20 @@
 /* eslint camelcase: 0 */
+const { assign, merge, get, pickBy } = require('lodash')
+
 const { eventFormConfig } = require('../macros')
 const { transformEventResponseToFormBody } = require('../transformers')
 const { buildFormWithStateAndErrors } = require('../../builders')
-const { assign, merge, get, pickBy } = require('lodash')
 const { getAdvisers } = require('../../adviser/repos')
-const { filterDisabledOption } = require('../../filters')
-
-async function getActiveAdvisers (token, currentAdviser) {
-  const allAdvisers = await getAdvisers(token)
-  return allAdvisers.results.filter(filterDisabledOption(currentAdviser))
-}
+const { filterActiveAdvisers } = require('../../adviser/filters')
 
 async function renderEditPage (req, res, next) {
   try {
     const eventData = transformEventResponseToFormBody(res.locals.event)
-    const advisers = await getActiveAdvisers(req.session.token, get(eventData, 'organiser.id'))
+
+    const currentAdviser = get(eventData, 'organiser.id')
+    const advisers = await getAdvisers(req.session.token)
+    const activeAdvisers = filterActiveAdvisers({ advisers: advisers.results, includeAdviser: currentAdviser })
+
     const eventId = get(eventData, 'id', '')
     const eventName = get(eventData, 'name')
     const lead_team = eventData.lead_team || get(req, 'session.user.dit_team.id')
@@ -22,7 +22,7 @@ async function renderEditPage (req, res, next) {
 
     const eventForm =
       buildFormWithStateAndErrors(
-        eventFormConfig({ eventId, advisers }),
+        eventFormConfig({ eventId, advisers: activeAdvisers }),
         mergedData,
         get(res.locals, 'form.errors.messages'),
       )
