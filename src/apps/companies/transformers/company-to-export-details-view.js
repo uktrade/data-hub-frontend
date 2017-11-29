6d65d8ee2d29c3661485160c1ac2e2db969325e5
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2017-11-29 15:42:38 +0000

    Present export win category in company export details view

--- src/apps/companies/transformers/company-to-export-details-view.js
+++ src/apps/companies/transformers/company-to-export-details-view.js
@@ -5,10 +5,12 @@ const { getDataLabels } = require('../../../lib/controller-utils')
 const { exportDetailsLabels } = require('../labels')
 
 module.exports = function transformCompanyToExportDetailsView ({
+  export_experience_category,
   export_to_countries,
   future_interest_countries,
 }) {
   const viewRecord = {
+    exportExperienceCategory: export_experience_category,
     exportToCountries: flatten([export_to_countries])
       .map(country => (country.name || null))
       .join(', '),
