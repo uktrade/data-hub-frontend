d36a27b159390706eb7285ba6636ab150316b89a
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2017-12-01 14:39:12 +0000

    Add POST route for updating account management details

--- src/apps/companies/middleware/account-management.js
+++ src/apps/companies/middleware/account-management.js
@@ -1,5 +1,6 @@
 const { getAdvisers } = require('../../adviser/repos')
 const { filterActiveAdvisers } = require('../../adviser/filters')
+const { updateCompany } = require('../repos')
 
 async function populateAccountManagementForm (req, res, next) {
   try {
@@ -12,6 +13,23 @@ async function populateAccountManagementForm (req, res, next) {
   }
 }
 
+async function postAccountManagementDetails (req, res, next) {
+  const data = {
+    one_list_account_owner: req.body.one_list_account_owner,
+  }
+
+  try {
+    const result = await updateCompany(req.session.token, res.locals.company.id, data)
+
+    req.flash('success', 'Account management details updated')
+
+    res.redirect(`/companies/${result.id}`)
+  } catch (error) {
+    next(error)
+  }
+}
+
 module.exports = {
   populateAccountManagementForm,
+  postAccountManagementDetails,
 }
--- test/unit/apps/companies/middleware/account-management.test.js
+++ test/unit/apps/companies/middleware/account-management.test.js
@@ -5,10 +5,10 @@ const nock = require('nock')
 const config = require('~/config')
 const adviserFilters = require('~/src/apps/adviser/filters')
 
-function createMiddleware (saveCompany, filterActiveAdvisers) {
+function createMiddleware (updateCompany, filterActiveAdvisers) {
   return proxyquire('~/src/apps/companies/middleware/account-management', {
     '../repos': {
-      saveCompany: saveCompany,
+      updateCompany: updateCompany,
     },
     '../../adviser/filters': {
       filterActiveAdvisers: filterActiveAdvisers,
@@ -19,9 +19,9 @@ function createMiddleware (saveCompany, filterActiveAdvisers) {
 describe('Companies account management middleware', () => {
   beforeEach(() => {
     this.sandbox = sinon.sandbox.create()
-    this.saveCompanyStub = this.sandbox.stub()
+    this.updateCompanyStub = this.sandbox.stub()
     this.filterActiveAdvisersSpy = this.sandbox.spy(adviserFilters, 'filterActiveAdvisers')
-    this.middleware = createMiddleware(this.saveCompanyStub.resolves({ id: '1' }), this.filterActiveAdvisersSpy)
+    this.middleware = createMiddleware(this.updateCompanyStub.resolves({ id: '1' }), this.filterActiveAdvisersSpy)
 
     this.reqMock = {
       session: {
@@ -86,6 +86,46 @@ describe('Companies account management middleware', () => {
     it('should set the active advisers', () => {
       const expectedAdvisers = filter(this.activeInactiveAdviserData.results, 'is_active')
       expect(this.resMock.locals.advisers).to.deep.equal(expectedAdvisers)
+    })
+  })
+
+  describe('#postAccountManagementDetails', () => {
+    context('when all fields are valid', () => {
+      beforeEach(async () => {
+        await this.middleware.postAccountManagementDetails(this.reqMock, this.resMock, this.nextSpy)
+      })
+
+      it('should flash an updated message', async () => {
+        expect(this.reqMock.flash).to.be.calledWith('success', 'Account management details updated')
+        expect(this.reqMock.flash).to.be.calledOnce
+      })
+
+      it('should redirect', async () => {
+        expect(this.resMock.redirect).to.be.calledWith(`/companies/${this.resMock.locals.company.id}`)
+        expect(this.resMock.redirect).to.be.calledOnce
+      })
+    })
+
+    context('when there is an error', () => {
+      beforeEach(async () => {
+        this.error = new Error('Error')
+        const middleware = createMiddleware(this.updateCompanyStub.rejects(this.error), this.filterActiveAdvisersSpy)
+
+        await middleware.postAccountManagementDetails(this.reqMock, this.resMock, this.nextSpy)
+      })
+
+      it('should not flash an updated message', async () => {
+        expect(this.reqMock.flash).to.not.be.called
+      })
+
+      it('should not redirect', async () => {
+        expect(this.resMock.redirect).to.not.be.called
+      })
+
+      it('should call next with an error', () => {
+        expect(this.nextSpy).to.have.been.calledWith(this.error)
+        expect(this.nextSpy).to.be.calledOnce
+      })
     })
   })
 })
