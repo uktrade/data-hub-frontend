54eb967546acdd60825c4805094120fe6acee629
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2017-12-19 13:42:56 +0000

    Remove redundant company create step

--- test/acceptance/features/companies/step_definitions/save.js
+++ test/acceptance/features/companies/step_definitions/save.js
@@ -2,19 +2,8 @@ const { client } = require('nightwatch-cucumber')
 const { defineSupportCode } = require('cucumber')
 const { set, get, assign } = require('lodash')
 
-defineSupportCode(({ Given, When }) => {
+defineSupportCode(({ When }) => {
   const Company = client.page.Company()
-
-  Given(/^a company is created$/, async function () {
-    await client
-      .url(this.urls.companies.search)
-
-    await Company
-      .createUkNonPrivateOrNonPublicLimitedCompany({}, (company) => {
-        set(this.state, 'company', company)
-      })
-      .wait() // wait for backend to sync
-  })
 
   When(/^the Account management details are updated$/, async function () {
     await Company
