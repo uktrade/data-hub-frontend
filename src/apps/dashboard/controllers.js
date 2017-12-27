d6e1fbc776f505ed1fd490ff5e8ad695c103e69f
Ben Chidgey <ben.chidgey@digital.trade.gov.uk>
2017-12-27 14:41:13 +0000

    handle permissions for showing Interactions on dashboard

--- src/apps/dashboard/controllers.js
+++ src/apps/dashboard/controllers.js
@@ -1,7 +1,13 @@
+const { get } = require('lodash')
+
+const { GLOBAL_NAV_ITEMS } = require('../constants')
+
+const { isPermittedRoute } = require('../middleware')
 const { fetchHomepageData } = require('./repos')
 
 async function renderDashboard (req, res, next) {
   try {
+    const userPermissions = get(res, 'locals.user.permissions')
     const {
       contacts,
       interactions,
@@ -12,6 +18,7 @@ async function renderDashboard (req, res, next) {
       .render('dashboard/views/dashboard', {
         contacts,
         interactions,
+        interactionsPermitted: isPermittedRoute('/interactions', GLOBAL_NAV_ITEMS, userPermissions),
       })
   } catch (error) {
     next(error)
