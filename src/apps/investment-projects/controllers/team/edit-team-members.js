3c55cc5804fec665165ac13083640da830e5334d
Zac Tolley <zac@thetolleys.com>
2018-01-08 23:54:22 +0000

    Simplified edit team controller
    The controller for editing a team really only needs to one thing, render the form. The extra controller to set a flash message wasn't really needed and belongs in the middleware that saves the record.

--- src/apps/investment-projects/controllers/team/edit-team-members.js
+++ src/apps/investment-projects/controllers/team/edit-team-members.js
@@ -1,22 +1,10 @@
-const { isEmpty } = require('lodash')
-
-function getHandler (req, res, next) {
+function renderTeamEdit (req, res, next) {
   res
     .breadcrumb('Project team', 'team')
     .breadcrumb('Team members')
     .render('investment-projects/views/team/edit-team-members')
 }
 
-function postHandler (req, res, next) {
-  if (isEmpty(res.locals.form.errors)) {
-    req.flash('success', 'Investment details updated')
-    return res.redirect(`/investment-projects/${res.locals.investmentData.id}/team`)
-  }
-
-  return next()
-}
-
 module.exports = {
-  getHandler,
-  postHandler,
+  renderTeamEdit,
 }
