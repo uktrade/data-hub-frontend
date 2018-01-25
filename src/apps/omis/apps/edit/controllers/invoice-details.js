59ecb7492747acba3f109f7cf225d9a935dff21f
Dom Smith <dom@teneightfive.com>
2018-01-25 00:13:35 +0000

    Add ability to edit invoice details
    This checks for the presence of the property which allows the invoice
    details to be edited.
    
    If present it shows the edit action for invoice details as well as
    activating the submit buttons on those particular forms.

---
+++ src/apps/omis/apps/edit/controllers/invoice-details.js
@@ -0,0 +1,14 @@
+const { get } = require('lodash')
+
+const { EditController } = require('../../../controllers')
+
+class EditInvoiceDetailsController extends EditController {
+  configure (req, res, next) {
+    if (req.form.options.disableFormAction) {
+      req.form.options.disableFormAction = !get(res.locals, 'order.canEditInvoiceDetails')
+    }
+    super.configure(req, res, next)
+  }
+}
+
+module.exports = EditInvoiceDetailsController
