91e500c94d4ebeefbfcd542504fd50afd2a87715
Ben Chidgey <ben.chidgey@digital.trade.gov.uk>
2018-02-08 18:02:42 +0000

    Company click edit button step definition
    A simple click edit button step definition currently being used on the
    company details page. Potentially in the future this work can be used
    globally.

---
+++ test/acceptance/features/companies/step_definitions/edit.js
@@ -0,0 +1,16 @@
+const { client } = require('nightwatch-cucumber')
+const { When } = require('cucumber')
+
+const { getSelectorForDetailsSectionEditButton } = require('../../../helpers/selectors')
+const Company = client.page.companies.Company()
+
+// TODO this could well become something generic used across all buttons
+When(/^I click the (.+) edit button/, async (linkTextContent) => {
+  const { selector: editButton } = getSelectorForDetailsSectionEditButton(linkTextContent)
+
+  await Company
+    .api.useXpath()
+    .waitForElementVisible(editButton)
+    .click(editButton)
+    .useCss()
+})
