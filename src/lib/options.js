53e1830c58bffeeb36412921ee34b807a0a6ddca
Zac Tolley <zac@thetolleys.com>
2018-02-09 11:41:02 +0000

    Update getOptions to filter for a given term
    Allows getting a list of options that start with a given term.

--- src/lib/options.js
+++ src/lib/options.js
@@ -5,12 +5,19 @@ const authorisedRequest = require('../lib/authorised-request')
 const { filterDisabledOption } = require('../apps/filters')
 const { transformObjectToOption } = require('../apps/transformers')
 
-async function getOptions (token, key, { createdOn, currentValue, includeDisabled = false, sorted = true } = {}) {
+async function getOptions (token, key, { createdOn, currentValue, includeDisabled = false, sorted = true, term } = {}) {
   const url = `${config.apiRoot}/metadata/${key}/`
   let options = await authorisedRequest(token, url)
 
   if (!includeDisabled) {
     options = options.filter(filterDisabledOption({ currentValue, createdOn }))
+  }
+
+  if (term) {
+    const lterm = term.toLowerCase()
+    options = options.filter((option) => {
+      return option.name.toLowerCase().startsWith(lterm)
+    })
   }
 
   const mappedOptions = options.map(transformObjectToOption)
