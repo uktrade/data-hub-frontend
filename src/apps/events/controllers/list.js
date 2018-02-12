68ffef09f68a740df498e0865bd2a10ed4c4266e
Zac Tolley <zac@thetolleys.com>
2018-02-12 20:39:11 +0000

    Added event facets
    More complex than eventa has a number of
    base fields including organiser. This means
    you have to be carefull deciding when to
    calculate facets and conditionally build
    the organiser filters taking into consideration
    the current orgainser.

--- src/apps/events/controllers/list.js
+++ src/apps/events/controllers/list.js
@@ -1,40 +1,26 @@
 const { get, omit, merge, assign } = require('lodash')
 const { eventFiltersFields, eventSortForm } = require('../macros')
 const { buildSelectedFiltersSummary } = require('../../builders')
-const { getAdvisers } = require('../../adviser/repos')
 
-async function renderEventList (req, res, next) {
-  try {
-    const advisers = await getAdvisers(req.session.token)
-    const filtersFields = eventFiltersFields({ advisers: advisers.results })
-    const selectedFilters = buildSelectedFiltersSummary(filtersFields, req.query)
-    const isUKSelected = get(selectedFilters, 'address_country.valueLabel') === 'United Kingdom'
+function renderEventList (req, res, next) {
+  const currentAdviserId = req.session.user.id
+  const filtersFields = eventFiltersFields({ currentAdviserId, ...res.locals.facets })
+  const selectedFilters = buildSelectedFiltersSummary(filtersFields, req.query)
 
-    const sortForm = merge({}, eventSortForm, {
-      hiddenFields: assign({}, omit(req.query, 'sortby')),
-      children: [
-        { value: req.query.sortby },
-      ],
-    })
+  const sortForm = merge({}, eventSortForm, {
+    hiddenFields: assign({}, omit(req.query, 'sortby')),
+    children: [
+      { value: req.query.sortby },
+    ],
+  })
 
-    const visibleFiltersFields = filtersFields.filter(field => {
-      if (field.name === 'uk_region') {
-        return isUKSelected
-      }
-      return true
-    })
-
-    res.render('events/views/list', {
-      sortForm,
-      selectedFilters,
-      title: 'Events',
-      countLabel: 'event',
-      highlightTerm: get(selectedFilters, 'name.valueLabel'),
-      filtersFields: visibleFiltersFields,
-    })
-  } catch (error) {
-    next(error)
-  }
+  res.render('events/views/list', {
+    sortForm,
+    selectedFilters,
+    filtersFields,
+    countLabel: 'event',
+    highlightTerm: get(selectedFilters, 'name.valueLabel'),
+  })
 }
 
 module.exports = {
