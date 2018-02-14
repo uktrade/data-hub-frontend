8e307abcf464f5f41697a60d5d738c1bb002d9e5
Dom Smith <dom@teneightfive.com>
2018-02-14 17:17:32 +0000

    Use HMPO form wizard middleware to set company local
    This uses the concept of middleware for the form wizard to set a
    company locals property that will be available within the views if
    there is a company ID set within the session model.
    
    The company is used across a few views within this journey and is
    currently set using the shared middleware from the OMIS app to get
    it from a URL param.
    
    The new journey will no longer contain an ID within the URL so needs
    an alternative way to be set.

--- test/unit/apps/omis/controllers/create.test.js
+++ test/unit/apps/omis/controllers/create.test.js
@@ -1,3 +1,136 @@
-describe('OMIS CreateController', () => {
+const { assign } = require('lodash')
 
+const FormController = require('~/src/apps/omis/controllers/form')
+const companyMock = require('~/test/unit/data/company.json')
+
+describe('OMIS CreateController', () => {
+  beforeEach(() => {
+    this.nextSpy = sandbox.spy()
+    this.errorSpy = sandbox.spy()
+    this.getDitCompanyStub = sandbox.stub().resolves(companyMock)
+
+    this.ControllerClass = proxyquire('~/src/apps/omis/controllers/create', {
+      '../../companies/repos': {
+        getDitCompany: this.getDitCompanyStub,
+      },
+      '../../../../config/logger': {
+        error: this.errorSpy,
+      },
+    })
+
+    this.controller = new this.ControllerClass({ route: '/' })
+  })
+
+  describe('middlewareLocals()', () => {
+    beforeEach(() => {
+      sandbox.stub(FormController.prototype, 'middlewareLocals')
+      sandbox.stub(this.controller, 'use')
+
+      this.controller.middlewareLocals()
+    })
+
+    it('should call parent method', () => {
+      expect(FormController.prototype.middlewareLocals).to.have.been.calledOnce
+    })
+
+    it('should call set company method', () => {
+      expect(this.controller.use).to.have.been.calledWith(this.controller.setCompany)
+    })
+  })
+
+  describe('setCompany()', () => {
+    context('when a company exists in the session', () => {
+      beforeEach(async () => {
+        this.reqMock = assign({}, globalReq, {
+          session: {
+            token: '12345',
+          },
+          sessionModel: {
+            get: () => companyMock.id,
+          },
+        })
+        this.resMock = {
+          locals: {},
+        }
+      })
+
+      context('when company promise is resolved', () => {
+        beforeEach(async () => {
+          await this.controller.setCompany(this.reqMock, this.resMock, this.nextSpy)
+        })
+
+        it('should call get company method', () => {
+          expect(this.getDitCompanyStub).to.be.calledOnce
+          expect(this.getDitCompanyStub).to.be.calledWith('12345', companyMock.id)
+        })
+
+        it('should set company property on locals', () => {
+          expect(this.resMock.locals).to.have.property('company')
+          expect(this.resMock.locals.company).to.deep.equal(companyMock)
+        })
+
+        it('should not log an error', () => {
+          expect(this.errorSpy).not.to.be.called
+        })
+
+        it('should call next', () => {
+          expect(this.nextSpy).to.be.calledOnce
+          expect(this.nextSpy).to.be.calledWith()
+        })
+      })
+
+      context('when company promise is rejected', () => {
+        beforeEach(async () => {
+          this.errorMock = new Error()
+          this.getDitCompanyStub.rejects(this.errorMock)
+
+          await this.controller.setCompany(this.reqMock, this.resMock, this.nextSpy)
+        })
+
+        it('should call get company method', () => {
+          expect(this.getDitCompanyStub).to.be.calledOnce
+          expect(this.getDitCompanyStub).to.be.calledWith('12345', companyMock.id)
+        })
+
+        it('should not set company property on locals', () => {
+          expect(this.resMock.locals).not.to.have.property('company')
+        })
+
+        it('should log an error', () => {
+          expect(this.errorSpy).to.be.calledOnce
+          expect(this.errorSpy).to.be.calledWith(this.errorMock)
+        })
+
+        it('should call next', () => {
+          expect(this.nextSpy).to.be.calledOnce
+          expect(this.nextSpy).to.be.calledWith()
+        })
+      })
+    })
+
+    context('when a company doesn\'t exist in the session', () => {
+      beforeEach(async () => {
+        const reqMock = assign({}, globalReq, {
+          sessionModel: {
+            get: () => false,
+          },
+        })
+
+        await this.controller.setCompany(reqMock, {}, this.nextSpy)
+      })
+
+      it('should not call get company promise', () => {
+        expect(this.getDitCompanyStub).not.to.be.called
+      })
+
+      it('should not log an error', () => {
+        expect(this.errorSpy).not.to.be.called
+      })
+
+      it('should call next', () => {
+        expect(this.nextSpy).to.be.calledOnce
+        expect(this.nextSpy).to.be.calledWith()
+      })
+    })
+  })
 })
