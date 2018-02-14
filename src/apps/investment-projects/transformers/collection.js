4a796621fcd318ad81a93c81d8a9bb0e710333ef
Zac Tolley <zac@thetolleys.com>
2018-02-14 17:30:35 +0000

    Moved project code in invest list to sub-title

--- src/apps/investment-projects/transformers/collection.js
+++ src/apps/investment-projects/transformers/collection.js
@@ -6,19 +6,19 @@ const labels = require('../labels')
 function transformInvestmentProjectToListItem ({
   id,
   name,
-  project_code,
   stage,
   investment_type,
   investor_company,
   estimated_land_date,
   sector,
+  project_code,
 }) {
   const metaItems = [
     { key: 'stage', value: stage, type: 'badge' },
     { key: 'investment_type', value: investment_type, type: 'badge', badgeModifier: 'secondary' },
     { key: 'investor_company', value: investor_company },
-    { key: 'estimated_land_date', value: estimated_land_date, type: 'dateMonthYear', isInert: true },
     { key: 'sector', value: sector },
+    { key: 'estimated_land_date', value: estimated_land_date, type: 'dateMonthYear', isInert: true },
   ].map(({ key, value, type, badgeModifier, isInert }) => {
     if (!value) return
     return assign({}, pickBy({ value, type, badgeModifier, isInert }), {
@@ -29,8 +29,11 @@ function transformInvestmentProjectToListItem ({
   return {
     id,
     name,
+    subTitle: {
+      value: project_code,
+      label: 'Project code',
+    },
     type: 'investment-project',
-    code: project_code,
     meta: compact(metaItems),
   }
 }
