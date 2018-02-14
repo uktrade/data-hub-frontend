8a8f08d74754e55c300f56d582c853fd9ca677c1
Dom Smith <dom@teneightfive.com>
2018-02-14 17:17:32 +0000

    Use HMPO form wizard middleware to set company local
    This uses the concept of middleware for the form wizard to set a
    company locals property that will be available within the views if
    there is a company ID set within the session model.
    
    The company is used across a few views within this journey and is
    currently set using the shared middleware from the OMIS app to get
    it from a URL param.
    
    The new journey will no longer contain an ID within the URL so needs
    an alternative way to be set.

--- src/apps/omis/controllers/create.js
+++ src/apps/omis/controllers/create.js
@@ -1,6 +1,27 @@
 const FormController = require('./form')
+const logger = require('../../../../config/logger')
+const { getDitCompany } = require('../../companies/repos')
 
 class CreateController extends FormController {
+  middlewareLocals () {
+    super.middlewareLocals()
+
+    this.use(this.setCompany)
+  }
+
+  async setCompany (req, res, next) {
+    const companyId = req.sessionModel.get('company')
+
+    if (companyId) {
+      try {
+        res.locals.company = await getDitCompany(req.session.token, companyId)
+      } catch (error) {
+        logger.error(error)
+      }
+    }
+
+    next()
+  }
 }
 
 module.exports = CreateController
