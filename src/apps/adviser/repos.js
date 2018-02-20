47fd7d104c3a9425d25158658724777a5a08afb9
Zac Tolley <zac@thetolleys.com>
2018-02-20 15:41:08 +0000

    Update adviser repos to use async await
    The previous version of adviser search was written
    before moving to node 8, so was using promises,
    this new version switches to async/await and is
    more consistent tiwh current code styles.
    
    Also added tests to confirm adviser search works
    as expected.

--- src/apps/adviser/repos.js
+++ src/apps/adviser/repos.js
@@ -1,12 +1,7 @@
-const { get } = require('lodash')
+const { get, isEmpty } = require('lodash')
 const config = require('../../../config')
-const logger = require('../../../config/logger')
 const authorisedRequest = require('../../lib/authorised-request')
 
-// TODO: Make sure this is removed and replaced with a better way to
-// filter/select advisers
-// TODO: advisers with no name are filtered on the front end. Make changes on
-// the back end to make this not necessary.
 function getAdvisers (token) {
   return authorisedRequest(token, `${config.apiRoot}/adviser/?limit=100000&offset=0`)
     .then(response => {
@@ -23,43 +18,41 @@ function getAdviser (token, id) {
   return authorisedRequest(token, `${config.apiRoot}/adviser/${id}/`)
 }
 
-function adviserSearch (token, term) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      if (!term || term.trim().length === 0) {
-        return resolve(null)
+function fetchAdviserSearchResults (token, firstName, lastName) {
+  let url = `${config.apiRoot}/adviser/?first_name__icontains=${firstName}`
+  if (!isEmpty(lastName)) url += `&last_name__icontains=${lastName}`
+
+  return authorisedRequest(token, { url })
+}
+
+function adviserNameSort (a, b) {
+  if (a.name.toLowerCase() < b.name.toLowerCase()) return -1
+  if (a.name.toLowerCase() > b.name.toLowerCase()) return 1
+  return 0
+}
+
+async function adviserSearch (token, term) {
+  if (isEmpty(term)) {
+    return
+  }
+
+  const [firstName, lastName] = term.trim().toLowerCase().split(' ')
+  const adviserResults = await fetchAdviserSearchResults(token, firstName, lastName)
+
+  // API only supports contains, so filter out results that don't start with term
+  // Then reduce the result down to id and name
+  // And finally sort things
+  const filteredAdvisers = adviserResults.results
+    .filter((adviser) => {
+      if (isEmpty(lastName)) {
+        return adviser.first_name.toLowerCase().startsWith(firstName)
       }
+      return adviser.first_name.toLowerCase().startsWith(firstName) &&
+        adviser.last_name.toLowerCase().startsWith(lastName)
+    })
+    .sort(adviserNameSort)
 
-      const parts = term.trim().toLowerCase().split(' ')
-      let url = `${config.apiRoot}/adviser/?first_name__icontains=${parts[0]}`
-      if (parts.length > 1) url += `&last_name__icontains=${parts[1]}`
-
-      const data = await authorisedRequest(token, { url })
-
-      // API only supports contains, so filter out results that don't start with term
-      // Then reduce the result down to id and name
-      // And finally sort things
-      const filtered = data.results.filter((adviser) => {
-        if (parts.length === 1) {
-          return adviser.first_name.toLowerCase().startsWith(parts[0])
-        }
-        return adviser.first_name.toLowerCase().startsWith(parts[0]) && adviser.last_name.toLowerCase().startsWith(parts[1])
-      })
-        .map((adviser) => {
-          return { id: adviser.id, name: `${adviser.first_name} ${adviser.last_name}` }
-        })
-        .sort((a, b) => {
-          if (a.name.toLowerCase() < b.name.toLowerCase()) return -1
-          if (a.name.toLowerCase() > b.name.toLowerCase()) return 1
-          return 0
-        })
-
-      resolve(filtered)
-    } catch (error) {
-      logger.error(error)
-      reject(error)
-    }
-  })
+  return filteredAdvisers
 }
 
 module.exports = {
