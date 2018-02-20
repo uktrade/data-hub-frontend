e2f236cafe5ef2f2b5dcb55e836171bcb9395de8
Zac Tolley <zac@thetolleys.com>
2018-02-20 14:10:23 +0000

    Add API endpoint to get metadata options
    Adds an endpoint that an ajax client can use to get
    a list of options and optionally pass a term to select
    a subset of those options

--- src/apps/api/router.js
+++ src/apps/api/router.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 
 const { postcodeLookupHandler } = require('./controllers')
+const { getOptionsHandler } = require('./controllers')
 
 router.get('/postcodelookup/:postcode', postcodeLookupHandler)
+router.get('/options/:entity', getOptionsHandler)
 
 module.exports = router
