522faf5c985205137bb4c161d951cf457edb7924
Zac Tolley <zac@thetolleys.com>
2018-02-27 17:24:45 +0000

    Always show region filter for the events list
    Users are unaware of the region filter, and do not realise that
    they need to select the UK in order to see the region filter.
    
    This change simply makes the region filter always visible.

--- src/apps/events/controllers/list.js
+++ src/apps/events/controllers/list.js
@@ -1,4 +1,4 @@
-const { get, omit, merge, assign } = require('lodash')
+const { get, omit, merge } = require('lodash')
 const { eventFiltersFields, eventSortForm } = require('../macros')
 const { buildSelectedFiltersSummary } = require('../../builders')
 const { getAdvisers } = require('../../adviser/repos')
@@ -8,29 +8,21 @@ async function renderEventList (req, res, next) {
     const advisers = await getAdvisers(req.session.token)
     const filtersFields = eventFiltersFields({ advisers: advisers.results })
     const selectedFilters = buildSelectedFiltersSummary(filtersFields, req.query)
-    const isUKSelected = get(selectedFilters, 'address_country.valueLabel') === 'United Kingdom'
 
     const sortForm = merge({}, eventSortForm, {
-      hiddenFields: assign({}, omit(req.query, 'sortby')),
+      hiddenFields: omit(req.query, 'sortby'),
       children: [
         { value: req.query.sortby },
       ],
     })
 
-    const visibleFiltersFields = filtersFields.filter(field => {
-      if (field.name === 'uk_region') {
-        return isUKSelected
-      }
-      return true
-    })
-
     res.render('events/views/list', {
       sortForm,
+      filtersFields,
       selectedFilters,
       title: 'Events',
       countLabel: 'event',
       highlightTerm: get(selectedFilters, 'name.valueLabel'),
-      filtersFields: visibleFiltersFields,
     })
   } catch (error) {
     next(error)
