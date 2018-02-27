b6a20c3154eee2e347fcb08d67673c98f3a56107
Dom Smith <dom@teneightfive.com>
2018-02-27 10:57:29 +0000

    Add support for a new shared fixture step definition
    This creates a new step definition for navigating to fixtures directly
    rather than via search.

--- test/acceptance/features/step_definitions/common.js
+++ test/acceptance/features/step_definitions/common.js
@@ -1,6 +1,8 @@
-const { get } = require('lodash')
+const { assign, camelCase, find, get, set } = require('lodash')
 const { client } = require('nightwatch-cucumber')
 const { When } = require('cucumber')
+
+const fixtures = require('../../features/setup/fixtures')
 
 When(/^I (?:navigate|go|open|visit).*? `(.+)` page$/, async function (pageName) {
   try {
@@ -9,4 +11,16 @@ When(/^I (?:navigate|go|open|visit).*? `(.+)` page$/, async function (pageName) 
   } catch (error) {
     throw new Error(`The page object '${pageName}' does not exist`)
   }
+})
+
+When(/^I (?:navigate|go|open|visit).*? `(.+)` page using `(.+)` `(.+)` fixture$/, async function (pageName, entity, fixtureName) {
+  const Page = get(client.page, pageName)()
+
+  const fixture = find(fixtures[entity], { name: fixtureName })
+
+  // TODO: Need to find a way to remove needing to store the item in state
+  const entityTypeFieldName = camelCase(entity)
+  set(this.state, entityTypeFieldName, assign({}, get(this.state, entityTypeFieldName), fixture))
+
+  await Page.navigate(Page.url(fixtureName))
 })
