f046230314059fb720fbabdd6b3dc07a515c6660
Ben Chidgey <ben.chidgey@digital.trade.gov.uk>
2018-03-01 17:32:18 +0000

    Add headquarters filters
    Add in headquarter filters to companies collections
    Update reference to labels const

--- src/apps/companies/middleware/collection.js
+++ src/apps/companies/middleware/collection.js
@@ -1,5 +1,6 @@
 const { pick, pickBy, assign } = require('lodash')
 
+const removeArray = require('../../../lib/remove-array')
 const { search, searchLimitedCompanies } = require('../../search/services')
 const { transformApiResponseToSearchCollection } = require('../../search/transformers')
 const {
@@ -58,13 +59,52 @@ async function getLimitedCompaniesCollection (req, res, next) {
   }
 }
 
+async function getGlobalHQCompaniesCollection (req, res, next) {
+  const searchTerm = res.locals.searchTerm = req.query.term
+  const globalHQId = '43281c5e-92a4-4794-867b-b4d5f801e6f3'
+  const { id: companyId } = res.locals.company
+
+  if (!searchTerm) {
+    return next()
+  }
+
+  try {
+    res.locals.results = await search({
+      searchTerm,
+      searchEntity: 'company',
+      requestBody: {
+        ...req.body,
+        headquarter_type: globalHQId,
+      },
+      token: req.session.token,
+      page: req.query.page,
+      isAggregation: false,
+    })
+      .then(transformApiResponseToSearchCollection(
+        { query: req.query },
+        transformCompanyToListItem,
+        (item) => {
+          return {
+            ...item,
+            url: `/companies/${companyId}/hierarchies/ghq/${item.id}/add`,
+          }
+        }
+      ))
+
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
+
 function getRequestBody (req, res, next) {
-  const selectedFiltersQuery = pick(req.query, [
+  const selectedFiltersQuery = removeArray(pick(req.query, [
     'name',
     'sector',
     'country',
     'uk_region',
-  ])
+    'headquarter_type',
+  ]), 'headquarter_type')
 
   const selectedSortBy = req.query.sortby ? {
     sortby: req.query.sortby,
