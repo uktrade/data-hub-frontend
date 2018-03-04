e2868caada8bf36c86c1f08e4c795e1e04966bc6
Dom Smith <dom@teneightfive.com>
2018-03-04 19:55:55 +0000

    Rename pk to id in fixtures
    In a previous pull request it was pointed out that `pk` is more of a
    django term that a term we use on the frontend for referencing an
    entity.
    
    This change renames the `pk` property to `id`.

--- test/acceptance/pages/events/Fixture.js
+++ test/acceptance/pages/events/Fixture.js
@@ -5,7 +5,7 @@ const { event } = require('../../fixtures')
 module.exports = {
   url: function eventFixtureUrl (eventName) {
     const fixture = find(event, { name: eventName })
-    const eventId = fixture ? fixture.pk : event.oneDayExhibition.pk
+    const eventId = fixture ? fixture.id : event.oneDayExhibition.id
 
     return `${process.env.QA_HOST}/events/${eventId}`
   },
--- test/acceptance/pages/interactions/Fixture.js
+++ test/acceptance/pages/interactions/Fixture.js
@@ -5,7 +5,7 @@ const { interaction } = require('../../fixtures')
 module.exports = {
   url: function interactionFixtureUrl (interactionName) {
     const fixture = find(interaction, { subject: interactionName })
-    const interactionId = fixture ? fixture.pk : interaction.attendedGammaEvent.pk
+    const interactionId = fixture ? fixture.id : interaction.attendedGammaEvent.id
 
     return `${process.env.QA_HOST}/interactions/${interactionId}`
   },
