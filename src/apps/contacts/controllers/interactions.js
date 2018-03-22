ad293d8dad808f8132c8aa348c17475064b03fc0
Zac Tolley <zac@thetolleys.com>
2018-03-22 11:47:53 +0000

    Update contacts to use addButton
    Updated the contact collection pages
    to use the new addButton property

--- src/apps/contacts/controllers/interactions.js
+++ src/apps/contacts/controllers/interactions.js
@@ -2,7 +2,12 @@ function renderInteractions (req, res, next) {
   try {
     res
       .breadcrumb('Interactions')
-      .render('contacts/views/interactions')
+      .render('contacts/views/interactions', {
+        actionButtons: [{
+          label: 'Add interaction',
+          url: `/contacts/${req.params.contactId}/interactions/create`,
+        }],
+      })
   } catch (error) {
     next(error)
   }
