76082a793e3c3848d9db754920c26215e2034fad
Reupen Shah <reupen@users.noreply.github.com>
2018-03-23 15:03:01 +0000

    Use business type provided by the API when adding a CH company
    DH-1607

--- src/apps/companies/transformers/company-to-form.js
+++ src/apps/companies/transformers/company-to-form.js
@@ -1,13 +1,10 @@
 /* eslint-disable camelcase */
 const {
   assign,
-  find,
   get,
   isPlainObject,
   mapValues,
 } = require('lodash')
-
-const metadataRepository = require('../../../lib/metadata')
 
 // TODO: This is a temporary transformer to transform an API response into
 // a format needed for the form view
@@ -34,15 +31,6 @@ module.exports = function transformCompanyToForm (body) {
     }
     return get(body, `${key}.id`)
   })
-
-  // TODO we need to create an "other" business_type on DH to place any Companies House company_category that do not match DH business_type
-  if (body.company_category) {
-    const businessType = find(metadataRepository.businessTypeOptions, (type) => {
-      return type.name.toLowerCase() === body.company_category.toLowerCase()
-    })
-
-    formatted.business_type = get(businessType, 'id')
-  }
 
   formatted.headquarter_type = get(formatted, 'headquarter_type.id', 'not_headquarters')
 
