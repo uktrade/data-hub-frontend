72dfdbde8888a2ece492aff0685c1d6879b6e555
Alex Foxleigh <alex@alexfoxleigh.com>
2018-03-29 18:08:22 +0100

    Wrote acceptance tests for heirarchies
    - Sets a company up as a GHQ
    - Adds a subsidiary
    - Checks there are subsidaries once added
    - Removes subsidiary
    - Sets company back to 'not a headquarters'

--- src/apps/companies/controllers/subsidiaryLink.js
+++ src/apps/companies/controllers/subsidiaryLink.js
@@ -1,9 +1,10 @@
 function renderLinkSubsidiary (req, res, next) {
   const { id: companyId, name: companyName } = res.locals.company
+  const { companyDetailsLabels } = require('../labels')
 
   res
     .breadcrumb(companyName, `/companies/${companyId}`)
-    .breadcrumb('Link Subsidiary')
+    .breadcrumb(companyDetailsLabels.link_subsidiary)
     .render('companies/views/link-subsidiary.njk')
 }
 
