a8120dfdcc108fd0a48009540abba2fd9b7afa59
John Fieldman <hello@johnfieldman.com>
2018-04-16 14:05:21 +0100

    DH-1140 revert deleted files from the api controller refactoring

--- test/acceptance/features/step_definitions/details.js
+++ test/acceptance/features/step_definitions/details.js
@@ -32,12 +32,11 @@ const removeFalsey = (details, state) => {
   })
 }
 
-const AssertDetailsTableRowCount = function () {
-  return async function (detailsTableSelector, expectedDetails) {
-    await Details.api.elements('xpath', `${detailsTableSelector.selector}//th`, (result) => {
-      client.expect(result.value.length, 'Table row count').to.equal(expectedDetails.length)
-    })
-  }
+const assertDetailsTableRowCount = async function (detailsTableSelector, expectedDetails) {
+  await Details.api.elements('xpath', `${detailsTableSelector.selector}//th`, (result) => {
+    console.log('>>>>>>>>>>>>>>>>>>>>>> ', result)
+    client.expect(result.value.length, 'Table row count').to.equal(expectedDetails.length)
+  })
 }
 
 const assertDetailsTableContent = async function (detailsTableSelector, expectedDetails) {
@@ -132,17 +131,15 @@ Then(/^view should (not\s?)?contain the Documents link$/, async (noDocumentsLink
 Then(/^the (.+) details are displayed$/, async function (detailsTableTitle, dataTable) {
   const expectedDetails = removeFalsey(dataTable.hashes(), this.state)
   const detailsTableSelector = Details.getSelectorForDetailsTable(detailsTableTitle)
-  const assertDetailsTableRowCount = new AssertDetailsTableRowCount(detailsTableSelector, expectedDetails)
 
-  await assertDetailsTableRowCount
+  await assertDetailsTableRowCount(detailsTableSelector, expectedDetails)
   await assertDetailsTableContent.bind(this)(detailsTableSelector, expectedDetails)
 })
 
 Then(/^the details are displayed$/, async function (dataTable) {
   const expectedDetails = removeFalsey(dataTable.hashes(), this.state)
   const detailsTableSelector = Details.getSelectorForDetailsTable()
-  const assertDetailsTableRowCount = new AssertDetailsTableRowCount(detailsTableSelector, expectedDetails)
 
-  await assertDetailsTableRowCount
+  await assertDetailsTableRowCount(detailsTableSelector, expectedDetails)
   await assertDetailsTableContent.bind(this)(detailsTableSelector, expectedDetails)
 })
