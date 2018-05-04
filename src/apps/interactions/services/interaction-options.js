b26a67c6bb0ddc8f0159adfbb4e2a065d19931e4
Zac Tolley <zac@thetolleys.com>
2018-05-05 00:15:09 +0100

    Update policy feedback form and add support in controller
    Updates the options and edit controller for interactions
    to support a new type, policy feedback.
    
    Also updated the previous form definition to add type and area

--- src/apps/interactions/services/interaction-options.js
+++ src/apps/interactions/services/interaction-options.js
@@ -11,6 +11,7 @@ const SERVICE_DELIVERY_STATUS_COMPLETED = '47329c18-6095-e211-a939-e4115bead28a'
 
 async function getInteractionOptions (req, res) {
   const token = req.session.token
+  const kind = req.params.kind
   const createdOn = get(res.locals, 'interaction.created_on')
   const companyId = get(res.locals, 'company.id')
 
@@ -27,26 +28,34 @@ async function getInteractionOptions (req, res) {
     advisers: activeAdvisers.map(transformObjectToOption),
     contacts: contacts.filter(contact => !contact.archived).map(transformContactToOption),
     teams: await getOptions(token, 'team', { createdOn }),
-    services: await getServiceOptions(req, res, createdOn),
   }
 
-  options.tapServices = options.services
-    .filter(service => includes(service.label, '(TAP)'))
-    .map(service => service.value)
+  if (kind === 'policy-feedback') {
+    options.areas = await getOptions(token, 'policy-area', { createdOn })
+    options.types = await getOptions(token, 'policy-issue-type', { createdOn })
+    options.channels = await getOptions(token, 'communication-channel', { createdOn })
+    return options
+  }
 
-  options.successfulServiceStatuses = [
-    SERVICE_DELIVERY_STATUS_COMPLETED,
-  ]
+  options.services = await getServiceOptions(req, res, createdOn)
 
-  if (req.params.kind === 'service-delivery') {
+  if (kind === 'service-delivery') {
+    options.tapServices = options.services
+      .filter(service => includes(service.label, '(TAP)'))
+      .map(service => service.value)
+
+    options.successfulServiceStatuses = [
+      SERVICE_DELIVERY_STATUS_COMPLETED,
+    ]
+
     const activeEvents = await getActiveEvents(token, createdOn)
     options.events = activeEvents.map(transformObjectToOption)
 
     options.statuses = await getOptions(token, 'service-delivery-status', { createdOn, sorted: false })
-  } else {
-    options.channels = await getOptions(token, 'communication-channel', { createdOn })
+    return options
   }
 
+  options.channels = await getOptions(token, 'communication-channel', { createdOn })
   return options
 }
 
