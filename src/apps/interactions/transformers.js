4f05441f1cf78396db6ad605907bbcefa7fce8a3
Zac Tolley <zac@thetolleys.com>
2018-05-11 12:45:02 +0100

    Add support for policy feedback to interaction list

--- src/apps/interactions/transformers.js
+++ src/apps/interactions/transformers.js
@@ -1,5 +1,5 @@
 /* eslint-disable camelcase */
-const { get, assign, isNil, mapKeys, pickBy, camelCase } = require('lodash')
+const { get, assign, isNil, pickBy, camelCase } = require('lodash')
 const { format, isValid } = require('date-fns')
 
 const { transformDateObjectToDateString } = require('../transformers')
@@ -128,6 +128,8 @@ function transformInteractionResponseToViewRecord ({
   net_company_receipt,
   dit_team,
   contact,
+  policy_issue_type,
+  policy_area,
   investment_project,
   communication_channel,
   event,
@@ -142,6 +144,8 @@ function transformInteractionResponseToViewRecord ({
     dit_team,
     service,
     service_delivery_status,
+    policy_issue_type,
+    policy_area,
     grant_amount_offered: grant_amount_offered ? {
       type: 'currency',
       name: grant_amount_offered,
@@ -163,9 +167,15 @@ function transformInteractionResponseToViewRecord ({
     documents: transformDocumentsLink(archived_documents_url_path),
   }
 
-  return pickBy(mapKeys(transformed, (value, key) => {
-    return kindLabels[key]
-  }))
+  const result = {}
+  Object.keys(transformed).forEach((key) => {
+    const label = kindLabels[key]
+    if (label) {
+      result[label] = transformed[key]
+    }
+  })
+
+  return pickBy(result)
 }
 
 function transformInteractionFormBodyToApiRequest (props) {
