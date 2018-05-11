dca631a829c3bc2957e3d63fc4b41bbf59db1387
Zac Tolley <zac@thetolleys.com>
2018-05-11 14:53:34 +0100

    Enable policy feedback in interaction creation selection

--- src/apps/interactions/macros/kind-form.js
+++ src/apps/interactions/macros/kind-form.js
@@ -5,28 +5,25 @@ module.exports = function ({
   return {
     buttonText: 'Continue',
     errors,
-    children: [
-      {
-        macroName: 'MultipleChoiceField',
-        type: 'radio',
-        label: 'What would you like to record?',
-        name: 'kind',
-        options: [{
-          value: 'interaction',
-          label: 'A standard interaction',
-          hint: 'For example, an email, phone call or meeting',
-        }, {
-          value: 'service_delivery',
-          label: 'A service that you have provided',
-          hint: 'For example, account management, a significant assist or an event',
-        },
-        /*, {
-          value: 'policy_feedback',
-          label: 'Policy feedback',
-          hint: 'For example, when a company wants to give UK government policy feedback',
-        } */
-        ],
+    children: [{
+      macroName: 'MultipleChoiceField',
+      type: 'radio',
+      label: 'What would you like to record?',
+      name: 'kind',
+      options: [{
+        value: 'interaction',
+        label: 'A standard interaction',
+        hint: 'For example, an email, phone call or meeting',
+      }, {
+        value: 'service_delivery',
+        label: 'A service that you have provided',
+        hint: 'For example, account management, a significant assist or an event',
       },
-    ],
+      {
+        value: 'policy_feedback',
+        label: 'Capture policy feedback',
+        hint: 'For example, and issue or comment on government policy from a company',
+      }],
+    }],
   }
 }
