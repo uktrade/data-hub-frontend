7e6041019c574ffb800736428728a8bf82458e37
Zac Tolley <zac@thetolleys.com>
2018-05-15 11:05:50 +0100

    Add a transformer for company timeline items

---
+++ src/apps/timeline/labels.js
@@ -0,0 +1,10 @@
+const listLabels = {
+  data_source: 'Source',
+  description: 'Description',
+}
+
+const sourceLabels = {
+  'companies_house.companies': 'Companies House',
+}
+
+module.exports = { listLabels, sourceLabels }
