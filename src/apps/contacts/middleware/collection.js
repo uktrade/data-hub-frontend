160f6c108907983e18c7169387f0bcbc429e5e98
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2018-06-25 16:10:03 +0100

    Utilise common getRequestBody in contacts

--- src/apps/contacts/middleware/collection.js
+++ src/apps/contacts/middleware/collection.js
@@ -1,9 +1,6 @@
-const { pick, pickBy } = require('lodash')
-
 const { search } = require('../../search/services')
 const { transformApiResponseToSearchCollection } = require('../../search/transformers')
 const { transformContactToListItem } = require('../transformers')
-const removeArray = require('../../../lib/remove-array')
 
 async function getContactsCollection (req, res, next) {
   try {
@@ -25,30 +22,6 @@ async function getContactsCollection (req, res, next) {
   }
 }
 
-function getRequestBody (req, res, next) {
-  const selectedFiltersQuery = removeArray(pick(req.query, [
-    'archived',
-    'name',
-    'company_name',
-    'company_sector_descends',
-    'address_country',
-    'company_uk_region',
-  ]), 'archived')
-
-  const selectedSortBy = req.query.sortby
-    ? { sortby: req.query.sortby }
-    : null
-
-  req.body = {
-    ...req.body,
-    ...selectedSortBy,
-    ...pickBy(selectedFiltersQuery),
-  }
-
-  next()
-}
-
 module.exports = {
   getContactsCollection,
-  getRequestBody,
 }
