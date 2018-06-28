8c7fc0f82dda95e8dd7c2832f5a74c1ee23602a1
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2018-06-28 13:41:56 +0100

    Move common search related code to module
    This code is used in multiple places

---
+++ src/modules/search/builders/index.js
@@ -0,0 +1,5 @@
+const buildSearchAggregation = require('./search-aggregation')
+
+module.exports = {
+  buildSearchAggregation,
+}
---
+++ src/modules/search/builders/search-aggregation.js
@@ -0,0 +1,21 @@
+const { isArray, assign } = require('lodash')
+
+const { filterNonPermittedItem } = require('../../permissions/filters')
+
+function buildSearchAggregation ({ aggregations, userPermissions, entityDetails }) {
+  if (!isArray(aggregations)) { return }
+
+  const userEntities = entityDetails.filter(filterNonPermittedItem(userPermissions))
+
+  return userEntities.map((defaultEntity) => {
+    return assign(
+      {},
+      defaultEntity,
+      aggregations.find((apiResponseEntity) => {
+        return apiResponseEntity.entity === defaultEntity.entity
+      })
+    )
+  })
+}
+
+module.exports = buildSearchAggregation
