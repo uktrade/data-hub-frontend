d6c71aead6aa8c945b7ca5d3051bffb9ac2e3696
John Fieldman <hello@johnfieldman.com>
2018-07-11 11:15:19 +0100

    https://trello.com/c/ZnsFf3u9/171-event-attendees-double-clicking-adds-the-same-attendee-twice
    Some users still double click on links and this creates an unwanted behaviour, i.e. multiple entries.
    I've done some discovery on what's the best way to aproach this, and becuase in the node layer it creates multiple `threads`
    you can't really control the state of multiple requests unless you create an observer pattern in the authorised-request file.
    
    Knowing that the authorised-request file is being used with almos all request, this pattern is not fit for purpose so I decided to solve it
    very simply in the client by preventing any double or multi click actions on a link with a predefined class, thus preserving the native browser behaviour.

---
+++ assets/javascripts/modules/prevent-link-double-click.js
@@ -0,0 +1,39 @@
+const CONSTANTS = {
+  selectors: {
+    anchor: 'js-prevent-double-click',
+  },
+  events: {
+    click: 'click',
+  },
+}
+
+const PreventLinkDoubleClick = {
+  counter: 0,
+
+  init () {
+    if (document.querySelectorAll(`.${CONSTANTS.selectors.anchor}`).length) {
+      this.bindEvents()
+    }
+  },
+
+  preventDoubleClick (event) {
+    const target = event.target
+    const targetAnchor = target.closest(`.${CONSTANTS.selectors.anchor}`)
+
+    if (!targetAnchor) {
+      return
+    }
+
+    if (this.counter >= 1) {
+      event.preventDefault()
+    } else {
+      this.counter += 1
+    }
+  },
+
+  bindEvents () {
+    document.addEventListener(CONSTANTS.events.click, this.preventDoubleClick.bind(this))
+  },
+}
+
+module.exports = PreventLinkDoubleClick
