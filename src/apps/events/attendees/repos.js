344968e3ebcd969a26c83d4b868bd8142aee4972
Zac Tolley <zac@thetolleys.com>
2018-07-12 16:59:42 +0100

    Change signature of find attendees method

--- src/apps/events/attendees/repos.js
+++ src/apps/events/attendees/repos.js
@@ -1,8 +1,11 @@
 const config = require('../../../../config')
 const authorisedRequest = require('../../../lib/authorised-request')
 
-async function fetchEventAttendees (token, eventId, page = 1, sortby) {
-  const limit = 10
+async function fetchEventAttendees ({ token, eventId, page = 1, sortby, limit = 10 }) {
+  if (!eventId) {
+    return null
+  }
+
   const offset = limit * (page - 1)
 
   const eventAttendees = await authorisedRequest(token, {
