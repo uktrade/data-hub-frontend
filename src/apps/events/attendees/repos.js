ebd3a664581aeba4f812d7dfe23cd3500bfe2ad5
Zac Tolley <zac@thetolleys.com>
2018-07-13 12:54:12 +0100

    Add lookup attendees by contact as well as event
    In order to check if an attendee exists on an event the
    fetchEvent attendees function now lets you pass the contact
    as well, this allows a check to see if they alreadu exist

--- src/apps/events/attendees/repos.js
+++ src/apps/events/attendees/repos.js
@@ -1,7 +1,9 @@
+const { isNil, pickBy } = require('lodash')
+
 const config = require('../../../../config')
 const authorisedRequest = require('../../../lib/authorised-request')
 
-async function fetchEventAttendees ({ token, eventId, page = 1, sortby, limit = 10 }) {
+async function fetchEventAttendees ({ token, eventId, page = 1, sortby, limit = 10, contactId }) {
   if (!eventId) {
     return null
   }
@@ -10,12 +12,13 @@ async function fetchEventAttendees ({ token, eventId, page = 1, sortby, limit = 
 
   const eventAttendees = await authorisedRequest(token, {
     url: `${config.apiRoot}/v3/interaction`,
-    qs: {
+    qs: pickBy({
       limit,
       offset,
       sortby,
       event_id: eventId,
-    },
+      contact_id: contactId,
+    }, value => !isNil(value)),
   })
 
   return eventAttendees
