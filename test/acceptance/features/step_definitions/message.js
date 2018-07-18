0a3e3159b04d3231f1f096662aa9000ff9ba0913
Zac Tolley <zac@thetolleys.com>
2018-07-18 12:21:55 +0100

    Change event details to show disabled as message
    Changes the behaviour of the event details view to show
    the disabled on information as a message and not badge.
    
    As part of this the layout for event details and attendee
    list is refactored to use a common layout.

--- test/acceptance/features/step_definitions/message.js
+++ test/acceptance/features/step_definitions/message.js
@@ -3,7 +3,12 @@ const { Then } = require('cucumber')
 
 const Message = client.page.message()
 
-Then(/^I see the (success|error) message$/, async (messageType) => {
+Then(/^I see (the|an) (success|error|info) message$/, async (thean, messageType) => {
   await Message
     .verifyMessage(messageType)
+})
+
+Then(/^I see (the|an) (success|error|info) message containing "(.+)"$/, async (thean, messageType, message) => {
+  await Message
+    .verifyMessage(messageType, message)
 })
