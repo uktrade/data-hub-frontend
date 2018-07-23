a5976da964170763d642cef6b8bc3b88ed4a21fc
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2018-07-23 16:25:15 +0100

    Move filterDisabledOption to module
    This was a remaining method that could be safely
    moved to the permissions module. All usages have
    been updated.

--- src/modules/permissions/filters.js
+++ src/modules/permissions/filters.js
@@ -1,5 +1,20 @@
 const { get, intersection } = require('lodash')
 
+function filterDisabledOption ({ currentValue = null, createdOn } = {}) {
+  const createdOnTime = Date.parse(createdOn) || Date.now()
+
+  return function (item) {
+    if (!item.disabled_on) {
+      return true
+    }
+
+    const isDisabled = Date.parse(item.disabled_on) > createdOnTime
+    const isCurrentValue = item.id === currentValue
+
+    return (isDisabled || isCurrentValue)
+  }
+}
+
 function filterNonPermittedItem (userPermissions) {
   return function (item) {
     const hasPermissions = get(item, 'permissions.length', 0)
@@ -12,4 +27,5 @@ function filterNonPermittedItem (userPermissions) {
 
 module.exports = {
   filterNonPermittedItem,
+  filterDisabledOption,
 }
--- test/unit/modules/permissions/filters.test.js
+++ test/unit/modules/permissions/filters.test.js
@@ -1,6 +1,151 @@
-const { filterNonPermittedItem } = require('~/src/modules/permissions/filters')
+const moment = require('moment')
+
+const { filterDisabledOption, filterNonPermittedItem } = require('~/src/modules/permissions/filters')
+
+const lastMonth = moment().subtract(1, 'months').toISOString()
+const nextMonth = moment().add(1, 'months').toISOString()
 
 describe('filters', () => {
+  describe('#filterDisabledOption', () => {
+    context('when an option was disabled in the past', () => {
+      beforeEach(() => {
+        this.options = [{
+          id: '1234',
+          name: 'Freds',
+          disabled_on: lastMonth,
+        }]
+      })
+
+      context('when the option is not the current field value', () => {
+        beforeEach(() => {
+          this.filteredOptions = this.options.filter(filterDisabledOption({
+            currentValue: '3',
+            createdOn: lastMonth,
+          }))
+        })
+
+        it('should not include the option', () => {
+          expect(this.filteredOptions).to.have.length(0)
+        })
+      })
+
+      context('when the option is the current field value', () => {
+        beforeEach(() => {
+          this.filteredOptions = this.options.filter(filterDisabledOption({
+            currentValue: '1234',
+            createdOn: lastMonth,
+          }))
+        })
+
+        it('should include the option', () => {
+          expect(this.filteredOptions).to.have.length(1)
+        })
+      })
+
+      context('when there is no current value', () => {
+        beforeEach(() => {
+          this.filteredOptions = this.options.filter(filterDisabledOption())
+        })
+
+        it('should not include the option', () => {
+          expect(this.filteredOptions).to.have.length(0)
+        })
+      })
+    })
+
+    context('when an option is disabled in the future', () => {
+      beforeEach(() => {
+        this.options = [{
+          id: '1234',
+          name: 'Freds',
+          disabled_on: nextMonth,
+        }]
+      })
+
+      context('when the option is not the current field value', () => {
+        beforeEach(() => {
+          this.filteredOptions = this.options.filter(filterDisabledOption({
+            currentValue: '3',
+            createdOn: lastMonth,
+          }))
+        })
+
+        it('should include the option', () => {
+          expect(this.filteredOptions).to.have.length(1)
+        })
+      })
+
+      context('when the option is the current field value', () => {
+        beforeEach(() => {
+          this.filteredOptions = this.options.filter(filterDisabledOption({
+            currentValue: '1234',
+            createdOn: lastMonth,
+          }))
+        })
+
+        it('should include the option', () => {
+          expect(this.filteredOptions).to.have.length(1)
+        })
+      })
+
+      context('when there is no current value', () => {
+        beforeEach(() => {
+          this.filteredOptions = this.options.filter(filterDisabledOption())
+        })
+
+        it('should include the option', () => {
+          expect(this.filteredOptions).to.have.length(1)
+        })
+      })
+    })
+
+    context('when an option is not disabled', () => {
+      beforeEach(() => {
+        this.options = [{
+          id: '1234',
+          name: 'Freds',
+          disabled_on: null,
+        }]
+      })
+
+      context('when the option is not the current field value', () => {
+        beforeEach(() => {
+          this.filteredOptions = this.options.filter(filterDisabledOption({
+            currentValue: '3',
+            createdOn: lastMonth,
+          }))
+        })
+
+        it('should include the option', () => {
+          expect(this.filteredOptions).to.have.length(1)
+        })
+      })
+
+      context('when the option is the current field value', () => {
+        beforeEach(() => {
+          this.filteredOptions = this.options.filter(filterDisabledOption({
+            currentValue: '1234',
+            createdOn: lastMonth,
+          }))
+        })
+
+        it('should include the option', () => {
+          expect(this.filteredOptions).to.have.length(1)
+        })
+      })
+
+      context('when there is no current value', () => {
+        beforeEach(() => {
+          this.filteredOptions = this.options.filter(filterDisabledOption())
+        })
+
+        it('should include the option', () => {
+          expect(this.filteredOptions).to.have.length(1)
+        })
+      })
+    })
+  })
+
   describe('#filterNonPermittedItem', () => {
     beforeEach(() => {
       this.filterNonPermittedItem = filterNonPermittedItem
