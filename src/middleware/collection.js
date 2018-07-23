180eaa605aa0f34363befc67ca0877774c320a9a
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2018-07-23 15:11:03 +0100

    Remove redundant getCollection function
    Common function has been added to the common
    modules

--- src/middleware/collection.js
+++ src/middleware/collection.js
@@ -1,8 +1,6 @@
 const { pick, pickBy } = require('lodash')
 
 const removeArray = require('../lib/remove-array')
-const { search } = require('../modules/search/services')
-const { transformApiResponseToSearchCollection } = require('../modules/search/transformers')
 
 function getRequestBody (queryFields) {
   return function (req, res, next) {
@@ -22,25 +20,6 @@ function getRequestBody (queryFields) {
   }
 }
 
-function getCollection (searchEntity, ...itemTransformers) {
-  return async function (req, res, next) {
-    try {
-      res.locals.results = await search({
-        searchEntity: searchEntity,
-        requestBody: req.body,
-        token: req.session.token,
-        page: req.query.page,
-        isAggregation: false,
-      }).then(transformApiResponseToSearchCollection({ query: req.query }, ...itemTransformers))
-
-      next()
-    } catch (error) {
-      next(error)
-    }
-  }
-}
-
 module.exports = {
   getRequestBody,
-  getCollection,
 }
