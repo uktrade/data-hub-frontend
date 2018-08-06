327d2fe4a1830db36d7e45b589593d6318baf9af
John Fieldman <hello@johnfieldman.com>
2018-08-06 16:11:39 +0100

    Updated propositions document upload form to add a new file input when user requests it

--- src/apps/document-upload/macros/fields.js
+++ src/apps/document-upload/macros/fields.js
@@ -1,32 +1,47 @@
 module.exports = {
-  documentUpload: {
+  abandon: {
     macroName: 'TextField',
-    type: 'file',
+    type: 'textarea',
+    name: 'details',
+    hint: 'Add the reason you\'re abandoning this proposition',
+  },
+  adviser (advisers) {
+    return {
+      macroName: 'MultipleChoiceField',
+      name: 'adviser',
+      initialOption: '-- Select adviser --',
+      options: advisers,
+    }
+  },
+  complete: {
+    macroName: 'TextField',
+    type: 'textarea',
+    name: 'details',
+    hint: 'Add the document\'s Sharepoint URL, e.g. http://your-sharepoint-url~',
+  },
+  multipleDocumentUpload: {
+    macroName: 'AddAnother',
+    buttonName: 'add_item',
+    label: 'Filename',
     name: 'filename',
-    isLabelHidden: true,
+    children: [{
+      macroName: 'TextField',
+      type: 'file',
+      label: 'Filename',
+      name: 'filename',
+      isLabelHidden: true,
+    }],
   },
-  documentUpload01: {
-    macroName: 'TextField',
-    type: 'file',
-    name: 'filename01',
-    isLabelHidden: true,
+  deadline: {
+    macroName: 'DateFieldset',
+    name: 'deadline',
   },
-  documentUpload02: {
+  name: {
     macroName: 'TextField',
-    type: 'file',
-    name: 'filename02',
-    isLabelHidden: true,
+    name: 'name',
   },
-  documentUpload03: {
+  scope: {
     macroName: 'TextField',
-    type: 'file',
-    name: 'filename03',
-    isLabelHidden: true,
-  },
-  documentUpload04: {
-    macroName: 'TextField',
-    type: 'file',
-    name: 'filename04',
-    isLabelHidden: true,
+    name: 'scope',
   },
 }
