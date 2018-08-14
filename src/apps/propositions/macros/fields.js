c101b284695a40eed8d001523f4eecaee07f6dfb
John Fieldman <hello@johnfieldman.com>
2018-08-14 21:02:38 +0100

    cleaned up virus scan status logic

--- src/apps/propositions/macros/fields.js
+++ src/apps/propositions/macros/fields.js
@@ -25,30 +25,6 @@ module.exports = {
     name: 'filename',
     isLabelHidden: true,
   },
-  documentUpload01: {
-    macroName: 'TextField',
-    type: 'file',
-    name: 'filename01',
-    isLabelHidden: true,
-  },
-  documentUpload02: {
-    macroName: 'TextField',
-    type: 'file',
-    name: 'filename02',
-    isLabelHidden: true,
-  },
-  documentUpload03: {
-    macroName: 'TextField',
-    type: 'file',
-    name: 'filename03',
-    isLabelHidden: true,
-  },
-  documentUpload04: {
-    macroName: 'TextField',
-    type: 'file',
-    name: 'filename04',
-    isLabelHidden: true,
-  },
   deadline: {
     macroName: 'DateFieldset',
     name: 'deadline',
