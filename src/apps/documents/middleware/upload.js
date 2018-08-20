d13c0a7fae386b14e0bd5f93707c78c24206a38d
John Fieldman <hello@johnfieldman.com>
2018-08-20 13:42:52 +0100

    Created Documents Module
    For users to be able to upload and download documents we had to create an abstract data-hub application
    that synchronizes the API calls.
    
    For Upload we have the followings:
     - First call to the API generates a document ID and and s3 upload url
     - 2nd call uploads the file to s3
     - 3rd call generates the download url and signals the app that the upload process is finished
    
    For Download:
     - When the user clicks on the link in the interface it send a request to the API which generates an s3 url that gives access to the file for 30 minutes
     - The files is sent to the user via the browser

---
+++ src/apps/documents/middleware/upload.js
@@ -0,0 +1,56 @@
+/* eslint-disable camelcase */
+const { filter, map } = require('lodash')
+const formidable = require('formidable')
+
+const { chainUploadSequence } = require('../repos')
+
+function parseForm (req, res, apiConfig) {
+  const form = new formidable.IncomingForm()
+  const fiveGigabytes = 5000 * 1024 * 1024
+
+  form.maxFileSize = fiveGigabytes
+
+  form.parse(req, async (err, fields, files) => {
+    let index = 1
+
+    if (apiConfig.collectTextFields) {
+      await apiConfig.collectTextFields(req, res, fields)
+    }
+
+    map(files, async (file, value, collection) => {
+      res.locals.documents = {
+        id: fields.id,
+        parent_id: fields[fields.app],
+        file,
+        numberOfDocuments: filter(collection).length,
+        fields,
+        url: apiConfig.url,
+      }
+
+      if (!file.name.length) { return }
+      index++
+
+      await chainUploadSequence(req, res, index)
+    })
+
+    if (err) {
+      return res.status(500).json({ error: err })
+    }
+  })
+}
+
+function postUpload (req, res, next) {
+  const apiConfig = this
+
+  try {
+    parseForm(req, res, apiConfig)
+  } catch (error) {
+    if (error.statusCode !== 400) {
+      return next(error)
+    }
+  }
+}
+
+module.exports = {
+  postUpload,
+}
