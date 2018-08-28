2ea37378436592a84b3a6f53ce39f31ac4305d49
John Fieldman <hello@johnfieldman.com>
2018-08-28 15:56:47 +0100

    Integrate document upload with evidence feature
    The FDI Rulebook requires a section called `Evidence` where users can upload supporting documents for an investment project.
    The Evidence feature itself is only a tags dropdown a file upload and a comments textarea. For documents upload we're reusing the `Documents` module,
    and intsantiating it investment-projects/router by calling `postUpload` and binding the api configs necessary for building the request urls.
    
    As Evidence is only required for Investment projects, we've created it inside an `apps` folder within the investment-projects app. This is where we will also need move the `propositions` app as it's not shared across data-hub.

---
+++ src/apps/investment-projects/controllers/evidence.js
@@ -0,0 +1,26 @@
+const { notFound } = require('../../../middleware/errors')
+const { getEvidenceForInvestment } = require('../apps/evidence/repos')
+
+async function renderEvidenceView (req, res, next) {
+  if (!res.locals.features['investment-evidence']) {
+    return notFound(req, res, next)
+  }
+
+  try {
+    const token = req.session.token
+    const investmentId = req.params.investmentId
+    const evidence = await getEvidenceForInvestment(token, investmentId)
+
+    return res
+      .breadcrumb('Evidence')
+      .render('investment-projects/apps/evidence/views/list', {
+        evidence,
+      })
+  } catch (error) {
+    next(error)
+  }
+}
+
+module.exports = {
+  renderEvidenceView,
+}
---
+++ src/apps/investment-projects/middleware/evidence.js
@@ -0,0 +1,55 @@
+const { fetchDownloadLink, requestDeleteEvidence } = require('../apps/evidence/repos')
+const { transformedEvidenceTextFields } = require('../apps/evidence/transformers')
+
+function setEvidenceReturnUrl (req, res, next) {
+  res.locals.returnLink = `/investment-projects/${req.params.investmentId}/evidence`
+  next()
+}
+
+async function getDownloadLink (req, res, next) {
+  try {
+    const token = req.session.token
+    const evidenceId = req.params.evidenceId
+    const investmentId = req.params.investmentId
+    const s3 = await fetchDownloadLink(token, investmentId, evidenceId)
+
+    return res.redirect(s3.document_url)
+  } catch (error) {
+    next(error)
+  }
+}
+
+async function deleteEvidence (req, res, next) {
+  try {
+    const token = req.session.token
+    const evidenceId = req.params.evidenceId
+    const investmentId = req.params.investmentId
+
+    await requestDeleteEvidence(token, investmentId, evidenceId)
+      .then(() => {
+        req.flash('success', 'Evidence item deleted')
+        res.redirect(res.locals.returnLink)
+      })
+  } catch (error) {
+    next(error)
+  }
+}
+
+function setEvidenceDocumentsOptions (req, res, next) {
+  res.locals.documents = {
+    collectTextFields: transformedEvidenceTextFields,
+    url: {
+      app: 'investment',
+      document: 'evidence-document',
+    },
+  }
+
+  next()
+}
+
+module.exports = {
+  setEvidenceReturnUrl,
+  deleteEvidence,
+  getDownloadLink,
+  setEvidenceDocumentsOptions,
+}
