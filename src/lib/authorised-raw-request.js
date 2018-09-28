a56d2408c16c68d1b5482c51a255df325bed38ae
Stuart Axon <stu.axon@gmail.com>
2018-09-28 15:11:37 +0100

    Start breaking out arg parsing in authorised-request.

--- src/lib/authorised-raw-request.js
+++ src/lib/authorised-raw-request.js
@@ -7,25 +7,31 @@ const logger = require('../../config/logger')
 function hasValue (value) {
   return !isNil(value)
 }
+
+function parseOptions (opts) {
+  if (isString(opts)) {
+    return {
+      json: true,
+      method: 'GET',
+      url: opts,
+    }
+  }
+  return {
+    body: opts.body,
+    headers: pickBy(opts.headers, hasValue),
+    json: true,
+    method: opts.method || 'GET',
+    url: opts.url,
+    ...pickBy(opts.qs, hasValue),
+  }
+}
+
 // Accepts either options in a kashmap or a string with a url
 // Combines the options or url with the given token to create a
 // call to the API server
 // The response is not parsed
 module.exports = (token, opts) => {
-  let requestOptions = (isString(opts))
-    ? {
-      json: true,
-      method: 'GET',
-      url: opts,
-    }
-    : {
-      body: opts.body,
-      headers: pickBy(opts.headers, hasValue),
-      json: true,
-      method: opts.method || 'GET',
-      qs: pickBy(opts.qs, hasValue),
-      url: opts.url,
-    }
+  let requestOptions = parseOptions(opts)
 
   if (process.env.PROXY) {
     requestOptions.proxy = process.env.PROXY
@@ -37,8 +43,6 @@ module.exports = (token, opts) => {
   if (token) {
     set(requestOptions, 'headers.Authorization', `Bearer ${token}`)
   }
-
-  requestOptions = pickBy(requestOptions, hasValue)
 
   logger.debug('Send authorised raw request: ', requestOptions)
 
