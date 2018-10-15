876af084e0991ce9cd89c4f524f83db5fea5857e
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2018-10-15 11:06:27 +0100

    Add middleware to validate state
    Makes sure previous steps have been completed
    before presenting a step.

--- src/modules/form/state/middleware.js
+++ src/modules/form/state/middleware.js
@@ -1,4 +1,60 @@
-const { getFullRoute } = require('../helpers')
+const {
+  isEmpty,
+  get,
+  map,
+  compact,
+  find,
+} = require('lodash')
+
+const state = require('../state/current')
+const { getFullRoute, getNextPath } = require('../helpers')
+
+const mapStepsWithState = (steps, currentState) => {
+  return compact(map(steps, (step, stepId) => {
+    const completed = get(currentState, `steps.${step.path}.completed`, false)
+    const data = get(currentState, `steps.${step.path}.data`, {})
+
+    return {
+      completed,
+      id: stepId,
+      path: step.path,
+      nextPath: getNextPath(step, data),
+      validateJourney: step.validateJourney,
+    }
+  }))
+}
+
+const isValidJourney = (steps, currentStepId, currentState) => {
+  const stepsWithState = mapStepsWithState(steps, currentState)
+
+  const hasCompletedPreviousStep = (step) => {
+    if (step.id === 0 || step.validateJourney === false) {
+      return true
+    }
+
+    const previousStep = find(stepsWithState, stepWithState => stepWithState.nextPath === step.path)
+    return previousStep && previousStep.completed ? hasCompletedPreviousStep(previousStep) : false
+  }
+
+  const currentStepWithState = find(stepsWithState, stepWithState => stepWithState.id === currentStepId)
+  return hasCompletedPreviousStep(currentStepWithState)
+}
+
+const validateState = (req, res, next) => {
+  const { key, steps, currentStepId } = res.locals.journey
+  const currentState = state.getCurrent(req.session, key)
+  const isStartingJourney = isEmpty(currentState.steps) && currentStepId === 0
+
+  if (isStartingJourney) {
+    return next()
+  }
+
+  if (isValidJourney(steps, currentStepId, currentState)) {
+    return next()
+  }
+
+  res.redirect(key)
+}
 
 const setJourneyDetails = (journey, currentStep, currentStepId) => {
   return (req, res, next) => {
@@ -15,4 +71,5 @@ const setJourneyDetails = (journey, currentStep, currentStepId) => {
 
 module.exports = {
   setJourneyDetails,
+  validateState,
 }
