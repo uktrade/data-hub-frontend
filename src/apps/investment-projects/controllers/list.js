1dbc858b1567e1eec2d441ea692b7283f91d930e
jimsmith <jim.smith@gmail.com>
2018-10-16 14:51:29 +0100

    refactor csv export functions into library

--- src/apps/investment-projects/controllers/list.js
+++ src/apps/investment-projects/controllers/list.js
@@ -4,40 +4,11 @@ const { merge, omit } = require('lodash')
 const { buildSelectedFiltersSummary, buildFieldsWithSelectedEntities } = require('../../builders')
 const { getOptions } = require('../../../lib/options')
 const { investmentFiltersFields, investmentSortForm } = require('../macros')
+const { buildExportAction } = require('../../../lib/export-helper')
 
 const FILTER_CONSTANTS = require('../../../lib/filter-constants')
 const QUERY_STRING = FILTER_CONSTANTS.INVESTMENT_PROJECTS.SECTOR.PRIMARY.QUERY_STRING
 const SECTOR = FILTER_CONSTANTS.INVESTMENT_PROJECTS.SECTOR.NAME
-const MAX_EXPORT_ITEMS = FILTER_CONSTANTS.INVESTMENT_PROJECTS.SECTOR.MAX_EXPORT_ITEMS
-
-function hasExportPermission (userPermissions) {
-  return userPermissions.includes('investment.export_investmentproject')
-}
-
-function tooManyItems (resultCount) {
-  return resultCount > MAX_EXPORT_ITEMS
-}
-
-function buildExportMessage (resultCount) {
-  if (tooManyItems(resultCount)) {
-    return `Filter to less than ${MAX_EXPORT_ITEMS} projects to download`
-  }
-  return `You can now download these ${resultCount} projects`
-}
-
-function buildExportAction (userPermissions, queryString) {
-  if (!hasExportPermission(userPermissions)) {
-    return {
-      enabled: false,
-    }
-  }
-  return {
-    enabled: true,
-    buildMessage: (count) => buildExportMessage(count),
-    url: 'investment-projects' + queryString,
-    tooManyItems,
-  }
-}
 
 async function renderInvestmentList (req, res, next) {
   try {
@@ -60,7 +31,15 @@ async function renderInvestmentList (req, res, next) {
 
     const filtersFieldsWithSelectedOptions = await buildFieldsWithSelectedEntities(token, filtersFields, req.query)
     const selectedFilters = await buildSelectedFiltersSummary(filtersFieldsWithSelectedOptions, req.query)
-    const exportAction = await buildExportAction(user.permissions, '/export?' + qs.stringify(req.query))
+
+    const exportOptions = {
+      targetPermission: 'investment.export_investmentproject',
+      urlFragment: 'investment-projects',
+      maxItems: FILTER_CONSTANTS.INVESTMENT_PROJECTS.SECTOR.MAX_EXPORT_ITEMS,
+      entityPlural: 'projects',
+    }
+
+    const exportAction = await buildExportAction(qs.stringify(req.query), user.permissions, exportOptions)
 
     res.render('_layouts/collection', {
       sortForm,
