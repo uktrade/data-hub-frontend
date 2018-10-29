9e66ec75cf6ce879e2eac84c0da0dc3d1834ce9f
Ian Leggett <contactleggett@gmail.com>
2018-10-29 11:03:56 +0000

    Remove hard coded labels, labels now in API

--- src/apps/timeline/labels.js
+++ src/apps/timeline/labels.js
@@ -3,42 +3,4 @@ const listLabels = {
   description: 'Description',
 }
 
-const sourceLabels = {
-  'citrix.webinars.attendees': 'Citrix (DIT Webinars)',
-  'citrix.webinars.webinars': 'Citrix (DIT Webinars)',
-  'citrix.webinars.registrants': 'Citrix (DIT Webinars)',
-  'dit.datahub.interactions': 'DIT (DataHub)',
-  'dit.datahub.companies': 'DIT (DataHub)',
-  'dit.datahub.contacts': 'DIT (DataHub)',
-  'dit.datahub.dit_events': 'DIT (DataHub)',
-  'dit.datahub.fdi': 'DIT (DataHub)',
-  'dit.datahub.advisors': 'DIT (DataHub)',
-  'dit.eig-contact-form': 'DIT (EIG Contact Form)',
-  'dit.eig-email-guide': 'DIT (EIG Email Guide)',
-  'dit.eig-export-ops-published': 'DIT (Export Opportunities)',
-  'dit.eig-export-opp-responses': 'DIT (Export Opportunities)',
-  'dit.export-wins': 'DIT (Export Wins)',
-  'dit.find-a-buyer.suppliers': 'DIT (FAB)',
-  'dit.find-a-buyer.buyers': 'DIT (FAB)',
-  'dit.gov-uk-contact-form': 'DIT (GOV.UK Contact Form)',
-  'companies_house.companies': 'Companies House (Companies)',
-  'companies_house.accounts': 'Companies House (Accounts)',
-  'companies_house.psc': 'Companies House (PSC)',
-  'experian.email.clicked': 'Experian (Email Campaign)',
-  'experian.email.opened': 'Experian (Email Campaign)',
-  'experian.email.sent': 'Experian (Email Campaign)',
-  'hmrc.exporters': 'HMRC (Exporters)',
-  'hmrc.importers.f0_0': 'HMRC (Importers)',
-  'hmrc.importers.f1_0': 'HMRC (Importers)',
-  'horizon.events.events': 'Horizon (Events)',
-  'horizon.events.delegates': 'Horizon (Events)',
-  'ipo.journals': 'IPO (Journals)',
-  'zendesk.organizations': 'Zendesk (Organizations)',
-  'zendesk.users': 'Zendesk (Users)',
-  'zendesk.tickets': 'Zendesk (Tickets)',
-  'zendesk.tickets.contact_dit_form': 'Zendesk (Tickets)',
-  'zendesk.tickets.iigb': 'Zendesk (Tickets)',
-  'zendesk.tickets.soo': 'Zendesk (Tickets)',
-}
-
-module.exports = { listLabels, sourceLabels }
+module.exports = { listLabels }
--- src/apps/timeline/transformers.js
+++ src/apps/timeline/transformers.js
@@ -2,16 +2,16 @@
 const dateFns = require('date-fns')
 const { mediumDateFormat } = require('../../../config')
 
-const { listLabels, sourceLabels } = require('./labels')
+const { listLabels } = require('./labels')
 
-function transformTimelineToListItem ({ data_source, datetime, description }) {
+function transformTimelineToListItem ({ data_source_label, datetime, description }) {
   return {
     type: 'timeline',
     name: dateFns.format(datetime, mediumDateFormat),
     contentMetaModifier: 'stacked',
     meta: [{
       label: listLabels.data_source,
-      value: sourceLabels[data_source],
+      value: data_source_label,
     }, {
       label: listLabels.description,
       value: description,
