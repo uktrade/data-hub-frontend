0166c3dc962fe503c65d3f4bbf64d56a1190d915
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2018-11-09 12:35:28 +0000

    Update address module forms with actual fields
    Previously these were forms without elements.

--- src/apps/addresses/macros/index.js
+++ src/apps/addresses/macros/index.js
@@ -1,9 +1,11 @@
+const manualUkAddressForm = require('./manual-uk-address-form')
 const overseasAddressForm = require('./overseas-address-form')
 const postcodeLookupForm = require('./postcode-lookup-form')
 const selectUkAddressForm = require('./select-uk-address-form')
 const ukOrOverseasForm = require('./uk-or-overseas-form')
 
 module.exports = {
+  manualUkAddressForm,
   overseasAddressForm,
   postcodeLookupForm,
   selectUkAddressForm,
