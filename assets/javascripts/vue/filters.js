14e46c346a6354344c6c3115c9e09e2745b6d1bb
Ian Leggett <ileggett@equalexperts.com>
2018-11-26 17:26:30 +0000

    Simplify highlight method

--- assets/javascripts/vue/filters.js
+++ assets/javascripts/vue/filters.js
@@ -2,19 +2,15 @@ function highlight (str, words) {
   if (!words) { return str }
 
   const queryWords = words.split(' ').filter((word) => word.length >= 1)
-  const start = '~'
-  const end = '§'
   const openTag = '<span class=\'highlight\'>'
   const closeTag = '</span>'
 
   queryWords.forEach((word) => {
-    const iQuery = new RegExp(word, 'ig')
+    const iQuery = new RegExp('(' + word + ')(?![^<]*>|[^<>]*</)', 'ig')
     str = str.replace(iQuery, (matchedTxt) => {
-      return (start + matchedTxt + end)
+      return (openTag + matchedTxt + closeTag)
     })
   })
-
-  str = str.replace(new RegExp(start, 'g'), openTag).replace(new RegExp(end, 'g'), closeTag)
   return str
 }
 
