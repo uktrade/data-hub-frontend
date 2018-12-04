65adb36d8140171b22bad751a4506567cd1fbfa9
Peter Williams <retex.williams@gmail.com>
2018-12-04 15:24:22 +0000

    Remove subsidiaries linking for DnB company

--- src/apps/companies/transformers/company-to-subsidiary-list-item.js
+++ src/apps/companies/transformers/company-to-subsidiary-list-item.js
@@ -1,10 +1,11 @@
 const transformCompanyToListItem = require('./company-to-list-item')
 
-module.exports = function transformCompanyToSubsidiaryListItem ({ archived: globalHeadquartersArchived }) {
+module.exports = function transformCompanyToSubsidiaryListItem ({ archived: globalHeadquartersArchived }, DnB) {
   return (company) => {
     const listItem = transformCompanyToListItem({
       ...company,
       global_headquarters_archived: globalHeadquartersArchived,
+      DnB,
     })
     listItem.meta = listItem.meta.filter(metaItem => metaItem.label !== 'Global HQ')
     return listItem
