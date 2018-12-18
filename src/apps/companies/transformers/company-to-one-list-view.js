892a829cbc71070da5d1da7f2b6f91c28018f401
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2018-12-18 09:35:02 +0000

    Refactor company to One List view transformer

--- src/apps/companies/transformers/company-to-one-list-view.js
+++ src/apps/companies/transformers/company-to-one-list-view.js
@@ -3,25 +3,27 @@ const { get, compact } = require('lodash')
 
 const { getDataLabels } = require('../../../lib/controller-utils')
 const { accountManagementDisplayLabels } = require('../labels')
+const { NONE_TEXT } = require('../constants')
 
-const NONE_TEXT = 'None'
-
-const transformOneListAccountOwner = (globalAccountManager) => {
-  const region = get(globalAccountManager, 'adviser.dit_team.uk_region.name')
-  const country = get(globalAccountManager, 'adviser.dit_team.country.name')
+const transformGlobalAccountManager = ({ dit_team, name }) => {
+  const region = get(dit_team, 'uk_region.name')
+  const country = get(dit_team, 'country.name')
   const items = compact([
-    get(globalAccountManager, 'adviser.name'),
-    get(globalAccountManager, 'adviser.dit_team.name'),
+    name,
+    get(dit_team, 'name'),
     region || country,
   ])
 
   return items.length ? items : NONE_TEXT
 }
 
-module.exports = function transformCompanyToOneListView (company, globalAccountManager) {
+module.exports = ({
+  one_list_group_global_account_manager,
+  one_list_group_tier,
+}) => {
   const viewRecord = {
-    one_list_group_global_account_manager: transformOneListAccountOwner(globalAccountManager),
-    one_list_tier: get(company, 'one_list_group_tier.name', NONE_TEXT),
+    one_list_group_global_account_manager: transformGlobalAccountManager(one_list_group_global_account_manager || {}),
+    one_list_tier: get(one_list_group_tier, 'name', NONE_TEXT),
   }
 
   return getDataLabels(viewRecord, accountManagementDisplayLabels)
