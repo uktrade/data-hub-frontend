12055557478dce6bbc297f2194e0dc603c4cef6d
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2018-12-19 13:16:19 +0000

    Show additional business information for company

--- src/apps/companies/transformers/index.js
+++ src/apps/companies/transformers/index.js
@@ -1,7 +1,8 @@
 const transformCompaniesHouseToListItem = require('./companies-house-to-list-item')
 const transformCompaniesHouseToView = require('./companies-house-to-view')
-const transformCompanyToBusinessHierarchyView = require('./company-to-business-hierarchy-view')
+const transformCompanyToAdditionalInformationView = require('./company-to-additional-information-view')
 const transformCompanyToAddressesView = require('./company-to-addresses-view')
+const transformCompanyToBusinessHierarchyView = require('./company-to-business-hierarchy-view')
 const transformCompanyToExportDetailsView = require('./company-to-export-details-view')
 const transformCompanyToForm = require('./company-to-form')
 const transformCompanyToKnownAsView = require('./company-to-known-as-view')
@@ -15,8 +16,9 @@ const transformCoreTeamToCollection = require('./one-list-core-team-to-collectio
 module.exports = {
   transformCompaniesHouseToListItem,
   transformCompaniesHouseToView,
-  transformCompanyToBusinessHierarchyView,
+  transformCompanyToAdditionalInformationView,
   transformCompanyToAddressesView,
+  transformCompanyToBusinessHierarchyView,
   transformCompanyToExportDetailsView,
   transformCompanyToForm,
   transformCompanyToKnownAsView,
