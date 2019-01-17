64204381ec5662a2abb82a76ab03005bf9ffc3ba
JaranF Work-MBP <jaran.finn@digital.trade.gov.uk>
2019-01-17 18:39:16 +0000

    Unit tests for new function formatISO8601DateTime
    Two unit tests but does not yet have a unit test for an unparsable
    string which might get passed to it in error.

--- test/unit/lib/date.test.js
+++ test/unit/lib/date.test.js
@@ -1,4 +1,4 @@
-const { parseDateString } = require(`~/common/date`)
+const { parseDateString, formatISO8601DateTime } = require(`~/common/date`)
 
 describe('Date tests', () => {
   describe('parse human dates', () => {
@@ -15,6 +15,18 @@ describe('Date tests', () => {
     it('should not parse fred smith', () => {
       const actual = parseDateString('Fred Smith')
       expect(actual).to.be.null
+    })
+  })
+  describe('parse dates into an ISO8601 format', () => {
+    it('should parse 2017-06-02T13:18:06.544524', () => {
+      const actual = formatISO8601DateTime('2017-06-02T13:18:06.544524')
+      const expected = '2017-06-02T13:18:06'
+      expect(actual).to.equal(expected)
+    })
+    it('should parse 31 January 2019', () => {
+      const actual = formatISO8601DateTime('31 January 2019')
+      const expected = '2019-01-31T00:00:00'
+      expect(actual).to.equal(expected)
     })
   })
 })
