f58f1071e1d77b458e65e2b67bb85a654a66edcd
JaranF Work-MBP <jaran.finn@digital.trade.gov.uk>
2019-01-31 18:05:00 +0000

    Removed all comments

--- src/apps/investment-projects/controllers/audit.js
+++ src/apps/investment-projects/controllers/audit.js
@@ -6,8 +6,6 @@ const { getInvestmentProjectAuditLog } = require('../repos')
 function grammaticizeBooleanChanges (changeName) {
   const toGrammaticalFormHash = {
     'competitor_countries': 'Is the client considering other countries?',
-    // '': 'Has the UK location for this investment been decided yet?',
-    // '': 'Can client provide total investment value?',
     'new_tech_to_uk': 'Does the project bring ‘New To World’ Technology, IP or Business Model to the UK site?',
     'government_assistance': 'Is this project receiving government financial assistance?',
     'non_fdi_r_and_d_budget': 'Is this project associated with a non-FDI R&D project?',
@@ -16,10 +14,6 @@ function grammaticizeBooleanChanges (changeName) {
 }
 
 function transformProjectChangesToListItems (changes) {
-  // const key = Object.keys(changes)
-  // console.log(key)
-  // console.log(Object.prototype.toString.call(changes[key]))
-  // if (Object.prototype.toString.call(changes[key]) === '[object Array]') { console.log('length %d', changes[key].length) console.log('    ' + changes[key][0]) console.log('    ' + changes[key][1]) }
   let returnArray = []
   transform(changes, function (result, value, key) {
     let transformedChange = {}
@@ -28,7 +22,6 @@ function transformProjectChangesToListItems (changes) {
     transformedChange.ChangeToDesc = value[1]
     returnArray.push(transformedChange)
   }, returnArray)
-  // console.log(returnArray[0])
   return returnArray
 }
 
@@ -38,7 +31,7 @@ function formatAuditLog (logEntry) {
     timestamp: formatISO8601DateTime(logEntry.timestamp),
     changes: transformProjectChangesToListItems(logEntry.changes),
   }
-} // Dependent TESTS test/unit/apps/investment-projects/controllers/audit.test.js:66:41)
+}
 
 async function getInvestmentAudit (req, res, next) {
   try {
