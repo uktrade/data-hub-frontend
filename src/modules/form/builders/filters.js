8a7637215a39b3f8913556e03a13fdc143ab9b98
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-02-08 16:25:40 +0000

    Add function to build filter summary

--- src/modules/form/builders/filters.js
+++ src/modules/form/builders/filters.js
@@ -1,6 +1,70 @@
-const { isEmpty, pickBy, castArray } = require('lodash')
+const qs = require('querystring')
+const {
+  isEmpty,
+  pickBy,
+  castArray,
+  isFunction,
+  find,
+  compact,
+  isArray,
+  filter,
+  map,
+} = require('lodash')
 
 const { getOptions } = require('../../../lib/options') // todo change this dependency
+
+const labels = {
+  MultipleChoiceField (field, fieldValue) {
+    const options = isFunction(field.options) ? field.options() : field.options
+    return find(options, (option) => option.value === fieldValue).label
+  },
+  Typeahead (field, fieldValue) {
+    return find(field.selectedOptions, (fieldOption) => fieldOption.value === fieldValue).label
+  },
+}
+
+const getRemoveHrefQueryString = (query, field, fieldValue) => {
+  return pickBy({
+    ...query,
+    [field.name]: isArray(query[field.name]) ? filter(query[field.name], q => q !== fieldValue) : null,
+  })
+}
+
+const getFilterLabel = (field, fieldValue) => {
+  if (labels[field.macroName]) {
+    return labels[field.macroName](field, fieldValue)
+  }
+
+  if (isArray(field.value)) {
+    return field.value[0]
+  }
+
+  return field.value
+}
+
+function buildSelectedFiltersSummary (hydratedFiltersFields, query = {}, baseUrl) {
+  return compact(map(hydratedFiltersFields, (field) => {
+    if (isEmpty(field.value) || field.summarise === false) {
+      return
+    }
+
+    const filters = map(castArray(field.value), (fieldValue) => {
+      return {
+        value: fieldValue,
+        label: getFilterLabel(field, fieldValue),
+        removeHref: `${baseUrl}?${qs.stringify(getRemoveHrefQueryString(query, field, fieldValue))}`,
+      }
+    })
+
+    if (!isEmpty(filters)) {
+      return {
+        filters,
+        name: field.name,
+        label: field.label,
+      }
+    }
+  }))
+}
 
 const hydrateFiltersFields = async (token, filtersFields, query) => {
   const getSelectedOptions = async (name, entity, value) => {
@@ -20,5 +84,6 @@ const hydrateFiltersFields = async (token, filtersFields, query) => {
 }
 
 module.exports = {
+  buildSelectedFiltersSummary,
   hydrateFiltersFields,
 }
