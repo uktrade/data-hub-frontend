a95dfb66dfe83e3e847ddb5b109199c291523fd5
Ian Leggett <contactleggett@gmail.com>
2019-02-13 15:50:28 +0000

    Update the adviser search to use new query params from new API changes

--- src/apps/api/controllers/advisers.js
+++ src/apps/api/controllers/advisers.js
@@ -1,11 +1,14 @@
-const { adviserSearch } = require('../../adviser/repos')
+const { fetchAdviserSearchResults } = require('../../adviser/repos')
 const { transformAdviserToOption } = require('../../adviser/transformers')
 
 async function getAdviserOptionsHandler (req, res, next) {
   try {
     const token = req.session.token
-    const term = req.query.term
-    const advisers = await adviserSearch(token, term)
+    const params = {
+      term: req.query.autocomplete,
+      is_active: req.query.is_active,
+    }
+    const advisers = await fetchAdviserSearchResults(token, params)
 
     res.json(advisers.map(transformAdviserToOption))
   } catch (error) {
