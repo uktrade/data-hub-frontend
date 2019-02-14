db5aff396294f21c188ddec702071167638e6f7c
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-02-14 17:52:35 +0000

    Hide DIT region section for non UK business

--- src/apps/companies/transformers/company-to-region-view.js
+++ src/apps/companies/transformers/company-to-region-view.js
@@ -2,9 +2,13 @@
 const { get } = require('lodash')
 
 const { NOT_SET_TEXT } = require('../constants')
+const unitedKingdomId = '80756b9a-5d95-e211-a939-e4115bead28a'
 
 module.exports = ({
+  registered_address_country,
   uk_region,
 }) => {
-  return get(uk_region, 'name', NOT_SET_TEXT)
+  if (registered_address_country.id === unitedKingdomId) {
+    return get(uk_region, 'name', NOT_SET_TEXT)
+  }
 }
