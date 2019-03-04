3fcfe67a710cbc49672d13d8dd2109d9afa8d26b
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-03-04 16:40:56 +0000

    Use new layout flag over redirect query
    The dependency on the redirect query string
    parameter is unnecessary as there will be a flag
    that can achieve the same outcome.

--- src/apps/companies/controllers/archive.js
+++ src/apps/companies/controllers/archive.js
@@ -2,44 +2,48 @@
 const { archiveCompany: archive, unarchiveCompany: unarchive } = require('../repos')
 const logger = require('../../../../config/logger')
 
+function getDetailsUrl (features, company) {
+  return features['companies-new-layout'] ? `/companies/${company.id}/business-details` : `/companies/${company.id}`
+}
+
 async function archiveCompany (req, res) {
-  const { company } = res.locals
+  const { company, features } = res.locals
   const { archived_reason, archived_reason_other } = req.body
   const reason = archived_reason_other || archived_reason
-  const returnUrl = req.query.redirect || `/companies/${company.id}`
+  const detailsUrl = getDetailsUrl(features, company)
 
   if (!reason) {
     req.flash('error', 'A reason must be supplied to archive a company')
-    return res.redirect(returnUrl)
+    return res.redirect(detailsUrl)
   }
 
   try {
     await archive(req.session.token, company.id, reason)
 
     req.flash('success', 'Company archived')
-    res.redirect(returnUrl)
+    res.redirect(detailsUrl)
   } catch (error) {
     logger.error(error)
 
     req.flash('error', 'Company could not be archived')
-    res.redirect(returnUrl)
+    res.redirect(detailsUrl)
   }
 }
 
 async function unarchiveCompany (req, res) {
-  const { company } = res.locals
-  const returnUrl = req.query.redirect || `/companies/${company.id}`
+  const { company, features } = res.locals
+  const detailsUrl = getDetailsUrl(features, company)
 
   try {
     await unarchive(req.session.token, company.id)
 
     req.flash('success', 'Company unarchived')
-    res.redirect(returnUrl)
+    res.redirect(detailsUrl)
   } catch (error) {
     logger.error(error)
 
     req.flash('error', 'Company could not be archived')
-    res.redirect(returnUrl)
+    res.redirect(detailsUrl)
   }
 }
 
