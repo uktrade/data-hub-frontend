24d7bd4111638398bbdb22fb92cc1cdf300a22d7
Pippo Raimondi <filipporai@gmail.com>
2019-03-21 12:15:33 +0000

    fixup! Migrate investment project document spec to cypress

--- test/functional/cypress/specs/contacts/contact-documents-spec.js
+++ test/functional/cypress/specs/contacts/contact-documents-spec.js
@@ -5,14 +5,14 @@ describe('Contact Documents', () => {
     cy.visit('/contacts/default-contact-with-document/documents')
 
     cy.get(selectors.document.documentHeader).should('contain', 'Document')
-    cy.get(selectors.document.document).should('contain', 'View files and documents')
-    cy.get(selectors.document.document).should('contain', '(will open another website)')
+    cy.get(selectors.document.documentContent).should('contain', 'View files and documents')
+    cy.get(selectors.document.documentContent).should('contain', '(will open another website)')
   })
 
   it('should display appropriate message when there is not a link to a document', () => {
     cy.visit('/contacts/5555d636-1d24-416a-aaf0-3fb220d59aaa/documents')
     cy.get(selectors.document.documentHeader).should('contain', 'Document')
-    cy.get(selectors.document.document).should(
+    cy.get(selectors.document.documentContent).should(
       'contain', 'There are no files or documents')
   })
 })
