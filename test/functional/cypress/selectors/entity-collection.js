6e22b2c7ef9dc1486790e8bc727128394db19e64
Pippo Raimondi <filipporai@gmail.com>
2019-03-25 17:44:46 +0000

    Migrate company filter test to cypress

--- test/functional/cypress/selectors/entity-collection.js
+++ test/functional/cypress/selectors/entity-collection.js
@@ -1,3 +1,7 @@
 module.exports = {
   'addCompany': '[data-auto-id="Add company"]',
+  'collection': '.c-collection',
+  'collectionRowMessage': '.c-collection__export-message',
+  'collectionRowButton': '.c-collection__header-actions > a',
+  'collectionRemoveAllFilter': '.c-collection__filter-remove-all',
 }
---
+++ test/functional/cypress/specs/companies/filter-spec.js
@@ -0,0 +1,79 @@
+const selectors = require('../../selectors')
+
+describe('Company Collections Filter', () => {
+  before(() => {
+    cy.visit('/companies?sortby=collectionTest')
+    cy.get(selectors.entityList.entities).children().should('have.length', 9)
+    cy.get(selectors.entityCollection.collection).should('contain', '100,172 companies')
+  })
+
+  beforeEach(() => {
+    cy.server()
+    cy.route('/companies?*').as('filterResults')
+  })
+
+  it('should filter by name', () => {
+    cy.get(selectors.companyFilter.name)
+      .type('FilterByCompany')
+      .type('{enter}')
+
+    cy.get(selectors.entityList.entities).children().should('have.length', 1)
+    cy.get(selectors.entityCollection.collection).should('contain', '1 company matching FilterByCompany')
+    cy.get(selectors.entityCollection.collectionRowMessage).should('contain', 'You can now download this company')
+    cy.get(selectors.entityCollection.collectionRowButton).should('be.visible')
+  })
+
+  it('should filter by active status', () => {
+    cy.get(selectors.companyFilter.statusActive).click()
+
+    cy.wait('@filterResults').then((xhr) => {
+      expect(xhr.url).to.contain('?sortby=collectionTest&custom=true&name=FilterByCompany&archived=false')
+    })
+
+    cy.get(selectors.entityList.entities).children().should('have.length', 1)
+  })
+
+  it('should filter by inactive status', () => {
+    cy.get(selectors.companyFilter.statusInactive).click()
+
+    cy.wait('@filterResults').then((xhr) => {
+      expect(xhr.url).to.contain('?sortby=collectionTest&custom=true&name=FilterByCompany&archived=false&archived=true')
+    })
+
+    cy.get(selectors.entityList.entities).children().should('have.length', 1)
+  })
+
+  it('should filter by country', () => {
+    cy.get(selectors.companyFilter.afghanistan).click()
+
+    cy.wait('@filterResults').then((xhr) => {
+      expect(xhr.url).to.contain('?sortby=collectionTest&custom=true&' +
+        'name=FilterByCompany&' +
+        'country=87756b9a-5d95-e211-a939-e4115bead28a&' +
+        'archived=false&' +
+        'archived=true')
+    })
+
+    cy.get(selectors.entityList.entities).children().should('have.length', 1)
+  })
+
+  it('should filter by region', () => {
+    cy.get(selectors.companyFilter.alderney).click()
+
+    cy.wait('@filterResults').then((xhr) => {
+      expect(xhr.url).to.contain('?sortby=collectionTest&custom=true&' +
+        'name=FilterByCompany&' +
+        'country=87756b9a-5d95-e211-a939-e4115bead28a&' +
+        'uk_region=934cd12a-6095-e211-a939-e4115bead28a&' +
+        'archived=false&' +
+        'archived=true')
+    })
+
+    cy.get(selectors.entityList.entities).children().should('have.length', 1)
+  })
+
+  it('should remove all filters', () => {
+    cy.get(selectors.entityCollection.collectionRemoveAllFilter).click()
+    cy.get(selectors.entityCollection.collection).should('contain', '100,172 companies')
+  })
+})
