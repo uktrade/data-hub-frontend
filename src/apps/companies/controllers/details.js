ae6aa0f360a9aacd7df2f0e58d105444b723dca8
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-03-26 10:24:11 +0000

    Remove new layout flag check and redirect
    Using a 301 permanent redirect as users will have
    company details bookmarked and this will send them
    to the default route of the new layout which is
    /interactions.

--- src/apps/companies/controllers/details.js
+++ src/apps/companies/controllers/details.js
@@ -1,25 +1,5 @@
-const config = require('../../../../config')
-const {
-  transformCompanyToView,
-  transformCompaniesHouseToView,
-  transformCompanyToOneListView,
-} = require('../transformers')
-
 async function renderDetails (req, res) {
-  const { company, features } = res.locals
-
-  if (company.duns_number || features['companies-new-layout']) {
-    return res.redirect('interactions')
-  }
-
-  res
-    .breadcrumb(company.name)
-    .render('companies/views/_deprecated/details', {
-      companyDetails: transformCompanyToView(company),
-      accountManagementDetails: transformCompanyToOneListView(company),
-      chDetails: company.companies_house_data ? transformCompaniesHouseToView(company.companies_house_data) : null,
-      oneListEmail: config.oneList.email,
-    })
+  return res.redirect(301, 'interactions')
 }
 
 module.exports = {
