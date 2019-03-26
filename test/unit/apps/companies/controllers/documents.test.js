d3a66f33dd1b67a6f8c0c1495915fac20d04f4a2
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-03-26 14:40:55 +0000

    Remove companies documents view and redirect
    In the new layout for companies, documents are
    viewed from the business details view.
    
    This change permanently redirects the user to the
    business details route.

--- test/unit/apps/companies/controllers/documents.test.js
+++ test/unit/apps/companies/controllers/documents.test.js
@@ -1,70 +1,26 @@
 const buildMiddlewareParameters = require('~/test/unit/helpers/middleware-parameters-builder.js')
 
-const companyMock = require('~/test/unit/data/companies/minimal-company.json')
 const { renderDocuments } = require('~/src/apps/companies/controllers/documents')
 
 describe('Companies documents controller', () => {
   describe('#renderDocuments', () => {
-    const commonTests = (expectedCompanyName, expectedCompanyId, expectedArchivedDocumentsPath) => {
-      it('should call breadcrumb', () => {
-        expect(this.middlewareParameters.resMock.breadcrumb).to.be.calledTwice
-      })
+    beforeEach(async () => {
+      this.middlewareParameters = buildMiddlewareParameters({})
 
-      it('should call breadcrumb with', () => {
-        expect(this.middlewareParameters.resMock.breadcrumb).to.be.calledWith(expectedCompanyName, `/companies/${expectedCompanyId}`)
-      })
-
-      it('should call breadcrumb with', () => {
-        expect(this.middlewareParameters.resMock.breadcrumb).to.be.calledWith('Documents')
-      })
-
-      it('should call render', () => {
-        expect(this.middlewareParameters.resMock.render).to.be.calledOnce
-      })
-
-      it('should call render with', () => {
-        expect(this.middlewareParameters.resMock.render).to.be.calledWith('companies/views/documents', {
-          archivedDocumentPath: expectedArchivedDocumentsPath,
-        })
-      })
-    }
-
-    context('when documents path is an empty string', () => {
-      beforeEach(() => {
-        this.middlewareParameters = buildMiddlewareParameters({
-          company: {
-            ...companyMock,
-            archived_documents_url_path: '',
-          },
-        })
-
-        renderDocuments(
-          this.middlewareParameters.reqMock,
-          this.middlewareParameters.resMock,
-        )
-      })
-
-      commonTests(companyMock.name, companyMock.id, '')
+      await renderDocuments(
+        this.middlewareParameters.reqMock,
+        this.middlewareParameters.resMock,
+        this.middlewareParameters.nextSpy
+      )
     })
 
-    context('when documents path contains a url', () => {
-      const archivedDocumentsPath = 'mock-document-url'
+    it('should permanently redirect to business details', () => {
+      expect(this.middlewareParameters.resMock.redirect).to.have.been.calledWith(301, 'business-details')
+      expect(this.middlewareParameters.resMock.redirect).to.have.been.calledOnce
+    })
 
-      beforeEach(() => {
-        this.middlewareParameters = buildMiddlewareParameters({
-          company: {
-            ...companyMock,
-            archived_documents_url_path: archivedDocumentsPath,
-          },
-        })
-
-        renderDocuments(
-          this.middlewareParameters.reqMock,
-          this.middlewareParameters.resMock,
-        )
-      })
-
-      commonTests(companyMock.name, companyMock.id, archivedDocumentsPath)
+    it('should not render a template', () => {
+      expect(this.middlewareParameters.resMock.render).to.not.be.called
     })
   })
 })
