b673caaae6d4d89264c7b6f5398f50ccb910c47e
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-03-26 11:03:31 +0000

    Remove deprecated local nav constant
    As the new layout will always be presented, the
    deprecated local nav can be removed.

--- src/apps/companies/middleware/local-navigation.js
+++ src/apps/companies/middleware/local-navigation.js
@@ -1,29 +1,15 @@
 /* eslint-disable camelcase */
-const { get } = require('lodash')
-
 const { setLocalNav } = require('../../middleware')
-const { DEPRECATED_LOCAL_NAV, LOCAL_NAV } = require('../constants')
+const { LOCAL_NAV } = require('../constants')
 
 function setCompaniesLocalNav (req, res, next) {
-  const { company, features } = res.locals
+  const { company } = res.locals
 
-  if (company.duns_number || features['companies-new-layout']) {
-    const navItems = LOCAL_NAV.filter(({ path }) => {
-      return (path !== 'advisers' || company.one_list_group_tier)
-    })
+  const navItems = LOCAL_NAV.filter(({ path }) => {
+    return (path !== 'advisers' || company.one_list_group_tier)
+  })
 
-    setLocalNav(navItems)(req, res, next)
-  } else {
-    const headquarterType = get(company, 'headquarter_type.name')
-
-    const navItems = DEPRECATED_LOCAL_NAV.filter(({ path }) => {
-      return (path !== 'subsidiaries' || headquarterType === 'ghq') &&
-        (path !== 'timeline' || company.company_number) &&
-        (path !== 'advisers' || company.one_list_group_tier)
-    })
-
-    setLocalNav(navItems)(req, res, next)
-  }
+  setLocalNav(navItems)(req, res, next)
 }
 
 module.exports = setCompaniesLocalNav
