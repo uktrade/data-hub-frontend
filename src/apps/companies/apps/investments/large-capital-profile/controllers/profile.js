9e725381c2d91c1fd8dd1cd5602f8739e7babba7
Paul Gain <paul.gain@javexconsulting.com>
2019-03-27 15:19:36 +0000

    Removal of local vertical navigation

--- src/apps/companies/apps/investments/large-capital-profile/controllers/profile.js
+++ src/apps/companies/apps/investments/large-capital-profile/controllers/profile.js
@@ -1,22 +1,17 @@
 const transformProfile = require('../transformers/transform-profile')
 const { getCompanyProfiles } = require('../repos')
 
-const horizontalNav = 'companies/apps/investments/large-capital-profile/views/nav-horizontal'
-const verticalNav = 'companies/apps/investments/large-capital-profile/views/nav-vertical'
-
 const renderProfile = async (req, res, next) => {
   const token = req.session.token
-  const { company, features } = res.locals
+  const { company } = res.locals
   const { edit } = req.query
   const data = { company, edit }
-
-  const view = company.duns_number || features['companies-new-layout'] ? horizontalNav : verticalNav
 
   try {
     const profiles = await getCompanyProfiles(token, company.id)
     const profile = profiles.results && profiles.results[0]
     data.profile = profile ? transformProfile(profile) : data.profile
-    res.render(view, data)
+    res.render('companies/apps/investments/large-capital-profile/views/profile', data)
   } catch (error) {
     next(error)
   }
