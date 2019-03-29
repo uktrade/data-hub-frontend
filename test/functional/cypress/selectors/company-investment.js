1ab8bb61e14364694719639ed37e5c54b3ac8c1d
Paul Gain <paul.gain@javexconsulting.com>
2019-03-29 16:15:36 +0000

    Adding incomplete fields to Companies/Investment Large capital profile
    * Added incomplete fields to all three sections.
    * Creates a simple reusable nunjucks macro for each task list item, more
    complex macros will follow.
    * Ensured responsiveness across all devices (mobile, tablet and desktop)
    * Functional tests in part, more to follow.
    
    https://uktrade.atlassian.net/browse/IPBETA-270

--- test/functional/cypress/selectors/company-investment.js
+++ test/functional/cypress/selectors/company-investment.js
@@ -10,7 +10,29 @@ module.exports = {
     incompleteFields: '[data-auto-id=investor-details] .incomplete-fields',
     summary: '[data-auto-id=investor-details] summary',
     edit: '[data-auto-id=investor-details-edit]',
-    ave: '[data-auto-id=investor-details-save]',
+    save: '[data-auto-id=investor-details-save]',
+    taskList: {
+      investorType: {
+        name: '[data-auto-id=investor-type] .task-list-item-name',
+        incomplete: '[data-auto-id=investor-type] .task-list-item-incomplete',
+      },
+      globalAssetsUnderManagement: {
+        name: '[data-auto-id=global-assets-under-management] .task-list-item-name',
+        incomplete: '[data-auto-id=global-assets-under-management] .task-list-item-incomplete',
+      },
+      investableCapital: {
+        name: '[data-auto-id=investable-capital] .task-list-item-name',
+        incomplete: '[data-auto-id=investable-capital] .task-list-item-incomplete',
+      },
+      investorDescription: {
+        name: '[data-auto-id=investor-description] .task-list-item-name',
+        incomplete: '[data-auto-id=investor-description] .task-list-item-incomplete',
+      },
+      investorChecks: {
+        name: '[data-auto-id=investor-checks] .task-list-item-name',
+        incomplete: '[data-auto-id=investor-checks] .task-list-item-incomplete',
+      },
+    },
   },
   investorRequirements: {
     incompleteFields: '[data-auto-id=investor-requirements] .incomplete-fields',
