d11dd8eff014e349242b76d02433634c67647544
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-04-01 16:48:58 +0100

    Add barebones company matching app

---
+++ test/unit/apps/companies/apps/matching/controllers/select.js
@@ -0,0 +1,41 @@
+const buildMiddlewareParameters = require('~/test/unit/helpers/middleware-parameters-builder.js')
+
+const companyMock = require('~/test/unit/data/companies/company-v4.json')
+
+const { renderSelect } = require('~/src/apps/companies/apps/matching/controllers/select')
+
+describe('Companies matching select controller', () => {
+  describe('#renderSelect', () => {
+    beforeEach(async () => {
+      this.middlewareParameters = buildMiddlewareParameters({
+        company: companyMock,
+      })
+
+      await renderSelect(
+        this.middlewareParameters.reqMock,
+        this.middlewareParameters.resMock,
+        this.middlewareParameters.nextSpy,
+      )
+    })
+
+    it('should add two breadcrumbs', () => {
+      expect(this.middlewareParameters.resMock.breadcrumb).to.be.calledTwice
+    })
+
+    it('should add a company breadcrumb', () => {
+      expect(this.middlewareParameters.resMock.breadcrumb).to.be.calledWithExactly(companyMock.name, `/companies/${companyMock.id}`)
+    })
+
+    it('should add a "Select match" breadcrumb', () => {
+      expect(this.middlewareParameters.resMock.breadcrumb).to.be.calledWithExactly('Select the match')
+    })
+
+    it('should render the view', () => {
+      expect(this.middlewareParameters.resMock.render.firstCall.args[0]).to.equal('companies/apps/matching/views/select')
+    })
+
+    it('should set the heading', () => {
+      expect(this.middlewareParameters.resMock.render.firstCall.args[1].heading).to.equal(`Select the match for ${companyMock.name}`)
+    })
+  })
+})
