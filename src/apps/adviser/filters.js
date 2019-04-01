9df826e72bb25ea6a786367952ec5e0ea4333999
Ian Leggett <contactleggett@gmail.com>
2019-04-01 10:12:11 +0100

    Fix the events form to record organiser

--- src/apps/adviser/filters.js
+++ src/apps/adviser/filters.js
@@ -2,6 +2,7 @@ const { get } = require('lodash')
 
 function filterActiveAdvisers ({ advisers = [], includeAdviser }) {
   return advisers.filter((adviser) => {
+    // This returns all active users plus the include advisor (whether they are active or not)
     return get(adviser, 'is_active', true) || adviser.id === includeAdviser
   })
 }
