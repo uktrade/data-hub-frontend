2fe9e6b0b1b3b9fef3fc555a7ae1bb2fe011f451
zak a <zak.assali@digital.trade.gov.uk>
2019-04-24 09:24:13 +0100

    Add homepage redesign

--- src/apps/dashboard/controllers.js
+++ src/apps/dashboard/controllers.js
@@ -1,25 +1,47 @@
 const { get } = require('lodash')
+const rp = require('request-promise')
+const moment = require('moment')
 
 const GLOBAL_NAV_ITEMS = require('../global-nav-items')
 
 const { isPermittedRoute } = require('../middleware')
 const { fetchHomepageData } = require('./repos')
+const config = require('../../../config')
 
 async function renderDashboard (req, res, next) {
   try {
     const userPermissions = get(res, 'locals.user.permissions')
-    const {
+    const { contacts, interactions } = await fetchHomepageData(
+      req.session.token
+    )
+
+    const articleFeed = await rp({
+      uri: config.zen.announcementsURL,
+      json: true,
+      timeout: 1000,
+    })
+      .then(feed =>
+        feed.articles.map(item => ({
+          heading: item.title,
+          link: item.html_url,
+          date: moment(item.created_at)
+            .fromNow(),
+        }))
+      )
+      .catch(err => {
+        console.log(err)
+      })
+
+    res.title('Dashboard').render('dashboard/views/dashboard', {
       contacts,
       interactions,
-    } = await fetchHomepageData(req.session.token)
-
-    res
-      .title('Dashboard')
-      .render('dashboard/views/dashboard', {
-        contacts,
-        interactions,
-        interactionsPermitted: isPermittedRoute('/interactions', GLOBAL_NAV_ITEMS, userPermissions),
-      })
+      articleFeed,
+      interactionsPermitted: isPermittedRoute(
+        '/interactions',
+        GLOBAL_NAV_ITEMS,
+        userPermissions
+      ),
+    })
   } catch (error) {
     next(error)
   }
