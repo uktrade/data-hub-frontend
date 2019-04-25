a2fa2360ec70032ea1c4960ce9e5c4b907916c1b
zak a <zak.assali@digital.trade.gov.uk>
2019-04-25 13:17:25 +0100

    Add PR request changes

--- test/unit/components/info-feed.test.js
+++ test/unit/components/info-feed.test.js
@@ -37,56 +37,47 @@ const data = {
 }
 
 describe('Info feed component', () => {
+  beforeEach(() => {
+    this.component = renderComponentToDom('info-feed', data)
+    this.heading = this.component.querySelector('.dashboard-section-title')
+    this.subHeading = this.component.querySelector(
+      '.dashboard-section__subheading'
+    )
+    this.outboundLink = this.component.querySelector(
+      '.dashboard-section__link'
+    )
+    this.infoFeedLink = this.component.querySelectorAll(
+      '.dashboard-section__info-feed-link'
+    )
+    this.infoFeedDate = this.component.querySelectorAll(
+      '.dashboard-section__info-feed-date'
+    )
+  })
   describe('Render correct heading', () => {
-    beforeEach(() => {
-      this.component = renderComponentToDom('info-feed', data)
-      this.heading = this.component.querySelector('.dashboard-section-title')
-    })
-    it('show heading', () => {
+    it('should render heading', () => {
       expect(this.heading.textContent).to.equal(data.heading)
     })
   })
 
   describe('Render correct sub-heading', () => {
-    beforeEach(() => {
-      this.component = renderComponentToDom('info-feed', data)
-      this.subHeading = this.component.querySelector(
-        '.dashboard-section__subheading'
-      )
-    })
-    it('render sub-heading', () => {
+    it('should render sub-heading', () => {
       expect(this.subHeading.textContent).to.equal(data.subHeading)
     })
   })
 
   describe('Render correct outbound link', () => {
-    beforeEach(() => {
-      this.component = renderComponentToDom('info-feed', data)
-      this.outboundLink = this.component.querySelector(
-        '.dashboard-section__link'
-      )
-    })
-    it('render outbound link', () => {
+    it('should render outbound link', () => {
       expect(this.outboundLink.textContent).to.equal(data.outboundLinkText)
       expect(this.outboundLink.href).to.equal(data.outboundLinkURL)
     })
   })
 
   describe('render feed values', () => {
-    beforeEach(() => {
-      this.component = renderComponentToDom('info-feed', data)
-      this.infoFeedLink = this.component.querySelectorAll(
-        '.dashboard-section__info-feed-link'
-      )
-      this.infoFeedDate = this.component.querySelectorAll(
-        '.dashboard-section__info-feed-date'
-      )
-    })
-    it('only render as many feed items as feed limit value', () => {
+    it('should only render as many feed items as feed limit value', () => {
       expect(this.infoFeedLink.length).to.equal(data.feedLimit)
       expect(this.infoFeedDate.length).to.equal(data.feedLimit)
     })
-    it('render correct feed items', () => {
+    it('should render correct feed items', () => {
       expect(this.infoFeedLink[0].text).to.equal(data.dataFeed[0].heading)
       expect(this.infoFeedLink[0].href).to.equal(data.dataFeed[0].link)
       expect(this.infoFeedLink[1].text).to.equal(data.dataFeed[1].heading)
