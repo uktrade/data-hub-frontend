3a17247eb1bc62bb0da310fa24512b156aa6e3c5
Pippo Raimondi <filipporai@gmail.com>
2019-04-29 11:43:29 +0100

    Add OMIS edit draft tests to cypress

---
+++ test/functional/cypress/selectors/omis/create.js
@@ -0,0 +1,11 @@
+module.exports = {
+  contact: '#field-contact',
+  country: '#field-primary_market',
+  sectorYes: '[for="field-use_sector_from_company-1"]',
+  sectorNo: '[for="field-use_sector_from_company-2"]',
+  sector: '#field-sector',
+  continue: '.button',
+  validationMsg: '.c-error-summary__error-text',
+  suggestedSectorMsg: '.c-message--muted',
+  archivedMsg: '.c-message',
+}
