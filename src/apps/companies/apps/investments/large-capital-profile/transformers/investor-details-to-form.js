68fa2a96c86add2cbab1d1da77068d835fc62b84
Paul Gain <paul.gain@javexconsulting.com>
2019-05-01 17:48:43 +0100

    Add Investor details required checks
    Added 4 GOV.UK radio buttons:
    * Cleared
    * Issues identified
    * Not yet checked
    * Checks not required - See Investor Screening Report (ISR) guidance
    
    The first two radio buttons conditionally show a GOV.UK date field
    where the user can define a 'Date of most recent checks'.
    
    Displayed the radio button selection and date within the details view.
    
    https://uktrade.atlassian.net/browse/IPBETA-270

---
+++ src/apps/companies/apps/investments/large-capital-profile/transformers/investor-details-to-form.js
@@ -0,0 +1,61 @@
+/* eslint-disable camelcase */
+const { find, get } = require('lodash')
+const moment = require('moment')
+const types = require('../constants')
+
+const transformObjectToOption = ({ value, label }) => ({ value, text: label })
+
+const parseDate = (dateStr) => {
+  const date = moment(dateStr, 'YYYY-MM-DD', true)
+  return {
+    day: date.date(),
+    month: date.month() + 1,
+    year: date.year(),
+  }
+}
+
+const transformInvestorTypes = (investorTypes, { investorType }) => {
+  const options = investorTypes.map(transformObjectToOption)
+  const { value } = investorType
+
+  if (value) {
+    find(options, (item) => item.value === value).selected = true
+  }
+
+  options.unshift({
+    value: null,
+    text: '-- Please select an investor type --',
+  })
+
+  return options
+}
+
+const transformRequiredChecks = (requiredChecksMetaData, { requiredChecks }) => {
+  const radioButtons = requiredChecksMetaData.map(transformObjectToOption)
+
+  if (requiredChecks.conducted) {
+    find(radioButtons, (item) => item.value === requiredChecks.conducted.id).checked = true
+  }
+
+  const retVal = {
+    ...requiredChecks,
+    cleared: find(radioButtons, item => item.text === types.CLEARED),
+    issuesIdentified: find(radioButtons, item => item.text === types.ISSUES_IDENTIFIED),
+    notYetChecked: find(radioButtons, item => item.text === types.NOT_YET_CHECKED),
+    notRequired: find(radioButtons, item => item.text === types.CHECKS_NOT_REQUIRED),
+  }
+
+  const selectedType = get(requiredChecks, 'conducted.name')
+  if (selectedType === types.CLEARED) {
+    retVal.cleared.date = parseDate(requiredChecks.conductedOn)
+  } else if (selectedType === types.ISSUES_IDENTIFIED) {
+    retVal.issuesIdentified.date = parseDate(requiredChecks.conductedOn)
+  }
+
+  return retVal
+}
+
+module.exports = {
+  transformInvestorTypes,
+  transformRequiredChecks,
+}
