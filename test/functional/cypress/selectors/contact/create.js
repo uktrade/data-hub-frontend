b66202dfefd007df3d7778244d8964adadd3d641
Pippo Raimondi <filipporai@gmail.com>
2019-05-03 16:28:17 +0100

    Migrate contact save feature tests to Cypress

---
+++ test/functional/cypress/selectors/contact/create.js
@@ -0,0 +1,15 @@
+const msgCls = '.c-form-group__error-message'
+
+module.exports = {
+  addContact: '.c-form-actions .button',
+  validationHeader: '.c-error-summary__heading',
+  validationFieldMsg: {
+    name: `[for="field-first_name"] ${msgCls}`,
+    surname: `[for="field-last_name"] ${msgCls}`,
+    primaryContact: `#group-field-primary ${msgCls}`,
+    countryCode: `[for="field-telephone_countrycode"] ${msgCls}`,
+    phone: `#group-field-telephone_number > label > ${msgCls}`,
+    email: `#group-field-email > label > ${msgCls}`,
+  },
+  validationSummary: (index) => { return `.c-error-summary__list li:nth-child(${index + 1})` },
+}
