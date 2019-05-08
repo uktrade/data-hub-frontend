5f256cb0822dab4c0282025b20f5f751ac5b1524
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-05-08 16:39:53 +0100

    Remove references of redundant address fields

--- test/unit/apps/companies/controllers/edit.test.js
+++ test/unit/apps/companies/controllers/edit.test.js
@@ -325,7 +325,7 @@ describe('Company edit controller', () => {
       })
     })
 
-    context('when editing a government department with a trading address', () => {
+    context('when editing a government department', () => {
       beforeEach(async () => {
         const reqMock = assign(this.reqMock, {
           query: {
@@ -336,7 +336,6 @@ describe('Company edit controller', () => {
           locals: {
             formData: {
               business_type: '9cd14e94-5d95-e211-a939-e4115bead28a',
-              trading_address_1: 'address',
             },
             company: {
               id: 1,
