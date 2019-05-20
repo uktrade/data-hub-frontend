c1218bd496543d310e3e6305656f0d29e9ccd638
Paul Gain <paul.gain@javexconsulting.com>
2019-05-20 12:54:00 +0100

    Improved the required checks within Large capital profile
    The API has been improved as it now accepts null for both fields:
     1. required_checks_conducted_on
     2. required_checks_conducted_by
    
    This has simplified the frontend as there are less checks to perform.
    
    Refactored the logic around obtaining the Adviser id from a POST with
    supporting unit tests.
    
    https://uktrade.atlassian.net/browse/IPBETA-270

--- src/apps/companies/apps/investments/large-capital-profile/transformers/investor-details-to-api.js
+++ src/apps/companies/apps/investments/large-capital-profile/transformers/investor-details-to-api.js
@@ -1,7 +1,7 @@
-const getRequiredChecksDate = (body, field) => {
-  const day = body[`${field}-day`]
-  const month = body[`${field}-month`]
-  const year = body[`${field}-year`]
+const getRequiredChecksDate = (body) => {
+  const day = body[`${body.requiredChecks}-day`]
+  const month = body[`${body.requiredChecks}-month`]
+  const year = body[`${body.requiredChecks}-year`]
   if (day && month && year) {
     return `${year}-${month}-${day}`
   }
@@ -9,50 +9,25 @@ const getRequiredChecksDate = (body, field) => {
   return null
 }
 
+const getRequiredChecksAdviser = (body) => {
+  const adviserId = body[`${body.requiredChecks}-adviser`]
+  return adviserId === undefined ? null : adviserId
+}
+
 const nullifyIfEmptyString = (value) => {
   return value === '' ? null : value
 }
 
-const getAdviser = ({ conducted, clearedId, issuesIdentifiedId, adviser }) => {
-  if (conducted === clearedId) {
-    return adviser[0]
-  }
-
-  if (conducted === issuesIdentifiedId) {
-    return adviser[1]
-  }
-
-  return null
-}
-
 const transformInvestorDetails = (body) => {
-  const {
-    investorType,
-    globalAssetsUnderManagement,
-    investableCapital,
-    investorDescription,
-    conducted,
-  } = body
-
-  const investorDetails = {
-    investor_type: investorType,
-    global_assets_under_management: nullifyIfEmptyString(globalAssetsUnderManagement),
-    investable_capital: nullifyIfEmptyString(investableCapital),
-    investor_description: investorDescription,
-    required_checks_conducted: conducted,
+  return {
+    investor_type: body.investorType || null,
+    global_assets_under_management: nullifyIfEmptyString(body.globalAssetsUnderManagement),
+    investable_capital: nullifyIfEmptyString(body.investableCapital),
+    investor_description: body.investorDescription,
+    required_checks_conducted: body.requiredChecks,
+    required_checks_conducted_on: getRequiredChecksDate(body),
+    required_checks_conducted_by: getRequiredChecksAdviser(body),
   }
-
-  const adviser = getAdviser(body)
-  if (adviser) {
-    investorDetails.required_checks_conducted_by = getAdviser(body)
-  }
-
-  const requiredChecksDate = getRequiredChecksDate(body, conducted)
-  if (requiredChecksDate) {
-    investorDetails.required_checks_conducted_on = requiredChecksDate
-  }
-
-  return investorDetails
 }
 
 module.exports = transformInvestorDetails
