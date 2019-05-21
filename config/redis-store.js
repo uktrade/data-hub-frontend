acc0a1a64f52ab5fe532cda97847e4848147825e
zak a <zak.assali@digital.trade.gov.uk>
2019-05-21 15:50:58 +0100

    Change redis config and add isTest flag

--- config/redis-store.js
+++ config/redis-store.js
@@ -6,34 +6,36 @@ const logger = require('./logger')
 const config = require('./')
 
 const RedisStore = redisConnect(session)
-
 let redisConfig = {
   port: config.redis.port,
   host: config.redis.host,
 }
 
-if (config.redis.url) {
-  const redisURL = url.parse(config.redis.url)
-  redisConfig = {
-    port: redisURL.port || config.redis.port,
-    host: redisURL.hostname || config.redis.host,
+const getRedisConfig = function () {
+  if (config.redis.url) {
+    const redisURL = url.parse(config.redis.url)
+    redisConfig = {
+      port: redisURL.port || config.redis.port,
+      host: redisURL.hostname || config.redis.host,
+    }
+    if (redisURL.auth) {
+      redisConfig.password = redisURL.auth.split(':')[1]
+    }
   }
-  if (redisURL.auth) {
-    redisConfig.password = redisURL.auth.split(':')[1]
+  if (config.redis.useTLS) {
+    redisConfig.tls = {
+      // allows self-signed certificates
+      rejectUnauthorized: false,
+    }
+    // allows us to retry unsent commands after a timeout
+    redisConfig.retry_unfulfilled_commands = true
   }
-}
-if (config.redis.useTLS) {
-  redisConfig.tls = {
-    // allows self-signed certificates
-    rejectUnauthorized: false,
-  }
-  // allows us to retry unsent commands after a timeout
-  redisConfig.retry_unfulfilled_commands = true
+  return redisConfig
 }
 
-const client = Redis.createClient(redisConfig)
+const client = Redis.createClient(getRedisConfig())
 
-client.on('error', (e) => {
+client.on('error', e => {
   logger.log('error', 'Error connecting to redis')
   logger.log('error', e)
   throw e
@@ -57,4 +59,4 @@ const redisStore = new RedisStore({
   ttl: config.session.ttl / 1000,
   secret: config.session.secret,
 })
-module.exports = redisStore
+module.exports = { redisStore, getRedisConfig }
