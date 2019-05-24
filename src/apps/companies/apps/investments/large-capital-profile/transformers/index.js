c072ef63b2640f4e91cebf650aa370881e3eff45
Paul Gain <paul.gain@javexconsulting.com>
2019-05-24 08:42:31 +0100

    Added 4 new radio buttons for 'Minimum equity percentage'
    1. 0% - Not required
    2. 1-19%
    3. 20-49%
    4. 50% +
    
    https://uktrade.atlassian.net/browse/IPBETA-272

--- src/apps/companies/apps/investments/large-capital-profile/transformers/index.js
+++ src/apps/companies/apps/investments/large-capital-profile/transformers/index.js
@@ -1,5 +1,5 @@
 const { transformInvestorTypes, transformRequiredChecks, transformAdvisers } = require('./investor-details-to-form')
-const { transformCheckboxes } = require('./investor-requirements-to-form')
+const { transformCheckboxes, transformRadioButtons } = require('./investor-requirements-to-form')
 const transformInvestorDetails = require('./investor-details-to-api')
 const transformInvestorRequirements = require('./investor-requirements-to-api')
 const transformProfile = require('./transform-profile')
@@ -8,6 +8,7 @@ module.exports = {
   transformProfile,
   transformAdvisers,
   transformCheckboxes,
+  transformRadioButtons,
   transformInvestorTypes,
   transformRequiredChecks,
   transformInvestorDetails,
