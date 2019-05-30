46c59270c2cc510da99f1d031997c397a06a4ac6
zak a <zak.assali@digital.trade.gov.uk>
2019-05-30 13:58:36 +0100

    Add additional filters to investments page

---
+++ test/functional/cypress/specs/investment-project/filter-spec.js
@@ -0,0 +1,55 @@
+const selectors = require('../../selectors')
+
+describe('Investments Collections Filter', () => {
+  before(() => {
+    cy.visit('/investments')
+    cy
+      .get(selectors.entityCollection.entities)
+      .children()
+      .should('have.length', 10)
+  })
+
+  beforeEach(() => {
+    cy.server()
+    cy.route('/investments/projects?*').as('filterResults')
+  })
+
+  it('should filter by sector', () => {
+    const sector = selectors.filter.investments.sector
+    const { typeahead } = selectors.filter
+    cy
+      .get(typeahead(sector).selectedOption)
+      .click()
+      .get(typeahead(sector).textInput)
+      .type('Advanced Engineering')
+      .get(typeahead(sector).options)
+      .should('have.length', 1)
+      .get(typeahead(sector).firstOption)
+      .click()
+
+    cy.wait('@filterResults').then(xhr => {
+      expect(xhr.url).to.contain(
+        'sector_descends=af959812-6095-e211-a939-e4115bead28a'
+      )
+    })
+  })
+
+  it('should filter by country', () => {
+    const country = selectors.filter.investments.country
+    const { typeahead } = selectors.filter
+
+    cy
+      .get(typeahead(country).selectedOption)
+      .click()
+      .get(typeahead(country).textInput)
+      .type('United Kingdom')
+      .get(typeahead(country).options)
+      .should('have.length', 1)
+      .get(typeahead(country).firstOption)
+      .click()
+
+    cy.wait('@filterResults').then(xhr => {
+      expect(xhr.url).to.contain('country=80756b9a-5d95-e211-a939-e4115bead28a')
+    })
+  })
+})
