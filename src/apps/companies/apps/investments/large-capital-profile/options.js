21445f5feb3cbdf706a14c2c61eaf10fed07628b
Paul Gain <paul.gain@javexconsulting.com>
2019-05-30 11:59:34 +0100

    Add getGovUKOptions metadata API call
    Currently getOptions() makes an API call and transforms all metadata
    items into an object that is consumed by all Data Hub components.
    
    From:
    
    {
      "id": "1",
      "name": "Angel syndicate",
    }
    
    To:
    
    {
      "value": "1",
      "label": "Angel syndicate"
    }
    
    The new function getGovUKOptions() transforms objects to be readily
    consumed by GOV.UK components:
    
    From:
    
    {
      "id": "1",
      "name": "Angel syndicate",
    }
    
    To:
    
    {
      "value": "1",
      "text": "Angel syndicate"
    }
    
    The new function also takes a transformer for metadata
    custom transformations.

--- src/apps/companies/apps/investments/large-capital-profile/options.js
+++ src/apps/companies/apps/investments/large-capital-profile/options.js
@@ -1,26 +1,34 @@
 const { getOptions } = require('../../../../../lib/options')
 const { getAdvisers } = require('../../../../adviser/repos')
+const { transformObjectToGovUKOption } = require('../../../../../apps/transformers')
 
-const DO_NOT_SORT = { sorted: false }
+const OPTIONS = {
+  sorted: false,
+  transformer: transformObjectToGovUKOption,
+}
 
 const getInvestorDetailsOptions = (token) => {
   return [
-    getOptions(token, 'capital-investment/investor-type'),
-    getOptions(token, 'capital-investment/required-checks-conducted'),
+    getOptions(token, 'capital-investment/investor-type', OPTIONS),
+    getOptions(token, 'capital-investment/required-checks-conducted', OPTIONS),
     getAdvisers(token),
   ]
 }
 
 const getInvestorRequirementsOptions = (token) => {
   return [
-    getOptions(token, 'capital-investment/deal-ticket-size', DO_NOT_SORT),
-    getOptions(token, 'capital-investment/large-capital-investment-type'),
-    getOptions(token, 'capital-investment/return-rate', DO_NOT_SORT),
-    getOptions(token, 'capital-investment/time-horizon', DO_NOT_SORT),
-    getOptions(token, 'capital-investment/restriction', DO_NOT_SORT),
-    getOptions(token, 'capital-investment/construction-risk', DO_NOT_SORT),
-    getOptions(token, 'capital-investment/equity-percentage', DO_NOT_SORT),
-    getOptions(token, 'capital-investment/desired-deal-role', DO_NOT_SORT),
+    getOptions(token, 'capital-investment/deal-ticket-size', OPTIONS),
+    getOptions(token, 'capital-investment/large-capital-investment-type', {
+      sorted: true,
+      sortPropertyName: 'text',
+      transformer: transformObjectToGovUKOption,
+    }),
+    getOptions(token, 'capital-investment/return-rate', OPTIONS),
+    getOptions(token, 'capital-investment/time-horizon', OPTIONS),
+    getOptions(token, 'capital-investment/restriction', OPTIONS),
+    getOptions(token, 'capital-investment/construction-risk', OPTIONS),
+    getOptions(token, 'capital-investment/equity-percentage', OPTIONS),
+    getOptions(token, 'capital-investment/desired-deal-role', OPTIONS),
   ]
 }
 
