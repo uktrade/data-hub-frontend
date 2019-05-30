a45f770095589b537a9e7c078cae1b9f2c89b0b8
zak a <zak.assali@digital.trade.gov.uk>
2019-05-30 14:10:11 +0100

    Update filter selectors

--- test/functional/cypress/selectors/filter.js
+++ test/functional/cypress/selectors/filter.js
@@ -2,11 +2,38 @@ module.exports = {
   name: '#field-name',
   statusActive: '[for="field-archived-1"]',
   statusInactive: '[for="field-archived-2"]',
-  firstCountry: '[for="field-country-1"]',
   firstUkRegion: '[for="field-uk_region-1"]',
+  country: '#group-field-country',
+  sector: '#group-field-sector_descends',
   contacts: {
+    sector: '#group-field-company_sector_descends',
     companyName: '#field-company_name',
-    firstCountry: '[for="field-address_country-1"]',
+    country: '#group-field-address_country',
     firstUkRegion: '[for="field-company_uk_region-1"]',
+  },
+  investments: {
+    companyName: '#group-field-sector_descends',
+    country: '#group-field-investor_company_country',
+    firstUkRegion: '[for="field-uk_region_location-1"]',
+    sector: '#group-field-sector_descends',
+  },
+  omis: {
+    country: '#group-field-primary_market',
+    firstUkRegion: '[for="field-uk_region-1"]',
+    sector: '#group-field-sector_descends',
+  },
+  events: {
+    country: '#group-field-address_country',
+  },
+  typeahead (id) {
+    return {
+      fieldset: id,
+      placeHolder: `${id} .multiselect__tags`,
+      textInput: `${id} .multiselect__input`,
+      selectedOption: `${id} .multiselect__single`,
+      addAnotherBtn: `${id} .js-AddItems__add--typeahead`,
+      firstOption: `${id} .multiselect__element:nth-child(1)`,
+      options: `${id} .multiselect__element`,
+    }
   },
 }
