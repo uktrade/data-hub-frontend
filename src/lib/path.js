f7376f1edc270260c2e6b16711cb73b3e3d75ecc
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-05-31 12:47:30 +0100

    Move join paths helper to common library

---
+++ src/lib/path.js
@@ -0,0 +1,21 @@
+const { trimEnd, trimStart, map, join, compact } = require('lodash')
+
+const joinPaths = (paths) => {
+  const cleanPaths = compact(map(paths, (path, index) => {
+    if (index === 0) {
+      return trimEnd(path, '/')
+    }
+
+    if (index === paths.length - 1) {
+      return trimStart(path, '/')
+    }
+
+    return trimStart(trimEnd(path, '/'), '/')
+  }))
+
+  return join(cleanPaths, '/')
+}
+
+module.exports = {
+  joinPaths,
+}
