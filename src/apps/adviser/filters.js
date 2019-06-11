10e771024a2161eb9fe397c1f3d65512f2e1686a
zak a <zak.assali@digital.trade.gov.uk>
2019-06-11 09:57:37 +0100

    Simplify filter logic

--- src/apps/adviser/filters.js
+++ src/apps/adviser/filters.js
@@ -1,11 +1,11 @@
 const { get, includes } = require('lodash')
 
-function filterActiveAdvisers ({ advisers = [], includeAdviser }) {
+function filterActiveAdvisers ({ advisers = [], includeAdviser = [] }) {
+  includeAdviser = [...includeAdviser]
   return advisers.filter(adviser => {
     // This returns all active users plus the include advisor (whether they are active or not)
     return (
       get(adviser, 'is_active', true) ||
-      adviser.id === includeAdviser ||
       includes(includeAdviser, adviser.id)
     )
   })
