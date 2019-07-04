989dce28f86afec09e1026f4ad751502b505003a
Pippo Raimondi <filipporai@gmail.com>
2019-07-04 16:07:22 +0100

    Update the way we set names to the visual screenshots

--- test/visual/wdio.conf.js
+++ test/visual/wdio.conf.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 const WdioImage = require ('@uktrade/wdio-image-diff-js').default
 const browserstack = require('browserstack-local')
+const { addAttachment } = require('@wdio/allure-reporter').default
 
 const IMPLICIT_TIMEOUT = process.env.WDIO_IMPLICIT_TIMEOUT || 90000
 const BASE_URL = process.env.BASE_URL || 'http://localhost:3000'
@@ -8,7 +9,7 @@ const BASE_URL = process.env.BASE_URL || 'http://localhost:3000'
 const browserStackUser = process.env.BROWSERSTACK_USERNAME || ''
 const browserStackKey = process.env.BROWSERSTACK_ACCESS_KEY || ''
 const isRemote = !!process.env.IS_REMOTE
-
+let testName = 'undefined'
 
 const remoteConfig = {
   services: ['browserstack'],
@@ -59,15 +60,33 @@ const defaultConfig = {
   connectionRetryCount: 3,
   services: ['chromedriver'],
   framework: 'mocha',
-  reporters: ['spec'],
+  reporters: ['spec', ['allure', {
+    outputDir: 'allure-results',
+    disableWebdriverStepsReporting: true,
+    disableWebdriverScreenshotsReporting: true,
+  }]],
   mochaOpts: {
     timeout: 60000,
   },
-  before: function () {
+  before: () => {
     browser.setTimeout({ 'implicit': IMPLICIT_TIMEOUT })
     const wdioImageDiff = new WdioImage(browser, { threshold: 0.1, width: 1792, height: 1008, })
     browser.imageDiff = wdioImageDiff
   },
+  beforeTest: (test) => {
+    testName = `${test.fullTitle} - ${browser.capabilities.browserName}`
+    browser.imageDiff.testName = testName
+  },
+  afterEach: () => {
+    const fs = require('fs')
+    const path = require('path')
+    const filepath = path.join(`/Users/filipporaimondi/code/data-hub-frontend/visual-screenshots/baseline/${testName}.png`)
+
+    fs.readFile(filepath, function(err, data) {
+      if (err) throw err
+      addAttachment('Baseline', Buffer.from(data, 'base64'))
+    })
+  }
 }
 
 exports.config = isRemote
