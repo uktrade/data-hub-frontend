270738946192c7f467771d66c36696c81a354aaa
zak a <zak.assali@digital.trade.gov.uk>
2019-07-09 07:44:03 +0100

    Refactor transformInteractionFormBodyToApiRequest & Add additional tests

--- src/apps/interactions/transformers/interaction-form-body-to-api.js
+++ src/apps/interactions/transformers/interaction-form-body-to-api.js
@@ -1,4 +1,4 @@
-const { omit } = require('lodash')
+const { omit, flatten } = require('lodash')
 
 const castCompactArray = require('../../../lib/cast-compact-array')
 const { transformDateObjectToDateString } = require('../../transformers')
@@ -11,56 +11,48 @@ function transformInteractionFormBodyToApiRequest (props, services) {
   const advisersArray = castCompactArray(props.dit_participants).map(
     adviser => ({ adviser: adviser })
   )
-  const selectedServiceOption = services.filter(
+  const selectedServiceOption = services.find(
     service => service.value === props.service
   )
-
   const serviceAnswers = {}
 
-  const service = selectedServiceOption.map(option => {
-    if (option.secondaryOptions.length) {
-      return props.subService
-    } else {
-      return option.value
+  const serviceHasSecondaryOptions = !!selectedServiceOption.secondaryOptions
+    .length
+
+  const service = serviceHasSecondaryOptions
+    ? props.subService.find(id => id.length)
+    : selectedServiceOption.value
+
+  const serviceOptionStore = serviceHasSecondaryOptions
+    ? flatten(selectedServiceOption.secondaryOptions.map(option => option.interactionQuestions))
+    : selectedServiceOption.interactionQuestions
+
+  serviceOptionStore.map(interactionQuestion => {
+    for (const [key, value] of Object.entries(props)) {
+      if (key === interactionQuestion.value) {
+        serviceAnswers[key] = {
+          [value]: {},
+        }
+      }
     }
   })
 
-  selectedServiceOption.map(question => {
-    return question.secondaryOptions.length ? question.secondaryOptions.map(secondaryOption => {
-      return secondaryOption.interactionQuestions.map((interactionQuestion) => {
-        const selectedQuestionKey = interactionQuestion.value
-        for (const [key, value] of Object.entries(props)) {
-          if (key === selectedQuestionKey) {
-            serviceAnswers[key] = {
-              [value]: {},
-            }
-          }
-        }
-      })
-    }) : question.interactionQuestions.map((interactionQuestion) => {
-      const selectedQuestionKey = interactionQuestion.value
-      for (const [key, value] of Object.entries(props)) {
-        if (key === selectedQuestionKey) {
-          serviceAnswers[key] = {
-            [value]: {},
-          }
-        }
-      }
-    })
-  })
-  return omit({
-    ...props,
-    service: service[0],
-    service_answers: serviceAnswers,
-    date: transformDateObjectToDateString('date')(props),
-    grant_amount_offered: props.grant_amount_offered || null,
-    net_company_receipt: props.net_company_receipt || null,
-    contacts: contactTypesArray,
-    dit_participants: advisersArray,
-    policy_areas: policyAreasArray,
-    policy_issue_types: policyIssueTypesArray,
-    status: INTERACTION_STATUS.COMPLETE,
-  }, ['date_day', 'date_month', 'date_year'])
+  return omit(
+    {
+      ...props,
+      service,
+      service_answers: serviceAnswers,
+      date: transformDateObjectToDateString('date')(props),
+      grant_amount_offered: props.grant_amount_offered || null,
+      net_company_receipt: props.net_company_receipt || null,
+      contacts: contactTypesArray,
+      dit_participants: advisersArray,
+      policy_areas: policyAreasArray,
+      policy_issue_types: policyIssueTypesArray,
+      status: INTERACTION_STATUS.COMPLETE,
+    },
+    ['date_day', 'date_month', 'date_year']
+  )
 }
 
 module.exports = transformInteractionFormBodyToApiRequest
--- test/unit/apps/interactions/transformers/interaction-form-body-to-api.test.js
+++ test/unit/apps/interactions/transformers/interaction-form-body-to-api.test.js
@@ -6,15 +6,19 @@ const transformedServices = transformServicesOptions(serviceOptions)
 describe('#transformInteractionFormBodyToApiRequest', () => {
   context('when all fields are populated', () => {
     beforeEach(() => {
-      this.transformed = transformInteractionFormBodyToApiRequest({
-        date_year: '2018',
-        date_month: '01',
-        date_day: '02',
-        grant_amount_offered: '1000',
-        net_company_receipt: '500',
-        policy_areas: '4444',
-        policy_issue_types: '5555',
-      }, transformedServices)
+      this.transformed = transformInteractionFormBodyToApiRequest(
+        {
+          date_year: '2018',
+          date_month: '01',
+          date_day: '02',
+          grant_amount_offered: '1000',
+          net_company_receipt: '500',
+          policy_areas: '4444',
+          policy_issue_types: '5555',
+          service: 'sv1',
+        },
+        transformedServices
+      )
     })
 
     it('should set the date', () => {
@@ -40,10 +44,14 @@ describe('#transformInteractionFormBodyToApiRequest', () => {
 
   context('when the optional fields have not been entered', () => {
     beforeEach(() => {
-      this.transformed = transformInteractionFormBodyToApiRequest({
-        grant_amount_offered: '',
-        net_company_receipt: '',
-      }, transformedServices)
+      this.transformed = transformInteractionFormBodyToApiRequest(
+        {
+          grant_amount_offered: '',
+          net_company_receipt: '',
+          service: 'sv1',
+        },
+        transformedServices
+      )
     })
 
     it('should set the grant amount offered to null', () => {
@@ -54,4 +62,80 @@ describe('#transformInteractionFormBodyToApiRequest', () => {
       expect(this.transformed.net_company_receipt).to.be.null
     })
   })
+
+  context(
+    'when selected service has no interaction questions or sub service',
+    () => {
+      beforeEach(() => {
+        this.expectedServiceId = 'sv1'
+        this.transformed = transformInteractionFormBodyToApiRequest(
+          {
+            service: this.expectedServiceId,
+          },
+          transformedServices
+        )
+      })
+
+      it('should return service id & have no answer options', () => {
+        expect(this.transformed.service).to.equal(this.expectedServiceId)
+        expect(this.transformed.service_answers).to.deep.equal({})
+      })
+    }
+  )
+
+  context(
+    'when selected service has interaction questions and sub service',
+    () => {
+      beforeEach(() => {
+        this.subServiceValues = ['', 'sv2', '']
+        this.expectedServiceLabel = 'Providing Export Advice & Information'
+        this.transformed = transformInteractionFormBodyToApiRequest(
+          {
+            service: this.expectedServiceLabel,
+            subService: this.subServiceValues,
+            'sv2-q1': 'sv2-q1-a1',
+          },
+          transformedServices
+        )
+      })
+
+      it('should return subServiceId as output service id', () => {
+        expect(this.transformed.service).to.equal('sv2')
+      })
+
+      it('should return formated service answers', () => {
+        expect(this.transformed.service_answers).to.deep.equal({
+          'sv2-q1': { 'sv2-q1-a1': {} },
+        })
+      })
+    }
+  )
+
+  context(
+    'when selected service has interaction questions and no sub service',
+    () => {
+      beforeEach(() => {
+        this.subServiceValues = ['', 'sv3', '']
+        this.expectedServiceLabel = 'sv3'
+        this.transformed = transformInteractionFormBodyToApiRequest(
+          {
+            service: this.expectedServiceLabel,
+            subService: this.subServiceValues,
+            'sv3-q1': 'sv3-q1-a1',
+          },
+          transformedServices
+        )
+      })
+
+      it('should return subServiceId as output service id', () => {
+        expect(this.transformed.service).to.equal('sv3')
+      })
+
+      it('should return formated service answers', () => {
+        expect(this.transformed.service_answers).to.deep.equal({
+          'sv3-q1': { 'sv3-q1-a1': {} },
+        })
+      })
+    }
+  )
 })
