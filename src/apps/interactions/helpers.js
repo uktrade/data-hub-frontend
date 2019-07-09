c4acb2700efc04a7a5943ff70fe8f18d45549ec5
zak a <zak.assali@digital.trade.gov.uk>
2019-07-09 12:37:15 +0100

    Add interaction form status check helper
    this allows us to check if we're in a interaction service form and populate with the correct service list structure

--- src/apps/interactions/helpers.js
+++ src/apps/interactions/helpers.js
@@ -1,9 +1,14 @@
-const { get } = require('lodash')
+const { get, includes } = require('lodash')
+const { INTERACTION_CONTEXTS } = require('./constants')
 
-const getReturnLink = (interactions) => {
+const getReturnLink = interactions => {
   return get(interactions, 'returnLink', '/interactions')
 }
 
+const isInteractionServiceForm = (key, context) =>
+  key === 'service' && includes(INTERACTION_CONTEXTS, context) === true
+
 module.exports = {
   getReturnLink,
+  isInteractionServiceForm,
 }
