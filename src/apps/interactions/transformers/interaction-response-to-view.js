641c7b5c4865eda766cf28f5437d346090b27654
zak a <zak.assali@digital.trade.gov.uk>
2019-07-09 08:21:12 +0100

    Add service name deliminator formatting on interactiontable view & tests

--- src/apps/interactions/transformers/interaction-response-to-view.js
+++ src/apps/interactions/transformers/interaction-response-to-view.js
@@ -6,13 +6,15 @@ const labels = require('../labels')
 const { getDataLabels } = require('../../../lib/controller-utils')
 
 const transformEntityLink = (entity, entityPath, noLinkText = null) => {
-  return entity ? {
-    url: `/${entityPath}/${entity.id}`,
-    name: entity.name,
-  } : noLinkText
+  return entity
+    ? {
+      url: `/${entityPath}/${entity.id}`,
+      name: entity.name,
+    }
+    : noLinkText
 }
 
-const transformDocumentsLink = (archived_documents_url_path) => {
+const transformDocumentsLink = archived_documents_url_path => {
   if (archived_documents_url_path) {
     return {
       url: config.archivedDocumentsBaseUrl + archived_documents_url_path,
@@ -25,59 +27,92 @@ const transformDocumentsLink = (archived_documents_url_path) => {
   return { name: 'There are no files or documents' }
 }
 
-function transformInteractionResponseToViewRecord ({
-  company,
-  notes,
-  date,
-  dit_participants,
-  service,
-  service_delivery_status,
-  grant_amount_offered,
-  net_company_receipt,
-  contacts,
-  policy_areas,
-  policy_issue_types,
-  policy_feedback_notes,
-  investment_project,
-  communication_channel,
-  event,
-  kind,
-  archived_documents_url_path,
-}, canShowDocuments = false) {
+function transformInteractionResponseToViewRecord (
+  {
+    company,
+    notes,
+    date,
+    dit_participants,
+    service = {},
+    service_delivery_status,
+    grant_amount_offered,
+    net_company_receipt,
+    contacts,
+    policy_areas,
+    policy_issue_types,
+    policy_feedback_notes,
+    investment_project,
+    communication_channel,
+    event,
+    kind,
+    archived_documents_url_path,
+  },
+  canShowDocuments = false
+) {
   const defaultEventText = kind === 'service_delivery' ? 'No' : null
   const kindLabels = labels[camelCase(kind)]
-  const formattedPolicyAreas = (policy_areas || []).map(policy_area => policy_area.name).join(', ')
-  const formattedPolicyTypes = (policy_issue_types || []).map(policy_type => policy_type.name).join(', ')
+  const formattedPolicyAreas = (policy_areas || [])
+    .map(policy_area => policy_area.name)
+    .join(', ')
+  const formattedPolicyTypes = (policy_issue_types || [])
+    .map(policy_type => policy_type.name)
+    .join(', ')
 
-  const formatParticipantName = (participant) => get(participant, 'team')
-    ? `${participant.adviser.name}, ${participant.team.name}`
-    : participant.adviser.name
+  const formatParticipantName = participant =>
+    get(participant, 'team')
+      ? `${participant.adviser.name}, ${participant.team.name}`
+      : participant.adviser.name
 
-  const formattedParticipants = (dit_participants || []).map(participant => formatParticipantName(participant))
+  const formattedParticipants = (dit_participants || []).map(participant =>
+    formatParticipantName(participant)
+  )
+
+  const getServiceValues = () => {
+    if (!service) return
+    const splitServiceName = service.name.split(' : ')
+    const serviceName =
+      splitServiceName[1] && !service.name.includes('A Specific DIT')
+        ? splitServiceName[1]
+        : service.name
+
+    return {
+      ...service,
+      name: serviceName,
+    }
+  }
 
   const viewRecord = {
     company: transformEntityLink(company, 'companies'),
     contacts: contacts.map(contact => transformEntityLink(contact, 'contacts')),
-    service,
+    service: getServiceValues(),
     service_delivery_status,
-    grant_amount_offered: grant_amount_offered ? {
-      type: 'currency',
-      name: grant_amount_offered,
-    } : null,
-    net_company_receipt: net_company_receipt ? {
-      type: 'currency',
-      name: net_company_receipt,
-    } : null,
+    grant_amount_offered: grant_amount_offered
+      ? {
+        type: 'currency',
+        name: grant_amount_offered,
+      }
+      : null,
+    net_company_receipt: net_company_receipt
+      ? {
+        type: 'currency',
+        name: net_company_receipt,
+      }
+      : null,
     notes: notes,
     date: {
       type: 'date',
       name: date,
     },
     dit_participants: formattedParticipants,
-    investment_project: transformEntityLink(investment_project, 'investments/projects'),
+    investment_project: transformEntityLink(
+      investment_project,
+      'investments/projects'
+    ),
     event: transformEntityLink(event, 'events', defaultEventText),
     communication_channel: communication_channel,
-    documents: canShowDocuments ? transformDocumentsLink(archived_documents_url_path) : null,
+    documents: canShowDocuments
+      ? transformDocumentsLink(archived_documents_url_path)
+      : null,
     policy_issue_types: formattedPolicyTypes,
     policy_areas: formattedPolicyAreas,
     policy_feedback_notes: policy_feedback_notes,
