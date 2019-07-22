4984a76d30c6152932d0d7dd088df19a05e65c2e
Pippo Raimondi <filipporai@gmail.com>
2019-07-22 14:47:31 +0100

    Add new baselines and update home page baseline name

--- test/visual/specs/data-hub-pages-spec.js
+++ test/visual/specs/data-hub-pages-spec.js
@@ -1,6 +1,6 @@
 const assert = require('assert')
 
-describe('Visual Test', () => {
+describe('Data Hub core pages', () => {
   it('should visually check data hub home page is correct', async () => {
     await browser.url('')
     await browser.imageDiff.take()
@@ -9,7 +9,7 @@ describe('Visual Test', () => {
     })
   })
 
-  it('should visually check data hub visual page is correct', async () => {
+  it('should validate contacts page is visually correct', async () => {
     await browser.url('/contacts')
     await browser.imageDiff.take()
     await browser.imageDiff.validate().then(result => {
@@ -17,7 +17,7 @@ describe('Visual Test', () => {
     })
   })
 
-  it('should visually check data hub visual page is correct', async () => {
+  it('should validate companies page is visually correct', async () => {
     await browser.url('/companies')
     await browser.imageDiff.take()
     await browser.imageDiff.validate().then(result => {
@@ -25,7 +25,7 @@ describe('Visual Test', () => {
     })
   })
 
-  it('should visually check data hub visual page is correct', async () => {
+  it('should validate interactions page is visually correct', async () => {
     await browser.url('/interactions')
     await browser.imageDiff.take()
     await browser.imageDiff.validate().then(result => {
@@ -33,7 +33,7 @@ describe('Visual Test', () => {
     })
   })
 
-  it('should visually check data hub visual page is correct', async () => {
+  it('should validate investments page is visually correct', async () => {
     await browser.url('/investments/projects')
     await browser.imageDiff.take()
     await browser.imageDiff.validate().then(result => {
@@ -41,7 +41,7 @@ describe('Visual Test', () => {
     })
   })
 
-  it('should visually check data hub visual page is correct', async () => {
+  it('should validate orders page is visually correct', async () => {
     await browser.url('/omis')
     await browser.imageDiff.take()
     await browser.imageDiff.validate().then(result => {
