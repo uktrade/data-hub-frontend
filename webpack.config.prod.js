771079bcf4b68b467fed9d24bdd8abce9f3ea8e1
zak a <zak.assali@digital.trade.gov.uk>
2019-08-02 11:13:29 +0100

    Fix failing build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   devtool: false,
@@ -9,18 +10,18 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('production'),
+        NODE_ENV: JSON.stringify('production'),
       },
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-      },
-      output: {
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        compress: {},
+        output: {},
         comments: false,
+        warnings: false,
+        sourceMap: true,
+        dead_code: true,
       },
-      sourceMap: true,
-      dead_code: true,
     }),
     new ExtractTextPlugin('css/[name].[contenthash:8].css'),
   ],
