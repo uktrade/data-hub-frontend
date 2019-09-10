5c5e2ba8937aeb809b6fd573e0487e38af538471
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-09-10 13:41:15 +0100

    Remove i18n-future dependency
    i18n-future is dependent on Lodash 3.7.0. A
    security vulnerability has been highlighted with
    the Lodash dependency.
    
    The i18n-future package has not been released for
    over three years and is no longer maintained.
    
    As a result it is best this dependency is removed.

--- src/apps/omis/apps/create/router.js
+++ src/apps/omis/apps/create/router.js
@@ -1,11 +1,6 @@
-const path = require('path')
 const router = require('express').Router()
 const wizard = require('hmpo-form-wizard')
-const i18nFuture = require('i18n-future')
 
-const i18n = i18nFuture({
-  path: path.resolve(__dirname, '../../locales/__lng__/__ns__.json'),
-})
 const steps = require('./steps')
 const fields = require('../../fields')
 const { CreateController } = require('../../controllers')
@@ -15,7 +10,6 @@ const config = {
   name: 'omis-create-order',
   journeyName: 'omis-create-order',
   template: '_layouts/form-wizard-step',
-  translate: i18n.translate.bind(i18n),
 }
 
 router.use(wizard(steps, fields, config))
--- src/apps/omis/apps/edit/controllers/edit-handler.js
+++ src/apps/omis/apps/edit/controllers/edit-handler.js
@@ -1,13 +1,8 @@
-const path = require('path')
-const i18nFuture = require('i18n-future')
 const { reduce } = require('lodash')
 
 const EditController = require('../../../controllers/edit')
 const steps = require('../steps')
 const fields = require('../fields')
-const i18n = i18nFuture({
-  path: path.resolve(__dirname, '../../../locales/__lng__/__ns__.json'),
-})
 
 function editHandler (req, res, next) {
   const step = steps[`/${req.params.step}`]
@@ -27,7 +22,6 @@ function editHandler (req, res, next) {
     next: nextUrl,
     backLink: nextUrl,
     controller: EditController,
-    translate: i18n.translate.bind(i18n),
     successMessage: 'Changes saved',
   }
   const overrides = {
