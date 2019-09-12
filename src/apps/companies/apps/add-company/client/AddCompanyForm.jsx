141a229185d92aae70eb5b0d1952010d61dad857
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-09-12 16:09:42 +0100

    Add "still cannot find company" step
    When the user has searched for a company and they
    still cannot find it then they have the option
    to click manually enter the information.

--- src/apps/companies/apps/add-company/client/AddCompanyForm.jsx
+++ src/apps/companies/apps/add-company/client/AddCompanyForm.jsx
@@ -1,11 +1,18 @@
 /* eslint-disable camelcase */
 
 import React, { useState } from 'react'
-import { H3, LoadingBox } from 'govuk-react'
+import {
+  H3,
+  LoadingBox,
+  UnorderedList,
+  ListItem,
+  Details,
+} from 'govuk-react'
 import PropTypes from 'prop-types'
 import { compact, get } from 'lodash'
 import {
   FieldDnbCompany,
+  FieldInput,
   FieldRadios,
   FieldSelect,
   Form,
@@ -25,7 +32,7 @@ const COMPANY_LOCATION_OPTIONS = {
   },
 }
 
-function AddCompanyForm ({ host, csrfToken, foreignCountries }) {
+function AddCompanyForm ({ host, csrfToken, foreignCountries, regions, sectors }) {
   const [submitted, setSubmitted] = useState(false)
   const [isSubmitting, setIsSubmitting] = useState(false)
 
@@ -118,20 +125,99 @@ function AddCompanyForm ({ host, csrfToken, foreignCountries }) {
             />
           </Step>
 
-          <Step name="companyDetails" forwardButtonText="Add company">
-            <H3>Confirm you want to add this company to Data Hub</H3>
+          {!form.values.cannotFind && (
+            <Step name="companyDetails" forwardButtonText="Add company">
+              <H3>Confirm you want to add this company to Data Hub</H3>
 
-            <DefinitionList header={getCompanyName(form.values)}>
-              <DefinitionList.Row
-                label="Companies House number"
-                description={getCompaniesHouseNumber(form.values)}
+              <DefinitionList header={getCompanyName(form.values)}>
+                <DefinitionList.Row
+                  label="Companies House number"
+                  description={getCompaniesHouseNumber(form.values)}
+                />
+                <DefinitionList.Row
+                  label="Address"
+                  description={getCompanyAddress(form.values)}
+                />
+              </DefinitionList>
+            </Step>
+          )}
+
+          {form.values.cannotFind && (
+            <Step name="unhappy" forwardButtonText="Add company">
+
+              <Details summary="Why am I seeing this?">
+                The company you want to add to Data Hub cannot be found in the external databases Data Hub checks.
+                You will need to provide information about the company, so the company can be added to Data Hub
+                while the Data Hub support team checks with the company the information you have provided.
+              </Details>
+
+              <FieldRadios
+                name="organisation_type"
+                label="Organisation type"
+                required="Select organisation"
+                options={[
+                  { label: 'Business', value: 'business' },
+                  { label: 'Charity', value: 'charity' },
+                  { label: 'Public body', value: 'publicbody' },
+                ]}
               />
-              <DefinitionList.Row
-                label="Address"
-                description={getCompanyAddress(form.values)}
+
+              <FieldInput
+                label="Name of company"
+                name="name"
+                required="Enter name"
+                type="text"
               />
-            </DefinitionList>
-          </Step>
+
+              <FieldInput
+                label="Company's website"
+                name="website"
+                required="Enter website"
+                type="text"
+              />
+
+              <FieldInput
+                label="Company's telephone number"
+                name="telephone"
+                type="text"
+              />
+
+              <FieldSelect
+                name="region"
+                label="DIT region"
+                emptyOption="-- Select DIT region --"
+                options={regions}
+                required="Select DIT region"
+              />
+
+              <FieldSelect
+                name="sector"
+                label="DIT sector"
+                emptyOption="-- Select DIT sector --"
+                options={sectors}
+                required="Select DIT sector"
+              />
+
+              <H3>What happens next</H3>
+              <p>
+                You are requesting a new company record to be added to Data Hub. This is what will happen next:
+                <UnorderedList>
+                  <ListItem>
+                    The information you have provided will be sent to the Data Hub support team and they will check this
+                    information with the company
+                  </ListItem>
+                  <ListItem>
+                    In the meantime, you can continue to record interactions with this company
+                  </ListItem>
+                  <ListItem>
+                    You will be sent an email with the outcome of the investigation
+                  </ListItem>
+                </UnorderedList>
+              </p>
+
+            </Step>
+          )}
+
         </LoadingBox>
       )}
     </Form>
