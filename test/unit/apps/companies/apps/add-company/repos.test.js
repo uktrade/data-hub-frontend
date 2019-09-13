16ba8823e5b4d04207af8dd6472f8aab4ffe9e8f
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-09-13 14:51:20 +0100

    Add function to retrieve organisation types
    This is a subset of existing business types

--- test/unit/apps/companies/apps/add-company/repos.test.js
+++ test/unit/apps/companies/apps/add-company/repos.test.js
@@ -1,6 +1,7 @@
 const config = require('~/config')
-const { fetchForeignCountries } = require('~/src/apps/companies/apps/add-company/repos')
+const { fetchForeignCountries, fetchOrganisationTypes } = require('~/src/apps/companies/apps/add-company/repos')
 const countriesFixture = require('../../../../data/metadata/country')
+const businessTypeFixture = require('../../../../data/metadata/business-type')
 const { transformObjectToOption } = require('~/src/apps/transformers')
 
 const token = 'abcd'
@@ -41,6 +42,45 @@ describe('Add company form repos', () => {
       it('should return countries excluding "Italy"', () => {
         expect(this.results).to.be.deep.equal(foreignCountriesFixture)
       })
+    })
+  })
+
+  describe('#fetchOrganisationTypes', () => {
+    beforeEach(async () => {
+      nock(config.apiRoot)
+        .get('/metadata/business-type/')
+        .reply(200, businessTypeFixture)
+
+      this.actual = await fetchOrganisationTypes(token)
+    })
+
+    it('should return the organisation types', () => {
+      expect(this.actual).to.deep.equal([
+        {
+          value: '9dd14e94-5d95-e211-a939-e4115bead28a',
+          label: 'Charity',
+        },
+        {
+          value: '9cd14e94-5d95-e211-a939-e4115bead28a',
+          label: 'Government department or other public body',
+        },
+        {
+          value: '98d14e94-5d95-e211-a939-e4115bead28a',
+          label: 'Limited company',
+        },
+        {
+          value: '8b6eaf7e-03e7-e611-bca1-e4115bead28a',
+          label: 'Limited partnership',
+        },
+        {
+          value: '9ad14e94-5d95-e211-a939-e4115bead28a',
+          label: 'Partnership',
+        },
+        {
+          value: '99d14e94-5d95-e211-a939-e4115bead28a',
+          label: 'Sole Trader',
+        },
+      ])
     })
   })
 })
