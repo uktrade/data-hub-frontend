2d43dee6862fbdd393ce74d52a9cfcdcc0f0d1ca
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-09-13 14:50:26 +0100

    Refactor test to support more tests

--- test/unit/apps/companies/apps/add-company/repos.test.js
+++ test/unit/apps/companies/apps/add-company/repos.test.js
@@ -1,44 +1,46 @@
 const config = require('~/config')
-const repos = require('~/src/apps/companies/apps/add-company/repos')
+const { fetchForeignCountries } = require('~/src/apps/companies/apps/add-company/repos')
 const countriesFixture = require('../../../../data/metadata/country')
 const { transformObjectToOption } = require('~/src/apps/transformers')
 
 const token = 'abcd'
 
 describe('Add company form repos', () => {
-  describe('when fetchForeignCountries() is called with default "domesticCountry"', () => {
-    const foreignCountriesFixture = countriesFixture
-      .filter(c => c.name !== 'United Kingdom')
-      .map(transformObjectToOption)
+  describe('#fetchForeignCountries', () => {
+    context('when "domesticCountry" is not set', () => {
+      const foreignCountriesFixture = countriesFixture
+        .filter(c => c.name !== 'United Kingdom')
+        .map(transformObjectToOption)
 
-    beforeEach(async () => {
-      nock(config.apiRoot)
-        .get('/metadata/country/')
-        .reply(200, countriesFixture)
+      beforeEach(async () => {
+        nock(config.apiRoot)
+          .get('/metadata/country/')
+          .reply(200, countriesFixture)
 
-      this.results = await repos.fetchForeignCountries({ token })
+        this.results = await fetchForeignCountries({ token })
+      })
+
+      it('should return countries excluding "United Kingdom"', () => {
+        expect(this.results).to.be.deep.equal(foreignCountriesFixture)
+      })
     })
 
-    it('should return countries excluding "United Kingdom"', () => {
-      expect(this.results).to.be.deep.equal(foreignCountriesFixture)
-    })
-  })
+    context('when "domesticCountry" is Italy', () => {
+      const foreignCountriesFixture = countriesFixture
+        .filter(c => c.name !== 'Italy')
+        .map(transformObjectToOption)
 
-  describe('when fetchForeignCountries() is called with "domesticCountry" equal to "Italy"', () => {
-    const foreignCountriesFixture = countriesFixture
-      .filter(c => c.name !== 'Italy')
-      .map(transformObjectToOption)
+      beforeEach(async () => {
+        nock(config.apiRoot)
+          .get('/metadata/country/')
+          .reply(200, countriesFixture)
 
-    beforeEach(async () => {
-      nock(config.apiRoot)
-        .get('/metadata/country/')
-        .reply(200, countriesFixture)
+        this.results = await fetchForeignCountries({ token, domesticCountry: 'Italy' })
+      })
 
-      this.results = await repos.fetchForeignCountries({ token, domesticCountry: 'Italy' })
-    })
-
-    it('should return countries excluding "Italy"', () => {
-      expect(this.results).to.be.deep.equal(foreignCountriesFixture)
+      it('should return countries excluding "Italy"', () => {
+        expect(this.results).to.be.deep.equal(foreignCountriesFixture)
+      })
     })
   })
 })
