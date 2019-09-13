5a0088dc24f19389bdfd2f803ab8ca9ed3bfb772
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-09-13 14:51:20 +0100

    Add function to retrieve organisation types
    This is a subset of existing business types

--- src/apps/companies/apps/add-company/repos.js
+++ src/apps/companies/apps/add-company/repos.js
@@ -1,14 +1,43 @@
-const { filter } = require('lodash')
+const { filter, map, includes, sortBy } = require('lodash')
 
 const { getOptions } = require('../../../../lib/options')
 
 const METADATA_COUNTRY_UK_LABEL = 'United Kingdom'
+const COMPANY_LABEL = 'Company'
+const LIMITED_COMPANY_LABEL = 'Limited company'
+const BUSINESS_TYPE_WHITELIST = {
+  charity: '9dd14e94-5d95-e211-a939-e4115bead28a',
+  company: '98d14e94-5d95-e211-a939-e4115bead28a',
+  governmentDepartmentOrOtherPublicBody: '9cd14e94-5d95-e211-a939-e4115bead28a',
+  limitedPartnership: '8b6eaf7e-03e7-e611-bca1-e4115bead28a',
+  partnership: '9ad14e94-5d95-e211-a939-e4115bead28a',
+  soleTrader: '99d14e94-5d95-e211-a939-e4115bead28a',
+}
 
 async function fetchForeignCountries ({ token, domesticCountry = METADATA_COUNTRY_UK_LABEL }) {
   const countries = await getOptions(token, 'country')
   return filter(countries, ({ label }) => { return label !== domesticCountry })
 }
 
+async function fetchOrganisationTypes (token) {
+  const businessTypeOptions = await getOptions(token, 'business-type')
+  const filteredBusinessTypeOptions = filter(businessTypeOptions, ({ value }) => {
+    return includes(BUSINESS_TYPE_WHITELIST, value)
+  })
+
+  return sortBy(map(filteredBusinessTypeOptions, (businessTypeOption) => {
+    if (businessTypeOption.label === COMPANY_LABEL) {
+      return {
+        ...businessTypeOption,
+        label: LIMITED_COMPANY_LABEL,
+      }
+    }
+
+    return businessTypeOption
+  }), 'label')
+}
+
 module.exports = {
   fetchForeignCountries,
+  fetchOrganisationTypes,
 }
