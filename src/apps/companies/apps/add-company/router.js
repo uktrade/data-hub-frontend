8ef0003fc7878b8908f84bc610e3054ce74c216b
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-09-16 15:43:26 +0100

    Hook up "Add company" click to Express endpoint

--- src/apps/companies/apps/add-company/router.js
+++ src/apps/companies/apps/add-company/router.js
@@ -7,13 +7,9 @@ const {
   postAddDnbCompanyInvestigation,
 } = require('./controllers')
 
-router
-  .route('/')
-  .get(renderAddCompanyForm)
-  .post(postAddDnbCompany)
-
+router.get('/', renderAddCompanyForm)
+router.post('/dnb/company-create', postAddDnbCompany)
 router.post('/dnb/company-search', postSearchDnbCompanies)
-
 router.post('/dnb/company-investigation', postAddDnbCompanyInvestigation)
 
 module.exports = router
