0fa4c415b3e261d04a3f5475d6bdb7cac29c7e0a
Rafal Enden <git@enden.com>
2019-09-20 14:33:13 +0100

    fix: Require website or a telephone number on the new "Add company" form

--- test/functional/cypress/selectors/company-lists/create.js
+++ test/functional/cypress/selectors/company-lists/create.js
@@ -1,8 +1,8 @@
 module.exports = {
   label: 'form label',
   hint: 'form span',
-  errorLabel: 'form label span:nth-child(2)',
-  input: 'form label input',
+  error: 'form div > div',
+  input: 'form input',
   submit: 'form button',
   cancel: 'form a',
 }
