9a9198e47f7b765255cbf0ba9ca8df4049ccaff4
Pippo Raimondi <filipporai@gmail.com>
2019-10-15 17:40:13 +0100

    Migrate LEP acceptance tests to cypress specs

--- test/end-to-end/cypress/specs/LEP/collection-spec.js
+++ test/end-to-end/cypress/specs/LEP/collection-spec.js
@@ -1,15 +1,53 @@
 const selectors = require('../../../../selectors')
 
-describe('company Collections', () => {
-  before(() => {
-    cy.visit(`/companies`)
+describe('Collection', () => {
+  describe('company', () => {
+    before(() => {
+      cy.visit('/companies')
+    })
+
+    it('should return the results summary for a company collection', () => {
+      verifyCollection()
+    })
   })
 
-  it('should return the results summary for a company collection', () => {
-    cy.get(selectors.collection.headerCount).invoke('text').then((headerCount) => {
-      cy.get(selectors.collection.items).should((collectionItems) => {
-        expect(headerCount).to.eq(collectionItems.length.toString())
+  describe('contact', () => {
+    before(() => {
+      cy.visit('/contacts')
+    })
+
+    it('should return the results summary for a contact collection', () => {
+      verifyCollection()
+    })
+  })
+
+  context('investment', () => {
+    describe('projects', () => {
+      before(() => {
+        cy.visit('/investments/projects')
+      })
+
+      it('should return the results summary for a contact collection', () => {
+        verifyCollection()
+      })
+    })
+
+    describe('project', () => {
+      before(() => {
+        cy.visit('/investments/projects/ba1f0b14-5fe4-4c36-bf6a-ddf115272977/details')
+      })
+
+      it('should return the results summary for a contact collection', () => {
+        verifyCollection()
       })
     })
   })
 })
+
+const verifyCollection = () => {
+  cy.get(selectors.collection.headerCount).invoke('text').then((headerCount) => {
+    cy.get(selectors.collection.items).should((collectionItems) => {
+      expect(headerCount).to.eq(collectionItems.length.toString())
+    })
+  })
+}
