13b90ea459e3132173bde4b2ac431d48a1a6686a
Ian Leggett <contactleggett@gmail.com>
2019-10-15 10:30:08 +0100

    Add new page for user to add/remove company to/from lists

---
+++ src/apps/company-lists/client/AddRemoveFromListSection.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { get } from 'lodash'
+import { AddRemoveFromListForm } from 'data-hub-components'
+import ErrorSummary from '@govuk-react/error-summary'
+import PropTypes from 'prop-types'
+
+const AddRemoveFromListSection = (
+  {
+    list,
+    companyId,
+    token,
+    createNewListUrl,
+    cancelLinkUrl,
+  }) => {
+  const [errorMessage, setErrorMessage] = useState(null)
+  async function onSubmit (list) {
+    try {
+      await addRemoveFromList(token, companyId, list, cancelLinkUrl)
+      return cancelLinkUrl
+    } catch (error) {
+      if (get(error, 'response.status') === 404) {
+        setErrorMessage('Request failed with status code 404')
+      } else {
+        setErrorMessage(error.message || error.toString())
+      }
+    }
+  }
+
+  async function addRemoveFromList (token, companyId, list) {
+    await axios({
+      method: 'POST',
+      url: `/companies/${companyId}/lists/add-remove?_csrf=${token}`,
+      data: {
+        list,
+      },
+    })
+  }
+
+  return (
+    <div>
+      {errorMessage && (
+        <ErrorSummary
+          heading="There was an error adding/removing from lists"
+          description={errorMessage}
+          errors={[]}
+        />
+      )}
+      <AddRemoveFromListForm
+        list={list}
+        companyId={companyId}
+        onSubmitHandler={onSubmit}
+        createNewListUrl={createNewListUrl}
+        cancelLinkUrl={cancelLinkUrl}
+      />
+    </div>
+  )
+}
+
+AddRemoveFromListSection.propTypes = {
+  list: PropTypes.object.isRequired,
+  companyId: PropTypes.string.isRequired,
+  token: PropTypes.string.isRequired,
+  createNewListUrl: PropTypes.string.isRequired,
+  cancelLinkUrl: PropTypes.string.isRequired,
+}
+
+export default AddRemoveFromListSection
---
+++ src/apps/company-lists/controllers/add-remove.js
@@ -0,0 +1,61 @@
+const { getAllCompanyLists, getListsCompanyIsIn, addCompanyToList, removeCompanyFromList } = require('../repos')
+const { transformCompaniesInLists } = require('../transformers')
+
+async function handleAddRemoveCompanyToList (req, res, next) {
+  const { list } = req.body
+  const { id } = res.locals.company
+  const listsToUpdate = []
+  try {
+    for (let [listId, value] of Object.entries(list)) {
+      const addOrRemoveCompany = (value === 'yes' ? addCompanyToList : removeCompanyFromList)
+      listsToUpdate.push(addOrRemoveCompany(req.session.token, listId, id))
+    }
+    await Promise.all(listsToUpdate)
+    req.flash('success', 'Lists changes for this company have been saved.')
+    res.send()
+  } catch (error) {
+    req.flash('error', 'Could not add or remove to list')
+    next(error)
+  }
+}
+
+async function fetchListsCompanyIsOn (req, res, next) {
+  try {
+    const [allLists, allListsCompaniesIn] = await Promise.all([
+      getAllCompanyLists(req.session.token),
+      getListsCompanyIsIn(req.session.token, res.locals.company.id),
+    ])
+    res.locals.listsCompanyIsIn = await transformCompaniesInLists(allLists, allListsCompaniesIn)
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
+
+async function renderAddRemoveForm (req, res, next) {
+  const { company, listsCompanyIsIn, csrfToken } = res.locals
+  try {
+    res
+      .breadcrumb(company.name, `/companies/${company.id}`)
+      .breadcrumb('lists')
+      .breadcrumb('Add and remove from lists')
+      .render('company-lists/views/add-remove-list-container', {
+        heading: `Add and remove ${company.name} from lists`,
+        props: {
+          list: listsCompanyIsIn,
+          companyId: company.id,
+          token: csrfToken,
+          createNewListUrl: `/companies/${company.id}/lists/create`,
+          cancelLinkUrl: `/companies/${company.id}`,
+        },
+      })
+  } catch (error) {
+    next(error)
+  }
+}
+
+module.exports = {
+  handleAddRemoveCompanyToList,
+  renderAddRemoveForm,
+  fetchListsCompanyIsOn,
+}
