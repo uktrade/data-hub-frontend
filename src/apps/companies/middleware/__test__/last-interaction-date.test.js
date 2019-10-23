6d7a2c21875f04007b5491a7a4bdf287d62a609e
Graham Veal <grahamveal@gmail.com>
2019-10-23 10:23:45 +0100

    Add new Last Interaction between filter
    Add new middleware to handle new param
      We have one param in the FE as a date range
      This needs to get converted into two date params for the BE

---
+++ src/apps/companies/middleware/__test__/last-interaction-date.test.js
@@ -0,0 +1,111 @@
+const moment = require('moment')
+const middleware = require('../last-interaction-date')
+const buildMiddlewareParameters = require('../../../../../test/unit/helpers/middleware-parameters-builder')
+
+const QUERY_PARAM = 'interaction_between'
+const START_DATE_PARAM = 'last_interaction_date_before'
+const END_DATE_PARAM = 'last_interaction_date_after'
+
+function getTimestamp (offset) {
+  return moment().subtract(offset, 'month').utc().format('YYYY-MM-DD')
+}
+
+function callMiddleware (value) {
+  const { reqMock, resMock, nextSpy } = buildMiddlewareParameters({
+    requestBody: {
+      [QUERY_PARAM]: value,
+    },
+  })
+
+  middleware(reqMock, resMock, nextSpy)
+
+  return reqMock
+}
+
+function checkBody (reqMock, startValue, endValue) {
+  expect(reqMock.body[QUERY_PARAM]).to.be.undefined
+  expect(reqMock.body[START_DATE_PARAM]).to.equal(getTimestamp(startValue))
+  expect(reqMock.body[END_DATE_PARAM]).to.equal(getTimestamp(endValue))
+}
+
+describe('Last interaction date middleware', () => {
+  describe('np param', () => {
+    it('should delete the param from req.body', () => {
+      const { reqMock, resMock, nextSpy } = buildMiddlewareParameters({
+        requestBody: {},
+      })
+
+      middleware(reqMock, resMock, nextSpy)
+
+      expect(reqMock.body[QUERY_PARAM]).to.be.undefined
+      expect(reqMock.body[START_DATE_PARAM]).to.be.undefined
+      expect(reqMock.body[END_DATE_PARAM]).to.be.undefined
+    })
+  })
+
+  describe('an empty param', () => {
+    it('should delete the param from req.body', () => {
+      const reqMock = callMiddleware('')
+
+      expect(reqMock.body[QUERY_PARAM]).to.be.undefined
+      expect(reqMock.body[START_DATE_PARAM]).to.be.undefined
+      expect(reqMock.body[END_DATE_PARAM]).to.be.undefined
+    })
+  })
+
+  describe('a single value param', () => {
+    describe('with a value of 1', () => {
+      it('should add 3 and 6 months', () => {
+        const reqMock = callMiddleware('1')
+        checkBody(reqMock, 1, 3)
+      })
+    })
+
+    describe('with a value of 2', () => {
+      it('should add 3 and 6 months', () => {
+        const reqMock = callMiddleware('2')
+        checkBody(reqMock, 3, 6)
+      })
+    })
+
+    describe('with a value of 3', () => {
+      it('should add 6 and 12 months', () => {
+        const reqMock = callMiddleware('3')
+        checkBody(reqMock, 6, 12)
+      })
+    })
+
+    describe('with a value of 4', () => {
+      it('should add 12 and 24 months', () => {
+        const reqMock = callMiddleware('4')
+        checkBody(reqMock, 12, 24)
+      })
+    })
+
+    describe('with a value of 5', () => {
+      it('should not add any dates', () => {
+        const reqMock = callMiddleware('5')
+
+        expect(reqMock.body[QUERY_PARAM]).to.be.undefined
+        expect(reqMock.body[START_DATE_PARAM]).to.be.undefined
+        expect(reqMock.body[END_DATE_PARAM]).to.be.undefined
+      })
+    })
+  })
+
+  describe('a multi value param', () => {
+    describe('With two known values', () => {
+      it('Should add the correct values', () => {
+        const reqMock = callMiddleware(['1', '3'])
+        checkBody(reqMock, 1, 12)
+      })
+    })
+
+    describe('With one unknown value', () => {
+      it('Should add the known values', () => {
+        const reqMock = callMiddleware(['1', '5'])
+        checkBody(reqMock, 1, 3)
+      })
+    })
+  })
+})
---
+++ src/apps/companies/middleware/last-interaction-date.js
@@ -0,0 +1,47 @@
+const moment = require('moment')
+const { QUERY_FIELDS_MAP } = require('../constants')
+
+const QUERY_PARAM = QUERY_FIELDS_MAP.lastInteractionDate
+const START_DATE_PARAM = 'last_interaction_date_before'
+const END_DATE_PARAM = 'last_interaction_date_after'
+const DATE_FORMAT = 'YYYY-MM-DD'
+
+function getTimestamp (offset) {
+  return moment().subtract(offset, 'month').utc().format(DATE_FORMAT)
+}
+
+module.exports = (req, res, next) => {
+  const value = req.body[QUERY_PARAM]
+
+  if (value) {
+    const range = [].concat(value)
+    const values = []
+    const l = range.length
+
+    for (let i = 0; i < l; i++) {
+      switch (range[i]) {
+        case '1':
+          values.push(1, 3)
+          break
+        case '2':
+          values.push(3, 6)
+          break
+        case '3':
+          values.push(6, 12)
+          break
+        case '4':
+          values.push(12, 24)
+          break
+      }
+    }
+
+    if (values.length) {
+      req.body[START_DATE_PARAM] = getTimestamp(Math.min(...values))
+      req.body[END_DATE_PARAM] = getTimestamp(Math.max(...values))
+    }
+  }
+
+  delete req.body[QUERY_PARAM]
+
+  next()
+}
