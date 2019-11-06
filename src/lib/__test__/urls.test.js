65294dd6ce1febd1e3e1b7cb544c7d6339495858
Graham Veal <grahamveal@gmail.com>
2019-11-06 07:11:12 +0000

    Add urls to nunjucks globals for use in templates
    So the all templates and macros have access to the urls module
    This means hard coded urls can be removed from templates as well as node code
    
    Also move the test file to the correct location
    
    Use the urls module for search to prove it works as expected and have a usage example

--- src/lib/__test__/urls.test.js
+++ src/lib/__test__/urls.test.js
@@ -1,13 +1,13 @@
 const faker = require('faker')
 const proxyquire = require('proxyquire')
 
-const modulePath = '../../../src/lib/urls'
+const modulePath = '../urls'
 
 describe('urls', () => {
   let urls
   before(() => {
     urls = proxyquire(modulePath, {
-      '../../config': {
+      '../config': {
         greatProfileUrl: 'http://a.b.c.com/path/{id}',
       },
     })
@@ -55,6 +55,18 @@ describe('urls', () => {
       expect(urls.contacts.index.mountPoint).to.equal('/contacts')
       expect(urls.contacts.index.route).to.equal('/')
       expect(urls.contacts.index()).to.equal('/contacts')
+    })
+  })
+
+  describe('search', () => {
+    it('should return the correct values ', () => {
+      expect(urls.search.index.mountPoint).to.equal('/search')
+      expect(urls.search.index.route).to.equal('/')
+      expect(urls.search.index()).to.equal('/search')
+
+      const type = faker.lorem.word()
+      expect(urls.search.type.route).to.equal('/:searchPath?')
+      expect(urls.search.type(type)).to.equal(`/search/${type}`)
     })
   })
 })
