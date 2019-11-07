22582010e8fdbedee97807cb0109f5ce9a3b30e4
Rafal Enden <git@enden.com>
2019-11-07 13:11:45 +0000

    Remove the old company edit form

--- src/apps/companies/transformers/index.js
+++ src/apps/companies/transformers/index.js
@@ -1,27 +1,19 @@
-const transformCompanyFormToApi = require('./company-form-to-api')
 const transformCompanyToBusinessHierarchyView = require('./company-to-business-hierarchy-view')
 const transformCompanyToExportDetailsView = require('./company-to-export-details-view')
-const transformCompanyToForm = require('./company-to-form')
 const transformCompanyToAboutView = require('./company-to-about-view')
 const transformCompanyToListItem = require('./company-to-list-item')
 const transformCompanyToOneListView = require('./company-to-one-list-view')
 const transformCompanyToRegionView = require('./company-to-region-view')
-const transformCompanyToSectorView = require('./company-to-sector-view')
-const transformCompanyToView = require('./company-to-view')
 const transformCompanyToSubsidiaryListItem = require('./company-to-subsidiary-list-item')
 const transformCoreTeamToCollection = require('./one-list-core-team-to-collection')
 
 module.exports = {
-  transformCompanyFormToApi,
   transformCompanyToBusinessHierarchyView,
   transformCompanyToExportDetailsView,
-  transformCompanyToForm,
   transformCompanyToAboutView,
   transformCompanyToListItem,
   transformCompanyToOneListView,
   transformCompanyToRegionView,
-  transformCompanyToSectorView,
   transformCompanyToSubsidiaryListItem,
-  transformCompanyToView,
   transformCoreTeamToCollection,
 }
