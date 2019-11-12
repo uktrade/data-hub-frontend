814e54cc14e97995dbdc399bb8ff43127312ae04
Pippo Raimondi <filipporai@gmail.com>
2019-11-12 17:20:30 +0000

    Make test more robust

--- test/end-to-end/cypress/specs/DIT/audit-spec.js
+++ test/end-to-end/cypress/specs/DIT/audit-spec.js
@@ -9,6 +9,8 @@ describe('company', () => {
 
   it('should display name of the person who made company record changes', () => {
     cy.get(selectors.companyAudit.save).click()
+    cy.get(selectors.message.successful).should('be.visible')
+
     cy.visit('/companies/0f5216e0-849f-11e6-ae22-56b6b6499611/audit')
 
     cy.get(selectors.collection.items)
@@ -25,6 +27,8 @@ describe('contact', () => {
 
   it('should display name of the person who made contact record changes', () => {
     cy.get(selectors.contactCreate.save).click()
+    cy.get(selectors.message.successful).should('be.visible')
+
     cy.visit('/contacts/9b1138ab-ec7b-497f-b8c3-27fed21694ef/audit')
 
     cy.get(selectors.collection.items)
