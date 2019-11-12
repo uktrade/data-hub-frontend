44609ec4da598940bd0225f9785f4bdd0d376d24
Peter Hudec <peterhudec@peterhudec.com>
2019-11-12 12:24:11 +0000

    Fixed the failing test on the missing add advisers route

--- src/apps/companies/__test__/router.test.js
+++ src/apps/companies/__test__/router.test.js
@@ -5,6 +5,7 @@ describe('Company router', () => {
     const paths = router.stack.filter(r => r.route).map(r => r.route.path)
     expect(paths).to.deep.equal([ '/',
       '/export',
+      '/:companyId/advisers/add',
       '/:companyId/exports/edit',
       '/:companyId/archive',
       '/:companyId/unarchive',
