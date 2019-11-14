394f1cc3d871f5b6792ad7c9a80d1fcd9ab29838
Peter Hudec <peterhudec@peterhudec.com>
2019-11-14 16:09:07 +0000

    Fixed typos and the cancel url

--- src/apps/companies/client/AddAdviser.jsx
+++ src/apps/companies/client/AddAdviser.jsx
@@ -13,19 +13,19 @@ const StyledForm = styled('form')({
 const AddAdviser = ({ csrfToken }) =>
   <div>
     <H3>Do you want to add yourself as the first point of contact?</H3>
-    <Details summary="How do I add someone else as the lead ITA?">
+    <Details summary="How do I add someone else as the Lead ITA?">
       You can only add yourself as the Lead ITA.
-      If you think another Internal Trade Adviser is the first point of contact
+      If you think another International Trade Adviser is the first point of contact
       for this company, they will need to add themselves.
     </Details>
     <H3>What happens next?</H3>
-    <UnorderedList>
+    <UnorderedList listStyleType="bullet">
       <ListItem>
         Your name and team will be displayed on top of the company page,
         as well as in the Lead Adviser tab
       </ListItem>
       <ListItem>
-        This will also replace Lead ITSs set on any subsidiaries of this company
+        This will also replace Lead ITAs set on any subsidiaries of this company
       </ListItem>
       <ListItem>
         Other ITAs will be able to replace you as the Lead ITA for the company
@@ -34,7 +34,7 @@ const AddAdviser = ({ csrfToken }) =>
     <StyledForm method="POST">
       <input type="hidden" name="_csrf" value={csrfToken}/>
       <Button>Add myself as Lead ITA</Button>
-      <Link href="..">Cancel</Link>
+      <Link href="../advisers">Cancel</Link>
     </StyledForm>
   </div>
 
