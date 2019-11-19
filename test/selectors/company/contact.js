87f9a981547d4a4d73b77985a8bbc0d4957bb6f0
Pippo Raimondi <filipporai@gmail.com>
2019-11-19 15:13:51 +0000

    Migrate companies contact feature to cypress

---
+++ test/selectors/company/contact.js
@@ -0,0 +1,12 @@
+module.exports = {
+  add: '[data-auto-id="Add contact"]',
+  name: '#field-first_name',
+  lastName: '#field-last_name',
+  jobTitle: '#field-job_title',
+  primaryContactYes: '[for="field-primary-1"]',
+  countryCode: '#field-telephone_countrycode',
+  phone: '#field-telephone_number',
+  email: '#field-email',
+  sameCompanyAddressYes: '[for="field-address_same_as_company-1"]',
+  save: 'button.govuk-button.js-prevent-multiple-submits',
+}
