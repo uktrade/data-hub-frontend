cab22ff1520917045fb1893913af0ad1056918c8
Pippo Raimondi <filipporai@gmail.com>
2019-11-19 15:13:51 +0000

    Migrate companies contact feature to cypress

--- test/end-to-end/cypress/specs/DIT/companies-spec.js
+++ test/end-to-end/cypress/specs/DIT/companies-spec.js
@@ -1,5 +1,7 @@
 const selectors = require('../../../../selectors')
 
+const { createContact } = require('../../support/user-actions')
+
 describe('Advisors', () => {
   const globalManagerTable = 2
   const AdviserTable = 3
@@ -22,5 +24,28 @@ describe('Advisors', () => {
       .and('contain', 'IG - Specialists - Knowledge Intensive Industry')
       .and('contain', 'London')
       .and('contain', 'Jenny Carey')
+  })
+})
+
+describe('Contacts', () => {
+  const data = {
+    name: 'Company',
+    lastName: 'Contact',
+    jobTitle: 'Coffee machine operator',
+    countryCode: '44',
+    phone: '0778877778800',
+    email: 'company.contact@dit.com',
+  }
+
+  it('should list a newly created contact in collection page', () => {
+    cy.visit('/contacts/create?company=0fb3379c-341c-4da4-b825-bf8d47b26baa')
+    createContact(data)
+
+    cy.visit('/companies/0fb3379c-341c-4da4-b825-bf8d47b26baa/contacts')
+    cy.get(selectors.collection.items)
+      .should('contain', 'Company Contact')
+      .and('contain', 'Coffee machine operator')
+      .and('contain', '(44) 0778877778800')
+      .and('contain', 'company.contact@dit.com')
   })
 })
