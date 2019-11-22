16d3fffea566cc83bff89e3262203b7da5c5b5f8
Rafal Enden <git@enden.com>
2019-11-22 19:36:11 +0000

    fixup! Add support for testing JSX files using Mocha

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,7 @@ module.exports = {
   'plugins': [
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-class-properties',
+    'istanbul',
   ],
   'presets': [
     ['@babel/preset-react'],
