8be78d9fb703ffaa6a964dc98dde5e2ec5e4d979
Rafal Enden <git@enden.com>
2019-11-25 16:04:50 +0000

    Add "Strict-Transport-Security" and "X-XSS-Protection" headers to increase security

---
+++ src/middleware/__test__/headers.test.js
@@ -0,0 +1,56 @@
+const headers = require('../headers')
+
+describe('headers middleware', () => {
+  context('when the resource is not an asset', () => {
+    it('should set headers', () => {
+      const reqMock = { url: '/' }
+      const resMock = { set: sinon.spy() }
+      const nextMock = sinon.spy()
+
+      headers(reqMock, resMock, nextMock)
+
+      expect(resMock.set.args).to.be.deep.equal([
+        [
+          'Cache-Control',
+          'no-cache, no-store, must-revalidate, private',
+        ],
+        [
+          'Pragma',
+          'no-cache',
+        ],
+        [
+          'X-Frame-Options',
+          'DENY',
+        ],
+        [
+          'X-Content-Type-Options',
+          'nosniff',
+        ],
+        [
+          'X-XSS-Protection',
+          '1; mode=block',
+        ],
+        [
+          'Strict-Transport-Security',
+          'max-age=15552000',
+        ],
+      ])
+
+      expect(nextMock).to.be.called
+    })
+  })
+
+  context('when the resource is an asset', () => {
+    it('should not set the headers', () => {
+      const reqMock = { url: '/javascripts/foo.js' }
+      const resMock = { set: sinon.spy() }
+      const nextMock = sinon.spy()
+
+      headers(reqMock, resMock, nextMock)
+
+      expect(resMock.set).not.to.be.called
+
+      expect(nextMock).to.be.called
+    })
+  })
+})
