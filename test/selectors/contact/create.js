e8382dfd6259b8091a4840cf6b0cdbed974ef9cc
Pippo Raimondi <filipporai@gmail.com>
2019-11-28 16:15:24 +0000

    Migrate contact save feature to cypress

--- test/selectors/contact/create.js
+++ test/selectors/contact/create.js
@@ -1,8 +1,22 @@
 const msgCls = '.c-form-group__error-message'
 
 module.exports = {
-  addContact: '.c-form-actions .govuk-button',
+  add: '[data-auto-id="Add contact"]',
+  name: '#field-first_name',
+  lastName: '#field-last_name',
+  jobTitle: '#field-job_title',
+  primaryContactYes: '[for="field-primary-1"]',
+  countryCode: '#field-telephone_countrycode',
+  phone: '#field-telephone_number',
+  email: '#field-email',
+  sameCompanyAddressYes: '[for="field-address_same_as_company-1"]',
+  sameCompanyAddressNo: '[for="field-address_same_as_company-2"]',
+  postCodeLookupAddress1: '#field-address_1',
+  postCodeLookupAddress2: '#field-address_2',
+  postCodeLookupTown: '#field-address_town',
+  postCodeLookupCountry: '#field-address_country',
   save: 'div.c-form-actions > button.govuk-button',
+  details: '.c-details-container__content',
   validationHeader: '.c-error-summary__heading',
   validationFieldMsg: {
     name: `[for="field-first_name"] ${msgCls}`,
