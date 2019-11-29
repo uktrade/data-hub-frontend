ce097e8fecbcb81b84bf5818598213ea140e2d4a
Paul Gain <paul@javexconsulting.com>
2019-11-29 15:35:16 +0000

    Fix for when a user edits a Client Relationship Manager

--- src/apps/adviser/filters.js
+++ src/apps/adviser/filters.js
@@ -1,12 +1,17 @@
-const { get, includes } = require('lodash')
+const {
+  get,
+  includes,
+  castArray,
+  compact,
+} = require('lodash')
 
-function filterActiveAdvisers ({ advisers = [], includeAdviser = [] }) {
-  includeAdviser = [...includeAdviser]
+function filterActiveAdvisers ({ advisers = [], includeAdviser }) {
+  let adviserIds = compact(castArray(includeAdviser))
   return advisers.filter(adviser => {
     // This returns all active users plus the include advisor (whether they are active or not)
     return (
       get(adviser, 'is_active', true) ||
-      includes(includeAdviser, adviser.id)
+      includes(adviserIds, adviser.id)
     )
   })
 }
