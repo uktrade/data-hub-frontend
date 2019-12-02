a2986c0e06da2049fbfd6651ba00e943f7a6f7f4
Rafal Enden <git@enden.com>
2019-12-02 13:48:54 +0000

    Remove the old company business details page

--- src/apps/companies/apps/business-details/__test__/router.test.js
+++ src/apps/companies/apps/business-details/__test__/router.test.js
@@ -4,7 +4,7 @@ describe('Business details routes', () => {
   it('should define all routes', () => {
     const paths = router.stack.filter(r => r.route).map(r => r.route.path)
     expect(paths).to.deep.equal([
-      '/:companyId/business-details2',
+      '/:companyId/business-details',
     ])
   })
 })
--- src/apps/companies/apps/business-details/router.js
+++ src/apps/companies/apps/business-details/router.js
@@ -3,6 +3,6 @@ const router = require('express').Router()
 const urls = require('../../../../lib/urls')
 const { renderBusinessDetails } = require('./controllers')
 
-router.get(urls.companies.businessDetails2.route, renderBusinessDetails)
+router.get(urls.companies.businessDetails.route, renderBusinessDetails)
 
 module.exports = router
