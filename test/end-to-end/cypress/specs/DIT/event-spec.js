b34263b369844dc5fa4b7c191bbc7b6a320b4a24
Pippo Raimondi <filipporai@gmail.com>
2019-12-03 11:09:37 +0000

    Ensure attendee test has enough time for es to index newly created event

--- test/end-to-end/cypress/specs/DIT/event-spec.js
+++ test/end-to-end/cypress/specs/DIT/event-spec.js
@@ -16,6 +16,37 @@ const assertKeyValueTable = (dataAutoId, expected) => {
   })
 }
 
+const createEvent = () => {
+  cy.get(selectors.eventCreate.eventName).type('Eventful event')
+  cy.get(selectors.eventCreate.eventType).select('Account management')
+  cy.get(selectors.eventCreate.startDateDay).type(today.format('DD'))
+  cy.get(selectors.eventCreate.startDateMonth).type(today.format('MM'))
+  cy.get(selectors.eventCreate.startDateYear).type(today.format('YYYY'))
+  cy.get(selectors.eventCreate.endDateDay).type(today.format('DD'))
+  cy.get(selectors.eventCreate.endDateMonth).type(today.format('MM'))
+  cy.get(selectors.eventCreate.endDateYear).type(today.format('YYYY'))
+  cy.get(selectors.eventCreate.addressLine1).type('Address1')
+  cy.get(selectors.eventCreate.addressLine2).type('Address2')
+  cy.get(selectors.eventCreate.addressTown).type('Campinas')
+  cy.get(selectors.eventCreate.addressCountry).select('Brazil')
+  cy.get(selectors.eventCreate.service).select('Account Management : Northern Powerhouse')
+  cy.get(selectors.eventCreate.organiser).click()
+  cy.get(selectors.eventCreate.organiserInput)
+    .type('Barry')
+  cy.get(selectors.eventCreate.organiserOption)
+    .should('be.visible')
+  cy.get(selectors.eventCreate.organiserInput)
+    .type('{enter}')
+  cy.get(selectors.eventCreate.sharedYes).click()
+  cy.get(selectors.eventCreate.relatedProgrammes).eq(0).select('CEN Energy')
+  cy.get(selectors.eventCreate.addAnotherProgramme).click()
+  cy.get(selectors.eventCreate.relatedProgrammes).eq(1).select('CEN Services')
+
+  cy.get(selectors.eventCreate.saveEvent).click()
+
+  cy.get(selectors.message.successful).should('contain', 'Event created')
+}
+
 const today = Cypress.moment()
 
 describe('Event', () => {
@@ -39,41 +70,13 @@ describe('Event', () => {
     })
 
     it('should create an event successfully', () => {
-      cy.get(selectors.eventCreate.eventName).type('Eventful event')
-      cy.get(selectors.eventCreate.eventType).select('Account management')
-      cy.get(selectors.eventCreate.startDateDay).type(today.format('DD'))
-      cy.get(selectors.eventCreate.startDateMonth).type(today.format('MM'))
-      cy.get(selectors.eventCreate.startDateYear).type(today.format('YYYY'))
-      cy.get(selectors.eventCreate.endDateDay).type(today.format('DD'))
-      cy.get(selectors.eventCreate.endDateMonth).type(today.format('MM'))
-      cy.get(selectors.eventCreate.endDateYear).type(today.format('YYYY'))
-      cy.get(selectors.eventCreate.addressLine1).type('Address1')
-      cy.get(selectors.eventCreate.addressLine2).type('Address2')
-      cy.get(selectors.eventCreate.addressTown).type('Campinas')
-      cy.get(selectors.eventCreate.addressCountry).select('Brazil')
-      cy.get(selectors.eventCreate.service).select('Account Management : Northern Powerhouse')
-      cy.get(selectors.eventCreate.organiser).click()
-      cy.get(selectors.eventCreate.organiserInput)
-        .type('Barry')
-      cy.get(selectors.eventCreate.organiserOption)
-        .should('be.visible')
-      cy.get(selectors.eventCreate.organiserInput)
-        .type('{enter}')
-      cy.get(selectors.eventCreate.sharedYes).click()
-      cy.get(selectors.eventCreate.relatedProgrammes).eq(0).select('CEN Energy')
-      cy.get(selectors.eventCreate.addAnotherProgramme).click()
-      cy.get(selectors.eventCreate.relatedProgrammes).eq(1).select('CEN Services')
-
-      cy.get(selectors.eventCreate.saveEvent).click()
-
-      cy.get(selectors.message.successful).should('contain', 'Event created')
+      createEvent()
     })
-  })
 
-  describe('attendee', () => {
-    it('should create an attendee on the newly created event', () => {
+    it('should create an attendee on a event', () => {
+      createEvent()
+
       cy.visit('/events')
-
       cy.contains('Eventful event').click()
       cy.contains('Attendees').click()
       cy.get(selectors.entityCollection.addAttendee).click()
