7de28891eeca60416bbf084eb274a76a660c5fc1
Mark Henwood <mcbhenwood@icloud.com>
2019-12-05 09:39:15 +0000

    Improve name of controller method

--- src/apps/healthcheck/__test__/controllers.test.js
+++ src/apps/healthcheck/__test__/controllers.test.js
@@ -1,7 +1,7 @@
 const proxyquire = require('proxyquire')
 
 describe('Health check controller', () => {
-  describe('#getHandler with healthy service dependencies', () => {
+  describe('#getMicroserviceHealthcheck with healthy service dependencies', () => {
     beforeEach(() => {
       this.serviceDependencies = [
         {
@@ -27,34 +27,34 @@ describe('Health check controller', () => {
     })
 
     it('should set cache control', async () => {
-      await this.controller.getHandler(this.req, this.res, this.next)
+      await this.controller.getMicroserviceHealthcheck(this.req, this.res, this.next)
 
       expect(this.res.set).to.be.calledWith('Cache-Control', 'no-cache, no-store, must-revalidate')
       expect(this.res.set).to.have.been.calledOnce
     })
 
     it('should return a 200 status code', async () => {
-      await this.controller.getHandler(this.req, this.res, this.next)
+      await this.controller.getMicroserviceHealthcheck(this.req, this.res, this.next)
 
       expect(this.res.status).to.be.calledWith(200)
       expect(this.res.status).to.have.been.calledOnce
     })
 
     it('should return OK', async () => {
-      await this.controller.getHandler(this.req, this.res, this.next)
+      await this.controller.getMicroserviceHealthcheck(this.req, this.res, this.next)
 
       expect(this.res.status().send).to.be.calledWith('OK')
       expect(this.res.status().send).to.have.been.calledOnce
     })
 
     it('should not call the logger', async () => {
-      await this.controller.getHandler(this.req, this.res, this.next)
+      await this.controller.getMicroserviceHealthcheck(this.req, this.res, this.next)
 
       expect(this.logger().error.notCalled).to.be.true
     })
   })
 
-  describe('#getHandler with unhealthy service dependencies', () => {
+  describe('#getMicroserviceHealthcheck with unhealthy service dependencies', () => {
     beforeEach(() => {
       this.serviceDependencyError = { name: 'example service dependency', error: Error('example error') }
       this.serviceDependencies = [
@@ -81,28 +81,28 @@ describe('Health check controller', () => {
     })
 
     it('should set cache control', async () => {
-      await this.controller.getHandler(this.req, this.res, this.next)
+      await this.controller.getMicroserviceHealthcheck(this.req, this.res, this.next)
 
       expect(this.res.set).to.be.calledWith('Cache-Control', 'no-cache, no-store, must-revalidate')
       expect(this.res.set).to.have.been.calledOnce
     })
 
     it('should return a 503 status code', async () => {
-      await this.controller.getHandler(this.req, this.res, this.next)
+      await this.controller.getMicroserviceHealthcheck(this.req, this.res, this.next)
 
       expect(this.res.status).to.be.calledWith(503)
       expect(this.res.status).to.have.been.calledOnce
     })
 
     it('should return Service Unavailable', async () => {
-      await this.controller.getHandler(this.req, this.res, this.next)
+      await this.controller.getMicroserviceHealthcheck(this.req, this.res, this.next)
 
       expect(this.res.status().send).to.be.calledWith('Service Unavailable')
       expect(this.res.status().send).to.have.been.calledOnce
     })
 
     it('should call the logger', async () => {
-      await this.controller.getHandler(this.req, this.res, this.next)
+      await this.controller.getMicroserviceHealthcheck(this.req, this.res, this.next)
 
       expect(this.logger.error).to.have.been.calledOnce
       expect(this.logger.error).to.be.calledWith(
