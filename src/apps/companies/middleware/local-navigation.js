b1233e5001ef83849aad9b95863f22bb90b5dfe4
Ian Leggett <contactleggett@gmail.com>
2019-12-06 10:08:46 +0000

    Remove the lead adviser feature flag

--- src/apps/companies/middleware/local-navigation.js
+++ src/apps/companies/middleware/local-navigation.js
@@ -4,23 +4,14 @@ const { isItaTierDAccount } = require('../../../lib/is-tier-type-company')
 const { LOCAL_NAV } = require('../constants')
 
 function setCompaniesLocalNav (req, res, next) {
-  const { company, features } = res.locals
-  if (features.lead_advisers) {
-    const isCoreTeamCompany = !isItaTierDAccount(company) && company.one_list_group_tier
-    const navItems = LOCAL_NAV.map((item) =>
-      item.path === 'advisers' && isCoreTeamCompany
-        ? { ...item, label: 'Core team' }
-        : item
-    )
-    setLocalNav(navItems)(req, res, next)
-  } else {
-    const navItems = LOCAL_NAV.map((item) =>
-      item.path === 'advisers'
-        ? { ...item, label: 'Core team' }
-        : item
-    ).filter((item) => item.path !== 'advisers' || company.one_list_group_tier)
-    setLocalNav(navItems)(req, res, next)
-  }
+  const { company } = res.locals
+  const isCoreTeamCompany = !isItaTierDAccount(company) && company.one_list_group_tier
+  const navItems = LOCAL_NAV.map((item) =>
+    item.path === 'advisers' && isCoreTeamCompany
+      ? { ...item, label: 'Core team' }
+      : item
+  )
+  setLocalNav(navItems)(req, res, next)
 }
 
 module.exports = setCompaniesLocalNav
