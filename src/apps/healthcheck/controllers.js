13daaab605b170b9ff14909cdf201a717868b3b9
Mark Henwood <mcbhenwood@icloud.com>
2019-12-09 11:27:30 +0000

    Reduce duplication of controller logic

--- src/apps/healthcheck/controllers.js
+++ src/apps/healthcheck/controllers.js
@@ -1,5 +1,7 @@
 const logger = require('../../config/logger')
 const serviceDependencies = require('./serviceDependencies')
+const failureDependencies = serviceDependencies.filter((dependency) => (!dependency.warningOnly))
+const warningDependencies = serviceDependencies.filter((dependency) => (dependency.warningOnly))
 
 function pingdomTemplate (statusMessage) {
   return `
@@ -10,9 +12,8 @@ function pingdomTemplate (statusMessage) {
   `.trim()
 }
 
-function healthCheckFailuresOnly (allDependencies) {
-  const failureDependencies = allDependencies.filter((dependency) => (!dependency.warningOnly))
-  const promiseArray = failureDependencies.map((dependency) => {
+function healthCheck (dependencies) {
+  const promiseArray = dependencies.map((dependency) => {
     return dependency.healthCheck()
       .then((result) => result)
       .catch((error) => {
@@ -23,73 +24,40 @@ function healthCheckFailuresOnly (allDependencies) {
   return Promise.all(promiseArray)
 }
 
-function healthCheckWarningsOnly (allDependencies) {
-  const failureDependencies = allDependencies.filter((dependency) => (dependency.warningOnly))
-  const promiseArray = failureDependencies.map((dependency) => {
-    return dependency.healthCheck()
-      .then((result) => result)
-      .catch((error) => {
-        return { name: dependency.name, error }
+function renderPingdomXml (req, res, next, dependencies) {
+  return healthCheck(dependencies)
+    .then((results) => {
+      return results.filter((result) => result.statusText !== 'OK')
+    })
+    .then((errors) => {
+      res.set({
+        'Content-Type': 'text/xml',
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
       })
-  })
 
-  return Promise.all(promiseArray)
+      if (errors.length) {
+        errors.forEach((dependency) => {
+          logger.error(`${dependency.name} health check failed`, dependency.error)
+        })
+
+        return res
+          .status(503)
+          .send(pingdomTemplate('Service Unavailable'))
+      }
+
+      return res
+        .status(200)
+        .send(pingdomTemplate('OK'))
+    })
+    .catch(next)
 }
 
 function getPingdomFailures (req, res, next) {
-  return healthCheckFailuresOnly(serviceDependencies)
-    .then((results) => {
-      return results.filter((result) => result.statusText !== 'OK')
-    })
-    .then((errors) => {
-      res.set({
-        'Content-Type': 'text/xml',
-        'Cache-Control': 'no-cache, no-store, must-revalidate',
-      })
-
-      if (errors.length) {
-        errors.forEach((dependency) => {
-          logger.error(`${dependency.name} health check failed`, dependency.error)
-        })
-
-        return res
-          .status(503)
-          .send(pingdomTemplate('Service Unavailable'))
-      }
-
-      return res
-        .status(200)
-        .send(pingdomTemplate('OK'))
-    })
-    .catch(next)
+  return renderPingdomXml(req, res, next, failureDependencies)
 }
 
 function getPingdomWarnings (req, res, next) {
-  return healthCheckWarningsOnly(serviceDependencies)
-    .then((results) => {
-      return results.filter((result) => result.statusText !== 'OK')
-    })
-    .then((errors) => {
-      res.set({
-        'Content-Type': 'text/xml',
-        'Cache-Control': 'no-cache, no-store, must-revalidate',
-      })
-
-      if (errors.length) {
-        errors.forEach((dependency) => {
-          logger.error(`${dependency.name} health check failed`, dependency.error)
-        })
-
-        return res
-          .status(503)
-          .send(pingdomTemplate('Service Unavailable'))
-      }
-
-      return res
-        .status(200)
-        .send(pingdomTemplate('OK'))
-    })
-    .catch(next)
+  return renderPingdomXml(req, res, next, warningDependencies)
 }
 
 function getMicroserviceHealthcheck (req, res, next) {
