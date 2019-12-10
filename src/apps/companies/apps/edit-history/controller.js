c760d8121c820bbd22d7f4649156a4796211fff3
Paul Gain <paul@javexconsulting.com>
2019-12-10 22:21:49 +0000

    Add Edit History API call and transformers

--- src/apps/companies/apps/edit-history/controller.js
+++ src/apps/companies/apps/edit-history/controller.js
@@ -1,15 +1,23 @@
 const { companies } = require('../../../../lib/urls')
+const { getCompanyAuditLog } = require('../../repos')
+const { transformCompanyAuditLog } = require('./transformers')
 
 async function renderEditHistory (req, res, next) {
   try {
     const { company } = res.locals
+    const { token } = req.session
+
+    const companyAuditLog = await getCompanyAuditLog(token, company.id)
+    const editHistory = transformCompanyAuditLog(companyAuditLog.results)
 
     res
       .breadcrumb(company.name, companies.detail(company.id))
       .breadcrumb('Business details', companies.businessDetails(company.id))
       .breadcrumb('Edit History')
       .render('companies/apps/edit-history/views/client-container', {
-        props: {},
+        props: {
+          editHistory,
+        },
       })
   } catch (error) {
     next(error)
