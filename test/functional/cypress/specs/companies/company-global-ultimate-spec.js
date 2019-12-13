368a649e2ad344412e15bb43e6e4101f01dc216d
Rafal Enden <git@enden.com>
2019-12-13 09:24:23 +0000

    Clarify “Ultimate HQ” label on the company business details page

--- test/functional/cypress/specs/companies/company-global-ultimate-spec.js
+++ test/functional/cypress/specs/companies/company-global-ultimate-spec.js
@@ -12,6 +12,12 @@ describe('Company Global Ultimate HQ', () => {
       cy.get(selectors.localHeader().badge(1)).should('be.visible')
     })
 
+    it('should display an "What does this mean?" details', () => {
+      cy.get(selectors.localHeader().metaList)
+        .should('contain', 'What does this mean?')
+        .should('contain', 'This HQ is in control of all related company records for DnB Global Ultimate')
+    })
+
     it('should display a single subsidiary', () => {
       const expected = 'Data Hub contains 1 other company record related to this company'
       cy.get(selectors.localHeader().description.paragraph(1)).should('have.text', expected)
