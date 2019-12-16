3c1f68b4ab3279a958bf4797967ee8da8b2b5d5e
Rafal Enden <git@enden.com>
2019-12-16 11:08:49 +0000

    Remove the current company from the D&B Hierarchy page
    It's done to make the number of related companies consistent with the business details page.

---
+++ test/sandbox/fixtures/v4/company/company-dnb-global-ultimate-subsidaries.js
@@ -0,0 +1,10 @@
+module.exports = {
+  'count': 3,
+  'next': null,
+  'previous': null,
+  'results': [
+    require('./company-dnb-global-ultimate.json'),
+    require('./company-dnb-subsidiary.json'),
+    require('./company-dnb-ltd.json'),
+  ],
+}
