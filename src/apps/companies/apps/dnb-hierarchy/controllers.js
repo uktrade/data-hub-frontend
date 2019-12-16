314377c392eb8dc5ba6bb7f92db3c7d9dee562a5
Rafal Enden <git@enden.com>
2019-12-16 11:08:49 +0000

    Remove the current company from the D&B Hierarchy page
    It's done to make the number of related companies consistent with the business details page.

--- src/apps/companies/apps/dnb-hierarchy/controllers.js
+++ src/apps/companies/apps/dnb-hierarchy/controllers.js
@@ -32,17 +32,32 @@ async function renderDnbHierarchy (req, res, next) {
   }
 }
 
+function removeCurrentCompany (dunsNumber, { count, results }) {
+  if (results.find(c => c.duns_number !== dunsNumber)) {
+    return {
+      count: count > 0 ? count - 1 : 0,
+      results: results.filter(c => c.duns_number !== dunsNumber),
+    }
+  }
+
+  // istanbul ignore next: Covered by functional tests
+  return { count, results }
+}
+
 async function fetchDnbHierarchyHandler (req, res, next) {
   try {
     const { company } = res.locals
     const { token } = req.session
     const { page } = req.query
 
-    const { count, results } = await getDnbHierarchy(
-      token,
-      company.global_ultimate_duns_number,
-      config.paginationDefaultSize,
-      page
+    const { count, results } = removeCurrentCompany(
+      company.duns_number,
+      await getDnbHierarchy(
+        token,
+        company.global_ultimate_duns_number,
+        config.paginationDefaultSize,
+        page
+      )
     )
 
     res.json({
