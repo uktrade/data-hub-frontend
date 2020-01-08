030e98ba30f66b453107348242df4c4ef21d6125
Pippo Raimondi <filipporai@gmail.com>
2020-01-08 15:52:25 +0000

    Dynamically instrument code on test environments

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,22 @@
 module.exports = {
-  "sourceType": "unambiguous",
-  "plugins": [
-    "@babel/plugin-proposal-object-rest-spread",
-    "@babel/plugin-proposal-class-properties"
+  'sourceType': 'unambiguous',
+  'env': {
+    'test': {
+      'plugins': ['istanbul'],
+    },
+  },
+  'plugins': [
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-class-properties',
   ],
-  "presets": [
-    ["@babel/preset-react"],
-    ["@babel/preset-env", {
-      "useBuiltIns": "usage",
-      "corejs": 2,
-      "targets": {
-        "browsers": ["last 2 versions", "ie >= 11"]
-      }
-    }]
-  ]
+  'presets': [
+    ['@babel/preset-react'],
+    ['@babel/preset-env', {
+      'useBuiltIns': 'usage',
+      'corejs': 2,
+      'targets': {
+        'browsers': ['last 2 versions', 'ie >= 11'],
+      },
+    }],
+  ],
 }
