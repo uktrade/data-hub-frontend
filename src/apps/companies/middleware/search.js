3bbcc46d82c1a607b26dd0c341e0573fdc1f1cd7
Peter Hudec <peterhudec@peterhudec.com>
2020-01-13 11:15:16 +0000

    Fix a bug with trailing commas in postcode filter

--- src/apps/companies/middleware/search.js
+++ src/apps/companies/middleware/search.js
@@ -1,5 +1,6 @@
 module.exports = (req, res, next) => {
   const postcode = req.body.uk_postcode
-  req.body.uk_postcode = postcode && postcode.split(',')
+  req.body.uk_postcode =
+    postcode && postcode.split(',').filter((x) => !x.match(/^\s*$/))
   next()
 }
