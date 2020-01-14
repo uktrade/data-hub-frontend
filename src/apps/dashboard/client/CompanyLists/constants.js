ea8fdf61cf096dd75c883e2692e63b82a8ddd41c
Ian Leggett <contactleggett@gmail.com>
2020-01-14 14:13:29 +0000

    Include advisers into the company list response

--- src/apps/dashboard/client/CompanyLists/constants.js
+++ src/apps/dashboard/client/CompanyLists/constants.js
@@ -2,3 +2,6 @@
 export const ALPHABETICAL = 'alphabetical'
 export const RECENT = 'recent'
 export const LEAST_RECENT = 'least-recent'
+export const UNKNOWN_ADVISER = 'unknown adviser'
+export const UNKNOWN_TEAM = 'unknown team'
+export const MULTIPLE_ADVISERS = 'Multiple advisers'
