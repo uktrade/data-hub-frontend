18386e9bcce396102adc9ee04731ffbc8674ce65
Ian Leggett <contactleggett@gmail.com>
2020-01-14 14:13:29 +0000

    Include advisers into the company list response

--- src/apps/dashboard/client/CompanyLists/Table.jsx
+++ src/apps/dashboard/client/CompanyLists/Table.jsx
@@ -4,6 +4,9 @@ import moment from 'moment'
 import React from 'react'
 import LinesEllipsis from 'react-lines-ellipsis'
 import styled from 'styled-components'
+import { upperFirst } from 'lodash'
+
+import { UNKNOWN_ADVISER, UNKNOWN_TEAM, MULTIPLE_ADVISERS } from './constants'
 
 import urls from '../../../../lib/urls'
 import SecondaryButton from './SecondaryButton'
@@ -14,20 +17,37 @@ const StyledCellLink = styled.a({
   marginBottom: 0,
 })
 
+const Advisers = ({ ditParticipants }) =>
+  ditParticipants.length === 0
+    ? `${upperFirst(UNKNOWN_ADVISER)} - ${upperFirst(UNKNOWN_TEAM)}`
+    : ditParticipants.length > 1
+    ? MULTIPLE_ADVISERS
+    : ditParticipants.map((adviser, index) => (
+        <div key={index}>
+          {adviser.team
+            ? adviser.name
+              ? `${adviser.name} - ${adviser.team}`
+              : `${upperFirst(UNKNOWN_ADVISER)} - ${adviser.team}`
+            : `${adviser.name} - ${UNKNOWN_TEAM}`}
+        </div>
+      ))
+
 const CompaniesTable = ({ companies }) => (
   <Table
     head={
       <Table.Row>
         <Table.CellHeader>Company name</Table.CellHeader>
         <Table.CellHeader>Last interaction</Table.CellHeader>
+        <Table.CellHeader>Subject</Table.CellHeader>
+        <Table.CellHeader>Added by</Table.CellHeader>
         {/* CellHeader complains if it has no children */}
         <Table.CellHeader colSpan="2">{''}</Table.CellHeader>
       </Table.Row>
     }
   >
-    {companies.map(({ company, latestInteraction }) => (
+    {companies.map(({ company, latestInteraction, ditParticipants }) => (
       <Table.Row key={company.id}>
-        <Table.Cell setWidth="25%">
+        <Table.Cell setWidth="20%">
           <Link href={`companies/${company.id}`}>
             <LinesEllipsis
               text={company.name}
@@ -43,7 +63,7 @@ const CompaniesTable = ({ companies }) => (
             ? moment(latestInteraction.date).format('D MMM YYYY')
             : '-'}
         </Table.Cell>
-        <Table.Cell setWidth="50%">
+        <Table.Cell setWidth="30%">
           {latestInteraction.id ? (
             <Link href={`interactions/${latestInteraction.id}`}>
               <LinesEllipsis
@@ -57,6 +77,9 @@ const CompaniesTable = ({ companies }) => (
           ) : (
             latestInteraction.subject
           )}
+        </Table.Cell>
+        <Table.Cell setWidth="20%">
+          <Advisers ditParticipants={ditParticipants} />
         </Table.Cell>
         <Table.Cell>
           <SecondaryButton
