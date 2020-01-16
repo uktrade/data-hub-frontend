fa39b9488d235e64185b25981859da3b86defe32
Ian Leggett <contactleggett@gmail.com>
2020-01-16 10:30:04 +0000

    Removes consts

--- src/apps/dashboard/client/CompanyLists/constants.js
+++ src/apps/dashboard/client/CompanyLists/constants.js
@@ -2,6 +2,3 @@
 export const ALPHABETICAL = 'alphabetical'
 export const RECENT = 'recent'
 export const LEAST_RECENT = 'least-recent'
-export const UNKNOWN_ADVISER = 'unknown adviser'
-export const UNKNOWN_TEAM = 'unknown team'
-export const MULTIPLE_ADVISERS = 'Multiple advisers'
