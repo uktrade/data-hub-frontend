cf15d5cd209b7acccf86206f69b6126754944890
Miriam Forner <mforner13@gmail.com>
2020-01-20 16:48:30 +0000

    Update events UK region functional test

--- test/functional/cypress/specs/events/filter-spec.js
+++ test/functional/cypress/specs/events/filter-spec.js
@@ -50,18 +50,38 @@ describe('Events Collections Filter', () => {
     cy.get(selectors.entityCollection.collectionRowButton).should('be.visible')
   })
 
-  it('should filter by region', () => {
-    cy.get(selectors.filter.events.firstUkRegion).click()
+  // it('should filter by region', () => {
+  //   cy.get(selectors.filter.events.firstUkRegion).click()
+
+  //   cy.wait('@filterResults').then((xhr) => {
+  //     expect(xhr.url).to.contain(
+  //       'uk_region=934cd12a-6095-e211-a939-e4115bead28a'
+  //     )
+  //   })
+
+  //   cy.get(selectors.entityCollection.entities)
+  //     .children()
+  //     .should('have.length', 1)
+  // })
+
+  it('should filter by uk region', () => {
+    const { typeahead } = selectors.filter
+    const { ukRegion } = selectors.filter.events
+    cy.get(typeahead(ukRegion).selectedOption)
+      .click()
+      .get(typeahead(ukRegion).textInput)
+      .type('North West')
+      .get(typeahead(ukRegion).options)
+      .should('have.length', 1)
+      .get(typeahead(ukRegion).textInput)
+      .type('{enter}')
+      .type('{esc}')
 
     cy.wait('@filterResults').then((xhr) => {
       expect(xhr.url).to.contain(
-        'uk_region=934cd12a-6095-e211-a939-e4115bead28a'
+        'uk_region=824cd12a-6095-e211-a939-e4115bead28a'
       )
     })
-
-    cy.get(selectors.entityCollection.entities)
-      .children()
-      .should('have.length', 1)
   })
 
   it('should remove all filters', () => {
