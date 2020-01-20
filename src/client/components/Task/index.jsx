4e89512275420eb5f47efc4fd44c077b5a3b5d4a
Peter Hudec <peterhudec@peterhudec.com>
2020-01-20 15:23:17 +0000

    Add docstrings

--- src/client/components/Task/index.jsx
+++ src/client/components/Task/index.jsx
@@ -18,6 +18,87 @@ const startOnRenderPropTypes = {
   onSuccessDispatch: PropTypes.string,
 }
 
+/**
+ * @typedef SuccessAction
+ * @property {string} type - The value of the {onSuccessDispatch} option
+ * @property {string} name - The _task_ name
+ * @property {string} id - The _task_ id
+ * @property {any} payload - The payload with which the _task_ was started
+ * @property {any} result - The result with which the _task_ succeeded
+ */
+
+/**
+ * Task start options
+ * @typedef StartOptions
+ * @property {string} [onSuccessDispatch=] - This is a mechanism to process the
+ * result of a _tasks_ success. If set, a {SuccessAction} with this value
+ * as its `type` will be dispatched when the _task_ succeeds, but before the
+ * _task_ is _cleared_.
+ */
+
+/**
+ * The shape of the object returned by `getTask` in the `Task` component
+ * @typedef Task
+ * @property {'progress' | 'error'} status - The current status of the task
+ * @property {Boolean} progress - Whether the task is in progress
+ * @property {Boolean} error - Whether the task is in error state
+ * @property {(name, id, StartOptions) => void} start - Starts a task.
+ */
+
+/**
+ * Enables starting and reading states of _registered tasks_. A _task_ is a
+ * function which takes a _payload_ as its only argument and returns a
+ * {Promise}. _Tasks_ are registered by passing a map of names to _tasks_ to the
+ * `tasksSagaFactory` function and plugging in the resulting saga e.g.
+ * To register a task that always succeeds with the value `123` under the name
+ * `'foo'` you can do:
+ * `sagaMiddleware.run(taskSagaFactory({foo: payload => Promise.resolve(123)}))`.
+ * The acction names are used in the default error view so it's best to keep
+ * them human readable.
+ * A task can be in three possible states:
+ * - Idle: it hasn't started yet, or it has succeeded
+ * - Progress: the promise hasn't resolved yet
+ * - Error: the promise rejected
+ * The task doesn't have any success state, but a success action dispatch
+ * mechanism. To process the tasks result i.e. the value of the resolved
+ * promise, you can start the action with the `onSuccessDispatch` option, which
+ * will be used as the type of the {SuccessAction}, which you can then subscribe
+ * to in a reducer to store its {result} to the state.
+ * @param {Object} props
+ * @param {(name, id) => Task} props.children - Accepts a function as a single
+ * child, which will receive a `getTask` function as its only argument.
+ * The `getTask` function takes a _task_ name and ID and returns the {Task}
+ * object representing the _task_.
+ * @example
+ * sagaMiddleware.run(taskSagaFactory({
+ *   square: payload =>
+ *     new Promise(resolve => setTimeout(resolve, 1000, payload ** 2)),
+ * }))
+ * <Task>
+ *   {getTask => {
+ *     const task = getTask('square', 'foo')
+ *     return (d
+ *       <button
+ *         disabled={task.progress}
+ *         onClick={() => task.start({
+ *           payload: 7,
+ *           onSuccessDispatch: 'SQUARED',
+ *         })}
+ *       >
+ *         start
+ *       </button>
+ *     )
+ *   }}
+ * </Task>
+ * // Clicking on the button will dispatch the following action after a second
+ * const successAction = {
+ *   type: 'SQUARED',
+ *   name: 'square',
+ *   id: 'foo',
+ *   payload: 7,
+ *   result: 49,
+ * }
+ */
 const Task = connect(
   (state) => state.tasks,
   (dispatch) => ({
@@ -36,7 +117,6 @@ const Task = connect(
     return {
       ...taskState,
       progress: taskState.status === 'progress',
-      success: taskState.status === 'success',
       error: taskState.status === 'error',
       start: (options) => start(name, id, options),
     }
@@ -47,6 +127,18 @@ Task.propTypes = {
   children: PropTypes.func.isRequired,
 }
 
+/**
+ * Starts a task, if not in _progress_ or _error_ state when the component is
+ * renderded.
+ * @param {Object} props
+ * @param {string} props.name - The _task_ name
+ * @param {string} props.id - The _task_ id
+ * @param {any} [props.payload=] - The payload start the _task_ with
+ * @param {string} [props.onSuccessDispatch=] - If set, a {SuccessAction} with
+ * this value as its {type} will be dispatched
+ * @example
+ * <Task.StartOnRender name="foo" id="a" payload={123} onSuccessDispatch="FOO"/>
+ */
 Task.StartOnRender = connect(
   (state, { name, id }) => _.get(state, ['tasks', name, id], {}),
   (dispatch, { id, name }) => ({
@@ -70,6 +162,30 @@ Task.StartOnRender.propTypes = {
   ...startOnRenderPropTypes,
 }
 
+/**
+ * Renders the progress or error states of a _task_ or nothing
+ * @param {Object} props
+ * @param {string} props.name - The _task_ name
+ * @param {string} props.id - The _task_ id
+ * @param {string} [props.noun=props.name] - The noun that should be used in the
+ * error view e.g. "Could not load <noun>"
+ * @param {string} [props.progressMessage='Loading <noun>'] - The message that
+ * will be forwarded to the progress view
+ * @param {Progress} [props.renderProgress=ProgressIndicator] - You can override
+ * the default progress view
+ * @param {Component} [props.renderError=Err] - You can override the default
+ * error view
+ * @param {StartOptions} [props.startOnRender=] - If set to a {StartOptions}
+ * object, it will also behave like the {StartOnRender} component
+ * @example
+ * <Task.Status
+ *   name="foo"
+ *   id="foo"
+ *   noun="Something"
+ *   progressMessage="Loading something"
+ *   startOnRender={{ payload: 123, onSuccessDispatch: 'FOO'}}
+ * />
+ */
 Task.Status = ({
   name,
   id,
