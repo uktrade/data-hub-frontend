b8dd0a56d9ae510ec4495d2b5e655d5fa534f0c5
Miriam Forner <mforner13@gmail.com>
2020-01-23 14:13:26 +0000

    Add functional test for the uk regions type ahead filter

--- test/sandbox/routes/v3/search/investment-project.js
+++ test/sandbox/routes/v3/search/investment-project.js
@@ -2,4 +2,24 @@ var investmentProjects = require('../../../fixtures/v3/search/investment-project
 
 exports.investmentProjects = function(req, res) {
   res.json(investmentProjects)
+
+  if (req.body.uk_region_location) {
+    var regionQuery = req.body.uk_region_location
+    var regions = typeof regionQuery === 'string' ? [regionQuery] : regionQuery
+    var regionResultIds = _.get(investmentProjects.results, 'actual_uk_regions')
+    var ukRegionFilteredResults = _.filter(investmentProjects.results, function(
+      investmentProject
+    ) {
+      return _.intersection(
+        regions,
+        investmentProject.actual_uk_regions.map(function(region) {
+          return region.id
+        })
+      ).length
+    })
+    return res.json({
+      count: ukRegionFilteredResults.length,
+      results: ukRegionFilteredResults,
+    })
+  }
 }
