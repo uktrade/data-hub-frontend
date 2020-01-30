d9c51ab2e4123ccec8701fa6181ff762b9c6e661
Peter Hudec <peterhudec@peterhudec.com>
2020-01-30 13:48:13 +0000

    Visual test waits for company list to load

--- test/visual/specs/data-hub-pages-spec.js
+++ test/visual/specs/data-hub-pages-spec.js
@@ -7,8 +7,8 @@ describe('Data Hub', () => {
     })
 
     it('content', async () => {
-      // Wait for the company list progress indicator to disappear
-      await new Promise((resolve) => setTimeout(resolve, 1000))
+      // Wait for the initial list to be loaded
+      await $('=Edit list name')
       await browser.imageDiff.takeElement('.grid-row')
       await browser.imageDiff.validate().then((result) => {
         assert.strictEqual(result, 0)
