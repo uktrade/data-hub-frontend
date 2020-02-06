ffea60eaf636b4d19e072688cea56f8dfab28627
Peter Hudec <peterhudec@peterhudec.com>
2020-02-06 12:59:25 +0000

    Improve orders (OMIS) UK Region filter test

--- test/functional/cypress/specs/omis/filter-spec.js
+++ test/functional/cypress/specs/omis/filter-spec.js
@@ -16,17 +16,19 @@ describe('OMIS Collections Filter', () => {
     cy.get(typeahead(ukRegion).selectedOption)
       .click()
       .get(typeahead(ukRegion).textInput)
-      .type('All')
+      .type('North West')
       .get(typeahead(ukRegion).options)
 
     cy.get(typeahead(ukRegion).selectedOption)
       .type('{enter}')
       .type('{esc}')
 
-    cy.wait('@filterResults').then((xhr) => {
-      expect(xhr.url).to.contain(
-        'uk_region=1718e330-6095-e211-a939-e4115bead28a'
-      )
+    cy.wait('@filterResults').then(() => {
+      cy.get('article header').within(() => {
+        cy.contains('1 order')
+        cy.contains('North West')
+      })
+      cy.get('article ol').should('have.length', 1)
     })
   })
 
