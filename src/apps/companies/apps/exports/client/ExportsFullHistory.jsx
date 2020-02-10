32133293e1ebbf1dded0f3a021cd5149300c4de2
David Matthews <dwm503@gmail.com>
2020-02-10 12:48:55 +0000

    fixup! add full export history page

--- src/apps/companies/apps/exports/client/ExportsFullHistory.jsx
+++ src/apps/companies/apps/exports/client/ExportsFullHistory.jsx
@@ -10,25 +10,12 @@ const Wrapper = styled('div')`
   margin-top: 16px;
 `
 
-const DEFAULT_ITEMS_PER_PAGE = 10
-
 function ExportFullHistory({ data }) {
-  const [activePage, setActivePage] = useState(1)
-
-  const index = activePage - 1
-  const offset = index * DEFAULT_ITEMS_PER_PAGE
-  const limit = (index + 1) * DEFAULT_ITEMS_PER_PAGE
-
   return (
     <Wrapper>
       <H3>Export markets history</H3>
       <CollectionList
         items={data.results}
-        onPageClick={(page, event) => {
-          setActivePage(page)
-          event.preventDefault()
-        }}
-        activePage={activePage}
         totalItems={data.count}
         itemName="result"
       />
