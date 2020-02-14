5ae3a7816e0afa80a201311e1523d46f0418581a
David Matthews <dwm503@gmail.com>
2020-02-14 15:45:28 +0000

    fixup! fixup! fixup! fixup! add full export history page

--- src/apps/companies/apps/exports/client/ExportsFullHistory.jsx
+++ src/apps/companies/apps/exports/client/ExportsFullHistory.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { H3 } from '@govuk-react/heading'
-import { SPACING } from '@govuk-react/constants'
+import { H2 } from '@govuk-react/heading'
+import { SPACING, LEVEL_SIZE } from '@govuk-react/constants'
 import { CollectionList } from 'data-hub-components'
 
 const Wrapper = styled('div')`
@@ -12,7 +12,7 @@ const Wrapper = styled('div')`
 function ExportFullHistory({ data }) {
   return (
     <Wrapper>
-      <H3>Export markets history</H3>
+      <H2 size={LEVEL_SIZE[3]}>Export markets history</H2>
       <CollectionList
         items={data.results}
         totalItems={data.count}
