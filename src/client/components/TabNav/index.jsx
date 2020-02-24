657c3c4dd8e46e23aa630fe150d3b07a9476809e
Peter Hudec <peterhudec@peterhudec.com>
2020-02-24 13:54:02 +0000

    Add the TabNav component

---
+++ src/client/components/TabNav/index.jsx
@@ -0,0 +1,71 @@
+import { get } from 'lodash'
+import PropTypes from 'prop-types'
+import React from 'react'
+import { Tabs } from 'govuk-react'
+import styled from 'styled-components'
+
+import multiInstance from '../../utils/multiinstance'
+import { TAB_NAV__SELECT } from '../../actions'
+import reducer from './reducer'
+
+const StyledTabsList = styled(Tabs.List)({
+  a: {
+    cursor: 'pointer',
+  },
+})
+
+const TabNav = ({
+  defaultSelected,
+  selectedIdx = defaultSelected,
+  onChange,
+  tabs = [],
+  title,
+  ...props
+}) => (
+  <div {...props}>
+    <Tabs>
+      <Tabs.Title>{title}</Tabs.Title>
+      <StyledTabsList>
+        {tabs.map(({ label }, idx) => (
+          <Tabs.Tab
+            key={idx}
+            onClick={() => onChange(idx)}
+            selected={idx === selectedIdx}
+          >
+            {label}
+          </Tabs.Tab>
+        ))}
+      </StyledTabsList>
+    </Tabs>
+    {get(tabs, [selectedIdx, 'content'])}
+  </div>
+)
+
+TabNav.propTypes = {
+  title: PropTypes.string.isRequired,
+  tabs: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.oneOfType([PropTypes.string, PropTypes.element])
+        .isRequired,
+      content: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.element,
+        PropTypes.number,
+      ]),
+    })
+  ),
+  defaultSelected: PropTypes.number,
+}
+
+export default multiInstance({
+  name: 'TabNav',
+  dispatchToProps: (dispatch) => ({
+    onChange: (selectedIdx) =>
+      dispatch({
+        type: TAB_NAV__SELECT,
+        selectedIdx,
+      }),
+  }),
+  component: TabNav,
+  reducer,
+})
