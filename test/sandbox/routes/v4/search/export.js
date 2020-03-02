17f4d14a16142695963f54fbac62c4ec67e1f7b1
Graham Veal <grahamveal@gmail.com>
2020-03-02 22:30:08 +0000

    Add functional tests for interactions in export history

--- test/sandbox/routes/v4/search/export.js
+++ test/sandbox/routes/v4/search/export.js
@@ -3,22 +3,32 @@ var emptyFullExportHistory = require('../../../fixtures/v4/export/empty-full-exp
 var unkownUserExportHistory = require('../../../fixtures/v4/export/unkown-user-export-history.json')
 var updateOnlyExportHistory = require('../../../fixtures/v4/export/update-only-export-history.json')
 var countryExportHistory = require('../../../fixtures/v4/export/country-export-history.json')
+var exportHistoryWithInteractions = require('../../../fixtures/v4/export/history-with-interactions.json')
+
 var dnbCorp = require('../../../fixtures/v4/company/company-dnb-corp.json')
 var marsExportsLtd = require('../../../fixtures/v4/company/company-mars-exports-ltd.json')
 var dnbSubsidiary = require('../../../fixtures/v4/company/company-dnb-subsidiary.json')
+var investigationLtd = require('../../../fixtures/v4/company/company-investigation-ltd.json')
 
 exports.fetchExportHistory = function(req, res) {
+  var companyId = req.body.company
+
   if (req.body.country === '975f66a0-5d95-e211-a939-e4115bead28a') {
     return res.json(countryExportHistory)
   }
-  if (req.body.company === dnbCorp.id) {
+
+  if (companyId === dnbCorp.id) {
     return res.json(fullExportHistoryPage1)
   }
-  if (req.body.company === marsExportsLtd.id) {
+  if (companyId === marsExportsLtd.id) {
     return res.json(unkownUserExportHistory)
   }
-  if (req.body.company === dnbSubsidiary.id) {
+  if (companyId === dnbSubsidiary.id) {
     return res.json(updateOnlyExportHistory)
   }
+  if (companyId === investigationLtd.id) {
+    return res.json(exportHistoryWithInteractions)
+  }
+
   return res.json(emptyFullExportHistory)
 }
