cb2fd496fcfce99c54004abe74473fc36f62108e
Peter Hudec <peterhudec@peterhudec.com>
2020-03-20 14:10:49 +0000

    Updated wording on referral badge

--- src/client/components/ReferralList/Referral.jsx
+++ src/client/components/ReferralList/Referral.jsx
@@ -42,7 +42,7 @@ const Referral = ({
       badge={
         dateAccepted
           ? {
-              text: 'Referral accepted',
+              text: 'Accepted referral',
               borderColour: GREEN,
             }
           : {
