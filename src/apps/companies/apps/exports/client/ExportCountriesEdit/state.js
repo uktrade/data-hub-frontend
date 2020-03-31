b29ca53313667ed1a3918dea2464fdf2f6454f0a
Graham Veal <grahamveal@gmail.com>
2020-03-31 15:49:30 +0100

    Change ExportCountriesEdit to use Sagas and the Task

---
+++ src/apps/companies/apps/exports/client/ExportCountriesEdit/state.js
@@ -0,0 +1,7 @@
+export const ID = 'exportCountriesEdit'
+export const TASK_NAME = 'Export countries edit'
+export const API_ERROR = 'api-error'
+export const API_WARN = 'api-fail'
+export const SAVED = 'edit-saved'
+
+export const state2props = (state) => state[ID]
