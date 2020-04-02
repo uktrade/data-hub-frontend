cc60c6e21d49d66b04fea374503022cd1666ee1c
Peter Hudec <peterhudec@peterhudec.com>
2020-04-02 12:28:26 +0100

    Add filter to ReferralList

--- src/client/components/ReferralList/index.jsx
+++ src/client/components/ReferralList/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import HintText from '@govuk-react/hint-text'
+import { SelectInput } from '@govuk-react/select'
 import { SPACING } from '@govuk-react/constants'
 import pluralize from 'pluralize'
 import styled from 'styled-components'
@@ -7,8 +8,12 @@ import styled from 'styled-components'
 import ContentWithHeading from '../ContentWithHeading'
 import Task from '../Task'
 import Referral from './Referral'
-import { REFFERAL_LIST__LOADED } from '../../actions'
+import {
+  REFFERAL_LIST__LOADED,
+  REFFERAL_LIST__FILTER_CHANGE,
+} from '../../actions'
 import multiInstance from '../../utils/multiinstance'
+import { SENT, RECEIVED } from './constants'
 import reducer from './reducer'
 
 const StyledOl = styled.ol({
@@ -18,10 +23,30 @@ const StyledOl = styled.ol({
   },
 })
 
+const StyledLabel = styled.label({
+  display: 'flex',
+  alignItems: 'baseline',
+})
+
+const StyledSelectInput = styled(SelectInput)({
+  marginLeft: 10,
+  // We need to increase specificity with & to override the 50% width
+  '&': {
+    width: 'initial',
+  },
+})
+
 export default multiInstance({
   name: 'ReferralList',
   reducer,
-  component: ({ id, referrals }) => (
+  dispatchToProps: (dispatch) => ({
+    onFilterChange: (filter) =>
+      dispatch({
+        type: REFFERAL_LIST__FILTER_CHANGE,
+        filter,
+      }),
+  }),
+  component: ({ id, referrals, onFilterChange, filter }) => (
     <Task.Status
       name="Referrals"
       id={id}
@@ -30,29 +55,50 @@ export default multiInstance({
         onSuccessDispatch: REFFERAL_LIST__LOADED,
       }}
     >
-      {() =>
-        referrals && (
-          <ContentWithHeading
-            heading={pluralize('Referral', referrals.length, true)}
-          >
-            {referrals.length ? (
-              <StyledOl>
-                {referrals.map(({ id, ...referral }) => (
-                  <li key={id}>
-                    <Referral id={id} {...referral} />
-                  </li>
-                ))}
-              </StyledOl>
-            ) : (
-              <HintText>
-                You have not received or sent any referrals.
-                <br />
-                You can refer companies to other advisers from a company page.
-              </HintText>
-            )}
-          </ContentWithHeading>
-        )
-      }
+      {() => {
+        if (referrals) {
+          const filteredReferrals = referrals.filter(
+            ({ direction }) => direction === filter
+          )
+          return (
+            <ContentWithHeading
+              heading={pluralize(
+                `${filter} referral`,
+                filteredReferrals.length,
+                true
+              )}
+              headingActions={
+                <StyledLabel>
+                  View
+                  <StyledSelectInput
+                    value={filter}
+                    onChange={(e) => onFilterChange(e.target.value)}
+                  >
+                    <option value={RECEIVED}>Received referrals</option>
+                    <option value={SENT}>Sent referrals</option>
+                  </StyledSelectInput>
+                </StyledLabel>
+              }
+            >
+              {referrals.length ? (
+                <StyledOl>
+                  {filteredReferrals.map(({ id, ...referral }) => (
+                    <li key={id}>
+                      <Referral id={id} {...referral} />
+                    </li>
+                  ))}
+                </StyledOl>
+              ) : (
+                <HintText>
+                  You have not received or sent any referrals.
+                  <br />
+                  You can refer companies to other advisers from a company page.
+                </HintText>
+              )}
+            </ContentWithHeading>
+          )
+        }
+      }}
     </Task.Status>
   ),
 })
--- src/client/components/ReferralList/task.js
+++ src/client/components/ReferralList/task.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+import { SENT, RECEIVED } from './constants'
+
 const handleError = (e) => Promise.reject(Error(e.response.data.detail))
 
 const convertAdviser = ({ name, contact_email, dit_team }) => ({
@@ -9,18 +11,23 @@ const convertAdviser = ({ name, contact_email, dit_team }) => ({
 })
 
 export default () =>
-  axios
-    .get('/api-proxy/v4/company-referral')
+  Promise.all([
+    axios.get('/api-proxy/v4/company-referral'),
+    axios.get('/api-proxy/whoami/'),
+  ])
     .catch(handleError)
-    .then(({ data: { results } }) =>
-      results.map((referral) => ({
-        companyId: referral.company.id,
-        id: referral.id,
-        subject: referral.subject,
-        companyName: referral.company.name,
-        date: referral.created_on,
-        dateAccepted: referral.completed_on,
-        sender: convertAdviser(referral.created_by),
-        recipient: convertAdviser(referral.recipient),
-      }))
+    .then(([{ data: { results } }, { data: { id } }]) =>
+      results
+        .map((referral) => ({
+          companyId: referral.company.id,
+          id: referral.id,
+          subject: referral.subject,
+          companyName: referral.company.name,
+          date: referral.created_on,
+          dateAccepted: referral.completed_on,
+          sender: convertAdviser(referral.created_by),
+          recipient: convertAdviser(referral.recipient),
+          direction: referral.created_by.id === id ? SENT : RECEIVED,
+        }))
+        .sort((a, b) => new Date(a.date) - new Date(b.date))
     )
