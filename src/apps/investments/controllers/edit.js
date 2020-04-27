75bdc30262856a3cfe05b4e05f8d5663dfb38e7c
Miriam Forner <mforner13@gmail.com>
2020-04-27 16:41:01 +0100

    Conditionally display project value field
    The project value field will only be displayed if the project's actual or estimated land date is before 01/04/2020.

--- src/apps/investments/controllers/edit.js
+++ src/apps/investments/controllers/edit.js
@@ -1,9 +1,21 @@
+// For projects landing after 01/04/2020, the project value field is not needed
+const valueCutOffDate = new Date('2020-04-01')
+
 function editDetailsGet(req, res) {
   res.breadcrumb('Edit details').render('investments/views/details-edit')
 }
 
 function editValueGet(req, res) {
-  res.breadcrumb('Edit value').render('investments/views/value-edit')
+  const {
+    estimated_land_date: estimatedLandDate,
+    actual_land_date: actualLandDate,
+  } = res.locals.form.state
+
+  res.breadcrumb('Edit value').render('investments/views/value-edit', {
+    projectValueNeeded:
+      (estimatedLandDate || actualLandDate) &&
+      new Date(estimatedLandDate || actualLandDate) < valueCutOffDate,
+  })
 }
 
 function renderRequirementsForm(req, res) {
