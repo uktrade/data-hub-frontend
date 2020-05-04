461b78724281e3307b584485845146c759d7562b
Adam L Edwards <adam@aeweb.io>
2020-05-04 12:14:21 +0100

    Add Dropdown story

---
+++ src/client/components/DropdownMenu/__stories__/DropdownMenu.stories.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import { action } from '@storybook/addon-actions'
+import { GREY_3, BLACK } from 'govuk-colours'
+
+import DropdownMenu, { DropdownButton } from '../DropdownMenu'
+import defaultReadme from './default.md'
+import noCloseLabelReadme from './noCloseLabel.md'
+import usageReadme from './usage.md'
+
+export default {
+  component: DropdownMenu,
+  title: 'DropdownMenu',
+  parameters: {
+    readme: {
+      sidebar: usageReadme,
+    },
+  },
+}
+
+const withState = (Component) => () => {
+  const [isOpen, setState] = React.useState(false)
+  const clickAction = action('click')
+  return (
+    <Component
+      open={isOpen}
+      onClick={(nextState) => {
+        clickAction(nextState)
+        setState(nextState)
+      }}
+    />
+  )
+}
+
+const DefaultComponent = (props) => (
+  <DropdownMenu label="View Options" closedLabel="Hide Options" {...props}>
+    <DropdownButton buttonColour={GREY_3} buttonTextColour={BLACK}>
+      Add to or remove from list
+    </DropdownButton>
+    <DropdownButton buttonColour={GREY_3} buttonTextColour={BLACK}>
+      Add to pipeline
+    </DropdownButton>
+  </DropdownMenu>
+)
+
+export const Default = withState(DefaultComponent)
+
+Default.story = {
+  parameters: {
+    readme: {
+      content: defaultReadme,
+    },
+  },
+}
+
+const NoClosedLabelComponent = (props) => (
+  <DropdownMenu label="View Options" {...props}>
+    <DropdownButton buttonColour={GREY_3} buttonTextColour={BLACK}>
+      Add to or remove from list
+    </DropdownButton>
+    <DropdownButton buttonColour={GREY_3} buttonTextColour={BLACK}>
+      Add to pipeline
+    </DropdownButton>
+  </DropdownMenu>
+)
+
+export const NoClosedLabel = withState(NoClosedLabelComponent)
+
+NoClosedLabel.story = {
+  parameters: {
+    readme: {
+      content: noCloseLabelReadme,
+    },
+  },
+}
