e69b3c69ced20a4a6aea73a1c21c28ad6705f1ad
Pippo Raimondi <filipporai@gmail.com>
2020-05-06 17:58:19 +0100

    Update browserstack service config

--- test/visual/wdio.conf.js
+++ test/visual/wdio.conf.js
@@ -55,7 +55,13 @@ exports.config = {
   waitforTimeout: 10000,
   connectionRetryTimeout: 90000,
   connectionRetryCount: 3,
-  services: ['browserstack'],
+  services: [
+    [
+      'browserstack', {
+        browserstackLocal: true
+      }
+    ]
+  ],
   user: browserStackUser,
   key: browserStackKey,
   browserstackLocal: true,
