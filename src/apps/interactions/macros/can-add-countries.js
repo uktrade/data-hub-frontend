c2a96daf3d3e48dc1d7e4f1111c45aa379220431
Rafal Enden <git@enden.com>
2020-05-11 18:03:35 +0100

    Remove the old interaction form

--- src/apps/interactions/macros/can-add-countries.js
+++
@@ -1,9 +0,0 @@
-const { THEMES } = require('../constants')
-
-module.exports = (theme, interaction) => {
-  if (interaction) {
-    return false
-  }
-
-  return theme === THEMES.EXPORT || theme === THEMES.OTHER
-}
