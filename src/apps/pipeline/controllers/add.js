0c48af886a8a941dcf09fb5068ba22417fea3d08
Adam L Edwards <adam@aeweb.io>
2020-05-11 15:02:54 +0100

    Add company post with flash controller

--- src/apps/pipeline/controllers/add.js
+++ src/apps/pipeline/controllers/add.js
@@ -1,4 +1,6 @@
 const urls = require('../../../lib/urls')
+const { authorisedRequest } = require('../../../lib/authorised-request')
+const config = require('../../../config')
 
 function renderAddToPipeline(req, res) {
   const { company } = res.locals
@@ -14,6 +16,25 @@ function renderAddToPipeline(req, res) {
     })
 }
 
+async function addCompanyToPipeline(req, res) {
+  const { company } = res.locals
+  try {
+    const response = await authorisedRequest(req.session.token, {
+      url: `${config.apiRoot}/v4/pipeline-item`,
+      method: 'POST',
+      body: {
+        company: company.id,
+        ...req.body,
+      },
+    })
+    req.flash('success', 'Pipeline changes for this company have been saved')
+    return res.send(response)
+  } catch ({ error, statusCode }) {
+    return res.status(statusCode).send(error)
+  }
+}
+
 module.exports = {
   renderAddToPipeline,
+  addCompanyToPipeline,
 }
--- src/apps/pipeline/router.js
+++ src/apps/pipeline/router.js
@@ -2,8 +2,13 @@ const urls = require('../../lib/urls')
 
 const router = require('express').Router()
 
-const { renderAddToPipeline } = require('./controllers/add')
+const {
+  renderAddToPipeline,
+  addCompanyToPipeline,
+} = require('./controllers/add')
 
-router.get(urls.pipeline.index.route, renderAddToPipeline)
+router
+  .get(urls.pipeline.index.route, renderAddToPipeline)
+  .post(urls.pipeline.index.route, addCompanyToPipeline)
 
 module.exports = router
