89a8e647ceff9d476439cc9e3ecaa11edab8f6c4
David Matthews <dwm503@gmail.com>
2020-05-18 17:05:26 +0100

    test: add functional tests

--- test/sandbox/routes/v4/pipeline-item/index.js
+++ test/sandbox/routes/v4/pipeline-item/index.js
@@ -2,10 +2,25 @@ var pipelineItemLambdaPlc = require('../../../fixtures/v4/pipeline-item/pipeline
 var pipelineNoResult = require('../../../fixtures/v4/pipeline-item/pipeline-item-no-results.json')
 var pipelineCreate = require('../../../fixtures/v4/pipeline-item/pipeline-item-create.json')
 var lambdaPlc = require('../../../fixtures/v4/company/company-lambda-plc.json')
+var leads = require('../../../fixtures/v4/pipeline-item/leads.json')
+var inProgress = require('../../../fixtures/v4/pipeline-item/in-progress.json')
+var win = require('../../../fixtures/v4/pipeline-item/win.json')
 
 exports.getPipelineItems = function(req, res) {
   if (req.query.company_id === lambdaPlc.id) {
     res.json(pipelineItemLambdaPlc)
+    return
+  }
+  if (req.query.status === 'leads') {
+    res.json(leads)
+    return
+  }
+  if (req.query.status === 'in_progress') {
+    res.json(inProgress)
+    return
+  }
+  if (req.query.status === 'win') {
+    res.json(win)
     return
   }
   res.json(pipelineNoResult)
