3e2ad93639e7139f056c8af6c84acaabfa29fa5b
Adam L Edwards <adam@aeweb.io>
2020-05-19 16:15:33 +0100

    Add edit Task and state

--- src/apps/my-pipeline/client/state.js
+++ src/apps/my-pipeline/client/state.js
@@ -2,5 +2,7 @@ export const ID = 'pipeline'
 export const TASK_GET_PIPELINE_BY_COMPANY = 'TASK_GET_PIPELINE_BY_COMPANY'
 
 export const TASK_ADD_COMPANY_TO_PIPELINE = 'TASK_ADD_COMPANY_TO_PIPELINE'
+export const TASK_GET_PIPELINE_ITEM = 'TASK_GET_PIPELINE_ITEM'
+export const TASK_EDIT_PIPELINE_ITEM = 'TASK_EDIT_PIPELINE_ITEM'
 
 export const state2props = (state) => state[ID]
