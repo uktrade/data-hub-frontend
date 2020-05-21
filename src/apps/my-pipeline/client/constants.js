1e81ff66e2c95341d37d0afc220fc00f231fe9f9
Adam L Edwards <adam@aeweb.io>
2020-05-21 11:00:35 +0100

    Update PropTypes

---
+++ src/apps/my-pipeline/client/constants.js
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types'
+
+export const PipelineItemPropType = PropTypes.exact({
+  company: PropTypes.exact({
+    export_potential: PropTypes.string,
+    id: PropTypes.string,
+    name: PropTypes.string,
+    turnover: PropTypes.string,
+  }),
+  created_on: PropTypes.string,
+  id: PropTypes.string,
+  name: PropTypes.string,
+  status: PropTypes.string,
+})
+
+export const PipelineItemsPropType = PropTypes.exact({
+  companyId: PropTypes.string,
+  count: PropTypes.number,
+  results: PropTypes.arrayOf(PipelineItemPropType),
+})
