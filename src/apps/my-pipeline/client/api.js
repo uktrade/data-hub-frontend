6c0bb00786570055aae8f2990329faf0daf4cf3d
Adam L Edwards <adam@aeweb.io>
2020-05-22 11:45:54 +0100

    Add consistent error messaging for pipelines

--- src/apps/my-pipeline/client/api.js
+++ src/apps/my-pipeline/client/api.js
@@ -2,20 +2,38 @@ import axios from 'axios'
 
 const endpoint = '/api-proxy/v4/pipeline-item'
 
+function handleSuccess(result) {
+  return result
+}
+
+function handleError(error) {
+  const message = error?.response?.data?.detail || error.message
+  return Promise.reject({
+    message,
+    ...error,
+  })
+}
+
 function getPipelineItems(filter) {
-  return axios.get(`${endpoint}`, { params: filter })
+  return axios
+    .get(`${endpoint}`, { params: filter })
+    .then(handleSuccess, handleError)
 }
 
 function getPipelineItem(pipelineItemId) {
-  return axios.get(`${endpoint}/${pipelineItemId}`)
+  return axios
+    .get(`${endpoint}/${pipelineItemId}`)
+    .then(handleSuccess, handleError)
 }
 
 function createPipelineItem(values) {
-  return axios.post(`${endpoint}`, values)
+  return axios.post(`${endpoint}`, values).then(handleSuccess, handleError)
 }
 
 function updatePipelineItem(pipelineItemId, values) {
-  return axios.patch(`${endpoint}/${pipelineItemId}`, values)
+  return axios
+    .patch(`${endpoint}/${pipelineItemId}`, values)
+    .then(handleSuccess, handleError)
 }
 
 const pipelineApi = {
