03a8f8a81b6ee29a3edab4acc25952db6638b42a
Ian Leggett <contactleggett@gmail.com>
2020-05-29 14:18:47 +0100

    Update styles for the tabs in mobile

--- src/client/components/Pipeline/index.jsx
+++ src/client/components/Pipeline/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
 import { BLUE, WHITE } from 'govuk-colours'
-import { FONT_SIZE } from '@govuk-react/constants'
+import {
+  FONT_SIZE,
+  MEDIA_QUERIES,
+  SPACING_POINTS,
+} from '@govuk-react/constants'
 
 import TabNav from '../TabNav'
 import PipelineList from './PipelineList'
@@ -11,29 +15,41 @@ const SubTabs = styled(TabNav)`
   margin-top: -15px; /* Because we are in a tabpanel of an existing TabNav, it has a 30px margin at the top but we don't want that much */
 
   [role='tablist'] {
-    padding: 0;
-    margin: 0 0 2px 0;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: center; /* Backwards compatible */
-    justify-content: space-between;
-    color: ${BLUE};
-    background-color: ${WHITE};
+    ${MEDIA_QUERIES.TABLET} {
+      padding: 0;
+      margin: 0 0 2px 0;
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      justify-content: center; /* Backwards compatible */
+      justify-content: space-between;
+      color: ${BLUE};
+      background-color: ${WHITE};
+    }
+    span {
+      &::before {
+        margin-left: ${SPACING_POINTS[5]}px;
+      }
+    }
   }
 
   [role='tab'] {
-    background: none;
     border: none;
-    color: ${BLUE};
-    font-size: ${FONT_SIZE.SIZE_24};
-    line-height: 1.3;
-    padding: 0 0 6px 0;
+    ${MEDIA_QUERIES.TABLET} {
+      background: none;
+      border: none;
+      color: ${BLUE};
+      font-size: ${FONT_SIZE.SIZE_24};
+      line-height: 1.3;
+      padding: 0 0 6px 0;
+    }
 
     &[aria-selected='true'] {
-      border-bottom: 8px solid ${BLUE};
-      color: ${BLUE};
-      padding: 0;
+      ${MEDIA_QUERIES.TABLET} {
+        border-bottom: 8px solid ${BLUE};
+        color: ${BLUE};
+        padding: 0;
+      }
     }
   }
 `
