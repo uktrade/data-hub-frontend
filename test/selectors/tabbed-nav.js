3eec9e03662fe913f4f31d7f5e3e7fdbe4325dec
Ian Leggett <contactleggett@gmail.com>
2020-05-29 14:18:47 +0100

    Update styles for the tabs in mobile

--- test/selectors/tabbed-nav.js
+++ test/selectors/tabbed-nav.js
@@ -2,7 +2,7 @@ module.exports = () => {
   const tabbedNavSelector = `[role="tablist"]`
   return {
     container: tabbedNavSelector,
-    item: (number) => `${tabbedNavSelector} button:nth-child(${number})`,
+    item: (number) => `${tabbedNavSelector} span:nth-child(${number}) button`,
     selected: `${tabbedNavSelector} button[aria-selected="true"]`,
     buttons: `${tabbedNavSelector} button`,
   }
