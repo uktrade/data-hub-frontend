693152691a8e9017fbf2c9175a1f3f859b013613
Ian Leggett <contactleggett@gmail.com>
2020-06-03 10:54:13 +0100

    Add Main component for full page layouts

---
+++ src/client/components/Main/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { MEDIA_QUERIES, SPACING, SITE_WIDTH } from '@govuk-react/constants'
+
+const OuterContainer = styled('main')({
+  paddingTop: SPACING.SCALE_5,
+  textAlign: 'center',
+})
+
+const InnerContainer = styled('div')({
+  maxWidth: SITE_WIDTH,
+  marginLeft: SPACING.SCALE_3,
+  marginRight: SPACING.SCALE_3,
+  textAlign: 'left',
+  [MEDIA_QUERIES.LARGESCREEN]: {
+    marginLeft: SPACING.SCALE_5,
+    marginRight: SPACING.SCALE_5,
+  },
+  // no 1020px breakpoint in constants yet
+  '@media only screen and (min-width:1020px)': {
+    margin: '0 auto',
+  },
+})
+
+const Main = ({ children, ...props }) => (
+  <OuterContainer {...props} role="main" id="main-content">
+    <InnerContainer>{children}</InnerContainer>
+  </OuterContainer>
+)
+
+Main.propTypes = {
+  children: PropTypes.node,
+}
+
+Main.defaultProps = {
+  children: undefined,
+}
+
+export default Main
