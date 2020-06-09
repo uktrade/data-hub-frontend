9ea53e91ab1556dec97ac3fcc6abd7cd0e32a4eb
Ian Leggett <contactleggett@gmail.com>
2020-06-09 12:41:03 +0100

    Use session storage if flash messages are empty

--- src/client/components/LocalHeader/FlashMessages.jsx
+++ src/client/components/LocalHeader/FlashMessages.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { isEmpty } from 'lodash'
 
 import {
   ERROR_COLOUR,
@@ -14,7 +15,7 @@ import { FONT_WEIGHTS } from '@govuk-react/constants/lib/typography'
 import { FONT_SIZE, SPACING } from '@govuk-react/constants'
 import UnorderedList from '@govuk-react/unordered-list'
 import { StatusMessage } from 'data-hub-components'
-import flashUtils from '../../utils/flash-messages'
+import { getMessages, clearMessages } from '../../utils/flash-messages'
 
 const StyledBody = styled('p')`
   margin-bottom: 0;
@@ -47,41 +48,36 @@ const messageColours = {
 }
 
 const FlashMessages = ({ flashMessages }) => {
-  const flashMessagesFromStorage = flashUtils.getMessages()
-  flashUtils.clearMessages()
-
-  if (flashMessages || flashMessagesFromStorage) {
-    return (
-      <UnorderedList listStyleType="none" data-auto-id="flash">
-        {Object.entries(flashMessages || flashMessagesFromStorage).map(
-          ([type, message]) => {
-            const parts = String(type).split(':')
-            return parts.length > 1
-              ? message.map((message) => (
-                  <li key={message.body}>
-                    <StyledStatusMessage colour={messageColours[parts[0]]}>
-                      <StyledHeading>{message.heading}</StyledHeading>
-                      <StyledBody
-                        dangerouslySetInnerHTML={{ __html: message.body }}
-                      />
-                    </StyledStatusMessage>
-                  </li>
-                ))
-              : message.map((message) => (
-                  <li key={message}>
-                    <StyledStatusMessage colour={messageColours[type]}>
-                      <StyledMessage
-                        dangerouslySetInnerHTML={{ __html: message }}
-                      />
-                    </StyledStatusMessage>
-                  </li>
-                ))
-          }
-        )}
-      </UnorderedList>
-    )
-  }
-  return null
+  const flashMessagesFromStorage = getMessages()
+  clearMessages()
+  return !isEmpty(flashMessages) || flashMessagesFromStorage ? (
+    <UnorderedList listStyleType="none" data-auto-id="flash">
+      {Object.entries(
+        !isEmpty(flashMessages) ? flashMessages : flashMessagesFromStorage
+      ).map(([type, messages]) => {
+        // Example "success:with-body" -  If the string you pass in the first argument "type"
+        // has a colon then the message argument accepts two props in an object, one for the heading
+        // and one for the body. The first part of the string is used to indicate colour, success - green, info - blue
+        const parts = String(type).split(':')
+        return parts.length > 1
+          ? messages.map(({ body, heading }) => (
+              <li key={body}>
+                <StyledStatusMessage colour={messageColours[parts[0]]}>
+                  <StyledHeading>{heading}</StyledHeading>
+                  <StyledBody dangerouslySetInnerHTML={{ __html: body }} />
+                </StyledStatusMessage>
+              </li>
+            ))
+          : messages.map((body, i) => (
+              <li key={i}>
+                <StyledStatusMessage colour={messageColours[type]}>
+                  <StyledMessage dangerouslySetInnerHTML={{ __html: body }} />
+                </StyledStatusMessage>
+              </li>
+            ))
+      })}
+    </UnorderedList>
+  ) : null
 }
 
 FlashMessages.propTypes = {
