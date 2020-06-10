f7b266db4b42d6fdc18499bab7046241cc82781e
Pippo Raimondi <filipporai@gmail.com>
2020-06-10 14:00:50 +0100

    Add put feature flag endpoint

--- test/sandbox/routes/v3/feature-flag/feature-flag.js
+++ test/sandbox/routes/v3/feature-flag/feature-flag.js
@@ -1,5 +1,17 @@
 var featureFlag = require('../../../fixtures/v3/feature-flag/feature-flag.json')
 
+let updatedFeatureFlag = null
+
 exports.featureFlag = function(req, res) {
+  if (updatedFeatureFlag) {
+    return res.json(updatedFeatureFlag)
+  }
   res.json(featureFlag)
+}
+
+exports.updateFeatureFlag = (req, res) => {
+  updatedFeatureFlag = featureFlag.map((flag) =>
+    flag.code === req.body.code ? { ...req.body } : flag
+  )
+  res.json(updatedFeatureFlag)
 }
