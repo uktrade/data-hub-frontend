2e28c1b5c737ea3aef0f57c64d1e1eba5eb611a9
Paul Gain <paul@javexconsulting.com>
2020-06-15 12:06:37 +0100

    Add DA Investment Project interaction e2e test

---
+++ test/end-to-end/cypress/specs/DA/add-interaction-spec.js
@@ -0,0 +1,72 @@
+const { investments } = require('../../../../../src/lib/urls')
+const fixtures = require('../../fixtures')
+const selectors = require('../../../../selectors')
+
+describe('DA add Investment Project interaction', () => {
+  context('The browser URL has updated', () => {
+    before(() => {
+      cy.visit(
+        investments.projects.interactions.index(
+          fixtures.investmentProject.newGolfCourse.id
+        )
+      )
+    })
+
+    it('should have the correct url', () => {
+      cy.get('[data-auto-id="Add interaction"]')
+        .click()
+        .location('pathname')
+        .should(
+          'eq',
+          investments.projects.interactions.createType(
+            fixtures.investmentProject.newGolfCourse.id,
+            'investment',
+            'interaction'
+          )
+        )
+    })
+  })
+
+  context('DA completes the form and clicks "Add interaction"', () => {
+    before(() => {
+      cy.server()
+        .route('POST', '/api-proxy/v3/interaction')
+        .as('post')
+
+      cy.visit(
+        investments.projects.interactions.createType(
+          fixtures.investmentProject.newGolfCourse.id,
+          'investment',
+          'interaction'
+        )
+      )
+    })
+
+    it('should add an interaction', () => {
+      const subject = 'The best Investment Project interaction'
+      const formSelectors = selectors.interactionForm
+
+      cy.contains('Add interaction for Mars Exports Ltd')
+        .get(formSelectors.service)
+        .select('Investment - Services')
+        .get(formSelectors.contact)
+        .selectTypeaheadOption('Fred Peterson')
+        .get(formSelectors.communicationChannel)
+        .selectTypeaheadOption('Email/Website')
+        .get(formSelectors.subject)
+        .type(subject)
+        .get(formSelectors.notes)
+        .type('Conversation about Investment services')
+        .get(formSelectors.policyFeedbackNo)
+        .click()
+        .get(formSelectors.add)
+        .click()
+        .wait('@post')
+        .should((xhr) => {
+          expect(xhr.status, 'successful POST').to.equal(201)
+        })
+
+      cy.contains('h1', subject)
+    })
+  })
+})
