8db20f1bfb64f1a9545916c037450a2d4305fce7
Pippo Raimondi <filipporai@gmail.com>
2020-06-16 10:44:52 +0100

    Add reset feature flag capability as suggest in PR

--- test/cypress/plugins/index.js
+++ test/cypress/plugins/index.js
@@ -4,6 +4,6 @@ require('dotenv').config()
 module.exports = (on, config) => {
   on('task', require('@cypress/code-coverage/task'))
   on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))
-  config.env.sandbox_url = process.env.API_ROOT || config.env.sandbox_url
+  config.env.sandbox_url = process.env.API_ROOT
   return config
 }
