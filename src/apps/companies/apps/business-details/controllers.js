59fd53bdee2270c37cd796f5a25bd0ebc596a083
Rachael Robinson <rachaelcodes@users.noreply.github.com>
2020-06-16 16:49:16 +0100

    Update One List Form view permission

--- src/apps/companies/apps/business-details/controllers.js
+++ src/apps/companies/apps/business-details/controllers.js
@@ -10,6 +10,7 @@ function canEditOneList(permissions) {
   return (
     permissions &&
     permissions.includes('company.change_company') &&
+    permissions.includes('company.change_one_list_core_team_member') &&
     permissions.includes(
       'company.change_one_list_tier_and_global_account_manager'
     )
