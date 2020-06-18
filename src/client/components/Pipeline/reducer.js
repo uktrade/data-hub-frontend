1b61c26a7005a9821acdd2399fcdccb22b8b1f2b
Adam L Edwards <adam@aeweb.io>
2020-06-18 15:05:29 +0100

    Add filter option to redux state

--- src/client/components/Pipeline/reducer.js
+++ src/client/components/Pipeline/reducer.js
@@ -1,11 +1,28 @@
-import { PIPELINE__LIST_LOADED } from '../../actions'
+import {
+  PIPELINE__LIST_LOADED,
+  PIPELINE__FILTER_ARCHIVE_CHANGED,
+} from '../../actions'
 
-export default (state = {}, { type, result }) => {
+const initialState = {
+  filter: {
+    includeArchive: false,
+  },
+}
+
+export default (state = initialState, { type, result }) => {
   switch (type) {
     case PIPELINE__LIST_LOADED:
       return {
         ...state,
         items: result,
+      }
+    case PIPELINE__FILTER_ARCHIVE_CHANGED:
+      return {
+        ...state,
+        filter: {
+          ...state.filter,
+          ...result,
+        },
       }
     default:
       return state
