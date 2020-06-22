e9e84ce24c3fb408557d85c28e93ce2c1b47ef09
David Matthews <dwm503@gmail.com>
2020-06-22 13:22:06 +0100

    feature: move company and created_on to meta list and restyle header

--- src/client/components/Pipeline/PipelineItem.jsx
+++ src/client/components/Pipeline/PipelineItem.jsx
@@ -6,18 +6,23 @@ import Button from '@govuk-react/button'
 import Link from '@govuk-react/link'
 import GridRow from '@govuk-react/grid-row'
 import GridCol from '@govuk-react/grid-col'
-import { SPACING, MEDIA_QUERIES } from '@govuk-react/constants'
+import { SPACING, MEDIA_QUERIES, FONT_SIZE } from '@govuk-react/constants'
 import { BLUE } from 'govuk-colours'
 import {
   Card,
   CardDetailsList,
-  CardHeader,
 } from 'data-hub-components/dist/activity-feed/activities/card'
 import { NumberUtils } from 'data-hub-components'
 
 import urls from '../../../lib/urls'
 import Tag from '../Tag'
 import LIKELIHOOD_TO_SUCCEED from './constants'
+
+const StyledH3 = styled('h3')`
+  font-size: ${FONT_SIZE.SIZE_27};
+  font-weight: bold;
+  margin-bottom: ${SPACING.SCALE_2};
+`
 
 const StyledGridCol = styled(GridCol)`
   display: flex;
@@ -36,6 +41,9 @@ const StyledGridValue = styled(GridCol)`
   margin-top: ${SPACING.SCALE_1};
   ${MEDIA_QUERIES.TABLET} {
     margin-top: 0;
+  }
+  a {
+    text-decoration-line: none;
   }
 `
 const StyledGridLabel = styled(StyledGridValue)`
@@ -68,51 +76,63 @@ const StyledTagSpacing = styled.span`
 `
 
 function buildMetaList({
+  company,
   potential_value,
   sector,
   contact,
   expected_win_date,
+  created_on,
 }) {
   const list = [
-    sector && { id: 0, label: 'Project sector', value: sector.segment },
-    contact && { id: 1, label: 'Company contact', value: contact.name },
+    {
+      id: 0,
+      label: 'Company',
+      value: company.name,
+      href: urls.companies.detail(company.id),
+    },
+    sector && { id: 1, label: 'Project sector', value: sector.segment },
+    contact && { id: 2, label: 'Company contact', value: contact.name },
     potential_value && {
-      id: 2,
+      id: 3,
       label: 'Potential export value',
       value: NumberUtils.currencyGBP(potential_value),
     },
     expected_win_date && {
-      id: 3,
+      id: 4,
       label: 'Expected date for win',
       value: moment(expected_win_date).format('MMM Y'),
+    },
+    {
+      id: 5,
+      label: 'Created',
+      value: moment(created_on).format('DD MMM Y'),
     },
   ]
   // remove falsy values
   return list.filter(Boolean)
 }
 
-const PipelineItemMeta = ({ label, value }) => (
+const PipelineItemMeta = ({ label, value, href }) => (
   <GridRow>
     <StyledGridLabel>{label}</StyledGridLabel>
-    <StyledGridValue>{value}</StyledGridValue>
+    {href ? (
+      <StyledGridValue>
+        <Link href={href}>{value}</Link>
+      </StyledGridValue>
+    ) : (
+      <StyledGridValue>{value}</StyledGridValue>
+    )}
   </GridRow>
 )
-const PipelineItem = ({
-  item: { id, name, company, created_on, likelihood_to_win, ...meta },
-}) => (
+
+const PipelineItem = ({ item: { id, name, likelihood_to_win, ...meta } }) => (
   <Card>
-    <CardHeader
-      company={{ name }}
-      heading={
-        <Link href={urls.companies.detail(company.id)}>{company.name}</Link>
-      }
-      startTime={created_on}
-    />
+    <StyledH3>{name}</StyledH3>
     <StyledGridRow>
       <GridCol>
         <CardDetailsList
           itemRenderer={(metaItem) => <PipelineItemMeta {...metaItem} />}
-          items={buildMetaList(meta)}
+          items={buildMetaList({ ...meta })}
         />
       </GridCol>
       <StyledGridCol>
