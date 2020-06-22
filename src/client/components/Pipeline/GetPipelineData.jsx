f4cc67d5ae9f51d271005a6b54e2f13f338dbea9
Adam L Edwards <adam@aeweb.io>
2020-06-22 19:00:18 +0100

    Add GetPiplineData component

---
+++ src/client/components/Pipeline/GetPipelineData.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+
+import styled from 'styled-components'
+
+import { spacing } from '@govuk-react/lib'
+
+import Task from '../Task'
+import { ID as STATE_ID, TASK_GET_PIPELINE_LIST } from './state'
+import { PIPELINE__LIST_LOADED } from '../../actions'
+
+import ProgressIndicator from '../ProgressIndicator'
+import ErrorSummary from '@govuk-react/error-summary'
+
+const StyledError = styled.div`
+  ${spacing.responsive({
+    size: 3,
+    property: 'margin',
+    direction: ['top'],
+  })}
+`
+
+function PipelineStates({ items, children, task, status, includeArchive }) {
+  const { progress, error, errorMessage } = task
+
+  React.useEffect(() => {
+    task.start({
+      payload: { status, ...(!includeArchive && { archived: false }) },
+      onSuccessDispatch: PIPELINE__LIST_LOADED,
+    })
+  }, [status, includeArchive])
+
+  if (error) {
+    return (
+      <StyledError>
+        <ErrorSummary
+          heading="There is a problem"
+          description={`Error: ${errorMessage}`}
+          errors={[]}
+        />
+      </StyledError>
+    )
+  }
+
+  if (progress && !items) {
+    return <ProgressIndicator message="Loading projects..." />
+  }
+
+  return !!items && children({ items, progress })
+}
+
+const GetPipeLineData = ({ status, lists, children, filter }) => {
+  const { includeArchive } = filter
+  return (
+    <>
+      <Task>
+        {(getTask) => {
+          return (
+            <PipelineStates
+              items={lists[status]}
+              status={status}
+              includeArchive={includeArchive}
+              task={getTask(TASK_GET_PIPELINE_LIST, `${STATE_ID}_${status}`)}
+            >
+              {children}
+            </PipelineStates>
+          )
+        }}
+      </Task>
+    </>
+  )
+}
+
+export default GetPipeLineData
