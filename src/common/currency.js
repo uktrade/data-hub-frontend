693d4ebfd342b9f14bc55958e73f1430e7faf2f8
Paul Gain <paul@javexconsulting.com>
2020-06-25 14:41:42 +0100

    Convert Annual Turnover from USD to GBP (#2775)
    * Bump data-hub-components
    
    * Add GBPUSD exchange rate
    
    * Convert GBP to USD
    
    * Convert USD to GBP and round to 2 significant figures
    
    * Update test
    
    * Bump data-hub-components
    
    * Updatd yarn lock file
    
    * Update Annual Turnover
    
    * Add edit company functional test

--- src/common/currency.js
+++ src/common/currency.js
@@ -1,5 +1,8 @@
 const { isNumber } = require('lodash')
-const { EXCHANGE_RATE_USD_TO_GBP } = require('./constants')
+const {
+  EXCHANGE_RATE_USD_TO_GBP,
+  EXCHANGE_RATE_GBP_TO_USD,
+} = require('./constants')
 
 const convertUsdToGbp = (usd) => {
   if (!isNumber(usd)) {
@@ -9,6 +12,15 @@ const convertUsdToGbp = (usd) => {
   return usd * EXCHANGE_RATE_USD_TO_GBP
 }
 
+const convertGbpToUsd = (gbp) => {
+  if (!isNumber(gbp)) {
+    return gbp
+  }
+
+  return gbp * EXCHANGE_RATE_GBP_TO_USD
+}
+
 module.exports = {
   convertUsdToGbp,
+  convertGbpToUsd,
 }
