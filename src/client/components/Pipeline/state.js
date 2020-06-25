f519551ac8d478765d80a2a1f3522ed768dd3608
Adam L Edwards <adam@aeweb.io>
2020-06-25 20:40:28 +0100

    Update store for sort state

--- src/client/components/Pipeline/state.js
+++ src/client/components/Pipeline/state.js
@@ -1,4 +1,4 @@
-import { PIPELINE__FILTER_ARCHIVE_CHANGED } from '../../actions'
+import { PIPELINE__LIST_FILTER_SORT_CHANGED } from '../../actions'
 
 export const ID = 'pipelineList'
 export const TASK_GET_PIPELINE_LIST = 'TASK_GET_PIPELINE_LIST'
@@ -6,10 +6,16 @@ export const TASK_GET_PIPELINE_LIST = 'TASK_GET_PIPELINE_LIST'
 export const state2props = (state) => state[ID]
 
 const updateArchiveFilter = (includeArchive) => ({
-  type: PIPELINE__FILTER_ARCHIVE_CHANGED,
+  type: PIPELINE__LIST_FILTER_SORT_CHANGED,
   result: { includeArchive },
+})
+
+const updateSort = (sortBy) => ({
+  type: PIPELINE__LIST_FILTER_SORT_CHANGED,
+  result: { sortBy },
 })
 
 export const dispatchToProps = {
   updateArchiveFilter,
+  updateSort,
 }
