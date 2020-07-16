15dbce13305a7b8101f6c67c23bb50d4e7ed201e
David Matthews <dwm503@gmail.com>
2020-07-16 17:08:58 +0100

    chore: remove uglifiy config as it is now default behaviour

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -12,16 +12,6 @@ module.exports = {
         NODE_ENV: JSON.stringify('production'),
       },
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-      },
-      output: {
-        comments: false,
-      },
-      sourceMap: true,
-      dead_code: true,
-    }),
     new ExtractTextPlugin('css/[name].[contenthash:8].css'),
   ],
 }
