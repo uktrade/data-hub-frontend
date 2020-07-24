d0359ec1cf3540d9f45e4ad34ed8599a2a3d73aa
Lauren <laurenqurashi@me.com>
2020-07-24 15:39:43 +0100

    Add CompanyLists component to Datahub and amend import locations

---
+++ src/client/components/CompanyLists/CreateListForm.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import Button from '@govuk-react/button'
+import Link from '@govuk-react/link'
+import PropTypes from 'prop-types'
+import { FormStateful, FieldInput, FormActions } from 'data-hub-components'
+
+const CreateListForm = ({
+  name,
+  label,
+  hint,
+  maxLength,
+  onSubmitHandler,
+  cancelUrl,
+}) => {
+  const cancelLink = `${cancelUrl}`
+  return (
+    <FormStateful onSubmit={onSubmitHandler}>
+      <FieldInput
+        name={name}
+        type="text"
+        label={label}
+        required="Enter a name for your list"
+        hint={hint}
+        validate={(value) =>
+          value && value.length > maxLength
+            ? `Enter list name which is no longer than ${maxLength} characters`
+            : null
+        }
+      />
+      <FormActions>
+        <Button>Create list</Button>
+        <Link href={cancelLink}>Cancel</Link>
+      </FormActions>
+    </FormStateful>
+  )
+}
+
+CreateListForm.propTypes = {
+  onSubmitHandler: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  hint: PropTypes.string,
+  cancelUrl: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  maxLength: PropTypes.number.isRequired,
+}
+
+CreateListForm.defaultProps = {
+  hint: '',
+}
+
+export default CreateListForm
