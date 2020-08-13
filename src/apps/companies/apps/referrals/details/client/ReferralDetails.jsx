a6245a0a0da0cd50f050566b12c325811a962969
David Matthews <dwm503@gmail.com>
2020-08-13 10:14:02 +0100

    chore: remove company state check from referral details

--- src/apps/companies/apps/referrals/details/client/ReferralDetails.jsx
+++ src/apps/companies/apps/referrals/details/client/ReferralDetails.jsx
@@ -63,89 +63,84 @@ export default connect(({ referrerUrl, ...state }) => ({
           onSuccessDispatch: REFERRAL_DETAILS,
         }}
       >
-        {() =>
-          company && (
-            <>
-              <SummaryTable caption={subject}>
-                <SummaryTable.Row heading="Company">
-                  <Link href={urls.companies.detail(company.id)}>
-                    {company.name}
+        {() => (
+          <>
+            <SummaryTable caption={subject}>
+              <SummaryTable.Row heading="Company">
+                <Link href={urls.companies.detail(company.id)}>
+                  {company.name}
+                </Link>
+              </SummaryTable.Row>
+              {contact && (
+                <SummaryTable.Row heading="Contact">
+                  <Link href={urls.contacts.contact(contact.id)}>
+                    {contact.name}
                   </Link>
                 </SummaryTable.Row>
-                {contact && (
-                  <SummaryTable.Row heading="Contact">
-                    <Link href={urls.contacts.contact(contact.id)}>
-                      {contact.name}
-                    </Link>
-                  </SummaryTable.Row>
-                )}
-                <SummaryTable.Row heading="Sending adviser">
-                  {sendingAdviser && <AdviserDetails {...sendingAdviser} />}
-                </SummaryTable.Row>
-                <SummaryTable.Row heading="Receiving adviser">
-                  {receivingAdviser && <AdviserDetails {...receivingAdviser} />}
-                </SummaryTable.Row>
-                <SummaryTable.Row heading="Date of referral">
-                  {DateUtils.format(date)}
-                </SummaryTable.Row>
-                <SummaryTable.Row heading="Notes">{notes}</SummaryTable.Row>
-              </SummaryTable>
-              {completed ? (
-                <SummaryTable caption="Referral accepted">
-                  <SummaryTable.Row heading="Date">
-                    {DateUtils.format(completed.on)}
-                  </SummaryTable.Row>
-                  <SummaryTable.Row heading="By">
-                    <AdviserDetails {...completed.by} />
-                  </SummaryTable.Row>
-                  <SummaryTable.Row heading="With interaction">
-                    <Link href={urls.interactions.detail(interaction.id)}>
-                      {interaction.subject}
-                    </Link>
-                  </SummaryTable.Row>
-                </SummaryTable>
-              ) : (
-                <>
-                  <Details summary="Why can I not edit the referral?">
-                    <p>
-                      For now, you can't edit the referral once it's been sent.
-                    </p>
-                    <p>Contact the recipient if something's changed.</p>
-                  </Details>
-                  <FormActions>
-                    <Button
-                      as={Link}
-                      href={urls.companies.referrals.interactions.create(
-                        company.id,
-                        referralId
-                      )}
-                    >
-                      Accept referral
-                    </Button>
-                    <SecondaryButton
-                      as={Link}
-                      href={urls.companies.referrals.help(
-                        company.id,
-                        referralId
-                      )}
-                    >
-                      I cannot accept the referral
-                    </SecondaryButton>
-                    <Link
-                      href={
-                        cameFromHomePage
-                          ? urls.companies.referrals.list()
-                          : urls.companies.detail(company.id)
-                      }
-                    >
-                      Back
-                    </Link>
-                  </FormActions>
-                </>
               )}
-            </>
-          )
-        }
+              <SummaryTable.Row heading="Sending adviser">
+                {sendingAdviser && <AdviserDetails {...sendingAdviser} />}
+              </SummaryTable.Row>
+              <SummaryTable.Row heading="Receiving adviser">
+                {receivingAdviser && <AdviserDetails {...receivingAdviser} />}
+              </SummaryTable.Row>
+              <SummaryTable.Row heading="Date of referral">
+                {DateUtils.format(date)}
+              </SummaryTable.Row>
+              <SummaryTable.Row heading="Notes">{notes}</SummaryTable.Row>
+            </SummaryTable>
+            {completed ? (
+              <SummaryTable caption="Referral accepted">
+                <SummaryTable.Row heading="Date">
+                  {DateUtils.format(completed.on)}
+                </SummaryTable.Row>
+                <SummaryTable.Row heading="By">
+                  <AdviserDetails {...completed.by} />
+                </SummaryTable.Row>
+                <SummaryTable.Row heading="With interaction">
+                  <Link href={urls.interactions.detail(interaction.id)}>
+                    {interaction.subject}
+                  </Link>
+                </SummaryTable.Row>
+              </SummaryTable>
+            ) : (
+              <>
+                <Details summary="Why can I not edit the referral?">
+                  <p>
+                    For now, you can't edit the referral once it's been sent.
+                  </p>
+                  <p>Contact the recipient if something's changed.</p>
+                </Details>
+                <FormActions>
+                  <Button
+                    as={Link}
+                    href={urls.companies.referrals.interactions.create(
+                      company.id,
+                      referralId
+                    )}
+                  >
+                    Accept referral
+                  </Button>
+                  <SecondaryButton
+                    as={Link}
+                    href={urls.companies.referrals.help(company.id, referralId)}
+                  >
+                    I cannot accept the referral
+                  </SecondaryButton>
+                  <Link
+                    href={
+                      cameFromHomePage
+                        ? urls.companies.referrals.list()
+                        : urls.companies.detail(company.id)
+                    }
+                  >
+                    Back
+                  </Link>
+                </FormActions>
+              </>
+            )}
+          </>
+        )}
       </Task.Status>
     )
   }
