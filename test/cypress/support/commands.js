abd514178f3fbc75f26423d4960cf51edf23be0f
elcct <pawel@mikesz.com>
2020-09-14 09:19:43 +0100

    Add Cypress commands for testfixtureapi.

--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -178,6 +178,26 @@ Cypress.Commands.add('resetFeatureFlags', () => {
   return cy.request('POST', `${backend_url}${uri}`)
 })
 
+Cypress.Commands.add('reseed', () => {
+  backend_url = Cypress.env('sandbox_url')
+  uri = '/testfixtureapi/reset-fixtures/'
+  return cy.request('POST', `${backend_url}${uri}`)
+})
+
+Cypress.Commands.add('createUser', (userData) => {
+  backend_url = Cypress.env('sandbox_url')
+  uri = '/testfixtureapi/create-user/'
+  return cy.request('POST', `${backend_url}${uri}`, userData)
+})
+
+Cypress.Commands.add('loadFixture', (fixture) => {
+  backend_url = Cypress.env('sandbox_url')
+  uri = '/testfixtureapi/load-fixture/'
+  return cy.request('POST', `${backend_url}${uri}`, {
+    fixture: fixture,
+  })
+})
+
 // This command helps us to check colours in cypress as cypress always return rgb, and our govuk-colours library uses hexes.
 const compareColor = (color, property) => (targetElement) => {
   const tempElement = document.createElement('div')
