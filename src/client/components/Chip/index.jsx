e8d1279c77a634789cb80944f7c7e94b3bfba81b
Ian Leggett <contactleggett@gmail.com>
2020-11-02 13:08:20 +0000

    Replace units with variables

--- src/client/components/Chip/index.jsx
+++ src/client/components/Chip/index.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import { GREY_3 } from 'govuk-colours'
+import { SPACING } from '@govuk-react/constants'
 
 const StyledSpan = styled('span')`
   display: inline-block;
   padding: 12px;
   margin: 4px;
   background-color: ${GREY_3};
-  border-radius: 10px;
+  border-radius: ${SPACING.SCALE_2};
 `
 
 const Chip = ({ label }) => <StyledSpan>{label}</StyledSpan>
