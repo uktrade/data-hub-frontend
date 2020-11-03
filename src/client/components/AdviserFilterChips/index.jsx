445d565e8653485066c8d27782c1a55a2f26dcb3
Ian Leggett <contactleggett@gmail.com>
2020-11-03 11:59:06 +0000

    Consolidate map into one

--- src/client/components/AdviserFilterChips/index.jsx
+++ src/client/components/AdviserFilterChips/index.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import { Chip } from '../../components'
 
 const AdviserFilterChips = ({ selectedAdvisers }) => {
-  const advisers = selectedAdvisers.map(({ advisers }) => advisers)
   return (
     <>
-      {advisers.map(({ name, id }) => (
-        <Chip label={name} key={id} />
+      {selectedAdvisers.map(({ advisers: { name, id } }) => (
+        <Chip key={id}>{name}</Chip>
       ))}
     </>
   )
