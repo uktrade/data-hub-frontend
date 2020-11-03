93f540ed0b5a95a1379c9126ce7e1a2619003edf
Ian Leggett <contactleggett@gmail.com>
2020-11-03 16:34:09 +0000

    Remove explicit ref to return jsx with children

--- src/client/components/Chip/index.jsx
+++ src/client/components/Chip/index.jsx
@@ -1,21 +1,13 @@
-import React from 'react'
 import styled from 'styled-components'
-import PropTypes from 'prop-types'
 import { GREY_3 } from 'govuk-colours'
 import { SPACING } from '@govuk-react/constants'
 
-const StyledSpan = styled('span')`
+const Chip = styled('span')`
   display: inline-block;
   padding: 12px;
   margin: 4px;
   background-color: ${GREY_3};
   border-radius: ${SPACING.SCALE_2};
 `
-
-const Chip = ({ children }) => <StyledSpan>{children}</StyledSpan>
-
-Chip.propTypes = {
-  children: PropTypes.string.isRequired,
-}
 
 export default Chip
