fe2837db14b3aef7880df07eb70c5dbfc1407357
Tom Kingston <tom@kingstonlabs.co.uk>
2020-11-17 16:21:00 +0000

    Added react land date before and after

---
+++ src/client/components/RoutedDateField/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+import qs from 'qs'
+import { get } from 'lodash'
+
+import DateField from '../DateField'
+
+const RoutedDateField = ({ qsParamName, ...props }) => (
+  <Route>
+    {({ location, history }) => {
+      const qsParams = qs.parse(location.search.slice(1))
+      return (
+        <DateField
+          {...props}
+          onChange={(e) => {
+            history.push({
+              search: qs.stringify({
+                ...qsParams,
+                [qsParamName]: e.target.value,
+              }),
+            })
+          }}
+          initialValue={get(qsParams, qsParamName, '')}
+        />
+      )
+    }}
+  </Route>
+)
+
+export default RoutedDateField
