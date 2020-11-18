c43fc686d9d610b70f183cf91b9b6d1a4b25b977
Rachael Robinson <rachaelcodes@users.noreply.github.com>
2020-11-18 15:21:13 +0000

    Add a typeahead filter for sectors

--- src/apps/investments/client/projects/state.js
+++ src/apps/investments/client/projects/state.js
@@ -1,27 +1,45 @@
 import qs from 'qs'
 
+import { sectorOptions } from './labels'
+
 export const TASK_GET_PROJECTS_LIST = 'TASK_GET_PROJECTS_LIST'
 export const TASK_GET_ADVISER_NAME = 'TASK_GET_ADVISER_NAME'
 
 export const ID = 'projectsList'
 
-const collectionListPayload = ({
+const parseVariablePropType = (prop) =>
+  prop ? (Array.isArray(prop) ? prop : [prop]) : prop
+
+const searchParamProps = ({
   adviser = false,
+  // snake-case as comes from the query
+  sector_descends = false,
   sortby = 'created_on:desc',
   page = 1,
-}) => {
-  const searchParamProps = {
-    adviser: adviser ? (Array.isArray(adviser) ? adviser : [adviser]) : adviser,
-    sortby,
-    page,
-  }
+}) => ({
+  adviser: parseVariablePropType(adviser),
+  sector_descends: parseVariablePropType(sector_descends),
+  sortby,
+  page,
+})
 
+const collectionListPayload = (paramProps) => {
   return Object.fromEntries(
-    Object.entries(searchParamProps).filter((v) => v[1])
+    Object.entries(searchParamProps(paramProps)).filter((v) => v[1])
   )
 }
 
-export const state2props = ({ router, ...state }) => ({
-  ...state[ID],
-  payload: collectionListPayload(qs.parse(router.location.search.slice(1))),
+const getOptionMetadata = ({ sector_descends }) => ({
+  sectors: sector_descends
+    ? sectorOptions.filter((option) => sector_descends.includes(option.value))
+    : [],
 })
+
+export const state2props = ({ router, ...state }) => {
+  const queryProps = qs.parse(router.location.search.slice(1))
+  return {
+    ...state[ID],
+    payload: collectionListPayload(queryProps),
+    optionMetadata: getOptionMetadata(queryProps),
+  }
+}
