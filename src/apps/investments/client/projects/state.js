0325d2d8308c255875c62cc8440500c2829e2400
Ian Leggett <contactleggett@gmail.com>
2020-11-25 13:16:12 +0000

    Add call to action to the remaining filters chips

--- src/apps/investments/client/projects/state.js
+++ src/apps/investments/client/projects/state.js
@@ -1,6 +1,11 @@
 import qs from 'qs'
 
-import { sectorOptions } from './labels'
+import {
+  sortOptions,
+  sectorOptions,
+  estimatedLandDateBefore,
+  estimatedLandDateAfter,
+} from './metadata'
 
 export const TASK_GET_PROJECTS_LIST = 'TASK_GET_PROJECTS_LIST'
 export const TASK_GET_ADVISER_NAME = 'TASK_GET_ADVISER_NAME'
@@ -11,13 +16,13 @@ const parseVariablePropType = (prop) =>
   prop ? (Array.isArray(prop) ? prop : [prop]) : prop
 
 const searchParamProps = ({
+  sortby = 'created_on:desc',
+  page = 1,
   adviser = false,
   // snake-case as comes from the query
   sector_descends = false,
-  sortby = 'created_on:desc',
   estimated_land_date_before = null,
   estimated_land_date_after = null,
-  page = 1,
 }) => ({
   adviser: parseVariablePropType(adviser),
   sector_descends: parseVariablePropType(sector_descends),
@@ -27,29 +32,51 @@ const searchParamProps = ({
   page,
 })
 
+// Filters out falsey values
 const collectionListPayload = (paramProps) => {
   return Object.fromEntries(
     Object.entries(searchParamProps(paramProps)).filter((v) => v[1])
   )
 }
-
-const getOptionMetadata = ({
-  sector_descends,
-  estimated_land_date_before,
-  estimated_land_date_after,
-}) => ({
-  sectors: sector_descends
-    ? sectorOptions.filter((option) => sector_descends.includes(option.value))
-    : [],
-  estimated_land_date_before,
-  estimated_land_date_after,
-})
+// Gets the label or name of the selected filter
+const getFilterLabel = (metadataOptions, filterProp) =>
+  metadataOptions.filter((option) => filterProp.includes(option.value))
 
 export const state2props = ({ router, ...state }) => {
+  // Get the query props if they exist
   const queryProps = qs.parse(router.location.search.slice(1))
+
+  // Get the filtered props
+  const filteredQueryProps = collectionListPayload(queryProps)
+
+  // If they exist we deconstruct them, if no prop then set a default
+  const {
+    sector_descends = [],
+    estimated_land_date_before,
+    estimated_land_date_after,
+  } = queryProps
+
+  // Set the selected filters
+  const selectedFilters = {
+    selectedSectors: getFilterLabel(sectorOptions, sector_descends),
+    selectedEstimatedLandDatesBefore: [
+      {
+        label: estimatedLandDateBefore,
+        value: estimated_land_date_before,
+      },
+    ],
+    selectedEstimatedLandDatesAfter: [
+      {
+        label: estimatedLandDateAfter,
+        value: estimated_land_date_after,
+      },
+    ],
+  }
+
   return {
     ...state[ID],
-    payload: collectionListPayload(queryProps),
-    optionMetadata: getOptionMetadata(queryProps),
+    payload: filteredQueryProps,
+    optionMetadata: { sortOptions, sectorOptions, estimatedLandDateBefore },
+    selectedFilters,
   }
 }
