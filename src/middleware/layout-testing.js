20f5d5d31c40a9b0988d7bc26b6d1d41ccb6e299
Ian Leggett <contactleggett@gmail.com>
2020-12-07 10:15:30 +0000

    Log out warning if more than one layoutTesting feature flag is used

--- src/middleware/layout-testing.js
+++ src/middleware/layout-testing.js
@@ -1,30 +1,28 @@
 const { isEmpty } = require('lodash')
+const logger = require('../config/logger')
 
 module.exports = (queryParam) => (req, res, next) => {
   const { features } = res.locals
   res.locals.isLayoutTesting = false
-  const layoutTestingFeature = Object.keys(features).filter((x) =>
-    x.includes('layoutTesting')
+  const layoutTestingFeature = Object.keys(features).filter((feature) =>
+    feature.includes('layoutTesting')
   )
 
+  layoutTestingFeature.length > 1 &&
+    logger.warn(
+      'You can ONLY have one "layoutTesting" feature flag set at a time otherwise only the first one in the list will be used. Deactivate or remove one from Django admin.'
+    )
+
   if (!isEmpty(layoutTestingFeature)) {
-    if (layoutTestingFeature.length > 1) {
-      throw new Error(
-        'You can only have one "layoutTesting" feature running. Remove one of the features from admin.'
-      )
-    } else {
-      const {
-        dit_team: { id },
-      } = res.locals.user
-      const [featureFlag] = layoutTestingFeature
-      const [, teamId] = featureFlag.split(':')
-      res.locals.isLayoutTesting = teamId === id
-      if (res.locals.isLayoutTesting && isEmpty(req.query)) {
-        return res.redirect(`${req.originalUrl}?layoutTesting=${queryParam}`)
-      }
-      next()
+    const {
+      dit_team: { id },
+    } = res.locals.user
+    const [featureFlag] = layoutTestingFeature
+    const [, teamId] = featureFlag.split(':')
+    res.locals.isLayoutTesting = teamId === id
+    if (res.locals.isLayoutTesting && isEmpty(req.query)) {
+      return res.redirect(`${req.originalUrl}?layoutTesting=${queryParam}`)
     }
-  } else {
     next()
   }
 }
