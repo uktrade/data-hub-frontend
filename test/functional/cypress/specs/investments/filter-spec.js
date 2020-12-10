f89ffc436c55ca6673a37e21082b098d51a6439a
Tom Kingston <tom@kingstonlabs.co.uk>
2020-12-10 14:45:53 +0000

    Add tests for checkboxes

--- test/functional/cypress/specs/investments/filter-spec.js
+++ test/functional/cypress/specs/investments/filter-spec.js
@@ -1,9 +1,11 @@
 import urls from '../../../../../src/lib/urls'
 
+const PROSPECT_STAGE_ID = '8a320cc9-ae2e-443e-9d26-2f36452c2ced'
 const PUCK_ADVISER_ID = 'e83a608e-84a4-11e6-ae22-56b6b6499611'
 const ADVANCED_ENGINEERING_SECTOR_ID = 'af959812-6095-e211-a939-e4115bead28a'
 const UK_COUNTRY_ID = '80756b9a-5d95-e211-a939-e4115bead28a'
 const SOUTH_EAST_UK_REGION_ID = '884cd12a-6095-e211-a939-e4115bead28a'
+const FDI_INVESTMENT_TYPE_ID = '3e143372-496c-4d1e-8278-6fdd3da9b48b'
 
 /**
  * Enter `input` into an advisers typeahead `element` and select the first result
@@ -20,6 +22,10 @@ const selectFirstAdvisersTypeaheadOption = ({ element, input }) =>
     cy.get('[class*="menu"] > div').click()
   })
 
+const clickCheckboxGroupOption = ({ element, value }) => {
+  cy.get(element).find(`input[value="${value}"]`).click()
+}
+
 /**
  * Enter `input` into a typeahead `element` and select the first result
  */
@@ -27,6 +33,27 @@ const selectFirstTypeaheadOption = ({ element, input }) => {
   cy.get(element).type(input)
   cy.get(element).find('[class*="menu"] > div').click()
   cy.get(element).click()
+}
+
+/**
+ * Assert that a checkbox is checked or unchecked
+ */
+const assertCheckboxGroupOption = ({ element, value, checked = true }) => {
+  const checkbox = cy.get(element).find(`input[value="${value}"]`)
+  if (checked) {
+    checkbox.should('be.checked')
+  } else {
+    checkbox.should('not.be.checked')
+  }
+}
+
+/**
+ * Assert that none of the options in a checkbox group are selected
+ */
+const assertCheckboxGroupNoneSelected = (element) => {
+  cy.get(element)
+    .find('input')
+    .each((child) => cy.wrap(child).should('not.be.checked'))
 }
 
 /**
@@ -91,6 +118,7 @@ describe('Investments Collections Filter', () => {
     cy.get('#field-sector').as('sectorFilter')
     cy.get('#field-country').as('countryFilter')
     cy.get('#field-uk_region').as('ukRegionFilter')
+    cy.get('#field-investment_type').as('investmentTypeFilter')
     cy.get('#field-estimated_land_date_before').as('estimatedDateBefore')
     cy.get('#field-estimated_land_date_after').as('estimatedDateAfter')
     cy.get('#field-actual_land_date_before').as('actualDateBefore')
@@ -120,6 +148,8 @@ describe('Investments Collections Filter', () => {
         .next()
         .should('have.id', 'field-uk_region')
         .next()
+        .should('have.id', 'field-investment_type')
+        .next()
         .should('have.id', 'field-estimated_land_date_before')
         .next()
         .should('have.id', 'field-estimated_land_date_after')
@@ -127,6 +157,22 @@ describe('Investments Collections Filter', () => {
         .should('have.id', 'field-actual_land_date_before')
         .next()
         .should('have.id', 'field-actual_land_date_after')
+    })
+
+    it('should filter by stage', () => {
+      clickCheckboxGroupOption({
+        element: '@stageFilter',
+        value: PROSPECT_STAGE_ID,
+      })
+      assertCheckboxGroupOption({
+        element: '@stageFilter',
+        value: PROSPECT_STAGE_ID,
+        checked: true,
+      })
+      cy.get('@stageFilter').should('contain', 'Prospect')
+      assertChipExists({ label: 'Prospect', position: 1 })
+
+      testRemoveChip({ element: '@stageFilter' })
     })
 
     it('should filter by advisers', () => {
@@ -141,9 +187,7 @@ describe('Investments Collections Filter', () => {
       })
       cy.get('@adviserFilter').should('contain', 'Puck Head')
       assertChipExists({ label: 'Puck Head', position: 1 })
-    })
 
-    it('should remove the advisers filter', () => {
       testRemoveChip({
         element: '@adviserFilter',
         placeholder: 'Search advisers',
@@ -158,9 +202,7 @@ describe('Investments Collections Filter', () => {
         input: 'adv',
         expectedOption: 'Advanced Engineering',
       })
-    })
 
-    it('should remove the sector filter', () => {
       testRemoveChip({
         element: '@sectorFilter',
         placeholder: 'Search sectors',
@@ -175,9 +217,7 @@ describe('Investments Collections Filter', () => {
         input: 'sin',
         expectedOption: 'Singapore',
       })
-    })
 
-    it('should remove the country filter', () => {
       testRemoveChip({
         element: '@countryFilter',
         placeholder: 'Search countries',
@@ -192,13 +232,27 @@ describe('Investments Collections Filter', () => {
         input: 'sou',
         expectedOption: 'South East',
       })
-    })
 
-    it('should remove the uk region filter', () => {
       testRemoveChip({
         element: '@ukRegionFilter',
         placeholder: 'Search UK regions',
       })
+    })
+
+    it('should filter by investment type', () => {
+      clickCheckboxGroupOption({
+        element: '@investmentTypeFilter',
+        value: FDI_INVESTMENT_TYPE_ID,
+      })
+      assertCheckboxGroupOption({
+        element: '@investmentTypeFilter',
+        value: FDI_INVESTMENT_TYPE_ID,
+        checked: true,
+      })
+      cy.get('@investmentTypeFilter').should('contain', 'FDI')
+      assertChipExists({ label: 'FDI', position: 1 })
+
+      testRemoveChip({ element: '@investmentTypeFilter' })
     })
 
     it('should filter the estimated land date before', () => {
@@ -212,9 +266,7 @@ describe('Investments Collections Filter', () => {
         label: 'Estimated land date before : 1 January 2020',
         position: 1,
       })
-    })
 
-    it('should remove the estimated land date before filter', () => {
       testRemoveChip({ element: '@estimatedDateBefore' })
     })
 
@@ -229,9 +281,7 @@ describe('Investments Collections Filter', () => {
         label: 'Estimated land date after : 2 January 2020',
         position: 1,
       })
-    })
 
-    it('should remove the estimated land date before filter', () => {
       testRemoveChip({ element: '@estimatedDateBefore' })
     })
 
@@ -246,9 +296,7 @@ describe('Investments Collections Filter', () => {
         label: 'Actual land date before : 1 February 2020',
         position: 1,
       })
-    })
 
-    it('should remove the actual land date before filter', () => {
       testRemoveChip({ element: '@actualDateBefore' })
     })
 
@@ -263,9 +311,7 @@ describe('Investments Collections Filter', () => {
         label: 'Actual land date after : 2 February 2020',
         position: 1,
       })
-    })
 
-    it('should remove the actual land date before filter', () => {
       testRemoveChip({ element: '@actualDateBefore' })
     })
   })
@@ -274,10 +320,12 @@ describe('Investments Collections Filter', () => {
     before(() => {
       cy.visit(urls.investments.projects.index(), {
         qs: {
+          stage: PROSPECT_STAGE_ID,
           adviser: PUCK_ADVISER_ID,
           sector_descends: ADVANCED_ENGINEERING_SECTOR_ID,
           country: UK_COUNTRY_ID,
           uk_region: SOUTH_EAST_UK_REGION_ID,
+          investment_type: FDI_INVESTMENT_TYPE_ID,
           estimated_land_date_before: '2020-01-01',
           estimated_land_date_after: '2020-01-02',
           actual_land_date_before: '2020-02-01',
@@ -286,38 +334,50 @@ describe('Investments Collections Filter', () => {
       })
     })
     it('should set the selected filter values and filter indicators', () => {
-      assertChipExists({ position: 1, label: 'Puck Head' })
+      assertChipExists({ position: 1, label: 'Prospect' })
+      assertCheckboxGroupOption({
+        element: '@stageFilter',
+        value: PROSPECT_STAGE_ID,
+        checked: true,
+      })
+      assertChipExists({ position: 2, label: 'Puck Head' })
       cy.get('@adviserFilter').should('contain', 'Puck Head')
-      assertChipExists({ position: 2, label: 'Advanced Engineering' })
+      assertChipExists({ position: 3, label: 'Advanced Engineering' })
       cy.get('@sectorFilter').should('contain', 'Advanced Engineering')
-      assertChipExists({ position: 3, label: 'United Kingdom' })
+      assertChipExists({ position: 4, label: 'United Kingdom' })
       cy.get('@countryFilter').should('contain', 'United Kingdom')
-      assertChipExists({ position: 4, label: 'South East' })
+      assertChipExists({ position: 5, label: 'South East' })
       cy.get('@ukRegionFilter').should('contain', 'South East')
+      assertChipExists({ position: 6, label: 'FDI' })
+      assertCheckboxGroupOption({
+        element: '@investmentTypeFilter',
+        value: FDI_INVESTMENT_TYPE_ID,
+        checked: true,
+      })
       assertChipExists({
         label: 'Estimated land date before : 1 January 2020',
-        position: 5,
+        position: 7,
       })
       cy.get('@estimatedDateBefore')
         .find('input')
         .should('have.attr', 'value', '2020-01-01')
       assertChipExists({
         label: 'Estimated land date after : 2 January 2020',
-        position: 6,
+        position: 8,
       })
       cy.get('@estimatedDateAfter')
         .find('input')
         .should('have.attr', 'value', '2020-01-02')
       assertChipExists({
         label: 'Actual land date before : 1 February 2020',
-        position: 7,
+        position: 9,
       })
       cy.get('@actualDateBefore')
         .find('input')
         .should('have.attr', 'value', '2020-02-01')
       assertChipExists({
         label: 'Actual land date after : 2 February 2020',
-        position: 8,
+        position: 10,
       })
       cy.get('@actualDateAfter')
         .find('input')
@@ -327,7 +387,7 @@ describe('Investments Collections Filter', () => {
     it('should clear all filters', () => {
       cy.get('#filter-chips').find('button').as('chips')
       cy.get('#clear-filters').as('clearFilters')
-      cy.get('@chips').should('have.length', 8)
+      cy.get('@chips').should('have.length', 10)
       cy.get('@clearFilters').click()
       cy.get('@chips').should('have.length', 0)
       cy.get('@estimatedDateBefore')
@@ -340,6 +400,8 @@ describe('Investments Collections Filter', () => {
       cy.get('@sectorFilter').should('contain', 'Search sectors...')
       cy.get('@countryFilter').should('contain', 'Search countries...')
       cy.get('@ukRegionFilter').should('contain', 'Search UK regions...')
+      assertCheckboxGroupNoneSelected('@stageFilter')
+      assertCheckboxGroupNoneSelected('@investmentTypeFilter')
     })
   })
 })
