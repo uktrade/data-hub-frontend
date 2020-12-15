779ca65c515f65926f7827e3e0ec49e93f47e186
Tom Kingston <tom@kingstonlabs.co.uk>
2020-12-15 11:51:53 +0000

    Refactor RoutedDownloadDataHeader into 2 components
    Splits out the component to ensure each part only does one thing.
    Storybook only demonstrates the lower level component as routing does
    not make sense in the context of storybook.

--- src/client/components/RoutedDownloadDataHeader/index.jsx
+++ src/client/components/RoutedDownloadDataHeader/index.jsx
@@ -2,62 +2,20 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import qs from 'qs'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
 import { isEmpty } from 'lodash'
 
-import { Button } from 'govuk-react'
-import { SPACING_POINTS } from '@govuk-react/constants'
+import DownloadDataHeader from '../DownloadDataHeader'
 
-import { decimal } from '../../utils/number-utils'
-import { CollectionHeaderRow } from '../../components'
-
-const StyledLink = styled('a')`
-  margin-bottom: 0;
-  margin-left: ${SPACING_POINTS[2]}px;
-`
-
-const RoutedDownloadDataHeader = ({
-  id = null,
-  count = 0,
-  maxItems = 5000,
-}) => {
-  if (!count) {
-    return null
-  }
-
-  if (count >= maxItems) {
-    return (
-      <CollectionHeaderRow id={id}>
-        Filter to fewer than {decimal(maxItems)} projects to download
-      </CollectionHeaderRow>
-    )
-  }
-
+const RoutedDownloadDataHeader = ({ baseDownloadLink, ...props }) => {
   return (
     <Route>
       {({ location }) => {
         const { page, ...qsParams } = qs.parse(location.search.slice(1))
-        let downloadLink = '/investments/projects/export'
+        let downloadLink = baseDownloadLink
         if (!isEmpty(qsParams)) {
           downloadLink += `?${qs.stringify({ ...qsParams })}`
         }
-
-        const downloadAction = (
-          <Button
-            key="download"
-            as={StyledLink}
-            href={downloadLink}
-            download={true}
-          >
-            Download
-          </Button>
-        )
-        return (
-          <CollectionHeaderRow id={id} actions={[downloadAction]}>
-            You can now download{' '}
-            {count === 1 ? 'this project' : `these ${count} projects`}
-          </CollectionHeaderRow>
-        )
+        return <DownloadDataHeader downloadLink={downloadLink} {...props} />
       }}
     </Route>
   )
--- test/functional/cypress/specs/investments/download-spec.js
+++ test/functional/cypress/specs/investments/download-spec.js
@@ -1,36 +1,45 @@
-const selectors = require('../../../../selectors')
-
 const { investments } = require('../../../../../src/lib/urls')
 
 describe('Investment Project Collections', () => {
-  before(() => {
-    cy.visit(investments.projects.index(), {
-      qs: { page: 2, estimated_land_date_before: '2020-11-19' },
+  context('when filters are applied', () => {
+    before(() => {
+      cy.visit(investments.projects.index(), {
+        qs: { page: 2, estimated_land_date_before: '2020-11-19' },
+      })
+      cy.get('[data-cy="download-data-header"]')
+        .as('downloadDataHeader')
+        .find('a')
+        .as('downloadDataButton')
+    })
+
+    it('should show download link', () => {
+      cy.get('@downloadDataHeader')
+        .should('have.length', 1)
+        .should('contain', 'You can now download these 12 projects')
+
+      cy.get('@downloadDataButton')
+        .should('have.length', 1)
+        .should('contain', 'Download')
+        .should(
+          'have.attr',
+          'href',
+          '/investments/projects/export?estimated_land_date_before=2020-11-19'
+        )
     })
   })
 
-  it('should show download link', () => {
-    cy.get(selectors.investment.header.downloadData)
-      .should('have.length', 1)
-      .should('contain', 'You can now download these 12 projects')
+  context('when over 5,000 projects are returned', () => {
+    before(() => {
+      cy.visit(investments.projects.index())
+      cy.get('[data-cy="download-data-header"]').as('downloadDataHeader')
+    })
 
-    cy.get(selectors.investment.header.downloadDataButton)
-      .should('have.length', 1)
-      .should('contain', 'Download')
-      .should(
-        'have.attr',
-        'href',
-        '/investments/projects/export?estimated_land_date_before=2020-11-19'
-      )
-  })
+    it('should show "filter to fewer than 5,000"', () => {
+      cy.get('@downloadDataHeader')
+        .should('have.length', 1)
+        .should('contain', 'Filter to fewer than 5,000 projects to download')
 
-  it('should show "filter to fewer than 5,000" when too many projects', () => {
-    cy.visit(investments.projects.index())
-
-    cy.get(selectors.investment.header.downloadData)
-      .should('have.length', 1)
-      .should('contain', 'Filter to fewer than 5,000 projects to download')
-
-    cy.get(selectors.investment.header.downloadDataButton).should('not.exist')
+      cy.get('@downloadDataHeader').find('a').should('not.exist')
+    })
   })
 })
