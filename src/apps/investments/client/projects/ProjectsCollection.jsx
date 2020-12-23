de6d2cad0334ff1d8c7aeb0e6f94bfca1e5d8819
Ian Leggett <contactleggett@gmail.com>
2020-12-23 14:04:26 +0000

    Remove the toggle section component

--- src/apps/investments/client/projects/ProjectsCollection.jsx
+++ src/apps/investments/client/projects/ProjectsCollection.jsx
@@ -6,7 +6,6 @@ import {
   RoutedAdvisersTypeahead,
   RoutedTypeahead,
   CollectionFilters,
-  ToggleSection,
   FilteredCollectionList,
   RoutedDateField,
   RoutedCheckboxGroupField,
@@ -76,117 +75,111 @@ const ProjectsCollection = ({
       baseDownloadLink="/investments/projects/export"
     >
       <CollectionFilters taskProps={collectionListMetadataTask}>
-        <ToggleSection
-          label="Company information"
-          data-cy="company-information-filters"
-          isOpen={true}
-        >
-          <RoutedCheckboxGroupField
-            label="Stage"
-            name="stage"
-            qsParam="stage"
-            options={optionMetadata.projectStageOptions}
-            selectedOptions={selectedFilters.selectedStages}
-            data-cy="stage-filter"
-          />
-          <RoutedAdvisersTypeahead
-            taskProps={adviserListTask}
-            isMulti={true}
-            label="Advisers"
-            name="adviser"
-            qsParam="adviser"
-            placeholder="Search advisers..."
-            noOptionsMessage={() => <>No advisers found</>}
-            selectedOptions={selectedFilters.selectedAdvisers}
-            data-cy="adviser-filter"
-          />
-          <RoutedTypeahead
-            isMulti={true}
-            label="Sector"
-            name="sector"
-            qsParam="sector_descends"
-            placeholder="Search sectors..."
-            options={optionMetadata.sectorOptions}
-            selectedOptions={selectedFilters.selectedSectors}
-            data-cy="sector-filter"
-          />
-          <RoutedTypeahead
-            isMulti={true}
-            label="Country of origin"
-            name="country"
-            qsParam="country"
-            placeholder="Search countries..."
-            options={optionMetadata.countryOptions}
-            selectedOptions={selectedFilters.selectedCountries}
-            data-cy="country-filter"
-          />
-          <RoutedTypeahead
-            isMulti={true}
-            label="UK Region"
-            name="uk_region"
-            qsParam="uk_region"
-            placeholder="Search UK regions..."
-            options={optionMetadata.ukRegionOptions}
-            selectedOptions={selectedFilters.selectedUkRegions}
-            data-cy="uk-region-filter"
-          />
-          <RoutedCheckboxGroupField
-            label="Status"
-            name="project_status"
-            qsParam="status"
-            options={optionMetadata.projectStatusOptions}
-            selectedOptions={selectedFilters.selectedProjectStatuses}
-            data-cy="project-status-filter"
-          />
-          <RoutedCheckboxGroupField
-            label="Type of investment"
-            name="investment_type"
-            qsParam="investment_type"
-            options={optionMetadata.investmentTypeOptions}
-            selectedOptions={selectedFilters.selectedInvestmentTypes}
-            data-cy="investment-type-filter"
-          />
-          <RoutedCheckboxGroupField
-            label="Likelihood to land"
-            name="likelihood_to_land"
-            qsParam="likelihood_to_land"
-            options={optionMetadata.likelihoodToLandOptions}
-            selectedOptions={selectedFilters.selectedLikelihoodToLands}
-            data-cy="likelihood-to-land-filter"
-          />
-          <RoutedDateField
-            label="Estimated land date before"
-            name="estimated_land_date_before"
-            qsParamName="estimated_land_date_before"
-            data-cy="estimated-land-date-before-filter"
-          />
-          <RoutedDateField
-            label="Estimated land date after"
-            name="estimated_land_date_after"
-            qsParamName="estimated_land_date_after"
-            data-cy="estimated-land-date-after-filter"
-          />
-          <RoutedDateField
-            label="Actual land date before"
-            name="actual_land_date_before"
-            qsParamName="actual_land_date_before"
-            data-cy="actual-land-date-before-filter"
-          />
-          <RoutedDateField
-            label="Actual land date after"
-            name="actual_land_date_after"
-            qsParamName="actual_land_date_after"
-            data-cy="actual-land-date-after-filter"
-          />
-          <RoutedCheckboxGroupField
-            label="Level of involvement specified"
-            name="involvement_level"
-            qsParam="level_of_involvement_simplified"
-            options={optionMetadata.involvementLevelOptions}
-            selectedOptions={selectedFilters.selectedInvolvementLevels}
-            data-cy="involvement-level-filter"
-          />
-        </ToggleSection>
+        <RoutedCheckboxGroupField
+          label="Stage"
+          name="stage"
+          qsParam="stage"
+          options={optionMetadata.projectStageOptions}
+          selectedOptions={selectedFilters.selectedStages}
+          data-cy="stage-filter"
+        />
+        <RoutedAdvisersTypeahead
+          taskProps={adviserListTask}
+          isMulti={true}
+          label="Advisers"
+          name="adviser"
+          qsParam="adviser"
+          placeholder="Search advisers..."
+          noOptionsMessage={() => <>No advisers found</>}
+          selectedOptions={selectedFilters.selectedAdvisers}
+          data-cy="adviser-filter"
+        />
+        <RoutedTypeahead
+          isMulti={true}
+          label="Sector"
+          name="sector"
+          qsParam="sector_descends"
+          placeholder="Search sectors..."
+          options={optionMetadata.sectorOptions}
+          selectedOptions={selectedFilters.selectedSectors}
+          data-cy="sector-filter"
+        />
+        <RoutedTypeahead
+          isMulti={true}
+          label="Country of origin"
+          name="country"
+          qsParam="country"
+          placeholder="Search countries..."
+          options={optionMetadata.countryOptions}
+          selectedOptions={selectedFilters.selectedCountries}
+          data-cy="country-filter"
+        />
+        <RoutedTypeahead
+          isMulti={true}
+          label="UK Region"
+          name="uk_region"
+          qsParam="uk_region"
+          placeholder="Search UK regions..."
+          options={optionMetadata.ukRegionOptions}
+          selectedOptions={selectedFilters.selectedUkRegions}
+          data-cy="uk-region-filter"
+        />
+        <RoutedCheckboxGroupField
+          label="Status"
+          name="project_status"
+          qsParam="status"
+          options={optionMetadata.projectStatusOptions}
+          selectedOptions={selectedFilters.selectedProjectStatuses}
+          data-cy="project-status-filter"
+        />
+        <RoutedCheckboxGroupField
+          label="Type of investment"
+          name="investment_type"
+          qsParam="investment_type"
+          options={optionMetadata.investmentTypeOptions}
+          selectedOptions={selectedFilters.selectedInvestmentTypes}
+          data-cy="investment-type-filter"
+        />
+        <RoutedCheckboxGroupField
+          label="Likelihood to land"
+          name="likelihood_to_land"
+          qsParam="likelihood_to_land"
+          options={optionMetadata.likelihoodToLandOptions}
+          selectedOptions={selectedFilters.selectedLikelihoodToLands}
+          data-cy="likelihood-to-land-filter"
+        />
+        <RoutedDateField
+          label="Estimated land date before"
+          name="estimated_land_date_before"
+          qsParamName="estimated_land_date_before"
+          data-cy="estimated-land-date-before-filter"
+        />
+        <RoutedDateField
+          label="Estimated land date after"
+          name="estimated_land_date_after"
+          qsParamName="estimated_land_date_after"
+          data-cy="estimated-land-date-after-filter"
+        />
+        <RoutedDateField
+          label="Actual land date before"
+          name="actual_land_date_before"
+          qsParamName="actual_land_date_before"
+          data-cy="actual-land-date-before-filter"
+        />
+        <RoutedDateField
+          label="Actual land date after"
+          name="actual_land_date_after"
+          qsParamName="actual_land_date_after"
+          data-cy="actual-land-date-after-filter"
+        />
+        <RoutedCheckboxGroupField
+          label="Level of involvement specified"
+          name="involvement_level"
+          qsParam="level_of_involvement_simplified"
+          options={optionMetadata.involvementLevelOptions}
+          selectedOptions={selectedFilters.selectedInvolvementLevels}
+          data-cy="involvement-level-filter"
+        />
       </CollectionFilters>
     </FilteredCollectionList>
   )
