a662681335a108864dca2e9335869a4a68ff2710
Ian Leggett <contactleggett@gmail.com>
2021-01-24 11:23:29 +0000

    Include the old props for njk Investments collection lists

--- src/apps/investments/transformers/collection.js
+++ src/apps/investments/transformers/collection.js
@@ -1,8 +1,12 @@
 /* eslint camelcase: 0 */
-const { isArray, assign } = require('lodash')
+const { isArray, assign, compact, pickBy } = require('lodash')
 const { format } = require('date-fns')
 
 const urls = require('../../../lib/urls')
+
+// TODO: Remove labels when all collection lists are reworked into React.
+const labels = require('../labels')
+// end
 
 function transformInvestmentProjectToListItem({
   id,
@@ -30,7 +34,46 @@ function transformInvestmentProjectToListItem({
     },
   ].filter((metadata) => metadata.value)
 
+  // TODO: Remove metaItems when all collection lists are reworked into React.
+  const metaItems = [
+    { key: 'stage', value: stage, type: 'badge' },
+    {
+      key: 'investment_type',
+      value: investment_type,
+      type: 'badge',
+      badgeModifier: 'secondary',
+    },
+    { key: 'status', value: status, type: 'badge', badgeModifier: 'secondary' },
+    { key: 'investor_company', value: investor_company },
+    { key: 'sector', value: sector },
+    {
+      key: 'estimated_land_date',
+      value: estimated_land_date,
+      type: 'dateMonthYear',
+      isInert: true,
+    },
+  ].map(({ key, value, type, badgeModifier, isInert }) => {
+    if (!value) return
+    return assign({}, pickBy({ value, type, badgeModifier, isInert }), {
+      label: labels.investmentProjectMetaItemLabels[key],
+    })
+  })
+  // end
+
   return {
+    // TODO: Remove all these props when all collection lists are reworked into React.
+    id,
+    name,
+    type: 'investments/project',
+    subTitle: {
+      type: 'project',
+      label: 'Project code',
+      badges,
+      value: project_code,
+      metadata,
+    },
+    // end
+    meta: compact(metaItems),
     headingUrl: urls.investments.projects.details(id),
     headingText: name,
     subheading: `Project code ${project_code}`,
