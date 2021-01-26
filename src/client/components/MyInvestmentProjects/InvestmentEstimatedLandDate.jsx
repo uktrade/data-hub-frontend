ecb8697620f5cbb868b65a6a56a1ecfec34df065
cgsunkel <christopher.sunkel@digital.trade.gov.uk>
2021-01-26 12:18:30 +0000

    Add InvestmentEstimatedLandDate component

--- src/client/components/MyInvestmentProjects/InvestmentEstimatedLandDate.jsx
+++ src/client/components/MyInvestmentProjects/InvestmentEstimatedLandDate.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { typography } from '@govuk-react/lib'
+import { SPACING } from '@govuk-react/constants'
+import { YELLOW, BLACK } from 'govuk-colours'
+import { format, endOfToday, differenceInDays } from 'date-fns'
+
+const StyledPanel = styled('div')`
+  padding: ${SPACING.SCALE_4};
+  color: ${BLACK};
+  background-color: ${YELLOW};
+`
+const StyledTitle = styled('h2')`
+  margin-top: ${SPACING.SCALE_4};
+  margin-bottom: ${SPACING.SCALE_4};
+  text-align: center;
+  font-size: ${typography.font({ size: 36, weight: 'bold' })};
+`
+const StyledBody = styled('div')`
+  text-align: center;
+  font-size: ${typography.font({ size: 16 })};
+`
+
+const getDifferenceInDays = (estimatedLandDate) => {
+  const today = endOfToday()
+  const difference = differenceInDays(estimatedLandDate, today)
+  return difference === 1
+    ? difference + ' day'
+    : difference === 0
+    ? 'Today'
+    : difference < 0
+    ? difference * -1 + ' days ago'
+    : difference + ' days'
+}
 
 const InvestmentEstimatedLandDate = ({ estimatedLandDate }) => (
-  <div>Estimated land date: {estimatedLandDate}</div>
+  <StyledPanel>
+    <StyledBody>Estimated land date</StyledBody>
+    <StyledTitle>{getDifferenceInDays(estimatedLandDate)}</StyledTitle>
+    <StyledBody>{format(estimatedLandDate, 'ddd, DD MMM YYYY')}</StyledBody>
+  </StyledPanel>
 )
 
 InvestmentEstimatedLandDate.propTypes = {
---
+++ src/client/components/MyInvestmentProjects/__stories__/InvestmentEstimatedLandDate.stories.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import InvestmentEstimatedLandDate from 'InvestmentEstimatedLandDate'
+import exampleReadme from './example.md'
+import usageReadme from './usage.md'
+import { addDays, subDays, endOfToday } from 'date-fns'
+
+const today = endOfToday()
+const futureDate = addDays(today, 100)
+const tomorrow = addDays(today, 1)
+const pastDate = subDays(today, 10)
+
+storiesOf('InvestmentEstimatedLandDate', module)
+  .addParameters({
+    options: { theme: undefined },
+    readme: {
+      content: exampleReadme,
+      sidebar: usageReadme,
+    },
+  })
+  .add('Default', () => (
+    <InvestmentEstimatedLandDate estimatedLandDate={futureDate} />
+  ))
+  .add('estimatedLandDate is today', () => (
+    <InvestmentEstimatedLandDate estimatedLandDate={today} />
+  ))
+  .add('estimatedLandDate is tomorrow', () => (
+    <InvestmentEstimatedLandDate estimatedLandDate={tomorrow} />
+  ))
+  .add('estimatedLandDate is in the past', () => (
+    <InvestmentEstimatedLandDate estimatedLandDate={pastDate} />
+  ))
