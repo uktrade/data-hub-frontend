b9fa6a88b4cbfaa8a2a16c275ecbb0be3bef3c6d
Ian Leggett <contactleggett@gmail.com>
2021-01-29 09:16:16 +0000

    Add 'my projects' chart to new dashboard

--- src/client/components/InvestmentProjectSummary/index.jsx
+++ src/client/components/InvestmentProjectSummary/index.jsx
@@ -1,5 +1,48 @@
 import React from 'react'
+import Chart from '../Chart'
 
-const InvestmentProjectSummary = () => <div>My Project Summary...</div>
+const InvestmentProjectSummary = () => (
+  <Chart
+    title="My project summary"
+    sortName="sortBy"
+    sortLabel="Date range"
+    sortOptions={[
+      { label: 'Current financial year', value: '1' },
+      { label: '2019-2020', value: '2' },
+    ]}
+    subject="Projects"
+    description="Projects in the current financial year"
+    headers={['Stage', 'Amount']}
+    name="stage"
+    url="/investments/projects"
+    data={[
+      {
+        key: 'Prospect',
+        param: 1,
+        value: 7,
+      },
+      {
+        key: 'Assigned',
+        param: 2,
+        value: 20,
+      },
+      {
+        key: 'Active',
+        param: 3,
+        value: 5,
+      },
+      {
+        key: 'Verfiy win',
+        param: 4,
+        value: 2,
+      },
+      {
+        key: 'Won',
+        param: 5,
+        value: 15,
+      },
+    ]}
+  />
+)
 
 export default InvestmentProjectSummary
