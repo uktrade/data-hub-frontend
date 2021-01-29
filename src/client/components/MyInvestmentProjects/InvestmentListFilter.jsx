ee036390b55b91e96aa9a4873505e1e52b6c99dc
Paul Gain <paul@javexconsulting.com>
2021-01-29 15:21:50 +0000

    Add Investment list along with placeholder components

---
+++ src/client/components/MyInvestmentProjects/InvestmentListFilter.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const InvestmentListFilter = ({}) => <>Stage filter...</>
+
+export default InvestmentListFilter
---
+++ src/client/components/MyInvestmentProjects/InvestmentListHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import InvestmentListFilter from './InvestmentListFilter'
+import InvestmentListSort from './InvestmentListSort'
+
+const InvestmentListHeader = () => (
+  <>
+    <InvestmentListFilter />
+    <InvestmentListSort />
+  </>
+)
+
+export default InvestmentListHeader
