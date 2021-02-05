8cdc44facce36fb6fc4facb7fa48777374f4fb37
Paul Gain <paul@javexconsulting.com>
2021-02-05 14:18:08 +0000

    Add Personalised Dashbard list pagination

--- src/client/components/MyInvestmentProjects/reducer.js
+++ src/client/components/MyInvestmentProjects/reducer.js
@@ -1,13 +1,28 @@
-import { MY_INVESTMENTS__LIST_LOADED } from '../../actions'
+import {
+  MY_INVESTMENTS__LIST_LOADED,
+  MY_INVESTMENTS__PAGE_SELECTED,
+} from '../../actions'
 
-const initialState = {}
+const initialState = {
+  count: 0,
+  results: [],
+  itemsPerPage: 10,
+  page: 1,
+}
 
-export default (state = initialState, { type, result }) => {
+export default (state = initialState, { type, result, page }) => {
   switch (type) {
     case MY_INVESTMENTS__LIST_LOADED:
+      const { results, count } = result
       return {
         ...state,
-        items: [...result],
+        results,
+        count,
+      }
+    case MY_INVESTMENTS__PAGE_SELECTED:
+      return {
+        ...state,
+        page,
       }
     default:
       return state
