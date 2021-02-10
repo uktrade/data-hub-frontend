d3ae3f27746894fb2d5be03c1b572a14a7172b41
Paul Gain <paul@javexconsulting.com>
2021-02-10 09:30:25 +0000

    Add show all details (expand/collapse investment project details) (#3317)

--- src/client/components/MyInvestmentProjects/InvestmentListHeader.jsx
+++ src/client/components/MyInvestmentProjects/InvestmentListHeader.jsx
@@ -3,10 +3,7 @@ import styled from 'styled-components'
 const InvestmentListHeader = styled('header')`
   background-color: #f8f8f8;
   display: flex;
-  justify-content: flex-end;
-  label:first-child {
-    margin-right: 15px;
-  }
+  justify-content: space-around;
 `
 
 export default InvestmentListHeader
