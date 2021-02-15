651d8cbd9566ce68df503659431c59bec8e95efb
Paul Gain <paul@javexconsulting.com>
2021-02-15 13:24:41 +0000

    Update USDGBP exchange rate

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,5 +1,5 @@
 // https://www.bloomberg.com/quote/USDGBP:CUR
-const EXCHANGE_RATE_USD_TO_GBP = 0.7486
+const EXCHANGE_RATE_USD_TO_GBP = 0.7189
 const EXCHANGE_RATE_GBP_TO_USD = parseFloat(
   Number(1 / EXCHANGE_RATE_USD_TO_GBP).toFixed(4)
 )
