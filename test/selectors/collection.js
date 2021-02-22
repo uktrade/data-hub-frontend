b2e1cfd992f792d868e478ff0991e8062f7410e4
Rachael Robinson <rachaelcodes@users.noreply.github.com>
2021-02-22 11:58:55 +0000

    fix flaky test

--- test/selectors/collection.js
+++ test/selectors/collection.js
@@ -2,6 +2,8 @@ module.exports = {
   header: '.c-collection__header-intro',
   headerCount: '.c-collection__result-count',
   items: '.c-entity-list__item',
+  itemsHeader: '.c-entity-list__item .c-entity__header',
+  itemsList: '.c-entity-list__item .c-meta-list',
   error: 'h3',
   contentTable: (table) => {
     return `.c-details-container__content > table:nth-child(${table})`
