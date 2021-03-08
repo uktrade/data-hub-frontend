7f180d78eed5861127038b113585b8dedec956fc
Paul Gain <paul@javexconsulting.com>
2021-03-08 16:48:28 +0000

    New Data Hub search bar (new dashboard)

--- src/client/components/Search/index.jsx
+++ src/client/components/Search/index.jsx
@@ -1,5 +1,44 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import Input from '@govuk-react/input'
+import VisuallyHidden from '@govuk-react/visually-hidden'
+import styled from 'styled-components'
 
-const Search = () => <div>Search Data Hub...</div>
+import urls from '../../../lib/urls'
+import SearchButton from '../SearchButton'
+
+const StyledSearchContainer = styled('div')`
+  position: relative;
+  width: 100%;
+  max-width: 900px;
+`
+
+const StyledSearchInput = styled(Input)`
+  width: 100%;
+`
+
+const Search = ({ csrfToken }) => (
+  <form
+    method="GET"
+    action={`${urls.search.type('companies')}?_csrf=${csrfToken}`}
+  >
+    <StyledSearchContainer role="search">
+      <label htmlFor="search-input">
+        <VisuallyHidden>Input your search term</VisuallyHidden>
+      </label>
+      <StyledSearchInput
+        name="term"
+        type="text"
+        id="search-input"
+        placeholder="Search Data Hub"
+      />
+      <SearchButton />
+    </StyledSearchContainer>
+  </form>
+)
+
+Search.propTypes = {
+  csrfToken: PropTypes.string.isRequired,
+}
 
 export default Search
