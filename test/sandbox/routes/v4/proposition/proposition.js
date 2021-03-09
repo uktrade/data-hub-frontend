539c093c9a6afda9066251c1816b44b8d0571c7f
Tom Kingston <tom@kingstonlabs.co.uk>
2021-03-09 16:33:12 +0000

    Adjust proposition sandbox to always show nearby dates

--- test/sandbox/routes/v4/proposition/proposition.js
+++ test/sandbox/routes/v4/proposition/proposition.js
@@ -1,5 +1,15 @@
 var outstandingPropositions = require('../../../fixtures/v4/proposition/outstanding_propositions.json')
 
 exports.propositions = function (req, res) {
+  outstandingPropositions.results = outstandingPropositions.results.map(
+    ({ deadline, ...rest }, i) => {
+      let newDeadline = new Date()
+      newDeadline.setDate(newDeadline.getDate() + 3 * i)
+      return {
+        deadline: newDeadline,
+        ...rest,
+      }
+    }
+  )
   res.json(outstandingPropositions)
 }
