41b9635a4e4b431711e0b614fa298d408d07d39c
Ian Leggett <contactleggett@gmail.com>
2021-03-09 16:10:37 +0000

    Refactor chart component and amend styling

--- src/client/components/PieChart/_stories_/pieChart.stories.jsx
+++ src/client/components/PieChart/_stories_/pieChart.stories.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { stageData } from '../_stories_/pieChartDummyData'
 import PieChart from 'PieChart'
 import exampleReadme from './example.md'
 import usageReadme from './usage.md'
+
+const stageData = [
+  {
+    id: 'Prospect',
+    label: 'Prospect',
+    value: 7,
+  },
+  {
+    id: 'Assign PM',
+    label: 'Assign PM',
+    value: 2,
+  },
+  {
+    id: 'Active',
+    label: 'Active',
+    value: 5,
+  },
+  {
+    id: 'Verify win',
+    label: 'Verify win',
+    value: 2,
+  },
+  {
+    id: 'Won',
+    label: 'Won',
+    value: 2,
+  },
+]
 
 storiesOf('PieChart', module)
   .addParameters({
@@ -13,4 +40,4 @@ storiesOf('PieChart', module)
       sidebar: usageReadme,
     },
   })
-  .add('Default', () => <PieChart data={stageData} />)
+  .add('Default', () => <PieChart data={stageData} height={450} />)
--- src/client/components/PieChart/index.jsx
+++ src/client/components/PieChart/index.jsx
@@ -1,37 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { BLUE, YELLOW, GREEN, TURQUOISE, GRASS_GREEN } from 'govuk-colours'
+import {
+  BLUE,
+  YELLOW,
+  GREEN,
+  TURQUOISE,
+  GRASS_GREEN,
+  GREY_2,
+} from 'govuk-colours'
+import { FONT_WEIGHTS } from '@govuk-react/constants'
 import { ResponsivePie } from '@nivo/pie'
 
 const segmentColours = [BLUE, TURQUOISE, GREEN, YELLOW, GRASS_GREEN]
 
-const legends = [
-  {
-    anchor: 'bottom',
-    direction: 'row',
-    translateY: 56,
-    itemWidth: 100,
-    itemHeight: 18,
-    itemTextColor: '#999',
-    symbolSize: 18,
-    symbolShape: 'circle',
-    effects: [
-      {
-        on: 'hover',
-        style: {
-          itemTextColor: '#000',
-        },
-      },
-    ],
-  },
-]
-
-const CenteredMetric = ({ dataWithArc, centerX, centerY }) => {
+const CentredMetric = ({ dataWithArc, centerX, centerY }) => {
   let total = 0
   dataWithArc.forEach((datum) => {
     total += datum.value
   })
-  const customY1 = centerY - 20
+  const customY1 = centerY - 30
   const customY2 = centerY + 20
   return (
     <>
@@ -41,8 +28,8 @@ const CenteredMetric = ({ dataWithArc, centerX, centerY }) => {
         textAnchor="middle"
         dominantBaseline="central"
         style={{
-          fontSize: '60px',
-          fontWeight: '600',
+          fontSize: '80px',
+          fontWeight: FONT_WEIGHTS.bold,
         }}
       >
         {total}
@@ -54,7 +41,7 @@ const CenteredMetric = ({ dataWithArc, centerX, centerY }) => {
         dominantBaseline="central"
         style={{
           fontSize: '20px',
-          fontWeight: '600',
+          fontWeight: FONT_WEIGHTS.bold,
         }}
       >
         {'Projects'}
@@ -63,28 +50,39 @@ const CenteredMetric = ({ dataWithArc, centerX, centerY }) => {
   )
 }
 
-const transformDataIds = (data) => {
-  data.forEach((stage) => {
-    stage.id = stage.label
-  })
-  return data
-}
-
-const PieChart = ({ data }) => (
-  <div style={{ height: 747 }}>
+const PieChart = ({ data, height }) => (
+  <div style={{ height }}>
     <ResponsivePie
-      data={transformDataIds(data)}
+      theme={{
+        fontSize: '16px',
+      }}
+      data={data}
       colors={segmentColours}
-      margin={{ top: 0, right: 120, bottom: 80, left: 120 }}
+      margin={{ top: 30, right: 70, bottom: 150, left: 0 }}
       startAngle={-90}
-      innerRadius={0.8}
+      innerRadius={0.75}
       borderWidth={1}
-      borderColor={{ from: 'color', modifiers: [['darker', 0.2]] }}
+      borderColor="transparent"
       radialLabelsSkipAngle={10}
-      radialLabel={(d) => `${d.id}: ${d.value}`}
+      radialLabel={(d) => d.value}
+      radialLabelsLinkColor={GREY_2}
+      radialLabelsTextColor={GREY_2}
       radialLabelsLinkStrokeWidth={3}
       enableSliceLabels={false}
-      layers={['slices', 'radialLabels', legends, CenteredMetric]}
+      layers={['slices', 'radialLabels', CentredMetric, 'legends']}
+      legends={[
+        {
+          anchor: 'bottom-left',
+          direction: 'column',
+          translateY: 150,
+          itemWidth: 100,
+          itemHeight: 18,
+          itemTextColor: '#999',
+          itemsSpacing: 5,
+          symbolSize: 18,
+          symbolShape: 'square',
+        },
+      ]}
     />
   </div>
 )
