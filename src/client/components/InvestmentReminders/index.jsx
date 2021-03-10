2c1687b737644a8cacb3f69d35e5afe32038b130
Tom Kingston <tom@kingstonlabs.co.uk>
2021-03-10 15:03:58 +0000

    Add notification badge to reminders component

--- src/client/components/InvestmentReminders/index.jsx
+++ src/client/components/InvestmentReminders/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 import { connect } from 'react-redux'
+import { H2 } from '@govuk-react/heading'
+import { FONT_SIZE, FONT_WEIGHTS } from '@govuk-react/constants'
 
 import { OUTSTANDING_PROPOSITIONS__LOADED } from '../../actions'
 import Task from '../Task/index.jsx'
-import ToggleSection from '../ToggleSection'
+import NotificationBadge from '../NotificationBadge'
 
 import OutstandingPropositions from './OutstandingPropositions'
 import { ID, TASK_GET_OUTSTANDING_PROPOSITIONS, state2props } from './state'
+
+const StyledHeading = styled(H2)`
+  font-size: ${FONT_SIZE.SIZE_19};
+  font-weight: ${FONT_WEIGHTS.bold};
+  margin: 0;
+`
 
 const InvestmentReminders = ({ adviser, outstandingPropositions }) => (
   <Task.Status
@@ -20,20 +29,17 @@ const InvestmentReminders = ({ adviser, outstandingPropositions }) => (
     }}
   >
     {() => (
-      <ToggleSection
-        id="investment-reminders-toggle"
-        label={
-          <>
-            Reminders (
-            {outstandingPropositions && outstandingPropositions.count})
-          </>
-        }
-        isOpen={true}
-      >
+      <>
+        <StyledHeading>
+          Reminders{' '}
+          <NotificationBadge
+            label={outstandingPropositions && outstandingPropositions.count}
+          />
+        </StyledHeading>
         <OutstandingPropositions
           outstandingPropositions={outstandingPropositions}
         />
-      </ToggleSection>
+      </>
     )}
   </Task.Status>
 )
