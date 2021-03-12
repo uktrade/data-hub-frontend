cf75efd567dca9a3159b8344e02b57a8b7d2695a
Pippo Raimondi <filipporai@gmail.com>
2021-03-12 09:16:56 +0000

    Add open all button

--- src/apps/investments/client/opportunities/Opportunities.jsx
+++ src/apps/investments/client/opportunities/Opportunities.jsx
@@ -2,24 +2,64 @@ import React from 'react'
 
 import TabNav from '../../../../client/components/TabNav'
 import ToggleSection from '../../../../client/components/ToggleSection'
+import SummaryList from '../../../../client/components/SummaryList'
+import ButtonLink from '../../../../client/components/ButtonLink'
+
+const rows = [
+  { label: 'Some Label 1', value: 'Example Ltd' },
+  { label: 'Some Label 2', value: 'Incomplete' },
+]
+
+class WipButton extends React.Component {
+  onButtonPress = () => {
+    this.setState({
+      isOpen: true,
+    })
+  }
+
+  render() {
+    return (
+      <ButtonLink onClick={this.onButtonPress} float="right">
+        Open All
+      </ButtonLink>
+    )
+  }
+}
 
 const Opportunities = () => (
-  <TabNav
-    id="TabNav"
-    label="Dashboard"
-    selectedIndex={'details'}
-    tabs={{
-      details: {
-        label: 'Details',
-        content: (
-          <>
-            <ToggleSection label="Opportunity details" id="t.details" />
-            <ToggleSection label="Opportunity requirements" id="t.req" />
-          </>
-        ),
-      },
-    }}
-  />
+  <>
+    <WipButton />
+    <TabNav
+      id="TabNav"
+      label="Dashboard"
+      selectedIndex={'details'}
+      tabs={{
+        details: {
+          label: 'Details',
+          content: (
+            <>
+              <ToggleSection
+                label="Opportunity details"
+                id="toggle_details"
+                fieldCount={rows.length}
+                showRequiredField={true}
+              >
+                <SummaryList rows={rows} />
+              </ToggleSection>
+              <ToggleSection
+                label="Opportunity requirements"
+                id="toggle_requirements"
+                fieldCount={rows.length}
+                showRequiredField={true}
+              >
+                <SummaryList rows={rows} />
+              </ToggleSection>
+            </>
+          ),
+        },
+      }}
+    />
+  </>
 )
 
 export default Opportunities
