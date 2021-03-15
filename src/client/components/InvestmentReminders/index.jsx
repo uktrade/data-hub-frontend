d0dc2d5eaf0e595c9e4d8eb30a6fdf3b2b56fe9a
Tom Kingston <tom@kingstonlabs.co.uk>
2021-03-15 11:41:39 +0000

    Put dashboard asides into ToggleSections

--- src/client/components/InvestmentReminders/index.jsx
+++ src/client/components/InvestmentReminders/index.jsx
@@ -1,24 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
-import { H2 } from '@govuk-react/heading'
-import { FONT_SIZE, FONT_WEIGHTS } from '@govuk-react/constants'
-
-import NotificationBadge from '../NotificationBadge'
 
 import OutstandingPropositions from './OutstandingPropositions'
 
-const StyledHeading = styled(H2)`
-  font-size: ${FONT_SIZE.SIZE_19};
-  font-weight: ${FONT_WEIGHTS.bold};
-  margin: 0;
-`
-
 const InvestmentReminders = ({ outstandingPropositions }) => (
   <div data-test="investment-reminders">
-    <StyledHeading data-test="investment-reminders-heading">
-      Reminders <NotificationBadge label={outstandingPropositions.count} />
-    </StyledHeading>
     <OutstandingPropositions {...outstandingPropositions} />
   </div>
 )
