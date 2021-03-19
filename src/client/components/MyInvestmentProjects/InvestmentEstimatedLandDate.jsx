ac022549593887cda0a2561ac799a33eb1ad45d8
Tom Kingston <tom@kingstonlabs.co.uk>
2021-03-19 09:24:14 +0000

    Apply RAG colours and opacity to estimated land date

--- src/client/components/MyInvestmentProjects/InvestmentEstimatedLandDate.jsx
+++ src/client/components/MyInvestmentProjects/InvestmentEstimatedLandDate.jsx
@@ -1,39 +1,69 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { typography } from '@govuk-react/lib'
-import { SPACING } from '@govuk-react/constants'
-import { YELLOW, BLACK } from 'govuk-colours'
+import { FONT_SIZE, FONT_WEIGHTS, SPACING } from '@govuk-react/constants'
+import { BLACK, BUTTON_COLOUR, GREY_3, RED, YELLOW } from 'govuk-colours'
 import { format } from 'date-fns'
 
 import { DATE_DAY_LONG_FORMAT } from '../../../common/constants'
-import { getDifferenceInDays } from '../../utils/date-utils'
+import {
+  getDifferenceInDays,
+  getDifferenceInDaysLabel,
+} from '../../utils/date-utils'
+import { rgba } from '../../utils/colors'
 
 const StyledPanel = styled('div')`
   padding: ${SPACING.SCALE_4};
   color: ${BLACK};
   background-color: ${YELLOW};
 `
+
+const GreenPanel = styled(StyledPanel)`
+  background-color: ${rgba(BUTTON_COLOUR, 0.3)};
+`
+const AmberPanel = styled(StyledPanel)`
+  background-color: ${rgba(YELLOW, 0.5)};
+`
+const RedPanel = styled(StyledPanel)`
+  background-color: ${rgba(RED, 0.4)};
+`
+const GreyPanel = styled(StyledPanel)`
+  background-color: ${rgba(GREY_3, 0.5)};
+`
+
 const StyledTitle = styled('h2')`
   margin-top: ${SPACING.SCALE_4};
   margin-bottom: ${SPACING.SCALE_4};
   text-align: center;
-  font-size: ${typography.font({ size: 36, weight: 'bold' })};
+  font-size: ${FONT_SIZE.SIZE_24};
+  font-weight: ${FONT_WEIGHTS.bold};
 `
 const StyledBody = styled('div')`
   text-align: center;
-  font-size: ${typography.font({ size: 16 })};
+  font-size: ${FONT_SIZE.SIZE_14};
 `
 
-const InvestmentEstimatedLandDate = ({ estimatedLandDate }) => (
-  <StyledPanel>
-    <StyledBody>Estimated land date</StyledBody>
-    <StyledTitle>{getDifferenceInDays(estimatedLandDate)}</StyledTitle>
-    <StyledBody>
-      {format(new Date(estimatedLandDate), DATE_DAY_LONG_FORMAT)}
-    </StyledBody>
-  </StyledPanel>
-)
+const InvestmentEstimatedLandDate = ({ estimatedLandDate }) => {
+  const difference = getDifferenceInDays(estimatedLandDate)
+  const Panel =
+    difference >= 90
+      ? GreenPanel
+      : difference >= 30
+      ? AmberPanel
+      : difference >= 0
+      ? RedPanel
+      : GreyPanel
+
+  return (
+    <Panel>
+      <StyledBody>Estimated land date</StyledBody>
+      <StyledTitle>{getDifferenceInDaysLabel(estimatedLandDate)}</StyledTitle>
+      <StyledBody>
+        {format(new Date(estimatedLandDate), DATE_DAY_LONG_FORMAT)}
+      </StyledBody>
+    </Panel>
+  )
+}
 
 InvestmentEstimatedLandDate.propTypes = {
   estimatedLandDate: PropTypes.string.isRequired,
