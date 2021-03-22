6a04011cd1ebe16b80225f960de0169724f21b2e
Chloe <chloe.wong@madetech.com>
2021-03-22 17:07:53 +0000

    Move export segments and export sub segments to separate constants file

--- src/apps/companies/apps/edit-company/client/CommonFields.jsx
+++ src/apps/companies/apps/edit-company/client/CommonFields.jsx
@@ -9,44 +9,7 @@ import {
   FieldWrapper,
 } from '../../../../../client/components'
 import { WEBSITE_REGEX } from '../../add-company/client/constants'
-
-const export_segments = [
-  { value: 'hep', label: 'High export potential' },
-  { value: 'non-hep', label: 'Not high export potential' },
-]
-
-const export_sub_segments = [
-  { value: 'sustain_nurture_and_grow', label: 'Sustain: nurture & grow' },
-  {
-    value: 'sustain_develop_export_capability',
-    label: 'Sustain: develop export capability',
-  },
-  {
-    value: 'sustain_communicate_benefits',
-    label: 'Sustain: communicate benefits',
-  },
-  {
-    value: 'sustain_increase_competitiveness',
-    label: 'Sustain: increase competitiveness',
-  },
-  { value: 'reassure_nurture_and_grow', label: 'Reassure: nurture & grow' },
-  {
-    value: 'reassure_develop_export_capability',
-    label: 'Reassure: develop export capability',
-  },
-  { value: 'reassure_leave_be', label: 'Reassure: leave be' },
-  { value: 'reassure_change_the_game', label: 'Reassure: change the game' },
-  {
-    value: 'promote_develop_export_capability',
-    label: 'Promote: develop export capability',
-  },
-  {
-    value: 'promote_communicate_benefits',
-    label: 'Promote: communicate benefits',
-  },
-  { value: 'promote_change_the_game', label: 'Promote: change the game' },
-  { value: 'challenge', label: 'Challenge' },
-]
+import { export_segments, export_sub_segments } from './constants'
 
 const websiteValidator = (value) =>
   value && !WEBSITE_REGEX.test(value) ? 'Enter a valid website URL' : null
