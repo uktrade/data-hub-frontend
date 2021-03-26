9d81cf734d4f5c7439bdfc9e7ce192451bd1f1dd
Peter Hudec <peterhudec@peterhudec.com>
2021-03-26 17:20:28 +0000

    Add the Create UK investment opportunity page

---
+++ test/functional/cypress/specs/investments/create-uk-investment-opportunity.js
@@ -0,0 +1,17 @@
+it('Create UK Investment Opportunity page', () => {
+  cy.visit('/investments/opportunities/create')
+
+  cy.contains('There is a problem').should('not.exist')
+  cy.contains('Enter an opportunity name').should('not.exist')
+  cy.contains('Save').click()
+  cy.contains('There is a problem')
+  cy.contains('Enter an opportunity name')
+
+  cy.get('input[name="name"]').type('Foooo{enter}')
+  cy.url().should(
+    'eq',
+    `${
+      Cypress.config().baseUrl
+    }/investments/opportunities/new-large-capital-uk-opportunity-id`
+  )
+})
