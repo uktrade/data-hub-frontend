92c5133192edd0f4425dfc8400113e0bc0967e12
Vincent Farah <vincent.farah@madetech.com>
2021-03-26 19:39:12 +0000

    Interaction detail setup to utilise feature flag Setup interaction type to deal with trade agreements feature flagged Setup themes to deal with Trade Agreement Setup Scenario 1, 2

---
+++ src/apps/interactions/__stories__/InteractionDetails.stories.jsx
@@ -0,0 +1,57 @@
+import React, { Fragment } from 'react'
+import { storiesOf } from '@storybook/react'
+import InteractionDetailsForm from '../apps/details-form/client/InteractionDetailsForm'
+
+const DEFAULT_INTERACTION = {
+  companyId: '3cb006bb-c8bd-4062-bc8c-c086273b84e1',
+  initialValues: {
+    company: '3cb006bb-c8bd-4062-bc8c-c086273b84e1',
+    date: { day: '28', month: '03', year: '2021' },
+    contacts: [],
+    dit_participants: [
+      { value: '65f1a4cc-0171-4db8-8768-0428bd99dd71', label: '' },
+    ],
+  },
+  isTradeAgreementInteractionEnabled: true,
+}
+
+const InteractionDetailsTemplate = (args) => (
+  <Fragment>
+    <InteractionDetailsForm {...args}></InteractionDetailsForm>
+  </Fragment>
+)
+
+export const DefaulInteractionWithTradeAgreement = InteractionDetailsTemplate.bind(
+  {}
+)
+DefaulInteractionWithTradeAgreement.args = {
+  ...DEFAULT_INTERACTION,
+  isTradeAgreementInteractionEnabled: true,
+}
+
+export const DefaulInteractionWithoutTradeAgreement = InteractionDetailsTemplate.bind(
+  {}
+)
+DefaulInteractionWithoutTradeAgreement.args = {
+  ...DEFAULT_INTERACTION,
+  isTradeAgreementInteractionEnabled: false,
+}
+
+storiesOf('Company Interactions', module)
+  .addParameters({
+    options: { theme: undefined },
+  })
+  .add('With Trade Agreement', () => (
+    <Fragment>
+      <DefaulInteractionWithTradeAgreement
+        {...DefaulInteractionWithTradeAgreement.args}
+      ></DefaulInteractionWithTradeAgreement>
+    </Fragment>
+  ))
+  .add('Without Trade Agreement', () => (
+    <Fragment>
+      <DefaulInteractionWithoutTradeAgreement
+        {...DefaulInteractionWithoutTradeAgreement.args}
+      ></DefaulInteractionWithoutTradeAgreement>
+    </Fragment>
+  ))
--- src/apps/interactions/apps/details-form/client/StepInteractionType.jsx
+++ src/apps/interactions/apps/details-form/client/StepInteractionType.jsx
@@ -3,86 +3,115 @@ import React from 'react'
 import { FieldRadios } from '../../../../../client/components'
 import { useFormContext } from '../../../../../client/components/Form/hooks'
 import { THEMES, KINDS } from '../../../constants'
+import { FieldHelp } from '../../../../../client/components'
 
 const getOnChangeHandler = (fieldName, setFieldValue) => (e) => {
   setFieldValue('service', '')
   setFieldValue(fieldName, e.target.value)
 }
 
-const getInvestmentOnChangeHandler = (setFieldValue) => (e) => {
+const setInteractionKindFieldValues = (setFieldValue, e) => {
   setFieldValue('service', '')
   setFieldValue('theme', e.target.value)
   setFieldValue('kind', KINDS.INTERACTION)
 }
 
-const StepInteractionType = () => {
+const getInvestmentOnChangeHandler = (setFieldValue) => (e) => {
+  setInteractionKindFieldValues(setFieldValue, e)
+}
+
+const getTradeAgreementOnChangeHandler = (setFieldValue) => (e) => {
+  setInteractionKindFieldValues(setFieldValue, e)
+}
+
+const StepInteractionType = ({ isTradeAgreementInteractionEnabled }) => {
   const { setFieldValue } = useFormContext()
+  const exportOption = {
+    label: 'Export',
+    value: THEMES.EXPORT,
+    onChange: getOnChangeHandler('theme', setFieldValue),
+    children: (
+      <FieldRadios
+        label="What would you like to record?"
+        name="kind"
+        required="Select what you would like to record"
+        options={[
+          {
+            label: 'A standard interaction',
+            hint: 'For example, an email, phone call or meeting',
+            value: KINDS.INTERACTION,
+            onChange: getOnChangeHandler('kind', setFieldValue),
+          },
+          {
+            label: 'A service that you have provided',
+            hint: 'For example, a significant assist or an event',
+            value: KINDS.SERVICE_DELIVERY,
+            onChange: getOnChangeHandler('kind', setFieldValue),
+          },
+        ]}
+      />
+    ),
+  }
+  const investmentOption = {
+    label: 'Investment',
+    value: THEMES.INVESTMENT,
+    onChange: getInvestmentOnChangeHandler(setFieldValue),
+  }
+  const tradeAgreementOption = {
+    label: 'Trade Agreement',
+    value: THEMES.TRADE_AGREEMENT,
+    onChange: getTradeAgreementOnChangeHandler(setFieldValue),
+  }
+  const otherOption = {
+    label: 'Other',
+    value: THEMES.OTHER,
+    onChange: getOnChangeHandler('theme', setFieldValue),
+    children: (
+      <FieldRadios
+        label="What would you like to record?"
+        name="kind"
+        required="Select what you would like to record"
+        onChange={getOnChangeHandler('kind', setFieldValue)}
+        options={[
+          {
+            label: 'A standard interaction',
+            hint: 'For example, an email, phone call or meeting',
+            value: KINDS.INTERACTION,
+            onChange: getOnChangeHandler('kind', setFieldValue),
+          },
+          {
+            label: 'A service that you have provided',
+            hint: 'For example, a significant assist or an event',
+            value: KINDS.SERVICE_DELIVERY,
+            onChange: getOnChangeHandler('kind', setFieldValue),
+          },
+        ]}
+      />
+    ),
+  }
+  const configuredFieldRadiosOptions = isTradeAgreementInteractionEnabled
+    ? [exportOption, investmentOption, tradeAgreementOption, otherOption]
+    : [exportOption, investmentOption, otherOption]
   return (
-    <FieldRadios
-      name="theme"
-      label="What is this regarding?"
-      required="Select what you would like to record"
-      options={[
-        {
-          label: 'Export',
-          value: THEMES.EXPORT,
-          onChange: getOnChangeHandler('theme', setFieldValue),
-          children: (
-            <FieldRadios
-              label="What would you like to record?"
-              name="kind"
-              required="Select what you would like to record"
-              options={[
-                {
-                  label: 'A standard interaction',
-                  hint: 'For example, an email, phone call or meeting',
-                  value: KINDS.INTERACTION,
-                  onChange: getOnChangeHandler('kind', setFieldValue),
-                },
-                {
-                  label: 'A service that you have provided',
-                  hint: 'For example, a significant assist or an event',
-                  value: KINDS.SERVICE_DELIVERY,
-                  onChange: getOnChangeHandler('kind', setFieldValue),
-                },
-              ]}
-            />
-          ),
-        },
-        {
-          label: 'Investment',
-          value: THEMES.INVESTMENT,
-          onChange: getInvestmentOnChangeHandler(setFieldValue),
-        },
-        {
-          label: 'Other',
-          value: THEMES.OTHER,
-          onChange: getOnChangeHandler('theme', setFieldValue),
-          children: (
-            <FieldRadios
-              label="What would you like to record?"
-              name="kind"
-              required="Select what you would like to record"
-              onChange={getOnChangeHandler('kind', setFieldValue)}
-              options={[
-                {
-                  label: 'A standard interaction',
-                  hint: 'For example, an email, phone call or meeting',
-                  value: KINDS.INTERACTION,
-                  onChange: getOnChangeHandler('kind', setFieldValue),
-                },
-                {
-                  label: 'A service that you have provided',
-                  hint: 'For example, a significant assist or an event',
-                  value: KINDS.SERVICE_DELIVERY,
-                  onChange: getOnChangeHandler('kind', setFieldValue),
-                },
-              ]}
-            />
-          ),
-        },
-      ]}
-    />
+    <>
+      {isTradeAgreementInteractionEnabled && (
+        <FieldHelp
+          helpSummary="Guidance regarding a Trade Agreement"
+          helpText="If your Interaction was set up to focus on a Trade Agreement or
+          contributes to implementing a Trade Agreement, select 'Trade
+          Agreement’"
+          footerUrl="https://data-services-help.trade.gov.uk/data-hub/how-articles/trade-agreement-activity/recording-trade-agreement-activity/"
+          footerUrlDescription="See more guidance"
+        />
+      )}
+
+      <FieldRadios
+        name="theme"
+        label="What is this regarding?"
+        required="Select what you would like to record"
+        options={configuredFieldRadiosOptions}
+      />
+    </>
   )
 }
 
