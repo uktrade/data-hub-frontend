baa7f92612a685f348d8f15e0945ac510851616d
Paul Gain <paul@javexconsulting.com>
2021-03-29 08:16:34 +0100

    Add No investment projects

---
+++ src/client/components/MyInvestmentProjects/NoInvestmentProjects.jsx
@@ -0,0 +1,99 @@
+import React from 'react'
+import UnorderedList from '@govuk-react/unordered-list'
+import { H3 } from '@govuk-react/heading'
+import styled from 'styled-components'
+import Button from '@govuk-react/button'
+import Link from '@govuk-react/link'
+
+import {
+  MEDIA_QUERIES,
+  SPACING,
+  FONT_SIZE,
+  FONT_WEIGHTS,
+} from '@govuk-react/constants'
+
+import { investments } from '../../../lib/urls'
+import { DARK_GREY } from '../../utils/colors'
+import TimelineImage from './timeline.png'
+
+const StyledContainer = styled('div')`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  ${MEDIA_QUERIES.TABLET} {
+    margin: 25px 0 50px 0;
+  }
+  ${MEDIA_QUERIES.DESKTOP} {
+    margin: 45px 0 95px 0;
+  }
+`
+
+const StyledParagraph = styled('p')`
+  color: ${DARK_GREY};
+  ${MEDIA_QUERIES.TABLET} {
+    margin-bottom: 25px;
+    font-size: ${FONT_SIZE.SIZE_24};
+    font-weight: ${FONT_WEIGHTS.bold};
+  }
+`
+
+const StyledImage = styled('img')`
+  display: none;
+  ${MEDIA_QUERIES.TABLET} {
+    display: block;
+    width: 655px;
+    margin-bottom: ${SPACING.SCALE_3};
+  }
+  ${MEDIA_QUERIES.DESKTOP} {
+    width: 760px;
+    margin: ${SPACING.SCALE_3} 0 35px 0;
+  }
+`
+
+const StyledUnorderedList = styled(UnorderedList)`
+  color: ${DARK_GREY};
+  margin-bottom: ${SPACING.SCALE_4};
+  ${MEDIA_QUERIES.TABLET} {
+    margin-left: 7px;
+    margin-bottom: 40px;
+    li::marker {
+      font-size: 30px;
+    }
+  }
+`
+
+const StyledListItem = styled('li')`
+  ${MEDIA_QUERIES.TABLET} {
+    font-size: ${FONT_SIZE.SIZE_24};
+    font-weight: ${FONT_WEIGHTS.bold};
+  }
+`
+
+const NoInvestmentProjects = () => (
+  <StyledContainer>
+    <H3>No investment projects</H3>
+    <StyledParagraph>
+      View and track investment projects from your dashboard
+    </StyledParagraph>
+    <StyledImage
+      src={TimelineImage}
+      alt="An image of the stage timeline and Estimated land date"
+    />
+    <div>
+      <StyledParagraph>Once added, you'll be able to:</StyledParagraph>
+      <StyledUnorderedList listStyleType="bullet">
+        <StyledListItem>view the progress of each project</StyledListItem>
+        <StyledListItem>move projects onto the next stage</StyledListItem>
+        <StyledListItem>view estimated land dates</StyledListItem>
+        <StyledListItem>
+          view project summaries for the next financial year
+        </StyledListItem>
+      </StyledUnorderedList>
+    </div>
+    <Button as={Link} href={investments.index()}>
+      View projects
+    </Button>
+  </StyledContainer>
+)
+
+export default NoInvestmentProjects
