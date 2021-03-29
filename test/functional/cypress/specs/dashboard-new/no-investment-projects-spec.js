736d62751963d1bb8253a20a9da222daaa9abcb7
Paul Gain <paul@javexconsulting.com>
2021-03-29 08:17:24 +0100

    Add functional tests

---
+++ test/functional/cypress/specs/dashboard-new/no-investment-projects-spec.js
@@ -0,0 +1,88 @@
+const urls = require('../../../../../src/lib/urls')
+
+describe('Dashboard - no investment projects', () => {
+  beforeEach(() => {
+    cy.setFeatureFlag(
+      'layoutTesting:9010dd28-9798-e211-a939-e4115bead28a',
+      true
+    )
+    cy.visit('/myinvestmentprojects')
+    cy.get('[data-test="tablist"]').as('tabList')
+    cy.get('[data-test="tabpanel"]').as('tabPanel')
+  })
+  after(() => {
+    cy.resetFeatureFlags()
+  })
+
+  context('Tabbed navigation', () => {
+    it('should contain two tabs named My projects and My Companies lists', () => {
+      cy.get('@tabList').find('button').eq(0).should('have.text', 'My projects')
+
+      cy.get('@tabList')
+        .find('button')
+        .eq(1)
+        .should('have.text', 'My companies lists')
+    })
+
+    it('should have a heading of `No investment projects`', () => {
+      cy.get('@tabPanel')
+        .find('h3')
+        .should('have.text', 'No investment projects')
+    })
+
+    it('should have a heading paragraph', () => {
+      cy.get('@tabPanel')
+        .find('p')
+        .eq(0)
+        .should(
+          'have.text',
+          'View and track investment projects from your dashboard'
+        )
+    })
+
+    it('should have an image of the stage timeline and Estimated land date', () => {
+      cy.get('@tabPanel')
+        .find('img')
+        .should(
+          'have.attr',
+          'alt',
+          'An image of the stage timeline and Estimated land date'
+        )
+    })
+
+    it('should have a body paragraph', () => {
+      cy.get('@tabPanel')
+        .find('p')
+        .eq(1)
+        .should('have.text', "Once added, you'll be able to:")
+    })
+
+    it('should have a list with 4 items', () => {
+      cy.get('@tabPanel').find('ul').children().should('have.length', 4)
+    })
+
+    it('should have a list with informative information', () => {
+      cy.get('@tabPanel')
+        .find('ul')
+        .children()
+        .first()
+        .should('have.text', 'view the progress of each project')
+        .next()
+        .should('have.text', 'move projects onto the next stage')
+        .next()
+        .should('have.text', 'view estimated land dates')
+        .next()
+        .should(
+          'have.text',
+          'view project summaries for the next financial year'
+        )
+    })
+
+    it('should have a link to the investment projects page', () => {
+      cy.get('@tabPanel')
+        .find('a')
+        .should('have.text', 'View projects')
+        .and('have.attr', 'href', urls.investments.index())
+    })
+  })
+})
