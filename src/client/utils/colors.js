fe3f8fb64dd49b9cad707d6bb32ccfa7757428bb
Ian Leggett <contactleggett@gmail.com>
2021-04-01 12:56:35 +0100

    Add investment projects 'next steps' component to the dashboard

--- src/client/utils/colors.js
+++ src/client/utils/colors.js
@@ -26,3 +26,4 @@ export const rgba = (colorHex, alpha) => `rgba(${hexToRgb(colorHex)},${alpha})`
 // - https://github.com/alphagov/govuk-frontend (referenced by GOV.UK Design System)
 export const DARK_GREY = '#505a5f'
 export const MID_GREY = '#b1b4b6'
+export const FOCUS_COLOUR = '#ffdd00'
