dcaa80cfbeb3a2c7e560f66eeed5cebe3ac0029c
Tom Kingston <tom@kingstonlabs.co.uk>
2021-04-13 15:38:27 +0100

    Use intercept for no project filter test

--- test/functional/cypress/specs/dashboard-new/no-projects-filter-spec.js
+++ test/functional/cypress/specs/dashboard-new/no-projects-filter-spec.js
@@ -1,7 +1,9 @@
+import { INVESTMENT_PROJECT_STAGES } from '../../fakers/constants'
+import { investmentProjectListFaker } from '../../fakers/investment-projects'
+
 describe('Dashboard - no investment projects', () => {
   before(() => {
     cy.setUserFeatures(['personalised-dashboard'])
-    cy.visit('/')
   })
 
   after(() => {
@@ -10,14 +12,33 @@ describe('Dashboard - no investment projects', () => {
 
   beforeEach(() => {
     cy.get('[data-test="tabpanel"]').find('select').eq(0).as('stageSelect')
+    cy.intercept('POST', '/api-proxy/v3/search/investment_project', (req) => {
+      req.reply(
+        req.body.stage === INVESTMENT_PROJECT_STAGES.verifyWin.id
+          ? { body: { count: 0, results: [] } }
+          : {
+              body: {
+                count: 10,
+                results: investmentProjectListFaker(10),
+              },
+            }
+      )
+    }).as('apiRequest')
+    cy.visit('/myinvestmentprojects')
+    cy.wait('@apiRequest')
+
+    cy.get('[data-test="tabpanel"]').find('select').eq(0).as('stageSelect')
   })
 
   context('When a filter is applied and there are zero projects', () => {
     it('should display "No investment projects"', () => {
-      cy.get('@stageSelect')
-        .select('Verify win')
-        .get('[data-test="tabpanel"] p')
-        .should('have.text', 'No investment projects')
+      cy.get('@stageSelect').select('Verify win')
+      cy.wait('@apiRequest')
+
+      cy.get('[data-test="tabpanel"] p').should(
+        'have.text',
+        'No investment projects'
+      )
     })
   })
 })
