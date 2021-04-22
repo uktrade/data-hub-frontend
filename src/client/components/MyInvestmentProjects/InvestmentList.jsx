68390d57f250210086b9584a05ba1d09d42655d1
Paul Gain <paul@javexconsulting.com>
2021-04-22 09:42:07 +0100

    Change horizontal line colour from red to grey

--- src/client/components/MyInvestmentProjects/InvestmentList.jsx
+++ src/client/components/MyInvestmentProjects/InvestmentList.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { GREY_1 } from 'govuk-colours'
 
 import InvestmentListItem from './InvestmentListItem'
 
 const StyledOrderedList = styled('ol')`
   margin-top: 0;
-  ${({ isPaginated }) => isPaginated && `border-bottom: 2px solid red;`}
+  ${({ isPaginated }) => isPaginated && `border-bottom: 2px solid ${GREY_1};`}
 `
 
 const InvestmentList = ({ items, isPaginated, showDetails }) => (
