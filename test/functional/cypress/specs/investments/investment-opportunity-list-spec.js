6ef0e515cbbc4011a080c0c728c515afac52435c
Pippo Raimondi <filipporai@gmail.com>
2021-04-27 11:33:43 +0100

    Add download button test

--- test/functional/cypress/specs/investments/investment-opportunity-list-spec.js
+++ test/functional/cypress/specs/investments/investment-opportunity-list-spec.js
@@ -52,5 +52,18 @@ describe('Investment opportunities', () => {
     it('should display 4 profiles', () => {
       cy.get('h3').should('have.length', 4)
     })
+
+    it('should display download profile text', () => {
+      cy.get('[data-test="download-data-header"]').should(
+        'contain',
+        'You can now download these 4 opportunities'
+      )
+    })
+
+    it('should display download profile button', () => {
+      cy.get('[data-test="download-data-header"] > div > a')
+        .should('be.visible')
+        .should('have.attr', 'href', '/investments/opportunities/export')
+    })
   })
 })
