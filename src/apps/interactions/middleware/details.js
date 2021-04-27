f1f7d5ff0a7a432986ab387a29947b1ea31c4c04
Faissal Bensefia <faissal@madetech.com>
2021-04-27 11:28:26 +0100

    Fix trade agreements feature flag always being evaluated as true

--- src/apps/interactions/middleware/details.js
+++ src/apps/interactions/middleware/details.js
@@ -3,7 +3,11 @@ const { getDitCompany } = require('../../companies/repos')
 
 async function getInteractionDetails(req, res, next, interactionId) {
   try {
-    const interaction = await fetchInteraction(req, interactionId, res.locals)
+    const interaction = await fetchInteraction(
+      req,
+      interactionId,
+      res.locals.features['trade-agreement-interaction-v4-endpoint']
+    )
     res.locals.interaction = interaction
 
     if (!res.locals.company) {
