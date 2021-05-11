ae1546c963fa88db58563d629aa418bc21cb21d2
Pippo Raimondi <filipporai@gmail.com>
2021-05-11 06:50:13 +0100

    Add test for pagination in sort functionality

--- test/sandbox/routes/v4/investment/investment.js
+++ test/sandbox/routes/v4/investment/investment.js
@@ -1,6 +1,7 @@
 var incompleteLargeCapitalOpportunity = require('../../../fixtures/v4/investment/large-capital-opportunity-incomplete.json')
 var completeLargeCapitalOpportunity = require('../../../fixtures/v4/investment/large-capital-opportunity-complete.json')
 var largeCapitalOpportunityList = require('../../../fixtures/v4/investment/large-capital-opportunity-list.json')
+var largeCapitalOpportunityListOnePage = require('../../../fixtures/v4/investment/large-capital-opportunity-list-one-page.json')
 
 exports.getLargeCapitalOpportunity = function (req, res) {
   if (req.params.opportunityId == completeLargeCapitalOpportunity.id) {
@@ -11,5 +12,8 @@ exports.getLargeCapitalOpportunity = function (req, res) {
 }
 
 exports.getLargeCapitalOpportunityList = function (req, res) {
+  if (req.body.sortby == 'name:asc') {
+    return res.json(largeCapitalOpportunityListOnePage)
+  }
   res.json(largeCapitalOpportunityList)
 }
