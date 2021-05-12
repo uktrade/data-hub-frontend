7a5b592481e997f4a00dfb0d62e8675a0182766a
Paul Gain <paul@javexconsulting.com>
2021-05-12 09:15:50 +0100

    Add <aside> bottom spacing

--- src/client/components/PersonalisedDashboard/Aside.jsx
+++ src/client/components/PersonalisedDashboard/Aside.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
+import { SPACING } from '@govuk-react/constants'
 
 import PrimaryContainer from './PrimaryContainer'
 
+const StyledAside = styled('aside')`
+  margin-bottom: ${SPACING.SCALE_6};
+`
+
 const Aside = ({ children }) => (
-  <aside>
+  <StyledAside>
     {children.map((child, i) => (
       <PrimaryContainer key={i}>{child}</PrimaryContainer>
     ))}
-  </aside>
+  </StyledAside>
 )
 
 export default Aside
