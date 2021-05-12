19b8f78acec9feb4aa2c92e19994ea137dc87dfd
Tom Kingston <tom@kingstonlabs.co.uk>
2021-05-12 09:43:42 +0100

    Add strip scripts back for axios requests

--- src/lib/request.js
+++ src/lib/request.js
@@ -1,4 +1,29 @@
 const axios = require('axios')
+const { isString } = require('lodash')
+
+const logger = require('../config/logger')
+
+/**
+ * Strip scripts from JSON
+ *
+ * Note that this shouldn't be needed since everything should be escaped when
+ * it is rendered.
+ */
+const stripScripts = (key, value) => {
+  if (isString(value)) {
+    return stripScript(value)
+  }
+  return value
+}
+
+const stripScript = (text) => {
+  const SCRIPT_REGEX = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi
+  while (SCRIPT_REGEX.test(text)) {
+    logger.warn('Found script tag in response')
+    text = text.replace(SCRIPT_REGEX, '')
+  }
+  return text
+}
 
 /**
  * This error is thrown when a response gives a bad status code.
@@ -25,8 +50,16 @@ class StatusCodeError extends Error {
  * breaking tests.
  */
 const request = async (props) => {
+  const baseConfig = isString(props) ? { url: props } : props
+  let transformResponse = [].concat(axios.defaults.transformResponse)
+  if (!props.responseType || props.responseType === 'json') {
+    transformResponse.push((data) =>
+      JSON.parse(JSON.stringify(data), stripScripts)
+    )
+  }
+
   try {
-    return await axios(props)
+    return await axios({ ...baseConfig, transformResponse })
   } catch (error) {
     const { response } = error
     if (response) {
