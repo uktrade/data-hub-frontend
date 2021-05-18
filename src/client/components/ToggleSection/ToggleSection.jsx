3cb5857822b1b6ef11773a90ff326b279a955bf6
Tom Kingston <tom@kingstonlabs.co.uk>
2021-05-18 10:06:25 +0100

    Add spacing to default ToggleContent

--- src/client/components/ToggleSection/ToggleSection.jsx
+++ src/client/components/ToggleSection/ToggleSection.jsx
@@ -3,20 +3,28 @@ import { BLACK, YELLOW } from 'govuk-colours'
 import { SPACING } from '@govuk-react/constants'
 
 import {
+  ButtonContent,
   MultiInstanceToggleSection,
   ToggleButton,
-  ButtonContent,
+  ToggleContent,
 } from './BaseToggleSection'
 
 export const ToggleSection = styled(MultiInstanceToggleSection)`
   margin-bottom: ${SPACING.SCALE_2};
 
   ${ToggleButton} {
+    padding: ${SPACING.SCALE_2} 0 ${SPACING.SCALE_1};
+
     &:focus ${ButtonContent} {
       color: ${BLACK};
       background-color: ${YELLOW};
       box-shadow: 0 -2px ${YELLOW}, 0 4px ${BLACK};
     }
+  }
+
+  ${ToggleContent} {
+    margin: 0;
+    padding: ${SPACING.SCALE_2} 0;
   }
 `
 
