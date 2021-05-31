9936b2e873ee871f5d5992b009548f7a5ed1b0fd
Paul Gain <paul@javexconsulting.com>
2021-05-31 23:55:36 +0100

    Add contacts collection

--- src/apps/contacts/client/ContactsCollection.jsx
+++ src/apps/contacts/client/ContactsCollection.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const ContactsCollection = () => {
+import { CONTACTS__LOADED } from '../../../client/actions'
+import { FilteredCollectionList } from '../../../client/components'
+
+import { TASK_GET_CONTACTS_LIST, ID, state2props } from './state'
+
+const ContactsCollection = ({
+  payload,
+  optionMetadata,
+  selectedFilters,
+  ...props
+}) => {
+  const collectionListTask = {
+    name: TASK_GET_CONTACTS_LIST,
+    id: ID,
+    progressMessage: 'loading contacts',
+    startOnRender: {
+      payload,
+      onSuccessDispatch: CONTACTS__LOADED,
+    },
+  }
+
   return (
-    <div>
-      <h2>Contacts Collection to go here...</h2>
-    </div>
+    <FilteredCollectionList
+      {...props}
+      collectionName="Contacts"
+      sortOptions={optionMetadata.sortOptions}
+      taskProps={collectionListTask}
+      selectedFilters={selectedFilters}
+      baseDownloadLink="/contacts/export"
+      entityName="contact"
+    ></FilteredCollectionList>
   )
 }
 
-export default ContactsCollection
+export default connect(state2props)(ContactsCollection)
