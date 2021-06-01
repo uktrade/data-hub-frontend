3596b40c616444be94b3817bf635dd13deac5c7d
Chloe <chloe.wong@madetech.com>
2021-06-01 17:09:34 +0100

    WIP Implemented company faker

--- test/functional/cypress/specs/companies/collection-address-spec.js
+++ test/functional/cypress/specs/companies/collection-address-spec.js
@@ -1,10 +1,47 @@
 import { assertBreadcrumbs } from '../../support/assertions'
+import { companies } from '../../../../../src/lib/urls'
 
-const selectors = require('../../../../selectors')
+import { companyFaker, companyListFaker } from '../../fakers/companies'
+
+// const selectors = require('../../../../selectors')
 
 describe('Company Collection Address', () => {
+  const usCompany = companyFaker({
+    address: {
+      line_1: '3525 Eastham Drive',
+      line_2: '',
+      town: 'Culver City',
+      county: 'CA',
+      postcode: '90232',
+      // area: {
+      //   id: '81756b9a-5d95-e211-a939-e4115bead28b',
+      //   name: 'California'
+      // },
+      area: 'California',
+      country: {
+        id: '81756b9a-5d95-e211-a939-e4115bead28a',
+        name: 'United States',
+      },
+    },
+  })
+
+  const otherCompanies = companyListFaker(9)
+  const companyList = [usCompany, ...otherCompanies]
+
   before(() => {
-    cy.visit('/companies?sortby=collectionTest')
+    cy.intercept('POST', '/api-proxy/v4/search/company', {
+      body: {
+        count: companyList.length,
+        results: companyList,
+      },
+    }).as('apiRequest')
+    cy.visit(companies.react.index())
+    cy.wait('@apiRequest')
+  })
+
+  beforeEach(() => {
+    cy.get('[data-test="collection-item"]').as('collectionItems')
+    cy.get('@collectionItems').eq(0).as('firstListItem')
   })
 
   it('should render home breadcumb', () => {
@@ -14,12 +51,13 @@ describe('Company Collection Address', () => {
     })
   })
 
-  it('should contain california displaying area for a us address', () => {
-    cy.get(selectors.entityCollection.entity(5))
-      .should('contain', 'Social Gaming Network')
-      .and(
+  it('should contain california displaying area for an US address', () => {
+    cy.get('@firstListItem')
+      .debug()
+      .find('[data-test="metadata"]')
+      .should(
         'contain',
-        ' 3525 Eastham Drive, Culver City, CA, 90232, California, United States'
+        '3525 Eastham Drive, Culver City, CA, 90232, California, United States'
       )
   })
 })
