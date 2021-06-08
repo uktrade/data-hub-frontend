94d936218d5ce4b4e8e73793fdb2c329fc09a826
Tom Kingston <tom@kingstonlabs.co.uk>
2021-06-08 16:28:04 +0100

    Format archived get param correctly for api

--- src/apps/companies/client/tasks.js
+++ src/apps/companies/client/tasks.js
@@ -11,15 +11,17 @@ import { transformResponseToCompanyCollection } from './transformers'
 
 const handleError = (error) => Promise.reject(Error(error.response.data.detail))
 
-function getCompanies({ limit = 10, page, ...rest }) {
-  let offset = limit * (parseInt(page, 10) - 1) || 0
+const getCompanies = ({ limit = 10, page, archived, ...rest }) => {
+  const offset = limit * (parseInt(page, 10) - 1) || 0
+  let options = { limit, offset, ...rest }
+
+  // The archived param only needs to be present when a single option is picked
+  if (archived && archived.length === 1) {
+    options.archived = archived[0] === 'true'
+  }
 
   return axios
-    .post('/api-proxy/v4/search/company', {
-      limit,
-      offset,
-      ...rest,
-    })
+    .post('/api-proxy/v4/search/company', options)
     .then(({ data }) => transformResponseToCompanyCollection(data))
     .catch(handleError)
 }
@@ -31,7 +33,7 @@ function getCompanies({ limit = 10, page, ...rest }) {
  *
  * @returns {promise} - the promise containing a list of options for each category
  */
-function getCompaniesMetadata() {
+const getCompaniesMetadata = () => {
   return Promise.all([
     getSectorOptions(urls.metadata.sector()),
     getHeadquarterTypeOptions(urls.metadata.headquarterType()),
