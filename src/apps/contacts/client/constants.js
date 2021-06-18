808939cb671814286149a15bdead6765f940983f
Paul Gain <paul@javexconsulting.com>
2021-06-18 13:52:15 +0100

    Visual design refinements

--- src/apps/contacts/client/constants.js
+++ src/apps/contacts/client/constants.js
@@ -13,11 +13,11 @@ export const STATUS_OPTIONS = [
 ]
 
 export const SORT_OPTIONS = [
-  { value: 'created_on:desc', name: 'Newest' },
+  { value: 'created_on:desc', name: 'Recently created' },
   { value: 'created_on:asc', name: 'Oldest' },
   { value: 'modified_on:desc', name: 'Recently updated' },
   { value: 'modified_on:asc', name: 'Least recently updated' },
-  { value: 'last_name:asc', name: 'Last name: A-Z' },
-  { value: 'address_country.name:asc', name: 'Country: A-Z' },
-  { value: 'company.name:asc', name: 'Company: A-Z' },
+  { value: 'last_name:asc', name: 'Last name A-Z' },
+  { value: 'address_country.name:asc', name: 'Country A-Z' },
+  { value: 'company.name:asc', name: 'Company A-Z' },
 ]
