9fd42b610d32192f8fc3b3b993db798f9e7c0742
Ian Leggett <contactleggett@gmail.com>
2021-06-21 15:25:34 +0100

    Add adviser typeahead filter

--- src/apps/interactions/client/InteractionsCollection.jsx
+++ src/apps/interactions/client/InteractionsCollection.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import { INTERACTIONS__LOADED } from '../../../client/actions'
+import {
+  INTERACTIONS__LOADED,
+  INTERACTIONS_SELECTED_ADVISERS,
+} from '../../../client/actions'
 import { LABELS, KIND_OPTIONS } from './constants'
 
 import {
   FilteredCollectionList,
   RoutedCheckboxGroupField,
+  RoutedAdvisersTypeahead,
   CollectionFilters,
 } from '../../../client/components'
 
-import { ID, state2props, TASK_GET_INTERACTIONS_LIST } from './state'
+import {
+  ID,
+  state2props,
+  TASK_GET_INTERACTIONS_LIST,
+  TASK_GET_INTERACTIONS_ADVISER_NAME,
+} from './state'
 
 const InteractionCollection = ({
   payload,
@@ -25,6 +34,15 @@ const InteractionCollection = ({
     startOnRender: {
       payload,
       onSuccessDispatch: INTERACTIONS__LOADED,
+    },
+  }
+  const adviserListTask = {
+    name: TASK_GET_INTERACTIONS_ADVISER_NAME,
+    id: ID,
+    progressMessage: 'loading advisers',
+    startOnRender: {
+      payload: payload.adviser,
+      onSuccessDispatch: INTERACTIONS_SELECTED_ADVISERS,
     },
   }
   return (
@@ -49,6 +67,17 @@ const InteractionCollection = ({
           options={KIND_OPTIONS}
           selectedOptions={selectedFilters.selectedKind}
           data-test="status-filter"
+        />
+        <RoutedAdvisersTypeahead
+          taskProps={adviserListTask}
+          isMulti={true}
+          legend="Advisers"
+          name="adviser"
+          qsParam="adviser"
+          placeholder="Search advisers"
+          noOptionsMessage={() => <>No advisers found</>}
+          selectedOptions={selectedFilters.selectedAdvisers}
+          data-test="adviser-filter"
         />
       </CollectionFilters>
     </FilteredCollectionList>
