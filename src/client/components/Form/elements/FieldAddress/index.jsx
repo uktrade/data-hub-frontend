f0db1af0edd9277d29dbc62b436945d2183c37d0
Peter Hudec <peterhudec@peterhudec.com>
2021-06-21 17:28:48 +0100

    Remove the name prop from FieldAddress

--- src/client/components/Form/elements/FieldAddress/index.jsx
+++ src/client/components/Form/elements/FieldAddress/index.jsx
@@ -36,7 +36,6 @@ const StyledFieldPostcode = styled(FieldInput)`
 `
 
 const FieldAddress = ({
-  name,
   label,
   legend,
   hint,
@@ -156,7 +155,7 @@ const FieldAddress = ({
   }
 
   return (
-    <FieldWrapper {...{ name, label, legend, hint }} showBorder={true}>
+    <FieldWrapper {...{ label, legend, hint }} showBorder={true}>
       {isUK && (
         <>
           <StyledFieldPostcode
@@ -243,7 +242,6 @@ const FieldAddress = ({
 }
 
 FieldAddress.propTypes = {
-  name: PropTypes.string.isRequired,
   label: PropTypes.node,
   legend: PropTypes.node,
   hint: PropTypes.node,
