1c5a6d6b96e1fe5bbe24408161938983b63397db
Peter Hudec <peterhudec@peterhudec.com>
2021-06-21 18:08:54 +0100

    Add documentation to the Resource component

--- src/client/components/Resource/index.jsx
+++ src/client/components/Resource/index.jsx
@@ -1,10 +1,30 @@
 import _ from 'lodash'
+import PropTypes from 'prop-types'
 import React from 'react'
 
 import multiInstance from '../../utils/multiinstance'
 import { apiProxyAxios } from '../Task/utils'
 import Task from '../Task'
 
+/**
+ * @function Resource
+ * This component abstracts away the loading of a resource which has an ID.
+ * It's basically a {Task} whith the result persisted in the state without the
+ * need to deal with the {onSuccessDispatch} mechanism and to add any reducers.
+ * @param {Object} props
+ * @param {string} props.name - The name of the task which loads the resource
+ * @param {string} props.id - The unique ID of the resource for the given name.
+ * The ID will be the task's payload.
+ * @param {result -> React.node} props.children - The single child should be
+ * a function which will be passed the value the task resolves with with its
+ * attributes deeply converted from snake to camel case in case it's an object.
+ * @example
+ * <Resource name="My task name" id="foo">
+ *   {result =>
+ *     <pre>{JSON.stringify(result, null, 2)}</pre>
+ *   }
+ * </Resource>
+ */
 const Resource = multiInstance({
   name: 'Resource',
   actionPattern: 'RESOURCE',
@@ -25,6 +45,13 @@ const Resource = multiInstance({
   ),
 })
 
+Resource.propTypes = {
+  name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  children: PropTypes.func,
+  taskStatusProps: Task.Status.propTypes,
+}
+
 export default Resource
 
 const deepKeysToCamelCase = (x) =>
@@ -39,6 +66,24 @@ const deepKeysToCamelCase = (x) =>
     ])
   )
 
+/**
+ * A utility factory for creating a {Resource} preset to a specific API endpoint
+ * @param {string} name - The name of the resource and its task.
+ * @param {*} endpoint - The path of the API endpoint for the given resource
+ * without the leading slash.
+ * @returns A resource component preconfigured for a specific task name,
+ * which doesn't require the {name} prop.
+ * @example
+ * const Company = createResource('Company', 'v4/company')
+ * // Then in src/client/index.jsx you can just spread Company.tasks into
+ * // the tasks prop of the Provider component
+ * <Provider tasks={{
+ *   // ...
+ *   ...Company.tasks,
+ * }}>
+ *   // ...
+ * </Provider/>
+ */
 export const createResource = (name, endpoint) => {
   const Component = (props) => <Resource {...props} name={name} />
   Component.propTypes = _.omit(Component.propTypes, 'name')
