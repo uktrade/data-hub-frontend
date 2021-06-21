8a8400e55dab3a635540022b303cb5a96fb19a59
Ian Leggett <contactleggett@gmail.com>
2021-06-21 15:25:34 +0100

    Add adviser typeahead filter

--- src/apps/interactions/client/tasks.js
+++ src/apps/interactions/client/tasks.js
@@ -4,11 +4,12 @@ import { transformResponseToCollection } from './transformers'
 
 const handleError = (e) => Promise.reject(Error(e.response.data.detail))
 
-export const getInteractions = ({ limit = 10, page = 1, kind }) =>
+export const getInteractions = ({ limit = 10, page = 1, kind, adviser }) =>
   axios
     .post('/api-proxy/v3/search/interaction', {
       limit,
       kind,
+      dit_participants__adviser: adviser,
       offset: limit * (page - 1),
       sortby: 'date:desc',
     })
