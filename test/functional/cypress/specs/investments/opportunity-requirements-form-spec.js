2d48ac5aab002ce9c4a201ed3b916e31253d4646
Miriam Forner <mforner13@gmail.com>
2021-07-15 11:15:20 +0100

    WIP tests

--- test/functional/cypress/specs/investments/opportunity-requirements-form-spec.js
+++ test/functional/cypress/specs/investments/opportunity-requirements-form-spec.js
@@ -1,153 +1,101 @@
-const { 
-  assertFormFields,
-  assertBreadcrumbs,
-  assertLocalHeader,
-  assertFieldInput, 
-  assertFieldCheckbox, 
-  assertFieldRadios 
-} = require('../../support/assertions')
-const fixtures = require('../../fixtures')
+// const {
+//   assertFormFields,
+//   assertBreadcrumbs,
+//   assertLocalHeader,
+//   assertFieldInput,
+//   assertFieldCheckbox,
+//   assertFieldRadios,
+// } = require('../../support/assertions')
+// const fixtures = require('../../fixtures')
 const { investments } = require('../../../../../src/lib/urls')
 
-const capitalOpportunitiesCompleteRequirements = require('../../../../sandbox/fixtures/v4/investment/large-capital-opportunity-complete.json')
-const capitalOpportunitiesIncompleteRequirements = require('../../../../sandbox/fixtures/v4/investment/large-capital-opportunity-incomplete.json')
+const opportunityWithCompleteRequirements = require('../../../../sandbox/fixtures/v4/investment/large-capital-opportunity-complete.json')
+const opportunityWithIncompleteRequirements = require('../../../../sandbox/fixtures/v4/investment/large-capital-opportunity-incomplete.json')
 
-const { transformInvestmentOpportunityDetails } = require('../../../../../src/apps/investments/client/opportunities/Details/transformers')
+// const {
+//   transformInvestmentOpportunityDetails,
+// } = require('../../../../../src/apps/investments/client/opportunities/Details/transformers')
 
-const assertSelectFieldRadios = ({ element, legend, value }) => 
-  cy 
-    .wrap(element)
-    .as('fieldRadio')
-    .find('legend')
-    .first()
-    .should('have.text', legend)
-    .parent()
-    .find('input')
-    .should('have.length', 4)
-    .then(
-      () =>
-        value &&
-        cy
-          .get('@fieldRadio')
-          .find('input:checked')
-          .next()
-    )
+// const assertSelectFieldRadios = ({ element, legend, value }) =>
+//   cy
+//     .wrap(element)
+//     .as('fieldRadio')
+//     .find('legend')
+//     .first()
+//     .should('have.text', legend)
+//     .parent()
+//     .find('input')
+//     .should('have.length', 4)
+//     .then(() => value && cy.get('@fieldRadio').find('input:checked').next())
 
 const navigateToForm = (opportunity) => {
   cy.visit(investments.opportunities.details(opportunity.id))
 }
 
-const testOpportunityRequirementsForm = ({ opportunity }) => {
-  const requirementsFields = transformInvestmentOpportunityDetails(opportunity)
-  before(() => {
-    navigateToForm(opportunity)
-    cy.get(
-      '#opportunity_requirements_toggle > div > [data-test="toggle-section-button"]'
-    ).click()
-  })
-
-  it('should render breadcrumbs', () => {
-    assertBreadcrumbs({
-      Home: '/',
-      Investments: '/investments',
-      'UK Opportunities': investments.opportunities.index(),
-      [opportunity.name]: null,
-    })
-  })
-
-  it('should render the header', () => {
-    assertLocalHeader(opportunity.name)
-    cy.get('#localHeaderDetails')
-        .contains('li', 'Status')
-  })
-
-  it('should display the Edit button', () => {
-    cy.get(
-      '#opportunity_requirements_toggle > div > [data-test="toggle-section-button"]'
+describe('A capital opportunity without existing requirements', () => {
+  context('When editing an opportunity without existing requirements', () => {
+    beforeEach(() => {
+      navigateToForm(opportunityWithIncompleteRequirements)
+      cy.get(
+        '#opportunity_requirements_toggle > div > [data-test="toggle-section-button"]'
       ).click()
-  })
-
-  it('should render expected form fields with existing values', () => {
-    assertFormFields(cy.get('form'), [
-      {
-        assert: assertFieldInput,
-        label: 'Total investment sought',
-        value: requirementsFields.totalInvestmentSought,
-      }, 
-      {
-        assert: assertFieldInput,
-        label: 'Investment secured so far',
-        value: requirementsFields.currentInvestmentSecured
-      },
-      {
-        assert: assertFieldCheckbox,
-        legend: 'Types of investment',
-        value: requirementsFields.investmentTypes,
-        optionCount: 8,
-      },
-      {
-        assert: assertSelectFieldRadios,
-        legend: 'Estimated return rate',
-        value: requirementsFields.returnRateValues,
-        optionsCount: 4,
-      },
-      {
-        assert: assertSelectFieldRadios,
-        legend: 'Timescales',
-        value: requirementsFields.timeHorizonsValues,
-        optionsCount: 4,
-      },
-    ])
-  })
-
-}
-
-describe('Edit the capital opportunity requirements', () => {
-  context('When editing capital opportunities without existing requirements data', () => {
-    testOpportunityRequirementsForm({ opportunity:  capitalOpportunitiesIncompleteRequirements })
-
+      cy.get('#opportunity_requirements_toggle').contains('Edit').click()
+    })
+    it('should display the requirements form when you click "Edit"', () => {
+      cy.get('form').should('be.visible')
+    })
+    it('should take user back to the table view if they click "Cancel"', () => {
+      cy.contains('Cancel').click()
+      cy.get(
+        '#opportunity_requirements_toggle > div > [data-test="toggle-section-button"]'
+      ).click()
+      cy.get('table').should('be.visible')
+    })
+    it('should allow the form to be submitted with no fields filled in (all optional)', () => {
+      cy.contains('Save').click()
+      cy.get('table').should('be.visible')
+    })
+    it('should give an error if a non-numerical value is given for GBP value fields', () => {
+      cy.get('#total_investment_sought').type('hello')
+      cy.get('#current_investment_secured').type('hello')
+      cy.contains('Save').click()
+      cy.get('#form-errors').should('be.visible')
+      cy.get('#form-errors').should(
+        'contain',
+        'Total investment sought value must be a number'
+      )
+      cy.get('#form-errors').should(
+        'contain',
+        'Investment secured so far value must be a number'
+      )
+      cy.get('#field-total_investment_sought').should(
+        'contain',
+        'Total investment sought value must be a number'
+      )
+      cy.get('#field-current_investment_secured').should(
+        'contain',
+        'Investment secured so far value must be a number'
+      )
+    })
     it('should allow the user to fill in all fields', () => {
-      cy.get('[data-test="total_investment_sought"]').type('100')
-      cy.get('[data-test="current_investment_secured"]').type('200')
-      cy.get('[data-test="investment_types"]').select('Direct Investment in Corporate Debt')
-      cy.get('[data-test="estimated_return_rate"]').select('Up to 5% IRR')
-      cy.get('[data-test="time_horizons"]').select('5-9 years')
+      cy.get('#total_investment_sought').type('100')
+      cy.get('#current_investment_secured').type('200')
+      cy.get('[type="checkbox"]').check('Direct Investment in Corporate Debt')
+      cy.get('#estimated_return_rate').select('Up to 5% IRR')
+      cy.get('#time_horizons').select('5-9 years')
       cy.contains('Save').click()
     })
+    // TODO intercept request to API and check that the fields are right in it!!
   })
 
-  context('When editing capital opportunities with existing requirements data', () => {
-    testOpportunityRequirementsForm({ opportunity: capitalOpportunitiesCompleteRequirements })
-
-    it('should diplay existing requirements values', () => {
-      cy.get('[data-test="values"]').should('have.values')
+  context('When editing an opportunity with existing requirements', () => {
+    beforeEach(() => {
+      navigateToForm(opportunityWithCompleteRequirements)
+      cy.get(
+        '#opportunity_requirements_toggle > div > [data-test="toggle-section-button"]'
+      ).click()
+      cy.get('#opportunity_requirements_toggle').contains('Edit').click()
     })
+    it('should diplay existing values in the form', () => {})
   })
-
-  context('When making changes that miss-out required fields', () => {
-    testOpportunityRequirementsForm({ opportunity: capitalOpportunitiesIncompleteRequirements })
-
-    it('should highlight errorneous from input', () => {
-      cy.get('[data-test="values"]')
-        .should('have.class', 'has-error')
-        .contains('required')
-    })
-  })
-
-  context('When no changes with existing requirements data', () => {
-    testOpportunityRequirementsForm({ opportunity: capitalOpportunitiesCompleteRequirements })
-
-    it('should allow to user to cancel and edit functionality', () => {
-      cy.contains('Cancel').click()
-    })
-  })
-
-  context('When no changes without existing requirements data', () => {
-    testOpportunityRequirementsForm({ opportunity: capitalOpportunitiesIncompleteRequirements })
-    
-    it('should allow to user to cancel and edit functionality', () => {
-      cy.contains('Cancel').click()
-    })
-  })
-
 })
