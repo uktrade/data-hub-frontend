1b2ee9a8814427d0fa6cb7e8673c4847f4b2f60e
Ian Leggett <contactleggett@gmail.com>
2021-07-21 15:31:28 +0100

    Swap out the Nunjucks interactions collection for the React interaction list

--- src/apps/interactions/router.js
+++ src/apps/interactions/router.js
@@ -1,38 +1,36 @@
 const router = require('express').Router()
 
-const urls = require('../../lib/urls')
-
 const {
   DEFAULT_COLLECTION_QUERY,
   APP_PERMISSIONS,
   QUERY_FIELDS,
   QUERY_DATE_FIELDS,
 } = require('./constants')
-const { renderInteractionList } = require('./controllers/list')
+// const { renderInteractionList } = require('./controllers/list')
 const {
   exportCollection,
 } = require('../../modules/search/middleware/collection')
 const { getRequestBody } = require('../../middleware/collection')
 
 const { setDefaultQuery, handleRoutePermissions } = require('../middleware')
-const {
-  getInteractionCollection,
-  getInteractionsRequestBody,
-  getInteractionSortForm,
-} = require('./middleware/collection')
+// const {
+//   getInteractionCollection,
+//   getInteractionsRequestBody,
+//   getInteractionSortForm,
+// } = require('./middleware/collection')
 
 const subAppRouter = require('./router.sub-app')
 
 router.use(handleRoutePermissions(APP_PERMISSIONS))
 
-router.get(
-  '/',
-  setDefaultQuery(DEFAULT_COLLECTION_QUERY),
-  getInteractionsRequestBody,
-  getInteractionCollection,
-  getInteractionSortForm,
-  renderInteractionList
-)
+// router.get(
+//   '/',
+//   setDefaultQuery(DEFAULT_COLLECTION_QUERY),
+//   getInteractionsRequestBody,
+//   getInteractionCollection,
+//   getInteractionSortForm,
+//   renderInteractionList
+// )
 
 router.get(
   '/export',
@@ -41,7 +39,7 @@ router.get(
   exportCollection('interaction')
 )
 
-router.get(urls.interactions.react.route, (req, res, next) => {
+router.get('/', (req, res, next) => {
   try {
     const { user } = req.session
     return res.render('interactions/views/interactions', {
