f327ac35554a24ec5e70519eca93d1d1642cf4ed
Miriam Forner <mforner13@gmail.com>
2021-07-21 15:24:03 +0100

    Create LocalHeaderDetails component with story

---
+++ src/client/components/LocalHeaderDetails/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { GREY_1, GREY_4 } from 'govuk-colours'
+import { SPACING } from '@govuk-react/constants'
+
+const StyledHeaderDetails = styled('div')`
+  background-color: ${GREY_4};
+`
+
+const StyledHeaderList = styled('li')`
+  display: inline-block;
+  padding-right: ${SPACING.SCALE_5};
+`
+
+const StyledHeaderListLabel = styled('label')`
+  color: ${GREY_1};
+`
+
+const LocalHeaderDetails = ({ items }) => (
+  <StyledHeaderDetails
+    aria-label="local header details"
+    data-test="localHeaderDetails"
+    role="region"
+  >
+    {items.map((item) => (
+      <StyledHeaderList key={item.label}>
+        <StyledHeaderListLabel>{item.label}</StyledHeaderListLabel>
+        <p>{item.value}</p>
+      </StyledHeaderList>
+    ))}
+  </StyledHeaderDetails>
+)
+
+LocalHeaderDetails.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+}
+
+export default LocalHeaderDetails
