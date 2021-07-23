13d46bf0120ea0c3f52ed4590d34019cc8dc3ab9
Edmond DeLos Reyes <dredmonds@gmail.com>
2021-07-23 11:38:31 +0100

    Add team filter to collection list

--- test/functional/cypress/fakers/team.js
+++ test/functional/cypress/fakers/team.js
@@ -1,8 +1,14 @@
 import faker from 'faker'
+import { listFaker } from './utils'
 
 const teamFaker = () => ({
   id: faker.datatype.uuid(),
   name: faker.name.jobDescriptor(),
 })
 
-export default teamFaker
+const teamListFaker = (length = 1, overrides) =>
+  listFaker({ fakerFunction: teamFaker, length, overrides })
+
+export { teamFaker, teamListFaker }
+
+export default teamListFaker
