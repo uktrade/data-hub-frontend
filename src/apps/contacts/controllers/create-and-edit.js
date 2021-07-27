6866a760071651aeaa692e3595b532926e9015be
Peter Hudec <peterhudec@peterhudec.com>
2021-07-27 17:49:49 +0100

    Wrap in try catch

--- src/apps/contacts/controllers/create-and-edit.js
+++ src/apps/contacts/controllers/create-and-edit.js
@@ -1,7 +1,12 @@
-module.exports = (req, res) =>
-  res.render('contacts/views/create-and-edit', {
-    props: {
-      contactId: req.params.contactId,
-      companyId: req.query.company,
-    },
-  })
+module.exports = (req, res, next) => {
+  try {
+    res.render('contacts/views/create-and-edit', {
+      props: {
+        contactId: req.params.contactId,
+        companyId: req.query.company,
+      },
+    })
+  } catch (e) {
+    next(e)
+  }
+}
