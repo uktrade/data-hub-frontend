d129dcaf7d687c6a727dcc819280879527f14abc
Paul Gain <paul@javexconsulting.com>
2021-08-06 15:13:55 +0100

    Update dependencies

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,7 +1,4 @@
 import { addons } from '@storybook/addons'
-import 'storybook-readme/register'
-import '@storybook/addon-actions/register'
-import '@storybook/addon-knobs/register'
 
 addons.setConfig({
   panelPosition: 'right',
