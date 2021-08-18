7d4b3cf56e715d4d697035e806d6b462a141a627
Pippo Raimondi <filipporai@gmail.com>
2021-08-18 13:19:25 +0100

    Add waits and adjust screen resolution before taking screenshots

--- test/visual/cypress/specs/interaction-spec.js
+++ test/visual/cypress/specs/interaction-spec.js
@@ -2,7 +2,10 @@ const urls = require('../../../../src/lib/urls')
 
 describe('interaction page', () => {
   before(() => {
+    cy.viewport(1980, 1440)
+    cy.intercept('POST', '/api-proxy/v3/search/interaction').as('apiRequest')
     cy.visit(urls.interactions.index())
+    cy.wait('@apiRequest')
   })
 
   it('should render interaction page correctly', () => {
--- test/visual/cypress/specs/investment-spec.js
+++ test/visual/cypress/specs/investment-spec.js
@@ -2,7 +2,12 @@ const urls = require('../../../../src/lib/urls')
 
 describe('investment project', () => {
   before(() => {
+    cy.viewport(1980, 1440)
+    cy.intercept('POST', '/api-proxy/v3/search/investment_project').as(
+      'apiRequest'
+    )
     cy.visit(urls.investments.index())
+    cy.wait('@apiRequest')
   })
 
   it('should render investment project page correctly', () => {
@@ -13,7 +18,12 @@ describe('investment project', () => {
 
 describe('investment profile', () => {
   before(() => {
+    cy.viewport(1980, 1440)
+    cy.intercept('POST', '/api-proxy/v4/search/large-investor-profile').as(
+      'apiRequest'
+    )
     cy.visit(urls.investments.profiles.index())
+    cy.wait('@apiRequest')
   })
 
   it('should render investment profile page correctly', () => {
@@ -24,7 +34,12 @@ describe('investment profile', () => {
 
 describe('investment opportunity', () => {
   before(() => {
+    cy.viewport(1980, 1440)
+    cy.intercept('POST', '/api-proxy/v4/search/large-capital-opportunity').as(
+      'apiRequest'
+    )
     cy.visit(urls.investments.opportunities.index())
+    cy.wait('@apiRequest')
   })
 
   it('should render investment opportunity page correctly', () => {
