08a7d58eb45efb2742a2a1b01ccde86f426839b7
Edmond DeLos Reyes <dredmonds@gmail.com>
2021-09-13 12:43:10 +0100

    New implementation of event-details tab information using reactjs

---
+++ test/functional/cypress/specs/events/details-react-spec.js
@@ -0,0 +1,57 @@
+const { assertKeyValueTable } = require('../../support/assertions')
+
+const fixtures = require('../../fixtures')
+const selectors = require('../../../../selectors')
+
+describe('Event Details', () => {
+  it('should display one day event details with files documents', () => {
+    cy.visit(`/events/${fixtures.event.oneDayExhibition.id}/details`)
+
+    cy.get(selectors.entityCollection.editEvent).should('be.visible')
+
+    assertKeyValueTable('eventDetails', {
+      'Type of event': 'Exhibition',
+      'Event date': '1 January 2021',
+      'Event location type': 'HQ',
+      Address:
+        'Day Court Exhibition Centre, Day Court Lane, China, SW9 9AB, China',
+      Region: '',
+      Notes: 'This is a dummy event for testing.',
+      'Lead team': 'CBBC Hangzhou',
+      Organiser: 'John Rogers',
+      'Other teams': 'CBBC HangzhouCBBC North West',
+      'Related programmes': 'Grown in Britain',
+      'Related Trade Agreements': 'UK - Japan',
+      Service: 'Events : UK Based',
+      Documents: 'View files and documents (opens in a new window or tab)',
+    })
+  })
+
+  describe('Disabled event with no document', () => {
+    before(() => {
+      cy.visit(`/events/${fixtures.event.teddyBearExpo.id}/details`)
+    })
+
+    it('should display no document link details', () => {
+      assertKeyValueTable('eventDetails', {
+        'Type of event': 'Exhibition',
+        'Event date': '1 January 2021',
+        'Event location type': 'HQ',
+        Address:
+          'Day Court Exhibition Centre, Day Court Lane, China, SW9 9AB, China',
+        Region: '',
+        Notes: 'This is a dummy event for testing.',
+        'Lead team': 'CBBC Hangzhou',
+        Organiser: 'John Rogers',
+        'Other teams': 'CBBC HangzhouCBBC North West',
+        'Related programmes': 'Grown in Britain',
+        'Related Trade Agreements': '',
+        Service: 'Events : UK Based',
+      })
+    })
+
+    it('should hide edit event button for disabled events', () => {
+      cy.get(selectors.entityCollection.editEvent).should('not.exist')
+    })
+  })
+})
