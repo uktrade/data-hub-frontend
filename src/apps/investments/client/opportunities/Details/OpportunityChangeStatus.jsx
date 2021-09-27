56cc1598cf3c8fe382fb6e983dffb8b50d76c815
Peter Hudec <peterhudec@peterhudec.com>
2021-09-27 13:35:21 +0100

    Simplify code in OpportunityChangeStatus

--- src/apps/investments/client/opportunities/Details/OpportunityChangeStatus.jsx
+++ src/apps/investments/client/opportunities/Details/OpportunityChangeStatus.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import React from 'react'
 import PropTypes from 'prop-types'
+
 import urls from '../../../../../lib/urls'
 import LocalHeader from '../../../../../client/components/LocalHeader/LocalHeader'
 import Button from '@govuk-react/button'
@@ -15,66 +16,55 @@ import {
 import OpportunityResource from '../../../../../client/components/Resource/Opportunity'
 import FieldOpportunityStatuses from '../../../../../client/components/Form/elements/FieldOpportunityStatuses'
 
-const OpportunityChangeStatus = (state) => {
-  const { opportunityId } = state
-
-  const buildBreadcrumbs = (opportunityName) => {
-    return [
-      { link: urls.dashboard(), text: 'Home' },
-      { link: urls.investments.index(), text: 'Investments' },
-      {
-        link: urls.investments.opportunities.index(),
-        text: 'UK opportunities',
-      },
-      {
-        link: urls.investments.opportunities.details(opportunityId),
-        text: opportunityName,
-      },
-      { text: 'Change status' },
-    ]
-  }
+const OpportunityChangeStatus = ({ opportunityId }) => {
+  const opportunityUrl = urls.investments.opportunities.details(opportunityId)
 
   return (
     <OpportunityResource id={opportunityId}>
-      {(opportunity) =>
-        // TODO: Isn't this always true?
-        opportunity.name.length && (
-          <>
-            <LocalHeader
-              breadcrumbs={buildBreadcrumbs(opportunity.name)}
-              heading={'Change opportunity status'}
-            />
-            <Main>
-              <FormStateful
-                showErrorSummary={true}
-                onSubmit={async (values) => {
-                  await axios.patch(
-                    `/api-proxy/v4/large-capital-opportunity/${opportunityId}`,
-                    {
-                      status: values.status,
-                    }
-                  )
-                  return urls.investments.opportunities.details(opportunityId)
-                }}
-              >
-                <FieldOpportunityStatuses
-                  name="status"
-                  initialValue={opportunity.status.id}
-                  data-test="types-of-status"
-                />
-                <FormActions>
-                  <Button data-test="edit-button">Save</Button>
-                  <Link
-                    href={urls.investments.opportunities.details(opportunityId)}
-                  >
-                    Cancel
-                  </Link>
-                </FormActions>
-              </FormStateful>
-            </Main>
-          </>
-        )
-      }
+      {({ name, status }) => (
+        <>
+          <LocalHeader
+            breadcrumbs={[
+              { link: urls.dashboard(), text: 'Home' },
+              { link: urls.investments.index(), text: 'Investments' },
+              {
+                link: urls.investments.opportunities.index(),
+                text: 'UK opportunities',
+              },
+              {
+                link: opportunityUrl,
+                text: name,
+              },
+              { text: 'Change status' },
+            ]}
+            heading={'Change opportunity status'}
+          />
+          <Main>
+            <FormStateful
+              showErrorSummary={true}
+              onSubmit={async (values) => {
+                await axios.patch(
+                  `/api-proxy/v4/large-capital-opportunity/${opportunityId}`,
+                  {
+                    status: values.status,
+                  }
+                )
+                return opportunityUrl
+              }}
+            >
+              <FieldOpportunityStatuses
+                name="status"
+                initialValue={status.id}
+                data-test="types-of-status"
+              />
+              <FormActions>
+                <Button data-test="edit-button">Save</Button>
+                <Link href={opportunityUrl}>Cancel</Link>
+              </FormActions>
+            </FormStateful>
+          </Main>
+        </>
+      )}
     </OpportunityResource>
   )
 }
