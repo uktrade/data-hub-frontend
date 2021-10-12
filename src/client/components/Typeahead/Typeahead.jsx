c0ad4799c22539471cea130d26c26176000cd319
Lauren <laurenqurashi25@gmail.com>
2021-10-12 12:00:07 +0100

    WIP: Add custom components to move Multi Select up

--- src/client/components/Typeahead/Typeahead.jsx
+++ src/client/components/Typeahead/Typeahead.jsx
@@ -5,7 +5,8 @@ import AsyncSelect from 'react-select/async'
 import defaultStyles, { errorStyles } from './styles'
 import Highlighter from './Highlighter'
 
-const { Input, Option, NoOptionsMessage, MenuList } = components
+const { Input, Option, NoOptionsMessage, MenuList, SelectContainer } =
+  components
 
 export const filterOption = ({ label = '' }, query) =>
   label.toLowerCase().includes(query.toLowerCase())
@@ -75,7 +76,77 @@ const AssistiveText = ({ name }) => (
   </span>
 )
 
-const Typeahead = ({ options, styles, error, name, ...props }) => {
+const SelectedValuesContainer = ({ isDisabled, getValue, ...props }) => {
+  const { getOptionValue, formatOptionLabel, removeValue } = props.selectProps
+
+  const getValueLabel = (opt) => formatOptionLabel?.(opt, 'value') || opt.label
+  const getKey = (opt, index) => `${getOptionValue(opt)}-${index}`
+
+  const toMultiValue = (opt, index) => (
+    <components.MultiValue
+      getValue={getValue}
+      {...props}
+      components={{
+        Container: components.MultiValueContainer,
+        Label: components.MultiValueLabel,
+        Remove: components.MultiValueRemove,
+      }}
+      //isFocused={isOptionFocused}
+      isDisabled={isDisabled}
+      key={getKey(opt, index)}
+      index={index}
+      removeProps={{
+        onClick: () => removeValue(opt),
+        onTouchEnd: () => removeValue(opt),
+        onMouseDown: (e) => {
+          e.preventDefault()
+          e.stopPropagation()
+        },
+      }}
+      data={opt}
+    >
+      {getValueLabel(opt)}
+    </components.MultiValue>
+  )
+
+  return (
+    <div style={{ margin: '.5rem 0', display: 'flex', flexFlow: 'row wrap' }}>
+      {getValue().map(toMultiValue)}
+    </div>
+  )
+}
+
+const CustomSelectContainer = ({
+  children,
+  className,
+  innerProps,
+  isFocused,
+  ...commonProps
+}) => {
+  const selectContainerProps = {
+    ...commonProps,
+  }
+
+  return (
+    <SelectContainer
+      className={className}
+      innerProps={innerProps}
+      isFocused={isFocused}
+      {...selectContainerProps}
+    >
+      <SelectedValuesContainer {...commonProps} />
+      {children}
+    </SelectContainer>
+  )
+}
+
+const Typeahead = ({ value, options, styles, error, name, ...props }) => {
+  // const [setValue] = useState()
+  // const onChange = useCallback((newValue) => setValue(newValue), [])
+  // const removeValue = useCallback(
+  //   (removed) => setValue(value.filter((v) => v.value !== removed.value)),
+  //   [value]
+  // )
   const customisedProps = {
     styles: {
       ...(error ? errorStyles : defaultStyles),
@@ -86,6 +157,7 @@ const Typeahead = ({ options, styles, error, name, ...props }) => {
       MenuList: CustomMenuList,
       Input: CustomInput,
       NoOptionsMessage: CustomNoOptionsMessage,
+      SelectContainer: CustomSelectContainer,
     },
     filterOption,
     'data-aria-id': `autocomplete-${name}`,
@@ -96,7 +168,12 @@ const Typeahead = ({ options, styles, error, name, ...props }) => {
   return (
     <>
       {options ? (
-        <Select {...customisedProps} options={options} />
+        <Select
+          {...customisedProps}
+          options={options}
+          // onChange={onChange}
+          // removeValue={removeValue}
+        />
       ) : (
         <AsyncSelect {...customisedProps} />
       )}
