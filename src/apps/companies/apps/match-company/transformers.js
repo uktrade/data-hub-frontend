0a0de4e6585b8b7d9f794a74009d5e3e2d257784
Tom Kingston <tom@kingstonlabs.co.uk>
2021-10-13 16:00:08 +0100

    Only use address area if it is present

--- src/apps/companies/apps/match-company/transformers.js
+++ src/apps/companies/apps/match-company/transformers.js
@@ -1,20 +1,24 @@
-const { omit } = require('lodash')
+const { isEmpty, omit } = require('lodash')
 
 const transformToDnbInvestigation = (
   { id, name, address },
   website,
   telephone_number
 ) => {
-  return {
+  const dnbInvestigation = {
     company: id,
     name,
     website,
     telephone_number,
     address: {
-      ...omit(address, 'country'),
+      ...omit(address, ['country', 'area']),
       country: address.country.id,
     },
   }
+  if (!isEmpty(address.area)) {
+    dnbInvestigation.address.area = address.area
+  }
+  return dnbInvestigation
 }
 
 module.exports = {
