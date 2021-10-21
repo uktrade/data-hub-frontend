daff1bb20a1f74e5a737e07ae25d305754b039d2
cgsunkel <christopher.sunkel@digital.trade.gov.uk>
2021-10-21 14:51:41 +0100

    Add Cypress component test runner and sample tests

--- test/cypress/plugins/index.js
+++ test/cypress/plugins/index.js
@@ -2,6 +2,11 @@
 require('dotenv').config()
 
 module.exports = (on, config) => {
+  if (config.testingType === 'component') {
+    require('@cypress/react/plugins/react-scripts')(on, config)
+
+    return config
+  }
   require('@cypress/code-coverage/task')(on, config)
   on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))
   on("task", { log(message) {
