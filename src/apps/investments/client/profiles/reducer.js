743f75418aea45761ccd2989e42e615363b81ea8
elcct <pawel@mikesz.com>
2021-10-22 14:32:41 +0100

    Remove unused feature flags.

--- src/apps/investments/client/profiles/reducer.js
+++ src/apps/investments/client/profiles/reducer.js
@@ -1,6 +1,5 @@
 import {
   INVESTMENTS__PROFILES_LOADED,
-  INVESTMENTS__PROFILES_SELECT_PAGE,
   INVESTMENTS_PROFILES__FILTER_OPTIONS_LOADED,
 } from '../../../../client/actions'
 
@@ -22,7 +21,7 @@ const initialState = {
   },
 }
 
-export default (state = initialState, { type, page, result }) => {
+export default (state = initialState, { type, result }) => {
   switch (type) {
     case INVESTMENTS__PROFILES_LOADED:
       return {
@@ -30,13 +29,6 @@ export default (state = initialState, { type, page, result }) => {
         count: result.count,
         results: result.results,
         isComplete: true,
-      }
-    // TODO: Remove this after the feature flag capital-investments-filters
-    // is removed
-    case INVESTMENTS__PROFILES_SELECT_PAGE:
-      return {
-        ...state,
-        page,
       }
     case INVESTMENTS_PROFILES__FILTER_OPTIONS_LOADED:
       return {
