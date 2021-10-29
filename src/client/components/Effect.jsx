9897c5186a5f81bf08a728174b95a2597f1a6d0a
Peter Hudec <peterhudec@peterhudec.com>
2021-10-29 16:53:14 +0100

    Add the Effect component

---
+++ src/client/components/Effect.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react'
+
+/**
+ * @function Effect
+ * @description Allows you to use the {React.useEffect} hook as a component.
+ * A typical use case is when you wan't to make a side effect inside of a
+ * function passed as a child to a component.
+ * @param {Object} props
+ * @param {any[]} props.dependencyList - The same as the second argument to the
+ * {React.useEffect} hook, but with all values JSON stringified.
+ * @param {() => undefined | (() => any)} props.effect - The same as the
+ * first argument to the {React.useEffect} hook, but with all values JSON
+ * stringified.
+ * @returns null
+ * @example
+ * <Route>
+ *   {({location: {pathname}}) =>
+ *      // You can't use a hook here, because it's inside a function,
+ *      // but you can use it by proxy of the Effect component
+ *      <Effect dependencyList={[pathname]} effect={() => alert(pathName)} />
+ *   }
+ * </Route>
+ */
+const Effect = ({ dependencyList, effect }) => {
+  useEffect(() => effect(), dependencyList?.map(JSON.stringify))
+  return null
+}
+
+export default Effect
