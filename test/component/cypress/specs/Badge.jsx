85ef7a0f6d49a0c55317383a47b84ff3b5118e73
cgsunkel <christopher.sunkel@digital.trade.gov.uk>
2021-11-03 11:13:37 +0000

    Rewrite Badge test

--- test/component/cypress/specs/Badge.jsx
+++ test/component/cypress/specs/Badge.jsx
@@ -1,56 +1,59 @@
 import React from 'react'
 import { mount } from '@cypress/react'
 import Badge from '../../../../src/client/components/Badge'
-import { GREY_2 } from 'govuk-colours'
 
 describe('Badge', () => {
-  let wrapper
+  const Component = (props) => <Badge {...props} />
 
   const badgeText = 'example'
 
   context('When no props are passed', () => {
     beforeEach(() => {
-      wrapper = mount(<Badge>{badgeText}</Badge>)
+      mount(<Component>{badgeText}</Component>)
     })
 
     it('should set the content with children', () => {
-      wrapper.should('have.text', 'example')
+      cy.get('[data-test="badge"]').should('contain', 'example')
     })
 
     it('should render with a default (grey) border', () => {
-      wrapper.its('borderColour').should('eq', GREY_2)
+      cy.get('[data-test="badge"]').should(
+        'have.css',
+        'border',
+        '2px solid rgb(191, 193, 195)'
+      )
     })
   })
 
   context('When a "label" prop is passed passed', () => {
     beforeEach(() => {
-      wrapper = mount(<Badge label="testLabel">{badgeText}</Badge>)
+      mount(<Component label="testLabel">{badgeText}</Component>)
     })
 
     it('should set the content with children', () => {
-      wrapper.should('have.text', 'example')
+      cy.get('[data-test="badge"]').should('contain', 'example')
     })
 
     it('should render the label prop', () => {
-      wrapper.should('have.text', 'testLabel')
+      cy.get('[data-test="badge"]').should('contain', 'testLabel')
     })
   })
 
   context('When a "borderColour" prop is passed passed', () => {
     beforeEach(() => {
-      wrapper = mount(<Badge borderColour="red">{badgeText}</Badge>)
+      mount(<Component borderColour="red">{badgeText}</Component>)
     })
 
     it('should set the content with children', () => {
-      wrapper.should('have.text', 'example')
-    })
-
-    it('should render the label prop', () => {
-      wrapper.should('have.text', 'testLabel')
+      cy.get('[data-test="badge"]').should('contain', 'example')
     })
 
     it('should render with a red border', () => {
-      wrapper.its('borderColour').should('eq', 'red')
+      cy.get('[data-test="badge"]').should(
+        'have.css',
+        'border',
+        '2px solid rgb(255, 0, 0)'
+      )
     })
   })
 })
