ca87ffd4e851688e59ff767b26fe55aebb61015e
Peter Hudec <peterhudec@peterhudec.com>
2021-11-11 11:49:28 +0000

    Refactor the OpportunityRequirementsForm with TaskForm

--- src/apps/investments/client/opportunities/Details/OpportunityRequirementsForm.jsx
+++ src/apps/investments/client/opportunities/Details/OpportunityRequirementsForm.jsx
@@ -15,17 +15,17 @@ import {
 } from './state'
 import {
   INVESTMENT_OPPORTUNITY__REQUIREMENTS_METADATA_LOADED,
-  INVESTMENT_OPPORTUNITY__REQUIREMENTS_CHANGE,
+  INVESTMENT_OPPORTUNITY__UPDATED,
 } from '../../../../../client/actions'
 
 import Task from '../../../../../client/components/Task'
+import TaskForm from '../../../../../client/components/Task/Form'
 import {
   Main,
   FieldInput,
   FieldCheckboxes,
   FieldRadios,
   FormActions,
-  MultiInstanceForm,
 } from '../../../../../client/components'
 import {
   TOTAL_INVESTMENT_SOUGHT_FIELD_NAME,
@@ -46,7 +46,7 @@ const StyledP = styled('p')`
 const IS_NUMBER = /^[0-9]*$/ // Input validation to eliminates input script injection
 
 const OpportunityRequirementsForm = (state) => {
-  const { opportunityId, opportunity, metadata } = state
+  const { opportunityId, opportunity, metadata, dispatch } = state
   const {
     totalInvestmentSought,
     currentInvestmentSecured,
@@ -62,108 +62,92 @@ const OpportunityRequirementsForm = (state) => {
   } = metadata
 
   return (
-    <Task>
-      {(getTask) => {
-        const saveRequirementsTask = getTask(
-          TASK_SAVE_OPPORTUNITY_REQUIREMENTS,
-          ID
-        )
-        return (
-          <Task.Status
-            name={TASK_GET_OPPORTUNITY_REQUIREMENTS_METADATA}
-            id={ID}
-            startOnRender={{
-              onSuccessDispatch:
-                INVESTMENT_OPPORTUNITY__REQUIREMENTS_METADATA_LOADED,
-            }}
-          >
-            {() =>
-              !!metadata.investmentTypes.length && (
-                <>
-                  <Main>
-                    <MultiInstanceForm
-                      id={ID}
-                      onSubmit={(values) => {
-                        saveRequirementsTask.start({
-                          payload: { values, opportunityId },
-                          onSuccessDispatch:
-                            INVESTMENT_OPPORTUNITY__REQUIREMENTS_CHANGE,
-                        })
-                      }}
-                    >
-                      <FieldInput
-                        label="Total investment sought"
-                        hint="Enter value in £"
-                        name={TOTAL_INVESTMENT_SOUGHT_FIELD_NAME}
-                        initialValue={totalInvestmentSought}
-                        type="text"
-                        validate={(value) =>
-                          !value || IS_NUMBER.test(value)
-                            ? null
-                            : 'Total investment sought value must be a number'
-                        }
-                      />
-                      <FieldInput
-                        label="Investment secured so far"
-                        hint="Enter value in £"
-                        name={CURRENT_INVESTMENT_SECURED_FIELD_NAME}
-                        initialValue={currentInvestmentSecured}
-                        type="text"
-                        validate={(value) =>
-                          !value || IS_NUMBER.test(value)
-                            ? null
-                            : 'Investment secured so far value must be a number'
-                        }
-                      />
-                      <StyledFieldCheckboxes
-                        legend="Types of investment"
-                        name={INVESTMENT_TYPES_FIELD_NAME}
-                        initialValue={investmentTypesValues.map(
-                          (obj) => obj.value
-                        )}
-                        options={investmentTypesOptions}
-                        data-test="types-of-investment"
-                      />
-                      <StyledP>
-                        If you can’t see the type of investment you need,
-                        request it from{' '}
-                        <Link>capitalinvestment@trade.gov.uk</Link>.
-                      </StyledP>
-                      <FieldRadios
-                        legend="Estimated return rate"
-                        name={ESTIMATED_RETURN_RATE_FIELD_NAME}
-                        initialValue={
-                          returnRateValues && returnRateValues.value
-                        }
-                        options={returnRatesOptions}
-                        data-test="estimated-return-rate"
-                      />
-                      <FieldRadios
-                        legend="Timescales"
-                        name={TIME_HORIZONS_FIELD_NAME}
-                        initialValue={timeHorizonsValues[0]?.value}
-                        options={timeScalesOptions}
-                        data-test="timescales"
-                      />
-                      <FormActions>
-                        <Button data-test="edit-button">Save</Button>
-                        <Link
-                          href={url.investments.opportunities.details(
-                            opportunityId
-                          )}
-                        >
-                          Cancel
-                        </Link>
-                      </FormActions>
-                    </MultiInstanceForm>
-                  </Main>
-                </>
-              )
-            }
-          </Task.Status>
-        )
+    <Task.Status
+      name={TASK_GET_OPPORTUNITY_REQUIREMENTS_METADATA}
+      id={ID}
+      startOnRender={{
+        onSuccessDispatch: INVESTMENT_OPPORTUNITY__REQUIREMENTS_METADATA_LOADED,
       }}
-    </Task>
+    >
+      {() =>
+        !!metadata.investmentTypes.length && (
+          <>
+            <Main>
+              <TaskForm
+                id="opportunity-requirements"
+                submissionTaskName={TASK_SAVE_OPPORTUNITY_REQUIREMENTS}
+                analyticsFormName="Opportunity requirements"
+                transformPayload={(values) => ({ opportunityId, values })}
+                onSuccess={(opportunity) =>
+                  dispatch({
+                    type: INVESTMENT_OPPORTUNITY__UPDATED,
+                    opportunity,
+                  })
+                }
+              >
+                <FieldInput
+                  label="Total investment sought"
+                  hint="Enter value in £"
+                  name={TOTAL_INVESTMENT_SOUGHT_FIELD_NAME}
+                  initialValue={totalInvestmentSought}
+                  type="text"
+                  validate={(value) =>
+                    !value || IS_NUMBER.test(value)
+                      ? null
+                      : 'Total investment sought value must be a number'
+                  }
+                />
+                <FieldInput
+                  label="Investment secured so far"
+                  hint="Enter value in £"
+                  name={CURRENT_INVESTMENT_SECURED_FIELD_NAME}
+                  initialValue={currentInvestmentSecured}
+                  type="text"
+                  validate={(value) =>
+                    !value || IS_NUMBER.test(value)
+                      ? null
+                      : 'Investment secured so far value must be a number'
+                  }
+                />
+                <StyledFieldCheckboxes
+                  legend="Types of investment"
+                  name={INVESTMENT_TYPES_FIELD_NAME}
+                  initialValue={investmentTypesValues.map((obj) => obj.value)}
+                  options={investmentTypesOptions}
+                  data-test="types-of-investment"
+                />
+                <StyledP>
+                  If you can’t see the type of investment you need, request it
+                  from <Link>capitalinvestment@trade.gov.uk</Link>.
+                </StyledP>
+                <FieldRadios
+                  legend="Estimated return rate"
+                  name={ESTIMATED_RETURN_RATE_FIELD_NAME}
+                  initialValue={returnRateValues && returnRateValues.value}
+                  options={returnRatesOptions}
+                  data-test="estimated-return-rate"
+                />
+                <FieldRadios
+                  legend="Timescales"
+                  name={TIME_HORIZONS_FIELD_NAME}
+                  initialValue={timeHorizonsValues[0]?.value}
+                  options={timeScalesOptions}
+                  data-test="timescales"
+                />
+                <FormActions>
+                  <Button data-test="edit-button">Save</Button>
+                  <Link
+                    href={url.investments.opportunities.details(opportunityId)}
+                  >
+                    Cancel
+                  </Link>
+                </FormActions>
+              </TaskForm>
+            </Main>
+          </>
+        )
+      }
+    </Task.Status>
   )
 }
 
