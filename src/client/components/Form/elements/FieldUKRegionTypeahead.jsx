c21f30ebe4a3582f170a6adae03f01e79fc0aba5
Peter Hudec <peterhudec@peterhudec.com>
2021-11-11 12:28:07 +0000

    Add the Resource/UKRegion and FieldUKRegionTypeahead component

---
+++ src/client/components/Form/elements/FieldUKRegionTypeahead.jsx
@@ -0,0 +1,29 @@
+import _ from 'lodash'
+import React from 'react'
+
+import UKRegionsResource from '../../Resource/UKRegions'
+import FieldTypeahead from './FieldTypeahead'
+
+const FieldUKRegionTypeahead = ({ ...props }) => (
+  <UKRegionsResource>
+    {(regions) => (
+      <FieldTypeahead
+        {...props}
+        label="UK regions"
+        options={regions.map(({ name, id }) => ({ label: name, value: id }))}
+        placeholder="-- Select UK region --"
+        aria-label="Select a uk region"
+      />
+    )}
+  </UKRegionsResource>
+)
+
+FieldUKRegionTypeahead.propTypes = _.omit(
+  FieldTypeahead,
+  'label',
+  'options',
+  'placeholder',
+  'aria-label'
+)
+
+export default FieldUKRegionTypeahead
