9d7fe5b0cb4743167c951f991afcec3d86090eeb
Peter Hudec <peterhudec@peterhudec.com>
2021-11-24 18:31:00 +0000

    Add the Task/ReactSelect and Task/Typehead components

--- src/client/components/Task/__stories__/TaskTypeahead.stories.jsx
+++ src/client/components/Task/__stories__/TaskTypeahead.stories.jsx
@@ -1,67 +1,39 @@
-/* eslint-disable */
 import React from 'react'
-import { storiesOf, addDecorator } from '@storybook/react'
-
-import { number, withKnobs } from '@storybook/addon-knobs'
+import { storiesOf } from '@storybook/react'
 
 import TaskReactSelect from '../ReactSelect'
 import TaskTypeahead from '../Typeahead'
 
-addDecorator(withKnobs)
-
-const delayKnobOptions = {range: true, min: 0, max: 10000, step: 500}
-
-const Base = ({component: Component, id, delay, ...props}) =>
-  <Component
-    id={`${Component.displayName}_${id}`}
-    optionsTaskName="Task typeahead example options"
-    defaultValues={['foo', 'bar']}
-    labelTaskName="Task typeahead example label"
-    interceptOptionsPayload={payload => ({
-      query: payload,
-      delay,
-    })}
-    {...props}
-  />
-
-storiesOf('Task/Typeahead')
-  .add('Multi-select', () => {
-    const delay = number('Options delay', 500, delayKnobOptions)
-    return (
-      <>
-        <h2><code>Task/Typeahead</code></h2>
-        <Base
-          component={TaskTypeahead}
-          id="multi-select"
-          isMulti={true}
-          delay={delay}
-        />
-        <h2><code>Task/ReactSelect</code></h2>
-        <Base
-          component={TaskReactSelect}
-          id="multi-select"
-          isMulti={true}
-          delay={delay}
-        />
-      </>
-    )
-  })
-  .add('Single-select', () => {
-    const delay = number('Options delay', 1000, delayKnobOptions)
-    return (
-      <>
-        <h2><code>Task/Typeahead</code></h2>
-        <Base
-          component={TaskTypeahead}
-          id="single-select"
-          delay={delay}
-        />
-        <h2><code>Task/ReactSelect</code></h2>
-        <Base
-          component={TaskReactSelect}
-          id="single-select"
-          delay={delay}
-        />
-      </>
-    )
-  })
+storiesOf('Task/Typeahead', module)
+  .add('ReactSelect', () => (
+    <>
+      <TaskReactSelect
+        placeholder="Select single"
+        id="task-react-select-example-single"
+        taskName="Task typeahead example options"
+      />
+      <br />
+      <TaskReactSelect
+        placeholder="Select multi"
+        id="task-react-select-example-multi"
+        taskName="Task typeahead example options"
+        isMulti={true}
+      />
+    </>
+  ))
+  .add('Typeahead', () => (
+    <>
+      <TaskTypeahead
+        placeholder="Select single"
+        id="task-typeahead-example-single"
+        taskName="Task typeahead example options"
+      />
+      <br />
+      <TaskTypeahead
+        placeholder="Select multi"
+        id="task-typeahead-example-multi"
+        taskName="Task typeahead example options"
+        isMulti={true}
+      />
+    </>
+  ))
