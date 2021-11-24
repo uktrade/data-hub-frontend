caa05ba111e039e59408bdf9145c77842e721e69
Peter Hudec <peterhudec@peterhudec.com>
2021-11-24 18:40:03 +0000

    Add the Resource/Advisers component

--- src/client/components/Resource/tasks.js
+++ src/client/components/Resource/tasks.js
@@ -1,3 +1,4 @@
+import Advisers from './Advisers'
 import Contact from './Contact'
 import Company from './Company'
 import Countries from './Countries'
@@ -6,6 +7,7 @@ import Opportunity from './Opportunity'
 import OpportunityStatuses from './OpportunityStatuses'
 
 export default {
+  ...Advisers.tasks,
   ...Company.tasks,
   ...Contact.tasks,
   ...Countries.tasks,
