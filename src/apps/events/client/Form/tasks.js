26ad7e0e19e0619e0d946e2faa2b28d01507eaba
Vincent Farah <vincent.farah@madetech.com>
2021-11-29 16:53:57 +0000

    RR-173: Fix spread issue

--- src/apps/events/client/Form/tasks.js
+++ src/apps/events/client/Form/tasks.js
@@ -58,12 +58,8 @@ const getEventFormAndMetadata = (data) => {
 const saveEvent = (values) => {
   const request = values.id ? apiProxyAxios.patch : apiProxyAxios.post
 
-  const payload = {
-    ...values,
-    metadata: undefined,
-    disabled_on: undefined,
-    archived_documents_url_path: undefined,
-  }
+  const { metadata, disabled_on, archived_documents_url_path, ...payload } =
+    values
   const endpoint = values.id ? `/v4/event/${values.id}` : '/v4/event'
   return request(endpoint, payload).catch(catchApiError)
 }
