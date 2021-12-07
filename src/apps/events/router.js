17eb577945e01e1026ac52c8054f5c02ccd96941
Vincent Farah <vincent.farah@madetech.com>
2021-12-07 08:24:41 +0000

    RR-231: Fixed routing issue with create Removed tests and code not needed

--- src/apps/events/router.js
+++ src/apps/events/router.js
@@ -5,8 +5,9 @@ const { APP_PERMISSIONS, LOCAL_NAV } = require('./constants')
 const { handleRoutePermissions, setLocalNav } = require('../middleware')
 const { getEventDetails } = require('./middleware/details')
 const { renderReactifiedEventsView } = require('./controllers/events')
-
 const attendeesRouter = require('./attendees/router')
+
+router.get('*', renderReactifiedEventsView)
 
 router.use(handleRoutePermissions(APP_PERMISSIONS))
 router.param('eventId', getEventDetails)
@@ -16,6 +17,5 @@ router.use(
   setLocalNav(LOCAL_NAV)
 )
 router.use('/:eventId/attendees', attendeesRouter)
-router.get('/*', renderReactifiedEventsView)
 
 module.exports = router
