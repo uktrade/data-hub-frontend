d94c797bdd1bae4becfbbf9b0d14adfe0452c161
Vincent Farah <vincent.farah@madetech.com>
2021-12-07 18:10:11 +0000

    RR-231: Rolled back the name change

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -116,7 +116,7 @@ import {
 } from '../client/modules/Events/CollectionList/state'
 
 import { TASK_GET_EVENT_SUMMARY_DETAILS } from '../client/modules/Events/EventDetails/state'
-import { getEventSummaryDetails } from '../client/modules/Events/EventDetails/tasks'
+import { getEventDetails } from '../client/modules/Events/EventDetails/tasks'
 import {
   TASK_GET_EVENTS_FORM_AND_METADATA,
   TASK_SAVE_EVENT,
@@ -303,7 +303,7 @@ function App() {
         [TASK_GET_EVENTS_LIST]: getEvents,
         [TASK_GET_EVENTS_METADATA]: getEventsMetadata,
         [TASK_GET_EVENTS_ORGANISER_NAME]: getAdviserNames,
-        [TASK_GET_EVENT_SUMMARY_DETAILS]: getEventSummaryDetails,
+        [TASK_GET_EVENT_SUMMARY_DETAILS]: getEventDetails,
         [TASK_GET_EVENTS_FORM_AND_METADATA]: getEventFormAndMetadata,
         [TASK_SAVE_EVENT]: saveEvent,
         [TASK_GET_ORDERS_METADATA]: getOrdersMetadata,
--- src/client/modules/Events/EventDetails/tasks.js
+++ src/client/modules/Events/EventDetails/tasks.js
@@ -1,7 +1,7 @@
 import { apiProxyAxios } from '../../../components/Task/utils'
 import { transformResponseToEventDetails } from '../transformers'
 
-export const getEventSummaryDetails = (eventId) =>
+export const getEventDetails = (eventId) =>
   apiProxyAxios
     .get(`/api-proxy/v4/event/${eventId}`)
     .then(({ data }) => transformResponseToEventDetails(data))
