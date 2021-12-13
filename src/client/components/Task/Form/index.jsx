e10097d55fbe8dd02b7b01e00374f528857c734c
cgsunkel <christopher.sunkel@digital.trade.gov.uk>
2021-12-13 14:29:13 +0000

    Add prop to allow custom colours for the submit button

--- src/client/components/Task/Form/index.jsx
+++ src/client/components/Task/Form/index.jsx
@@ -28,6 +28,8 @@ import { validateForm } from '../../Form/MultiInstanceForm'
 import Effect from '../../Effect'
 import HardRedirect from '../../HardRedirect'
 
+import { BUTTON_COLOUR } from 'govuk-colours'
+
 const addFlashMessage = (message) =>
   Array.isArray(message)
     ? addMessageWithBody('success', ...message)
@@ -53,6 +55,7 @@ const _TaskForm = ({
   transformPayload = (x) => x,
   onSuccess,
   submitButtonLabel = 'Save',
+  submitButtonColour = BUTTON_COLOUR,
   actionLinks = [],
   // State props
   onLoad,
@@ -282,7 +285,9 @@ const _TaskForm = ({
                             */}
                             {!steps.length && (
                               <FormActions>
-                                <Button>{submitButtonLabel}</Button>
+                                <Button buttonColour={submitButtonColour}>
+                                  {submitButtonLabel}
+                                </Button>
                                 {actionLinks.map(({ to, href, children }, i) =>
                                   to ? (
                                     <ReactRouter.Link key={i} to={to}>
@@ -435,6 +440,8 @@ const dispatchToProps = (dispatch) => ({
  * For the exact shape of the objects refrer to `./types.d.ts`.
  * @param {Props['submitButtonLabel']} [props.submitButtonLabel='Save'] -
  * The label of the submit button.
+ * @param {Props['submitButtonColour']} [props.submitButtonColour=BUTTON_COLOUR] -
+ * The colour of the submit button.
  * @param {Props['children']} [props.children] - The form fields should be
  * passed as children. Note that the form communicates with it's fields
  * through the context it provides to the {useFormContext} hook, which is used
