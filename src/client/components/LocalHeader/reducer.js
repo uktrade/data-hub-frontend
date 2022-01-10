9f943f42752384691fea8aee80eea0d1168cf883
Miriam Forner <mforner13@gmail.com>
2022-01-10 12:40:41 +0000

    Spread state in flashmessages reducer

--- src/client/components/LocalHeader/reducer.js
+++ src/client/components/LocalHeader/reducer.js
@@ -7,10 +7,14 @@ export default (state = {}, { type, flashMessages }) => {
   switch (type) {
     case FLASH_MESSAGE__ADD_TO_STATE:
       return {
+        ...state,
         flashMessages,
       }
     case FLASH_MESSAGE__CLEAR_FROM_STATE:
-      return {}
+      return {
+        ...state,
+        flashMessages: {},
+      }
     default:
       return state
   }
