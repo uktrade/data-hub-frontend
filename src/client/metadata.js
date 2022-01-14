4a7b9eb3f7f1dec14c849fb8b0e1fd044d934ccd
Chloe <chloe.wong@madetech.com>
2022-01-14 16:48:01 +0000

    CDS-282 added alphabetically sorting and comments

--- src/client/metadata.js
+++ src/client/metadata.js
@@ -32,6 +32,13 @@ async function getMetadataOptions(
     : data.map(transformMetadataOption)
 }
 
+/**
+ * Get an alphabetically sorted list of metadata options filtered by disabled on and context
+ * @url the metadata endpoint
+ * @context the service context for which to filter on
+ * @filterDisabled whether to filter each option based on its
+ * disabled_on key, defaulting to true
+ */
 async function getMetadataWithContextOptions(
   url,
   context,
@@ -47,6 +54,7 @@ async function getMetadataWithContextOptions(
   return filteringDisabledOptions
     .filter((service) => service.contexts.includes(context))
     .map(transformMetadataOption)
+    .sort((service1, service2) => (service1.label > service2.label ? 1 : -1))
 }
 
 /**
