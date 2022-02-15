ac3fe4f722d3f75194f7b774ed91cf8b42d746a0
Tom Kingston <tom@kingstonlabs.co.uk>
2022-02-15 12:58:31 +0000

    Add analytics on search results etc

--- assets/javascripts/modules/search-analytics.js
+++ assets/javascripts/modules/search-analytics.js
@@ -1,13 +1,35 @@
 const searchEntitySelector = '.js-search-entity'
+const searchResultsSelector = '.js-search-results'
 
 const SearchAnalytics = {
   init() {
+    window.dataLayer = window.dataLayer || []
+
     this.pageNumber =
       new URLSearchParams(window.location.search).get('page') || 1
     const entities = document.querySelectorAll(searchEntitySelector)
     if (entities) {
       for (let i = 0; i < entities.length; i++) {
         entities[i].addEventListener('click', this.onClickHandler.bind(this))
+      }
+    }
+    const searchResults = document.querySelectorAll(searchResultsSelector)
+    if (searchResults?.length) {
+      if (!sessionStorage.getItem('searchCount')) {
+        sessionStorage.setItem('searchCount', 1)
+      } else if (this.pageNumber == 1) {
+        // Only increment search count when visiting first page of results
+        sessionStorage.setItem(
+          'searchCount',
+          parseInt(sessionStorage.getItem('searchCount')) + 1
+        )
+      }
+      for (let i = 0; i < searchResults.length; i++) {
+        window.dataLayer.push({
+          searchCategory: searchResults[i].dataset.searchCategory,
+          resultsReturned: searchResults[i].dataset.resultsReturned,
+          countOfSearch: sessionStorage.getItem('searchCount'),
+        })
       }
     }
   },
@@ -17,7 +39,6 @@ const SearchAnalytics = {
     searchResultPageNumber,
     searchCategory,
   }) {
-    window.dataLayer = window.dataLayer || []
     window.dataLayer.push({
       event: 'searchResultClick',
       searchResultPageNumber,
