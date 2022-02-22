e530b146620d0faba5b98dd1d714c79c34c93171
Tom Kingston <tom@kingstonlabs.co.uk>
2022-02-22 13:58:27 +0000

    Use filter toggle sections on large captial profile page

--- src/apps/investments/client/profiles/LargeCapitalProfileCollection.jsx
+++ src/apps/investments/client/profiles/LargeCapitalProfileCollection.jsx
@@ -7,7 +7,7 @@ import {
   CollectionFilters,
   FilteredCollectionList,
   Filters,
-  ToggleSection,
+  FilterToggleSection,
 } from '../../../../client/components/'
 import { listSkeletonPlaceholder } from '../../../../client/components/SkeletonPlaceholder'
 import { TASK_GET_PROFILES_LIST, ID } from './state'
@@ -273,7 +273,7 @@ const LargeCapitalProfileCollection = ({
               },
             }}
           >
-            <ToggleSection
+            <FilterToggleSection
               id="FilteredLargeCapitalProfileCollection.core-filters"
               label="Core filters"
             >
@@ -304,8 +304,8 @@ const LargeCapitalProfileCollection = ({
                 label="Company name"
                 placeholder="Search company name"
               />
-            </ToggleSection>
-            <ToggleSection
+            </FilterToggleSection>
+            <FilterToggleSection
               id="FilteredLargeCapitalProfileCollection.investor-details-filters"
               label="Investor details"
             >
@@ -339,8 +339,8 @@ const LargeCapitalProfileCollection = ({
                 selectedOptions={selectedRequiredChecksConducted}
                 data-test="required-checks-conducted-filter"
               />
-            </ToggleSection>
-            <ToggleSection
+            </FilterToggleSection>
+            <FilterToggleSection
               id="FilteredLargeCapitalProfileCollection.investor-requirements-filters"
               label="Investor requirements"
             >
@@ -424,8 +424,8 @@ const LargeCapitalProfileCollection = ({
                 selectedOptions={selectedDesiredDealRole}
                 data-test="desired-deal-role-filter"
               />
-            </ToggleSection>
-            <ToggleSection
+            </FilterToggleSection>
+            <FilterToggleSection
               id="FilteredLargeCapitalProfileCollection.location-filters"
               label="Location"
             >
@@ -439,7 +439,7 @@ const LargeCapitalProfileCollection = ({
                 selectedOptions={selectedUkRegionsOfInterest}
                 data-test="uk-regions-of-interest"
               />
-            </ToggleSection>
+            </FilterToggleSection>
           </CollectionFilters>
         </FilteredCollectionList>
       )
