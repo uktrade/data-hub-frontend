930e798eeba56302fd92ab52c7d2a62adedf30aa
Miriam Forner <mforner13@gmail.com>
2022-03-01 15:19:30 +0000

    Add story for window link with custom aria label

--- src/client/components/NewWindowLink/__stories__/NewWindowLink.stories.jsx
+++ src/client/components/NewWindowLink/__stories__/NewWindowLink.stories.jsx
@@ -4,6 +4,12 @@ import NewWindowLink from 'NewWindowLink'
 
 const stories = storiesOf('NewWindowLink')
 
-stories.add('Default', () => (
-  <NewWindowLink href="https://example.com">This is a link</NewWindowLink>
-))
+stories
+  .add('Default', () => (
+    <NewWindowLink href="https://example.com">This is a link</NewWindowLink>
+  ))
+  .add('Custom aria-label', () => (
+    <NewWindowLink href="https://example.com" aria-label="custom help text">
+      This is a link
+    </NewWindowLink>
+  ))
