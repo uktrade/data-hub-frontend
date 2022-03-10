55e9c23d4d2fd1f81fb9ff51a72fd13b0cd98873
cgsunkel <christopher.sunkel@digital.trade.gov.uk>
2022-03-10 14:49:29 +0000

    Remove legacy interactions CollectionList code

--- src/apps/interactions/controllers/list.js
+++ src/apps/interactions/controllers/list.js
@@ -1,13 +1,6 @@
 const path = require('path')
-const qs = require('querystring')
 const { get, set } = require('lodash')
-const { collectionFilterFields } = require('../macros')
-const {
-  buildSelectedFiltersSummary,
-  buildFieldsWithSelectedEntities,
-} = require('../../builders')
 const { getOptions } = require('../../../lib/options')
-const { buildExportAction } = require('../../../lib/export-helper')
 const { getAdvisers } = require('../../adviser/repos')
 const { filterActiveAdvisers } = require('../../adviser/filters')
 const { transformAdviserToOption } = require('../../adviser/transformers')
@@ -17,13 +10,6 @@ const FILTER_CONSTANTS = require('../../../lib/filter-constants')
 
 const QUERY_STRING = FILTER_CONSTANTS.INTERACTIONS.SECTOR.PRIMARY.QUERY_STRING
 const SECTOR = FILTER_CONSTANTS.INTERACTIONS.SECTOR.NAME
-
-const exportOptions = {
-  targetPermission: 'interaction.export_interaction',
-  urlFragment: 'interactions',
-  maxItems: FILTER_CONSTANTS.COMPANIES.SECTOR.MAX_EXPORT_ITEMS,
-  entityName: 'interaction',
-}
 
 const filterServiceNames = (services) => {
   if (!services) return
@@ -106,43 +92,6 @@ async function getInteractionOptions(req, res) {
   return interactionOptions
 }
 
-async function renderInteractionList(req, res, next) {
-  try {
-    const { user } = req.session
-    const { id: currentAdviserId, permissions } = user
-    const { query } = req
-    const options = await getInteractionOptions(req, res)
-
-    const filtersFields = collectionFilterFields({
-      currentAdviserId,
-      permissions,
-      ...options,
-    })
-
-    const filtersFieldsWithSelectedOptions =
-      await buildFieldsWithSelectedEntities(req, filtersFields, query)
-    const selectedFilters = await buildSelectedFiltersSummary(
-      filtersFieldsWithSelectedOptions,
-      query
-    )
-    const exportAction = await buildExportAction(
-      qs.stringify(req.query),
-      user.permissions,
-      exportOptions
-    )
-
-    res.render('_layouts/collection', {
-      selectedFilters,
-      exportAction,
-      filtersFields: filtersFieldsWithSelectedOptions,
-      title: 'Interactions',
-      countLabel: 'interaction',
-    })
-  } catch (error) {
-    next(error)
-  }
-}
-
 function renderInteractionsForEntity(req, res, next) {
   try {
     const {
@@ -174,7 +123,6 @@ function renderInteractionsForEntity(req, res, next) {
 }
 
 module.exports = {
-  renderInteractionList,
   renderInteractionsForEntity,
   getInteractionOptions,
 }
