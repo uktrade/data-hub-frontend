0e757c2da725d9c4ba12d68c6288d0d3f0467830
Harriet H-W <harriet.horobinworley@digital.trade.gov.uk>
2022-03-13 13:45:42 +0000

    Add new route to get activities data for contact

--- src/lib/urls.js
+++ src/lib/urls.js
@@ -219,6 +219,9 @@ module.exports = {
     rename: url('/company-lists', '/:listId/rename'),
   },
   contacts: {
+    activity: {
+      data: url('/contacts', '/:contactId/activity/data'),
+    },
     index: url('/contacts', PRIMARY_LINK_PARAMS.contacts),
     export: url('/contacts', '/export'),
     audit: url('/contacts', '/:contactId/audit'),
