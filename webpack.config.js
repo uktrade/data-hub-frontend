af416002e000c98b3b4968581ebd797f8abe923b
Craig J. Bass <craig.bass@digital.trade.gov.uk>
2022-03-14 17:43:48 +0000

    Use pipe instead of inherit to avoid write on null
    This attempts to fix an error where
    ```
    [webpack-cli] TypeError: Cannot read property 'write' of null
    at data-hub-frontend/webpack.config.js:19:24
    ```
    After upgrading webpack to version 5

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ const StartServerAfterBuild = () => {
         } else {
           server = spawn(
             "npx nodemon --inspect --ignore 'src/**/__test__/**/*'",
-            { stdio: 'inherit', shell: true }
+            { stdio: 'pipe', shell: true }
           )
         }
       })
