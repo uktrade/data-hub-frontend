de6d6524eafd8a0bc9a312e51e3aa28a4755ffc8
Onyela Ogah <83657534+onyela-o@users.noreply.github.com>
2022-03-22 12:27:36 +0000

    DET-85 Remove intercept

--- test/functional/cypress/specs/contacts/interactions-spec.js
+++ test/functional/cypress/specs/contacts/interactions-spec.js
@@ -1,7 +1,3 @@
-// const selectors = require('../../../../selectors')
-const {
-  activityStreamResults,
-} = require('../../../../../src/client/components/ContactInteractions/fixtures/activityStreamResults')
 const urls = require('../../../../../src/lib/urls')
 const fixtures = require('../../fixtures')
 
@@ -9,17 +5,11 @@ describe('Contact interactions', () => {
   context('when viewing a contact with data on Activity Stream', () => {
     before(() => {
       const contactId = fixtures.contact.deanCox.id
-      cy.intercept(
-        'GET',
-        '/api-proxy/v4/activity-feed',
-        activityStreamResults
-      ).as('apiRequest')
       cy.visit(urls.contacts.contactInteractions(contactId))
-      cy.wait('@apiRequest')
     })
 
     it('should display the Interactions', () => {
-      cy.get(`[data-test=aventri]`).should('be.visible')
+      cy.get('[data-test=aventri]').should('be.visible')
     })
   })
 })
