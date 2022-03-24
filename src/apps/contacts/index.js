52b2cd115a30f058dab1847cc5143ce4d45724fd
cgsunkel <christopher.sunkel@digital.trade.gov.uk>
2022-03-24 16:16:03 +0000

    Remove unused code

--- src/apps/contacts/index.js
+++ src/apps/contacts/index.js
@@ -1,10 +1,8 @@
 const urls = require('../../lib/urls')
 const router = require('./router')
-const userFeatures = require('../../middleware/user-features')
 
 module.exports = {
   displayName: 'Contacts',
   mountpath: urls.contacts.index.mountPoint,
-  // router,
-  router: router.get(userFeatures('aventri-activities-contacts-user')),
+  router,
 }
