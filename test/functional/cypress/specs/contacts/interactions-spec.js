553caca31c74f517e05fa069ee77051afaa5a04c
cgsunkel <christopher.sunkel@digital.trade.gov.uk>
2022-03-24 16:16:25 +0000

    Add test for when feature flag is disabled

--- test/functional/cypress/specs/contacts/interactions-spec.js
+++ test/functional/cypress/specs/contacts/interactions-spec.js
@@ -2,16 +2,35 @@ const urls = require('../../../../../src/lib/urls')
 const fixtures = require('../../fixtures')
 
 describe('Contact interactions', () => {
-  context('when viewing a contact with data on Activity Stream', () => {
-    before(() => {
-      const contactId = fixtures.contact.deanCox.id
-      cy.setUserFeatures(['aventri-activities-contacts'])
+  context(
+    'when viewing a contact with data on Activity Stream with the feature flag enabled',
+    () => {
+      before(() => {
+        const contactId = fixtures.contact.deanCox.id
+        cy.setUserFeatures(['aventri-activities-contacts-user'])
+        cy.visit(urls.contacts.contactInteractions(contactId))
+      })
 
-      cy.visit(urls.contacts.contactInteractions(contactId))
-    })
+      it('should display the Interactions', () => {
+        cy.get('[data-test=aventri]').should('be.visible')
+      })
 
-    it('should display the Interactions', () => {
-      cy.get('[data-test=aventri]').should('be.visible')
-    })
-  })
+      after(() => {
+        cy.resetUser()
+      })
+    }
+  )
+  context(
+    'when viewing a contact with data on Activity Stream with the feature flag disabled',
+    () => {
+      before(() => {
+        const contactId = fixtures.contact.deanCox.id
+        cy.visit(urls.contacts.contactInteractions(contactId))
+      })
+
+      it('should only display non-Aventri interactions', () => {
+        cy.get('[data-test=aventri]').should('not.exist')
+      })
+    }
+  )
 })
