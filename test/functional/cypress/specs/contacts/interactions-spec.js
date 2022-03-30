46d432bfbcc163679ccb81d0e43a70e62fc175bd
Onyela Ogah <83657534+onyela-o@users.noreply.github.com>
2022-03-30 15:50:02 +0100

    DET-85 adding a test for error messaging (#4493)
    Co-Authored-By: Edmond De Los Reyes <28296624+dredmonds@users.noreply.github.com>
    Co-Authored-By: Harriet H-W <16647486+Harriethw@users.noreply.github.com>
    Co-Authored-By: Onyela Ogah <83657534+onyela-o@users.noreply.github.com>
    Co-Authored-By: Christopher Sunkel <36161814+cgsunkel@users.noreply.github.com>
    
    Co-authored-by: Chloe Wong <70902973+chloehkwong1@users.noreply.github.com>
    Co-authored-by: Edmond De Los Reyes <28296624+dredmonds@users.noreply.github.com>
    Co-authored-by: Harriet H-W <16647486+Harriethw@users.noreply.github.com>
    Co-authored-by: Christopher Sunkel <36161814+cgsunkel@users.noreply.github.com>

--- test/functional/cypress/specs/contacts/interactions-spec.js
+++ test/functional/cypress/specs/contacts/interactions-spec.js
@@ -1,26 +1,49 @@
 const urls = require('../../../../../src/lib/urls')
 const fixtures = require('../../fixtures')
 
+const { assertErrorDialog } = require('../../support/assertions')
+
 describe('Contact interactions', () => {
   const contactId = fixtures.contact.deanCox.id
 
-  context(
-    'when viewing a contact with data on Activity Stream with the feature flag enabled',
-    () => {
+  context('when the feature flag user-contact-activities is enabled', () => {
+    before(() => {
+      cy.setUserFeatures(['user-contact-activities'])
+    })
+
+    context('when viewing a contact with activities', () => {
       before(() => {
-        cy.setUserFeatures(['user-contact-activities'])
         cy.visit(urls.contacts.contactInteractions(contactId))
       })
 
       it('should display the Activity Stream activities', () => {
         cy.get('[data-test=aventri-activity]').should('be.visible')
       })
+    })
 
-      after(() => {
-        cy.resetUser()
-      })
-    }
-  )
+    context(
+      'viewing a contact when there is an error loading activities',
+      () => {
+        before(() => {
+          cy.intercept('GET', urls.contacts.activity.data(contactId), {
+            statusCode: 500,
+          })
+          cy.visit(urls.contacts.contactInteractions(contactId))
+        })
+
+        it('should render an error message', () => {
+          assertErrorDialog(
+            'TASK_GET_CONTACT_INTERACTIONS',
+            'Unable to load Contact Interactions.'
+          )
+        })
+      }
+    )
+
+    after(() => {
+      cy.resetUser()
+    })
+  })
   context('when viewing a contact with the feature flag disabled', () => {
     before(() => {
       cy.visit(urls.contacts.contactInteractions(contactId))
