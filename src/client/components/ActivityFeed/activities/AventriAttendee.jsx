f9251ef648007aa16c8506b9a9832b4242d6d888
Chloe Wong <70902973+chloehkwong1@users.noreply.github.com>
2022-05-04 12:39:53 +0100

    DET-97 Added styling for aventri event card
    Co-Authored-By: Harriet H-W <16647486+Harriethw@users.noreply.github.com>
    Co-Authored-By: Onyela Ogah <83657534+onyela-o@users.noreply.github.com>
    Co-Authored-By: Edmond De Los Reyes <28296624+dredmonds@users.noreply.github.com>

--- src/client/components/ActivityFeed/activities/AventriAttendee.jsx
+++ src/client/components/ActivityFeed/activities/AventriAttendee.jsx
@@ -2,9 +2,42 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import CardUtils from './card/CardUtils'
 import { ACTIVITY_TYPE } from '../constants'
-import { Card, CardHeader } from './card'
-import { GREEN } from 'govuk-colours'
+import { FONT_SIZE, FONT_WEIGHTS, SPACING } from '@govuk-react/constants'
+import { GREY_1, GREY_2 } from 'govuk-colours'
 import { format } from '../../../utils/date'
+import styled from 'styled-components'
+import Tag from '../../Tag'
+
+const ItemWrapper = styled('div')`
+  border-bottom: 1px solid ${GREY_2};
+  padding: ${SPACING.SCALE_3} 0;
+`
+
+const EventName = styled('h3')`
+  color: ${GREY_1};
+  font-size: ${FONT_SIZE.SIZE_20};
+  font-weight: ${FONT_WEIGHTS.bold};
+  line-height: ${FONT_SIZE.SIZE_24};
+  margin-top: ${SPACING.SCALE_2};
+  margin-bottom: ${SPACING.SCALE_2};
+`
+
+const Metadata = styled('div')`
+  color: ${GREY_1};
+  font-size: ${FONT_SIZE.SIZE_14};
+  line-height: ${FONT_SIZE.SIZE_24};
+`
+
+const TagRow = styled('div')`
+  display: flex;
+  justify-content: space-between;
+  padding-bottom: ${SPACING.SCALE_2};
+  margin-right: ${SPACING.SCALE_1};
+`
+
+const TagColumn = styled('div')`
+  display: flex;
+`
 
 const transformAventriAttendee = (activity) => {
   return {
@@ -16,15 +49,18 @@ export default function AventriAttendee({ activity }) {
   const { eventName, startDate } = transformAventriAttendee(activity)
 
   return (
-    <div data-test={'aventri-activity'}>
-      <Card>
-        <CardHeader
-          badge={{ borderColour: GREEN, text: 'Aventri Service Delivery' }}
-          heading={eventName}
-        />
-        <p>Date: {startDate || 'Unknown'}</p>
-      </Card>
-    </div>
+    <ItemWrapper data-test={'aventri-activity'}>
+      <TagRow>
+        <TagColumn>
+          <Tag colour="blue">Events</Tag>
+        </TagColumn>
+        <TagColumn>
+          <Tag colour="grey">Aventri Service Delivery</Tag>
+        </TagColumn>
+      </TagRow>
+      <EventName>{eventName}</EventName>
+      <Metadata>Date: {startDate || 'Unknown'}</Metadata>
+    </ItemWrapper>
   )
 }
 
