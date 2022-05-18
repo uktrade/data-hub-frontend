436602f13c277955dcf6bd280e45302b69a29edd
cgsunkel <christopher.sunkel@digital.trade.gov.uk>
2022-05-18 12:12:01 +0100

    Add Aventri feature flag to the rest of the contacts pages

--- src/apps/contacts/controllers/documents.js
+++ src/apps/contacts/controllers/documents.js
@@ -1,13 +1,23 @@
 const { get } = require('lodash')
 
 function renderDocuments(req, res) {
+  const contactId = req.params.contactId
+  const isContactActivitiesFeatureOn = res.locals.userFeatures?.includes(
+    'user-contact-activities'
+  )
+
   const archivedDocumentPath = get(
     res.locals,
     'contact.archived_documents_url_path'
   )
 
   return res.breadcrumb('Documents').render('contacts/views/documents', {
+    props: {
+      contactId: contactId,
+      isContactActivitiesFeatureOn,
+    },
     archivedDocumentPath,
+    isContactActivitiesFeatureOn,
   })
 }
 
