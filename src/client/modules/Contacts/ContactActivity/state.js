a9ac62902cdaba86ff26b1a88bb613b345c5dc17
Harriet H-W <harriet.horobinworley@digital.trade.gov.uk>
2022-05-25 11:42:40 +0100

    DET-123 state management for sort by and page number
    We want to sort Contact Activities by 'newest' or 'oldest' - based on
    when the activity was published. This might not be the final sorting
    methods we use, but as we only have limited data on activities for
    Aventri this makes sense for now.
    
    This commit re-configures the state management to use the url params for page number and sortby, and pass them to the api call in the task.
    
    Because the sort component includes a page count based on the route, we no longer need to handle it in redux, so that action can be deleted.
    
    Co-Authored-By: Onyela Ogah <83657534+onyela-o@users.noreply.github.com>

--- src/client/modules/Contacts/ContactActivity/state.js
+++ src/client/modules/Contacts/ContactActivity/state.js
@@ -1,7 +1,16 @@
+import qs from 'qs'
+
 export const TASK_GET_CONTACT_ACTIVITIES = 'TASK_GET_CONTACT_ACTIVITIES'
 
 export const ID = 'contactActivity'
 
-export const state2props = ({ ...state }) => ({
-  ...state[ID],
-})
+export const state2props = ({ ...state }) => {
+  const selectedSortBy = qs.parse(location.search.slice(1)).sortby || 'newest'
+  const page = qs.parse(location.search.slice(1)).page || '1'
+
+  return {
+    ...state[ID],
+    page,
+    selectedSortBy,
+  }
+}
--- src/client/modules/Contacts/ContactActivity/tasks.js
+++ src/client/modules/Contacts/ContactActivity/tasks.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 import urls from '../../../../lib/urls'
 
-export const getContactActivities = ({ contactId, page }) =>
+export const getContactActivities = ({ contactId, page, selectedSortBy }) =>
   axios
-    .get(urls.contacts.activity.data(contactId), { params: { page } })
+    .get(urls.contacts.activity.data(contactId), {
+      params: { page, selectedSortBy },
+    })
     .then(({ data }) => data)
     .catch(() => Promise.reject('Unable to load contact activity.'))
