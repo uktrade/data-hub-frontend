e3e0c9463946ef6b21152eafda521856235c31ed
Chloe Wong <70902973+chloehkwong1@users.noreply.github.com>
2022-05-27 09:56:38 +0100

    Display Aventri events attended status (#4583)
    * Display Aventri attended status and updated test spec
    
    Co-Authored-By: Chloe Wong <70902973+chloehkwong1@users.noreply.github.com>
    
    * Refactor Aventri virtual event attendance object variable
    
    * Adding styling to Attended status
    
    Co-Authored-By: Edmond De Los Reyes <28296624+dredmonds@users.noreply.github.com>
    
    * Added test scenario with unconfirmed virtual event attendance
    
    * Added styled span for virtual event attended status
    
    Co-Authored-By: Edmond De Los Reyes <28296624+dredmonds@users.noreply.github.com>
    
    * Amend checking of string equality
    
    Co-authored-by: Edmond De Los Reyes <edmond.delosreyes@digital.trade.gov.uk>
    Co-authored-by: Edmond De Los Reyes <28296624+dredmonds@users.noreply.github.com>

--- src/client/components/ActivityFeed/activities/AventriAttendee.jsx
+++ src/client/components/ActivityFeed/activities/AventriAttendee.jsx
@@ -3,23 +3,44 @@ import PropTypes from 'prop-types'
 import CardUtils from './card/CardUtils'
 import { ACTIVITY_TYPE } from '../constants'
 import { formatStartAndEndDate } from '../../../utils/date'
+import { GREY_1 } from 'govuk-colours'
+import styled from 'styled-components'
 
 import ActivityCardWrapper from './card/ActivityCardWrapper'
 import ActivityCardSubject from './card/ActivityCardSubject'
 import ActivityCardMetadata from './card/ActivityCardMetadata'
 import ActivityCardLabels from './card/ActivityCardLabels'
 
+const VIRTUAL_EVENT_ATTENDANCE_STATUS = 'Attended'
+
 const transformAventriAttendee = (activity) => ({
   eventName: activity.eventName,
   date: formatStartAndEndDate(activity.startDate, activity.endDate),
+  isVirtualAttendanceConfirmed:
+    activity.object['dit:aventri:virtual_event_attendance'] === 'Yes',
 })
+
+const StyledSpan = styled('span')`
+  & > span {
+    color: ${GREY_1};
+  }
+`
+
 export default function AventriAttendee({ activity }) {
-  const { eventName, date } = transformAventriAttendee(activity)
+  const { eventName, date, isVirtualAttendanceConfirmed } =
+    transformAventriAttendee(activity)
 
   return (
     <ActivityCardWrapper dataTest="aventri-activity">
       <ActivityCardLabels service="event" kind="aventri service delivery" />
-      <ActivityCardSubject>{eventName}</ActivityCardSubject>
+      <ActivityCardSubject>
+        {eventName}
+        {isVirtualAttendanceConfirmed && (
+          <StyledSpan>
+            : <span>{VIRTUAL_EVENT_ATTENDANCE_STATUS}</span>
+          </StyledSpan>
+        )}
+      </ActivityCardSubject>
       <ActivityCardMetadata
         metadata={[{ label: 'Event date', value: date || 'Unknown' }]}
       />
