b768ab6b6391b4fd3eedf1cef7d2cf3474dd0064
Harriet H-W <harriet.horobinworley@digital.trade.gov.uk>
2022-06-06 14:18:48 +0100

    fixup! DET-175:  Add new CollectionList for Events

--- src/client/modules/Events/CollectionList/ActivityCollectionList.jsx
+++ src/client/modules/Events/CollectionList/ActivityCollectionList.jsx
@@ -1,23 +1,9 @@
 import { GridCol } from 'govuk-react'
 import React from 'react'
 import { CollectionList } from '../../../components'
-import { EVENTS_ACTIVITIES__LOADED } from '../../../actions'
-import { listSkeletonPlaceholder } from '../../../components/SkeletonPlaceholder'
-import { ID, TASK_GET_EVENTS_ACTIVITIES } from './state'
 
-const ActivityCollectionList = ({ payload, results }) => {
-  console.log('activity comp')
+const ActivityCollectionList = ({ collectionListTask, results }) => {
   console.log(results)
-  const collectionListTask = {
-    name: TASK_GET_EVENTS_ACTIVITIES,
-    id: ID,
-    progressMessage: 'Loading events',
-    renderProgress: listSkeletonPlaceholder(),
-    startOnRender: {
-      payload,
-      onSuccessDispatch: EVENTS_ACTIVITIES__LOADED,
-    },
-  }
 
   return (
     <GridCol setWidth="full">
