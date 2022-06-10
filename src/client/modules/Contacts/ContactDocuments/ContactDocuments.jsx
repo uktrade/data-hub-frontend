642b93b66bac9dc9f4d9e54c20824adef7d046e8
cgsunkel <christopher.sunkel@digital.trade.gov.uk>
2022-06-10 17:10:50 +0100

    Rewrite contact documents page to use new component

--- src/client/modules/Contacts/ContactDocuments/ContactDocuments.jsx
+++ src/client/modules/Contacts/ContactDocuments/ContactDocuments.jsx
@@ -1,37 +1,16 @@
 import React from 'react'
-import styled from 'styled-components'
-import { SPACING } from '@govuk-react/constants'
-import { typography } from '@govuk-react/lib'
 
 import ContactResource from '../../../components/Resource/Contact'
-import { NewWindowLink } from '../../../components'
-
-const StyledSectionHeader = styled('div')`
-  ${typography.font({ size: 24, weight: 'bold' })};
-  margin-bottom: ${SPACING.SCALE_4};
-`
+import DocumentsSection from '../../../components/DocumentsSection'
 
 const ContactDocuments = ({ contactId, archivedDocumentPath }) => {
   return (
     <ContactResource id={contactId}>
       {(contact) => (
-        <>
-          <StyledSectionHeader data-test="document-heading">
-            Documents
-          </StyledSectionHeader>
-          {contact.archivedDocumentsUrlPath ? (
-            <NewWindowLink
-              href={archivedDocumentPath + contact.archivedDocumentsUrlPath}
-              data-test="document-link"
-            >
-              View files and documents
-            </NewWindowLink>
-          ) : (
-            <p data-test="no-documents-message">
-              There are no files or documents
-            </p>
-          )}
-        </>
+        <DocumentsSection
+          fileBrowserRoot={archivedDocumentPath}
+          documentPath={contact.archivedDocumentsUrlPath}
+        />
       )}
     </ContactResource>
   )
