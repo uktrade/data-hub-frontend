768e2917ab6fc22f677c5c4b33ba5be31ec0cfd6
Chloe Wong <70902973+chloehkwong1@users.noreply.github.com>
2022-06-16 17:06:01 +0100

    Add DSL query to get data hub events from activity stream

---
+++ src/apps/companies/apps/activity-feed/es-queries/data-hub-events-query.js
@@ -0,0 +1,17 @@
+const dataHubEventsQuery = () => ({
+  query: {
+    bool: {
+      must: [
+        {
+          term: {
+            'object.type': 'dit:dataHub:Event',
+          },
+        },
+      ],
+    },
+  },
+})
+
+module.exports = {
+  dataHubEventsQuery,
+}
