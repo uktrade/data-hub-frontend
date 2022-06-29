0a50fe5183cfc6f40b7f474c7cfff9c9709f7af8
Chloe Wong <70902973+chloehkwong1@users.noreply.github.com>
2022-06-29 17:08:08 +0100

    Moved EventAventriAttendees component into existing AventriAttendee component

--- src/client/modules/Events/EventAventriAttendees/index.jsx
+++ src/client/modules/Events/EventAventriAttendees/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { useParams } from 'react-router-dom'
+import { SPACING } from '@govuk-react/constants'
+import styled from 'styled-components'
 
 import urls from '../../../../lib/urls'
 import { DefaultLayout, LocalNav, LocalNavLink } from '../../../components'
@@ -11,6 +13,15 @@ import Task from '../../../components/Task'
 import { ID, state2props, TASK_GET_EVENT_AVENTRI_ATTENDEES } from './state'
 import { EVENTS__AVENTRI_ATTENDEES_LOADED } from '../../../actions'
 import Activity from '../../../components/ActivityFeed/Activity'
+
+const AttendeesList = styled('ol')`
+  list-style-type: none;
+  margin-top: ${SPACING.SCALE_2};
+
+  & > li {
+    margin-bottom: ${SPACING.SCALE_2};
+  }
+`
 
 const EventAventriAttendees = ({ aventriAttendees, aventriEventData }) => {
   const { aventriEventId } = useParams()
@@ -49,7 +60,7 @@ const EventAventriAttendees = ({ aventriAttendees, aventriEventData }) => {
               }}
             >
               {() => (
-                <GridRow data-test="eventAventriAttendee">
+                <GridRow data-test="event-aventri-attendee">
                   <GridCol setWidth="one-quarter">
                     <LocalNav data-test="event-aventri-nav">
                       <LocalNavLink
@@ -63,11 +74,13 @@ const EventAventriAttendees = ({ aventriAttendees, aventriEventData }) => {
                     </LocalNav>
                   </GridCol>
                   <GridCol setWidth="three-quarters">
-                    {aventriAttendees?.map((attendee, index) => (
-                      <li key={`aventri-attendee-${index}`}>
-                        <Activity activity={attendee}></Activity>
-                      </li>
-                    ))}
+                    <AttendeesList>
+                      {aventriAttendees?.map((attendee, index) => (
+                        <li key={`aventri-attendee-${index}`}>
+                          <Activity activity={attendee}></Activity>
+                        </li>
+                      ))}
+                    </AttendeesList>
                   </GridCol>
                 </GridRow>
               )}
