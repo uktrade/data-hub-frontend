93898fe15574a9d82a8a8c90f7401c2f51a41a3e
Harriet H-W <harriet.horobinworley@digital.trade.gov.uk>
2022-07-22 11:49:49 +0100

    DET-232: add sort to Aventri Attendees page
    We want to sort the Aventri Attendees by first name.
    This commit adds the react component and redux architecture needed to do so.

--- src/client/modules/Events/EventAventriAttendees/state.js
+++ src/client/modules/Events/EventAventriAttendees/state.js
@@ -1,10 +1,16 @@
+import qs from 'qs'
+
 export const TASK_GET_EVENT_AVENTRI_ATTENDEES =
   'TASK_GET_EVENT_AVENTRI_ATTENDEES'
 
 export const ID = 'eventAventriAttendees'
 
 export const state2props = ({ ...state }) => {
+  const selectedSortBy =
+    qs.parse(location.search.slice(1)).sortby || 'first_name:asc'
+
   return {
+    selectedSortBy,
     ...state[ID],
   }
 }
--- src/client/modules/Events/EventAventriAttendees/tasks.js
+++ src/client/modules/Events/EventAventriAttendees/tasks.js
@@ -1,9 +1,14 @@
 const { default: axios } = require('axios')
 const urls = require('../../../../lib/urls')
 
-export const getEventAventriAttendees = (aventriEventId) => {
+export const getEventAventriAttendees = ({
+  aventriEventId,
+  selectedSortBy,
+}) => {
   return axios
-    .get(urls.events.aventri.attendeesData(aventriEventId))
+    .get(urls.events.aventri.attendeesData(aventriEventId), {
+      params: { sortBy: selectedSortBy },
+    })
     .then(({ data }) => data)
     .catch(() => {
       return Promise.reject('Unable to load Aventri Attendees.')
