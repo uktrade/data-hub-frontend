d7478e66daf8a1693a62a1522d768c5c5a32ebed
Chloe Wong <70902973+chloehkwong1@users.noreply.github.com>
2022-07-29 10:51:03 +0100

    Added to query builder in controller

--- src/apps/companies/apps/activity-feed/controllers.js
+++ src/apps/companies/apps/activity-feed/controllers.js
@@ -361,7 +361,11 @@ async function fetchAventriAttendees(req, res, next) {
   }
 }
 
-const eventsColListQueryBuilder = ({ name }) => {
+const eventsColListQueryBuilder = ({
+  name,
+  earliestStartDate,
+  latestStartDate,
+}) => {
   const eventNameFilter = name
     ? {
         match: {
@@ -370,9 +374,21 @@ const eventsColListQueryBuilder = ({ name }) => {
       }
     : null
 
-  const filtersArray = [eventNameFilter]
+  const dateFilter =
+    earliestStartDate || latestStartDate
+      ? {
+          range: {
+            'object.startTime': {
+              gte: earliestStartDate,
+              lte: latestStartDate,
+            },
+          },
+        }
+      : null
 
-  const cleansedFiltersArray = filtersArray.filter((filter) => filter != null)
+  const filtersArray = [eventNameFilter, dateFilter]
+
+  const cleansedFiltersArray = filtersArray.filter((filter) => filter)
 
   const queryBuilder = [EVENT_ALL_ACTIVITY, ...cleansedFiltersArray]
   return queryBuilder
@@ -380,7 +396,7 @@ const eventsColListQueryBuilder = ({ name }) => {
 
 async function fetchAllActivityFeedEvents(req, res, next) {
   try {
-    const { sortBy, page, name } = req.query
+    const { sortBy, name, earliestStartDate, latestStartDate, page } = req.query
 
     const from = (page - 1) * ACTIVITIES_PER_PAGE
 
@@ -389,6 +405,8 @@ async function fetchAllActivityFeedEvents(req, res, next) {
       allActivityFeedEventsQuery({
         fullQuery: eventsColListQueryBuilder({
           name: name,
+          earliestStartDate: earliestStartDate,
+          latestStartDate: latestStartDate,
         }),
         from,
         ACTIVITIES_PER_PAGE,
