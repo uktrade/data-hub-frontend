0872aeafdc3efa49f63e4838b4c1cf482814316e
Christopher Sunkel <christopher.sunkel@digital.trade.gov.uk>
2022-08-02 12:52:50 +0100

    Remove ReferralList stories

--- src/client/components/ReferralList/__stories__/ReferralList.stories.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-
-import ReferralList from '..'
-
-storiesOf('ReferralList', module).add('Default', () => (
-  <ReferralList id="example" />
-))
