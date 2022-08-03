ad06c6b8ffc03798ad5eda38ae089256688b638c
Christopher Sunkel <christopher.sunkel@digital.trade.gov.uk>
2022-08-03 16:33:59 +0100

    Migrate Timeline to use docs addon

--- src/client/components/Timeline/__stories__/Timeline.stories.jsx
+++ src/client/components/Timeline/__stories__/Timeline.stories.jsx
@@ -2,17 +2,9 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 
 import Timeline from 'Timeline'
-import exampleReadme from '../example.md'
-import usageReadme from '../usage.md'
 
 storiesOf('Timeline', module)
-  .addParameters({
-    options: { theme: undefined },
-    readme: {
-      content: exampleReadme,
-      sidebar: usageReadme,
-    },
-  })
+  .addParameters({ component: Timeline })
   .add('Default', () => (
     <Timeline
       stages={['Prospect', 'Assign PM', 'Active', 'Verify win', 'Won']}
