ff9d0830e096b194ea969d354fa50df52c6a5333
Christopher Sunkel <christopher.sunkel@digital.trade.gov.uk>
2022-08-03 16:11:25 +0100

    Migrate NotificationBadge to use docs addon

--- src/client/components/NotificationBadge/__stories__/NotificationBadge.stories.jsx
+++ src/client/components/NotificationBadge/__stories__/NotificationBadge.stories.jsx
@@ -1,16 +1,8 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import exampleReadme from '../example.md'
-import usageReadme from '../usage.md'
 
 import NotificationBadge from 'NotificationBadge'
 
 storiesOf('NotificationBadge', module)
-  .addParameters({
-    options: { theme: undefined },
-    readme: {
-      content: exampleReadme,
-      sidebar: usageReadme,
-    },
-  })
+  .addParameters({ component: NotificationBadge })
   .add('Default', () => <NotificationBadge label={3} />)
