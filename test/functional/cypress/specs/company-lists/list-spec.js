7e1e3d22a6ef04c6de832e1689518965dd0ad72f
Harriet H-W <harriet.horobinworley@digital.trade.gov.uk>
2022-08-10 14:59:25 +0100

    TET-223: Delete unnecessary code in `company-lists/list-spec.js`
    The cookies preserve is not needed, and the `List A` is already tested when iterating through `expectedLists`

--- test/functional/cypress/specs/company-lists/list-spec.js
+++ test/functional/cypress/specs/company-lists/list-spec.js
@@ -5,17 +5,11 @@ const { assertAriaTablistTabSelected } = require('../../support/assertions')
 
 describe('My companies lists', () => {
   before(() => {
-    Cypress.Cookies.preserveOnce('datahub.sid')
     cy.visit('/')
   })
 
   it('my companies lists tab should be selected', () =>
     assertAriaTablistTabSelected('Dashboard', 'My companies lists'))
-
-  exports.describeSelectedList({
-    name: 'List A',
-    ...exports.expectedLists['List A'],
-  })
 
   Object.entries(exports.expectedLists).forEach(([name, expectedValues]) => {
     describe(`After selecting list "${name}"`, () => {
