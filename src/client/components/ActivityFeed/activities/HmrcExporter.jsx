a437b35de2b313c475be9907760aa688ab6aa1e1
Chloe Wong <70902973+chloehkwong1@users.noreply.github.com>
2022-08-15 14:04:58 +0100

    Added labels and feature flag for HMRC

--- src/client/components/ActivityFeed/activities/HmrcExporter.jsx
+++ src/client/components/ActivityFeed/activities/HmrcExporter.jsx
@@ -18,6 +18,10 @@ import {
   ANALYTICS_ACCORDION_TYPE,
   SOURCE_TYPES,
 } from '../constants'
+import CheckUserFeatureFlag from '../../CheckUserFeatureFlags'
+import { COMPANY_ACTIVITY_FEATURE_FLAG } from '../../../../apps/companies/apps/activity-feed/constants'
+import ActivityCardWrapper from './card/ActivityCardWrapper'
+import ActivityCardLabels from './card/ActivityCardLabels'
 
 export default class HmrcExporter extends React.PureComponent {
   static propTypes = {
@@ -46,39 +50,50 @@ export default class HmrcExporter extends React.PureComponent {
     })
 
     return (
-      <Card>
-        <CardHeader
-          company={showDnbHierarchy ? company : null}
-          blockText="HMRC"
-          subHeading="Exporters record"
-          heading={summary}
-          sourceType={SOURCE_TYPES.external}
-          startTime={startTime}
-        />
+      <CheckUserFeatureFlag userFeatureFlagName={COMPANY_ACTIVITY_FEATURE_FLAG}>
+        {(isFeatureFlagEnabled) =>
+          !isFeatureFlagEnabled ? (
+            <Card>
+              <CardHeader
+                company={showDnbHierarchy ? company : null}
+                blockText="HMRC"
+                subHeading="Exporters record"
+                heading={summary}
+                sourceType={SOURCE_TYPES.external}
+                startTime={startTime}
+              />
 
-        <CardDetails
-          summary="View key export details"
-          summaryVisuallyHidden={` for ${reference}`}
-          showDetails={showDetails}
-          analyticsAccordionType={ANALYTICS_ACCORDION_TYPE.HMRC}
-        >
-          <CardTable
-            rows={[
-              { header: 'Company name', content: reference },
-              {
-                header: 'Export Item code(s)',
-                content: (
-                  <CardDetailsList
-                    itemPropName="value"
-                    itemRenderer={DefaultItemRenderer}
-                    items={exportItemCodesCollection}
-                  />
-                ),
-              },
-            ]}
-          />
-        </CardDetails>
-      </Card>
+              <CardDetails
+                summary="View key export details"
+                summaryVisuallyHidden={` for ${reference}`}
+                showDetails={showDetails}
+                analyticsAccordionType={ANALYTICS_ACCORDION_TYPE.HMRC}
+              >
+                <CardTable
+                  rows={[
+                    { header: 'Company name', content: reference },
+                    {
+                      header: 'Export Item code(s)',
+                      content: (
+                        <CardDetailsList
+                          itemPropName="value"
+                          itemRenderer={DefaultItemRenderer}
+                          items={exportItemCodesCollection}
+                        />
+                      ),
+                    },
+                  ]}
+                />
+              </CardDetails>
+            </Card>
+          ) : (
+            <ActivityCardWrapper dataTest="hmrc-exporter-activity">
+              <ActivityCardLabels kind="HMRC Update" />
+              <h3>HMRC Exporters</h3>
+            </ActivityCardWrapper>
+          )
+        }
+      </CheckUserFeatureFlag>
     )
   }
 }
