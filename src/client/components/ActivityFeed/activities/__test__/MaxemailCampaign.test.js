dea1e9c9a18ba23009369f92d5a5208f2ea46101
Onyela Ogah <83657534+onyela-o@users.noreply.github.com>
2022-08-25 16:57:43 +0100

    fixup tests and logic

--- src/client/components/ActivityFeed/activities/__test__/MaxemailCampaign.test.js
+++ src/client/components/ActivityFeed/activities/__test__/MaxemailCampaign.test.js
@@ -1,3 +1,4 @@
+import { ACTIVITY_TYPE } from '../../constants'
 import CardUtils from '../card/CardUtils'
 
 const maxemailCampaign = {
@@ -27,35 +28,43 @@ const maxemailContact = {
 
 describe('Maxemail tests', () => {
   describe('Maxemail campaigns content checker', () => {
-    it('should return 1 when there is 1 contact', () => {
+    const renderResult = CardUtils.canRenderByTypes(
+      maxemailCampaign,
+      ACTIVITY_TYPE.MaxemailCampaign
+    )
+    it('should return true when there is 1 contact', () => {
       const actual = CardUtils.hasMaxemailContent(maxemailCampaign)
-      expect(actual).to.equal(1)
+      expect(renderResult && actual).to.equal(true)
     })
-    it('should return 0 when there is 0 contacts', () => {
+    it('should return false when there is 0 contacts', () => {
       maxemailCampaign['object.contacts'] = []
       const actual = CardUtils.hasMaxemailContent(maxemailCampaign)
-      expect(actual).to.equal(0)
+      expect(renderResult && actual).to.equal(false)
     })
   })
-  describe('Maxemail contact checker', () => {
+  describe('Maxemail contacts content checker', () => {
+    const renderContactResult = CardUtils.canRenderByTypes(
+      maxemailContact,
+      ACTIVITY_TYPE.MaxemailCampaign
+    )
     it('should return true when there is an email address', () => {
       const actual = CardUtils.hasMaxemailContent(maxemailContact)
-      expect(actual && true).to.equal(true)
+      expect(renderContactResult && actual).to.equal(true)
     })
-    it('should return false when there is no email address', () => {
+    it('should return true when there is one match in type ', () => {
       maxemailContact['object.type'] = 'dit:maxemail:Email:Sent'
       const actual = CardUtils.hasMaxemailContent(maxemailContact)
-      expect(actual && true).to.equal(true)
+      expect(renderContactResult && actual).to.equal(true)
     })
     it('should return false when there is no email address', () => {
       maxemailContact['dit:emailAddress'] = ''
       const actual = CardUtils.hasMaxemailContent(maxemailContact)
-      expect(actual && true).to.equal(0)
+      expect(renderContactResult && actual).to.equal(false)
     })
     it('should return false when the type does not match', () => {
       maxemailContact['object.type'] = 'dit:Interaction'
       const actual = CardUtils.hasMaxemailContent(maxemailContact)
-      expect(actual && true).to.equal(false)
+      expect(renderContactResult && actual).to.equal(false)
     })
   })
 })
