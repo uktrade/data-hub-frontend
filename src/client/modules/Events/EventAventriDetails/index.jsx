cf88e077a6e32c5e18d19aeaf3b90639f26b6da4
Onyela Ogah <83657534+onyela-o@users.noreply.github.com>
2022-09-05 13:40:33 +0100

    Temporarily disable attendees tab for Aventri events (#4859)
    * Delete Attendees tab
    
    * Delete attendees tab test

--- src/client/modules/Events/EventAventriDetails/index.jsx
+++ src/client/modules/Events/EventAventriDetails/index.jsx
@@ -106,14 +106,6 @@ const EventAventriDetails = ({ name, eventDate, location, fullAddress }) => {
                             >
                               Details
                             </LocalNavLink>
-                            <LocalNavLink
-                              dataTest="event-aventri-attendees-link"
-                              href={urls.events.aventri.attendees(
-                                aventriEventId
-                              )}
-                            >
-                              Attendees
-                            </LocalNavLink>
                           </LocalNav>
                         </GridCol>
                         <GridCol setWidth="three-quarters">
