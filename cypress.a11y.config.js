3ce28b0bb1994decda414f099cae79e4dbc305e8
Pippo Raimondi <piporamaa@gmail.com>
2022-10-10 09:02:36 +0100

    Fix e2e and a11y config setup

---
+++ cypress.a11y.config.js
@@ -0,0 +1,25 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  video: true,
+  videoUploadOnPasses: false,
+  screenshotOnRunFailure: false,
+  defaultCommandTimeout: 5000,
+  requestTimeout: 5000,
+  projectId: 'w97se2',
+  e2e: {
+    setupNodeEvents(on) {
+      on('task', {
+        log(message) {
+          /* eslint-disable no-console */
+          console.log(message)
+          /* eslint-enable no-console */
+          return null
+        },
+      })
+    },
+    specPattern: 'test/a11y/cypress/specs/**/*.js',
+    baseUrl: 'http://localhost:3000',
+    supportFile: 'test/a11y/support/index.js',
+  },
+})
