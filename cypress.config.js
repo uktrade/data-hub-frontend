6d35998c48e260f86bd3980ee0310eefbe5fe246
Pippo Raimondi <piporamaa@gmail.com>
2022-10-12 10:34:34 +0100

    Include component html

--- cypress.config.js
+++ cypress.config.js
@@ -13,20 +13,19 @@ module.exports = defineConfig({
       url: "http://localhost:3000/__coverage__",
     },
   },
+  component: {
+    supportFile: "test/cypress/support/index.js",
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
+    },
+  },
   e2e: {
     setupNodeEvents(on, config) {
       return require("./test/cypress/plugins/index.js")(on, config);
     },
     specPattern: "test/functional/cypress/specs/**/*.{js,jsx}",
     baseUrl: "http://localhost:3000",
-    supportFile: "test/cypress/support/index.js",
-  },
-
-  component: {
-    devServer: {
-      framework: "react",
-      bundler: "webpack",
-    },
     supportFile: "test/cypress/support/index.js",
   },
 });
