2a78e4bc6f387bb2c0260269e210bfdbbeec6d79
Edmond De Los Reyes <28296624+dredmonds@users.noreply.github.com>
2022-10-20 11:47:02 +0100

    Fix/change add company form layout (#4989)
    * RR-528 Change Add Company Form Layout
    
    * RR-528 Create and implement form layout component

---
+++ src/client/components/Layout/FormLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import GridRow from '@govuk-react/grid-row'
+import GridCol from '@govuk-react/grid-col'
+
+const FormLayout = ({ setWidth, children }) => (
+  <GridRow>
+    <GridCol setWidth={setWidth}>{children}</GridCol>
+  </GridRow>
+)
+
+FormLayout.propTypes = {
+  setWidth: PropTypes.string.isRequired,
+  children: PropTypes.element.isRequired,
+}
+
+export default FormLayout
