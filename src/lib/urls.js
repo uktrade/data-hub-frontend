ba22b8c1ebc3cea835eb77930543e54e49652509
Rachael Robinson <rachaelcodes@users.noreply.github.com>
2022-11-01 13:39:53 +0200

    Add export no recent interaction settings

--- src/lib/urls.js
+++ src/lib/urls.js
@@ -499,6 +499,11 @@ module.exports = {
           '/reminders/settings/investments-no-recent-interaction'
         ),
       },
+      exports: {
+        noRecentInteraction: url(
+          '/reminders/settings/exports-no-recent-interactions'
+        ),
+      },
     },
   },
 }
