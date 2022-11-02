402c67236b70d3cd43b31fada495263deed0bb0f
Rachael Robinson <rachaelcodes@users.noreply.github.com>
2022-11-02 11:12:32 +0200

    refactor to reduce repetition

--- test/functional/cypress/specs/reminders/settings/settings-spec.js
+++ test/functional/cypress/specs/reminders/settings/settings-spec.js
@@ -10,17 +10,13 @@ const nriEndpoint =
 const enriEndpoint =
   '/api-proxy/v4/reminder/subscription/no-recent-export-interaction'
 
-const eslToggle = '[data-test="estimated-land-date-toggle"]'
-const nriToggle = '[data-test="no-recent-interaction-toggle"]'
-const enriToggle = '[data-test="export-no-recent-interactions-toggle"]'
+const eslDataTest = 'estimated-land-date'
+const nriDataTest = 'no-recent-interaction'
+const enriDataTest = 'export-no-recent-interactions'
 
-const eslTable = '[data-test="estimated-land-date-table"]'
-const nriTable = '[data-test="no-recent-interaction-table"]'
-const enriTable = '[data-test="export-no-recent-interactions-table"]'
-
-const eslLink = '[data-test="estimated-land-date-link"]'
-const nriLink = '[data-test="no-recent-interaction-link"]'
-const enriLink = '[data-test="export-no-recent-interactions-link"]'
+const getToggle = (dataTest) => `[data-test="${dataTest}-toggle"]`
+const getTable = (dataTest) => `[data-test="${dataTest}-table"]`
+const getLink = (dataTest) => `[data-test="${dataTest}-link"]`
 
 const interceptAPICalls = ({
   esl_reminder_days = [30, 60],
@@ -56,6 +52,44 @@ const waitForAPICalls = () => {
   cy.wait('@enriApiRequest')
 }
 
+const assertSettingsTableVisible = (title, dataTest) => {
+  it(`should show the ${title} settings table and edit link`, () => {
+    cy.get(getTable(dataTest)).should('be.visible')
+    cy.get(getLink(dataTest)).should('be.visible')
+  })
+}
+const assertSettingsTableNotVisible = (title, dataTest) => {
+  it(`should hide the ${title} settings table and edit link`, () => {
+    cy.get(getTable(dataTest)).should('not.be.visible')
+    cy.get(getLink(dataTest)).should('not.be.visible')
+  })
+}
+
+const assertSettingsTableToggles = ({
+  title,
+  dataTest,
+  reminderText,
+  buttonText,
+}) => {
+  it(`should render the ${title} settings table`, () => {
+    assertKeyValueTable(`${dataTest}-table`, {
+      Reminders: reminderText,
+      'Email notifications': 'On',
+    })
+  })
+
+  it(`should hide the ${title} settings table on toggle`, () => {
+    cy.get(getToggle(dataTest))
+      .find('button')
+      .contains(buttonText)
+      .click()
+      .get(getTable(dataTest))
+      .should('not.be.visible')
+      .get(getLink(dataTest))
+      .should('not.be.visible')
+  })
+}
+
 describe('Reminders Settings', () => {
   context('Breadcrumbs and title', () => {
     before(() => {
@@ -86,14 +120,9 @@ describe('Reminders Settings', () => {
       waitForAPICalls()
     })
 
-    it('should hide all settings tables and edit links', () => {
-      cy.get(eslTable).should('not.be.visible')
-      cy.get(eslLink).should('not.be.visible')
-      cy.get(nriTable).should('not.be.visible')
-      cy.get(nriLink).should('not.be.visible')
-      cy.get(enriTable).should('not.be.visible')
-      cy.get(enriLink).should('not.be.visible')
-    })
+    assertSettingsTableNotVisible('ELD', eslDataTest)
+    assertSettingsTableNotVisible('NRI', nriDataTest)
+    assertSettingsTableNotVisible('ENRI', enriDataTest)
   })
 
   context(
@@ -106,37 +135,15 @@ describe('Reminders Settings', () => {
         waitForAPICalls()
       })
 
-      it('should show the ELD settings table and edit link', () => {
-        cy.get(eslTable).should('be.visible')
-        cy.get(eslLink).should('be.visible')
-      })
+      assertSettingsTableVisible('ELD', eslDataTest)
+      assertSettingsTableNotVisible('NRI', nriDataTest)
+      assertSettingsTableNotVisible('ENRI', enriDataTest)
 
-      it('should hide the NRI settings table and edit link', () => {
-        cy.get(nriTable).should('not.be.visible')
-        cy.get(nriLink).should('not.be.visible')
-      })
-
-      it('should hide the ENRI settings table and edit link', () => {
-        cy.get(enriTable).should('not.be.visible')
-        cy.get(enriLink).should('not.be.visible')
-      })
-
-      it('should render the ELD settings table', () => {
-        assertKeyValueTable('estimated-land-date-table', {
-          Reminders: '60 and 30 days before the estimated land date',
-          'Email notifications': 'On',
-        })
-      })
-
-      it('should hide the ELD settings table on toggle', () => {
-        cy.get(eslToggle)
-          .find('button')
-          .contains('Approaching estimated land date settings')
-          .click()
-          .get(eslTable)
-          .should('not.be.visible')
-          .get(eslLink)
-          .should('not.be.visible')
+      assertSettingsTableToggles({
+        title: 'ELD',
+        dataTest: eslDataTest,
+        reminderText: '60 and 30 days before the estimated land date',
+        buttonText: 'Approaching estimated land date settings',
       })
     }
   )
@@ -151,119 +158,51 @@ describe('Reminders Settings', () => {
         waitForAPICalls()
       })
 
-      it('should hide the ELD settings table and edit link', () => {
-        cy.get(eslTable).should('not.be.visible')
-        cy.get(eslLink).should('not.be.visible')
-      })
+      assertSettingsTableNotVisible('ELD', eslDataTest)
+      assertSettingsTableVisible('NRI', nriDataTest)
+      assertSettingsTableNotVisible('ENRI', enriDataTest)
 
-      it('should show the NRI settings table and edit link', () => {
-        cy.get(nriTable).should('be.visible')
-        cy.get(nriLink).should('be.visible')
-      })
-
-      it('should hide the ENRI settings table and edit link', () => {
-        cy.get(enriTable).should('not.be.visible')
-        cy.get(enriLink).should('not.be.visible')
-      })
-
-      it('should render the NRI settings table', () => {
-        assertKeyValueTable('no-recent-interaction-table', {
-          Reminders: '30, 50 and 70 days after the last interaction',
-          'Email notifications': 'On',
-        })
-      })
-
-      it('should hide the NRI settings table on toggle', () => {
-        cy.get(nriToggle)
-          .find('button')
-          .contains('Projects with no recent interaction settings')
-          .click()
-          .get(nriTable)
-          .should('not.be.visible')
-          .get(nriLink)
-          .should('not.be.visible')
+      assertSettingsTableToggles({
+        title: 'NRI',
+        dataTest: nriDataTest,
+        reminderText: '30, 50 and 70 days after the last interaction',
+        buttonText: 'Projects with no recent interaction settings',
       })
     }
   )
 
-<<<<<<< HEAD
-  context(
-    'When both estimated land settings and no recent interaction settings are visible',
-    () => {
-      const queryParams =
-        'investments_estimated_land_date=true&investments_no_recent_interaction=true'
-      before(() => {
-        interceptAPICalls()
-        cy.visit(`${urls.reminders.settings.index()}?${queryParams}`)
-        waitForAPICalls()
-      })
-=======
   context('When only no recent export interaction settings are visible', () => {
-    const queryParams = 'export_no_recent_interactions=true'
+    const queryParams = 'exports_no_recent_interactions=true'
     before(() => {
       interceptAPICalls()
       cy.visit(`${urls.reminders.settings.index()}?${queryParams}`)
       waitForAPICalls()
     })
->>>>>>> 2f3bd4089 (Add calls to no recent export interaction endpoint)
 
-    it('should hide the ELD settings table and edit link', () => {
-      cy.get(eslTable).should('not.be.visible')
-      cy.get(eslLink).should('not.be.visible')
-    })
+    assertSettingsTableNotVisible('ELD', eslDataTest)
+    assertSettingsTableNotVisible('NRI', nriDataTest)
+    assertSettingsTableVisible('ENRI', enriDataTest)
 
-    it('should show the NRI settings table and edit link', () => {
-      cy.get(nriTable).should('not.be.visible')
-      cy.get(nriLink).should('not.be.visible')
-    })
-
-    it('should hide the ENRI settings table and edit link', () => {
-      cy.get(enriTable).should('be.visible')
-      cy.get(enriLink).should('be.visible')
-    })
-
-    it('should render the ENRI settings table', () => {
-      assertKeyValueTable('export-no-recent-interactions-table', {
-        Reminders: '10, 30 and 40 days after the last interaction',
-        'Email notifications': 'On',
-      })
-    })
-
-    it('should hide the ENRI settings table on toggle', () => {
-      cy.get(enriToggle)
-        .find('button')
-        .contains('Companies with no recent interaction settings')
-        .click()
-        .get(enriTable)
-        .should('not.be.visible')
-        .get(enriLink)
-        .should('not.be.visible')
+    assertSettingsTableToggles({
+      title: 'ENRI',
+      dataTest: enriDataTest,
+      reminderText: '10, 30 and 40 days after the last interaction',
+      buttonText: 'Companies with no recent interaction settings',
     })
   })
 
   context('When all settings are visible', () => {
     const queryParams =
-      'estimated_land_date=true&no_recent_interaction=true&export_no_recent_interactions=true'
+      'investments_estimated_land_date=true&investments_no_recent_interaction=true&exports_no_recent_interactions=true'
     before(() => {
       interceptAPICalls()
       cy.visit(`${urls.reminders.settings.index()}?${queryParams}`)
       waitForAPICalls()
     })
 
-    it('should show the estimated land date settings table and edit link', () => {
-      cy.get(eslTable).should('be.visible')
-      cy.get(eslLink).should('be.visible')
-    })
-
-    it('should show the no recent interaction settings table and edit link', () => {
-      cy.get(nriTable).should('be.visible')
-      cy.get(nriLink).should('be.visible')
-    })
-
-    it('should show the no recent export interaction settings table and edit link', () => {
-      cy.get(enriTable).should('be.visible')
-      cy.get(enriLink).should('be.visible')
-    })
+    assertSettingsTableVisible('ELD', eslDataTest)
+    assertSettingsTableVisible('NRI', nriDataTest)
+    assertSettingsTableVisible('ENRI', enriDataTest)
   })
 
   context('When no settings have been set - the default', () => {
