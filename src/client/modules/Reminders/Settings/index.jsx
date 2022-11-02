1ec6bdb6d3b0d8a05a728941ef94a76dbfb099da
Rachael Robinson <rachaelcodes@users.noreply.github.com>
2022-11-02 11:12:32 +0200

    refactor to reduce repetition

--- src/client/modules/Reminders/Settings/index.jsx
+++ src/client/modules/Reminders/Settings/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { useLocation, Link as RouterLink } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import Link from '@govuk-react/link'
 import styled from 'styled-components'
-import Table from '@govuk-react/table'
 import { H2 } from '@govuk-react/heading'
-import { SPACING, MEDIA_QUERIES, LEVEL_SIZE } from '@govuk-react/constants'
+import { SPACING, LEVEL_SIZE } from '@govuk-react/constants'
 import { get } from 'lodash'
 import qs from 'qs'
 
@@ -17,25 +16,6 @@ import { TASK_GET_ALL_REMINDER_SUBSCRIPTIONS } from '../state'
 const ToggleSectionContainer = styled('div')({
   marginTop: SPACING.SCALE_3,
   marginBottom: '40px',
-})
-
-const StyledTable = styled(Table)({
-  marginTop: 0,
-  marginLeft: SPACING.SCALE_1,
-})
-
-const StyledCellHeader = styled(Table.CellHeader)({
-  [MEDIA_QUERIES.TABLET]: {
-    width: '33%',
-  },
-  [MEDIA_QUERIES.DESKTOP]: {
-    width: '33%',
-  },
-})
-
-const StyledRouterLink = styled(RouterLink)({
-  marginBottom: SPACING.SCALE_5,
-  display: 'block',
 })
 
 const StyledHomeLink = styled(Link)({
@@ -80,8 +60,7 @@ const RemindersSettings = () => {
           noRecentInteraction,
           exportNoRecentInteractions,
         }) => (
-          <div>
-            {/* TODO: check if div is the correct wrapper in this codebase */}
+          <>
             {/* TODO: Confirm H2 size */}
             {/* TODO: check the spacing around the home link */}
             {/* TODO: are we updating no-recent-interaction in this or a future ticket? */}
@@ -92,58 +71,20 @@ const RemindersSettings = () => {
                 id="estimated-land-date-toggle"
                 data-test="estimated-land-date-toggle"
                 isOpen={openESL}
-              >
-                <StyledTable data-test="estimated-land-date-table">
-                  <Table.Row>
-                    <StyledCellHeader>Reminders</StyledCellHeader>
-                    <Table.Cell>
-                      {estimatedLandDate.formattedReminderDays}
-                    </Table.Cell>
-                  </Table.Row>
-                  <Table.Row>
-                    <StyledCellHeader>Email notifications</StyledCellHeader>
-                    <Table.Cell>
-                      {estimatedLandDate.emailRemindersOnOff}
-                    </Table.Cell>
-                  </Table.Row>
-                </StyledTable>
-                <StyledRouterLink
-                  data-test="estimated-land-date-link"
-                  to={urls.reminders.settings.investments.estimatedLandDate()}
-                  aria-label="edit"
-                >
-                  Edit
-                </StyledRouterLink>
-              </RemindersToggleSection>
+                dataName={'estimated-land-date'}
+                data={estimatedLandDate}
+                to={urls.reminders.settings.investments.estimatedLandDate()}
+              />
               <RemindersToggleSection
                 label="Projects with no recent interaction settings"
                 id="no-recent-interaction-toggle"
                 data-test="no-recent-interaction-toggle"
                 isOpen={openNRI}
                 borderBottom={true}
-              >
-                <StyledTable data-test="no-recent-interaction-table">
-                  <Table.Row>
-                    <StyledCellHeader>Reminders</StyledCellHeader>
-                    <Table.Cell>
-                      {noRecentInteraction.formattedReminderDays}
-                    </Table.Cell>
-                  </Table.Row>
-                  <Table.Row>
-                    <StyledCellHeader>Email notifications</StyledCellHeader>
-                    <Table.Cell>
-                      {noRecentInteraction.emailRemindersOnOff}
-                    </Table.Cell>
-                  </Table.Row>
-                </StyledTable>
-                <StyledRouterLink
-                  data-test="no-recent-interaction-link"
-                  to={urls.reminders.settings.investments.noRecentInteraction()}
-                  aria-label="edit"
-                >
-                  Edit
-                </StyledRouterLink>
-              </RemindersToggleSection>
+                dataName={'no-recent-interaction'}
+                data={noRecentInteraction}
+                to={urls.reminders.settings.investments.noRecentInteraction()}
+              />
             </ToggleSectionContainer>
             <H2 size={LEVEL_SIZE[3]}>Export</H2>
             <ToggleSectionContainer>
@@ -153,34 +94,15 @@ const RemindersSettings = () => {
                 data-test="export-no-recent-interactions-toggle"
                 isOpen={openENRI}
                 borderBottom={true}
-              >
-                <StyledTable data-test="export-no-recent-interactions-table">
-                  <Table.Row>
-                    <StyledCellHeader>Reminders</StyledCellHeader>
-                    <Table.Cell>
-                      {exportNoRecentInteractions.formattedReminderDays}
-                    </Table.Cell>
-                  </Table.Row>
-                  <Table.Row>
-                    <StyledCellHeader>Email notifications</StyledCellHeader>
-                    <Table.Cell>
-                      {exportNoRecentInteractions.emailRemindersOnOff}
-                    </Table.Cell>
-                  </Table.Row>
-                </StyledTable>
-                <StyledRouterLink
-                  data-test="export-no-recent-interactions-link"
-                  to={urls.reminders.settings.exports.noRecentInteraction()}
-                  aria-label="edit"
-                >
-                  Edit
-                </StyledRouterLink>
-              </RemindersToggleSection>
+                dataName={'export-no-recent-interactions'}
+                data={exportNoRecentInteractions}
+                to={urls.reminders.settings.exports.noRecentInteraction()}
+              />
             </ToggleSectionContainer>
             <StyledHomeLink href={urls.dashboard()} aria-label="Home">
               Home
             </StyledHomeLink>
-          </div>
+          </>
         )}
       </Resource>
     </DefaultLayout>
--- src/client/modules/Reminders/tasks.js
+++ src/client/modules/Reminders/tasks.js
@@ -3,6 +3,16 @@ import { apiProxyAxios } from '../../components/Task/utils'
 import { formatDays, transformReminderDaysToForm } from './transformers'
 import { settings } from './constants'
 
+const transformNoRecentInteractionsSubscriptionSummary = (data) => ({
+  formattedReminderDays: formatDays(
+    data.reminder_days.sort((a, b) => a - b),
+    'days after the last interaction'
+  ),
+  emailRemindersOnOff: data.email_reminders_enabled
+    ? settings.ON
+    : settings.OFF,
+})
+
 const transformAllSubscriptions = ([eld, nri, enri]) => ({
   estimatedLandDate: {
     formattedReminderDays: formatDays(
@@ -13,24 +23,12 @@ const transformAllSubscriptions = ([eld, nri, enri]) => ({
       ? settings.ON
       : settings.OFF,
   },
-  noRecentInteraction: {
-    formattedReminderDays: formatDays(
-      nri.data.reminder_days.sort((a, b) => a - b),
-      'days after the last interaction'
-    ),
-    emailRemindersOnOff: nri.data.email_reminders_enabled
-      ? settings.ON
-      : settings.OFF,
-  },
-  exportNoRecentInteractions: {
-    formattedReminderDays: formatDays(
-      enri.data.reminder_days.sort((a, b) => a - b),
-      'days after the last interaction'
-    ),
-    emailRemindersOnOff: enri.data.email_reminders_enabled
-      ? settings.ON
-      : settings.OFF,
-  },
+  noRecentInteraction: transformNoRecentInteractionsSubscriptionSummary(
+    nri.data
+  ),
+  exportNoRecentInteractions: transformNoRecentInteractionsSubscriptionSummary(
+    enri.data
+  ),
 })
 
 export const getAllSubscriptions = () =>
