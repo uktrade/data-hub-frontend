9b91755f367089aff3aa17416c07062aac9b107b
Pippo Raimondi <piporamaa@gmail.com>
2022-11-03 08:32:41 +0000

    Add desktop and tablet view

--- src/client/components/LocalHeader/Banner.jsx
+++ src/client/components/LocalHeader/Banner.jsx
@@ -4,6 +4,7 @@ import { state2props } from './state'
 
 import PropTypes from 'prop-types'
 
+import { MEDIA_QUERIES } from '@govuk-react/constants'
 import styled from 'styled-components'
 import { WHITE } from 'govuk-colours'
 
@@ -17,13 +18,23 @@ const MEDIUMBLUE = '#003399'
 const StyledBody = styled('div')`
   background-color: ${MEDIUMBLUE};
   color: ${WHITE};
-  height: 40px;
+  ${MEDIA_QUERIES.TABLET} {
+    height: 40px;
+  }
+  ${MEDIA_QUERIES.DESKTOP} {
+    height: 40px;
+  }
 `
 
 const StyledDiv = styled('div')`
-  margin-left: 20%;
+  text-align: center;
   position: relative;
-  padding-top: 0.9%;
+  ${MEDIA_QUERIES.TABLET} {
+    padding-top: 1.5%;
+  }
+  ${MEDIA_QUERIES.DESKTOP} {
+    padding-top: 0.8%;
+  }
 `
 
 const StyledTextLink = styled('a')`
