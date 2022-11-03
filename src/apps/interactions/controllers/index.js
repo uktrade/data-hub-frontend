5739b499bc1e2f9ce37f1f7c7ff4db64e65ef237
Christopher Sunkel <christopher.sunkel@digital.trade.gov.uk>
2022-11-03 13:07:00 +0000

    Remove legacy code

--- src/apps/interactions/controllers/index.js
+++ src/apps/interactions/controllers/index.js
@@ -1,9 +1,7 @@
-const completeController = require('./complete')
 const detailsController = require('./details')
 const listController = require('./list')
 
 module.exports = {
-  completeController,
   detailsController,
   listController,
 }
