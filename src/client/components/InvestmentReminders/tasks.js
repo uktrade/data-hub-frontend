6e02663683859d41f7cf3a7a36cb15b15f46a833
Pippo Raimondi <piporamaa@gmail.com>
2022-11-14 18:41:42 +0000

    Update investment reminders component to accommodate new design

--- src/client/components/InvestmentReminders/tasks.js
+++ src/client/components/InvestmentReminders/tasks.js
@@ -1,13 +1,34 @@
 import { apiProxyAxios } from '../Task/utils'
 
-export const fetchOutstandingPropositions = ({ adviser }) =>
-  apiProxyAxios
-    .get('/v4/proposition', {
+export const fetchOutstandingPropositions = async ({ adviser }) => {
+  const investmentELDResponse = await apiProxyAxios.get(
+    '/v4/reminder/estimated-land-date',
+    {
       params: {
         adviser_id: adviser.id,
-        status: 'ongoing',
-        sortby: 'deadline',
-        limit: 3,
       },
-    })
-    .then(({ data }) => data)
+    }
+  )
+
+  const investmentNRIResponse = await apiProxyAxios.get(
+    '/v4/reminder/no-recent-investment-interaction',
+    {
+      params: {
+        adviser_id: adviser.id,
+      },
+    }
+  )
+
+  const investmentOPResponse = await apiProxyAxios.get('/v4/proposition', {
+    params: {
+      adviser_id: adviser.id,
+    },
+  })
+
+  const investmentELD = investmentELDResponse.data
+  const investmentNRI = investmentNRIResponse.data
+  const investmentOP = investmentOPResponse.data
+  const count = investmentELD.count + investmentNRI.count + investmentOP.count
+
+  return { count, investmentELD, investmentNRI, investmentOP }
+}
--- test/functional/cypress/specs/dashboard-new/reminders-spec.js
+++ test/functional/cypress/specs/dashboard-new/reminders-spec.js
@@ -1,31 +1,9 @@
-const {
-  addDays,
-  formatWithoutParsing,
-} = require('../../../../../src/client/utils/date')
-const { DATE_DAY_LONG_FORMAT } = require('../../../../../src/common/constants')
-
-import { faker } from '@faker-js/faker'
 import urls from '../../../../../src/lib/urls'
-import {
-  propositionFaker,
-  propositionListFaker,
-} from '../../fakers/propositions'
 
 describe('Dashboard reminders', () => {
-  const tomorrow = addDays(new Date(), 1)
-  const proposition1 = propositionFaker({
-    name: 'University Proposition',
-    investment_project: {
-      name: 'University buildings',
-      project_code: 'DHP-00007004',
-      id: faker.datatype.uuid(),
-    },
-    deadline: tomorrow,
-  })
-  const myPropositions = [proposition1, ...propositionListFaker(2)]
-
   before(() => {
     cy.setUserFeatures(['personalised-dashboard'])
+    cy.visit('/')
   })
 
   after(() => {
@@ -43,108 +21,50 @@ describe('Dashboard reminders', () => {
   })
 
   context('View reminders', () => {
-    before(() => {
-      cy.intercept('GET', '/api-proxy/v4/proposition*', {
-        body: {
-          count: myPropositions.length,
-          results: myPropositions,
-        },
-      }).as('apiRequest')
-      cy.visit('/')
-      cy.wait('@apiRequest')
-    })
-
     beforeEach(() => {
-      cy.get('[data-test="outstanding-propositions"]')
-        .as('outstandingPropositions')
-        .find('[data-test="outstanding-propositions-list"]')
-        .as('outstandingPropositionsList')
       cy.get('[data-test="notification-badge"]').as('notificationBadge')
     })
 
     it('should contain a notification badge in the reminders heading', () => {
       cy.get('@investmentRemindersHeading').should('contain.text', 'Reminders')
-      cy.get('@notificationBadge').should('have.text', '3')
+      cy.get('@notificationBadge').should('have.text', '32')
     })
 
-    it('should contain elements in the correct order', () => {
-      cy.get('@investmentReminders')
-        .find('[data-test="outstanding-propositions"]')
-        .should('have.length', 1)
-        .find('[data-test="outstanding-propositions-list"]')
-        .should('have.length', 1)
-    })
+    it('should count reminder links', () => {
+      cy.get('@investmentRemindersToggleButton').click()
 
-    it('should contain a heading', () => {
-      cy.get('[data-test="outstanding-propositions-heading"]')
-        .should('have.text', 'Outstanding propositions (3)')
-        .should('have.css', 'color', 'rgb(212, 53, 28)')
-    })
-
-    it('should display each of the outstanding propositions', () => {
-      cy.get('@outstandingPropositionsList')
-        .find('li')
-        .should('have.length', '3')
-        .first()
-        .as('outstandingProposition')
-
-      cy.get('@outstandingProposition')
-        .find('a')
-        .should('have.text', proposition1.name)
-        .should(
+      cy.contains('Approaching estimated land dates')
+        .should('be.visible')
+        .and(
           'have.attr',
           'href',
-          urls.investments.projects.propositions(
-            proposition1.investment_project.id
-          )
+          urls.reminders.investments.estimatedLandDate()
         )
 
-      cy.get('@outstandingProposition')
-        .find('[data-test="outstanding-proposition-project-code"]')
-        .should('have.text', proposition1.investment_project.project_code)
-
-      cy.get('@outstandingProposition')
-        .find('[data-test="outstanding-proposition-deadline"]')
-        .should(
-          'have.text',
-          `Due ${formatWithoutParsing(tomorrow, DATE_DAY_LONG_FORMAT)}`
+      cy.contains('Projects with no recent interactions')
+        .should('be.visible')
+        .and(
+          'have.attr',
+          'href',
+          urls.reminders.investments.noRecentInteraction()
         )
 
-      cy.get('@outstandingProposition')
-        .find('[data-test="outstanding-proposition-countdown"]')
-        .should('have.text', '1 day')
-    })
-  })
-
-  context('View empty reminders', () => {
-    before(() => {
-      cy.intercept('GET', '/api-proxy/v4/proposition*', {
-        body: {
-          count: 0,
-          results: [],
-        },
-      }).as('apiRequest')
-      cy.visit('/')
-      cy.wait('@apiRequest')
-    })
-
-    it('should contain a heading', () => {
-      cy.get('[data-test="outstanding-propositions-empty"]')
-        .should(
-          'have.text',
-          'Projects with propositions due will be displayed here.'
+      cy.contains('Outstanding propositions')
+        .should('be.visible')
+        .and(
+          'have.attr',
+          'href',
+          urls.reminders.investments.outstandingPropositions()
         )
-        .should('have.css', 'color', 'rgb(80, 90, 95)')
     })
 
-    it('should not contain a notification badge in the reminders heading', () => {
-      cy.get('[data-test="investment-reminders-section"]')
-        .find('[data-test="notification-badge"]')
-        .should('not.exist')
-    })
+    it('should have reminders count', () => {
+      cy.get('[data-testid="investment-eld"]').should('have.text', 14)
+      cy.get('[data-testid="investment-nri"]').should('have.text', 15)
+      cy.get('[data-testid="investment-op"]').should('have.text', 3)
 
-    it('should not contain a list', () => {
-      cy.get('[data-test="outstanding-propositions-list"]').should('not.exist')
+      cy.get('[data-testid="export-nri"]').should('have.text', 14)
+      cy.get('[data-testid="export-ni"]').should('have.text', 14)
     })
   })
 
