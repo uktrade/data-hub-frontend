602354d72f0d08aea81fed9e0eb76bd30fdc4ec9
Paul Wheatcroft <paul_wheatcroft@hotmail.com>
2022-11-25 15:32:33 +0000

    Added support for multiple url variable identifiers
    Co-Authored-By: Onyela Ogah <83657534+onyela-o@users.noreply.github.com>

---
+++ test/a11y/cypress/specs/integration/testIdentityNumbers.js
@@ -0,0 +1,8 @@
+export const testIdentityNumbers = {
+  ':companyId': 'a96470e0-d273-42b2-8632-d4cd1b58be75',
+  ':referralId': '',
+  ':countryId': '3e6debb4-1596-40c5-aa25-f00da0e05af9',
+  ':globalHqId': '3e6debb4-1596-40c5-aa25-f00da0e05af9',
+  ':subsidiaryCompanyId': '',
+  ':interactionId': '',
+}
