a02f6150d64b2fb4613643bae443ca82eb677483
Paul Wheatcroft <paul_wheatcroft@hotmail.com>
2022-11-30 11:55:10 +0000

    Amended array creation to remove data endpoints
    Co-Authored-By: Onyela Ogah <83657534+onyela-o@users.noreply.github.com>

--- test/a11y/cypress/specs/integration/testIdentityNumbers.js
+++ test/a11y/cypress/specs/integration/testIdentityNumbers.js
@@ -1,8 +1,10 @@
 export const testIdentityNumbers = {
-  ':companyId': 'a96470e0-d273-42b2-8632-d4cd1b58be75',
+  ':companyId': '0fb3379c-341c-4da4-b825-bf8d47b26baa',
   ':referralId': '',
   ':countryId': '3e6debb4-1596-40c5-aa25-f00da0e05af9',
   ':globalHqId': '3e6debb4-1596-40c5-aa25-f00da0e05af9',
   ':subsidiaryCompanyId': '',
   ':interactionId': '',
+  ':eventId': 'b93d4273-36fe-4008-ac40-fbc197910791',
+  ':aventriEventId': '1111',
 }
