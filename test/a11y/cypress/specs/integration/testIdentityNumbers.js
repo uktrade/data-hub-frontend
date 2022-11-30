f13a57197d3e4f98b560e09d78a6336c30c255ed
Paul Wheatcroft <paul_wheatcroft@hotmail.com>
2022-11-30 17:12:02 +0000

    Added url exclusion mechanism for tests that fail for pages that can't be found
    Co-Authored-By: Onyela Ogah <83657534+onyela-o@users.noreply.github.com>

--- test/a11y/cypress/specs/integration/testIdentityNumbers.js
+++ test/a11y/cypress/specs/integration/testIdentityNumbers.js
@@ -1,9 +1,9 @@
 export const testIdentityNumbers = {
-  ':companyId': '0fb3379c-341c-4da4-b825-bf8d47b26baa',
-  ':referralId': '',
+  ':companyId': 's07e2f19-8251-1a41-h27a-f98737520831',
+  ':referralId': 'cc5b9953-894c-44b4-a4ac-d0f6a6f6128f',
   ':countryId': '3e6debb4-1596-40c5-aa25-f00da0e05af9',
   ':globalHqId': '3e6debb4-1596-40c5-aa25-f00da0e05af9',
-  ':subsidiaryCompanyId': '',
+  ':subsidiaryCompanyId': 'cc7e2f19-7251-4a41-a27a-f98437720532',
   ':interactionId': '',
   ':eventId': 'b93d4273-36fe-4008-ac40-fbc197910791',
   ':aventriEventId': '1111',
