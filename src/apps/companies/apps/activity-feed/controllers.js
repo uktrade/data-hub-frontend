c63385538e40f108d7bb4abc74c0394b9ce50646
Chris Hopkins <chris.hopkins@madetech.com>
2022-12-02 15:53:42 +0000

    only get aventri id's if feature flag is enabled

--- src/apps/companies/apps/activity-feed/controllers.js
+++ src/apps/companies/apps/activity-feed/controllers.js
@@ -332,13 +332,29 @@ async function fetchActivityFeedHandler(req, res, next) {
         .map((company) => company.id)
     }
 
-    const aventriEvents = await getAventriEventsAttendedByCompanyContacts(
-      req,
-      next,
-      company.contacts
+    res.locals.userFeatures = await fetchUserFeatureFlags(req).catch(
+      // istanbul ignore next: Covered by functional tests
+      (error) => {
+        next(error)
+      }
     )
 
-    const aventriEventIds = Object.keys(aventriEvents)
+    // istanbul ignore next: Covered by functional tests
+    let isActivityStreamFeatureFlagEnabled = res.locals?.userFeatures?.includes(
+      ACTIVITY_STREAM_FEATURE_FLAG
+    )
+
+    let aventriEventIds = []
+    let aventriEvents = []
+    if (isActivityStreamFeatureFlagEnabled) {
+      aventriEvents = await getAventriEventsAttendedByCompanyContacts(
+        req,
+        next,
+        company.contacts
+      )
+
+      aventriEventIds = Object.keys(aventriEvents)
+    }
 
     const queries = getQueries({
       from,
