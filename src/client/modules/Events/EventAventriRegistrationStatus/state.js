3ac742d0c3a4e067e22b52ad265efb69d2849ea2
Chris Hopkins <chris.hopkins@madetech.com>
2022-12-05 14:05:03 +0000

    added unit tests for the mapping datahub status to aventri

--- src/client/modules/Events/EventAventriRegistrationStatus/state.js
+++ src/client/modules/Events/EventAventriRegistrationStatus/state.js
@@ -15,6 +15,22 @@ const parseQueryString = (queryString) => {
   }
 }
 
+export const mapUrlSlugToAventriRegistrationStatuses = (urlSlug) => {
+  let registrationStatus = null
+
+  const aventriRegistrationStatuses = Object.entries(
+    EVENT_AVENTRI_ATTENDEES_MAPPING
+  ).reduce((status, item) => {
+    if (item[1].urlSlug == urlSlug) {
+      registrationStatus = item[1].status
+      status.push(item[0])
+    }
+    return status
+  }, [])
+
+  return { registrationStatus, aventriRegistrationStatuses }
+}
+
 export const state2props = (state, router) => {
   const { match, location } = router
   const queryString = location.search.slice(1)
@@ -22,17 +38,8 @@ export const state2props = (state, router) => {
   const selectedSortBy =
     qs.parse(location.search.slice(1)).sortby || 'first_name:asc'
 
-  let registrationStatus = null
-
-  const aventriRegistrationStatuses = Object.entries(
-    EVENT_AVENTRI_ATTENDEES_MAPPING
-  ).reduce((status, item) => {
-    if (item[1].urlSlug == match.params.status) {
-      registrationStatus = item[1].status
-      status.push(item[0])
-    }
-    return status
-  }, [])
+  const { registrationStatus, aventriRegistrationStatuses } =
+    mapUrlSlugToAventriRegistrationStatuses(match.params.status)
 
   return {
     payload: { ...queryParams, selectedSortBy },
