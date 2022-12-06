d9c411da9e8045c47abe5a1dce6f6f8ab59e53f9
Paul Wheatcroft <paul_wheatcroft@hotmail.com>
2022-12-06 14:54:39 +0000

    Sanitising URL exclusions
    Co-Authored-By: Onyela Ogah <83657534+onyela-o@users.noreply.github.com>

--- test/a11y/cypress/specs/integration/config/urlTestExclusions.js
+++ test/a11y/cypress/specs/integration/config/urlTestExclusions.js
@@ -1,34 +1,49 @@
 export const urlTestExclusions = {
   companies: [
+    // 404 errors for the below
     { url: '/companies/:companyId/audit' },
-    { url: '/companies/:companyId/advisers' },
     { url: '/companies/:companyId/archive' },
     { url: '/companies/:companyId/lists' },
     { url: '/companies/:companyId/manage-company-list' },
+    // don't have permission to view this page
     { url: '/companies/:companyId/dnb-hierarchy' },
+    // No history countryId for the sandbox companyId
     { url: '/companies/:companyId/exports/history/:countryId' },
     { url: '/companies/:companyId/referrals/:referralId/interactions' },
   ],
   contacts: [
+    // 404 errors for the below
     { url: '/contacts/:contactId/archive' },
     { url: '/contacts/:contactId/unarchive' },
-    { url: '/contacts/:companyId' },
   ],
   companyLists: [
     { url: '/company-lists/:listId/delete' },
     { url: '/company-lists/:listId/rename' },
   ],
   dashboard: [],
+  interactions: [],
   investments: [
+    // don't have permission to view this page
     { url: '/investments/projects/:investmentId/propositions' },
     { url: '/investments/projects/:investmentId/propositions/:propositionId' },
+    // 404 error for the below
     { url: '/investments/opportunities/:opportunityId' },
+    // No sandbox data available for testing the below locations
     { url: '/investments/projects/create/investment-type/info/:anchor' },
+    // 404 error for the below
     { url: '/investments/:investmentId/edit-history' },
+    // No sandbox data available for testing the below locations
     { url: '/investments/:investmentId/interactions/:interactionsId' },
   ],
   omis: [
-    { url: '/omis/:companyId' },
+    // 404 error for the below
     { url: '/omis/edit/payment-reconciliation' },
+  ],
+  pipelines: [
+    // No sandbox data available for testing the below locations
+    { url: '/my-pipeline/:pipelineItemId/edit' },
+    { url: '/my-pipeline/:pipelineItemId/archive' },
+    { url: '/my-pipeline/:pipelineItemId/unarchive' },
+    { url: '/my-pipeline/:pipelineItemId/delete' },
   ],
 }
