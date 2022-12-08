42e0fc66c75a08b92f01bf7576b2d13976e4786f
richmadetech <richard.byrne@madetech.com>
2022-12-08 13:17:45 +0000

    Added export support service card design and query

--- src/apps/companies/apps/activity-feed/es-queries/data-hub-and-aventri-activity-query.js
+++ src/apps/companies/apps/activity-feed/es-queries/data-hub-and-aventri-activity-query.js
@@ -4,6 +4,7 @@ const dataHubAndAventriActivityQuery = ({
   types,
   companyIds,
   aventriEventIds,
+  contacts,
 }) => {
   const shouldCriteria = [
     {
@@ -18,6 +19,29 @@ const dataHubAndAventriActivityQuery = ({
             terms: {
               'object.attributedTo.id': [
                 ...companyIds.map((id) => `dit:DataHubCompany:${id}`),
+              ],
+            },
+          },
+        ],
+      },
+      bool: {
+        must: [
+          {
+            term: {
+              'object.attributedTo.id':
+                'dit:directoryFormsApi:SubmissionType:export-support-service',
+            },
+          },
+          {
+            term: {
+              'object.attributedTo.type':
+                'dit:directoryFormsApi:SubmissionAction:zendesk',
+            },
+          },
+          {
+            terms: {
+              'actor.dit:emailAddress': [
+                ...contacts.map((contact) => contact.email),
               ],
             },
           },
--- src/client/components/ActivityFeed/activities/DirectoryFormsApi.jsx
+++ src/client/components/ActivityFeed/activities/DirectoryFormsApi.jsx
@@ -23,33 +23,55 @@ export default class DirectoryFormsApi extends React.PureComponent {
 
   render() {
     const { activity } = this.props
+
+    const formType = get(activity, 'object.attributedTo.id')
     const sentDate = get(activity, 'object.published')
     const formData = get(
       activity,
       'object.dit:directoryFormsApi:Submission:Data'
     )
 
-    const metadata = [
-      { label: 'Date', value: format(sentDate) },
-      {
-        label: 'Name',
-        value: `${formData.first_name} ${formData.last_name}`,
-      },
-      { label: 'Job title', value: formData.position },
-      { label: 'Email', value: formData.email },
-    ]
-
-    return (
-      <ActivityCardWrapper>
-        <ActivityCardLabels
-          theme="great.gov.uk"
-          service="export"
-          kind="great.gov.uk Enquiry"
-        />
-        <ActivityCardSubject>Enquiry</ActivityCardSubject>
-        <ActivityCardNotes notes={formData.comment} />
-        <ActivityCardMetadata metadata={metadata} />
-      </ActivityCardWrapper>
-    )
+    //if export support submission
+    if (
+      formType === 'dit:directoryFormsApi:SubmissionType:export-support-service'
+    ) {
+      const metadata = [{ label: 'Date', value: format(sentDate) }]
+      return (
+        <ActivityCardWrapper dataTest="export-service-support">
+          <ActivityCardLabels
+            theme="export"
+            service="Export Service Support"
+            kind="Interaction"
+          />
+          <ActivityCardSubject dataTest="export-service-support-name">
+            {formData.enquiry_subject}
+          </ActivityCardSubject>
+          <ActivityCardNotes notes={formData.nature_of_enquiry} />
+          <ActivityCardMetadata metadata={metadata} />
+        </ActivityCardWrapper>
+      )
+    } else {
+      const metadata = [
+        { label: 'Date', value: format(sentDate) },
+        {
+          label: 'Name',
+          value: `${formData.first_name} ${formData.last_name}`,
+        },
+        { label: 'Job title', value: formData.position },
+        { label: 'Email', value: formData.email },
+      ]
+      return (
+        <ActivityCardWrapper>
+          <ActivityCardLabels
+            theme="great.gov.uk"
+            service="export"
+            kind="great.gov.uk Enquiry"
+          />
+          <ActivityCardSubject>Enquiry</ActivityCardSubject>
+          <ActivityCardNotes notes={formData.comment} />
+          <ActivityCardMetadata metadata={metadata} />
+        </ActivityCardWrapper>
+      )
+    }
   }
 }
