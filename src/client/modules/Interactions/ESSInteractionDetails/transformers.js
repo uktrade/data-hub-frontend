bda4ab7e6767cf09191cb4ebd8d5dd9dcae7980b
richmadetech <richard.byrne@madetech.com>
2022-12-21 14:49:19 +0000

    Added default subject for ESS Details Page

--- src/client/modules/Interactions/ESSInteractionDetails/transformers.js
+++ src/client/modules/Interactions/ESSInteractionDetails/transformers.js
@@ -3,10 +3,11 @@ const { formatLongDate } = require('../../../../client/utils/date')
 
 const transformResponseToESSInteractionDetails = ({ id, object }) => {
   const formData = get(object, 'dit:directoryFormsApi:Submission:Data')
-  const nature_of_enquiry = formData.nature_of_enquiry
   const essDetails = {
     id,
-    subject: nature_of_enquiry,
+    subject: formData.nature_of_enquiry
+      ? formData.nature_of_enquiry
+      : 'ESS Inbound Enquiry',
     question: formData.aaa_question,
     dateOfInteraction: formatLongDate(object.published),
     countries: formData.countries,
