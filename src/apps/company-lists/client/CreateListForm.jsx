ca22a289d5815dd63b2c7a069970a659c17eb8bd
santosh dasari <74972011+santoshdasa12345@users.noreply.github.com>
2022-12-30 09:52:47 +0000

    Update form layout

--- src/apps/company-lists/client/CreateListForm.jsx
+++ src/apps/company-lists/client/CreateListForm.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { FieldInput } from '../../../client/components'
+import { FieldInput, FormLayout } from '../../../client/components'
 import Form from '../../../client/components/Form'
+import FORM_LAYOUT from '../../../../../common/constants'
 
 const CreateListForm = ({
   id,
@@ -12,35 +13,37 @@ const CreateListForm = ({
   maxLength,
   csrfToken,
 }) => (
-  <Form
-    id="create-list-form"
-    submissionTaskName="Create list"
-    analyticsFormName="create-list-form"
-    redirectTo={() => cancelUrl}
-    flashMessage={() => 'Company list created'}
-    submitButtonLabel="Create list"
-    cancelRedirectTo={() => cancelUrl}
-    transformPayload={(values) => ({
-      id,
-      values,
-      csrfToken,
-    })}
-  >
-    {() => (
-      <FieldInput
-        name={name}
-        type="text"
-        label={label}
-        required="Enter a name for your list"
-        hint={hint}
-        validate={(value) =>
-          value && value.length > maxLength
-            ? `Enter list name which is no longer than ${maxLength} characters`
-            : null
-        }
-      />
-    )}
-  </Form>
+  <FormLayout setWidth={FORM_LAYOUT.ONE_THIRD}>
+    <Form
+      id="create-list-form"
+      submissionTaskName="Create list"
+      analyticsFormName="create-list-form"
+      redirectTo={() => cancelUrl}
+      flashMessage={() => 'Company list created'}
+      submitButtonLabel="Create list"
+      cancelRedirectTo={() => cancelUrl}
+      transformPayload={(values) => ({
+        id,
+        values,
+        csrfToken,
+      })}
+    >
+      {() => (
+        <FieldInput
+          name={name}
+          type="text"
+          label={label}
+          required="Enter a name for your list"
+          hint={hint}
+          validate={(value) =>
+            value && value.length > maxLength
+              ? `Enter list name which is no longer than ${maxLength} characters`
+              : null
+          }
+        />
+      )}
+    </Form>
+  </FormLayout>
 )
 
 CreateListForm.propTypes = {
