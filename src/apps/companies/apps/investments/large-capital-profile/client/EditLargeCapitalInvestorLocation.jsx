cfda0068950b735363e2b7f52f8a4edeba5715ba
santosh dasari <74972011+santoshdasa12345@users.noreply.github.com>
2023-01-12 16:29:17 +0000

    Add Form in react

--- src/apps/companies/apps/investments/large-capital-profile/client/EditLargeCapitalInvestorLocation.jsx
+++ src/apps/companies/apps/investments/large-capital-profile/client/EditLargeCapitalInvestorLocation.jsx
@@ -1,12 +1,69 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { FormLayout } from '../../../../../../client/components'
-import { FORM_LAYOUT } from '../../../../../../common/constants'
 
-const EditLargeCapitalInvestorLocation = ({}) => {
+import PropTypes from 'prop-types'
+import {
+  Form,
+  FieldTypeahead,
+  FormLayout,
+  Typeahead,
+  FieldTextarea,
+} from '../../../../../../client/components'
+import { FORM_LAYOUT } from '../../../../../../common/constants'
+import urls from '../../../../../../lib/urls'
+import { FieldUKRegionTypeahead } from '../../../../../../client/components/Form/elements/UKRegionOptions'
+import FieldCountrySelect from '../../../../../../client/components/Form/elements/FieldCountrySelect'
+
+const EditLargeCapitalInvestorLocation = ({
+  companyId,
+  profileId,
+  ukRegionLocations,
+  otherCountriesBeingConsidered,
+}) => {
   return (
     <FormLayout setWidth={FORM_LAYOUT.THREE_QUARTERS}>
-      Edit Large Capital Investor Location HERE
+      <Form
+        id="edit-large-capital-investor-location"
+        //Todo: analyticsFormName="editLargeCapitalInvestorDetails"
+        cancelButtonLabel="Return without saving"
+        cancelRedirectTo={() =>
+          urls.companies.investments.largeCapitalProfile(companyId)
+        }
+        flashMessage={() => 'Investor location details changes saved'}
+        submitButtonLabel="Save and return"
+        //Todo: submissionTaskName={TASK_SAVE_LARGE_CAPITAL_INVESTOR_DETAILS}
+        redirectTo={() =>
+          urls.companies.investments.largeCapitalProfile(companyId)
+        }
+        //Todo: transformPayload={(values) =>
+        //   transformInvestorDetailsToApi({ profileId, companyId, values })
+        // }
+      >
+        {() => (
+          <>
+            <FieldUKRegionTypeahead
+              name="uk_region_locations"
+              label="UK locations of interest"
+              //Todo: initialValue={transformObjectToValueLabel(investorType)}
+              //Todo: options={transformIdNameToValueLabel(investorType?.items)}
+              placeholder="-- Select region --"
+              arial-label="Uk locations of interest"
+              isMulti={true}
+            />
+            <FieldCountrySelect
+              isMulti={true}
+              label="Other countries the investor is considering"
+              name="other_countries_being_considered"
+              placeholder="-- Select country --"
+              options={otherCountriesBeingConsidered}
+            />
+            <FieldTextarea
+              name="notes_on_locations"
+              label="Notes on investor's location preferences"
+              type="text"
+            />
+          </>
+        )}
+      </Form>
     </FormLayout>
   )
 }
