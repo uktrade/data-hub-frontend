3b8294eba0de224a5c52d91530e1198ea1e4fb41
Chris Hopkins <chris.hopkins@madetech.com>
2023-01-16 16:26:46 +0000

    move duplicate exports to shared file

--- src/client/modules/Reminders/ExportsNewInteractionsList.jsx
+++ src/client/modules/Reminders/ExportsNewInteractionsList.jsx
@@ -1,11 +1,6 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { BLACK } from 'govuk-colours'
-import { SPACING, FONT_SIZE } from '@govuk-react/constants'
-import styled from 'styled-components'
-import qs from 'qs'
-
+import ExportsList from './ExportsList'
 import {
   REMINDERS__EXPORTS_NEW_INTERACTION_REMINDERS_LOADED,
   REMINDERS__EXPORTS_NEW_INTERACTION_REMINDERS_DELETED,
@@ -18,98 +13,6 @@ import {
   TASK_DELETE_EXPORT_NEW_INTERACTION_REMINDER,
   TASK_GET_EXPORTS_NEXT_NEW_INTERACTION_REMINDERS,
 } from './state'
-
-import { sortOptions, maxItemsToPaginate, itemsPerPage } from './constants'
-
-import { CollectionSort, RoutedPagination } from '../../components'
-import ExportItemRenderer from './ItemRenderers/ExportItemRenderer'
-import CollectionHeader from './CollectionHeader'
-import CollectionList from './CollectionList'
-import Effect from '../../components/Effect'
-import Task from '../../components/Task'
-
-const Summary = styled('p')({
-  color: BLACK,
-  paddingTop: SPACING.SCALE_2,
-  fontSize: FONT_SIZE.SIZE_19,
-})
-
-const ExportsList = ({
-  reminders,
-  pageOrigin,
-  summaryDataTest,
-  taskStatusName,
-  taskStatusSuccessFunction,
-  deleteTaskName,
-  getNextTaskName,
-  effectSuccessFunction,
-  deleteTaskSuccessFunction,
-}) => {
-  const { results, count, nextPending } = reminders
-  const location = useLocation()
-  const qsParams = qs.parse(location.search.slice(1))
-  const page = parseInt(qsParams.page, 10) || 1
-  const totalPages = Math.ceil(
-    Math.min(count, maxItemsToPaginate) / itemsPerPage
-  )
-
-  return (
-    <>
-      <CollectionHeader totalItems={count} pageOrigin={pageOrigin} />
-      {results.length === 0 ? (
-        <Summary data-test={summaryDataTest}>You have no reminders.</Summary>
-      ) : (
-        <CollectionSort sortOptions={sortOptions} totalPages={totalPages} />
-      )}
-      <Task.Status
-        name={taskStatusName}
-        id={ID}
-        startOnRender={{
-          payload: { page, sortby: qsParams.sortby },
-          onSuccessDispatch: taskStatusSuccessFunction,
-        }}
-      >
-        {() => (
-          <Task>
-            {(getTask) => {
-              const deleteTask = getTask(deleteTaskName, ID)
-              const getNextTask = getTask(getNextTaskName, ID)
-              return (
-                <>
-                  <Effect
-                    dependencyList={[nextPending]}
-                    effect={() =>
-                      nextPending &&
-                      getNextTask.start({
-                        payload: {
-                          page,
-                          sortby: qsParams.sortby,
-                        },
-                        onSuccessDispatch: effectSuccessFunction,
-                      })
-                    }
-                  />
-                  <CollectionList
-                    results={results}
-                    itemRenderer={ExportItemRenderer}
-                    disableDelete={deleteTask.status || nextPending}
-                    onDeleteReminder={(reminderId) => {
-                      deleteTask.start({
-                        payload: { id: reminderId },
-                        onSuccessDispatch: deleteTaskSuccessFunction,
-                      })
-                    }}
-                  />
-                  <RoutedPagination initialPage={page} items={count || 0} />
-                </>
-              )
-            }}
-          </Task>
-        )}
-      </Task.Status>
-    </>
-  )
-}
 
 const ExportsNewInteractionsList = ({ exportsNewInteractionReminders }) => {
   return (
@@ -130,85 +33,6 @@ const ExportsNewInteractionsList = ({ exportsNewInteractionReminders }) => {
         REMINDERS__EXPORTS_NEW_INTERACTION_REMINDERS_DELETED
       }
     />
-  )
-
-  const { results, count, nextPending } = exportsNewInteractionReminders
-  const location = useLocation()
-  const qsParams = qs.parse(location.search.slice(1))
-  const page = parseInt(qsParams.page, 10) || 1
-  const totalPages = Math.ceil(
-    Math.min(count, maxItemsToPaginate) / itemsPerPage
-  )
-
-  return (
-    <>
-      <CollectionHeader
-        totalItems={count}
-        pageOrigin="companies_new_interactions"
-      />
-      {results.length === 0 ? (
-        <Summary data-test="exports-new-reminders">
-          You have no reminders.
-        </Summary>
-      ) : (
-        <CollectionSort sortOptions={sortOptions} totalPages={totalPages} />
-      )}
-      <Task.Status
-        name={TASK_GET_EXPORTS_NEW_INTERACTION_REMINDERS}
-        id={ID}
-        startOnRender={{
-          payload: { page, sortby: qsParams.sortby },
-          onSuccessDispatch:
-            REMINDERS__EXPORTS_NEW_INTERACTION_REMINDERS_LOADED,
-        }}
-      >
-        {() => (
-          <Task>
-            {(getTask) => {
-              const deleteTask = getTask(
-                TASK_DELETE_EXPORT_NEW_INTERACTION_REMINDER,
-                ID
-              )
-              const getNextTask = getTask(
-                TASK_GET_EXPORTS_NEXT_NEW_INTERACTION_REMINDERS,
-                ID
-              )
-              return (
-                <>
-                  <Effect
-                    dependencyList={[nextPending]}
-                    effect={() =>
-                      nextPending &&
-                      getNextTask.start({
-                        payload: {
-                          page,
-                          sortby: qsParams.sortby,
-                        },
-                        onSuccessDispatch:
-                          REMINDERS__EXPORTS_NEW_INTERACTION_REMINDERS_GOT_NEXT,
-                      })
-                    }
-                  />
-                  <CollectionList
-                    results={results}
-                    itemRenderer={ExportItemRenderer}
-                    disableDelete={deleteTask.status || nextPending}
-                    onDeleteReminder={(reminderId) => {
-                      deleteTask.start({
-                        payload: { id: reminderId },
-                        onSuccessDispatch:
-                          REMINDERS__EXPORTS_NEW_INTERACTION_REMINDERS_DELETED,
-                      })
-                    }}
-                  />
-                  <RoutedPagination initialPage={page} items={count || 0} />
-                </>
-              )
-            }}
-          </Task>
-        )}
-      </Task.Status>
-    </>
   )
 }
 
