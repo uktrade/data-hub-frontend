1c1eca50dd74731e48fcbd73947ed67e2624f360
Onyela Ogah <83657534+onyela-o@users.noreply.github.com>
2023-01-24 16:54:44 +0000

    Build out CompanyTabbedLocalNavigation component
    Co-Authored-By: Paul Wheatcroft <72826129+PaulWheatcroft@users.noreply.github.com>
    Co-Authored-By: Barbora Kaminskas <106600778+babskami@users.noreply.github.com>

--- src/client/components/CompanyTabbedLocalNavigation/index.jsx
+++ src/client/components/CompanyTabbedLocalNavigation/index.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import { LOCAL_NAV } from '../../../apps/companies/constants'
+import { Link } from 'govuk-react'
 
 const CompanyTabbedLocalNavigation = () => {
-  return <div>This is a local navigation bar</div>
+  return (
+    <div className="govuk-grid-row">
+      <div className="govuk-grid-column-full">
+        <nav
+          className="govuk-tabs"
+          aria-label="local navigation"
+          data-test="tabbedLocalNav"
+        >
+          <ul className="govuk-tabs__list govuk-tabs__list--full">
+            {LOCAL_NAV.map((navItem) => {
+              return (
+                <li className="govuk-tabs__list-item govuk-tabs__list-item--full">
+                  <Link className="govuk-tabs__tab" href={navItem.path}>
+                    {navItem.label}
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        </nav>
+      </div>
+    </div>
+  )
 }
 
 export default CompanyTabbedLocalNavigation
