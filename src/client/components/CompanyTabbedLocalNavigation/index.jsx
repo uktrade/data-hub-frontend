123cef001044bbf4378cfa8afe7c7b8b93fc9e7c
Onyela Ogah <83657534+onyela-o@users.noreply.github.com>
2023-02-06 11:18:54 +0000

    Build out react component

--- src/client/components/CompanyTabbedLocalNavigation/index.jsx
+++ src/client/components/CompanyTabbedLocalNavigation/index.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { LOCAL_NAV } from '../../../apps/companies/constants'
-import { Link } from 'govuk-react'
 
-const CompanyTabbedLocalNavigation = () => {
+const CompanyTabbedLocalNavigation = (props) => {
+  const {
+    company: { id },
+  } = props
+
+  const generateFullUrl = (navItemPath) => `/companies/${id}/${navItemPath}`
+
+  const [active, setActive] = useState('tab-0')
+
+  useEffect(() => {
+    setActive(localStorage.getItem('active'))
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('active', active)
+  }, [active])
+
+  const handleClick = (e) => {
+    const index = e.target.id
+    if (index !== active) {
+      setActive(index)
+    }
+  }
+
   return (
     <div className="govuk-grid-row">
       <div className="govuk-grid-column-full">
@@ -12,12 +34,25 @@ const CompanyTabbedLocalNavigation = () => {
           data-test="tabbedLocalNav"
         >
           <ul className="govuk-tabs__list govuk-tabs__list--full">
-            {LOCAL_NAV.map((navItem) => {
+            {LOCAL_NAV.map((navItem, index) => {
               return (
-                <li className="govuk-tabs__list-item govuk-tabs__list-item--full">
-                  <Link className="govuk-tabs__tab" href={navItem.path}>
+                <li
+                  className="govuk-tabs__list-item govuk-tabs__list-item--full"
+                  key={`tab-${index}`}
+                >
+                  <a
+                    className={
+                      active == `tab-${index}`
+                        ? 'govuk-tabs__tab govuk-tabs__tab--selected'
+                        : 'govuk-tabs__tab'
+                    }
+                    href={generateFullUrl(navItem.path)}
+                    id={`tab-${index}`}
+                    onClick={handleClick}
+                    aria-label={navItem.ariaDescription}
+                  >
                     {navItem.label}
-                  </Link>
+                  </a>
                 </li>
               )
             })}
