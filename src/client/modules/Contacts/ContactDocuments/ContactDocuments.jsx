0eb4d769d54553284a03bc37ff83b7283ca66eee
Christopher Sunkel <christopher.sunkel@digital.trade.gov.uk>
2023-02-08 11:52:22 +0000

    Fixup! Address review feedback for documents

--- src/client/modules/Contacts/ContactDocuments/ContactDocuments.jsx
+++ src/client/modules/Contacts/ContactDocuments/ContactDocuments.jsx
@@ -4,19 +4,17 @@ import ContactResource from '../../../components/Resource/Contact'
 import DocumentsSection from '../../../components/DocumentsSection'
 import ContactLayout from '../../../components/Layout/ContactLayout'
 
-const ContactDocuments = ({ contactId, archivedDocumentPath, permissions }) => {
-  return (
-    <ContactResource id={contactId}>
-      {(contact) => (
-        <ContactLayout contact={contact} permissions={permissions}>
-          <DocumentsSection
-            fileBrowserRoot={archivedDocumentPath}
-            documentPath={contact.archivedDocumentsUrlPath}
-          />
-        </ContactLayout>
-      )}
-    </ContactResource>
-  )
-}
+const ContactDocuments = ({ contactId, archivedDocumentPath, permissions }) => (
+  <ContactResource id={contactId}>
+    {(contact) => (
+      <ContactLayout contact={contact} permissions={permissions}>
+        <DocumentsSection
+          fileBrowserRoot={archivedDocumentPath}
+          documentPath={contact.archivedDocumentsUrlPath}
+        />
+      </ContactLayout>
+    )}
+  </ContactResource>
+)
 
 export default ContactDocuments
