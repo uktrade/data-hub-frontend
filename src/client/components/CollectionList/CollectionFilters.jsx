15ed620f975ef3d01df5c03a2194183a59d063ac
Paul Wheatcroft <paul_wheatcroft@hotmail.com>
2023-02-14 10:48:12 +0000

    H2 heading added to CollectionFilters
    Fixes Issue ID: DAC_Section_Headings_01

--- src/client/components/CollectionList/CollectionFilters.jsx
+++ src/client/components/CollectionList/CollectionFilters.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { GridCol } from 'govuk-react'
 import Task from '../../../client/components/Task'
+import { H2 } from '@govuk-react/heading'
+import { LEVEL_SIZE } from '@govuk-react/constants'
 
 const CollectionFilters = ({ children, taskProps }) => {
   return (
@@ -8,6 +10,7 @@ const CollectionFilters = ({ children, taskProps }) => {
       <Task.Status {...taskProps}>
         {() => (
           <nav aria-label="Filters" data-test="collection-filters">
+            <H2 size={LEVEL_SIZE[3]}>Filters</H2>
             {children}
           </nav>
         )}
