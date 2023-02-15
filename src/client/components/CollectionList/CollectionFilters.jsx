756bf94af6faeb048d246202bcffcab58ca73df1
Paul Wheatcroft <paul_wheatcroft@hotmail.com>
2023-02-15 09:36:09 +0000

    Created FilterHeader component
    This was to enable the customisable styling needed to make the heading sit correctly on the page.

--- src/client/components/CollectionList/CollectionFilters.jsx
+++ src/client/components/CollectionList/CollectionFilters.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 import { GridCol } from 'govuk-react'
 import Task from '../../../client/components/Task'
 import { H2 } from '@govuk-react/heading'
-import { LEVEL_SIZE } from '@govuk-react/constants'
+import { FONT_WEIGHTS, HEADING_SIZES } from '@govuk-react/constants'
+import styled from 'styled-components'
+
+const FilterHeader = styled(H2)({
+  marginTop: 0,
+  fontWeight: FONT_WEIGHTS.bold,
+  fontSize: HEADING_SIZES.MEDIUM,
+  marginBottom: 0,
+})
 
 const CollectionFilters = ({ children, taskProps }) => {
   return (
@@ -10,7 +18,7 @@ const CollectionFilters = ({ children, taskProps }) => {
       <Task.Status {...taskProps}>
         {() => (
           <nav aria-label="Filters" data-test="collection-filters">
-            <H2 size={LEVEL_SIZE[3]}>Filters</H2>
+            <FilterHeader>Filters</FilterHeader>
             {children}
           </nav>
         )}
