0d3f1276d84b4f46a1cc0b49a37ddd53a20d15ed
Derek Baker <hello@derekbaker.dev>
2023-02-20 12:45:21 +0000

    WIP: Building up summary card for overview page

--- src/apps/companies/apps/company-overview/client/CompanyOverview.jsx
+++ src/apps/companies/apps/company-overview/client/CompanyOverview.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { SummaryTable } from '../../../../../client/components'
 
-const CompanyOverview = () => {
+const CompanyOverview = (props) => {
+  const { company } = props
   return (
     <>
-      <p>Hello World</p>
+      <SummaryTable
+        caption="Business Details"
+        data-test="documentsDetailsContainer"
+      >
+        <SummaryTable.Row heading="Business Details">
+          {company.company_number}
+        </SummaryTable.Row>
+
+        <SummaryTable.Row>Hello</SummaryTable.Row>
+        <SummaryTable.Row>Hello</SummaryTable.Row>
+        <SummaryTable.Row>Hello</SummaryTable.Row>
+        <SummaryTable.Row>Hello</SummaryTable.Row>
+        <SummaryTable.Row>Hello</SummaryTable.Row>
+        <SummaryTable.Row>Hello</SummaryTable.Row>
+      </SummaryTable>
     </>
   )
 }
