4850409d4e37b4443632ae797aa8af5be8c26c01
Marijn Kampf <marijn.kampf@digital.trade.gov.uk>
2023-02-21 09:32:50 +0000

    Show recent activity with filters

--- src/apps/companies/apps/company-overview/overview-table-cards/RecentActivityCard.jsx
+++ src/apps/companies/apps/company-overview/overview-table-cards/RecentActivityCard.jsx
@@ -3,9 +3,15 @@ import { Link, Table } from 'govuk-react'
 import PropTypes from 'prop-types'
 import { SummaryTable } from '../../../../../client/components'
 // import { fetchAllActivityFeedEvents } from '../../activity-feed/controllers'
+import { ActivityFeedApp } from '../../../../../client/components'
+import urls from '../../../../../lib/urls'
+
+import CompanyActivityFeed from '../../activity-feed/client/CompanyActivityFeed'
+
+const { FILTER_ITEMS, FILTER_KEYS } = require('../../activity-feed/constants')
 
 const BusinessDetailsCard = (props) => {
-  const { queryString } = props
+  const { company, queryString } = props
 
   return (
     <>
@@ -20,8 +26,18 @@ const BusinessDetailsCard = (props) => {
         }
         data-test="recentInteractionsContainer"
       >
-        <SummaryTable.Row heading="Acitivites">TO DO</SummaryTable.Row>
-
+        <Table.Cell colspan={2}>
+          <ActivityFeedApp
+            actions={!company.archived && CompanyActivityFeed.actions}
+            activityTypeFilter={FILTER_KEYS.dataHubActivity}
+            activityTypeFilters={FILTER_ITEMS}
+            isGlobalUltimate={company.is_global_ultimate}
+            dnbHierarchyCount={3}
+            // dnbHierarchyCount={dnbHierarchyCount}
+            apiEndpoint={urls.companies.activity.data(company.id)}
+            companyIsArchived={company.archived}
+          />
+        </Table.Cell>
         <Table.Row>
           <Table.Cell colSpan={2}>
             <Link href={`${queryString}/activity`}>View all activities</Link>
