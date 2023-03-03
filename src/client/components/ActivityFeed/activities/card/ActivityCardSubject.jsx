02b1c519d83f0473698bd97ead45aebc036432dd
Paul Wheatcroft <paul_wheatcroft@hotmail.com>
2023-03-03 09:33:56 +0000

    Revert ActivityCardSubject back to original code

--- src/client/components/ActivityFeed/activities/card/ActivityCardSubject.jsx
+++ src/client/components/ActivityFeed/activities/card/ActivityCardSubject.jsx
@@ -19,45 +19,19 @@ const StyledActivitySubject = styled('h3')`
   }
 `
 
-const StyledActivitySubjectSquash = styled('h3')`
-  font-size: ${FONT_SIZE.SIZE_20};
-  font-weight: ${FONT_WEIGHTS.bold};
-  line-height: ${FONT_SIZE.SIZE_24};
-  margin-bottom: 0;
-  & > a:link,
-  a:visited,
-  a:hover,
-  a:active {
-    text-decoration: none;
-    color: ${BLUE};
-  }
-`
-
 const ActivityCardSubject = ({
   children,
-  url,
-  linkDataTest,
-  isOverview,
   dataTest = 'activity-card-subject',
 }) => {
-  return isOverview ? (
+  return (
     <StyledActivitySubject data-test={dataTest}>
       {children}
     </StyledActivitySubject>
-  ) : (
-    <StyledActivitySubjectSquash data-test={dataTest}>
-      <Link dataTest={linkDataTest} href={url}>
-        {children}
-      </Link>
-    </StyledActivitySubjectSquash>
   )
 }
 
 ActivityCardSubject.propTypes = {
   children: PropTypes.node.isRequired,
-}
-ActivityCardSubject.defaultProps = {
-  isOverview: false,
 }
 
 export default ActivityCardSubject
