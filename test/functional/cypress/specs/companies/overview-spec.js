2a653c3aaeb088ed4c2943ef7a6c617c189bbe06
Marijn Kampf <marijn.kampf@digital.trade.gov.uk>
2023-03-06 14:08:47 +0000

    Fix merge issues

--- test/functional/cypress/specs/companies/overview-spec.js
+++ test/functional/cypress/specs/companies/overview-spec.js
@@ -32,18 +32,7 @@ describe('Company overview page', () => {
           urls.companies.overview.index(fixtures.company.allOverviewDetails.id)
         )
       })
-    }
-  )
-  context(
-    'when viewing the Business details card for a business that has all information added',
-    () => {
-      before(() => {
-        cy.visit(
-          urls.companies.overview.index(fixtures.company.allOverviewDetails.id)
-        )
-      })
 
-      it('the card should contain the Business details table including all keys and values for All Overview Details Inc.', () => {
       it('the card should contain the Business details table including all keys and values for All Overview Details Inc.', () => {
         cy.get('[data-test="businessDetailsContainer"]')
           .children()
@@ -55,7 +44,12 @@ describe('Company overview page', () => {
         cy.get('td').contains('01261539').parent().next().children()
         cy.get('th').contains('Trading Address')
         cy.get('td').children()
-        cy.get('li').contains('1 Way Road').parent().parent().parent().next()
+        cy.get('li')
+          .contains('Unit 10 Ockham Drive')
+          .parent()
+          .parent()
+          .parent()
+          .next()
         cy.get('th').contains('Website')
         cy.get('td')
           .contains('http://all-the-details.com')
@@ -68,15 +62,8 @@ describe('Company overview page', () => {
         cy.get('td').contains('260').parent().next()
         cy.get('th').contains('DIT Sector')
         cy.get('td').contains('Retail')
-        cy.get('th').contains('Turnover')
-        cy.get('td').contains('£1,000,000').parent().next()
-        cy.get('th').contains('Number of Employees')
-        cy.get('td').contains('260').parent().next()
-        cy.get('th').contains('DIT Sector')
-        cy.get('td').contains('Retail')
       })
 
-      it('the card should link to the business overview page', () => {
       it('the card should link to the business overview page', () => {
         cy.get('[data-test="business-page-link"]')
           .contains('View full business details')
@@ -86,37 +73,6 @@ describe('Company overview page', () => {
           '/companies/ba8fae21-2895-47cf-90ba-9273c94dab88/business-details'
         )
         cy.go('back')
-      })
-    }
-  )
-  context(
-    'when viewing the Business details card for a business that has no information added',
-    () => {
-      before(() => {
-        cy.visit(
-          urls.companies.overview.index(fixtures.company.noOverviewDetails.id)
-        )
-      })
-
-      it('the card should contain the Business details table with all values set to "Not set"', () => {
-        cy.get('[data-test="businessDetailsContainer"]')
-          .children()
-          .first()
-          .contains('Business details')
-          .next()
-          .children()
-        cy.get('th').contains('Companies House')
-        cy.get('td').contains('Not set').parent().next().children()
-        cy.get('th').contains('Trading Address')
-        cy.get('td').contains('Not set').parent().next()
-        cy.get('th').contains('Website')
-        cy.get('td').contains('Not set').parent().next()
-        cy.get('th').contains('Turnover')
-        cy.get('td').contains('Not set').parent().next()
-        cy.get('th').contains('Number of Employees')
-        cy.get('td').contains('Not set').parent().next()
-        cy.get('th').contains('DIT Sector')
-        cy.get('td').contains('Not set')
       })
     }
   )
