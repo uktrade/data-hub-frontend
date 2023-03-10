9cca1fac47223bdd4e321a84ba316c4e06a9b5ab
santosh dasari <74972011+santoshdasa12345@users.noreply.github.com>
2023-03-10 09:42:14 +0000

    Add default layout

--- src/client/modules/ExportPipeline/ExportDetails/index.jsx
+++ src/client/modules/ExportPipeline/ExportDetails/index.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { EXPORT_DETAIL_LOADED } from '../../../actions'
+import { DefaultLayout } from '../../../components'
+import Task from '../../../components/Task'
+import { ID, TASK_GET_EXPORT_DETAIL } from './state'
+
+const StyledSummaryTable = styled(SummaryTable)({
+  marginTop: 0,
+})
 
 const ExportDetailsForm = () => {
   const { exportId } = useParams()
-  return <span>Export Details Form</span>
+  const companyId = 'fd8220b1-c59f-413e-8f0b-61d692c15c3a'
+  return (
+    <DefaultLayout
+      heading="Export Details"
+      pageTitle="ExportDetailsForm"
+      useReactRouter={true}
+    >
+      <Task.Status
+        name={TASK_GET_EXPORT_DETAIL}
+        id={ID}
+        progressMessage="loading export details"
+        startOnRender={{
+          payload: exportId,
+          onSuccessDispatch: EXPORT_DETAIL_LOADED,
+        }}
+      >
+        {() => {
+          return <></>
+        }}
+      </Task.Status>
+    </DefaultLayout>
+  )
+  // <span>Export Details Form</span>
 }
 
 export default ExportDetailsForm
