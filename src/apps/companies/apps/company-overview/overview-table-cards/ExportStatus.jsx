7a6cec376a38f62ca27f10e262f07adb3c76bffc
Bau Nguyen <bauvannguyen123@gmail.com>
2023-03-13 16:10:11 +0000

    Update ExportStatus.jsx

--- src/apps/companies/apps/company-overview/overview-table-cards/ExportStatus.jsx
+++ src/apps/companies/apps/company-overview/overview-table-cards/ExportStatus.jsx
@@ -44,7 +44,17 @@ const GreenLabel = styled('label')`
 
 export const SUBSEGMENT = {
   sustain_nurture_and_grow: 'Sustain: Nurture & grow',
-  sustain_develop_export_capability: '',
+  sustain_develop_export_capability: 'Sustain: develop export capability',
+  sustain_communicate_benefits: 'Sustain: communicate benefits',
+  sustain_increase_competitiveness: 'Sustain: increase competitiveness',
+  reassure_nurture_and_grow: 'Reassure: nurture & grow',
+  reassure_develop_export_capability: 'Reassure: develop export capability',
+  reassure_leave_be: 'Reassure: leave be',
+  reassure_change_the_game: 'Reassure: change the game',
+  promote_develop_export_capability: 'Promote: develop export capability',
+  promote_communicate_benefits: 'Promote: communicate benefits',
+  promote_change_the_game: 'Promote: change the game',
+  challenge: 'Challenge',
 }
 
 const ExportStatus = ({ company, queryString }) => {
