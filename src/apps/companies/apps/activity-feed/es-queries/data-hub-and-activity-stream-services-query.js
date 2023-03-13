27a53114ed87b54182505f9800e779f0a651e4f7
Simon Baker <simon.baker@madetech.com>
2023-03-13 13:00:36 +0000

    Extract sort criteria into its own constant

--- src/apps/companies/apps/activity-feed/es-queries/data-hub-and-activity-stream-services-query.js
+++ src/apps/companies/apps/activity-feed/es-queries/data-hub-and-activity-stream-services-query.js
@@ -1,3 +1,19 @@
+const sortCriteria = {
+  _script: {
+    type: 'number',
+    script: {
+      lang: 'painless',
+      // Using the logic below to perform sorting, as we want to sort on published time, apart from events, which
+      // have a startTime set. The sort order needs to reflect their startTime (i.e. when the event is happening)
+      // in the list of activities, as opposed to when that activity was created.
+      source:
+        "doc.containsKey('object.startTime') ? doc['object.startTime'].value.toInstant().toEpochMilli() " +
+        ": doc['published'].value.toInstant().toEpochMilli()",
+    },
+    order: 'desc',
+  },
+}
+
 const dataHubAndActivityStreamServicesQuery = ({
   from,
   size,
@@ -69,21 +85,7 @@ const dataHubAndActivityStreamServicesQuery = ({
   const dsl = {
     from,
     size,
-    sort: {
-      _script: {
-        type: 'number',
-        script: {
-          lang: 'painless',
-          // Using the logic below to perform sorting, as we want to sort on published time, apart from events, which
-          // have a startTime set. The sort order needs to reflect their startTime (i.e. when the event is happening)
-          // in the list of activities, as opposed to when that activity was created.
-          source:
-            "doc.containsKey('object.startTime') ? doc['object.startTime'].value.toInstant().toEpochMilli() " +
-            ": doc['published'].value.toInstant().toEpochMilli()",
-        },
-        order: 'desc',
-      },
-    },
+    sort: sortCriteria,
     query: {
       bool: {
         filter: {
