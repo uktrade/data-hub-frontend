057ce5c62ec6c58cb0d7a32c09c04e4955289a4f
Simon Baker <simon.baker@madetech.com>
2023-03-13 13:21:41 +0000

    Extract sort criteria in external and my activity queries

--- src/apps/companies/apps/activity-feed/es-queries/external-activity-query.js
+++ src/apps/companies/apps/activity-feed/es-queries/external-activity-query.js
@@ -1,3 +1,19 @@
+const sortCriteria = {
+  _script: {
+    type: 'number',
+    script: {
+      lang: 'painless',
+      // Using the logic below to perform sorting, as we want to sort on published time, apart from events, which
+      // have a startTime set. The sort order needs to reflect their startTime (i.e. when the event is happening)
+      // in the list of activities, as opposed to when that activity was created.
+      source:
+        "doc.containsKey('object.startTime') ? doc['object.startTime'].value.toInstant().toEpochMilli() " +
+        ": doc['published'].value.toInstant().toEpochMilli()",
+    },
+    order: 'desc',
+  },
+}
+
 const externalActivityQuery = ({
   from,
   size,
@@ -103,21 +119,7 @@ const externalActivityQuery = ({
   const dsl = {
     from,
     size,
-    sort: {
-      _script: {
-        type: 'number',
-        script: {
-          lang: 'painless',
-          // Using the logic below to perform sorting, as we want to sort on published time, apart from events, which
-          // have a startTime set. The sort order needs to reflect their startTime (i.e. when the event is happening)
-          // in the list of activities, as opposed to when that activity was created.
-          source:
-            "doc.containsKey('object.startTime') ? doc['object.startTime'].value.toInstant().toEpochMilli() " +
-            ": doc['published'].value.toInstant().toEpochMilli()",
-        },
-        order: 'desc',
-      },
-    },
+    sort: sortCriteria,
     query: {
       bool: {
         filter: {
--- src/apps/companies/apps/activity-feed/es-queries/my-activity-query.js
+++ src/apps/companies/apps/activity-feed/es-queries/my-activity-query.js
@@ -1,22 +1,24 @@
+const sortCriteria = {
+  _script: {
+    type: 'number',
+    script: {
+      lang: 'painless',
+      // Using the logic below to perform sorting, as we want to sort on published time, apart from events, which
+      // have a startTime set. The sort order needs to reflect their startTime (i.e. when the event is happening)
+      // in the list of activities, as opposed to when that activity was created.
+      source:
+        "doc.containsKey('object.startTime') ? doc['object.startTime'].value.toInstant().toEpochMilli() " +
+        ": doc['published'].value.toInstant().toEpochMilli()",
+    },
+    order: 'desc',
+  },
+}
+
 const myActivityQuery = ({ from, size, types, user, companyIds }) => {
   return {
     from,
     size,
-    sort: {
-      _script: {
-        type: 'number',
-        script: {
-          lang: 'painless',
-          // Using the logic below to perform sorting, as we want to sort on published time, apart from events, which
-          // have a startTime set. The sort order needs to reflect their startTime (i.e. when the event is happening)
-          // in the list of activities, as opposed to when that activity was created.
-          source:
-            "doc.containsKey('object.startTime') ? doc['object.startTime'].value.toInstant().toEpochMilli() " +
-            ": doc['published'].value.toInstant().toEpochMilli()",
-        },
-        order: 'desc',
-      },
-    },
+    sort: sortCriteria,
     query: {
       bool: {
         must: [
