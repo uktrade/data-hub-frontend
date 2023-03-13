3056dc6dd5eafbbc856403971aff0876524a5cb5
Simon Baker <simon.baker@madetech.com>
2023-03-13 15:15:11 +0000

    Fix remaining unit test

--- src/apps/companies/apps/activity-feed/__test__/controllers.test.js
+++ src/apps/companies/apps/activity-feed/__test__/controllers.test.js
@@ -285,13 +285,21 @@ describe('Activity feed controllers', () => {
         const expectedEsQuery = {
           from: 0,
           size: 20,
-          sort: [
-            {
-              'object.startTime': {
-                order: 'desc',
+          sort: {
+            _script: {
+              type: 'number',
+              script: {
+                lang: 'painless',
+                // Using the logic below to perform sorting, as we want to sort on published time, apart from events, which
+                // have a startTime set. The sort order needs to reflect their startTime (i.e. when the event is happening)
+                // in the list of activities, as opposed to when that activity was created.
+                source:
+                  "doc.containsKey('object.startTime') ? doc['object.startTime'].value.toInstant().toEpochMilli() " +
+                  ": doc['published'].value.toInstant().toEpochMilli()",
               },
+              order: 'desc',
             },
-          ],
+          },
           query: {
             bool: {
               must: [
