f482581c1712e5e5d46aa4db71075835f205d2aa
santosh dasari <74972011+santoshdasa12345@users.noreply.github.com>
2023-03-13 12:36:37 +0000

    Add transformers and Summary table
    Fixed some error issue

--- src/client/modules/ExportPipeline/ExportDetails/tasks.js
+++ src/client/modules/ExportPipeline/ExportDetails/tasks.js
@@ -1,4 +1,7 @@
 import { apiProxyAxios } from '../../../components/Task/utils'
+import { transformExportPipelineDetails } from './transformers'
 
 export const getExportDetails = (exportId) =>
-  apiProxyAxios.get(`/v4/export/${exportId}`).then(({ data }) => data)
+  apiProxyAxios
+    .get(`/v4/export/${exportId}`)
+    .then(({ data }) => transformExportPipelineDetails(data))
