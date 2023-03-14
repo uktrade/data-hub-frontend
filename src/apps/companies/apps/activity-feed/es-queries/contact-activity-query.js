e36c3ea18392508a70e761b83b0a07409c57c536
Edmond De Los Reyes <edmond.delosreyes@digital.trade.gov.uk>
2023-03-14 12:11:41 +0000

    Implements ESS query sort criteria to performed logical sorting.

--- src/apps/companies/apps/activity-feed/es-queries/contact-activity-query.js
+++ src/apps/companies/apps/activity-feed/es-queries/contact-activity-query.js
@@ -1,3 +1,21 @@
+const sortCriteria = (sortOrder) => {
+  return {
+    _script: {
+      type: 'number',
+      script: {
+        lang: 'painless',
+        // Using the logic below to perform sorting, as we want to sort on published time, apart from events, which
+        // have a startTime set. The sort order needs to reflect their startTime (i.e. when the event is happening)
+        // in the list of activities, as opposed to when that activity was created.
+        source:
+          "doc.containsKey('object.startTime') ? doc['object.startTime'].value.toInstant().toEpochMilli() " +
+          ": doc['published'].value.toInstant().toEpochMilli()",
+      },
+      order: `${sortOrder}`,
+    },
+  }
+}
+
 const contactActivityQuery = (
   from,
   size,
@@ -9,6 +27,7 @@ const contactActivityQuery = (
   return {
     from,
     size,
+    sort: sortCriteria(sortOrder),
     query: {
       bool: {
         must: [
@@ -86,14 +105,6 @@ const contactActivityQuery = (
         ],
       },
     },
-    sort: [
-      {
-        published: {
-          order: sortOrder,
-          unmapped_type: 'long',
-        },
-      },
-    ],
   }
 }
 
