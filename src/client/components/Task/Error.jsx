c4f50fe14f4a0de8ffb339fac85f53ed8461ebb8
Chris Hopkins <chris.hopkins@madetech.com>
2023-03-16 09:24:27 +0000

    added conditional rendering to the error component

--- src/client/components/Task/Error.jsx
+++ src/client/components/Task/Error.jsx
@@ -18,6 +18,7 @@ import { spacing } from '@govuk-react/lib'
 
 import FormActions from '../Form/elements/FormActions'
 import SecondaryButton from '../SecondaryButton'
+import { isString } from 'lodash'
 
 const StyledRoot = styled.div(
   {
@@ -44,7 +45,7 @@ const StyledSecondaryButton = styled(SecondaryButton)({
 const Err = ({ errorMessage, retry, dismiss, noun, dismissable = true }) => (
   <StyledRoot data-test="error-dialog">
     <H2 size="MEDIUM">Could not load {noun}</H2>
-    <p>Error: {errorMessage}</p>
+    {isString(errorMessage) && <p>Error: {errorMessage}</p>}
     <FormActions>
       <StyledSecondaryButton onClick={retry}>Retry</StyledSecondaryButton>
       {dismissable && (
