cfa9e08c56555f21a2354fbc7b18f6574907ae73
Paul Gain <paul@javexconsulting.com>
2023-03-23 08:49:45 +0000

    Add export pipeline component

--- src/client/modules/ExportPipeline/ExportList/index.jsx
+++ src/client/modules/ExportPipeline/ExportList/index.jsx
@@ -1,5 +1,24 @@
 import React from 'react'
 
-const ExportList = () => <>...</>
+import Task from '../../../components/Task'
+import { EXPORT__PIPELINE_LIST_LOADED } from '../../../actions'
+import { ID, TASK_GET_EXPORT_PIPELINE_LIST } from './state'
+
+const ExportList = (data) => (
+  <Task.Status
+    name={TASK_GET_EXPORT_PIPELINE_LIST}
+    id={ID}
+    progressMessage="loading export pipeline list"
+    startOnRender={{
+      onSuccessDispatch: EXPORT__PIPELINE_LIST_LOADED,
+    }}
+  >
+    {() => (
+      <pre>
+        <code>{JSON.stringify(data.results, null, 2)}</code>
+      </pre>
+    )}
+  </Task.Status>
+)
 
 export default ExportList
--- src/client/modules/ExportPipeline/ExportList/state.js
+++ src/client/modules/ExportPipeline/ExportList/state.js
@@ -1,1 +1,2 @@
 export const TASK_GET_EXPORT_PIPELINE_LIST = 'TASK_GET_EXPORT_PIPELINE_LIST'
+export const ID = 'exportPipelineList'
