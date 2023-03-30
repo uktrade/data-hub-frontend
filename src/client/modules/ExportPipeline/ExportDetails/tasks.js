8a204d9c38e241ccd4d977b8b6f1434e942d9471
santosh dasari <74972011+santoshdasa12345@users.noreply.github.com>
2023-03-30 11:55:51 +0100

    export years and export item
    remove commented code
    
    remove
    
    add json

--- src/client/modules/ExportPipeline/ExportDetails/tasks.js
+++ src/client/modules/ExportPipeline/ExportDetails/tasks.js
@@ -2,4 +2,6 @@ import { apiProxyAxios } from '../../../components/Task/utils'
 import { transformExportPipelineDetails } from './transformers'
 
 export const getExportDetails = (exportId) =>
-  apiProxyAxios.get(`/v4/export/${exportId}`).then(({ data }) => data)
+  apiProxyAxios
+    .get(`/v4/export/${exportId}`)
+    .then(({ data }) => transformExportPipelineDetails(data))
