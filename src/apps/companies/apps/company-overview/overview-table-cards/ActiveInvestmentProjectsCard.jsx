351778f191fd44b64f16201902bc8290a3865832
Paul Wheatcroft <paul_wheatcroft@hotmail.com>
2023-04-03 09:16:37 +0100

    Returned list of investments sorted by estimated to land date
    Converted received date string to date format

--- src/apps/companies/apps/company-overview/overview-table-cards/ActiveInvestmentProjectsCard.jsx
+++ src/apps/companies/apps/company-overview/overview-table-cards/ActiveInvestmentProjectsCard.jsx
@@ -3,11 +3,8 @@ import { Link, Table } from 'govuk-react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { SummaryTable } from '../../../../../client/components'
-import { TASK_GET_PROJECTS_LIST, companyProjectsState2props } from './state'
-import { OVERVIEW__COMPANY_INVESTMENT_WON_COUNT } from '../../../../../client/actions'
-import Task from '../../../../../client/components/Task'
+import { companyProjectsState2props } from './state'
 import { connect } from 'react-redux'
-import { COMPANY_PROJECTS_LIST_ID } from '../../../../investments/client/projects/state'
 
 const StyledSummaryTable = styled(SummaryTable)`
   margin: 0;
@@ -23,6 +20,55 @@ const StyledLastTableCell = styled(Table.Cell)`
   padding-bottom: 0;
 `
 
+const ActiveInvestmentList = ({ props }) => {
+  const allInvestmentResults = props.resultList
+  for (const i of allInvestmentResults) {
+    let newDate = new Date(i.estimated_land_date)
+    i.estimated_land_date = newDate
+  }
+  allInvestmentResults.sort(
+    (objA, objB) =>
+      Number(objA.estimated_land_date) - Number(objB.estimated_land_date)
+  )
+  return allInvestmentResults.map((investment) => {
+    if (investment.stage.name === 'Active') {
+      return (
+        <>
+          <StyledTableRow>
+            <StyledLastTableCell colSpan={2}>
+              <Link href={`#`} data-test="investment-page-link">
+                {investment.name}
+              </Link>
+            </StyledLastTableCell>
+          </StyledTableRow>
+          <StyledTableRow>
+            <StyledLastTableCell colSpan={1}>
+              Estimated land date
+            </StyledLastTableCell>
+            <StyledLastTableCell colSpan={1}>
+              {new Date(investment.estimated_land_date).toString()}
+            </StyledLastTableCell>
+          </StyledTableRow>
+          <StyledTableRow>
+            <StyledLastTableCell colSpan={1}>
+              Last interaction date
+            </StyledLastTableCell>
+            <StyledLastTableCell colSpan={1}>Need to sort</StyledLastTableCell>
+          </StyledTableRow>
+        </>
+      )
+      // console.log(
+      //   '//////////',
+      //   investment.name,
+      //   `'Estimated land date' ${investment.estimated_land_date}`,
+      //   `'Last interaction date' ${investment.modified_on}`,
+      //   investment,
+      //   index
+      // )
+    }
+  })
+}
+
 const ActiveInvestmentProjectsCard = ({
   queryString,
   companyId,
@@ -30,37 +76,29 @@ const ActiveInvestmentProjectsCard = ({
   ...props
 }) => {
   return (
-    <>
-      <Task.Status
-        name={TASK_GET_PROJECTS_LIST}
-        id={COMPANY_PROJECTS_LIST_ID}
-        progressMessage="Loading projects"
-        startOnRender={{
-          payload: { companyId },
-          onSuccessDispatch: OVERVIEW__COMPANY_INVESTMENT_WON_COUNT,
-        }}
-      >
-        {() => (
-          <StyledSummaryTable
-            caption="Active investment projects"
-            data-test="investmentsStatusContainer"
-          >
-            <StyledTableRow>content goes here</StyledTableRow>
-            <StyledTableRow>
-              <StyledLastTableCell colSpan={2}>
-                <Link
-                  href={`${queryString}/investments/projects`}
-                  data-test="investment-page-link"
-                >
-                  View {props.stageList?.active ? props.stageList.active : '0'}{' '}
-                  more active investments
-                </Link>
-              </StyledLastTableCell>
-            </StyledTableRow>
-          </StyledSummaryTable>
+    <StyledSummaryTable
+      caption="Active investment projects"
+      data-test="investmentsStatusContainer"
+    >
+      <StyledTableRow>
+        {props.stageList?.active ? (
+          <ActiveInvestmentList props={props} />
+        ) : (
+          'No active investments'
         )}
-      </Task.Status>
-    </>
+      </StyledTableRow>
+      <StyledTableRow>
+        <StyledLastTableCell colSpan={2}>
+          <Link
+            href={`${queryString}/investments/projects`}
+            data-test="investment-page-link"
+          >
+            View {props.stageList?.active ? props.stageList.active : '0'} more
+            active investments
+          </Link>
+        </StyledLastTableCell>
+      </StyledTableRow>
+    </StyledSummaryTable>
   )
 }
 
