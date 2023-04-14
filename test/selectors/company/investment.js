c8b1345f54985f09df9eff6389b9554346ac56b5
Christopher Sunkel <christopher.sunkel@digital.trade.gov.uk>
2023-04-14 11:50:58 +0100

    Remove unused Growth Capital Profile code

--- test/selectors/company/investment.js
+++ test/selectors/company/investment.js
@@ -2,7 +2,6 @@ module.exports = {
   tabs: {
     investmentProjects: '[data-test="bodyMainContent"] li:nth-child(1)',
     largeCapitalProfile: '[data-test="bodyMainContent"] li:nth-child(2)',
-    growthCapitalProfile: '[data-test="bodyMainContent"] li:nth-child(3)',
   },
   subHeading: '[data-test="bodyMainContent"] h2',
 }
