e754feb2d68308e26ec8b4ba208a06e33cfafc9f
Chris Hopkins <102232401+chopkinsmade@users.noreply.github.com>
2023-04-20 14:58:12 +0100

    default the list export api call to exclude archived export items (#5533)

--- src/client/modules/ExportPipeline/ExportList/task.js
+++ src/client/modules/ExportPipeline/ExportList/task.js
@@ -3,12 +3,17 @@ import qs from 'qs'
 import { getPageOffset } from '../../../../client/utils/pagination.js'
 import { apiProxyAxios } from '../../../components/Task/utils'
 
-export const getExportPipelineList = ({ limit = 10, page = 1 }) => {
+export const getExportPipelineList = ({
+  limit = 10,
+  page = 1,
+  archived = false,
+}) => {
   const offset = getPageOffset({ limit, page })
   const queryParams = qs.stringify({
     limit,
     page,
     offset,
+    archived,
   })
   return apiProxyAxios.get(`/v4/export?${queryParams}`).then(({ data }) => data)
 }
