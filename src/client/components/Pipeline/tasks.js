1ed9886d0c47ca8f2e91ad26002a70804ec2b03b
Paul Gain <paul@javexconsulting.com>
2023-04-20 16:38:17 +0100

    Remove legacy export pipeline code

--- src/client/components/Pipeline/tasks.js
+++
@@ -1,7 +0,0 @@
-import pipelineApi from '../../../apps/my-pipeline/client/api'
-
-export function getPipelineList(filters) {
-  return pipelineApi.list(filters).then(({ data }) => ({
-    [filters.status]: data.results,
-  }))
-}
