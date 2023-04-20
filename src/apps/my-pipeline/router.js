c308a3d6bbc97e86ade3e465b16507bc88ee7e32
Paul Gain <paul@javexconsulting.com>
2023-04-20 16:39:06 +0100

    Add two redirects

---
+++ src/apps/my-pipeline/router.js
@@ -0,0 +1,19 @@
+const { Router } = require('express')
+
+const myPipelineRouter = Router()
+const companyPipelineRouter = Router()
+
+// Redirect /my-pipeline to /export
+myPipelineRouter.get('/', (req, res) => res.redirect('/export'))
+
+// Redirect
+// From: /companies/<company-id>/my-pipeline
+// To:   /export/create?companyId=<company-id>
+companyPipelineRouter.get('/:companyId/my-pipeline', (req, res) =>
+  res.redirect(`/export/create?companyId=${req.params.companyId}`)
+)
+
+module.exports = {
+  myPipelineRouter,
+  companyPipelineRouter,
+}
