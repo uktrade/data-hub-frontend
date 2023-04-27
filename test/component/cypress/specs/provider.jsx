7444e39c87df1f4dfd2f94f5cc39759fe58a20b3
Chris Hopkins <102232401+chopkinsmade@users.noreply.github.com>
2023-04-27 14:54:00 +0100

    Add validation to block 2 digit years (#5578)
    * Added validation for 2 digit years
    
    * fix for functional test

--- test/component/cypress/specs/provider.jsx
+++ test/component/cypress/specs/provider.jsx
@@ -6,6 +6,7 @@ import createSagaMiddleware from 'redux-saga'
 
 import rootSaga from '../../../../src/client/root-saga'
 import tasks from '../../../../src/client/components/Task/reducer'
+import Form from '../../../../src/client/components/Form'
 import Typeahead from '../../../../src/client/components/Typeahead/Typeahead'
 import FieldAddAnother from '../../../../src/client/components/Form/elements/FieldAddAnother/FieldAddAnother'
 import Resource from '../../../../src/client/components/Resource/Resource'
@@ -18,6 +19,7 @@ const reducer = (state, action) =>
     ...Resource.reducerSpread,
     ...Typeahead.reducerSpread,
     ...FieldAddAnother.reducerSpread,
+    ...Form.reducerSpread,
   })(action.type === 'RESET' ? undefined : state, action)
 
 export const store = legacy_createStore(
