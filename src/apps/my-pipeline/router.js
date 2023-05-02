89032da6cfb56cef648526ef988da90c781cf62a
Chris Hopkins <102232401+chopkinsmade@users.noreply.github.com>
2023-05-02 10:38:24 +0100

    Added export redirects from pipeline tabs (#5600)

--- src/apps/my-pipeline/router.js
+++ src/apps/my-pipeline/router.js
@@ -6,6 +6,9 @@ const companyPipelineRouter = Router()
 // Redirect /my-pipeline to /export
 myPipelineRouter.get('/', (req, res) => res.redirect('/export'))
 
+// Redirect /my-pipeline/:status to /export
+myPipelineRouter.get('/:status', (req, res) => res.redirect('/export'))
+
 // Redirect
 // From: /companies/<company-id>/my-pipeline
 // To:   /export/create?companyId=<company-id>
