13095b2086804993cd4591466d255bf62a199a5c
Paul Wheatcroft <paul_wheatcroft@hotmail.com>
2023-05-03 15:47:12 +0100

    restored selector file as it's used by tab-history-spec

---
+++ test/selectors/company-lists/add-remove.js
@@ -0,0 +1,5 @@
+module.exports = {
+  createButton: '#add-remove-list-form [data-test="create-list-button"]',
+  saveButton: '#add-remove-list-form [data-test="submit-button"]',
+  cancelLink: '#add-remove-list-form [data-test="cancel-button"]',
+}
