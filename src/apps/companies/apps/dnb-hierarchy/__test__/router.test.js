21e133530c4085ff576cf3ef5eb993a7232011fe
Chris Hopkins <chris.hopkins@madetech.com>
2023-05-24 15:37:01 +0100

    unit test fixes

--- src/apps/companies/apps/dnb-hierarchy/__test__/router.test.js
+++ src/apps/companies/apps/dnb-hierarchy/__test__/router.test.js
@@ -3,9 +3,6 @@ const router = require('../router')
 describe('D&B hierarchy routes', () => {
   it('should define all routes', () => {
     const paths = router.stack.filter((r) => r.route).map((r) => r.route.path)
-    expect(paths).to.deep.equal([
-      '/:companyId/dnb-hierarchy',
-      '/:companyId/dnb-hierarchy/data',
-    ])
+    expect(paths).to.deep.equal(['/:companyId/dnb-hierarchy/data'])
   })
 })
