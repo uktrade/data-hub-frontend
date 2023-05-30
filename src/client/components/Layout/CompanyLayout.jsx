5905601d3ac8776268503f4c563441c309fd3927
Christopher Sunkel <christopher.sunkel@digital.trade.gov.uk>
2023-05-30 12:27:58 +0100

    Add React template for company pages

---
+++ src/client/components/Layout/CompanyLayout.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import GridCol from '@govuk-react/grid-col'
+import GridRow from '@govuk-react/grid-row'
+
+import { CompanyLocalHeader, CompanyTabbedLocalNavigation, Main } from '..'
+
+const CompanyLayout = ({
+  company,
+  flashMessages,
+  breadcrumbs,
+  dnbRelatedCompaniesCount,
+  children,
+  returnUrl,
+  localNavItems,
+}) => {
+  return (
+    <>
+      <CompanyLocalHeader
+        breadcrumbs={breadcrumbs}
+        flashMessages={flashMessages}
+        company={company}
+        dnbRelatedCompaniesCount={dnbRelatedCompaniesCount}
+        returnUrl={returnUrl}
+      />
+      <Main>
+        <CompanyTabbedLocalNavigation
+          localNavItems={localNavItems}
+          company={company}
+        />
+        <GridRow>
+          <GridCol>{children}</GridCol>
+        </GridRow>
+      </Main>
+    </>
+  )
+}
+
+CompanyLayout.propTypes = {
+  company: PropTypes.object.isRequired,
+  permissions: PropTypes.array.isRequired,
+  children: PropTypes.element.isRequired,
+}
+
+export default CompanyLayout
