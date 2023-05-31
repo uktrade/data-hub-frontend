5a3eb6b7d8b2ee673687412e28ba894796cc7769
Christopher Sunkel <christopher.sunkel@digital.trade.gov.uk>
2023-05-31 11:12:05 +0100

    Refactor SectionSector to use resource data

--- src/apps/companies/apps/business-details/client/SectionSector.jsx
+++ src/apps/companies/apps/business-details/client/SectionSector.jsx
@@ -4,7 +4,7 @@ import Link from '@govuk-react/link'
 
 import { SummaryTable } from '../../../../../client/components/'
 
-const SectionSector = ({ businessDetails, isArchived, urls }) => (
+const SectionSector = ({ company, isArchived, urls }) => (
   <SummaryTable
     caption="DBT sector"
     data-test="sectorDetailsContainer"
@@ -12,12 +12,14 @@ const SectionSector = ({ businessDetails, isArchived, urls }) => (
       !isArchived && <Link href={`${urls.companyEdit}#sector`}>Edit</Link>
     }
   >
-    <SummaryTable.Row>{businessDetails.sector || 'Not set'}</SummaryTable.Row>
+    <SummaryTable.Row>
+      {company.sector ? company.sector.name : 'Not set'}
+    </SummaryTable.Row>
   </SummaryTable>
 )
 
 SectionSector.propTypes = {
-  businessDetails: PropTypes.object.isRequired,
+  company: PropTypes.object.isRequired,
   isArchived: PropTypes.bool.isRequired,
   urls: PropTypes.object.isRequired,
 }
