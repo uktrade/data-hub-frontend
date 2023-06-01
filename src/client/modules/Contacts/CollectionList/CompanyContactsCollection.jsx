c401053394f22b036c2f5b96530f1a5b5a12433e
Christopher Sunkel <christopher.sunkel@digital.trade.gov.uk>
2023-06-01 14:42:35 +0100

    Refactor company contact page to use new layout

--- src/client/modules/Contacts/CollectionList/CompanyContactsCollection.jsx
+++ src/client/modules/Contacts/CollectionList/CompanyContactsCollection.jsx
@@ -4,6 +4,8 @@ import { Link, Details } from 'govuk-react'
 
 import { CONTACTS__LOADED } from '../../../actions'
 import { FilteredCollectionList } from '../../../components'
+import { CompanyResource } from '../../../components/Resource'
+import CompanyLayout from '../../../components/Layout/CompanyLayout'
 
 import {
   TASK_GET_CONTACTS_LIST,
@@ -12,10 +14,13 @@ import {
 } from './state'
 
 const CompanyContactsCollection = ({
-  company,
+  companyId,
   payload,
   optionMetadata,
   selectedFilters,
+  dnbRelatedCompaniesCount,
+  returnUrl,
+  localNavItems,
   ...props
 }) => {
   const collectionListTask = {
@@ -25,42 +30,52 @@ const CompanyContactsCollection = ({
     startOnRender: {
       payload: {
         ...payload,
-        companyId: company.id,
+        companyId: companyId,
       },
       onSuccessDispatch: CONTACTS__LOADED,
     },
   }
 
   return (
-    <>
-      {company.archived && (
-        <Details
-          summary="Why can I not add a contact?"
-          data-test="archived-details"
+    <CompanyResource id={companyId}>
+      {(company) => (
+        <CompanyLayout
+          company={company}
+          breadcrumbs={[{ text: 'Contacts' }]}
+          dnbRelatedCompaniesCount={dnbRelatedCompaniesCount}
+          returnUrl={returnUrl}
+          localNavItems={localNavItems}
         >
-          Contacts cannot be added to an archived company.{' '}
-          <Link href={`/companies/${company.id}/unarchive`}>
-            Click here to unarchive
-          </Link>
-        </Details>
+          {company.archived && (
+            <Details
+              summary="Why can I not add a contact?"
+              data-test="archived-details"
+            >
+              Contacts cannot be added to an archived company.{' '}
+              <Link href={`/companies/${company.id}/unarchive`}>
+                Click here to unarchive
+              </Link>
+            </Details>
+          )}
+          <FilteredCollectionList
+            {...props}
+            collectionName="contact"
+            sortOptions={optionMetadata.sortOptions}
+            taskProps={collectionListTask}
+            selectedFilters={selectedFilters}
+            addItemUrl={
+              company.archived ? null : `/contacts/create?company=${company.id}`
+            }
+            entityName="contact"
+            defaultQueryParams={{
+              archived: ['false'],
+              sortby: 'modified_on:desc',
+              page: 1,
+            }}
+          />
+        </CompanyLayout>
       )}
-      <FilteredCollectionList
-        {...props}
-        collectionName="contact"
-        sortOptions={optionMetadata.sortOptions}
-        taskProps={collectionListTask}
-        selectedFilters={selectedFilters}
-        addItemUrl={
-          company.archived ? null : `/contacts/create?company=${company.id}`
-        }
-        entityName="contact"
-        defaultQueryParams={{
-          archived: ['false'],
-          sortby: 'modified_on:desc',
-          page: 1,
-        }}
-      ></FilteredCollectionList>
-    </>
+    </CompanyResource>
   )
 }
 
