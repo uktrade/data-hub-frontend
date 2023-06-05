ca0ccbc27d89d29faea9901d02090d96f48567a3
Christopher Sunkel <christopher.sunkel@digital.trade.gov.uk>
2023-06-05 18:36:55 +0100

    Refactor business details card

--- src/apps/companies/apps/company-overview/overview-table-cards/BusinessDetailsCard.jsx
+++ src/apps/companies/apps/company-overview/overview-table-cards/BusinessDetailsCard.jsx
@@ -1,117 +1,86 @@
 import React from 'react'
-import { Link, Table } from 'govuk-react'
+import { Link } from 'govuk-react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+
 import { NewWindowLink, SummaryTable } from '../../../../../client/components'
 import { currencyGBP } from '../../../../../client/utils/number-utils'
-import { GREY_1 } from '../../../../../client/utils/colours'
+import urls from '../../../../../lib/urls'
+import { buildCellContents } from './transformers'
+import {
+  StyledLastTableCell,
+  StyledSummaryTable,
+  StyledTableRow,
+} from './components'
 
-const StyledSummaryTable = styled(SummaryTable)`
-  margin: 0;
-  & > tbody th {
-    width: 50%;
-  }
-`
-const StyledTableRow = styled(Table.Row)`
-  border: 0;
-`
-const StyledLastTableCell = styled(Table.Cell)`
-  border: 0;
-  padding-bottom: 0;
-`
+const StyledAddressList = styled('ul')``
 
-const StyledSpan = styled('span')`
-  color: ${GREY_1};
-`
-
-const BusinessDetailsCard = ({ company, queryString, companiesHouseLink }) => {
-  const StyledAddressList = styled('ul')``
-  return (
-    <>
-      <StyledSummaryTable
-        caption="Business details"
-        data-test="businessDetailsContainer"
-      >
-        <SummaryTable.Row heading="Companies House">
-          {!company.company_number ? (
-            <StyledSpan>Not set</StyledSpan>
-          ) : (
-            <NewWindowLink
-              href={companiesHouseLink}
-              aria-label="Opens on Companies House website"
-              data-test="companies-house-link"
-            >
-              {company.company_number}
-            </NewWindowLink>
+const BusinessDetailsCard = ({ company }) => (
+  <StyledSummaryTable
+    caption="Business details"
+    data-test="businessDetailsContainer"
+  >
+    <SummaryTable.Row heading="Companies House">
+      {buildCellContents(
+        company.companyNumber,
+        <NewWindowLink
+          href={urls.external.companiesHouse(company.companyNumber)}
+          aria-label="Opens on Companies House website"
+          data-test="companies-house-link"
+        >
+          {company.companyNumber}
+        </NewWindowLink>
+      )}
+    </SummaryTable.Row>
+    <SummaryTable.Row heading="Trading Address">
+      {buildCellContents(
+        company.address,
+        <StyledAddressList>
+          {company.address?.line1 && <li>{company.address.line1}</li>}
+          {company.registeredAddress?.line2 && <li>{company.address.line2}</li>}
+          {company.registeredAddress?.town && <li>{company.address.town}</li>}
+          {company.registeredAddress?.postcode && (
+            <li>{company.address.postcode}</li>
           )}
-        </SummaryTable.Row>
-
-        <SummaryTable.Row heading="Trading Address">
-          {!company.address ? (
-            <StyledSpan>Not set</StyledSpan>
-          ) : (
-            <StyledAddressList>
-              {company.address?.line_1 && <li>{company.address.line_1}</li>}
-              {company.registered_address?.line_2 && (
-                <li>{company.address.line_2}</li>
-              )}
-              {company.registered_address?.town && (
-                <li>{company.address.town}</li>
-              )}
-              {company.registered_address?.postcode && (
-                <li>{company.address.postcode}</li>
-              )}
-            </StyledAddressList>
-          )}
-        </SummaryTable.Row>
-        <SummaryTable.Row heading="Website">
-          {!company.website ? (
-            <StyledSpan>Not set</StyledSpan>
-          ) : (
-            <NewWindowLink href={company.website}>
-              {company.website}
-            </NewWindowLink>
-          )}
-        </SummaryTable.Row>
-        <SummaryTable.Row heading="Turnover">
-          {!company.turnover_gbp && !company.turnover_range ? (
-            <StyledSpan>Not set</StyledSpan>
-          ) : company.turnover_gbp ? (
-            currencyGBP(company.turnover_gbp, {
+        </StyledAddressList>
+      )}
+    </SummaryTable.Row>
+    <SummaryTable.Row heading="Website">
+      {buildCellContents(
+        company.website,
+        <NewWindowLink href={company.website}>{company.website}</NewWindowLink>
+      )}
+    </SummaryTable.Row>
+    <SummaryTable.Row heading="Turnover">
+      {buildCellContents(
+        company.turnoverGbp && company.turnoverRange,
+        company.turnoverGbp
+          ? currencyGBP(company.turnoverGbp, {
               maximumSignificantDigits: 2,
             })
-          ) : (
-            company.turnover_range?.name
-          )}
-        </SummaryTable.Row>
-        <SummaryTable.Row heading="Number of Employees">
-          {!company.number_of_employees ? (
-            <StyledSpan>Not set</StyledSpan>
-          ) : (
-            company.number_of_employees
-          )}
-        </SummaryTable.Row>
-        <SummaryTable.Row heading="DBT Sector">
-          {!company.sector ? 'Not set' : company.sector.name}
-        </SummaryTable.Row>
-        <StyledTableRow>
-          <StyledLastTableCell colSpan={2}>
-            <Link
-              href={`${queryString}/business-details`}
-              data-test="business-page-link"
-            >
-              View full business details
-            </Link>
-          </StyledLastTableCell>
-        </StyledTableRow>
-      </StyledSummaryTable>
-    </>
-  )
-}
-
+          : company.turnoverRange?.name
+      )}
+    </SummaryTable.Row>
+    <SummaryTable.Row heading="Number of Employees">
+      {buildCellContents(company.numberOfEmployees, company.numberOfEmployees)}
+    </SummaryTable.Row>
+    <SummaryTable.Row heading="DBT Sector">
+      {buildCellContents(company.sector, company.sector?.name)}
+    </SummaryTable.Row>
+    <StyledTableRow>
+      <StyledLastTableCell colSpan={2}>
+        <Link
+          href={urls.companies.businessDetails(company.id)}
+          data-test="business-page-link"
+        >
+          View full business details
+        </Link>
+      </StyledLastTableCell>
+    </StyledTableRow>
+  </StyledSummaryTable>
+)
 BusinessDetailsCard.propTypes = {
   company: PropTypes.object.isRequired,
-  companiesHouseLink: PropTypes.string.isRequired,
 }
 
 export default BusinessDetailsCard
