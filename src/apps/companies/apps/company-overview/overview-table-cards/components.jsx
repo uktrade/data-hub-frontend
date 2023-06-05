ab7af4e79735849f89d72cfd891e2b470a81fae4
Christopher Sunkel <christopher.sunkel@digital.trade.gov.uk>
2023-06-05 18:35:52 +0100

    Refactor CompanyOverview page to use new template

---
+++ src/apps/companies/apps/company-overview/overview-table-cards/components.jsx
@@ -0,0 +1,29 @@
+/**
+ * Components shared across multiple cards
+ */
+
+import styled from 'styled-components'
+import { Table } from 'govuk-react'
+
+import { SummaryTable } from '../../../../../client/components'
+import { GREY_1 } from '../../../../../client/utils/colours'
+
+export const StyledLastTableCell = styled(Table.Cell)`
+  border: 0;
+  padding-bottom: 0;
+`
+
+export const StyledSummaryTable = styled(SummaryTable)`
+  margin: 0;
+  & > tbody th {
+    width: 50%;
+  }
+`
+
+export const StyledTableRow = styled(Table.Row)`
+  border: 0;
+`
+
+export const StyledSpan = styled('span')`
+  color: ${GREY_1};
+`
