dbb9bc040341f4e4d8dda5a4c7ede87b601b0194
Christopher Sunkel <christopher.sunkel@digital.trade.gov.uk>
2023-06-05 14:44:22 +0100

    Move isItaTierDAccount function to separate file so the CompanyOverview can access it

---
+++ src/client/modules/Companies/utils.js
@@ -0,0 +1,6 @@
+/**
+ * Checks if a company is in the D tier on the one list.
+ */
+export const isItaTierDAccount = (oneListGroupTier) =>
+  oneListGroupTier &&
+  oneListGroupTier.id === '1929c808-99b4-4abf-a891-45f2e187b410'
