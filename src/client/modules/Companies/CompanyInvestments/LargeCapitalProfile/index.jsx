d19a66a638a0415e0fe7e61225af111e26be9450
Christopher Sunkel <christopher.sunkel@digital.trade.gov.uk>
2023-06-07 16:53:38 +0100

    Refactor large capital profile page to use new layout

--- src/client/modules/Companies/CompanyInvestments/LargeCapitalProfile/index.jsx
+++ src/client/modules/Companies/CompanyInvestments/LargeCapitalProfile/index.jsx
@@ -7,8 +7,13 @@ import pluralize from 'pluralize'
 import styled from 'styled-components'
 
 import { SummaryTable, ToggleSection } from '../../../../components'
-import { LargeInvestorProfileResource } from '../../../../components/Resource'
+import {
+  CompanyResource,
+  LargeInvestorProfileResource,
+} from '../../../../components/Resource'
 import { RED } from '../../../../utils/colours'
+import CompanyLayout from '../../../../components/Layout/CompanyLayout'
+
 import ProfileDetailsTable from './ProfileDetailsTable'
 import EditProfileDetails from './EditProfileDetails'
 import ProfileRequirementsTable from './ProfileRequirementsTable'
@@ -68,54 +73,75 @@ const ProfileSection = ({
   </>
 )
 
-const LargeCapitalProfile = ({ companyId }) => {
+const LargeCapitalProfile = ({
+  companyId,
+  dnbRelatedCompaniesCount,
+  localNavItems,
+  flashMessages,
+}) => {
   const [detailsFormIsOpen, setDetailsFormIsOpen] = useState(false)
   const [requirementsFormIsOpen, setRequirementsFormIsOpen] = useState(false)
   const [locationFormIsOpen, setLocationFormIsOpen] = useState(false)
   return (
     <LargeInvestorProfileResource id={companyId}>
       {(profile) => (
-        <>
-          {profile.results.length != 0 ? (
-            <>
-              <H2 size={LEVEL_SIZE[3]}>Large capital investor profile</H2>
-              <ProfileSection
-                incompleteFields={profile.results[0].incompleteDetailsFields}
-                form={<EditProfileDetails profile={profile.results[0]} />}
-                toggleName="Investor details"
-                id="investor_details"
-                isEditing={detailsFormIsOpen}
-                onEdit={setDetailsFormIsOpen}
-              >
-                <ProfileDetailsTable profile={profile.results[0]} />
-              </ProfileSection>
-              <ProfileSection
-                incompleteFields={
-                  profile.results[0].incompleteRequirementsFields
-                }
-                form={<EditRequirementsForm profile={profile.results[0]} />}
-                toggleName="Investor requirements"
-                id="investor_requirements"
-                isEditing={requirementsFormIsOpen}
-                onEdit={setRequirementsFormIsOpen}
-              >
-                <ProfileRequirementsTable profile={profile.results[0]} />
-              </ProfileSection>
-              <ProfileSection
-                incompleteFields={profile.results[0].incompleteLocationFields}
-                form={<EditLocationForm profile={profile.results[0]} />}
-                toggleName="Location"
-                id="location"
-                isEditing={locationFormIsOpen}
-                onEdit={setLocationFormIsOpen}
-              >
-                <ProfileLocationTable profile={profile.results[0]} />
-              </ProfileSection>
-            </>
-          ) : (
-            <CreateLargeCapitalProfile companyId={companyId} />
+        <CompanyResource id={companyId}>
+          {(company) => (
+            <CompanyLayout
+              company={company}
+              breadcrumbs={[{ text: 'Investments' }]}
+              dnbRelatedCompaniesCount={dnbRelatedCompaniesCount}
+              localNavItems={localNavItems}
+              flashMessages={flashMessages}
+              isInvestment={true}
+              isLCP={true}
+            >
+              {profile.results.length != 0 ? (
+                <>
+                  <H2 size={LEVEL_SIZE[3]}>Large capital investor profile</H2>
+                  <ProfileSection
+                    incompleteFields={
+                      profile.results[0].incompleteDetailsFields
+                    }
+                    form={<EditProfileDetails profile={profile.results[0]} />}
+                    toggleName="Investor details"
+                    id="investor_details"
+                    isEditing={detailsFormIsOpen}
+                    onEdit={setDetailsFormIsOpen}
+                  >
+                    <ProfileDetailsTable profile={profile.results[0]} />
+                  </ProfileSection>
+                  <ProfileSection
+                    incompleteFields={
+                      profile.results[0].incompleteRequirementsFields
+                    }
+                    form={<EditRequirementsForm profile={profile.results[0]} />}
+                    toggleName="Investor requirements"
+                    id="investor_requirements"
+                    isEditing={requirementsFormIsOpen}
+                    onEdit={setRequirementsFormIsOpen}
+                  >
+                    <ProfileRequirementsTable profile={profile.results[0]} />
+                  </ProfileSection>
+                  <ProfileSection
+                    incompleteFields={
+                      profile.results[0].incompleteLocationFields
+                    }
+                    form={<EditLocationForm profile={profile.results[0]} />}
+                    toggleName="Location"
+                    id="location"
+                    isEditing={locationFormIsOpen}
+                    onEdit={setLocationFormIsOpen}
+                  >
+                    <ProfileLocationTable profile={profile.results[0]} />
+                  </ProfileSection>
+                </>
+              ) : (
+                <CreateLargeCapitalProfile companyId={companyId} />
+              )}
+            </CompanyLayout>
           )}
-        </>
+        </CompanyResource>
       )}
     </LargeInvestorProfileResource>
   )
