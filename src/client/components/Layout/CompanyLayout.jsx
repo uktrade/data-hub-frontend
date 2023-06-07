96d2e55091cac77806a928fba87bca1969dd7743
Christopher Sunkel <christopher.sunkel@digital.trade.gov.uk>
2023-06-07 16:52:19 +0100

    Add sub-navigation for investment pages

--- src/client/components/Layout/CompanyLayout.jsx
+++ src/client/components/Layout/CompanyLayout.jsx
@@ -4,6 +4,7 @@ import GridCol from '@govuk-react/grid-col'
 import GridRow from '@govuk-react/grid-row'
 
 import { CompanyLocalHeader2, CompanyTabbedLocalNavigation, Main } from '..'
+import CompanyInvestmentSubNavigation from '../CompanyTabbedLocalNavigation/CompanyInvestmentTab'
 
 const CompanyLayout = ({
   company,
@@ -13,33 +14,38 @@ const CompanyLayout = ({
   children,
   returnUrl,
   localNavItems,
-}) => {
-  return (
-    <>
-      <CompanyLocalHeader2
-        breadcrumbs={breadcrumbs}
-        flashMessages={flashMessages}
+  isInvestment = false,
+  isLCP = false,
+}) => (
+  <>
+    <CompanyLocalHeader2
+      breadcrumbs={breadcrumbs}
+      flashMessages={flashMessages}
+      company={company}
+      dnbRelatedCompaniesCount={dnbRelatedCompaniesCount}
+      returnUrl={returnUrl}
+    />
+    <Main>
+      <CompanyTabbedLocalNavigation
+        localNavItems={localNavItems}
         company={company}
-        dnbRelatedCompaniesCount={dnbRelatedCompaniesCount}
-        returnUrl={returnUrl}
       />
-      <Main>
-        <CompanyTabbedLocalNavigation
-          localNavItems={localNavItems}
-          company={company}
-        />
-        <GridRow>
-          <GridCol>{children}</GridCol>
-        </GridRow>
-      </Main>
-    </>
-  )
-}
+      {isInvestment && (
+        <CompanyInvestmentSubNavigation companyId={company.id} isLCP={isLCP} />
+      )}
+      <GridRow>
+        <GridCol>{children}</GridCol>
+      </GridRow>
+    </Main>
+  </>
+)
 
 CompanyLayout.propTypes = {
   company: PropTypes.object.isRequired,
   permissions: PropTypes.array.isRequired,
   children: PropTypes.element.isRequired,
+  isInvestment: PropTypes.bool,
+  isLCP: PropTypes.bool,
 }
 
 export default CompanyLayout
