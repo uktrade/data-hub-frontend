0ad83f4e4cf2746b6185c0d9b664fffa6a29ff67
Christopher Sunkel <christopher.sunkel@digital.trade.gov.uk>
2023-06-09 11:22:19 +0100

    Fixup! CompanyProjectsCollection

--- src/client/modules/Companies/CompanyInvestments/CompanyProjectsCollection.jsx
+++ src/client/modules/Companies/CompanyInvestments/CompanyProjectsCollection.jsx
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 
 import { CompanyResource } from '../../../components/Resource'
 import CompanyLayout from '../../../components/Layout/CompanyLayout'
-import urls from '../../../../lib/urls'
 import ProjectsCollection from '../../../../apps/investments/client/projects/ProjectsCollection'
 import { projectsState2props } from '../../../../apps/investments/client/projects/state'
 
@@ -18,15 +17,7 @@ const CompanyProjectsCollection = ({
     {(company) => (
       <CompanyLayout
         company={company}
-        breadcrumbs={[
-          { link: urls.dashboard(), text: 'Home' },
-          {
-            link: urls.companies.index(),
-            text: 'Companies',
-          },
-          { link: urls.companies.detail(company.id), text: company.name },
-          { text: 'Investment' },
-        ]}
+        breadcrumbs={[{ text: 'Investment' }]}
         dnbRelatedCompaniesCount={dnbRelatedCompaniesCount}
         returnUrl={returnUrl}
         localNavItems={localNavItems}
