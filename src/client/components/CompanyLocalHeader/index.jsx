e05e958b61e09868cef276fd9fc0d11fc471f4bc
Chris Hopkins <chris.hopkins@madetech.com>
2023-06-28 09:17:51 +0100

    moved if check to company tree link

--- src/client/components/CompanyLocalHeader/index.jsx
+++ src/client/components/CompanyLocalHeader/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import pluralize from 'pluralize'
 import Main from '@govuk-react/main'
 import GridCol from '@govuk-react/grid-col'
 import GridRow from '@govuk-react/grid-row'
@@ -99,8 +98,8 @@ const StyledMain = styled(Main)`
     font-size: ${FONT_SIZE.SIZE_20};
   }
 `
-const StyledDiv = styled('div')`
-  padding-bottom: 8px;
+const StyledRelatedCompaniesWrapper = styled('div')`
+  padding-bottom: 20px;
 `
 
 const isUltimate = (company) => !!company.isGlobalUltimate
@@ -139,14 +138,16 @@ const CompanyLocalHeader = ({
             <StyledAddress data-test="address">
               {addressToStringResource(company.address)}
             </StyledAddress>
-            <StyledDiv>
-              <Link
-                href={urls.companies.dnbHierarchy.tree(company.id)}
-                data-test="company-tree-link"
-              >
-                View related companies
-              </Link>
-            </StyledDiv>
+            {dnbRelatedCompaniesCount > 0 && (
+              <StyledRelatedCompaniesWrapper>
+                <Link
+                  href={urls.companies.dnbHierarchy.tree(company.id)}
+                  data-test="company-tree-link"
+                >
+                  View related companies
+                </Link>
+              </StyledRelatedCompaniesWrapper>
+            )}
           </GridCol>
           <GridCol setWith="one-third">
             <StyledButtonContainer>
@@ -195,19 +196,8 @@ const CompanyLocalHeader = ({
             )}
           </TypeWrapper>
         )}
-        {(dnbRelatedCompaniesCount > 0 ||
-          hasManagedAccountDetails(company)) && (
+        {hasManagedAccountDetails(company) && (
           <StyledDescription data-test="description">
-            {dnbRelatedCompaniesCount > 0 && (
-              <p>
-                Data Hub contains{' '}
-                <a href={urls.companies.dnbHierarchy.index(company.id)}>
-                  {dnbRelatedCompaniesCount} other company{' '}
-                  {pluralize('record', dnbRelatedCompaniesCount)}
-                </a>{' '}
-                related to this company
-              </p>
-            )}
             {hasManagedAccountDetails(company) && (
               <>
                 <p>
