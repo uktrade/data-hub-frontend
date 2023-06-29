61991cd72634132d2cc1d7ba7ee135534800db9e
Chris Hopkins <chris.hopkins@madetech.com>
2023-06-29 13:09:49 +0100

    added the count as part of the link

--- src/client/components/CompanyLocalHeader/index.jsx
+++ src/client/components/CompanyLocalHeader/index.jsx
@@ -144,7 +144,7 @@ const CompanyLocalHeader = ({
                   href={urls.companies.dnbHierarchy.tree(company.id)}
                   data-test="company-tree-link"
                 >
-                  View related companies
+                  {`View ${dnbRelatedCompaniesCount} related companies`}
                 </Link>
               </StyledRelatedCompaniesWrapper>
             )}
