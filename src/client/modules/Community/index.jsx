a8ebdb2367ed3438cc883178203c818410ea9cee
Derek Baker <hello@derekbaker.dev>
2023-07-10 17:42:38 +0100

    update links with new window links
    write test for mailto: link

--- src/client/modules/Community/index.jsx
+++ src/client/modules/Community/index.jsx
@@ -4,8 +4,7 @@ import feedbackImg from './img-feedback.jpg'
 import principlesImg from './img-principles.jpg'
 import trainingImg from './img-training.jpg'
 import researchImg from './img-research.jpg'
-import { DefaultLayout } from '../../components'
-import { Link } from 'govuk-react'
+import { DefaultLayout, NewWindowLink } from '../../components'
 import { H4 } from '@govuk-react/heading'
 import GridRow from '@govuk-react/grid-row'
 import GridCol from '@govuk-react/grid-col'
@@ -13,7 +12,7 @@ import styled from 'styled-components'
 import urls from '../../../lib/urls'
 
 const StyledHeading = styled(H4)`
-  font-size: 19px;
+  font-size: 24px;
   font-style: normal;
   font-weight: 700;
   line-height: 34px;
@@ -51,20 +50,21 @@ const Community = () => {
         },
       ]}
       useReactRouter={false}
-      // data-test="community-page"
     >
       <GridRow>
         <GridCol>
           <div data-test="community-roadmap">
             <StyledImage src={roadmapImg} alt="Roadmap" />
             <StyledHeading data-test="community-roadmap-header">
-              Take a look at what we're working on
+              Take a look at what we&apos;re working on
             </StyledHeading>
-            <StyledParagraph>
+            <StyledParagraph data-test="community-roadmap-paragraph">
               Our{' '}
-              <Link href={urls.external.community.roadmap()}>CRM roadmap</Link>{' '}
-              shows what we're currently working on and the work that is lined
-              up over the coming months
+              <NewWindowLink href={urls.external.community.roadmap()}>
+                CRM roadmap
+              </NewWindowLink>{' '}
+              shows what we&apos;re currently working on and the work that is
+              lined up over the coming months
             </StyledParagraph>
           </div>
         </GridCol>
@@ -76,9 +76,9 @@ const Community = () => {
             </StyledHeading>
             <StyledParagraph>
               Do you have a great idea? We would love to hear it. Tell us your{' '}
-              <Link href={urls.external.community.feedback()}>
+              <NewWindowLink href={urls.external.community.feedback()}>
                 feedback or experience
-              </Link>{' '}
+              </NewWindowLink>{' '}
               using our CRM tools.
             </StyledParagraph>
           </div>
@@ -95,9 +95,9 @@ const Community = () => {
               These set the direction for future work around customer
               relationship management and help us to plan and prioritise
               upcoming development.{' '}
-              <Link href={urls.external.community.principles()}>
+              <NewWindowLink href={urls.external.community.principles()}>
                 Find out more about our CRM principles.
-              </Link>
+              </NewWindowLink>
             </StyledParagraph>
           </div>
         </GridCol>
@@ -112,9 +112,9 @@ const Community = () => {
               We offer introductions to Data Hub and Data Workspace as well as
               bootcamps to help use our analysis and visualisation tools
               effectively.{' '}
-              <Link href={urls.external.community.training()}>
+              <NewWindowLink href={urls.external.community.training()}>
                 View available training
-              </Link>{' '}
+              </NewWindowLink>{' '}
               and sign up today!.
             </StyledParagraph>
           </div>
@@ -127,7 +127,7 @@ const Community = () => {
               We regularly run user research sessions to find out how our
               current tools are used and to test new solutions. We need your
               help, if you are interested
-              <Link
+              <NewWindowLink
                 href={
                   'mailto:' +
                   email +
@@ -139,7 +139,7 @@ const Community = () => {
               >
                 {' '}
                 sign up to take part in user research
-              </Link>
+              </NewWindowLink>
               .
             </StyledParagraph>
           </div>
--- test/functional/cypress/specs/community/community-spec.js
+++ test/functional/cypress/specs/community/community-spec.js
@@ -2,41 +2,41 @@ import urls from '../../../../../src/lib/urls'
 
 describe('Community', () => {
   context('When visiting the Community page', () => {
-    // a test for the page title.
-    // it('should display the correct page title', () => {
-    //   cy.visit(urls.community.index())
-    //   cy.get('[data-test="community-page"]')
-    //     .children()
-    //     .should('contain.text', 'CRM community')
-    // })
-    // // a test for the subheading.
-    // it('should display the correct subheading', () => {
-    //   cy.visit(urls.community.index())
-    //   cy.get('[data-test="community-page"]')
-    //     .children()
-    //     .should(
-    //       'contain.text',
-    //       'Find out about upcoming improvements to CRM and to help shape its future development.'
-    //     )
-    // })
-    // // a test for the breadcrumbs.
-    // it('should display the correct breadcrumbs', () => {
-    //   cy.visit(urls.community.index())
-    //   cy.get('[data-test="community-page"]')
-    //     .children()
-    //     .should('contain.text', 'Home')
-    //     .should('contain.text', 'CRM Community')
-    // })
-    // a test for the roadmap block.
+    it('should display the correct page title', () => {
+      cy.visit(urls.community.index())
+      cy.get('[data-test="localHeader"]')
+        .children()
+        .should('contain.text', 'CRM community')
+    })
+
+    it('should display the correct subheading', () => {
+      cy.visit(urls.community.index())
+      cy.get('[data-test="localHeader"]')
+        .children()
+        .should(
+          'contain.text',
+          'Find out about upcoming improvements to CRM and to help shape its future development.'
+        )
+    })
+
+    it('should display the correct breadcrumbs', () => {
+      cy.visit(urls.community.index())
+      cy.get('[data-test="localHeader"]')
+        .children()
+        .should('contain.text', 'Home')
+        .should('contain.text', 'CRM Community')
+    })
+
     it('should display the roadmap block', () => {
       cy.visit(urls.community.index())
       cy.get('[data-test="community-roadmap"]')
         .children()
-        .should('contain.text', 'Take a look at what we’re working on')
+        .should('exist')
+        .should('contain.text', "Take a look at what we're working on")
         .children()
         .should('have.attr', 'href', urls.external.community.roadmap())
     })
-    // a test for the feedback block.
+
     it('should display the feedback block', () => {
       cy.visit(urls.community.index())
       cy.get('[data-test="community-feedback"]')
@@ -46,7 +46,7 @@ describe('Community', () => {
         .children()
         .should('have.attr', 'href', urls.external.community.feedback())
     })
-    // a test for the principles block.
+
     it('should display the principles block', () => {
       cy.visit(urls.community.index())
       cy.get('[data-test="community-principles"]')
@@ -56,7 +56,7 @@ describe('Community', () => {
         .children()
         .should('have.attr', 'href', urls.external.community.principles())
     })
-    // a test for the training block
+
     it('should display the training block', () => {
       cy.visit(urls.community.index())
       cy.get('[data-test="community-training"]')
@@ -66,12 +66,25 @@ describe('Community', () => {
         .children()
         .should('have.attr', 'href', urls.external.community.training())
     })
-    // a test for the user research block.
+
     it('should display the user research block', () => {
       cy.visit(urls.community.index())
       cy.get('[data-test="community-research"]')
         .should('exist')
         .should('contain.text', 'Volunteer for user research')
+    })
+
+    it('should display the correct email subject', () => {
+      cy.visit(urls.community.index())
+      cy.get('[data-test="community-research"]')
+        .children()
+        .should('exist')
+        .children()
+        .should(
+          'have.attr',
+          'href',
+          'mailto:crmresearch@trade.gov.uk?subject=Volunteering for user research&body=Hello, I would like to volunteer to take part in future user research around CRM.'
+        )
     })
   })
 })
