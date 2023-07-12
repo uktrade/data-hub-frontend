606fc04622ec90c944d6d68548fdc9a67d918bca
Christopher Sunkel <christopher.sunkel@digital.trade.gov.uk>
2023-07-12 10:17:12 +0100

    Hide links to legacy file browser

--- src/client/components/DocumentsSection/index.jsx
+++ src/client/components/DocumentsSection/index.jsx
@@ -1,29 +1,14 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
-import { NewWindowLink, SectionHeader } from '../../components'
+import { SectionHeader } from '../../components'
 
-const DocumentsSection = ({ fileBrowserRoot, documentPath }) => {
+const DocumentsSection = () => {
   return (
     <>
       <SectionHeader type="document">Documents</SectionHeader>
-      {documentPath ? (
-        <NewWindowLink
-          href={fileBrowserRoot + documentPath}
-          data-test="document-link"
-        >
-          View files and documents
-        </NewWindowLink>
-      ) : (
-        <p data-test="no-documents-message">There are no files or documents</p>
-      )}
+      <p data-test="no-documents-message">There are no files or documents</p>
     </>
   )
-}
-
-DocumentsSection.propTypes = {
-  fileBrowserRoot: PropTypes.string.isRequired,
-  documentPath: PropTypes.string.isRequired,
 }
 
 export default DocumentsSection
--- test/functional/cypress/specs/contacts/documents-spec.js
+++ test/functional/cypress/specs/contacts/documents-spec.js
@@ -3,29 +3,6 @@ import { assertBreadcrumbs } from '../../support/assertions'
 const { contacts } = require('../../../../../src/lib/urls')
 
 describe('Contact Documents', () => {
-  context('when there is a document link', () => {
-    before(() => {
-      cy.visit('/contacts/default-contact-with-document/documents')
-    })
-
-    it('should render breadcrumbs', () => {
-      assertBreadcrumbs({
-        Home: '/',
-        Contacts: contacts.index(),
-        'Joseph Woof': '/contacts/5e75d636-1d24-416a-aaf0-3fb220d594ce',
-        Documents: null,
-      })
-    })
-
-    it('should display appropriate message when there is a link to a document', () => {
-      cy.get('[data-test=document-header]').should('contain', 'Documents')
-      cy.get('[data-test=document-link]').should(
-        'contain',
-        'View files and documents'
-      )
-    })
-  })
-
   context('when there is not a document link', () => {
     before(() => {
       cy.visit('/contacts/5555d636-1d24-416a-aaf0-3fb220d59aaa/documents')
