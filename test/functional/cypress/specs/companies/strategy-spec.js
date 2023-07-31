adddf69727470700c8ff455938ceed3b57da2332
Claudia Gonzalez-Casales <claudia.gonzalescasales@digital.trade.gov.uk>
2023-07-31 16:15:26 +0100

    Added tests for strategy form

--- test/functional/cypress/specs/companies/strategy-spec.js
+++ test/functional/cypress/specs/companies/strategy-spec.js
@@ -1,15 +1,45 @@
 const fixtures = require('../../fixtures')
 const urls = require('../../../../../src/lib/urls')
+const { assertFieldTextarea } = require('../../support/assertions')
+
+const companyAccountManagementUrl = urls.companies.accountManagement.index(
+  fixtures.company.allActivitiesCompany.id
+)
 
 describe('Company account management strategy', () => {
   context('When visiting the strategy create page', () => {
-    it('should display the h1 heading of Strategy', () => {
+    it('should display the header', () => {
       cy.visit(
         urls.companies.accountManagement.create(
           fixtures.company.allActivitiesCompany.id
         )
       )
-      cy.get('h1').contains('Strategy')
+      cy.get('h1').should(
+        'have.text',
+        `Add strategy for ${fixtures.company.allActivitiesCompany.name}`
+      )
+    })
+
+    it('should display the strategy field', () => {
+      cy.get('[data-test="field-strategy"]').then((element) => {
+        assertFieldTextarea({
+          element,
+          label: 'Strategy',
+          hint: "This should outline a plan than provides a concise overview of the business direction and DBT's approach to help them achieve that.",
+        })
+      })
+    })
+
+    it('save strategy button should link to company account management page', () => {
+      cy.get('[data-test="submit-button"]').contains('Save strategy').click()
+      cy.location('pathname').should('eq', companyAccountManagementUrl)
+      cy.go('back')
+    })
+
+    it('cancels form when back link is clicked', () => {
+      cy.get('[data-test="cancel-button"]').contains('Back').click()
+      cy.location('pathname').should('eq', companyAccountManagementUrl)
+      cy.go('back')
     })
   })
 })
