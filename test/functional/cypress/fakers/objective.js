e937c278b1457f8d790dbb1de96055ecf0fb64f2
Chris Hopkins <chris.hopkins@madetech.com>
2023-08-01 16:52:59 +0100

    added faker for objective, and 2 placeholder tests

---
+++ test/functional/cypress/fakers/objective.js
@@ -0,0 +1,22 @@
+import { faker } from '@faker-js/faker'
+
+import { listFaker } from './utils'
+
+const objectiveFaker = (overrides = {}) => ({
+  id: faker.string.uuid(),
+  company: faker.string.uuid(),
+  subject: faker.word.sample(),
+  detail: faker.word.words(),
+  target_date: faker.date.future(),
+  has_blocker: true,
+  blocker_description: faker.word.words(),
+  progress: faker.helpers.rangeToNumber({ min: 0, max: 100 }),
+  ...overrides,
+})
+
+const objectiveListFaker = (length = 1, overrides) =>
+  listFaker({ fakerFunction: objectiveFaker, length, overrides })
+
+export { objectiveFaker, objectiveListFaker }
+
+export default objectiveListFaker
