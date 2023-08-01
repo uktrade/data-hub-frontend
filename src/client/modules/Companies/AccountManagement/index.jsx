8407448e24582e4dc915c00c31f0b3e7beb7397a
Chris Hopkins <102232401+chopkinsmade@users.noreply.github.com>
2023-08-01 11:05:45 +0100

    CLS2-342/display-strategy-on-ac-tab (#5917)
    * Added strategy for company
    
    * styling updates
    
    * functional test fix

--- src/client/modules/Companies/AccountManagement/index.jsx
+++ src/client/modules/Companies/AccountManagement/index.jsx
@@ -1,16 +1,95 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { CompanyResource } from '../../../components/Resource'
+import { H3, Link } from 'govuk-react'
+import Button from '@govuk-react/button'
+import urls from '../../../../lib/urls'
+import { format } from '../../../../client/utils/date'
+import { GridCol, GridRow } from 'govuk-react'
+import styled from 'styled-components'
+import { DARK_GREY, GREY_3, TEXT_COLOUR } from '../../../utils/colours'
 import { DefaultLayout } from '../../../components'
+import { FONT_SIZE } from '@govuk-react/constants'
 
-const AccountManagement = () => {
+const LastUpdatedHeading = styled.div`
+  color: ${DARK_GREY};
+  font-weight: normal;
+  margin-top: -20px;
+  margin-bottom: -10px;
+  font-size: ${FONT_SIZE.SIZE_14};
+`
+
+const Strategy = ({ company }) => (
+  <>
+    <GridRow>
+      <GridCol>
+        <H3>Strategy</H3>
+      </GridCol>
+      {company.strategy && (
+        <div>
+          <Link
+            href={urls.companies.accountManagement.create(company.id)}
+            data-test="edit-strategy-link"
+          >
+            Edit
+          </Link>
+        </div>
+      )}
+    </GridRow>
+
+    {company.strategy && (
+      <>
+        <GridCol>
+          <GridRow>
+            <LastUpdatedHeading data-test="last-updated-details">
+              <span>{`Last updated by ${company?.modifiedBy?.name}: ${format(
+                company.modifiedOn
+              )}. `}</span>
+              <span>
+                View changes in{' '}
+                <Link
+                  href={urls.companies.editHistory.index(company.id)}
+                  data-test="edit-history-link"
+                >
+                  Edit history page
+                </Link>
+              </span>
+            </LastUpdatedHeading>
+          </GridRow>
+        </GridCol>
+        <p>{company.strategy}</p>
+      </>
+    )}
+
+    {!company.strategy && (
+      <Button
+        data-test="add-strategy-button"
+        as={Link}
+        href={urls.companies.accountManagement.create(company.id)}
+        buttonColour={GREY_3}
+        buttonTextColour={TEXT_COLOUR}
+      >
+        Add strategy
+      </Button>
+    )}
+  </>
+)
+
+const AccountManagement = ({}) => {
+  const { companyId } = useParams()
   return (
-    <DefaultLayout
-      heading={'Account Management'}
-      pageTitle={'Account Management'}
-      breadcrumbs={[]}
-      useReactRouter={false}
-    >
-      <h1>Account Management</h1>
-    </DefaultLayout>
+    <CompanyResource id={companyId}>
+      {(company) => (
+        <DefaultLayout
+          heading={'Account Management'}
+          pageTitle={'Account Management'}
+          breadcrumbs={[{ text: 'Account Management' }]}
+          useReactRouter={false}
+        >
+          <Strategy company={company} />
+        </DefaultLayout>
+      )}
+    </CompanyResource>
   )
 }
 
