90fe967e824005b6dd5f29f9937cbd16947e571c
Derek Baker <derek.baker89@gmail.com>
2023-08-16 15:09:02 +0100

    WIP: archived objectives test

---
+++ test/functional/cypress/specs/companies/archived-objectives-spec.js
@@ -0,0 +1,30 @@
+// import { companyFaker } from '../../fakers/companies'
+// import { objectiveFaker } from '../../fakers/objective'
+
+// const {
+//   assertErrorSummary,
+//   assertPayload,
+//   assertFieldDateShort,
+// } = require('../../support/assertions')
+
+// const { fill } = require('../../../../functional/cypress/support/form-fillers')
+
+// const fixtures = require('../../fixtures')
+// const urls = require('../../../../../src/lib/urls')
+
+// const companyId = fixtures.company.allActivitiesCompany.id
+
+// const assertHeader = (company) => {
+//   cy.get('h1').should('contain', 'Archived Objectives')
+// }
+
+// const assertBreadcrumbs = (company) => {
+//   it('should render breadcrumbs', () => {
+//     assertBreadcrumbs({
+//       Home: urls.dashboard.index(),
+//       Companies: urls.companies.index(),
+//       [company.name]: urls.companies.detail(company.id),
+//       'Account management': null,
+//     })
+//   })
+// }
