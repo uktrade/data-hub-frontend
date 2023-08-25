72b3fceb2fedd1acb3ff30ef926b9a15f204e354
Christopher Sunkel <christopher.sunkel@digital.trade.gov.uk>
2023-08-25 14:46:49 +0100

    Use hooks for location and history

--- src/client/modules/Investments/Projects/ProjectInteractions.jsx
+++ src/client/modules/Investments/Projects/ProjectInteractions.jsx
@@ -3,78 +3,74 @@ import PropTypes from 'prop-types'
 import { H2 } from 'govuk-react'
 import { LEVEL_SIZE } from '@govuk-react/constants'
 import qs from 'qs'
-import { Route } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
 
 import { CollectionList } from '../../../components'
 import { transformInteractionToListItem } from '../../../../apps/interactions/client/transformers'
 import { InteractionCollectionResource } from '../../../components/Resource'
 import urls from '../../../../lib/urls'
 
-const ProjectInteractions = ({ projectId }) => (
-  <Route>
-    {({ history, location }) => {
-      const parsedQueryString = qs.parse(location.search.slice(1))
-      const activePage = parseInt(parsedQueryString.page, 10) || 1
-      return (
-        <InteractionCollectionResource
-          payload={{
-            investment_project_id: projectId,
-            limit: 10,
-            offset: activePage * 10 - 10,
-            sortby: parsedQueryString.sortby,
-          }}
-        >
-          {(_, count, rawData) => {
-            const interactions = rawData.results.map(
-              transformInteractionToListItem
-            )
-            const sortOptions = [
-              {
-                name: 'Recently created',
-                value: '-date',
-              },
-              {
-                name: 'Oldest',
-                value: 'date',
-              },
-            ]
+const ProjectInteractions = ({ projectId }) => {
+  const history = useHistory()
+  const location = useLocation()
+  const parsedQueryString = qs.parse(location.search.slice(1))
+  const activePage = parseInt(parsedQueryString.page, 10) || 1
+  return (
+    <InteractionCollectionResource
+      payload={{
+        investment_project_id: projectId,
+        limit: 10,
+        offset: activePage * 10 - 10,
+        sortby: parsedQueryString.sortby,
+      }}
+    >
+      {(_, count, rawData) => {
+        const interactions = rawData.results.map(transformInteractionToListItem)
+        const sortOptions = [
+          {
+            name: 'Recently created',
+            value: '-date',
+          },
+          {
+            name: 'Oldest',
+            value: 'date',
+          },
+        ]
 
-            return (
-              <>
-                <H2 size={LEVEL_SIZE[3]}>Investment Interactions</H2>
-                <p>
-                  An interaction could be a meeting, call, email or another
-                  activity associated with this investment.
-                </p>
-                <CollectionList
-                  collectionName="interaction"
-                  items={interactions}
-                  count={count}
-                  isComplete={true}
-                  onPageClick={(currentPage) =>
-                    history.push({
-                      search: qs.stringify({
-                        ...parsedQueryString,
-                        page: currentPage,
-                      }),
-                    })
-                  }
-                  activePage={activePage}
-                  sortOptions={count ? sortOptions : null}
-                  addItemUrl={urls.investments.projects.interactions.createType(
-                    projectId,
-                    'investment',
-                    'interaction'
-                  )}
-                />
-              </>
-            )
-          }}
-        </InteractionCollectionResource>
-      )
-    }}
-  </Route>
-)
+        return (
+          <>
+            <H2 size={LEVEL_SIZE[3]}>Investment Interactions</H2>
+            <p>
+              An interaction could be a meeting, call, email or another activity
+              associated with this investment.
+            </p>
+            <CollectionList
+              collectionName="interaction"
+              items={interactions}
+              count={count}
+              isComplete={true}
+              onPageClick={(currentPage) =>
+                history.push({
+                  search: qs.stringify({
+                    ...parsedQueryString,
+                    page: currentPage,
+                  }),
+                })
+              }
+              activePage={activePage}
+              sortOptions={count ? sortOptions : null}
+              addItemUrl={urls.investments.projects.interactions.createType(
+                projectId,
+                'investment',
+                'interaction'
+              )}
+            />
+          </>
+        )
+      }}
+    </InteractionCollectionResource>
+  )
+}
 
 ProjectInteractions.propTypes = {
   projectId: PropTypes.string.isRequired,
--- src/client/modules/Investments/Projects/ProjectPropositions.jsx
+++ src/client/modules/Investments/Projects/ProjectPropositions.jsx
@@ -3,76 +3,72 @@ import PropTypes from 'prop-types'
 import { H2 } from 'govuk-react'
 import { LEVEL_SIZE } from '@govuk-react/constants'
 import qs from 'qs'
-import { Route } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
 
 import { CollectionList } from '../../../components'
 import { PropositionCollectionResource } from '../../../components/Resource'
 import urls from '../../../../lib/urls'
 import { transformPropositionToListItem } from './transformers'
 
-const ProjectPropositions = ({ projectId }) => (
-  <Route>
-    {({ history, location }) => {
-      const parsedQueryString = qs.parse(location.search.slice(1))
-      const activePage = parseInt(parsedQueryString.page, 10) || 1
-      return (
-        <PropositionCollectionResource
-          payload={{
-            investment_project_id: projectId,
-            limit: 10,
-            offset: activePage * 10 - 10,
-            sortby: parsedQueryString.sortby,
-          }}
-        >
-          {(_, count, rawData) => {
-            const propositions = rawData.results.map(
-              transformPropositionToListItem
-            )
-            const sortOptions = [
-              {
-                name: 'Recently created',
-                value: '-created_on',
-              },
-              {
-                name: 'Oldest',
-                value: 'created_on',
-              },
-            ]
+const ProjectPropositions = ({ projectId }) => {
+  const history = useHistory()
+  const location = useLocation()
+  const parsedQueryString = qs.parse(location.search.slice(1))
+  const activePage = parseInt(parsedQueryString.page, 10) || 1
+  return (
+    <PropositionCollectionResource
+      payload={{
+        investment_project_id: projectId,
+        limit: 10,
+        offset: activePage * 10 - 10,
+        sortby: parsedQueryString.sortby,
+      }}
+    >
+      {(_, count, rawData) => {
+        const propositions = rawData.results.map(transformPropositionToListItem)
+        const sortOptions = [
+          {
+            name: 'Recently created',
+            value: '-created_on',
+          },
+          {
+            name: 'Oldest',
+            value: 'created_on',
+          },
+        ]
 
-            return (
-              <>
-                <H2 size={LEVEL_SIZE[3]}>Investment Propositions</H2>
-                <p>
-                  You can record and manage propositions requested by investors
-                  and view the details of previous and current propositions.
-                </p>
-                <CollectionList
-                  collectionName="proposition"
-                  items={propositions}
-                  count={count}
-                  isComplete={true}
-                  onPageClick={(currentPage) =>
-                    history.push({
-                      search: qs.stringify({
-                        ...parsedQueryString,
-                        page: currentPage,
-                      }),
-                    })
-                  }
-                  activePage={activePage}
-                  sortOptions={count ? sortOptions : null}
-                  addItemUrl={urls.investments.projects.proposition.create(
-                    projectId
-                  )}
-                />
-              </>
-            )
-          }}
-        </PropositionCollectionResource>
-      )
-    }}
-  </Route>
-)
+        return (
+          <>
+            <H2 size={LEVEL_SIZE[3]}>Investment Propositions</H2>
+            <p>
+              You can record and manage propositions requested by investors and
+              view the details of previous and current propositions.
+            </p>
+            <CollectionList
+              collectionName="proposition"
+              items={propositions}
+              count={count}
+              isComplete={true}
+              onPageClick={(currentPage) =>
+                history.push({
+                  search: qs.stringify({
+                    ...parsedQueryString,
+                    page: currentPage,
+                  }),
+                })
+              }
+              activePage={activePage}
+              sortOptions={count ? sortOptions : null}
+              addItemUrl={urls.investments.projects.proposition.create(
+                projectId
+              )}
+            />
+          </>
+        )
+      }}
+    </PropositionCollectionResource>
+  )
+}
 
 ProjectPropositions.propTypes = {
   projectId: PropTypes.string.isRequired,
