3c332374b91cde8582ffb9f9a09f8800fc0dac38
Derek Baker <derek.baker89@gmail.com>
2023-09-25 14:30:33 +0100

    transform due date for api

--- src/client/modules/Investments/Projects/Tasks/transformers.js
+++ src/client/modules/Investments/Projects/Tasks/transformers.js
@@ -17,9 +17,33 @@ export const transformFormValuesForAPI = ({
   task: {
     title: taskTitle,
     description: taskDescription,
-    due_date: customDate ? transformValueForAPI(customDate) : taskDueDate,
+    due_date: getDueDate(taskDueDate, customDate),
     email_reminders_enabled: taskRemindersEnabled === OPTION_YES,
     reminder_days: taskReminderDays ? parseInt(taskReminderDays) : null,
     advisers: [],
   },
 })
+
+const getOneMonthFromNow = () => {
+  const date = new Date()
+  date.setMonth(date.getMonth() + 1)
+  return date.toISOString().slice(0, 10)
+}
+
+const getOneWeekFromNow = () => {
+  const date = new Date()
+  date.setDate(date.getDate() + 7)
+  return date.toISOString().slice(0, 10)
+}
+const getDueDate = (taskDueDate, customDate) => {
+  switch (taskDueDate) {
+    case 'custom':
+      return transformValueForAPI(customDate)
+    case 'month':
+      return getOneMonthFromNow()
+    case 'week':
+      return getOneWeekFromNow()
+    default:
+      null
+  }
+}
