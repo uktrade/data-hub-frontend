fea8de8b4a62b634384926310577269c88ac2bed
Marijn Kampf <marijn.kampf@digital.trade.gov.uk>
2023-09-26 16:36:32 +0100

    Update test

--- test/functional/cypress/specs/tasks/details-spec.js
+++ test/functional/cypress/specs/tasks/details-spec.js
@@ -5,7 +5,7 @@ const taskFixture = {
   id: 'efb5a10d-5117-4b90-abbf-d77ca379f58d',
   title: 'New task',
   description: 'test',
-  due_date: '2023-11-25',
+  due_date: '2023-12-25',
   reminder_days: 7,
   email_reminders_enabled: true,
   advisers: [
@@ -38,8 +38,8 @@ const taskFixture = {
 describe('View task details', () => {
   context('When visiting task details', () => {
     it('should display the h1 heading of New Page', () => {
-      cy.intercept('GET', '/api-proxy/v4/task/123', taskFixture)
-      cy.visit(urls.tasks.details('123'))
+      cy.intercept('GET', `/api-proxy/v4/task/${taskFixture.id}`, taskFixture)
+      cy.visit(urls.tasks.details(taskFixture.id))
       cy.get('h1').contains(taskFixture.title)
       assertSummaryTable({
         dataTest: 'task-details-table',
@@ -47,7 +47,7 @@ describe('View task details', () => {
         showEditLink: false,
         content: {
           Company: 'n/a',
-          'Date due': '25 November 2023',
+          'Date due': '25 December 2023',
           'Assigned to': 'Grace Gru',
           'Task description': 'test',
           'Reminders set': '7 days before due date',
