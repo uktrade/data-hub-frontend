16d587ff92e0102dd538025f94588bc1d1bf1687
Christopher Sunkel <christopher.sunkel@digital.trade.gov.uk>
2023-09-27 11:43:25 +0100

    Fixup! Update validator function

--- src/client/modules/Omis/validators.js
+++ src/client/modules/Omis/validators.js
@@ -1,15 +1,11 @@
 import { isDateInFuture } from '../../utils/date'
 import { transformDateObjectToDateString } from '../../transformers'
 
-export const validateIfDateInFuture = (value, field, { values }) => {
-  if (values && values.delivery_date) {
-    if (values.delivery_date.year != '') {
-      return isDateInFuture(
-        transformDateObjectToDateString(values.delivery_date)
-      )
-        ? null
-        : 'Delivery date of work must be in the future'
-    }
+export const validateIfDateInFuture = (values) => {
+  if (values?.year) {
+    return isDateInFuture(transformDateObjectToDateString(values))
+      ? null
+      : 'Delivery date of work must be in the future'
   }
   return null
 }
