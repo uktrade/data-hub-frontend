289f8ab298dbafbfbabfe45caf144b97cc3626ed
Chris Hopkins <chris.hopkins@madetech.com>
2023-09-29 15:04:12 +0100

    added redux actions for loading investment project

--- src/client/tasks.js
+++ src/client/tasks.js
@@ -96,6 +96,7 @@ import {
   TASK_GET_INVESTMENTS_PROJECTS_METADATA,
   TASK_EDIT_INVESTMENT_PROJECT_STATUS,
   TASK_UPDATE_INVESTMENT_PROJECT_STAGE,
+  TASK_GET_INVESTMENT_PROJECT,
 } from './modules/Investments/Projects/state'
 import * as investmentProjectTasks from './modules/Investments/Projects/tasks'
 
@@ -538,4 +539,5 @@ export const tasks = {
   [TASK_DELETE_PROJECT_DOCUMENT]: deleteProjectDocument,
   [TASK_EDIT_OMIS_QUOTE_INFORMATION]: updateOrder,
   [TASK_EDIT_OMIS_INTERNAL_INFORMATION]: updateOrder,
+  [TASK_GET_INVESTMENT_PROJECT]: investmentProjectTasks.getInvestmentProject,
 }
