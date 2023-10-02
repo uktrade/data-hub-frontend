d6bb2dcca3bbdaebb013d41be14bac964af42c3f
Claudia Gonzalez-Casales <claudia.gonzalescasales@digital.trade.gov.uk>
2023-10-02 12:02:57 +0100

    Added route and url for edit task

--- src/client/modules/Investments/Projects/Tasks/tasks.js
+++ src/client/modules/Investments/Projects/Tasks/tasks.js
@@ -1,12 +1,24 @@
 import { apiProxyAxios } from '../../../../components/Task/utils'
-import { transformFormValuesForAPI } from './transformers'
+import {
+  transformFormValuesForAPI,
+  transformFormValuesForAPIEdit,
+} from './transformers'
 
 export const saveInvestmentProjectTask = ({
   values,
   investmentProject,
   currentAdviserId,
-}) =>
-  apiProxyAxios.post(
-    '/v4/investmentprojecttask',
-    transformFormValuesForAPI(values, investmentProject, currentAdviserId)
-  )
+  task,
+}) => {
+  if (task) {
+    return apiProxyAxios.patch(
+      `/v4/task/${task.id}`,
+      transformFormValuesForAPIEdit(values, currentAdviserId)
+    )
+  } else {
+    return apiProxyAxios.post(
+      '/v4/investmentprojecttask',
+      transformFormValuesForAPI(values, investmentProject, currentAdviserId)
+    )
+  }
+}
