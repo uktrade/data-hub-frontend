d0247fafbd52278ff344c4bf271b829335481e71
Dean Elliott <dean.elliott@digital.trade.gov.uk>
2023-10-10 14:45:35 +0100

    Move some send referral functional tests into component tests (#6146)
    * Move some send referral functional tests into component tests
    
    * Add data test for finding elements in test
    
    * Add referral external link to urls module

--- test/functional/cypress/specs/companies/referrals/send-referral-spec.js
+++ test/functional/cypress/specs/companies/referrals/send-referral-spec.js
@@ -1,13 +1,7 @@
 const urls = require('../../../../../../src/lib/urls')
 const fixtures = require('../../../fixtures')
 const selectors = require('../../../../../selectors')
-const {
-  assertLocalHeader,
-  assertBreadcrumbs,
-  assertFieldTypeahead,
-  assertFieldTextarea,
-  assertFlashMessage,
-} = require('../../../support/assertions')
+const { assertFlashMessage } = require('../../../support/assertions')
 
 const selectTypeahead = (field, input) =>
   cy.get(field).within(() => {
@@ -39,116 +33,6 @@ describe('Send a referral form', () => {
       cy.visit(urls.companies.referrals.send(fixtures.company.withContacts.id))
     })
 
-    context('when viewing the "send referral" form', () => {
-      it('should display breadcrumbs', () => {
-        assertBreadcrumbs({
-          Home: urls.dashboard.index(),
-          Companies: urls.companies.index(),
-          'Venus Ltd': urls.companies.detail(fixtures.company.withContacts.id),
-          'Send a referral': null,
-        })
-      })
-
-      it('should display the header', () => {
-        assertLocalHeader('Send a referral')
-      })
-
-      it('should display guidance on sending a referral', () => {
-        cy.get('h2')
-          .contains('When to send a referral')
-          .next()
-          .should(
-            'have.text',
-            'Referrals are for when you want to ask another DBT advisor to help out an account you are working on.Read more guidance here (opens in new tab)'
-          )
-          .find('a')
-          .should(
-            'have.attr',
-            'href',
-            'https://data-services-help.trade.gov.uk/data-hub/updates/announcements/improving-collaboration-internal-referrals/'
-          )
-      })
-
-      it('should display the headings and four fields', () => {
-        cy.contains('Who do you want to refer this company to?').should(
-          'be.visible'
-        )
-
-        cy.get(selectors.sendReferral.adviserField).then((element) => {
-          return assertFieldTypeahead({
-            element,
-            label: 'Adviser',
-            placeholder: 'Search for an adviser',
-            hint: "This can be an adviser at post, a sector specialist or an international trade advisor. If you're not sure, you can find the right team and person on Digital Workspace (opens in new tab).",
-          })
-        })
-
-        cy.contains('Referral notes').should('be.visible')
-
-        enterSubjectDefault()
-
-        cy.get(selectors.sendReferral.notesField).then((element) => {
-          return assertFieldTextarea({
-            element,
-            label: 'Notes',
-            hint: "Include reasons you're referring this company and any specific opportunities.",
-          })
-        })
-
-        cy.get(selectors.sendReferral.contactField).then((element) => {
-          return assertFieldTypeahead({
-            element,
-            label: 'Company contact (optional)',
-            placeholder: 'Select a contact',
-            hint: 'Who should the recipient of the referral talk to? If the contact you are looking for is not listed you can add a new contact.',
-          })
-        })
-      })
-
-      it('should display "Continue" button', () => {
-        cy.contains('button', 'Continue').should('be.visible')
-      })
-
-      it('should display "Cancel" link', () => {
-        cy.contains('a', 'Cancel').should('be.visible')
-      })
-    })
-
-    context('When the "Cancel" link is clicked', () => {
-      it('should return to the company page', () => {
-        cy.contains('a', 'Cancel').click()
-        cy.url().should(
-          'contain',
-          urls.companies.overview.index(fixtures.company.withContacts.id)
-        )
-      })
-    })
-
-    context(
-      'when "Continue" button is clicked without filling in mandatory fields',
-      () => {
-        it('should display two error messages', () => {
-          cy.contains('button', 'Continue').click()
-          cy.get('form')
-            .should('contain', 'Select an adviser for the referral')
-            .and('contain', 'Enter a subject for the referral')
-            .and('contain', 'Enter notes for the referral')
-        })
-      }
-    )
-
-    context(
-      'when "Continue" button is clicked without chosing an adviser',
-      () => {
-        it('should display error message', () => {
-          enterSubjectDefault()
-          enterNotesDefault()
-          cy.contains('button', 'Continue').click()
-          cy.get('form').should('contain', 'Select an adviser for the referral')
-        })
-      }
-    )
-
     context(
       'when "Continue" button is clicked without specifying a subject',
       () => {
@@ -172,24 +56,6 @@ describe('Send a referral form', () => {
           enterSubjectDefault()
           cy.contains('button', 'Continue').click()
           cy.get('form').should('contain', 'Enter notes for the referral')
-        })
-      }
-    )
-
-    context(
-      'when more than 255 characters are entered in the subject field',
-      () => {
-        it('should display error message', () => {
-          cy.get(selectors.sendReferral.subjectField)
-            .click()
-            .type(
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec interdum velit. Nulla facilisi. In ultricies sed ex at malesuada. Proin quis faucibus felis, iaculis malesuada sem. Maecenas semper elit magna, tincidunt consectetur risus volutpat at. Sed'
-            )
-          cy.contains('button', 'Continue').click()
-          cy.get('form').should(
-            'contain',
-            'Enter a subject for the referral (Max 255 characters)'
-          )
         })
       }
     )
@@ -227,22 +93,6 @@ describe('Send a referral form', () => {
       })
     })
   })
-
-  context(
-    'when "Continue" button is clicked with just mandatory fields filled in',
-    () => {
-      it('should display confirmation component', () => {
-        cy.visit(
-          urls.companies.referrals.send(fixtures.company.withContacts.id)
-        )
-        enterAdviserDefault()
-        enterSubjectDefault()
-        enterNotesDefault()
-        cy.contains('button', 'Continue').click()
-        cy.contains('Check referral details').should('be.visible')
-      })
-    }
-  )
 
   context(
     'when "Continue" button is clicked after all fields filled in',
