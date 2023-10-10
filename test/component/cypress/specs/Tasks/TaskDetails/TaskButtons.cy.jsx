52f3919ac7bcfc88014acebb90fbbedd3be82405
Christopher Sunkel <christopher.sunkel@digital.trade.gov.uk>
2023-10-10 16:16:03 +0100

    Tidy up component test folder

---
+++ test/component/cypress/specs/Tasks/TaskDetails/TaskButtons.cy.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import { taskWithInvestmentProjectFaker } from '../../../../../functional/cypress/fakers/task'
+import DataHubProvider from '../../provider'
+import TaskButtons from '../../../../../../src/client/modules/Tasks/TaskDetails/TaskButtons'
+
+describe('Task buttons', () => {
+  const Component = (props) => (
+    <DataHubProvider>
+      <TaskButtons {...props} />
+    </DataHubProvider>
+  )
+
+  context('When a task is not completed', () => {
+    const investmentProjectTask = taskWithInvestmentProjectFaker()
+    const company =
+      investmentProjectTask.investmentProjectTask.investmentProject
+        .investorCompany
+
+    it('should show the Mark as complete button', () => {
+      cy.mount(<Component task={investmentProjectTask} company={company} />)
+      cy.get('[data-test="submit-button"]').should(
+        'contain.text',
+        'Mark as complete'
+      )
+    })
+  })
+
+  context('When a task is completed', () => {
+    const investmentProjectTask = taskWithInvestmentProjectFaker({
+      archived: true,
+    })
+    const company =
+      investmentProjectTask.investmentProjectTask.investmentProject
+        .investorCompany
+
+    it('should not show the Mark as complete button', () => {
+      cy.mount(<Component task={investmentProjectTask} company={company} />)
+      cy.get('[data-test="submit-button"]').should('not.exist')
+    })
+  })
+})
---
+++ test/component/cypress/specs/components/Form/FieldDate.cy.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+import { Form } from '../../../../../../src/client/components'
+import FieldDate from '../../../../../../src/client/components/Form/elements/FieldDate'
+import DataHubProvider from '../../provider'
+import {
+  assertFieldError,
+  assertNotExists,
+} from '../../../../../functional/cypress/support/assertions'
+
+describe('FieldDate', () => {
+  const Component = (props) => (
+    <DataHubProvider>
+      <Form
+        id="export-form"
+        analyticsFormName="export-form"
+        cancelRedirectTo={() => '/'}
+        submissionTaskName="EMPTY"
+      >
+        <FieldDate name="test-date" {...props} />
+      </Form>
+    </DataHubProvider>
+  )
+
+  beforeEach(() => {
+    cy.mount(<Component />)
+  })
+
+  context('when entering a 2 digit value for year', () => {
+    it('should display a validation message', () => {
+      cy.get('[data-test=test_date-day]').type('01').blur()
+      cy.get('[data-test=test_date-month]').type('01').blur()
+      cy.get('[data-test=test_date-year]').type('25').blur()
+
+      cy.get('[data-test=submit-button]').click()
+
+      assertFieldError(
+        cy.get('[data-test=field-test-date]'),
+        'Enter a year as 4 digits',
+        false
+      )
+    })
+  })
+
+  context('when entering a 4 digit value for year', () => {
+    it('should not display a validation message', () => {
+      cy.get('[data-test=test_date-day]').type('04').blur()
+      cy.get('[data-test=test_date-month]').type('09').blur()
+      cy.get('[data-test=test_date-year]').type('2028').blur()
+
+      cy.get('[data-test=submit-button]').click()
+
+      assertNotExists('[data-test=field-test-date-error]')
+    })
+  })
+})
