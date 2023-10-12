cfaacd5ffafd25c12790d93593dca371d9980201
Paul Gain <paul@javexconsulting.com>
2023-10-12 09:05:10 +0100

    Add export wins tab navigation

--- src/client/routes.js
+++ src/client/routes.js
@@ -16,6 +16,8 @@ import {
 } from './modules/ExportPipeline/ExportForm'
 import ExportFormDelete from './modules/ExportPipeline/ExportDelete'
 import ExportDetails from './modules/ExportPipeline/ExportDetails'
+import ExportWins from './modules/ExportWins/'
+import ExportWinsRedirect from './modules/ExportWins/Redirect'
 import CompanyHierarchy from './modules/Companies/CompanyHierarchy'
 import CompanyTree from './modules/Companies/CompanyHierarchy/CompanyTree'
 import Community from './modules/Community'
@@ -220,6 +222,23 @@ const routes = {
       path: '/export/:exportId/delete',
       module: 'datahub:companies',
       component: ExportFormDelete,
+    },
+  ],
+  exportWins: [
+    {
+      path: '/exportwins/unconfirmed',
+      module: 'datahub:companies',
+      component: ExportWins,
+    },
+    {
+      path: '/exportwins/confirmed',
+      module: 'datahub:companies',
+      component: ExportWins,
+    },
+    {
+      path: '/exportwins',
+      module: 'datahub:companies',
+      component: ExportWinsRedirect,
     },
   ],
   investments: [
