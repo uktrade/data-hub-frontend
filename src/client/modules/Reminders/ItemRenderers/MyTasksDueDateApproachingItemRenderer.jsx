b55c030083622f8c8c38d0894b525bca9c136879
Caitlin Barnard <caitlin@madetech.com>
2023-10-18 16:22:32 +0100

    Render tasks due date approaching items in list

--- src/client/modules/Reminders/ItemRenderers/MyTasksDueDateApproachingItemRenderer.jsx
+++ src/client/modules/Reminders/ItemRenderers/MyTasksDueDateApproachingItemRenderer.jsx
@@ -1,37 +1,48 @@
 import React from 'react'
-// import { SPACING } from '@govuk-react/constants'
+import { SPACING } from '@govuk-react/constants'
 import GridRow from '@govuk-react/grid-row'
 import GridCol from '@govuk-react/grid-col'
-// import { Link } from 'govuk-react'
-// import styled from 'styled-components'
+import { Link } from 'govuk-react'
+import styled from 'styled-components'
 
-import { ListItem, ItemFooter } from './styled'
-// import { DATE_DAY_LONG_FORMAT } from '../../../../common/constants'
-// import { format } from '../../../utils/date'
-// import urls from '../../../../lib/urls'
+import { ListItem, ItemHeader } from './styled'
+import { DATE_LONG_FORMAT_1 } from '../../../../common/constants'
+import { format } from '../../../utils/date'
+import urls from '../../../../lib/urls'
+import { GREY_1 } from '../../../utils/colours'
 
-// const ItemContent = styled('div')({
-//   marginBottom: SPACING.SCALE_2,
-//   marginTop: SPACING.SCALE_2,
-// })
+const ItemContent = styled('div')({
+  marginBottom: SPACING.SCALE_2,
+  marginTop: SPACING.SCALE_2,
+})
+
+const ItemHint = styled('span')({
+  color: GREY_1,
+})
 
 const MyTasksDueDateApproachingItemRenderer = (item) => (
   <ListItem key={item.id} data-test="reminders-list-item">
     <GridRow>
       <GridCol>
-        {/* <ItemHeader data-test="item-header">
-          Due {format(item.deadline, DATE_DAY_LONG_FORMAT)}
+        <ItemHeader data-test="item-header">
+          Received {format(item.created_on, DATE_LONG_FORMAT_1)}
+          <br />
+          <Link href={`${urls.tasks.details(item.id)}`}>{item.event}</Link>
         </ItemHeader>
         <ItemContent data-test="item-content">
-          <Link
-            href={`${urls.investments.projects.propositions(
-              item.investment_project.id
-            )}`}
-          >
-            {item.name}
-          </Link>
-        </ItemContent> */}
-        <ItemFooter data-test="item-footer">Test</ItemFooter>
+          <ul>
+            <li>
+              <ItemHint>Company:</ItemHint>{' '}
+              <Link href={`${urls.companies.detail(item.company.id)}`}>
+                {item.company.name}
+              </Link>
+            </li>
+            <li>
+              <ItemHint>Date due:</ItemHint>{' '}
+              {format(item.due_date, DATE_LONG_FORMAT_1)}
+            </li>
+          </ul>
+        </ItemContent>
       </GridCol>
     </GridRow>
   </ListItem>
