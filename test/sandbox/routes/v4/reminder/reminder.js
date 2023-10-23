d6daffe8c6ef00ab8e9624b9acbbf7f8226313b1
Caitlin Barnard <caitlin@madetech.com>
2023-10-23 13:45:27 +0100

    Update sandbox to use list

--- test/sandbox/routes/v4/reminder/reminder.js
+++ test/sandbox/routes/v4/reminder/reminder.js
@@ -1,10 +1,57 @@
+const { faker } = require('@faker-js/faker')
+
+const { addDays } = require('../../../../../src/client/utils/date')
 var summary = require('../../../fixtures/v4/reminder/summary.json')
-var myTasksDueDateApproaching = require('../../../fixtures/v4/reminder/my-tasks-due-date-approaching.json')
+
+const myTasksDueDateApproachingReminderFaker = (overrides = {}) => ({
+  id: faker.string.uuid(),
+  created_on: faker.date.between({
+    from: addDays(new Date(), 0),
+    to: addDays(new Date(), 365),
+  }),
+  event: faker.lorem.words(),
+  investment_project_task: {
+    id: faker.string.uuid(),
+    investment_project: {
+      id: faker.string.uuid(),
+      name: faker.lorem.words(),
+      project_code: 'DHP-12345678',
+      investor_company: {
+        id: faker.string.uuid(),
+        name: faker.company.name(),
+      },
+    },
+    task: {
+      id: faker.string.uuid(),
+      due_date: faker.date.between({
+        from: addDays(new Date(), 0),
+        to: addDays(new Date(), 365),
+      }),
+    },
+  },
+  ...overrides,
+})
+
+const myTasksDueDateApproachingReminderListFaker = (
+  length = 10,
+  overrides = {}
+) => {
+  let list = []
+  for (let i = 0; i < length; i++) {
+    list.push(myTasksDueDateApproachingReminderFaker(overrides))
+  }
+  return list
+}
 
 exports.summary = function (req, res) {
   res.json(summary)
 }
 
 exports.myTasksDueDateApproaching = function (req, res) {
-  res.json(myTasksDueDateApproaching)
+  res.json({
+    count: 1,
+    next: null,
+    previous: null,
+    results: myTasksDueDateApproachingReminderListFaker(),
+  })
 }
