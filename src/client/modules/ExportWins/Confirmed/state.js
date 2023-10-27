b03d999194945d6d65022f21abf4d18e0cde9807
Paul Gain <paul@javexconsulting.com>
2023-10-27 11:05:09 +0100

    Add Export Wins scaffolding (#6189)

---
+++ src/client/modules/ExportWins/Confirmed/state.js
@@ -0,0 +1,3 @@
+export const TASK_GET_EXPORT_WINS_CONFIRMED = 'TASK_GET_EXPORT_WINS_CONFIRMED'
+export const ID = 'exportWinsConfirmed'
+export const state2props = (state) => state[ID]
---
+++ src/client/modules/ExportWins/Unconfirmed/state.js
@@ -0,0 +1,4 @@
+export const TASK_GET_EXPORT_WINS_UNCONFIRMED =
+  'TASK_GET_EXPORT_WINS_UNCONFIRMED'
+export const ID = 'exportWinsUnconfirmed'
+export const state2props = (state) => state[ID]
