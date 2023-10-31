2135cc6f82eaddf9c99c46d3e4615eda04ccd910
Christopher Sunkel <christopher.sunkel@digital.trade.gov.uk>
2023-10-31 12:13:16 +0000

    Remove legacy code

--- src/apps/omis/apps/edit/controllers/index.js
+++ src/apps/omis/apps/edit/controllers/index.js
@@ -1,11 +1,9 @@
-const EditAssigneeTimeController = require('./assignee-time')
 const EditContactController = require('./contact')
 const editHandler = require('./edit-handler')
 const editLeadAssignee = require('./edit-lead-assignee')
 const editRedirect = require('./edit-redirect')
 
 module.exports = {
-  EditAssigneeTimeController,
   EditContactController,
   editHandler,
   editLeadAssignee,
