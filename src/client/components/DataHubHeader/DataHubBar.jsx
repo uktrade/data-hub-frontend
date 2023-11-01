aee0d18bba88d5e13235c4f066c8cd8d1d9e8406
Derek Baker <derek.baker89@gmail.com>
2023-11-01 12:26:58 +0000

    Improve margin

--- src/client/components/DataHubHeader/DataHubBar.jsx
+++ src/client/components/DataHubHeader/DataHubBar.jsx
@@ -94,7 +94,7 @@ const BetaTag = styled.strong({
 const NavigationLink = styled.a(({ hasFeatureGroup }) => ({
   color: WHITE,
   display: 'none',
-  marginLeft: '20px',
+  marginRight: '20px',
   '-webkit-font-smoothing': 'antialiased',
   [MEDIA_QUERIES.TABLET]: {
     display: 'block',
