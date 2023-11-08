8b2de6491ed8468003e246bf2436d8811150d7cf
Chris Hopkins <102232401+chopkinsmade@users.noreply.github.com>
2023-11-08 11:10:41 +0000

    Cls2 539 tasks mvp add back button (#6226)
    * WIP: Back button and tests added
    
    Margin left still needs adding
    
    * Update TaskButtons.jsx
    
    * Added the margin around the buttons
    
    ---------
    
    Co-authored-by: Paul Wheatcroft <paul.wheatcroft@digital.trade.gov.uk>

--- src/client/modules/Tasks/TaskDetails/TaskButtons.jsx
+++ src/client/modules/Tasks/TaskDetails/TaskButtons.jsx
@@ -2,59 +2,73 @@ import React from 'react'
 import Link from '@govuk-react/link'
 import Button from '@govuk-react/button'
 import { connect } from 'react-redux'
-import { GridCol, GridRow } from 'govuk-react'
+import { GridRow } from 'govuk-react'
+
+import { SPACING } from '@govuk-react/constants'
+
 import styled from 'styled-components'
 
-import { Form, FormLayout } from '../../../components'
+import { Form } from '../../../components'
 
 import urls from '../../../../lib/urls'
 import { TASK_ARCHIVE_TASK, buttonState2props } from './state'
-import { FORM_LAYOUT } from '../../../../common/constants'
 import { GREY_3, TEXT_COLOUR } from '../../../utils/colours'
 
-const StyledGridCol = styled(GridCol)`
-  margin-left: -75px;
+const ButtonWrapper = styled.div`
+  * {
+    margin-left: ${SPACING.SCALE_4};
+  }
 `
 
 export const TaskButtons = ({ task, editUrl }) => (
-  <GridRow>
-    <GridCol setWidth={'28%'}>
+  <>
+    <GridRow>
       {!task.archived && (
-        <FormLayout setWidth={FORM_LAYOUT.THREE_QUARTERS}>
-          <Form
-            id="archive-task-form"
-            analyticsFormName="archiveTaskForm"
-            redirectTo={() =>
-              urls.investments.projects.tasks.index(
-                task.investmentProjectTask.investmentProject.id
-              )
-            }
-            cancelRedirectTo={false}
-            submissionTaskName={TASK_ARCHIVE_TASK}
-            transformPayload={() => ({
-              taskId: task.id,
-            })}
-            flashMessage={() => 'Task marked as complete'}
-            submitButtonLabel="Mark as complete"
-            initialValues={task}
-          >
-            {() => <></>}
-          </Form>
-        </FormLayout>
+        <Form
+          id="archive-task-form"
+          analyticsFormName="archiveTaskForm"
+          redirectTo={() =>
+            urls.investments.projects.tasks.index(
+              task.investmentProjectTask.investmentProject.id
+            )
+          }
+          cancelRedirectTo={false}
+          submissionTaskName={TASK_ARCHIVE_TASK}
+          transformPayload={() => ({
+            taskId: task.id,
+          })}
+          flashMessage={() => 'Task marked as complete'}
+          submitButtonLabel="Mark as complete"
+          initialValues={task}
+        >
+          {() => <></>}
+        </Form>
       )}
-    </GridCol>
-    <StyledGridCol setWidth={'20%'}>
-      <Button
-        buttonColour={GREY_3}
-        buttonTextColour={TEXT_COLOUR}
-        as={Link}
-        href={editUrl}
-        data-test="edit-form-button"
-      >
-        Edit
-      </Button>
-    </StyledGridCol>
-  </GridRow>
+      <ButtonWrapper>
+        <Button
+          buttonColour={GREY_3}
+          buttonTextColour={TEXT_COLOUR}
+          as={Link}
+          href={editUrl}
+          data-test="edit-form-button"
+        >
+          Edit
+        </Button>
+
+        <Button
+          buttonColour={GREY_3}
+          buttonTextColour={TEXT_COLOUR}
+          as={Link}
+          href={urls.investments.projects.tasks.index(
+            task.investmentProjectTask.investmentProject.id
+          )} //TODO - when the my tasks dashboard is added this url needs to be more intelligent as there will be multiple entry points to this page
+          data-test="back-button"
+        >
+          Back
+        </Button>
+      </ButtonWrapper>
+    </GridRow>
+  </>
 )
 
 export default connect(buttonState2props)(TaskButtons)
