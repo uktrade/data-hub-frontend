ac86351c351bb9df1cc2e3184afd35873ffe118e
Christopher Sunkel <christopher.sunkel@digital.trade.gov.uk>
2023-11-09 17:56:52 +0000

    Reactify subscribers table

---
+++ src/client/modules/Omis/WorkOrderTables/SubscribersTable.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Link } from 'govuk-react'
+
+import { SummaryTable } from '../../../components'
+import { isOrderActive } from '../transformers'
+import urls from '../../../../lib/urls'
+import WideSummaryTableRow from '../../../../apps/companies/apps/business-details/client/WideSummaryTableRow'
+
+const buildSubscriberRows = (subscribers) =>
+  subscribers.map(({ name, ditTeam }) => (
+    <WideSummaryTableRow>
+      {name}
+      {ditTeam?.ukRegion ? ', ' + ditTeam.ukRegion.name : ''}
+    </WideSummaryTableRow>
+  ))
+
+const SubscribersTable = ({ subscribers, order }) => (
+  <SummaryTable
+    caption="Advisers in the UK"
+    data-test="subscribers-table"
+    actions={
+      isOrderActive(order) && (
+        <Link
+          href={urls.omis.edit.subscribers(order.id)}
+          aria-label="Add or remove advisers in the UK"
+          data-test="add-subscribers-link"
+          noVisitedState={true}
+        >
+          Add or remove
+        </Link>
+      )
+    }
+  >
+    {subscribers.length > 0 ? (
+      buildSubscriberRows(subscribers)
+    ) : (
+      <WideSummaryTableRow>No advisers added</WideSummaryTableRow>
+    )}
+  </SummaryTable>
+)
+
+export default SubscribersTable
