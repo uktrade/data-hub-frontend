5894a66d05d690838b0287df46040787850ccd20
Derek Baker <derek.baker89@gmail.com>
2023-12-04 16:43:59 +0000

    FIX: 'other' filter

--- src/client/components/Dashboard/my-tasks/tasks.js
+++ src/client/components/Dashboard/my-tasks/tasks.js
@@ -1,12 +1,12 @@
 import { apiProxyAxios } from '../../Task/utils'
 
-export const getMyTasks = ({ adviser, sortby = 'due_date:asc' }) =>
+export const getMyTasks = ({ adviser, created_by, sortby = 'due_date:asc' }) =>
   apiProxyAxios
     .post('/v4/search/task', {
       limit: 50,
       offset: 0,
-      created_by: adviser.id,
-      adviser: [adviser.id],
+      created_by: created_by,
+      adviser: adviser,
       sortby,
     })
     .then(({ data }) => data)
