91451251fc3006843504fe4b3164c420dcae0002
Derek Baker <derek.baker89@gmail.com>
2023-12-04 15:36:52 +0000

    WIP: connecting filter to state

---
+++ src/client/components/Dashboard/my-tasks/constants.js
@@ -0,0 +1,17 @@
+export const CREATED_BY_OPTIONS = [
+  {
+    label: 'Me',
+    value: 'me',
+  },
+  {
+    label: 'Others',
+    value: 'others',
+  },
+]
+
+export const SHOW_ALL_OPTION = {
+  label: 'Show all',
+  value: 'all-statuses',
+}
+
+export const STATUS_LIST_OPTIONS = [SHOW_ALL_OPTION, ...CREATED_BY_OPTIONS]
--- src/client/components/Dashboard/my-tasks/state.js
+++ src/client/components/Dashboard/my-tasks/state.js
@@ -1,4 +1,40 @@
+import { omitBy, isEmpty } from 'lodash'
+
+import { getQueryParamsFromLocation } from '../../../../client/utils/url'
+import { parsePage } from '../../../../client/utils/pagination'
+
+import { CREATED_BY_OPTIONS, SHOW_ALL_OPTION } from './constants'
+
 export const ID = 'getMyTasks'
 export const TASK_GET_MY_TASKS = 'TASK_GET_MY_TASKS'
 
-export const state2props = (state) => state[ID]
+const areFiltersActive = (queryParams) => {
+  const filters = omitBy(
+    queryParams,
+    (filterValue, filterName) =>
+      filterValue === 'all-statuses' || filterName === 'page'
+  )
+
+  return !isEmpty(filters)
+}
+
+// export const state2props = (state) => state[ID]
+
+export const state2props = ({ router }) => {
+  const queryParams = getQueryParamsFromLocation(router.location)
+  return {
+    payload: {
+      ...queryParams,
+      page: parsePage(queryParams.page),
+    },
+    filters: {
+      areActive: areFiltersActive(queryParams),
+      status: {
+        options: SHOW_ALL_OPTION,
+      },
+      createdBy: {
+        options: CREATED_BY_OPTIONS,
+      },
+    },
+  }
+}
