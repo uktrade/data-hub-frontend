ff2fb9569608f3c4af3edbba96e60043dc753570
Peter Hudec <peterhudec@peterhudec.com>
2023-12-06 16:53:55 +0000

    Added the InlineIcon component

---
+++ src/client/components/InlineIcon/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const InlineIcon = styled.span`
+  height: 2ex;
+  width: 2ex;
+  vertical-align: -10%;
+  display: inline-block;
+`
+
+export default InlineIcon
