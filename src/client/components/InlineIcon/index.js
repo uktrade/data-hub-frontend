32add07651b0db6330c696298919956aedb87740
Peter Hudec <peterhudec@peterhudec.com>
2023-12-07 11:44:37 +0000

    Added docstring to InlineIcon component

--- src/client/components/InlineIcon/index.js
+++ src/client/components/InlineIcon/index.js
@@ -1,5 +1,18 @@
 import styled from 'styled-components'
 
+/**
+ * @function InlineIcon
+ * @description
+ * Allows displaying `@govuk-react/icons` icons inline.
+ * @param {Object} props
+ * @param {React.ReactNode} props.children - A `@govuk-react/icons` element.
+ * @example
+ * import { IconImportant } from '@govuk-react/icons'
+ *
+ * <InlineIcon>
+ *   <IconImportant/>
+ * </InlineIcon>
+ */
 const InlineIcon = styled.span`
   height: 2ex;
   width: 2ex;
