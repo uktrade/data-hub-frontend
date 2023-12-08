cbb469acdc30c9aae1b7c140634987ec0971b4a9
Paul Gain <paul@javexconsulting.com>
2023-12-08 17:15:51 +0000

    Add the 'check before sending' form step

--- src/client/modules/ExportWins/Form/CheckBeforeSending.jsx
+++ src/client/modules/ExportWins/Form/CheckBeforeSending.jsx
@@ -1,32 +1,189 @@
 import React from 'react'
+import styled from 'styled-components'
 
+import { Step, ButtonLink, FieldInput, SummaryTable } from '../../../components'
 import { useFormContext } from '../../../../client/components/Form/hooks'
-import { Step, ButtonLink, FieldInput } from '../../../components'
 import { steps } from './constants'
+import {
+  transformTeamsAndAdvisers,
+  transformGoodsAndServices,
+  transformCustomerConfidential,
+} from './transformers'
+
+const StyledButtonLink = styled(ButtonLink)({
+  margin: 0,
+})
+
+const StyledContributingTeamsAndAdvisers = styled('div')({
+  fontWeight: 'bold',
+})
+
+const StyledOrderedList = styled('ol')({
+  marginTop: 10,
+})
 
 const CheckBeforeSendingStep = () => {
-  // eslint-disable-next-line no-unused-vars
   const { values, goToStep } = useFormContext()
+  const props = {
+    values,
+    goToStep,
+  }
   return (
     <Step name={steps.CHECK_BEFORE_SENDING}>
       <h1>Check before sending</h1>
+      <OfficerDetailsTable {...props} />
+      <CreditForThisWinTable {...props} />
+      <CustomerDetailsTable {...props} />
+      <WinDetailsTable {...props} />
+      <SupportGivenTable {...props} />
       <FieldInput name="test4" label="Test4" type="text" />
-      <ButtonLink
+    </Step>
+  )
+}
+
+const OfficerDetailsTable = ({ values, goToStep }) => (
+  <SummaryTable
+    caption="Officer details"
+    data-test="officer-details"
+    actions={
+      <StyledButtonLink
         onClick={() => {
           goToStep(steps.OFFICER_DETAILS)
         }}
       >
-        Change Officer details
-      </ButtonLink>
-      <ButtonLink
-        onClick={() => {
-          goToStep(steps.CREDIT_FOR_THIS_WIN)
-        }}
-      >
-        Change credit for this win
-      </ButtonLink>
-    </Step>
+        Change
+      </StyledButtonLink>
+    }
+  >
+    <SummaryTable.Row heading="Lead Officer name">
+      {values?.adviser?.label}
+    </SummaryTable.Row>
+    <SummaryTable.Row heading="Team type">{values.team_type}</SummaryTable.Row>
+    <SummaryTable.Row heading="HQ Team, region or post">
+      {values.hq_team_region_or_post}
+    </SummaryTable.Row>
+    <SummaryTable.ListRow
+      heading="Team members (optional)"
+      value={values.team_members}
+      emptyValue="Not set"
+    />
+  </SummaryTable>
+)
+
+const ContributingTeamsAndAdvisers = ({ teamsAndAdvisors }) => (
+  <>
+    <StyledContributingTeamsAndAdvisers>
+      Contributing teams and advisers
+    </StyledContributingTeamsAndAdvisers>
+    {teamsAndAdvisors.map((contributingTeam, index) => (
+      <StyledOrderedList index={index} length={teamsAndAdvisors.length}>
+        <li>Contributing officer: {contributingTeam.officer}</li>
+        <li>Team type: {contributingTeam.teamType}</li>
+        <li>HQ team, region or post: {contributingTeam.hqTeamRegionOrPost}</li>
+      </StyledOrderedList>
+    ))}
+  </>
+)
+
+const CreditForThisWinTable = ({ values, goToStep }) => {
+  const teamsAndAdvisors = transformTeamsAndAdvisers(values)
+  return (
+    <SummaryTable
+      caption="Credit for this win"
+      data-test="credit-for-this-win"
+      actions={
+        <StyledButtonLink
+          onClick={() => {
+            goToStep(steps.CREDIT_FOR_THIS_WIN)
+          }}
+        >
+          Change
+        </StyledButtonLink>
+      }
+    >
+      <SummaryTable.Row heading="Did any other teams help with this win?">
+        {values?.credit_for_win === 'no' ? 'No' : 'Yes'}
+        {values?.credit_for_win === 'yes' && (
+          <ContributingTeamsAndAdvisers teamsAndAdvisors={teamsAndAdvisors} />
+        )}
+      </SummaryTable.Row>
+    </SummaryTable>
   )
 }
+
+const CustomerDetailsTable = ({ values, goToStep }) => (
+  <SummaryTable
+    caption="Customer details"
+    data-test="customer-details"
+    actions={
+      <StyledButtonLink
+        onClick={() => {
+          goToStep(steps.CUSTOMER_DETAILS)
+        }}
+      >
+        Change
+      </StyledButtonLink>
+    }
+  >
+    <SummaryTable.Row heading="Contact name">
+      {values.contact?.label}
+    </SummaryTable.Row>
+    <SummaryTable.Row heading="HQ location">
+      {values.uk_region}
+    </SummaryTable.Row>
+    <SummaryTable.Row heading="Export potential">
+      {values.business_potential}
+    </SummaryTable.Row>
+    <SummaryTable.Row heading="Export experience">
+      {values.export_experience}
+    </SummaryTable.Row>
+  </SummaryTable>
+)
+
+const WinDetailsTable = ({ values, goToStep }) => (
+  <SummaryTable
+    caption="Win details"
+    data-test="win-details"
+    actions={
+      <StyledButtonLink
+        onClick={() => {
+          goToStep(steps.WIN_DETAILS)
+        }}
+      >
+        Change
+      </StyledButtonLink>
+    }
+  >
+    <SummaryTable.Row heading="Destination">
+      {values.destination_country?.label}
+    </SummaryTable.Row>
+    <SummaryTable.Row heading="Date won">
+      {`${values.win_date?.month}/${values.win_date?.year}`}
+    </SummaryTable.Row>
+    <SummaryTable.Row heading="Summary of support given">
+      {values.summary_of_support}
+    </SummaryTable.Row>
+    <SummaryTable.Row heading="Overseas customer">
+      {values.name_of_customer}
+    </SummaryTable.Row>
+    <SummaryTable.Row heading="Confidential">
+      {transformCustomerConfidential(values.name_of_customer_confidential)}
+    </SummaryTable.Row>
+    <SummaryTable.Row heading="Type of win">
+      {values.business_type}
+    </SummaryTable.Row>
+    <SummaryTable.Row heading="Total value">0</SummaryTable.Row>
+    <SummaryTable.ListRow
+      heading="What does the value relate to?"
+      value={transformGoodsAndServices(values.goods_and_services)}
+      emptyValue="Not set"
+    />
+    <SummaryTable.Row heading="Type of goods or services">
+      {values.goods_and_services_name}
+    </SummaryTable.Row>
+    <SummaryTable.Row heading="Sector">{values.sector?.label}</SummaryTable.Row>
+  </SummaryTable>
+)
+const SupportGivenTable = () => <>Support Given</>
 
 export default CheckBeforeSendingStep
---
+++ src/client/modules/ExportWins/Form/transformers.js
@@ -0,0 +1,24 @@
+import { isEmpty } from 'lodash'
+
+export const transformTeamsAndAdvisers = (values) =>
+  Object.keys(values)
+    .filter((key) => key.startsWith('contributing_officer_'))
+    .map((key) => {
+      // We can't use the map index here as the user may delete one of the
+      // items out of sequence. The FieldAddAnother component doesn't reindex
+      // after a delete so we have to go by the digit at the very end of the key
+      const index = key[key.length - 1]
+      return {
+        officer: values[`contributing_officer_${index}`]?.label,
+        teamType: values[`team_type_${index}`],
+        hqTeamRegionOrPost: values[`hq_team_region_or_post_${index}`],
+      }
+    })
+
+export const transformGoodsAndServices = (goodsAndServices = []) =>
+  goodsAndServices.map((gas) => ({
+    label: gas,
+  }))
+
+export const transformCustomerConfidential = (confidential = []) =>
+  isEmpty(confidential) ? 'No' : 'Yes'
