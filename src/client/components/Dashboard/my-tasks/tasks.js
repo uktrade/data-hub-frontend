31192a648822145f26725bcd17e10099148439a1
Caitlin Barnard <caitlin@madetech.com>
2023-12-11 16:05:45 +0000

    Fix tests to use limit of 50

--- src/client/components/Dashboard/my-tasks/tasks.js
+++ src/client/components/Dashboard/my-tasks/tasks.js
@@ -8,7 +8,7 @@ export const getMyTasks = ({
 }) =>
   apiProxyAxios
     .post('/v4/search/task', {
-      limit: 10,
+      limit: 50,
       offset: 0,
       created_by: created_by,
       not_created_by: not_created_by,
