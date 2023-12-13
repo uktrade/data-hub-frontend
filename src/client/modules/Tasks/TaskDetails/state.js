6cd6b70582d34cb9747fdffb6627d7d715c1eeb6
Chris Hopkins <chris.hopkins@madetech.com>
2023-12-13 14:25:57 +0000

    breadcrumbs fix

--- src/client/modules/Tasks/TaskDetails/state.js
+++ src/client/modules/Tasks/TaskDetails/state.js
@@ -1,3 +1,4 @@
+import { transformIdNameToValueLabel } from '../../../transformers'
 import { getTaskBreadcrumbs } from '../TaskForm/state'
 
 export const ID = 'taskDetails'
@@ -7,5 +8,12 @@ export const TASK_ARCHIVE_TASK = 'TASK_ARCHIVE_TASK'
 
 export const state2props = (state) => {
   const { task } = state[ID]
-  return { task: task, breadcrumbs: getTaskBreadcrumbs(task) }
+  return {
+    task: task,
+    breadcrumbs: getTaskBreadcrumbs({
+      ...task,
+      investmentProject: transformIdNameToValueLabel(task?.investmentProject),
+      company: transformIdNameToValueLabel(task?.company),
+    }),
+  }
 }
