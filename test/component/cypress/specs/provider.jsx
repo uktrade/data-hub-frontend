fb0df5d090a2d0c8d46881ab9acbdbca1fbadc1e
Chris Hopkins <chris.hopkins@madetech.com>
2023-12-18 09:21:37 +0000

    pass router to redux

--- test/component/cypress/specs/provider.jsx
+++ test/component/cypress/specs/provider.jsx
@@ -3,6 +3,10 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { combineReducers, applyMiddleware, legacy_createStore } from 'redux'
 import createSagaMiddleware from 'redux-saga'
+import { connectRouter } from 'connected-react-router'
+import { createBrowserHistory } from 'history'
+
+import queryString from 'qs'
 
 import rootSaga from '../../../../src/client/root-saga'
 import { reducers } from '../../../../src/client/reducers'
@@ -10,12 +14,17 @@ import { tasks as appTasks } from '../../../../src/client/tasks'
 
 const sagaMiddleware = createSagaMiddleware()
 
+const history = createBrowserHistory({
+  basename: queryString.stringify(new URL(document.baseURI).pathname),
+})
+
 const reducer = (state, action) =>
   combineReducers({
     ...reducers,
     activeFeatureGroups: () => [],
     modulePermissions: () => [],
     userPermissions: () => [],
+    router: connectRouter(history),
   })(action.type === 'RESET' ? undefined : state, action)
 
 export const store = legacy_createStore(
