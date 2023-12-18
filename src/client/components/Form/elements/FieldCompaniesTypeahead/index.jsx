67fd1b200f1d88fbda6fa0896ac36ce725000c1c
Chris Hopkins <102232401+chopkinsmade@users.noreply.github.com>
2023-12-18 13:21:16 +0000

    feature/CLS2-651-add-location-tag-search-results (#6352)
    * updated company typeahead to show region and country
    
    * switch to camel case for prop
    
    * added component test

--- src/client/components/Form/elements/FieldCompaniesTypeahead/index.jsx
+++ src/client/components/Form/elements/FieldCompaniesTypeahead/index.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { throttle } from 'lodash'
+import { get, throttle } from 'lodash'
+import styled from 'styled-components'
+import { SPACING } from '@govuk-react/constants'
+import { GridCol, GridRow } from 'govuk-react'
 
 import FieldTypeahead from '../FieldTypeahead'
 import { apiProxyAxios } from '../../../Task/utils'
+import Highlighter from '../../../Typeahead/Highlighter'
+import Tag from '../../../Tag'
+
+const getOptionLabel = (option) =>
+  `${option.label}${option.isInList ? ' (in your company lists)' : ''}`
+
+const StyledRow = styled(GridRow)(() => ({
+  paddingBottom: `${SPACING.SCALE_2}`,
+}))
+
+const CompanyResultWithTags = ({ option, searchString }) => (
+  <>
+    <StyledRow>
+      <GridCol>
+        <Highlighter
+          optionLabel={getOptionLabel(option)}
+          searchStr={searchString}
+        />
+      </GridCol>
+    </StyledRow>
+    <GridRow>
+      <GridCol>
+        <Tag colour={'blue'} data-test="location-tag">
+          {option.ukRegion
+            ? `${get(option, 'ukRegion.name')}, UK`
+            : get(option, 'address.country.name', 'unknown')}
+        </Tag>
+      </GridCol>
+    </GridRow>
+  </>
+)
 
 const FieldCompaniesTypeahead = ({
   name,
@@ -12,6 +46,7 @@ const FieldCompaniesTypeahead = ({
   isMulti,
   onlyShowActiveCompanies = true,
   placeholder = 'Type to search for companies',
+  loadOptions,
   ...props
 }) => {
   return (
@@ -21,25 +56,34 @@ const FieldCompaniesTypeahead = ({
       placeholder={placeholder}
       noOptionsMessage="Type to search for companies"
       required={required}
-      loadOptions={throttle(
-        (searchString) =>
-          apiProxyAxios
-            .get('/v4/company', {
-              params: {
-                autocomplete: searchString,
-                is_active: onlyShowActiveCompanies,
-              },
-            })
-            .then(({ data: { results } }) =>
-              results.map(({ id, name }) => ({
-                label: name,
-                chipLabel: name,
-                value: id,
-              }))
-            ),
-        500
-      )}
+      loadOptions={
+        loadOptions ??
+        throttle(
+          (searchString) =>
+            apiProxyAxios
+              .get('/v4/company', {
+                params: {
+                  autocomplete: searchString,
+                  is_active: onlyShowActiveCompanies,
+                },
+              })
+              .then(({ data: { results } }) =>
+                results.map(
+                  ({ id, name, is_in_adviser_list, uk_region, address }) => ({
+                    label: name,
+                    chipLabel: name,
+                    value: id,
+                    isInList: is_in_adviser_list,
+                    ukRegion: uk_region,
+                    address,
+                  })
+                )
+              ),
+          500
+        )
+      }
       isMulti={isMulti}
+      OptionContent={CompanyResultWithTags}
       {...props}
     />
   )
@@ -51,6 +95,7 @@ FieldCompaniesTypeahead.propTypes = {
   required: PropTypes.string,
   isMulti: PropTypes.bool,
   placeholder: PropTypes.string,
+  loadOptions: PropTypes.func,
 }
 
 export default FieldCompaniesTypeahead
