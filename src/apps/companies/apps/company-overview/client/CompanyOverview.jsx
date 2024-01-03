053275f56f5c0107d46ad1b5bf0cb050b7ec88c7
Christopher Sunkel <christopher.sunkel@digital.trade.gov.uk>
2024-01-03 16:14:44 +0000

    Migrate CompanyOverview page to use React Router

--- src/apps/companies/apps/company-overview/client/CompanyOverview.jsx
+++ src/apps/companies/apps/company-overview/client/CompanyOverview.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { GridCol, GridRow } from 'govuk-react'
 import styled from 'styled-components'
+import { useParams } from 'react-router-dom'
 
 import {
   AccountManagementCard,
@@ -13,7 +13,7 @@ import {
 } from '../overview-table-cards'
 import { FILTER_FEED_TYPE } from '../../activity-feed/constants'
 import { CompanyResource } from '../../../../../client/components/Resource/index'
-import CompanyLayout from '../../../../../client/components/Layout/CompanyLayout'
+import CompanyLayoutNew from '../../../../../client/components/Layout/CompanyLayoutNew'
 
 const CardContainer = styled('div')`
   border: 1px solid #b1b4b6;
@@ -21,56 +21,55 @@ const CardContainer = styled('div')`
   margin-bottom: 20px;
 `
 
-const CompanyOverview = ({ companyId, flashMessages }) => (
-  <CompanyResource id={companyId}>
-    {(company) => (
-      <CompanyLayout
-        company={company}
-        breadcrumbs={[{ text: 'Overview' }]}
-        flashMessages={flashMessages}
-      >
-        <GridRow>
-          <GridCol columnOneHalf={true}>
-            <CardContainer>
-              <BusinessDetailsCard company={company} />
-            </CardContainer>
-            <CardContainer>
-              <AccountManagementCard company={company} />
-            </CardContainer>
-            <CardContainer>
-              <ExportStatusCard company={company} />
-            </CardContainer>
-            <CardContainer>
-              <InvestmentStatusCard companyId={company.id} />
-            </CardContainer>
-          </GridCol>
-          <GridCol columnOneHalf={true}>
-            <CardContainer>
-              <ActivityCard
-                company={company}
-                numberOfItems={3}
-                feedType={FILTER_FEED_TYPE.RECENT}
-              />
-            </CardContainer>
-            <CardContainer>
-              <ActivityCard
-                company={company}
-                numberOfItems={2}
-                feedType={FILTER_FEED_TYPE.UPCOMING}
-              />
-            </CardContainer>
-            <CardContainer>
-              <ActiveInvestmentProjectsCard companyId={companyId} />
-            </CardContainer>
-          </GridCol>
-        </GridRow>
-      </CompanyLayout>
-    )}
-  </CompanyResource>
-)
-
-CompanyOverview.propTypes = {
-  companyId: PropTypes.string.isRequired,
+const CompanyOverview = () => {
+  const { companyId } = useParams()
+  return (
+    <CompanyResource id={companyId}>
+      {(company) => (
+        <CompanyLayoutNew
+          company={company}
+          breadcrumbs={[{ text: 'Overview' }]}
+          pageTitle="Overview"
+        >
+          <GridRow>
+            <GridCol columnOneHalf={true}>
+              <CardContainer>
+                <BusinessDetailsCard company={company} />
+              </CardContainer>
+              <CardContainer>
+                <AccountManagementCard company={company} />
+              </CardContainer>
+              <CardContainer>
+                <ExportStatusCard company={company} />
+              </CardContainer>
+              <CardContainer>
+                <InvestmentStatusCard companyId={company.id} />
+              </CardContainer>
+            </GridCol>
+            <GridCol columnOneHalf={true}>
+              <CardContainer>
+                <ActivityCard
+                  company={company}
+                  numberOfItems={3}
+                  feedType={FILTER_FEED_TYPE.RECENT}
+                />
+              </CardContainer>
+              <CardContainer>
+                <ActivityCard
+                  company={company}
+                  numberOfItems={2}
+                  feedType={FILTER_FEED_TYPE.UPCOMING}
+                />
+              </CardContainer>
+              <CardContainer>
+                <ActiveInvestmentProjectsCard companyId={companyId} />
+              </CardContainer>
+            </GridCol>
+          </GridRow>
+        </CompanyLayoutNew>
+      )}
+    </CompanyResource>
+  )
 }
 
 export default CompanyOverview
