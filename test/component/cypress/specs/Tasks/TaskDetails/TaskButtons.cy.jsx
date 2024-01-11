2db4e74b34e45a62d25c7e734c5e22614bdc247e
Samuele Mattiuzzo <samuele.mattiuzzo@digital.trade.gov.uk>
2024-01-11 14:36:38 +0000

    Component test: buttons

--- test/component/cypress/specs/Tasks/TaskDetails/TaskButtons.cy.jsx
+++ test/component/cypress/specs/Tasks/TaskDetails/TaskButtons.cy.jsx
@@ -31,6 +31,13 @@ describe('Task buttons', () => {
       assertLink('edit-form-button', urls.tasks.edit(task.id))
     })
 
+    it('should show the Create similar task button with expected url', () => {
+      assertLink(
+        'create-similar-task-button',
+        urls.createSimilarTask.edit(task.id)
+      )
+    })
+
     it('should show the Back link to dashboard when no return url exists', () => {
       assertLink('task-back-link', urls.dashboard.myTasks())
     })
@@ -49,6 +56,10 @@ describe('Task buttons', () => {
 
     it('should not show the Edit link', () => {
       cy.get('[data-test="submit-button"]').should('not.exist')
+    })
+
+    it('should not show the Create similar task link', () => {
+      cy.get('[data-test="create-similar-task-button"]').should('not.exist')
     })
 
     it('should show the Back link to dashboard when no return url exists', () => {
