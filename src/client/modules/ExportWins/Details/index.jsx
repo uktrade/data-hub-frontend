0d95e5d2db7c79d35699e392aa9abec0635a384a
Peter Hudec <peterhudec@peterhudec.com>
2024-01-17 14:21:51 +0000

    Implemented the ExportWins/Details page as per design

--- src/client/modules/ExportWins/Details/index.jsx
+++ src/client/modules/ExportWins/Details/index.jsx
@@ -1,34 +1,25 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import { Link } from 'govuk-react'
+import { Link as ReactRouterLink } from 'react-router-dom/cjs/react-router-dom'
+import styled from 'styled-components'
+import { SPACING } from '@govuk-react/constants'
 
 import { DefaultLayout, SummaryTable } from '../../../components'
-
-import ProgressIndicator from '../../../components/ProgressIndicator'
 import urls from '../../../../lib/urls'
 import { formatMediumDate } from '../../../utils/date'
 import { currencyGBP } from '../../../utils/number-utils'
-
 import ExportWin from '../../../components/Resource/ExportWin'
 
-// TODO: Get rid of this, it would be hard to explain why it's needed
-const Summary = ({ rows, ...props }) => (
-  <SummaryTable {...props}>
-    {rows.map(([heading, content]) => (
-      <SummaryTable.Row key={heading} heading={heading}>
-        {content}
-      </SummaryTable.Row>
-    ))}
-  </SummaryTable>
-)
+const VerticalSpacer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${SPACING.SCALE_1};
+  margin-bottom: ${SPACING.SCALE_5};
+`
 
 const ExportWinTitle = (props) => (
-  <ExportWin.Inline
-    {...props}
-    taskStatusProps={{
-      renderProgress: ProgressIndicator.Inline,
-    }}
-  >
+  <ExportWin.Inline {...props}>
     {(exportWin) => (
       <>
         {exportWin.name_of_export} to {exportWin.country?.name}
@@ -37,7 +28,22 @@ const ExportWinTitle = (props) => (
   </ExportWin.Inline>
 )
 
-export default () => (
+// TODO: Use the same functionality as in the create export win form
+const f = (breakdowns) =>
+  breakdowns.reduce(
+    (a, x) => ({
+      ...a,
+      [x.type.name]: {
+        // years: [...(a[x.type.name]?.years || []), x.year],
+        yearCount: (a[x.type.name]?.yearCount || 0) + 1,
+        // values: [...(a[x.type.name]?.values || []), x.value],
+        total: (a[x.type.name]?.total || 0) + x.value,
+      },
+    }),
+    {}
+  )
+
+const Detail = () => (
   <Route>
     {({
       match: {
@@ -59,126 +65,94 @@ export default () => (
           { text: <ExportWinTitle id={winId} /> },
         ]}
       >
-        <Summary
-          rows={[
-            [
-              'UK Company',
-              <ExportWin.Inline noun="UK company" id={winId}>
-                {({ company: { name, id } }) => (
-                  <Link href={urls.companies.detail(id)}>{name}</Link>
-                )}
-              </ExportWin.Inline>,
-            ],
-            [
-              'Contact',
-              <ExportWin.Inline noun="contact" id={winId}>
-                {({ companyContacts: [{ name, id }] }) => (
-                  <Link href={urls.contacts.contact(id)}>{name}</Link>
-                )}
-              </ExportWin.Inline>,
-            ],
-            [
-              'Goods or services',
-              <ExportWin.Detail
-                at="goodsVsServices.name"
-                noun="goods or services"
-                id={winId}
-              />,
-            ],
-            [
-              'Destination',
-              <ExportWin.Detail
-                at="country.name"
-                noun="destination"
-                id={winId}
-              />,
-            ],
-            [
-              'Export amount',
-              <ExportWin.Detail
-                at="totalExpectedExportValue"
-                noun="export amount"
-                id={winId}
-                children={currencyGBP}
-              />,
-            ],
-            [
-              'Date won',
-              <ExportWin.Detail
-                at="date"
-                noun="date won"
-                id={winId}
-                children={formatMediumDate}
-              />,
-            ],
-            [
-              'Last modified',
-              <ExportWin.Detail
-                at="modifiedOn"
-                noun="last modified"
-                id={winId}
-                children={formatMediumDate}
-              />,
-            ],
-            [
-              'Lead officer name',
-              <ExportWin.Detail
-                at="leadOfficer.name"
-                noun="lead officer"
-                id={winId}
-              />,
-            ],
-            [
-              'Customer response',
-              <ExportWin.Detail
-                at="customerResponse.comments"
-                noun="customer response"
-                id={winId}
-              />,
-            ],
-            [
-              'Export win confirmed',
-              <ExportWin.Detail
-                at="isLineManagerConfirmed"
-                noun="confirmed"
-                id={winId}
-                children={(x) => (x ? 'Yes' : 'No')}
-              />,
-            ],
-            [
-              'Date of feedback',
-              // '???',
-              <ExportWin.Detail
-                at="customerResponse.createdOn"
-                noun="date of feedback"
-                id={winId}
-                children={formatMediumDate}
-              />,
-            ],
-            [
-              'Adviser(s)',
-              <ExportWin.Inline noun="advisers" id={winId}>
-                {({ advisers }) =>
-                  advisers
-                    .map(
-                      ({ adviser, teamType }) =>
-                        `${adviser.name} — ${teamType.name}`
+        <ExportWin id={winId} progressBox={true}>
+          {(exportWin) => (
+            <>
+              <SummaryTable>
+                <SummaryTable.Row heading="UK Company">
+                  {exportWin && (
+                    <Link href={urls.companies.detail(exportWin.company.id)}>
+                      {exportWin.company.name}
+                    </Link>
+                  )}
+                </SummaryTable.Row>
+                <SummaryTable.Row heading="Contact">
+                  {exportWin && (
+                    <Link
+                      href={urls.contacts.contact(
+                        exportWin.companyContacts[0].id
+                      )}
+                    >
+                      {exportWin.companyContacts[0].name}
+                    </Link>
+                  )}
+                </SummaryTable.Row>
+                <SummaryTable.Row heading="Goods or services">
+                  {exportWin?.goodsVsServices.name}
+                </SummaryTable.Row>
+                <SummaryTable.Row heading="Destination country">
+                  {exportWin?.country.name}
+                </SummaryTable.Row>
+                {exportWin &&
+                  Object.entries(f(exportWin.breakdowns)).map(
+                    ([k, { total, yearCount }]) => (
+                      <SummaryTable.Row key={k} heading={k}>
+                        {`${currencyGBP(total)} over ${yearCount} years`}
+                      </SummaryTable.Row>
                     )
-                    .join(', ')
-                }
-              </ExportWin.Inline>,
-            ],
-          ]}
-        />
-        <ExportWin
-          id={winId}
-          taskStatusProps={{
-            renderProgress: ProgressIndicator.Inline,
-          }}
-        >
-          {(exportWin) => <pre>{JSON.stringify(exportWin, null, 2)}</pre>}
+                  )}
+                <SummaryTable.Row heading="Export amount">
+                  {exportWin && currencyGBP(exportWin.totalExpectedExportValue)}
+                </SummaryTable.Row>
+                <SummaryTable.Row heading="Date won">
+                  {exportWin && formatMediumDate(exportWin.date)}
+                </SummaryTable.Row>
+                <SummaryTable.Row heading="Last modified">
+                  {exportWin && formatMediumDate(exportWin.modifiedOn)}
+                </SummaryTable.Row>
+                <SummaryTable.Row heading="Lead officer name">
+                  {exportWin?.leadOfficer.name}
+                </SummaryTable.Row>
+                <SummaryTable.Row heading="Customer response">
+                  {exportWin?.customerResponse?.comments}
+                </SummaryTable.Row>
+                <SummaryTable.Row heading="Export win confirmed">
+                  {exportWin &&
+                    (exportWin.isLineManagerConfirmed ? 'Yes' : 'No')}
+                </SummaryTable.Row>
+                <SummaryTable.Row heading="Date of feedback">
+                  {formatMediumDate(exportWin?.customerResponse?.createdOn)}
+                </SummaryTable.Row>
+                <SummaryTable.Row heading="Adviser(s)">
+                  {exportWin?.advisers.map(
+                    ({ adviser, teamType }) =>
+                      `${adviser.name} — ${teamType.name}`
+                  )}
+                </SummaryTable.Row>
+              </SummaryTable>
+              <VerticalSpacer>
+                {/* {!exportWin?.isLineManagerConfirmed && ( */}
+                {true && (
+                  <Link
+                    as={ReactRouterLink}
+                    to={urls.companies.exportWins.customerFeedback(winId)}
+                  >
+                    View customer feedback
+                  </Link>
+                )}
+                <Link
+                  as={ReactRouterLink}
+                  to={urls.companies.exportWins.index()}
+                >
+                  Export wins
+                </Link>
+              </VerticalSpacer>
+            </>
+          )}
         </ExportWin>
       </DefaultLayout>
     )}
   </Route>
 )
+
+export default Detail
