6cea61cb1356dbe268c2292074847dbb5ed8d9ac
Paul Gain <paul@javexconsulting.com>
2024-01-18 13:32:15 +0000

    Add 'Credit for this win' form step

--- src/client/modules/ExportWins/Form/CustomerDetailsStep.jsx
+++ src/client/modules/ExportWins/Form/CustomerDetailsStep.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import { useFormContext } from '../../../../client/components/Form/hooks'
-import { Step } from '../../../components'
+import { Step, FieldInput } from '../../../components'
 import { steps } from './constants'
+
+const StyledFieldInput = styled(FieldInput)({
+  display: 'none',
+})
 
 const CustomerDetailsStep = () => {
   // eslint-disable-next-line no-unused-vars
@@ -10,6 +15,7 @@ const CustomerDetailsStep = () => {
   return (
     <Step name={steps.CUSTOMER_DETAILS}>
       <h1>Customer details</h1>
+      <StyledFieldInput name="hidden" type="text" />
     </Step>
   )
 }
