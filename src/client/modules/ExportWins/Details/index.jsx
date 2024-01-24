cbc6a7f48f173359c92b93300cb7af5afc75e0da
Peter Hudec <peterhudec@peterhudec.com>
2024-01-24 18:56:55 +0000

    Implemented latest design changes

--- src/client/modules/ExportWins/Details/index.jsx
+++ src/client/modules/ExportWins/Details/index.jsx
@@ -27,22 +27,6 @@ const ExportWinTitle = (props) => (
     )}
   </ExportWin.Inline>
 )
-
-// TODO: Use the same functionality as in the create export win form
-const f = (breakdowns) =>
-  breakdowns.reduce(
-    (a, x) => ({
-      ...a,
-      [x.type.name]: {
-        // years: [...(a[x.type.name]?.years || []), x.year],
-        yearCount: (a[x.type.name]?.yearCount || 0) + 1,
-        // values: [...(a[x.type.name]?.values || []), x.value],
-        total: (a[x.type.name]?.total || 0) + x.value,
-      },
-    }),
-    {}
-  )
-
 const Detail = () => (
   <Route>
     {({
@@ -66,88 +50,98 @@ const Detail = () => (
         ]}
       >
         <ExportWin id={winId} progressBox={true}>
-          {(exportWin) => (
-            <>
-              <SummaryTable>
-                <SummaryTable.Row heading="UK Company">
-                  {exportWin && (
-                    <Link href={urls.companies.detail(exportWin.company.id)}>
-                      {exportWin.company.name}
-                    </Link>
-                  )}
-                </SummaryTable.Row>
-                <SummaryTable.Row heading="Contact">
-                  {exportWin && (
-                    <Link
-                      href={urls.contacts.contact(
-                        exportWin.companyContacts[0].id
-                      )}
-                    >
-                      {exportWin.companyContacts[0].name}
-                    </Link>
-                  )}
-                </SummaryTable.Row>
-                <SummaryTable.Row heading="Goods or services">
-                  {exportWin?.goodsVsServices.name}
-                </SummaryTable.Row>
-                <SummaryTable.Row heading="Destination country">
-                  {exportWin?.country.name}
-                </SummaryTable.Row>
-                {exportWin &&
-                  Object.entries(f(exportWin.breakdowns)).map(
-                    ([k, { total, yearCount }]) => (
-                      <SummaryTable.Row key={k} heading={k}>
-                        {`${currencyGBP(total)} over ${yearCount} years`}
-                      </SummaryTable.Row>
-                    )
-                  )}
-                <SummaryTable.Row heading="Export amount">
-                  {exportWin && currencyGBP(exportWin.totalExpectedExportValue)}
-                </SummaryTable.Row>
-                <SummaryTable.Row heading="Date won">
-                  {exportWin && formatMediumDate(exportWin.date)}
-                </SummaryTable.Row>
-                <SummaryTable.Row heading="Last modified">
-                  {exportWin && formatMediumDate(exportWin.modifiedOn)}
-                </SummaryTable.Row>
-                <SummaryTable.Row heading="Lead officer name">
-                  {exportWin?.leadOfficer.name}
-                </SummaryTable.Row>
-                <SummaryTable.Row heading="Customer response">
-                  {exportWin?.customerResponse?.comments}
-                </SummaryTable.Row>
-                <SummaryTable.Row heading="Export win confirmed">
+          {(exportWin) => {
+            // TODO: Same / similar computation is used in the add export win form so it should be reused
+            const {
+              groupedBreakdowns = [],
+              totalAmount,
+              yearRange,
+              totalYears = yearRange?.max - yearRange?.min + 1,
+            } = exportWin?.breakdowns.reduce(
+              ({ groupedBreakdowns, totalAmount, yearRange }, x) => ({
+                totalAmount: totalAmount + x.value,
+                yearRange: {
+                  min: Math.min(yearRange.min, x.year),
+                  max: Math.max(yearRange.max, x.year),
+                },
+                groupedBreakdowns: {
+                  ...groupedBreakdowns,
+                  [x.type.name]: {
+                    yearCount:
+                      (groupedBreakdowns[x.type.name]?.yearCount || 0) + 1,
+                    total:
+                      (groupedBreakdowns[x.type.name]?.total || 0) + x.value,
+                  },
+                },
+              }),
+              {
+                groupedBreakdowns: {},
+                totalAmount: 0,
+                yearRange: { min: Infinity, max: 0 },
+              }
+            ) || {}
+
+            return (
+              <>
+                <SummaryTable>
+                  <SummaryTable.Row heading="Goods or services">
+                    {exportWin?.goodsVsServices.name}
+                  </SummaryTable.Row>
+                  <SummaryTable.Row heading="Destination country">
+                    {exportWin?.country.name}
+                  </SummaryTable.Row>
                   {exportWin &&
-                    (exportWin.isLineManagerConfirmed ? 'Yes' : 'No')}
-                </SummaryTable.Row>
-                <SummaryTable.Row heading="Date of feedback">
-                  {formatMediumDate(exportWin?.customerResponse?.createdOn)}
-                </SummaryTable.Row>
-                <SummaryTable.Row heading="Adviser(s)">
-                  {exportWin?.advisers.map(
-                    ({ adviser, teamType }) =>
-                      `${adviser.name} — ${teamType.name}`
+                    Object.keys(groupedBreakdowns).length > 1 &&
+                    Object.entries(groupedBreakdowns).map(
+                      ([k, { total, yearCount }]) => (
+                        <SummaryTable.Row key={k} heading={k}>
+                          {`${currencyGBP(total)} over ${yearCount} years`}
+                        </SummaryTable.Row>
+                      )
+                    )}
+                  <SummaryTable.Row heading="Total value">
+                    {exportWin &&
+                      `${currencyGBP(totalAmount)} over ${totalYears} years`}
+                  </SummaryTable.Row>
+                  <SummaryTable.Row heading="Date won">
+                    {exportWin && formatMediumDate(exportWin.date)}
+                  </SummaryTable.Row>
+                  <SummaryTable.Row heading="Lead officer name">
+                    {exportWin?.leadOfficer.name}
+                  </SummaryTable.Row>
+                  <SummaryTable.Row heading="Comments">
+                    {exportWin?.customerResponse?.comments}
+                  </SummaryTable.Row>
+                  <SummaryTable.Row heading="Export win confirmed">
+                    {exportWin &&
+                      (exportWin.isPersonallyConfirmed ? 'Yes' : 'No')}
+                  </SummaryTable.Row>
+                  {exportWin?.isPersonallyConfirmed &&
+                    exportWin?.breakdowns.length === 0 && (
+                      <SummaryTable.Row heading="What value do you estimate you would have achieved without our support?">
+                        ???
+                      </SummaryTable.Row>
+                    )}
+                </SummaryTable>
+                <VerticalSpacer>
+                  {exportWin?.isLineManagerConfirmed && (
+                    <Link
+                      as={ReactRouterLink}
+                      to={urls.companies.exportWins.customerFeedback(winId)}
+                    >
+                      View customer feedback
+                    </Link>
                   )}
-                </SummaryTable.Row>
-              </SummaryTable>
-              <VerticalSpacer>
-                {exportWin?.isLineManagerConfirmed && (
                   <Link
                     as={ReactRouterLink}
-                    to={urls.companies.exportWins.customerFeedback(winId)}
+                    to={urls.companies.exportWins.index()}
                   >
-                    View customer feedback
+                    Export wins
                   </Link>
-                )}
-                <Link
-                  as={ReactRouterLink}
-                  to={urls.companies.exportWins.index()}
-                >
-                  Export wins
-                </Link>
-              </VerticalSpacer>
-            </>
-          )}
+                </VerticalSpacer>
+              </>
+            )
+          }}
         </ExportWin>
       </DefaultLayout>
     )}
