defa5ef0538ce6c990dd69453b985bd659c18668
Sophie Wenban <s.maclennan@hotmail.co.uk>
2024-01-24 16:11:30 +0000

    Change Edit Project Summary so that the layout renders before the resource

--- src/client/modules/Investments/Projects/Details/EditProjectSummary.jsx
+++ src/client/modules/Investments/Projects/Details/EditProjectSummary.jsx
@@ -44,7 +44,8 @@ import { transformDateStringToDateObject } from '../../../../../apps/transformer
 import { OPTION_NO, OPTION_YES } from '../../../../../common/constants'
 import { GREY_2 } from '../../../../utils/colours'
 import { state2props, TASK_EDIT_INVESTMENT_PROJECT_SUMMARY } from './state'
-import ProjectLayout from '../../../../components/Layout/ProjectLayout'
+import ProjectLayoutNew from '../../../../components/Layout/ProjectLayoutNew'
+import InvestmentName from '../InvestmentName'
 
 const StyledFieldWrapper = styled(FieldWrapper)`
   border: 1px solid ${GREY_2};
@@ -68,147 +69,154 @@ const checkReferralSourceAdviser = (currentAdviser, referralSourceAdviser) =>
 const EditProjectSummary = ({ currentAdviserId, autoScroll }) => {
   const { projectId } = useParams()
   return (
-    <InvestmentResource id={projectId}>
-      {(project) => (
-        <ProjectLayout
-          project={project}
-          breadcrumbs={[
-            {
-              link: urls.investments.projects.details(project.id),
-              text: project.name,
-            },
-            { text: 'Edit details' },
-          ]}
-          pageTitle="Edit details"
-        >
-          <H2 size={LEVEL_SIZE[3]}>Update investment project summary</H2>
-          <Form
-            id="edit-project-summary"
-            analyticsFormName="editInvestmentProjectSummary"
-            cancelButtonLabel="Back"
-            cancelRedirectTo={() =>
-              urls.investments.projects.details(project.id)
-            }
-            flashMessage={() => 'Investment details updated'}
-            submitButtonlabel="Save"
-            redirectTo={() => urls.investments.projects.details(project.id)}
-            submissionTaskName={TASK_EDIT_INVESTMENT_PROJECT_SUMMARY}
-            transformPayload={(values) =>
-              transformProjectSummaryForApi({
-                projectId,
-                currentAdviserId,
-                values,
-              })
-            }
-          >
-            <FieldProjectName initialValue={project.name} />
-            <FieldProjectDescription initialValue={project.description} />
-            <FieldAnonDescription initialValue={project.anonymousDescription} />
-            <InvestmentTypesResource>
-              {(investmentTypes) => (
-                <FieldRadios
-                  name="investment_type"
-                  label="Investment type"
-                  initialValue={project.investmentType.id}
-                  options={transformArrayForTypeahead(investmentTypes).map(
-                    (option) => ({
-                      ...option,
-                      ...(option.label === 'FDI' && {
-                        children: (
-                          <FieldFDIType
-                            initialValue={transformObjectForTypeahead(
-                              project.fdiType
-                            )}
-                          />
-                        ),
-                      }),
-                    })
+    <ProjectLayoutNew
+      projectId={projectId}
+      projectName={<InvestmentName id={projectId} />}
+      breadcrumbs={[
+        {
+          link: urls.investments.projects.details(projectId),
+          text: <InvestmentName id={projectId} />,
+        },
+        { text: 'Edit details' },
+      ]}
+      pageTitle="Edit details"
+    >
+      <InvestmentResource id={projectId}>
+        {(project) => (
+          <>
+            <H2 size={LEVEL_SIZE[3]}>Update investment project summary</H2>
+            <Form
+              id="edit-project-summary"
+              analyticsFormName="editInvestmentProjectSummary"
+              cancelButtonLabel="Back"
+              cancelRedirectTo={() =>
+                urls.investments.projects.details(project.id)
+              }
+              flashMessage={() => 'Investment details updated'}
+              submitButtonlabel="Save"
+              redirectTo={() => urls.investments.projects.details(project.id)}
+              submissionTaskName={TASK_EDIT_INVESTMENT_PROJECT_SUMMARY}
+              transformPayload={(values) =>
+                transformProjectSummaryForApi({
+                  projectId,
+                  currentAdviserId,
+                  values,
+                })
+              }
+            >
+              <FieldProjectName initialValue={project.name} />
+              <FieldProjectDescription initialValue={project.description} />
+              <FieldAnonDescription
+                initialValue={project.anonymousDescription}
+              />
+              <InvestmentTypesResource>
+                {(investmentTypes) => (
+                  <FieldRadios
+                    name="investment_type"
+                    label="Investment type"
+                    initialValue={project.investmentType.id}
+                    options={transformArrayForTypeahead(investmentTypes).map(
+                      (option) => ({
+                        ...option,
+                        ...(option.label === 'FDI' && {
+                          children: (
+                            <FieldFDIType
+                              initialValue={transformObjectForTypeahead(
+                                project.fdiType
+                              )}
+                            />
+                          ),
+                        }),
+                      })
+                    )}
+                  />
+                )}
+              </InvestmentTypesResource>
+              <FieldProjectSector
+                initialValue={transformObjectForTypeahead(project.sector)}
+              />
+              <StyledFieldWrapper name="businessActivitiesWrapper">
+                <FieldBusinessActivity
+                  initialValue={transformArrayForTypeahead(
+                    project.businessActivities
                   )}
                 />
-              )}
-            </InvestmentTypesResource>
-            <FieldProjectSector
-              initialValue={transformObjectForTypeahead(project.sector)}
-            />
-            <StyledFieldWrapper name="businessActivitiesWrapper">
-              <FieldBusinessActivity
+                <FieldInput
+                  label="Other business activity (if not on list)"
+                  name="other_business_activity"
+                  type="text"
+                  initialValue={project.otherBusinessActivity || ''}
+                  placeholder="e.g. meet and greet dinner"
+                />
+              </StyledFieldWrapper>
+              <FieldClientContacts
+                companyId={project.investorCompany.id}
                 initialValue={transformArrayForTypeahead(
-                  project.businessActivities
+                  project.clientContacts
                 )}
               />
-              <FieldInput
-                label="Other business activity (if not on list)"
-                name="other_business_activity"
-                type="text"
-                initialValue={project.otherBusinessActivity || ''}
-                placeholder="e.g. meet and greet dinner"
-              />
-            </StyledFieldWrapper>
-            <FieldClientContacts
-              companyId={project.investorCompany.id}
-              initialValue={transformArrayForTypeahead(project.clientContacts)}
-            />
-            <FieldReferralSourceAdviser
-              name="is_referral_source"
-              initialValue={checkReferralSourceAdviser(
-                currentAdviserId,
-                project.referralSourceAdviser?.id
-              )}
-              label="Referral source adviser"
-              typeaheadInitialValue={
-                transformRadioOptionToBool(
-                  checkReferralSourceAdviser(
-                    currentAdviserId,
-                    project.referralSourceAdviser?.id
+              <FieldReferralSourceAdviser
+                name="is_referral_source"
+                initialValue={checkReferralSourceAdviser(
+                  currentAdviserId,
+                  project.referralSourceAdviser?.id
+                )}
+                label="Referral source adviser"
+                typeaheadInitialValue={
+                  transformRadioOptionToBool(
+                    checkReferralSourceAdviser(
+                      currentAdviserId,
+                      project.referralSourceAdviser?.id
+                    )
                   )
-                )
-                  ? null
-                  : transformObjectForTypeahead(project.referralSourceAdviser)
-              }
-            />
-            <FieldReferralSourceHierarchy
-              initialValue={project.referralSourceActivity?.id}
-              marketingInitialValue={
-                project.referralSourceActivityMarketing?.id
-              }
-              websiteInitialValue={project.referralSourceActivityWebsite?.id}
-              eventInitialValue={project.referralSourceActivityEvent}
-              eventPlaceholder="e.g. conversation at conference"
-            />
-            <FieldEstimatedLandDate
-              initialValue={transformDateStringToDateObject(
-                project.estimatedLandDate
-              )}
-            />
-            <FieldLikelihoodOfLanding
-              autoScroll={!!autoScroll}
-              initialValue={transformObjectForTypeahead(
-                project.likelihoodToLand
-              )}
-            />
-            <FieldActualLandDate
-              initialValue={transformDateStringToDateObject(
-                project.actualLandDate
-              )}
-            />
-            <FieldInvestmentInvestorType
-              label="New or existing investor"
-              initialValue={project.investorType?.id}
-            />
-            <FieldLevelOfInvolvement
-              initialValue={transformObjectForTypeahead(
-                project.levelOfInvolvement
-              )}
-            />
-            <FieldSpecificProgramme
-              initialValue={transformObjectForTypeahead(
-                project.specificProgramme
-              )}
-            />
-          </Form>
-        </ProjectLayout>
-      )}
-    </InvestmentResource>
+                    ? null
+                    : transformObjectForTypeahead(project.referralSourceAdviser)
+                }
+              />
+              <FieldReferralSourceHierarchy
+                initialValue={project.referralSourceActivity?.id}
+                marketingInitialValue={
+                  project.referralSourceActivityMarketing?.id
+                }
+                websiteInitialValue={project.referralSourceActivityWebsite?.id}
+                eventInitialValue={project.referralSourceActivityEvent}
+                eventPlaceholder="e.g. conversation at conference"
+              />
+              <FieldEstimatedLandDate
+                initialValue={transformDateStringToDateObject(
+                  project.estimatedLandDate
+                )}
+              />
+              <FieldLikelihoodOfLanding
+                autoScroll={!!autoScroll}
+                initialValue={transformObjectForTypeahead(
+                  project.likelihoodToLand
+                )}
+              />
+              <FieldActualLandDate
+                initialValue={transformDateStringToDateObject(
+                  project.actualLandDate
+                )}
+              />
+              <FieldInvestmentInvestorType
+                label="New or existing investor"
+                initialValue={project.investorType?.id}
+              />
+              <FieldLevelOfInvolvement
+                initialValue={transformObjectForTypeahead(
+                  project.levelOfInvolvement
+                )}
+              />
+              <FieldSpecificProgramme
+                initialValue={transformObjectForTypeahead(
+                  project.specificProgramme
+                )}
+              />
+            </Form>
+          </>
+        )}
+      </InvestmentResource>
+    </ProjectLayoutNew>
   )
 }
 
