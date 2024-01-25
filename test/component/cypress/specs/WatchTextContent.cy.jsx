600973fa22beb0b1b4b791fece9e8f66ab79472d
Peter Hudec <peterhudec@peterhudec.com>
2024-01-25 17:18:34 +0000

    Fixed a bug when WatchTextContent didnt work with a routed component

--- test/component/cypress/specs/WatchTextContent.cy.jsx
+++ test/component/cypress/specs/WatchTextContent.cy.jsx
@@ -15,9 +15,32 @@ const Counter = ({ children }) => {
 }
 
 describe('WatchTextContent', () => {
+  it('can be unmounted', () => {
+    const onTextContentChange = cy.stub()
+    cy.mount(
+      <>
+        First render
+        <WatchTextContent onTextContentChange={onTextContentChange}>
+          <ul>
+            <li>foo</li>
+            <li>bar</li>
+            <li>baz</li>
+          </ul>
+        </WatchTextContent>
+      </>
+    )
+
+    cy.contains('First render').then(() =>
+      expect(onTextContentChange.args).to.deep.eq([['foobarbaz']])
+    )
+
+    cy.mount(<div>Second render</div>)
+    cy.contains('First render').should('not.exist')
+    cy.contains('Second render')
+  })
+
   it("calls onTextContentChange when it's text content changes", () => {
     const onTextContentChange = cy.stub()
-
     cy.mount(
       <Counter>
         {(count) => (
@@ -37,11 +60,15 @@ describe('WatchTextContent', () => {
       </Counter>
     )
 
+    cy.contains('Headingfoobar0baz').should('not.exist')
     cy.get('.counter').click()
+    cy.contains('Headingfoobar1baz').should('not.exist')
     cy.get('.counter').click()
+    cy.contains('Headingfoobar2baz').should('not.exist')
     cy.get('.counter')
       .click()
       .then(() => {
+        cy.contains('Headingfoobar3baz').should('not.exist')
         expect(onTextContentChange.args).to.deep.eq([
           ['Headingfoobar0baz'],
           ['Headingfoobar1baz'],
