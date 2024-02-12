35c5281fa1617f46bb20bd1cbc5df74a1b731faf
Christopher Sunkel <christopher.sunkel@digital.trade.gov.uk>
2024-02-12 12:19:59 +0000

    Remove legacy code

--- src/apps/omis/middleware.js
+++ src/apps/omis/middleware.js
@@ -1,18 +1,6 @@
-const { assign, get, set } = require('lodash')
+const { assign } = require('lodash')
 
-const { getDitCompany } = require('../companies/repos')
-const { setHomeBreadcrumb } = require('../middleware')
 const { Order } = require('./models')
-const labels = require('./locales/en/default')
-
-async function setCompany(req, res, next, companyId) {
-  try {
-    res.locals.company = await getDitCompany(req, companyId)
-    next()
-  } catch (error) {
-    next(error)
-  }
-}
 
 async function setOrder(req, res, next, orderId) {
   try {
@@ -43,26 +31,6 @@ async function setOrder(req, res, next, orderId) {
   }
 }
 
-function setOrderBreadcrumb(req, res, next) {
-  const reference = get(res.locals, 'order.reference')
-
-  return setHomeBreadcrumb(reference)(req, res, next)
-}
-
-/*
-In order to remove the i18n-future dependency this
-function has been introduced to replace code which was
-calling the translation library. Translations are not
-required at this stage.
-*/
-function translate(req, res, next) {
-  set(res.locals, 'translate', (key) => get(labels, key))
-  next()
-}
-
 module.exports = {
-  setCompany,
   setOrder,
-  setOrderBreadcrumb,
-  translate,
 }
--- src/apps/omis/models.js
+++ src/apps/omis/models.js
@@ -5,31 +5,6 @@ const Order = {
   getById(req, id) {
     return authorisedRequest(req, `${config.apiRoot}/v3/omis/order/${id}`)
   },
-
-  previewQuote(req, id) {
-    return authorisedRequest(req, {
-      url: `${config.apiRoot}/v3/omis/order/${id}/quote/preview`,
-      method: 'POST',
-    })
-  },
-
-  getQuote(req, id) {
-    return authorisedRequest(req, `${config.apiRoot}/v3/omis/order/${id}/quote`)
-  },
-
-  createQuote(req, id) {
-    return authorisedRequest(req, {
-      url: `${config.apiRoot}/v3/omis/order/${id}/quote`,
-      method: 'POST',
-    })
-  },
-
-  cancelQuote(req, id) {
-    return authorisedRequest(req, {
-      url: `${config.apiRoot}/v3/omis/order/${id}/quote/cancel`,
-      method: 'POST',
-    })
-  },
 }
 
 module.exports = {
