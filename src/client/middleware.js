4c788792353578381be1ad6e05810055ecd76bea
Bau Nguyen <bauvannguyen123@gmail.com>
2024-02-20 15:03:14 +0000

    setup router compatibility package
    setup router compatibility package

--- src/client/middleware.js
+++ src/client/middleware.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import createSagaMiddleware from 'redux-saga'
-import { connectRouter, routerMiddleware } from 'connected-react-router'
 import queryString from 'qs'
 import { createBrowserHistory } from 'history'
+
+import { createReduxHistoryContext } from 'redux-first-history'
 
 import { reducers } from './reducers'
 
@@ -10,9 +11,9 @@ const preloadedState = {
   referrerUrl: window.document.referrer,
 }
 
-const sagaMiddleware = createSagaMiddleware()
+// const sagaMiddleware = createSagaMiddleware()
 
-const history = createBrowserHistory({
+const browserHistory = createBrowserHistory({
   // The baseURI is set to the <base/> tag by the spaFallbackSpread
   // middleware, which should be applied to each Express route where
   // react-router is expected to be used.
@@ -21,9 +22,16 @@ const history = createBrowserHistory({
 
 const store = configureStore({
   devTools: process.env.NODE_ENV === 'development',
-  middleware: [sagaMiddleware, routerMiddleware(history)],
+  middleware: [sagaMiddleware, routerMiddleware],
   preloadedState,
-  reducer: { ...reducers, router: connectRouter(history) },
+  reducer: { ...reducers, router: routerReducer },
 })
+
+const sagaMiddleware = createSagaMiddleware()
+
+const { createReduxHistory, routerMiddleware, routerReducer } =
+  createReduxHistoryContext({ history: browserHistory })
+
+const history = createReduxHistory(store)
 
 export { store, history, sagaMiddleware }
