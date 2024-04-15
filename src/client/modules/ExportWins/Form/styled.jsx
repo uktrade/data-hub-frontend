9d1c7252d065749462f0a5faec44aa7a0a206836
Paul Gain <paul@javexconsulting.com>
2024-04-15 16:46:58 +0100

    Style 'credit for this win' form step inline with the design

--- src/client/modules/ExportWins/Form/styled.jsx
+++ src/client/modules/ExportWins/Form/styled.jsx
@@ -1,7 +1,11 @@
+import { MEDIA_QUERIES } from '@govuk-react/constants'
 import styled from 'styled-components'
 
 import { DARK_GREY } from '../../../utils/colours'
 
 export const StyledHintParagraph = styled('p')({
   color: DARK_GREY,
+  [MEDIA_QUERIES.DESKTOP]: {
+    width: ({ widthPercent }) => `${widthPercent}%` || '100%',
+  },
 })
