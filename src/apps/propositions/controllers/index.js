b7b1ab5efd8c9df42b227da6a40e78537f3a71b9
Christopher <christopher.sunkel@digital.trade.gov.uk>
2024-04-23 12:44:01 +0100

    Remove unused complete proposition form and other dead code

--- src/apps/propositions/controllers/index.js
+++ src/apps/propositions/controllers/index.js
@@ -1,9 +1,5 @@
 const detailsController = require('./details')
-const editController = require('./edit')
-const listController = require('./list')
 
 module.exports = {
   detailsController,
-  editController,
-  listController,
 }
--- src/apps/propositions/macros/index.js
+++
@@ -1,9 +0,0 @@
-const collectionSortForm = require('./collection-sort-form')
-const fields = require('./fields')
-const completeForm = require('./complete-form')
-
-module.exports = {
-  collectionSortForm,
-  completeForm,
-  fields,
-}
