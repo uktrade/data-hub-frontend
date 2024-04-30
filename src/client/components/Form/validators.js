ba874cb4d4f63a8d054d8d31db3ad6ec43d7c80a
Sophie Wenban <s.maclennan@hotmail.co.uk>
2024-04-30 14:10:02 +0100

    Add validation to actual land date field to ensure only present and past dates are accepted

--- src/client/components/Form/validators.js
+++ src/client/components/Form/validators.js
@@ -1,3 +1,6 @@
+import { isDateInFuture } from '../../utils/date'
+import { transformDateObjectToDateString } from '../../transformers'
+
 const EMAIL_PATTERN =
   /(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-zA-Z0-9-]*[a-zA-Z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 
@@ -10,3 +13,8 @@ export const email = (x) =>
 
 export const number = (x, errorMessage) =>
   !x || IS_NUMBER.test(x) ? null : errorMessage
+
+export const validateIfDateInPast = (values) =>
+  isDateInFuture(transformDateObjectToDateString(values))
+    ? 'Actual land date cannot be in the future'
+    : null
