57f77a98508a7f367b5830f736fd0747c86e59ec
Peter Hudec <peterhudec@peterhudec.com>
2024-04-30 11:13:39 +0100

    The notification bell icon no longer uses inline css image

---
+++ src/client/components/NotificationAlert/BellIcon.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default (props) => (
+  <svg
+    {...props}
+    version="1.1"
+    id="Layer_1"
+    xmlns="http://www.w3.org/2000/svg"
+    x="0px"
+    y="0px"
+    viewBox="0 0 18 18"
+    width="18"
+    height="18"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    xmlSpace="preserve"
+  >
+    <path
+      fill="#FFF"
+      d="M6.9,15.9C6.9,15.9,6.9,15.9,6.9,15.9c0,1.2,1,2.1,2.1,2.1s2.1-1,2.1-2.1c0,0,0,0,0,0H6.9L6.9,15.9z M15.8,13.2
+    l-1.9-2.8V6.8c0-2.4-1.7-4.4-3.9-4.8V1c0-0.5-0.4-1-1-1C8.5,0,8,0.4,8,1V2C5.8,2.5,4.1,4.5,4.1,6.8v3.5l-1.9,2.8
+    c-0.3,0.4-0.2,1,0.3,1.3c0.2,0.1,0.4,0.2,0.5,0.2h12c0.5,0,1-0.4,1-1C16,13.5,15.9,13.3,15.8,13.2z"
+    />
+  </svg>
+)
