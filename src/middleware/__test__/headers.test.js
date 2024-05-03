22f355a02756e394afd20c9f40d9f4e794c2ad22
Peter Hudec <peterhudec@peterhudec.com>
2024-05-03 12:10:57 +0100

    Fixed failing unit tests of headers middleware

--- src/middleware/__test__/headers.test.js
+++ src/middleware/__test__/headers.test.js
@@ -1,5 +1,19 @@
 const headers = require('../headers')
 
+const NONCE = 'DUMMY-NONCE'
+
+const CSP_HEADER = [
+  'Content-Security-Policy',
+  [
+    `default-src 'self' 'nonce-${NONCE}'`,
+    `script-src 'self' 'nonce-${NONCE}' https://*.googletagmanager.com`,
+    `img-src 'self' https://*.google-analytics.com https://*.googletagmanager.com`,
+    `connect-src 'self' https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com`,
+  ].join(';'),
+]
+
+const nonceGenerator = () => NONCE
+
 describe('headers middleware', () => {
   context('when the resource is not an asset', () => {
     it('should set headers', () => {
@@ -7,9 +21,10 @@ describe('headers middleware', () => {
       const resMock = { set: sinon.spy() }
       const nextMock = sinon.spy()
 
-      headers(reqMock, resMock, nextMock)
+      headers(reqMock, resMock, nextMock, nonceGenerator)
 
       expect(resMock.set.args).to.be.deep.equal([
+        CSP_HEADER,
         ['Cache-Control', 'no-cache, no-store, must-revalidate, private'],
         ['Pragma', 'no-cache'],
         ['X-Frame-Options', 'DENY'],
@@ -28,9 +43,9 @@ describe('headers middleware', () => {
       const resMock = { set: sinon.spy() }
       const nextMock = sinon.spy()
 
-      headers(reqMock, resMock, nextMock)
+      headers(reqMock, resMock, nextMock, nonceGenerator)
 
-      expect(resMock.set).not.to.be.called
+      expect(resMock.set.args).to.be.deep.equal([CSP_HEADER])
 
       expect(nextMock).to.be.called
     })
--- src/middleware/headers.js
+++ src/middleware/headers.js
@@ -4,7 +4,23 @@ const logger = require('../config/logger')
 
 const STS_MAX_AGE = 180 * 24 * 60 * 60
 
-module.exports = function headers(req, res, next) {
+module.exports = function headers(req, res, next, nonceGenerator = uuid) {
+  res.locals ||= {}
+  res.locals.cspNonce = nonceGenerator()
+
+  const selfAndNonce = `'self' 'nonce-${res.locals.cspNonce}'`
+
+  res.set(
+    'Content-Security-Policy',
+    [
+      `default-src ${selfAndNonce}`,
+      // Taken from https://developers.google.com/tag-platform/security/guides/csp#google_analytics_4_google_analytics
+      `script-src ${selfAndNonce} https://*.googletagmanager.com`,
+      `img-src 'self' https://*.google-analytics.com https://*.googletagmanager.com`,
+      `connect-src 'self' https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com`,
+    ].join(';')
+  )
+
   if (
     req.url.indexOf('/css') === -1 &&
     req.url.indexOf('/javascripts') === -1 &&
@@ -12,18 +28,6 @@ module.exports = function headers(req, res, next) {
   ) {
     logger.http('Headers middleware -> Adding response headers')
 
-    res.locals.cspNonce = uuid()
-    const selfAndNonce = `'self' 'nonce-${res.locals.cspNonce}'`
-    res.set(
-      'Content-Security-Policy',
-      [
-        `default-src ${selfAndNonce}`,
-        // Taken from https://developers.google.com/tag-platform/security/guides/csp#google_analytics_4_google_analytics
-        `script-src ${selfAndNonce} https://*.googletagmanager.com`,
-        `img-src 'self' https://*.google-analytics.com https://*.googletagmanager.com`,
-        `connect-src 'self' https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com`,
-      ].join(';')
-    )
     res.set('Cache-Control', 'no-cache, no-store, must-revalidate, private')
     res.set('Pragma', 'no-cache')
     res.set('X-Frame-Options', 'DENY')
