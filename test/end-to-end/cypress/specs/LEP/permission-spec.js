d2b09e939d1af8ad8a637f4e814e473b3ca225fa
Peter Hudec <peterhudec@peterhudec.com>
2024-05-13 13:16:48 +0100

    Revert "Removed permissions test"
    This reverts commit eaacb774da1280b57c2c24202e02a1c7388fc7ae.

---
+++ test/end-to-end/cypress/specs/LEP/permission-spec.js
@@ -0,0 +1,167 @@
+const fixtures = require('../../fixtures')
+const {
+  companies,
+  contacts,
+  events,
+  interactions,
+  omis,
+  search,
+  investments,
+} = require('../../../../../src/lib/urls')
+
+const { assertError } = require('../../support/assertions')
+
+describe('LEP Permission', () => {
+  describe('companies', () => {
+    describe('orders', () => {
+      before(() => {
+        cy.visit(companies.orders(fixtures.company.lambdaPlc.id), {
+          failOnStatusCode: false,
+        })
+      })
+
+      it('should prevent LEP users from accessing the page', () => {
+        assertError("You don't have permission to view this page")
+        assertError('403')
+      })
+    })
+
+    describe('exports', () => {
+      before(() => {
+        cy.visit(companies.exports.index(fixtures.company.lambdaPlc.id), {
+          failOnStatusCode: false,
+        })
+      })
+
+      it('should prevent LEP users from accessing the page', () => {
+        assertError("You don't have permission to view this page")
+        assertError('403')
+      })
+    })
+  })
+
+  describe('event', () => {
+    before(() => {
+      cy.visit(events.index(), { failOnStatusCode: false })
+    })
+
+    it('should prevent LEP users from accessing the page', () => {
+      assertError("You don't have permission to view this page")
+      assertError('403')
+    })
+  })
+
+  describe('interaction', () => {
+    before(() => {
+      cy.visit(interactions.index(), { failOnStatusCode: false })
+    })
+
+    it('should prevent LEP users from accessing the page', () => {
+      assertError("You don't have permission to view this page")
+      assertError('403')
+    })
+  })
+
+  describe('omis', () => {
+    before(() => {
+      cy.visit(omis.index(), { failOnStatusCode: false })
+    })
+
+    it('should prevent LEP users from accessing the page', () => {
+      assertError("You don't have permission to view this page")
+      assertError('403')
+    })
+  })
+
+  context('search', () => {
+    describe('interaction', () => {
+      before(() => {
+        cy.visit(search.type('interactions'), { failOnStatusCode: false })
+      })
+
+      it('should prevent LEP users from accessing the page', () => {
+        assertError("You don't have permission to view this page")
+        assertError('403')
+      })
+    })
+
+    describe('event', () => {
+      before(() => {
+        cy.visit(search.type('events'), { failOnStatusCode: false })
+      })
+
+      it('should prevent LEP users from accessing the page', () => {
+        assertError("You don't have permission to view this page")
+        assertError('403')
+      })
+    })
+
+    describe('omis', () => {
+      before(() => {
+        cy.visit(search.type('omis'), { failOnStatusCode: false })
+      })
+
+      it('should prevent LEP users from accessing the page', () => {
+        assertError("You don't have permission to view this page")
+        assertError('403')
+      })
+    })
+  })
+
+  context('investment', () => {
+    describe('interaction', () => {
+      investmentProjectNewGolf =
+        fixtures.investmentProject.create.newGolfCourseDA()
+
+      before(() => {
+        cy.loadFixture([investmentProjectNewGolf])
+        cy.visit(
+          investments.projects.interactions.index(investmentProjectNewGolf.pk),
+          { failOnStatusCode: false }
+        )
+      })
+
+      it("should prevent LEP users from accessing an interaction they don't have access to", () => {
+        assertError("You don't have permission to view this page")
+        assertError('403')
+      })
+    })
+
+    describe('team', () => {
+      investmentProjectFancyDress =
+        fixtures.investmentProject.create.fancyDressManufacturing()
+
+      before(() => {
+        cy.loadFixture([investmentProjectFancyDress])
+        cy.visit(investments.projects.team(investmentProjectFancyDress.pk), {
+          failOnStatusCode: false,
+        })
+      })
+
+      it("should prevent LEP users from accessing a team they don't have access to", () => {
+        assertError("You don't have permission to view this page")
+        assertError('403')
+      })
+    })
+  })
+
+  context('contacts', () => {
+    describe('interactions', () => {
+      const company = fixtures.company.create.defaultCompany('contact lep')
+      const contact = fixtures.contact.create(company.pk)
+
+      before(() => {
+        cy.loadFixture([company])
+        cy.loadFixture([contact])
+        cy.visit(contacts.contactActivities(contact.pk), {
+          failOnStatusCode: false,
+        })
+      })
+
+      it('should prevent LEP users from accessing the page', () => {
+        assertError("You don't have permission to view this page")
+        assertError('403')
+      })
+    })
+  })
+})
