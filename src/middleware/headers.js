814cfee2d86193d1782fe9c57b2ed4f38edfdb71
Peter Hudec <peterhudec@peterhudec.com>
2024-05-23 16:58:45 +0100

    Comment out the CSP header

--- src/middleware/headers.js
+++ src/middleware/headers.js
@@ -30,22 +30,22 @@ module.exports = function headers(
 
   // We have to enable unsafe-eval in tests because code instrumented
   // for coverage by the Istanbul library ends up with lots of evals
-  const testCoverage = mode === 'test' ? ` 'unsafe-eval'` : ''
+  // const testCoverage = mode === 'test' ? ` 'unsafe-eval'` : ''
 
-  const selfAndNonce = `'self' 'nonce-${res.locals.cspNonce}'`
+  // const selfAndNonce = `'self' 'nonce-${res.locals.cspNonce}'`
 
-  res.set(
-    'Content-Security-Policy',
-    [
-      `default-src ${selfAndNonce}`,
-      // This prevents Data Hub to be loaded in iframes
-      `frame-ancestors 'none'`,
-      // Taken from https://developers.google.com/tag-platform/security/guides/csp#google_analytics_4_google_analytics
-      `script-src ${selfAndNonce} ${GOOGLE_TAG_MNGR}${testCoverage}`,
-      `img-src 'self' ${GOOGLE_ANALYTICS} ${GOOGLE_TAG_MNGR}`,
-      `connect-src 'self' ${GOOGLE_ANALYTICS} ${GOOGLE_TAG_MNGR} https://*.analytics.google.com`,
-    ].join(';')
-  )
+  // res.set(
+  //   'Content-Security-Policy',
+  //   [
+  //     `default-src ${selfAndNonce}`,
+  //     // This prevents Data Hub to be loaded in iframes
+  //     `frame-ancestors 'none'`,
+  //     // Taken from https://developers.google.com/tag-platform/security/guides/csp#google_analytics_4_google_analytics
+  //     `script-src ${selfAndNonce} ${GOOGLE_TAG_MNGR}${testCoverage}`,
+  //     `img-src 'self' ${GOOGLE_ANALYTICS} ${GOOGLE_TAG_MNGR}`,
+  //     `connect-src 'self' ${GOOGLE_ANALYTICS} ${GOOGLE_TAG_MNGR} https://*.analytics.google.com`,
+  //   ].join(';')
+  // )
 
   // This is equivalent to `frame-ancestors 'none'` in the above CSP policy,
   // but keeping it here for older browsers
