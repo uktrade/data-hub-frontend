4afb74cee3070626bfef4e6a7185bb27275e1df8
Oliver Roberts <oliver.roberts@digital.trade.gov.uk>
2024-05-29 09:32:25 +0100

    Remove superfluous else
    Co-authored-by: Sophie Wenban <74198488+swenban@users.noreply.github.com>

--- src/client/modules/Investments/Projects/Details/validators.js
+++ src/client/modules/Investments/Projects/Details/validators.js
@@ -2,7 +2,6 @@ export const capitalExpenditureValidator = (value) => {
   const minCapitalExpenditure = 15000000
   if (parseInt(value) < minCapitalExpenditure) {
     return 'Capital expenditure must be >= £15,000,000 for capital only project'
-  } else {
-    return null
   }
+  return null
 }
