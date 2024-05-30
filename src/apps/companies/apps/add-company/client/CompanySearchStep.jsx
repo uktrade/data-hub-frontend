4b20e2d50071edf605bbb80ba4951313f42eeb96
Paul Gain <paul@javexconsulting.com>
2024-05-30 11:25:33 +0100

    Fix functional tests - adding a company to DH

--- src/apps/companies/apps/add-company/client/CompanySearchStep.jsx
+++ src/apps/companies/apps/add-company/client/CompanySearchStep.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable camelcase */
 
 import React from 'react'
+import { flushSync } from 'react-dom'
 import { Link } from 'govuk-react'
 import PropTypes from 'prop-types'
 import { get } from 'lodash'
@@ -78,8 +79,25 @@ function CompanySearchStep({
         country={countryName}
         entityRenderer={DnbCompanyRenderer}
         onCannotFind={() => {
-          setFieldValue('cannotFind', true)
-          goForward()
+          // React v18 batching is causing issues with the sequencing of form steps.
+          // https://github.com/reactwg/react-18/discussions/21
+
+          // The CompanyNotFoundStep, where the user manually adds a company to Data Hub via
+          // a form, is skipped altogether. If time permits, we should refactor the code to
+          // include batch updates, as this would avoid unnecessary re-renders. However, this
+          // task is beyond the scope of upgrading React to the latest version. We should
+          // revisit this when we move the entire app to src/client/modules, which is necessary.
+          // For now, opting out of batching allows our functional tests to pass.
+
+          // Opt out of React v18 batching.
+          flushSync(() => {
+            setFieldValue('cannotFind', true)
+          })
+
+          // Opt out of React v18 batching.
+          flushSync(() => {
+            goForward()
+          })
         }}
         features={features}
       />
