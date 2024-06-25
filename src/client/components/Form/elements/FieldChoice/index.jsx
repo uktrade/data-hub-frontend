b422b1b22d18d28d941562c9cbd9b2492f972bd9
Paul Gain <paul@javexconsulting.com>
2024-06-25 16:12:57 +0100

    Add FieldChoice component

---
+++ src/client/components/Form/elements/FieldChoice/index.jsx
@@ -0,0 +1,101 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import MultiChoice from '@govuk-react/multi-choice'
+import Checkbox from '@govuk-react/checkbox'
+import Radio from '@govuk-react/radio'
+import styled from 'styled-components'
+
+import { useField, useFormContext } from '../../hooks'
+import FieldWrapper from '../FieldWrapper'
+
+const StyledMultiChoice = styled(MultiChoice)(({ inline }) => ({
+  ...(inline
+    ? {
+        display: 'flex',
+        marginRight: '10px',
+      }
+    : {}),
+}))
+
+const isRadio = (type) => type === 'radio'
+
+const getInitialValue = (initialValue, type) => {
+  if (initialValue) {
+    return initialValue
+  }
+  return isRadio(type) ? '' : []
+}
+
+const FieldChoice = ({
+  type,
+  name,
+  validate,
+  required,
+  label,
+  legend,
+  hint,
+  inline,
+  initialValue,
+  options = [],
+  ...props
+}) => {
+  const initValue = getInitialValue(initialValue, type)
+  const { value, error, touched, onBlur } = useField({
+    name,
+    validate,
+    required,
+    initialValue: initValue,
+  })
+
+  const { setFieldValue } = useFormContext()
+
+  const onChange = (option, type) => {
+    if (isRadio(type)) {
+      setFieldValue(name, option)
+    } else {
+      // Checkbox
+      setFieldValue(
+        name,
+        value.includes(option)
+          ? value.filter((v) => v !== option)
+          : [...value, option]
+      )
+    }
+  }
+
+  const checked = (option) =>
+    isRadio(type) ? value === option : value.includes(option)
+
+  const Component = isRadio(type) ? Radio : Checkbox
+
+  return (
+    <FieldWrapper {...{ ...props, name, label, legend, hint, error }}>
+      <StyledMultiChoice meta={{ error, touched }} inline={inline}>
+        {options.map((option) => (
+          <React.Fragment key={option.value}>
+            <Component
+              key={option.id}
+              value={option.value}
+              checked={checked(option, type)}
+              onChange={() => onChange(option, type)}
+              onBlur={onBlur}
+              name={name}
+              aria-label={option.label}
+            >
+              {option.label}
+            </Component>
+          </React.Fragment>
+        ))}
+      </StyledMultiChoice>
+    </FieldWrapper>
+  )
+}
+
+FieldChoice.Checkbox = (props) => <FieldChoice {...props} type="checkbox" />
+FieldChoice.Radio = (props) => <FieldChoice {...props} type="radio" />
+
+FieldChoice.propTypes = {
+  type: PropTypes.oneOf(['radio', 'checkbox']).isRequired,
+}
+
+export default FieldChoice
