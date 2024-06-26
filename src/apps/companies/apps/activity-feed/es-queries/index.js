63a78ea0ba3a6fc5f74644ad7559603166eb2de1
Christopher <christopher.sunkel@digital.trade.gov.uk>
2024-06-26 17:29:26 +0100

    Remove deprecated code related to interactions

--- src/apps/companies/apps/activity-feed/es-queries/index.js
+++ src/apps/companies/apps/activity-feed/es-queries/index.js
@@ -1,9 +1,7 @@
 const dataHubActivityQuery = require('./data-hub-activity-query')
 const myActivityQuery = require('./my-activity-query')
-const exportSupportServiceDetailQuery = require('./export-support-service-detail-query')
 
 module.exports = {
   myActivityQuery,
   dataHubActivityQuery,
-  exportSupportServiceDetailQuery,
 }
--- src/apps/companies/apps/activity-feed/repos.js
+++ src/apps/companies/apps/activity-feed/repos.js
@@ -3,13 +3,6 @@ const { get } = require('lodash')
 const config = require('../../../../config')
 const { authorisedRequest } = require('../../../../lib/authorised-request')
 
-async function fetchActivityFeed(req, body) {
-  return await authorisedRequest(req, {
-    url: `${config.apiRoot}/v4/activity-feed`,
-    body,
-  })
-}
-
 // istanbul ignore next: Covered by functional tests
 async function fetchUserFeatureFlags(req) {
   // istanbul ignore next: Covered by functional tests
@@ -17,6 +10,5 @@ async function fetchUserFeatureFlags(req) {
   return get(user, 'active_features', [])
 }
 module.exports = {
-  fetchActivityFeed,
   fetchUserFeatureFlags,
 }
