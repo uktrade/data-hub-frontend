628717405a9879e0c3c705294bcefaa71cd38767
Marijn Kampf <marijn.kampf@digital.trade.gov.uk>
2024-07-11 11:46:52 +0100

    Remove parameters so tests can also run on Gov PaaS.

--- tests/e2e/utils.ts
+++ tests/e2e/utils.ts
@@ -1,14 +1,15 @@
 export const BASE_URLS = {
   LOCALHOST: 'http://127.0.0.1:3000',
-  DEV: 'https://dev.datahub.uktrade.digital/',
-  STAGING: 'https://staging.datahub.uktrade.digital/',
-  UAT: 'https://uat.datahub.uktrade.digital/',
+  DEV: 'https://dev.datahub.uktrade.digital',
+  STAGING: 'https://staging.datahub.uktrade.digital',
+  UAT: 'https://uat.datahub.uktrade.digital',
   TEMP_PROD: 'https://datahub.prod.uktrade.digital',
+  PROD: 'https://www.datahub.trade.gov.uk',
 };
 
 export function getCompanyId(baseURL:string):string {
   // Returns an id of a company in the database
-  if (baseURL == BASE_URLS.TEMP_PROD) {
+  if (baseURL == BASE_URLS.TEMP_PROD || baseURL == BASE_URLS.PROD) {
     // Company in DB with Company Tree
     return 'eca13ac7-a098-e211-a939-e4115bead28a'
   }
