197bd777a6cab221dc8d4b9261838dc10ecf0b56
Marijn Kampf <marijn.kampf@digital.trade.gov.uk>
2024-07-11 08:40:47 +0100

    Use set of constants for common baseURLs

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
+import { BASE_URLS } from './tests/e2e/utils';
 
 /**
  * Read environment variables from file.
@@ -26,9 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-    // baseURL: 'https://dev.datahub.uktrade.digital/',
-    baseURL: 'https://datahub.prod.uktrade.digital',
+    baseURL: BASE_URLS.LOCALHOST,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
--- tests/e2e/utils.ts
+++ tests/e2e/utils.ts
@@ -1,6 +1,14 @@
+export const BASE_URLS = {
+  LOCALHOST: 'http://127.0.0.1:3000',
+  DEV: 'https://dev.datahub.uktrade.digital/',
+  STAGING: 'https://staging.datahub.uktrade.digital/',
+  UAT: 'https://uat.datahub.uktrade.digital/',
+  TEMP_PROD: 'https://datahub.prod.uktrade.digital',
+};
+
 export function getCompanyId(baseURL:string):string {
   // Returns an id of a company in the database
-  if (baseURL == 'https://datahub.prod.uktrade.digital') {
+  if (baseURL == BASE_URLS.TEMP_PROD) {
     // Company in DB with Company Tree
     return 'eca13ac7-a098-e211-a939-e4115bead28a'
   }
