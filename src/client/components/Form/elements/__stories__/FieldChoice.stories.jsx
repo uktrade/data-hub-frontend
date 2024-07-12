21076b23a4cf37ba9ebb2845ac973d415f519b3f
Paul Gain <paul@javexconsulting.com>
2024-07-12 14:13:28 +0100

    Refactor FieldChoice stories

--- src/client/components/Form/elements/__stories__/FieldChoice.stories.jsx
+++ src/client/components/Form/elements/__stories__/FieldChoice.stories.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { H1 } from '@govuk-react/heading'
 import styled, { css } from 'styled-components'
 
 import FieldChoice from '../FieldChoice'
@@ -22,6 +21,69 @@ const FieldChoiceCheckboxInline = styled(FieldChoice.Checkbox)`
   ${inline}
 `
 
+const RADIO = 'FieldChoice.Radio'
+const RADIO_INLINE = 'FieldChoiceRadioInline'
+const CHECKBOX = 'FieldChoice.Checkbox'
+const CHECKBOX_INLINE = 'FieldChoiceCheckboxInline'
+
+const COMPONENT_TYPE = {
+  RADIO,
+  RADIO_INLINE,
+  CHECKBOX,
+  CHECKBOX_INLINE,
+}
+
+const getCode = ({ component, props }) => `
+<Form>
+  <${component}
+   ${props}
+  />
+</Form>
+`
+
+const getCodeWithComment = ({ component, props, comment }) => `
+${comment} ${getCode({ component, props })}
+`
+
+const getParameters = ({ story, component, props, comment }) => ({
+  docs: {
+    description: {
+      story,
+    },
+    source: {
+      code: comment
+        ? getCodeWithComment({
+            component,
+            props,
+            comment,
+          })
+        : getCode({
+            component,
+            props,
+          }),
+    },
+  },
+})
+
+const formatCodeComments = (comments) =>
+  comments.map((comment) => `// ${comment.replace(/,/g, '')}`).join('\n')
+
+const formatOptionFlat = (option) =>
+  `{ value: '${option.value}', label: '${option.label}' }`
+
+const formatOptionsFlat = (options) => options.map(formatOptionFlat)
+
+const formatOption = (option) => `
+    {
+      value: '${option.value}',
+      label: '${option.label}',
+    }`
+
+const formatOptions = (options) => options.map(formatOption)
+
+const formatProps = (props) =>
+  props.map((prop) => prop.replace(/,/g, '')).join('\n   ')
+
 const options = [
   {
     value: '0',
@@ -41,10 +103,12 @@ const options = [
   },
 ]
 
-const formatOption = (option) =>
-  `{ value: '${option.value}', label: '${option.label}' }`
+const formattedOptions = formatOptions(options)
 
-const formatOptions = (options) => options.map(formatOption) //.join(', ')
+const initialValueComments = [
+  'The form will automatically set the initial values providing the name',
+  "field is set within the object that's returned by a transformer.",
+]
 
 export default {
   title: 'Form/Form Elements/FieldChoice',
@@ -61,9 +125,9 @@ export default {
   parameters: {
     docs: {
       description: {
-        component: `The <b>FieldChoice</b> component is designed to render a group of radio buttons or checkboxes by setting the prop type to either <b>"radio"</b> or <b>"checkbox"</b>.
-        The component sets the entire selected option (Object) to the Form's state which is helpful on user journeys where the final page is a summary page and you
-        need to pullout a name (or any other field) from a previous selection to display it to the user. At present, <b>FieldRadio</b> only writes the ID (String) to the form state.`,
+        component: `The <b>FieldChoice</b> component renders a group of radio buttons or checkboxes by setting the prop type to either <b>"radio"</b> or <b>"checkbox"</b>.
+        The component sets the entire selected option to the form's state which is helpful on user journeys where the final page is a summary page and you need to extract a label
+        (or any other field) from a previous selection to display to the user.`,
       },
     },
   },
@@ -88,299 +152,206 @@ const Template = (
   </Form>
 )
 
-const defaultFormProps = `\n  id="form-id"\n  analyticsFormName="formRadio"\n  submissionTaskName="SUBMISSION"`
-const defaultComponentProps = `name="country"`
-
-const getForm = ({ component, formProp, componentProp }) => {
-  const formProps = formProp
-    ? `${defaultFormProps}\n  ${formProp}`
-    : ` ${defaultFormProps}`
-
-  const componentProps = componentProp
-    ? `${defaultComponentProps}
-      ${componentProp}`
-    : `${defaultComponentProps}`
-  return `
-<Form ${formProps}>
-  {(state) => (
-    <${component}
-      ${componentProps}
-      options={[
-        ${formatOption(options[0])}
-        ${formatOption(options[1])}
-        ${formatOption(options[2])}
-        ${formatOption(options[3])}
-      ]}
-    />
-  )}
-</Form>
-`
-}
-
 // Radio
 export const Radio = Template.bind({})
-Radio.args = {
-  type: 'radio',
-}
-Radio.parameters = {
-  docs: {
-    description: {
-      story: 'A group of 4 radio buttons',
-    },
-    source: {
-      code: getForm({ component: 'FieldChoice.Radio' }),
-    },
-  },
-}
+Radio.args = { type: 'radio' }
+Radio.parameters = getParameters({
+  story: 'A group of 4 radio buttons',
+  component: COMPONENT_TYPE.RADIO,
+  props: formatProps(['name="country"', `options=[${formattedOptions}]`]),
+})
 
 export const RadioLabel = Template.bind({})
 RadioLabel.args = {
   ...Radio.args,
-  label: 'Countries',
+  label: 'Where do you live',
 }
-RadioLabel.parameters = {
-  docs: {
-    description: {
-      story: 'Radio button group label',
-    },
-    source: {
-      code: getForm({
-        component: 'FieldChoice.Radio',
-        componentProp: 'label="Countries"',
-      }),
-    },
-  },
-}
+RadioLabel.parameters = getParameters({
+  story: 'Radio button group label',
+  component: COMPONENT_TYPE.RADIO,
+  props: formatProps([
+    'name="country"',
+    'label="Where do you live"',
+    `options=[${formattedOptions}]`,
+  ]),
+})
 
-export const RadioHint = Template.bind({})
-RadioHint.args = {
+export const RadioLabelAndHint = Template.bind({})
+RadioLabelAndHint.args = {
   ...Radio.args,
-  hint: 'Country hint',
+  label: 'Where do you live',
+  hint: 'Select one option',
 }
-RadioHint.parameters = {
-  docs: {
-    description: {
-      story: 'Radio button group hint text',
-    },
-    source: {
-      code: getForm({
-        component: 'FieldChoice.Radio',
-        componentProp: 'hint="Country hint"',
-      }),
-    },
-  },
-}
+RadioLabelAndHint.parameters = getParameters({
+  story: 'Radio button group label and hint',
+  component: COMPONENT_TYPE.RADIO,
+  props: formatProps([
+    'name="country"',
+    'label="Where do you live"',
+    'hint="Select one option"',
+    `options=[${formattedOptions}]`,
+  ]),
+})
 
 export const RadioLegend = Template.bind({})
 RadioLegend.args = {
   ...Radio.args,
-  legend: <h1>My H1 legend</h1>,
+  legend: <h1>H1 legend</h1>,
 }
-RadioLegend.parameters = {
-  docs: {
-    description: {
-      story: 'Radio button group legend',
-    },
-    source: {
-      code: getForm({
-        component: 'FieldChoice.Radio',
-        componentProp: 'legend={<H1>My H1 legend</H1>}',
-      }),
-    },
-  },
-}
+RadioLegend.parameters = getParameters({
+  story: 'Radio button group legend',
+  component: COMPONENT_TYPE.RADIO,
+  props: formatProps([
+    'name="country"',
+    'legend={<h1>H1 legend</h1>}',
+    `options=[${formattedOptions}]`,
+  ]),
+})
 
 export const RadioInline = Template.bind({})
 RadioInline.args = {
   ...Radio.args,
   component: FieldChoiceRadioInline,
 }
-RadioInline.parameters = {
-  docs: {
-    description: {
-      story: 'Radio button group inline',
-    },
-    source: {
-      code: getForm({
-        component: 'FieldChoiceRadioInline',
-      }),
-    },
-  },
-}
+RadioInline.parameters = getParameters({
+  story: 'Radio button group inline',
+  component: COMPONENT_TYPE.RADIO_INLINE,
+  props: formatProps(['name="country"', `options=[${formattedOptions}]`]),
+})
 
 export const RadioRequired = Template.bind({})
 RadioRequired.args = {
   ...Radio.args,
   required: 'Select at least one country',
 }
-RadioRequired.parameters = {
-  docs: {
-    description: {
-      story:
-        'Radio button group where a selection is required. Click "Save" to view the form validation error message.',
-    },
-    source: {
-      code: getForm({
-        component: 'FieldChoice.Radio',
-        componentProp: 'required="Select at least one country"',
-      }),
-    },
-  },
-}
+RadioRequired.parameters = getParameters({
+  story:
+    'Radio button group where a selection is required. Click "Save" to view an error.',
+  component: COMPONENT_TYPE.RADIO,
+  props: formatProps([
+    'name="country"',
+    'required="Select at least one country"',
+    `options=[${formattedOptions}]`,
+  ]),
+})
 
 export const RadioSelected = Template.bind({})
 RadioSelected.args = {
   ...Radio.args,
   initialValues: {
-    // Radios require a single object
     country: options[0],
   },
 }
-RadioSelected.parameters = {
-  docs: {
-    description: {
-      story: 'Radio button group selected',
-    },
-    source: {
-      code: getForm({
-        component: 'FieldChoice.Radio',
-        formProp: `initialValues={{country: ${formatOption(options[0])}}}`,
-      }),
-    },
-  },
-}
+RadioSelected.parameters = getParameters({
+  story: 'Radio button group selected',
+  comment: formatCodeComments([
+    ...initialValueComments,
+    `For example: { country: ${formatOptionFlat(options[0])} }`,
+  ]),
+  component: COMPONENT_TYPE.RADIO,
+  props: formatProps(['name="country"', `options=[${formattedOptions}]`]),
+})
 
 // Checkbox
 export const Checkbox = Template.bind({})
 Checkbox.args = {
   type: 'checkbox',
+  name: 'destination',
 }
-Checkbox.parameters = {
-  docs: {
-    description: {
-      story: 'A group of 4 checkboxes.',
-    },
-    source: {
-      code: getForm({
-        component: 'FieldChoice.Checkbox',
-      }),
-    },
-  },
-}
+Checkbox.parameters = getParameters({
+  story: 'A group of 4 checkboxes.',
+  component: COMPONENT_TYPE.CHECKBOX,
+  props: formatProps(['name=destination', `options=[${formattedOptions}]`]),
+})
 
 export const CheckboxLabel = Template.bind({})
 CheckboxLabel.args = {
   ...Checkbox.args,
-  label: 'Countries',
+  label: 'Will you be travelling to any of these countries?',
 }
-CheckboxLabel.parameters = {
-  docs: {
-    description: {
-      story: 'Checkbox group label',
-    },
-    source: {
-      code: getForm({
-        component: 'FieldChoice.Checkbox',
-        componentProp: 'label="Countries"',
-      }),
-    },
-  },
-}
+CheckboxLabel.parameters = getParameters({
+  story: 'Checkbox group label',
+  component: COMPONENT_TYPE.CHECKBOX,
+  props: formatProps([
+    'name=destination',
+    'label="Will you be travelling to any of these countries?"',
+    `options=[${formattedOptions}]`,
+  ]),
+})
 
-export const CheckboxHint = Template.bind({})
-CheckboxHint.args = {
+export const CheckboxLabelAndHint = Template.bind({})
+CheckboxLabelAndHint.args = {
   ...Checkbox.args,
-  hint: 'Country hint',
+  label: 'Will you be travelling to any of these countries?',
+  hint: 'Select all that apply',
 }
-CheckboxHint.parameters = {
-  docs: {
-    description: {
-      story: 'Checkbox group hint text',
-    },
-    source: {
-      code: getForm({
-        component: 'FieldChoice.Checkbox',
-        componentProp: 'hint="Country hint"',
-      }),
-    },
-  },
-}
+CheckboxLabelAndHint.parameters = getParameters({
+  story: 'Checkbox group hint text',
+  component: COMPONENT_TYPE.CHECKBOX,
+  props: formatProps([
+    'name=destination',
+    'label="Will you be travelling to any of these countries?"',
+    'hint="Select all that apply"',
+    `options=[${formattedOptions}]`,
+  ]),
+})
 
 export const CheckboxLegend = Template.bind({})
 CheckboxLegend.args = {
   ...Checkbox.args,
-  legend: <H1>My H1 legend</H1>,
+  legend: <h1>Will you be travelling to any of these countries?</h1>,
 }
-CheckboxLegend.parameters = {
-  docs: {
-    description: {
-      story: 'Checkbox group legend',
-    },
-    source: {
-      code: getForm({
-        component: 'FieldChoice.Checkbox',
-        componentProp: 'legend={<H1>My H1 legend</H1>}',
-      }),
-    },
-  },
-}
+CheckboxLegend.parameters = getParameters({
+  story: 'Checkbox group legend',
+  component: COMPONENT_TYPE.CHECKBOX,
+  props: formatProps([
+    'name=destination',
+    'legend={<h1>Will you be travelling to any of these countries?</h1>}',
+    `options=[${formattedOptions}]`,
+  ]),
+})
 
 export const CheckboxInline = Template.bind({})
 CheckboxInline.args = {
   ...Checkbox.args,
   component: FieldChoiceCheckboxInline,
 }
-CheckboxInline.parameters = {
-  docs: {
-    description: {
-      story: 'Checkbox group inline',
-    },
-    source: {
-      code: getForm({
-        component: 'FieldChoiceCheckboxInline',
-      }),
-    },
-  },
-}
+CheckboxInline.parameters = getParameters({
+  story: 'Checkbox group inline',
+  component: COMPONENT_TYPE.CHECKBOX_INLINE,
+  props: formatProps(['name=destination', `options=[${formattedOptions}]`]),
+})
 
 export const CheckboxRequired = Template.bind({})
 CheckboxRequired.args = {
   ...Checkbox.args,
   required: 'Choose one or more countries',
 }
-CheckboxRequired.parameters = {
-  docs: {
-    description: {
-      story:
-        'Checkbox group where a user must choose at least one country. Click "Save" to view the form validation error message.',
-    },
-    source: {
-      code: getForm({
-        component: 'FieldChoice.Checkbox',
-        componentProp: 'required="Choose one or more countries"',
-      }),
-    },
-  },
-}
+CheckboxRequired.parameters = getParameters({
+  story:
+    'Checkbox group where a user must choose at least one country. Click "Save" to view an error.',
+  component: COMPONENT_TYPE.CHECKBOX,
+  props: formatProps([
+    'name=destination',
+    'required=Choose one or more countries',
+    `options=[${formattedOptions}]`,
+  ]),
+})
 
 export const CheckboxChecked = Template.bind({})
 CheckboxChecked.args = {
   ...Checkbox.args,
   initialValues: {
     // Checkboxes require an array of objects
-    country: [options[0], options[1]],
+    destination: [options[0], options[1]],
   },
 }
-CheckboxChecked.parameters = {
-  docs: {
-    description: {
-      story: 'Checkbox group checked',
-    },
-    source: {
-      code: getForm({
-        component: 'FieldChoice.Checkbox',
-        formProp: `initialValues={{ country: [${formatOptions([options[0], options[1]])}]}}`,
-      }),
-    },
-  },
-}
+CheckboxChecked.parameters = getParameters({
+  story: 'Checkbox group checked',
+  component: COMPONENT_TYPE.CHECKBOX,
+  props: formatProps(['name=destination', `options=[${formattedOptions}]`]),
+  comment: formatCodeComments([
+    ...initialValueComments,
+    `For example: { destination: [${formatOptionsFlat([options[0], options[1]])}] `,
+  ]),
+})
