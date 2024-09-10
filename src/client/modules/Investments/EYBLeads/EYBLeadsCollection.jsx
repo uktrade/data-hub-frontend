137ea9340466fba253431c788c91fdb9e8708aaa
Claudia Gonzalez-Casales <claudia.gonzalescasales@digital.trade.gov.uk>
2024-09-10 17:03:57 +0100

    WIP: Display company search filter

--- src/client/modules/Investments/EYBLeads/EYBLeadsCollection.jsx
+++ src/client/modules/Investments/EYBLeads/EYBLeadsCollection.jsx
@@ -15,20 +15,26 @@ import {
 import { sanitizeFilter } from '../../../filters'
 
 const QS_PARAMS = {
-  eybLeadCompanyName: 'eybLead_company_name',
+  eybLeadCompanyName: 'eyb_lead_company_name',
+  // assetClassesOfInterest: 'asset_classes_of_interest',
 }
 
-const resolveSelectedOptions = (values = [], options = []) =>
-  values
-    .map((id) => options.filter(({ value }) => value === id)[0])
-    .filter(Boolean)
+// const resolveSelectedOptions = (values = [], options = []) =>
+//   values
+//     .map((id) => options.filter(({ value }) => value === id)[0])
+//     .filter(Boolean)
 
 const EYBLeadCollection = ({ count, results, isComplete, filterOptions }) => {
   const qsParams = qs.parse(location.search.slice(1))
-  const selectedAssetClassesOfInterest = resolveSelectedOptions(
-    qsParams[QS_PARAMS.assetClassesOfInterest],
-    filterOptions.assetClassesOfInterest
-  )
+  // const selectedAssetClassesOfInterest = resolveSelectedOptions(
+  //   qsParams[QS_PARAMS.assetClassesOfInterest],
+  //   filterOptions.assetClassesOfInterest
+  // )
+
+  const resolveSelectedEYBLeadCompanyName = () => {
+    const companyName = qsParams[QS_PARAMS.eybLeadCompanyName]
+    return companyName ? [{ label: companyName, value: companyName }] : []
+  }
   return (
     <FilteredCollectionList
       count={count}
@@ -39,9 +45,9 @@ const EYBLeadCollection = ({ count, results, isComplete, filterOptions }) => {
         ...sanitizeFilter(eybLeadCompanyName, 'Company name'),
       })}
       taskProps={{
-        name: TASK_GET_PROFILES_LIST,
-        id: ID,
-        progressMessage: 'Loading profiles',
+        // name: TASK_GET_PROFILES_LIST,
+        // id: ID,
+        progressMessage: 'Loading EYB leads',
         renderProgress: listSkeletonPlaceholder({
           listItemFields: 2,
         }),
@@ -51,26 +57,26 @@ const EYBLeadCollection = ({ count, results, isComplete, filterOptions }) => {
             assetClassesOfInterest: qsParams[QS_PARAMS.assetClassesOfInterest],
             eybLeadCompanyName: qsParams[QS_PARAMS.eybLeadCompanyName],
           },
-          onSuccessDispatch: INVESTMENTS__PROFILES_LOADED,
+          // onSuccessDispatch: INVESTMENTS__PROFILES_LOADED,
         },
       }}
-      baseDownloadLink="/investments/profiles/export"
+      // baseDownloadLink="/investments/profiles/export"
       entityName="eybLead"
       selectedFilters={{
         eybLeadCompanyName: {
-          queryParam: QS_PARAMS.investorCompanyName,
+          queryParam: QS_PARAMS.eybLeadCompanyName,
           options: resolveSelectedEYBLeadCompanyName(),
         },
-        assetClassesOfInterest: {
-          queryParam: QS_PARAMS.assetClassesOfInterest,
-          options: selectedAssetClassesOfInterest,
-        },
+        //   assetClassesOfInterest: {
+        //     queryParam: QS_PARAMS.assetClassesOfInterest,
+        //     options: selectedAssetClassesOfInterest,
+        //   },
       }}
     >
       <CollectionFilters
         taskProps={{
-          name: 'Large investment profiles filters',
-          id: 'investments/profiles',
+          name: 'EYB leads filters',
+          id: 'investments/eyb-leads',
           progressMessage: 'Loading filters',
           renderProgress: () => (
             <>
@@ -83,10 +89,10 @@ const EYBLeadCollection = ({ count, results, isComplete, filterOptions }) => {
         }}
       >
         <FilterToggleSection
-          id="FilteredLargeCapitalProfileCollection.core-filters"
+          id="FilteredEYBLeadCollection.core-filters"
           label="Core filters"
         >
-          <Filters.Typeahead
+          {/* <Filters.Typeahead
             isMulti={true}
             label="Sector of interest"
             name="asset-class"
@@ -95,158 +101,13 @@ const EYBLeadCollection = ({ count, results, isComplete, filterOptions }) => {
             options={filterOptions.assetClassesOfInterest}
             selectedOptions={selectedAssetClassesOfInterest}
             data-test="asset-class-filter"
-          />
-          <Filters.Typeahead
-            isMulti={true}
-            label="Country of company origin"
-            name="country"
-            qsParam={QS_PARAMS.countryOfOrigin}
-            placeholder="Search country"
-            options={filterOptions.countries}
-            selectedOptions={selectedCountries}
-            data-test="country-filter"
-          />
+          /> */}
           <Filters.Input
-            id="LargeCapitalProfileCollection.investor-company-name"
-            qsParam="investor_company_name"
-            name="investor-company-name"
+            id="EYBLeadCollection.eyb-lead-company-name"
+            qsParam="eyb_lead_company_name"
+            name="eyb-lead-company-name"
             label="Company name"
             placeholder="Search company name"
-          />
-        </FilterToggleSection>
-        <FilterToggleSection
-          id="FilteredLargeCapitalProfileCollection.investor-details-filters"
-          label="Investor details"
-        >
-          <Filters.Typeahead
-            isMulti={true}
-            label="Investor type"
-            name="investor-type"
-            qsParam={QS_PARAMS.investorTypes}
-            placeholder="Search investor type"
-            options={filterOptions.investorTypes}
-            selectedOptions={selectedInvestorTypes}
-            data-test="investor-type-filter"
-          />
-          <Filters.NumericRange
-            qsParam={QS_PARAMS.globalAssetsUnderManagement}
-            id="LargeCapitalProfileCollection.global-assets-under-management"
-            label="Global assets under management £"
-          />
-          <Filters.NumericRange
-            qsParam={QS_PARAMS.investableCapital}
-            id="LargeCapitalProfileCollection.investable-capital"
-            label="Investable capital £"
-          />
-          <Filters.Typeahead
-            isMulti={true}
-            label="Check clearance"
-            name="required-checks-conducted"
-            qsParam={QS_PARAMS.requiredChecksConducted}
-            placeholder="Search clearance"
-            options={filterOptions.requiredChecksConducted}
-            selectedOptions={selectedRequiredChecksConducted}
-            data-test="required-checks-conducted-filter"
-          />
-        </FilterToggleSection>
-        <FilterToggleSection
-          id="FilteredLargeCapitalProfileCollection.investor-requirements-filters"
-          label="Investor requirements"
-        >
-          <Filters.Typeahead
-            isMulti={true}
-            label="Deal ticket size"
-            name="deal-ticket-size"
-            qsParam={QS_PARAMS.dealTicketSize}
-            placeholder="Search deal ticket size"
-            options={filterOptions.dealTicketSize}
-            selectedOptions={selectedDealTicketSize}
-            data-test="deal-ticket-size-filter"
-          />
-          <Filters.Typeahead
-            isMulti={true}
-            label="Types of investment"
-            name="types-of-investment"
-            qsParam={QS_PARAMS.investmentTypes}
-            placeholder="Search types of investment"
-            options={filterOptions.investmentTypes}
-            selectedOptions={selectedInvestmentTypes}
-            data-test="types-of-investment-filter"
-          />
-          <Filters.Typeahead
-            isMulti={true}
-            label="Minimum return rate"
-            name="minimum-return-rate"
-            qsParam={QS_PARAMS.minimumReturnRate}
-            placeholder="Search return rate"
-            options={filterOptions.minimumReturnRate}
-            selectedOptions={selectedMinimumReturnRate}
-            data-test="minimum-return-rate-filter"
-          />
-          <Filters.Typeahead
-            isMulti={true}
-            label="Time horizon"
-            name="time-horizon-tenor"
-            qsParam={QS_PARAMS.timeHorizon}
-            placeholder="Search time horizon"
-            options={filterOptions.timeHorizon}
-            selectedOptions={selectedTimeHorizon}
-            data-test="time-horizon-tenor-filter"
-          />
-          <Filters.Typeahead
-            isMulti={true}
-            label="Restrictions and conditions"
-            name="restrictions-conditions"
-            qsParam={QS_PARAMS.restrictions}
-            placeholder="Search restrictions"
-            options={filterOptions.restrictions}
-            selectedOptions={selectedRestrictions}
-            data-test="restrictions-conditions-filter"
-          />
-          <Filters.Typeahead
-            isMulti={true}
-            label="Construction risk"
-            name="construction-risk"
-            qsParam={QS_PARAMS.constructionRisk}
-            placeholder="Search construction risk"
-            options={filterOptions.constructionRisk}
-            selectedOptions={selectedConstructionRisk}
-            data-test="construction-risk-filter"
-          />
-          <Filters.Typeahead
-            isMulti={true}
-            label="Minimum equity percentage"
-            name="minimum-equity-percentage"
-            qsParam={QS_PARAMS.minimumEquityPercentage}
-            placeholder="Search equity percentage"
-            options={filterOptions.minimumEquityPercentage}
-            selectedOptions={selectedMinimumEquityPercentage}
-            data-test="minimum-equity-percentage-filter"
-          />
-          <Filters.Typeahead
-            isMulti={true}
-            label="Desired deal role"
-            name="desired-deal-role"
-            qsParam={QS_PARAMS.desiredDealRole}
-            placeholder="Search desired deal role"
-            options={filterOptions.desiredDealRole}
-            selectedOptions={selectedDesiredDealRole}
-            data-test="desired-deal-role-filter"
-          />
-        </FilterToggleSection>
-        <FilterToggleSection
-          id="FilteredLargeCapitalProfileCollection.location-filters"
-          label="Location"
-        >
-          <Filters.Typeahead
-            isMulti={true}
-            label="UK regions of interest"
-            name="uk-regions-of-interest"
-            qsParam={QS_PARAMS.ukRegionsOfInterest}
-            placeholder="Search UK region"
-            options={filterOptions.ukRegionsOfInterest}
-            selectedOptions={selectedUkRegionsOfInterest}
-            data-test="uk-regions-of-interest"
           />
         </FilterToggleSection>
       </CollectionFilters>
