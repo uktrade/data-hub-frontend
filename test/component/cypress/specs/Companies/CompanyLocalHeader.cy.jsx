20426d2b394053ebe6d38c1fd229a98a4c61aa6f
Peter Hudec <peterhudec@peterhudec.com>
2024-09-11 12:05:27 +0100

    Fixed wrong value of null assigned to the flashMessages prop

--- test/component/cypress/specs/Companies/CompanyLocalHeader.cy.jsx
+++ test/component/cypress/specs/Companies/CompanyLocalHeader.cy.jsx
@@ -179,7 +179,6 @@ describe('CompanyLocalHeader', () => {
       cy.mountWithProvider(
         <CompanyLocalHeader
           breadcrumbs={[{ text: 'Test breadcrumb' }]}
-          flashMessages={null}
           company={dnbGlobalUltimate}
           dnbRelatedCompaniesCount={12345}
           returnUrl={null}
@@ -228,7 +227,6 @@ describe('CompanyLocalHeader', () => {
       cy.mountWithProvider(
         <CompanyLocalHeader
           breadcrumbs={[{ text: 'Test breadcrumb' }]}
-          flashMessages={null}
           company={dnbUnderInvestigation}
           dnbRelatedCompaniesCount={null}
           returnUrl={null}
@@ -282,7 +280,6 @@ describe('CompanyLocalHeader', () => {
       cy.mountWithProvider(
         <CompanyLocalHeader
           breadcrumbs={[{ text: 'Test breadcrumb' }]}
-          flashMessages={null}
           company={archivedCompany}
           dnbRelatedCompaniesCount={null}
           returnUrl={null}
