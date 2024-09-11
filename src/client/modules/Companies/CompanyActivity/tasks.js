387c207dd5f75ce17b36f7e87c92f4c246b7d059
Dean Elliott <dean.elliott@digital.trade.gov.uk>
2024-09-11 01:34:54 +0100

    Replace endpoint with opensearch endpoint

--- src/client/modules/Companies/CompanyActivity/tasks.js
+++ src/client/modules/Companies/CompanyActivity/tasks.js
@@ -61,23 +61,21 @@ export const getCompanyActivities = ({
   dit_participants__team,
 }) =>
   apiProxyAxios
-    .post(
-      `/v4/company/${company}/activity`,
-      {
-        subject,
-        kind,
-        dit_participants__adviser,
-        company,
-        sortby,
-        date_before,
-        date_after,
-        service,
-        was_policy_feedback_provided,
-        policy_areas,
-        policy_issue_types,
-        company_one_list_group_tier,
-        dit_participants__team,
-      },
-      { params: { limit: limit, offset: getPageOffset({ limit, page }) } }
-    )
+    .post(`/v4/search/company-activity`, {
+      limit,
+      offset: getPageOffset({ limit, page }),
+      subject,
+      kind,
+      dit_participants__adviser,
+      company,
+      sortby,
+      date_before,
+      date_after,
+      service,
+      was_policy_feedback_provided,
+      policy_areas,
+      policy_issue_types,
+      company_one_list_group_tier,
+      dit_participants__team,
+    })
     .then(({ data }) => transformResponseToCollection(data))
