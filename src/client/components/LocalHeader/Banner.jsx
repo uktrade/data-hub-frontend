bb5e67749602dd9da6cd5f1e6836d715c409d874
Paul Gain <paul@javexconsulting.com>
2024-09-13 11:43:41 +0100

    Remove announcement banner centering and cleanup

--- src/client/components/LocalHeader/Banner.jsx
+++ src/client/components/LocalHeader/Banner.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
-import { MEDIA_QUERIES, SPACING } from '@govuk-react/constants'
+import { SPACING } from '@govuk-react/constants'
 import styled from 'styled-components'
 
 import { state2props } from './state'
@@ -12,30 +12,13 @@ import {
 } from '../../actions'
 import { MID_BLUE, WHITE } from '../../utils/colours'
 
-const StyledBody = styled('div')`
-  background-color: ${MID_BLUE};
+const Container = styled('div')`
   color: ${WHITE};
-  ${MEDIA_QUERIES.TABLET} {
-    height: 40px;
-  }
-  ${MEDIA_QUERIES.DESKTOP} {
-    height: 40px;
-  }
-`
-
-const StyledDiv = styled('div')`
-  text-align: center;
-  position: relative;
-  ${MEDIA_QUERIES.TABLET} {
-    padding-top: 1.5%;
-  }
-  ${MEDIA_QUERIES.DESKTOP} {
-    padding-top: 0.8%;
-  }
+  background-color: ${MID_BLUE};
+  padding: 10px 0 10px 0;
 `
 
 const StyledTextLink = styled('a')`
-  position: relative;
   color: ${WHITE};
   margin-left: ${SPACING.SCALE_1};
   &:visited,
@@ -46,9 +29,8 @@ const StyledTextLink = styled('a')`
 `
 
 const StyledDismissTextLink = styled('button')`
-  position: relative;
   color: ${WHITE};
-  margin-left: ${SPACING.SCALE_1};
+  margin-left: ${SPACING.SCALE_5};
   background: none;
   border: none;
   padding: 0;
@@ -76,17 +58,15 @@ const Banner = ({
   }
 
   return announcementLink !== latestAnnouncement.link ? (
-    <StyledBody>
-      <StyledDiv data-testid="feed-banner">
-        Update:
-        <StyledTextLink href={latestAnnouncement.link}>
-          {latestAnnouncement.heading}
-        </StyledTextLink>
-        <StyledDismissTextLink onClick={updateLocalStorage}>
-          Dismiss
-        </StyledDismissTextLink>
-      </StyledDiv>
-    </StyledBody>
+    <Container data-testid="feed-banner">
+      Update:
+      <StyledTextLink href={latestAnnouncement.link}>
+        {latestAnnouncement.heading}
+      </StyledTextLink>
+      <StyledDismissTextLink onClick={updateLocalStorage}>
+        Dismiss
+      </StyledDismissTextLink>
+    </Container>
   ) : null
 }
 
