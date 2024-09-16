f42616da7caeeef167cab6c99fb5093ff5856273
Sophie Wenban <s.maclennan@hotmail.co.uk>
2024-09-16 17:12:06 +0100

    Create EYB lead details page

---
+++ src/client/modules/Investments/EYBLeads/EYBLeadDetails.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { DefaultLayout } from '../../../components'
+
+const EYBLeadDetails = () => {
+  return (
+    <DefaultLayout
+      heading={'EYB Lead'}
+      pageTitle={'EYB Lead'}
+      breadcrumbs={[]}
+      useReactRouter={false}
+    >
+      <h1>EYB lead</h1>
+    </DefaultLayout>
+  )
+}
+
+export default EYBLeadDetails
