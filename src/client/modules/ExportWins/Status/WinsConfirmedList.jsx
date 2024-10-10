eb895b2fff68011c0131d63e29b5e9a1d6b8ff56
Paul Gain <paul@javexconsulting.com>
2024-10-10 15:00:34 +0100

    Add confirmed export wins list

---
+++ src/client/modules/ExportWins/Status/WinsConfirmedList.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import Link from '@govuk-react/link'
+
+import ExportWinsResource from '../../../components/Resource/ExportWins'
+import { currencyGBP } from '../../../utils/number-utils'
+import { formatMediumDate } from '../../../utils/date'
+import { CollectionItem } from '../../../components'
+import { sumExportValues } from './utils'
+import { SORT_OPTIONS, WIN_STATUS } from './constants'
+import urls from '../../../../lib/urls'
+
+export const WinsConfirmedList = ({ exportWins = [] }) => {
+  return exportWins.length === 0 ? null : (
+    <ul>
+      {exportWins.map((item) => (
+        <CollectionItem
+          key={item.id}
+          headingText={`${item.name_of_export} to ${item?.country?.name}`}
+          headingUrl={urls.companies.exportWins.editSummary(
+            item.company.id,
+            item.id
+          )}
+          subheading={item.company.name}
+          subheadingUrl={urls.companies.overview.index(item.company.id)}
+          metadata={[
+            {
+              label: 'Contact name:',
+              value: (
+                <Link href={urls.contacts.details(item.company_contacts[0].id)}>
+                  {item.company_contacts[0].name}
+                </Link>
+              ),
+            },
+            {
+              label: 'Total value:',
+              value: currencyGBP(sumExportValues(item)),
+            },
+            {
+              label: 'Date won:',
+              value: formatMediumDate(item.date),
+            },
+            {
+              label: 'Date responded:',
+              value: formatMediumDate(item.customer_response.responded_on),
+            },
+          ]}
+        />
+      ))}
+    </ul>
+  )
+}
+
+export default () => (
+  <ExportWinsResource.Paginated
+    id="export-wins-confirmed"
+    heading="confirmed"
+    shouldPluralize={false}
+    noResults="You don't have any confirmed export wins."
+    payload={{ confirmed: WIN_STATUS.CONFIRMED }}
+    sortOptions={SORT_OPTIONS}
+  >
+    {(page) => <WinsConfirmedList exportWins={page} />}
+  </ExportWinsResource.Paginated>
+)
