d69fbc695adbbfce16d73bf2fd23a70ff5368358
Paul Gain <paul@javexconsulting.com>
2024-10-16 14:07:34 +0100

    Add role tags component tests

---
+++ test/component/cypress/specs/Companies/Export/CompanyExportWins.cy.jsx
@@ -0,0 +1,162 @@
+import React from 'react'
+
+import { CompanyExportWinsList } from '../../../../../../src/client/modules/Companies/CompanyExports/ExportWins'
+import { companyExportWinFaker } from '../../../../../../test/functional/cypress/fakers/company-export-win'
+import { createTestProvider } from '../../provider'
+
+describe('CompanyExportWins', () => {
+  const companyExportWin = companyExportWinFaker()
+
+  it('should conditionally render tags', () => {
+    const createProvider = (exportWinsList) =>
+      createTestProvider({
+        'Export Wins': () => Promise.resolve(exportWinsList),
+        Company: () => Promise.resolve({ id: 123 }),
+        TASK_GET_REMINDER_SUMMARY: () => Promise.resolve(),
+      })
+
+    ;[
+      // The lead_officer id doesn't match the currentAdviserId
+      {
+        exportWinsList: [
+          {
+            ...companyExportWin,
+            lead_officer: {
+              id: '2',
+            },
+          },
+        ],
+        currentAdviserId: '1',
+        shouldRenderTag: false,
+      },
+      {
+        // The lead_officer id matches the currentAdviserId
+        exportWinsList: [
+          {
+            ...companyExportWin,
+            lead_officer: {
+              id: '1',
+            },
+          },
+        ],
+        currentAdviserId: '1',
+        shouldRenderTag: true,
+        role: 'Role: lead officer',
+      },
+      // The team_members array doesn't include the currentAdviserId
+      {
+        exportWinsList: [
+          {
+            ...companyExportWin,
+            team_members: [
+              {
+                id: '1',
+              },
+              {
+                id: '2',
+              },
+              {
+                id: '3',
+              },
+            ],
+          },
+        ],
+        currentAdviserId: '4',
+        shouldRenderTag: false,
+      },
+      {
+        // The team_members array includes the currentAdviserId
+        exportWinsList: [
+          {
+            ...companyExportWin,
+            team_members: [
+              {
+                id: '1',
+              },
+              {
+                id: '2',
+              },
+              {
+                id: '3',
+              },
+            ],
+          },
+        ],
+        currentAdviserId: '1',
+        shouldRenderTag: true,
+        role: 'Role: team member',
+      },
+      // The contributing_advisers array doesn't include the currentAdviserId
+      {
+        exportWinsList: [
+          {
+            ...companyExportWin,
+            contributing_advisers: [
+              {
+                adviser: {
+                  id: '1',
+                },
+              },
+              {
+                adviser: {
+                  id: '2',
+                },
+              },
+              {
+                adviser: {
+                  id: '3',
+                },
+              },
+            ],
+          },
+        ],
+        currentAdviserId: '4',
+        shouldRenderTag: false,
+      },
+      {
+        // The contributing_advisers array includes the currentAdviserId
+        exportWinsList: [
+          {
+            ...companyExportWin,
+            contributing_advisers: [
+              {
+                adviser: {
+                  id: '1',
+                },
+              },
+              {
+                adviser: {
+                  id: '2',
+                },
+              },
+              {
+                adviser: {
+                  id: '3',
+                },
+              },
+            ],
+          },
+        ],
+        currentAdviserId: '1',
+        shouldRenderTag: true,
+        role: 'Role: contributing officer',
+      },
+    ].forEach(({ exportWinsList, currentAdviserId, shouldRenderTag, role }) => {
+      const Provider = createProvider(exportWinsList)
+      cy.mount(
+        <Provider>
+          <CompanyExportWinsList
+            exportWins={exportWinsList}
+            currentAdviserId={currentAdviserId}
+          />
+        </Provider>
+      )
+
+      const assertion = shouldRenderTag ? 'exist' : 'not.exist'
+      cy.get('[data-test="collection-item-tags"]').should(assertion)
+      if (assertion === 'exist') {
+        cy.get('[data-test="collection-item-tag"]').should('have.text', role)
+      }
+    })
+  })
+})
