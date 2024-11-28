f34976d4a85f7a2410c37264219f1a7a9de2cb25
Paul Gain <paul@javexconsulting.com>
2024-11-28 13:22:31 +0000

    Remove the default layout, add spacing between the 'Edit' and 'Delete' buttons/link

--- src/client/modules/ExportPipeline/ExportDetails/index.jsx
+++ src/client/modules/ExportPipeline/ExportDetails/index.jsx
@@ -9,7 +9,7 @@ import { BREAKPOINTS } from '@govuk-react/constants'
 
 import urls from '../../../../lib/urls'
 import { EXPORT_LOADED } from '../../../actions'
-import { DefaultLayout, SummaryTable } from '../../../components'
+import { SummaryTable } from '../../../components'
 import Task from '../../../components/Task'
 import { ID, state2props, TASK_GET_EXPORT_DETAIL } from './state'
 import { format } from '../../../../client/utils/date'
@@ -23,6 +23,7 @@ const StyledSummaryTable = styled(SummaryTable)({
 
 const Container = styled('div')`
   display: flex;
+  gap: 20px;
   align-items: baseline;
   margin-bottom: 30px;
   ${Link} {
@@ -55,163 +56,135 @@ const EstimatedExport = ({
   return <span>Not set</span>
 }
 
-const getBreadcrumbs = (exportItem) => {
-  const defaultBreadcrumbs = [
-    {
-      link: urls.exportPipeline.index(),
-      text: 'Home',
-    },
-  ]
-
-  if (exportItem) {
-    return [...defaultBreadcrumbs, { text: exportItem.title }]
-  }
-
-  return defaultBreadcrumbs
-}
-
 const ExportDetailsForm = ({ exportItem }) => {
   const { exportId } = useParams()
 
   return (
-    <DefaultLayout
-      heading={exportItem ? exportItem.title : ''}
-      headingLink={
-        exportItem && {
-          url: `/companies/${exportItem.company.id}/overview`,
-          text: exportItem.company.name,
-        }
-      }
-      pageTitle="Export details"
-      breadcrumbs={getBreadcrumbs(exportItem)}
-      useReactRouter={false}
+    <Task.Status
+      name={TASK_GET_EXPORT_DETAIL}
+      id={ID}
+      progressMessage="loading export details"
+      startOnRender={{
+        payload: exportId,
+        onSuccessDispatch: EXPORT_LOADED,
+      }}
     >
-      <Task.Status
-        name={TASK_GET_EXPORT_DETAIL}
-        id={ID}
-        progressMessage="loading export details"
-        startOnRender={{
-          payload: exportId,
-          onSuccessDispatch: EXPORT_LOADED,
-        }}
-      >
-        {() => {
-          return (
-            exportItem && (
-              <>
-                <StyledSummaryTable>
-                  <SummaryTable.Row
-                    heading="Export title"
-                    children={exportItem.title}
+      {() => {
+        return (
+          exportItem && (
+            <>
+              <StyledSummaryTable>
+                <SummaryTable.Row
+                  heading="Export title"
+                  children={exportItem.title}
+                />
+                <SummaryTable.Row heading="Owner">
+                  {isEmpty(exportItem.owner)
+                    ? 'Not set'
+                    : exportItem?.owner.name}
+                </SummaryTable.Row>
+                <SummaryTable.ListRow
+                  heading="Team members"
+                  emptyValue="Not set"
+                  hideWhenEmpty={true}
+                  value={exportItem.team_members.map(
+                    transformIdNameToValueLabel
+                  )}
+                ></SummaryTable.ListRow>
+                <SummaryTable.Row
+                  heading="Total estimated export value"
+                  hideWhenEmpty={false}
+                >
+                  <EstimatedExport
+                    estimated_export_value_amount={
+                      exportItem.estimated_export_value_amount
+                    }
+                    estimated_export_value_years={
+                      exportItem.estimated_export_value_years
+                    }
                   />
-                  <SummaryTable.Row heading="Owner">
-                    {isEmpty(exportItem.owner)
-                      ? 'Not set'
-                      : exportItem?.owner.name}
-                  </SummaryTable.Row>
-                  <SummaryTable.ListRow
-                    heading="Team members"
-                    emptyValue="Not set"
-                    hideWhenEmpty={true}
-                    value={exportItem.team_members.map(
-                      transformIdNameToValueLabel
-                    )}
-                  ></SummaryTable.ListRow>
-                  <SummaryTable.Row
-                    heading="Total estimated export value"
-                    hideWhenEmpty={false}
-                  >
-                    <EstimatedExport
-                      estimated_export_value_amount={
-                        exportItem.estimated_export_value_amount
-                      }
-                      estimated_export_value_years={
-                        exportItem.estimated_export_value_years
-                      }
-                    />
-                  </SummaryTable.Row>
-                  <SummaryTable.Row
-                    heading="Estimated date for win"
-                    hideWhenEmpty={false}
-                  >
-                    {isEmpty(exportItem.estimated_win_date)
-                      ? 'Not set'
-                      : format(exportItem.estimated_win_date, 'MMMM yyyy')}
-                  </SummaryTable.Row>
-                  <SummaryTable.Row heading="Status" hideWhenEmpty={false}>
-                    {isEmpty(exportItem.status)
-                      ? 'Not set'
-                      : capitalize(exportItem.status)}
-                  </SummaryTable.Row>
-                  <SummaryTable.Row
-                    heading="Export potential"
-                    hideWhenEmpty={false}
-                  >
-                    {isEmpty(exportItem.export_potential)
-                      ? 'Not set'
-                      : capitalize(exportItem.export_potential)}
-                  </SummaryTable.Row>
-                  <SummaryTable.Row heading="Destination" hideWhenEmpty={false}>
-                    {isEmpty(exportItem.destination_country)
-                      ? 'Not set'
-                      : exportItem.destination_country.name}
-                  </SummaryTable.Row>
-                  <SummaryTable.Row heading="Main sector" hideWhenEmpty={false}>
-                    {isEmpty(exportItem.sector)
-                      ? 'Not set'
-                      : exportItem.sector.name}
-                  </SummaryTable.Row>
-                  <SummaryTable.ListRow
-                    heading="Company contacts"
-                    value={exportItem.contacts.map(transformIdNameToValueLabel)}
-                    emptyValue="Not set"
-                    hideWhenEmpty={false}
-                  />
-                  <SummaryTable.Row
-                    heading="Exporter experience"
-                    hideWhenEmpty={false}
-                  >
-                    {isEmpty(exportItem.exporter_experience)
-                      ? 'Not set'
-                      : exportItem.exporter_experience.name}
-                  </SummaryTable.Row>
-                  <SummaryTable.Row heading="Notes" hideWhenEmpty={false}>
-                    {isEmpty(exportItem.notes) ? 'Not set' : exportItem.notes}
-                  </SummaryTable.Row>{' '}
-                </StyledSummaryTable>
-                <Container>
-                  <Button
-                    as={'a'}
-                    href={urls.exportPipeline.edit(exportId)}
-                    buttonColour={GREY_3}
-                    buttonTextColour={BLACK}
-                    data-test="edit-export-details-button"
-                  >
-                    Edit
-                  </Button>
-                  <Button
-                    as={'a'}
-                    href={urls.companies.exportWins.createFromExport(
-                      exportItem.company.id,
-                      exportId
-                    )}
-                    data-test="convert-to-export-win"
-                  >
-                    Convert to export win
-                  </Button>
-                  <Link
-                    href={urls.exportPipeline.delete(exportId)}
-                    data-test="delete-export-details-button"
-                  >
-                    Delete
-                  </Link>
-                </Container>
-              </>
-            )
+                </SummaryTable.Row>
+                <SummaryTable.Row
+                  heading="Estimated date for win"
+                  hideWhenEmpty={false}
+                >
+                  {isEmpty(exportItem.estimated_win_date)
+                    ? 'Not set'
+                    : format(exportItem.estimated_win_date, 'MMMM yyyy')}
+                </SummaryTable.Row>
+                <SummaryTable.Row heading="Status" hideWhenEmpty={false}>
+                  {isEmpty(exportItem.status)
+                    ? 'Not set'
+                    : capitalize(exportItem.status)}
+                </SummaryTable.Row>
+                <SummaryTable.Row
+                  heading="Export potential"
+                  hideWhenEmpty={false}
+                >
+                  {isEmpty(exportItem.export_potential)
+                    ? 'Not set'
+                    : capitalize(exportItem.export_potential)}
+                </SummaryTable.Row>
+                <SummaryTable.Row heading="Destination" hideWhenEmpty={false}>
+                  {isEmpty(exportItem.destination_country)
+                    ? 'Not set'
+                    : exportItem.destination_country.name}
+                </SummaryTable.Row>
+                <SummaryTable.Row heading="Main sector" hideWhenEmpty={false}>
+                  {isEmpty(exportItem.sector)
+                    ? 'Not set'
+                    : exportItem.sector.name}
+                </SummaryTable.Row>
+                <SummaryTable.ListRow
+                  heading="Company contacts"
+                  value={exportItem.contacts.map(transformIdNameToValueLabel)}
+                  emptyValue="Not set"
+                  hideWhenEmpty={false}
+                />
+                <SummaryTable.Row
+                  heading="Exporter experience"
+                  hideWhenEmpty={false}
+                >
+                  {isEmpty(exportItem.exporter_experience)
+                    ? 'Not set'
+                    : exportItem.exporter_experience.name}
+                </SummaryTable.Row>
+                <SummaryTable.Row heading="Notes" hideWhenEmpty={false}>
+                  {isEmpty(exportItem.notes) ? 'Not set' : exportItem.notes}
+                </SummaryTable.Row>{' '}
+              </StyledSummaryTable>
+              <Container>
+                <Button
+                  as={'a'}
+                  href={urls.exportPipeline.edit(exportId)}
+                  buttonColour={GREY_3}
+                  buttonTextColour={BLACK}
+                  data-test="edit-export-details-button"
+                >
+                  Edit
+                </Button>
+                <Button
+                  as={'a'}
+                  href={urls.companies.exportWins.createFromExport(
+                    exportItem.company.id,
+                    exportId
+                  )}
+                  data-test="convert-to-export-win"
+                >
+                  Convert to export win
+                </Button>
+                <Link
+                  href={urls.exportPipeline.delete(exportId)}
+                  data-test="delete-export-details-button"
+                >
+                  Delete
+                </Link>
+              </Container>
+            </>
           )
-        }}
-      </Task.Status>
-    </DefaultLayout>
+        )
+      }}
+    </Task.Status>
   )
 }
 
