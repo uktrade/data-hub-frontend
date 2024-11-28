32bff380055c729852541d52980cff93d425ed31
Paul Gain <paul@javexconsulting.com>
2024-11-28 17:32:04 +0000

    Exclude path from A11y tests

--- test/a11y/cypress/config/urlTestExclusions.js
+++ test/a11y/cypress/config/urlTestExclusions.js
@@ -54,6 +54,7 @@ export const urlTestExclusions = [
     url: '/investments/projects/:projectId/edit-associated/:associatedProjectId',
   },
   { url: '/export/:exportId/details' },
+  { url: '/export/:exportId/interactions' },
   { url: '/omis/:orderId/edit/lead-adviser/:adviserId' },
   { url: '/interactions/ess/:essInteractionId/details' },
   {
