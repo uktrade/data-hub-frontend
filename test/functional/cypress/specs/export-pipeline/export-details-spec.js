9c611b4d3d5529ae1d94b07b755f6f46d5b10d0e
Paul Gain <paul@javexconsulting.com>
2024-11-28 13:25:33 +0000

    Wait for the /v4/export/<export-id> endpoint response before testing

--- test/functional/cypress/specs/export-pipeline/export-details-spec.js
+++ test/functional/cypress/specs/export-pipeline/export-details-spec.js
@@ -22,6 +22,7 @@ describe('Export Details summary ', () => {
         body: exportItem,
       }).as('getExportItemApiRequest')
       cy.visit(`/export/${exportItem.id}/details`)
+      cy.wait('@getExportItemApiRequest')
     })
 
     it('should render breadcrumbs', () => {
