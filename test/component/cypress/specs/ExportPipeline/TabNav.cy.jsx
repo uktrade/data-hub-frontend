c6f6cde1bfb65dbbf0be68b28fd831f48afd3ea1
Paul Gain <paul@javexconsulting.com>
2024-12-05 12:06:01 +0000

    Rename ExportTabNav to Export

--- test/component/cypress/specs/ExportPipeline/TabNav.cy.jsx
+++ test/component/cypress/specs/ExportPipeline/TabNav.cy.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import ExportTabNav from '../../../../../src/client/modules/ExportPipeline/ExportTabNav'
+import Export from '../../../../../src/client/modules/ExportPipeline/Export'
 import { assertBreadcrumbs } from '../../../../functional/cypress/support/assertions'
 import { assertLocalNav } from '../../../../end-to-end/cypress/support/assertions'
 import urls from '../../../../../src/lib/urls'
@@ -16,7 +16,7 @@ const exportProject = {
 
 describe('Export project tab navigation', () => {
   it('should render the breadcrumbs for details', () => {
-    cy.mountWithProvider(<ExportTabNav />, {
+    cy.mountWithProvider(<Export />, {
       initialPath: '/export/1/details',
       tasks: {
         Export: () => exportProject,
@@ -29,7 +29,7 @@ describe('Export project tab navigation', () => {
   })
 
   it('should render the same breadcrumbs for interactions', () => {
-    cy.mountWithProvider(<ExportTabNav />, {
+    cy.mountWithProvider(<Export />, {
       initialPath: '/export/1/interactions/',
       tasks: {
         Export: () => exportProject,
@@ -42,7 +42,7 @@ describe('Export project tab navigation', () => {
   })
 
   it('should render a company link and page heading', () => {
-    cy.mountWithProvider(<ExportTabNav />, {
+    cy.mountWithProvider(<Export />, {
       initialPath: '/export/1/details',
       tasks: {
         Export: () => exportProject,
@@ -57,7 +57,7 @@ describe('Export project tab navigation', () => {
   })
 
   it('should render two tabs: Project details and Interactions', () => {
-    cy.mountWithProvider(<ExportTabNav />)
+    cy.mountWithProvider(<Export />)
     cy.get('[role="tablist"]').should('exist')
     cy.get('[role="tab"]').as('tabItems')
     assertLocalNav('@tabItems', ['Project details', 'Interactions'])
