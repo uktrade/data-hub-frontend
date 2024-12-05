ef517e58e7502ef1f2a11a00e5ec3ed1f2a81ae8
Paul Gain <paul@javexconsulting.com>
2024-12-05 12:06:01 +0000

    Rename ExportTabNav to Export

--- src/client/routes.js
+++ src/client/routes.js
@@ -17,7 +17,7 @@ import {
   ExportFormEdit,
 } from './modules/ExportPipeline/ExportForm'
 import ExportFormDelete from './modules/ExportPipeline/ExportDelete'
-import ExportProjectTabNav from './modules/ExportPipeline/ExportTabNav'
+import Export from './modules/ExportPipeline/Export'
 import ExportWinsTabNav from './modules/ExportWins/Status/ExportWinsTabNav'
 import { CreateExportWin, EditExportWin } from './modules/ExportWins/Form'
 import ExportWinsRedirect from './modules/ExportWins/Status/Redirect'
@@ -731,7 +731,7 @@ function Routes() {
       path: '/export/:exportId/details',
       element: (
         <ProtectedRoute module={'datahub:companies'}>
-          <ExportProjectTabNav />
+          <Export />
         </ProtectedRoute>
       ),
     },
@@ -739,7 +739,7 @@ function Routes() {
       path: '/export/:exportId/interactions',
       element: (
         <ProtectedRoute module={'datahub:companies'}>
-          <ExportProjectTabNav />
+          <Export />
         </ProtectedRoute>
       ),
     },
