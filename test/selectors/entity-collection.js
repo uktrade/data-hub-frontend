7e65e00a11b159409495b5311e277e525e8b3718
Peter Hudec <peterhudec@peterhudec.com>
2024-12-10 16:10:48 +0000

    Fixed outdated tests

--- test/selectors/entity-collection.js
+++ test/selectors/entity-collection.js
@@ -1,5 +1,4 @@
 module.exports = {
-  addAttendee: '[data-test="Add attendee"]',
   addProposition: '[data-test="add-collection-item-button"]',
   collection: '.c-collection',
   sortBy: '[name="sortBy"] > select',
