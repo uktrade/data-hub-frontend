d7e0973793419311791d8c368c07346fe90a4449
Edmond De Los Reyes <edmond.delosreyes@digital.trade.gov.uk>
2024-12-12 11:36:23 +0000

    Layout test scenarios - Sort export project interaction list

---
+++ test/functional/cypress/specs/export-pipeline/export-interaction-list-spec.js
@@ -0,0 +1,12 @@
+describe('Export project interaction sort', () => {
+  // TODO: Data assertions
+  context('Default sort', () => {
+    it('should render default sort', () => {})
+    it('should render all sort options', () => {})
+  })
+  context('User sort options', () => {
+    it('should sort by "Recently created"', () => {})
+    it('should sort by "Company name A-Z"', () => {})
+    it('should sort by "Subject A-Z"', () => {})
+  })
+})
