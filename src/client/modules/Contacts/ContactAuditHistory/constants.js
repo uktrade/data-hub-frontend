c67803727d4e4e66cdf9f834e456171feb70580d
Chris Hopkins <chris.hopkins@madetech.com>
2024-12-20 11:53:39 +0000

    exclude consent from audit history

--- src/client/modules/Contacts/ContactAuditHistory/constants.js
+++ src/client/modules/Contacts/ContactAuditHistory/constants.js
@@ -1,4 +1,9 @@
-export const EXCLUDED_FIELDS = ['archived_by', 'archived_on', 'valid_email']
+export const EXCLUDED_FIELDS = [
+  'archived_by',
+  'archived_on',
+  'valid_email',
+  'consent_data',
+]
 
 export const CONTACT_FIELD_NAME_TO_LABEL_MAP = {
   address_1: 'Address line 1',
