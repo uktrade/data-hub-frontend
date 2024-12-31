4109b9d1e1a08438a12ae4a0b868bbd9eee8b63a
Paul Gain <paul@javexconsulting.com>
2024-12-31 13:39:39 +0000

    Swap out transformValueForAPI for formatDateWithYearMonth

--- src/client/modules/Tasks/TaskForm/validators.js
+++ src/client/modules/Tasks/TaskForm/validators.js
@@ -1,9 +1,11 @@
 import { isFuture } from 'date-fns'
 
-import { transformValueForAPI } from '../../../utils/date'
+import { parseDateWithYearMonth } from '../../../utils/date'
 
-export const validateIfDateInFuture = (values) =>
-  isFuture(transformValueForAPI(values)) ? null : 'Enter a date in the future'
+export const validateIfDateInFuture = ({ year, month, day }) =>
+  isFuture(parseDateWithYearMonth(year, month, day))
+    ? null
+    : 'Enter a date in the future'
 
 export const validateDaysRange = (value) =>
   !value || value < 1 || value > 365 ? 'Enter a number between 1 and 365' : null
