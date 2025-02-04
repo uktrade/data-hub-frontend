eb9a768d6f4ece0e5550a9164996ee94b4937950
Dean Elliott <dean.elliott@digital.trade.gov.uk>
2025-02-04 15:04:10 +0000

    Use util function to convert python variables to camelCase

--- src/client/modules/Events/StovaEventDetails/transformers.js
+++ src/client/modules/Events/StovaEventDetails/transformers.js
@@ -10,55 +10,29 @@ registrationStatusCounts
 import { compact } from 'lodash'
 
 import { formatStartAndEndDate } from '../../../components/ActivityFeed/activities/date'
+import { deepKeysToCamelCase } from '../../../utils'
 import { getDifferenceInDays } from '../../../utils/date'
 import { transformDateStringToDateObject } from '../../../transformers'
 
-export const transformResponeToStovaEventDetails = ({
-  stova_event_id,
-  // url,
-  // city,
-  // code,
-  name,
-  // state,
-  // country,
-  // timezone,
-  // description,
-  // max_reg,
-  start_date,
-  end_date,
-  // folder_id,
-  // live_date,
-  // close_date,
-  // created_by,
-  // price_type,
-  // modified_by,
-  // contact_info,
-  // created_date,
-  location_city,
-  location_name,
-  // modified_date,
-  // client_contact,
-  // location_state,
-  // default_language,
-  location_country,
-  // approval_required,
-  location_address1,
-  location_address2,
-  // location_address3,
-  location_postcode,
-  // standard_currency,
-}) => ({
-  name,
-  stovaEventId: stova_event_id,
-  eventDate: formatStartAndEndDate(start_date, end_date),
-  upcomingEvent:
-    getDifferenceInDays(transformDateStringToDateObject(end_date)) > 0,
-  location: location_name,
-  fullAddress: compact([
-    location_address1,
-    location_address2,
-    location_city,
-    location_postcode,
-    location_country,
-  ]),
-})
+export const transformResponeToStovaEventDetails = (stovaEvent) => {
+  const camelCaseResponse = deepKeysToCamelCase(stovaEvent)
+  return {
+    ...camelCaseResponse,
+    eventDate: formatStartAndEndDate(
+      camelCaseResponse.startDate,
+      camelCaseResponse.endDate
+    ),
+    upcomingEvent:
+      getDifferenceInDays(
+        transformDateStringToDateObject(camelCaseResponse.endDate)
+      ) > 0,
+    location: camelCaseResponse.locationName,
+    fullAddress: compact([
+      camelCaseResponse.locationAddress1,
+      camelCaseResponse.locationAddress2,
+      camelCaseResponse.locationCity,
+      camelCaseResponse.locationPostcode,
+      camelCaseResponse.locationCountry,
+    ]),
+  }
+}
