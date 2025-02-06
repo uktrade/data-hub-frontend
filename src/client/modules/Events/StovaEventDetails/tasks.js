51038b3086bf627066bf85536531b7f75cf6c74a
Dean Elliott <dean.elliott@digital.trade.gov.uk>
2025-02-06 08:39:15 +0000

    Remove tasks and transformer related files as using Resource component

--- src/client/modules/Events/StovaEventDetails/tasks.js
+++
@@ -1,8 +0,0 @@
-import { apiProxyAxios } from '../../../components/Task/utils'
-import { transformResponeToStovaEventDetails } from './transformers'
-
-export const getStovaEventDetails = (stovaEventId) => {
-  apiProxyAxios
-    .get(`/v4/company-activity/stova-events/${stovaEventId}`)
-    .then(({ data }) => transformResponeToStovaEventDetails(data))
-}
