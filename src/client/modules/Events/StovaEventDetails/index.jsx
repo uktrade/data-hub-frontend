3ba6a0360efe8777d91c143024fde7946cdb8be8
Dean Elliott <dean.elliott@digital.trade.gov.uk>
2025-02-06 09:46:00 +0000

    Move shared sort options to constant folder

--- src/client/modules/Events/StovaEventDetails/index.jsx
+++ src/client/modules/Events/StovaEventDetails/index.jsx
@@ -4,6 +4,7 @@ import { H3, Link } from 'govuk-react'
 import styled from 'styled-components'
 import { useParams } from 'react-router-dom'
 
+import { ATTENDEE_SORT_OPTIONS } from '../constants'
 import { formatDate, DATE_FORMAT_FULL } from '../../../utils/date-utils'
 import urls from '../../../../lib/urls'
 import CollectionItem from '../../../components/CollectionList/CollectionItem'
@@ -16,17 +17,6 @@ import { StovaEventResource } from '../../../components/Resource'
 const StyledSummaryTable = styled(SummaryTable)({
   marginTop: 0,
 })
-
-const ATTENDEE_SORT_OPTIONS = [
-  { name: 'Last name: A-Z', value: 'last_name_of_first_contact' },
-  { name: 'Last name: Z-A', value: '-last_name_of_first_contact' },
-  { name: 'First name: A-Z', value: 'first_name_of_first_contact' },
-  { name: 'First name: Z-A', value: '-first_name_of_first_contact' },
-  { name: 'Company name: A-Z', value: 'company__name' },
-  { name: 'Company name: Z-A', value: '-company__name' },
-  { name: 'Recently added', value: '-created_on' },
-  { name: 'Least recently added', value: 'created_on' },
-]
 
 const Attendees = ({ datahubEventId }) => (
   <div>
