e36b05dfa574a6cc59e14d410d90c26c0302f953
Bau Nguyen <bauvannguyen123@gmail.com>
2025-02-11 13:38:37 +0000

    Add spec test for stova attendees

--- test/sandbox/routes/v4/company-activity/company-activity.js
+++ test/sandbox/routes/v4/company-activity/company-activity.js
@@ -1,12 +1,8 @@
 import stovaEvent from '../../../fixtures/v4/company-activity/stova-event.json' assert { type: 'json' }
 
-function _getCompanyActivityById(res, req) {
-  var companyActivity = {
+export const getCompanyActivityById = (req, res) => {
+  const companyActivity = {
     '2df6560a-a7cc-49cb-a257-c4e109204eff': stovaEvent,
   }
   return res.status(200).json(companyActivity[req.params.stovaEventId])
-}
-
-export const getCompanyActivityById = function (req, res) {
-  return _getCompanyActivityById(res, req)
 }
