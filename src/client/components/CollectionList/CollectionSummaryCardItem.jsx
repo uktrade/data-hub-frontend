e6d98fab9da04241d5989aabc7bd484e95c27278
Stuart Mindt <stuart.mindt@madetech.com>
2025-03-10 12:34:10 +0000

    Removing key issues from console

--- src/client/components/CollectionList/CollectionSummaryCardItem.jsx
+++ src/client/components/CollectionList/CollectionSummaryCardItem.jsx
@@ -4,32 +4,33 @@ import Link from '@govuk-react/link'
 
 const CollectionSummaryCardItem = ({ title, links, rows }) => (
   <div
+    key={`collection-summary-card-item-${title}`}
     data-test="collection-item"
     className="govuk-summary-card govuk-!-margin-top-6"
   >
-    <div class="govuk-summary-card__title-wrapper">
-      <h2 class="govuk-summary-card__title">{title}</h2>
+    <div className="govuk-summary-card__title-wrapper">
+      <h2 className="govuk-summary-card__title">{title}</h2>
       <div className="govuk-summary-card__title-actions">
         {links &&
           links.length > 0 &&
           links.map((link, index) => (
-            <>
+            <span key={`link-${index}`}>
               <Link href={link.url} {...link.attrs}>
                 {link.text}
               </Link>
               {index < links.length - 1 && ' | '}
-            </>
+            </span>
           ))}
       </div>
     </div>
 
-    <div class="govuk-summary-card__content">
-      <dl class="govuk-summary-list">
+    <div className="govuk-summary-card__content">
+      <dl className="govuk-summary-list">
         {rows.map((row, index) => (
           <div
             data-test="metadata"
             className="govuk-summary-list__row"
-            key={index}
+            key={`meta-data-row-item-${index}`}
           >
             <dt className="govuk-summary-list__key">{row.label}</dt>
             <dd className="govuk-summary-list__value">{row.value}</dd>
