2febf9ad6dd52110778d9616b19895839eda592a
Stuart Mindt <stuart.mindt@madetech.com>
2025-03-12 13:48:31 +0000

    Removing previous change

--- test/end-to-end/cypress/specs/DA/local-nav-spec.js
+++ test/end-to-end/cypress/specs/DA/local-nav-spec.js
@@ -21,7 +21,6 @@ describe('DA Permission', () => {
         'Overview',
         'Contacts',
         'Details',
-        'Files',
         'Account management',
         'Investment',
         'Orders',
@@ -42,7 +41,6 @@ describe('DA Permission', () => {
       assertLocalNav(selectors.nav.headerNav, [
         'Companies',
         'Contacts',
-        'Files',
         'Investments',
         'Orders',
         'Market access',
@@ -73,7 +71,6 @@ describe('DA Permission', () => {
     it('should display DA only tabs', () => {
       assertLocalReactNav('[data-test=local-nav] > ul', [
         'Details',
-        'Files',
         'Audit history',
       ])
     })
@@ -93,7 +90,6 @@ describe('DA Permission', () => {
         'Project details',
         'Project team',
         'Tasks',
-        'Files',
         'Interactions',
         'Evaluations',
         'Propositions',
