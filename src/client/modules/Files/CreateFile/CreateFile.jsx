94914570635a3985bd4ebc2bff1d2731f5f30415
Stuart Mindt <stuart.mindt@madetech.com>
2025-03-12 10:36:31 +0000

    CLS2-1301 Enable adding of sharepoint file links to companies

---
+++ src/client/modules/Files/CreateFile/CreateFile.jsx
@@ -0,0 +1,133 @@
+import React from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { H3 } from 'govuk-react'
+
+import {
+  DefaultLayout,
+  FieldInput,
+  Form,
+  FormLayout,
+} from '../../../components'
+import { WEBSITE_REGEX } from '../../../../apps/companies/apps/add-company/client/constants'
+import { transformFileForApi } from './transformers'
+import urls from '../../../../lib/urls'
+import { CompanyResource } from '../../../components/Resource'
+
+import { FORM_LAYOUT } from '../../../../common/constants'
+import { TASK_CREATE_FILE } from './state'
+import {
+  DOCUMENT_TYPES,
+  RELATED_OBJECT_TYPES,
+} from '../CollectionList/constants'
+
+const CompanyName = ({ id }) => (
+  <CompanyResource.Inline id={id}>
+    {(company) => company.name}
+  </CompanyResource.Inline>
+)
+
+const websiteValidator = (value) =>
+  value && !WEBSITE_REGEX.test(value)
+    ? 'You must enter a valid SharePoint share link'
+    : null
+
+const documentTypeText = {
+  sharePoint: 'Add a new SharePoint link',
+}
+
+const SharePointForm = ({ relatedObjectId }) => {
+  return (
+    <Form
+      id="add-sharepoint-link-form"
+      submissionTaskName={TASK_CREATE_FILE}
+      analyticsFormName="addSharePointLink"
+      redirectTo={() => urls.companies.files(relatedObjectId)}
+      flashMessage={() => 'SharePoint link added successfully'}
+      submitButtonLabel="Add SharePoint link"
+      cancelButtonLabel="Cancel"
+      cancelRedirectTo={() => urls.companies.files(relatedObjectId)}
+      transformPayload={(values) =>
+        transformFileForApi({
+          relatedObjectId,
+          relatedObjectType: RELATED_OBJECT_TYPES.COMPANY,
+          documentType: DOCUMENT_TYPES.SHAREPOINT.type,
+          values,
+        })
+      }
+    >
+      <FieldInput
+        label="SharePoint share link url"
+        name="url"
+        type="url"
+        validate={websiteValidator}
+        initialValue=""
+        required="You must enter a SharePoint share link"
+        hint="Paste in the share link from the Sharepoint file. See how to share
+          SharePoint files and folders (opens in new tab) for information on
+          generating this."
+      />
+
+      <FieldInput
+        label="Link title (Optional)"
+        name="title"
+        type="text"
+        initialValue=""
+        hint="A short descriptive title to help others identify the file and it's
+          use"
+      />
+
+      <H3>Access requests</H3>
+      <p>
+        Access to this file will be handled via SharePoint permissions. People
+        who do not have access will need to request file access via SharePoint.
+      </p>
+    </Form>
+  )
+}
+
+const CreateFile = () => {
+  const [searchParams] = useSearchParams()
+  const relatedObjectId = searchParams.get('related_object_id')
+  const relatedObjectType = searchParams.get('related_object_type')
+  const documentType = searchParams.get('document_type')
+
+  const breadcrumbs = [{ link: '/', text: 'Home' }]
+  let pageTitle = null
+  let heading = null
+
+  if (relatedObjectType === RELATED_OBJECT_TYPES.COMPANY) {
+    pageTitle = `${DOCUMENT_TYPES.SHAREPOINT.label} - Files - ${(<CompanyName id={relatedObjectId} />)} - Companies`
+    heading = DOCUMENT_TYPES.SHAREPOINT.label
+    breadcrumbs.push(
+      { link: urls.companies.index(), text: 'Companies' },
+      {
+        link: urls.companies.detail(relatedObjectId),
+        text: <CompanyName id={relatedObjectId} />,
+      },
+      {
+        link: urls.companies.files(relatedObjectId),
+        text: 'Files',
+      },
+      {
+        text:
+          documentType === DOCUMENT_TYPES.SHAREPOINT.type
+            ? documentTypeText.sharePoint
+            : 'Empty',
+      }
+    )
+  }
+
+  return (
+    <DefaultLayout
+      pageTitle={pageTitle}
+      heading={heading}
+      breadcrumbs={breadcrumbs}
+    >
+      <FormLayout setWidth={FORM_LAYOUT.TWO_THIRDS}>
+        <SharePointForm relatedObjectId={relatedObjectId} />
+      </FormLayout>
+    </DefaultLayout>
+  )
+}
+
+export default CreateFile
