aab81abcc5ca018786541bc5500766d2a282246d
Dean Elliott <dean.elliott@digital.trade.gov.uk>
2025-04-10 17:41:50 +0100

    Add htmx to React

--- src/client/modules/Companies/CollectionListHtmx/index.jsx
+++ src/client/modules/Companies/CollectionListHtmx/index.jsx
@@ -5,31 +5,32 @@ import { DefaultLayout } from '../../../components'
 function CompaniesCollectionHtmx() {
   const [error] = useState(null)
   const [isLoaded, setIsLoaded] = useState(false)
-  const [html, setHTML] = useState({ __html: '' })
 
   useEffect(() => {
     fetch('/api-proxy/v4/company/htmx')
       .then((res) => res.text())
       .then((html) => {
         setIsLoaded(true)
-        setHTML({ __html: html })
+        const target = document.getElementById('target')
+        // Using state prevents htmx finding the htmx, so innerHTML required.
+        target.innerHTML = html
+        // Required to tell htmx to look for newly added htmx
+        window.htmx.process(target)
       })
+      .then(() => {})
   }, [])
 
   if (error) {
     return <div>Error: {error.message}</div>
-  } else if (!isLoaded) {
-    return <div>Loading...</div>
-  } else {
-    return (
-      <DefaultLayout heading="Companies HTMX" pageTitle="Companies HTMX">
-        <div id="target">
-          <h1>Hello</h1>
-          <div dangerouslySetInnerHTML={html} />
-        </div>
-      </DefaultLayout>
-    )
   }
+  return (
+    <DefaultLayout heading="Companies HTMX" pageTitle="Companies HTMX">
+      {/* Where to put HTML response from API for the filters */}
+      <div id="target">{isLoaded ? <div /> : <div>Loading...</div>}</div>
+      {/* Where to put the HTML response companies list */}
+      <div id="resultTarget"></div>
+    </DefaultLayout>
+  )
 }
 
 export default CompaniesCollectionHtmx
