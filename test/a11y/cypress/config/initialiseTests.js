3b89dc29c353dbe370868c2490225fafa5fc8233
Marijn Kampf <marijn.kampf@digital.trade.gov.uk>
2025-04-10 14:08:21 +0100

    Added optional functionality to initialise tests with custom fakers / intercept / path.
    Proof of concept with /files/.../delete
    
    Co-authored-by: Claudia Gonzalez-Casales <ClaudiaGC1339@users.noreply.github.com>

---
+++ test/a11y/cypress/config/initialiseTests.js
@@ -0,0 +1,32 @@
+import {
+  DOCUMENT_TYPES,
+  RELATED_OBJECT_TYPES,
+} from '../../../../src/client/modules/Files/CollectionList/constants'
+import urls from '../../../../src/lib/urls'
+import { genericDocumentFaker } from '../../../functional/cypress/fakers/generic-documents'
+import { testIdentityNumbers } from './testIdentityNumbers'
+
+export const initialiseTests = {
+  [`/files/${testIdentityNumbers[':fileId']}/delete`]: () => {
+    const genericDocument = genericDocumentFaker({
+      id: testIdentityNumbers[':fileId'],
+      related_object_id: testIdentityNumbers[':companyId'],
+      related_object: {
+        id: testIdentityNumbers[':companyId'],
+      },
+    })
+
+    cy.intercept(
+      'GET',
+      `/api-proxy/v4/document/${genericDocument.id}`,
+      genericDocument
+    ).as('genericDocumentRequest')
+
+    // Return URL with additional parameters
+    return urls.companies.files.delete(genericDocument.id, {
+      related_object_id: testIdentityNumbers[':companyId'],
+      related_object_type: RELATED_OBJECT_TYPES.COMPANY,
+      document_type: DOCUMENT_TYPES.SHAREPOINT.type,
+    })
+  },
+}
--- test/a11y/cypress/specs/e2e/all-accessibility.spec.js
+++ test/a11y/cypress/specs/e2e/all-accessibility.spec.js
@@ -1,14 +1,21 @@
-import urls from '../../../../../src/lib/urls'
-import { urlTestExclusions } from '../../config/urlTestExclusions'
-import { cleanseArrayOfUrls, createArrayOfUrls } from '../../config/utils'
+import urls from '../../../../src/lib/urls'
+import { initialiseTests } from '../config/initialiseTests'
+import { urlTestExclusions } from '../config/urlTestExclusions'
+import { cleanseArrayOfUrls, createArrayOfUrls } from '../config/utils'
 
 const arrayOfUrls = createArrayOfUrls(urls)
 
-const filteredArrayOfUrls = cleanseArrayOfUrls(arrayOfUrls, urlTestExclusions)
+let filteredArrayOfUrls = cleanseArrayOfUrls(arrayOfUrls, urlTestExclusions)
+// TODO remove limited tests
+filteredArrayOfUrls = [filteredArrayOfUrls[12], filteredArrayOfUrls[11]]
 
 filteredArrayOfUrls.map((path) => {
   describe(`${path}`, () => {
     before(() => {
+      if (initialiseTests[path]) {
+        path = initialiseTests[path]()
+      }
+
       cy.visit(path, { timeout: 20000 })
       // Wait until page has loaded first
       cy.initA11y()
