c0cfabaa4d74dc4a838b60c7c62530c5777650df
Marijn Kampf <marijn.kampf@digital.trade.gov.uk>
2025-04-15 12:34:53 +0100

    WIP

--- test/a11y/cypress/config/urlTestExclusions.js
+++ test/a11y/cypress/config/urlTestExclusions.js
@@ -4,6 +4,7 @@ export const urlTestExclusions = [
   { url: '/companies/:companyId/investments/large-capital-profile' },
   { url: '/search/' },
   { url: '/search/:searchPath?' },
+  { url: '/companies/:companyId/overview' },
   // 404 errors and or no sandbox data available
   { url: '/companies/:companyId/audit' },
   { url: '/companies/:companyId/archive' },
