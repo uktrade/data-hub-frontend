69ebd00ac7f6171e59306f2332e1a86de00a2513
Dean Elliott <dean.elliott@digital.trade.gov.uk>
2025-04-17 16:15:52 +0100

    Change h3 header to be h2 for accessibility order but still styled like h3 Update tests to target h2 instead of h3

--- test/functional/cypress/specs/dashboard/data-hub-feed-spec.js
+++ test/functional/cypress/specs/dashboard/data-hub-feed-spec.js
@@ -14,7 +14,7 @@ describe('Dashboard - Data Hub feed', () => {
     })
 
     it('Should contain a header and no updates available text', () => {
-      cy.get('@dataHubFeed').find('h3').should('have.text', "What's new?")
+      cy.get('@dataHubFeed').find('h2').should('have.text', "What's new?")
 
       cy.get('@dataHubFeed')
         .find('p')
@@ -45,7 +45,7 @@ describe('Dashboard - Data Hub feed', () => {
     })
 
     it('should display only one feed item', () => {
-      cy.get('@dataHubFeed').find('h3').should('have.text', "What's new?")
+      cy.get('@dataHubFeed').find('h2').should('have.text', "What's new?")
 
       cy.get('@dataHubFeed')
         .find('[data-test="data-hub-feed-link-0"]')
